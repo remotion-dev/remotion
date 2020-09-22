d97ee92bad16340d82b0a9f6aa81d6dc68eef59f
Jonny Burger <jonathanburger11@gmail.com>
2020-09-22 15:29:44 +0200

    tiles example
    Former-commit-id: a0b7b696e08f26e8a32f80e3d4023c413b28b2d4

---
+++ packages/example/src/Tiles/Tile.tsx
@@ -0,0 +1,54 @@
+import {spring, useCurrentFrame, useVideoConfig} from '@remotion/core';
+import React from 'react';
+
+const size = 200;
+
+export const Tile: React.FC<{
+	index: number;
+}> = ({index}) => {
+	const videoConfig = useVideoConfig();
+	const frame = useCurrentFrame();
+	const springConfig = {
+		damping: 100,
+		mass: 0.5,
+		stiffness: 10,
+		restSpeedThreshold: 0.00001,
+		restDisplacementThreshold: 0.0001,
+		fps: videoConfig.fps,
+		frame,
+		velocity: 2,
+	};
+	const scale = spring({
+		...springConfig,
+		velocity: 0,
+		from: 0,
+		to: 1,
+	});
+	const rotate = spring({
+		...springConfig,
+		velocity: 0,
+		from: 0,
+		to: 1,
+	});
+	return (
+		<div
+			style={{
+				height: size,
+				width: size,
+				position: 'absolute',
+				zIndex: 1000 - index,
+				left: videoConfig.width / 2 - size / 2,
+				top: videoConfig.height / 2 - size / 2,
+				borderRadius: 20,
+				background: 'radial-gradient(ellipse at bottom, white, #eee)',
+				boxShadow: '0 0 3px rgba(0, 0, 0, 0.1)',
+				transform: ` rotateZ(${index * 8 * rotate}deg) translateY(${
+					index * 15
+				}px) scale(${scale}`,
+				justifyContent: 'center',
+				alignItems: 'center',
+				display: 'flex',
+			}}
+		></div>
+	);
+};
