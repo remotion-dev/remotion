a90b20e28b51a7f43377b77207c3e8bc54f6b511
Jonny Burger <jonathanburger11@gmail.com>
2020-10-11 23:10:36 +0200

    circles
    Former-commit-id: 8621747f0baef3828e5923d350b04d576c3a1edb

--- packages/example/src/RealStickers/index.tsx
+++ packages/example/src/RealStickers/index.tsx
@@ -1,10 +1,68 @@
-import {interpolate, registerVideo, useVideoConfig} from '@remotion/core';
+import {
+	interpolate,
+	registerVideo,
+	useCurrentFrame,
+	useVideoConfig,
+} from '@remotion/core';
 import React from 'react';
-import {seeds} from '../seeds';
 import {output} from './data';
+
+const svgPath = require('svg-path-properties');
+
+const spacePerSticker = 270;
+const stickersInSmallestCircle = 7;
+const stickerSize = 180;
 
 export const RealStickers = () => {
 	const videoConfig = useVideoConfig();
+	const _cData = (function () {
+		let data: {
+			cx: number;
+			cy: number;
+			rx: number;
+			ry: number;
+			stickersLength: number;
+		}[] = [];
+		const circumferenceNeededForSmallestCircle =
+			spacePerSticker * stickersInSmallestCircle;
+		let radius = circumferenceNeededForSmallestCircle / (Math.PI * 2);
+		// eslint-disable-next-line fp/no-mutating-methods
+		data.push({
+			cx: videoConfig.width / 2,
+			cy: videoConfig.height / 2,
+			rx: radius,
+			ry: radius,
+			stickersLength: stickersInSmallestCircle,
+		});
+		for (let i = 0; i < 10; i++) {
+			const nextRadius = radius + spacePerSticker * 1.2;
+			radius = nextRadius;
+			const stickersFittingInNextRadius = Math.floor(
+				((radius * Math.PI * 2) / circumferenceNeededForSmallestCircle) *
+					stickersInSmallestCircle
+			);
+			data.push({
+				cx: videoConfig.width / 2,
+				cy: videoConfig.height / 2,
+				ry: nextRadius,
+				rx: nextRadius,
+				stickersLength: stickersFittingInNextRadius,
+			});
+		}
+		return data;
+	})();
+
+	const frame = useCurrentFrame();
+
+	const frameToPick = Math.round(
+		interpolate({
+			input: frame,
+			inputRange: [0, videoConfig.durationInFrames],
+			outputRange: [1, 221],
+			extrapolateLeft: 'clamp',
+		})
+	);
+	const f = require('./imgs/Rotato Frame ' + frameToPick + '.png').default;
 	return (
 		<div
 			style={{
@@ -13,24 +71,51 @@ export const RealStickers = () => {
 				backgroundColor: 'white',
 			}}
 		>
-			{output.map((o, i) => {
-				const xOffset = interpolate({
-					input: seeds[i],
-					inputRange: [0, 1],
-					outputRange: [0, videoConfig.width],
-				});
-				const yOffset = interpolate({
-					input: seeds[seeds.length - i],
-					inputRange: [0, 1],
-					outputRange: [0, videoConfig.height],
-				});
-				return (
-					<img
-						src={`https://anysticker.imgix.net/${o.source}?w=200&h=200&fm=png&fill=solid&fit=fill&auto=compress`}
-						style={{}}
-					></img>
-				);
-			})}
+			<div
+				style={{
+					transform: 'scale(0.7)',
+					width: videoConfig.width,
+					height: videoConfig.height,
+				}}
+			>
+				{output.map((o, i) => {
+					const circle = (function () {
+						let totalStickersFitted = 0;
+						for (let j = 0; j < _cData.length; j++) {
+							const c = _cData[j];
+							totalStickersFitted += c.stickersLength;
+							if (totalStickersFitted > i) {
+								return {
+									circle: j,
+									indexOfCircle: i - totalStickersFitted + c.stickersLength,
+								};
+							}
+						}
+						throw new Error('wtf');
+					})();
+					const {cx, cy, rx, ry, stickersLength} = _cData[circle.circle];
+					const d = `M${cx - rx},${cy}a${rx},${ry} 0 1,0 ${
+						rx * 2
+					},0a${rx},${ry} 0 1,0 -${rx * 2},0`;
+
+					const p = svgPath.svgPathProperties(d);
+					const length = p.getTotalLength();
+					const point = p.getPointAtLength(
+						(length / stickersLength) * circle.indexOfCircle
+					);
+					return (
+						<img
+							src={`https://anysticker.imgix.net/${o.source}?w=${stickerSize}&h=${stickerSize}&fm=png&fill=solid&fit=fill&auto=compress`}
+							style={{
+								position: 'absolute',
+								left: point.x - stickerSize / 2,
+								top: point.y - stickerSize / 2,
+							}}
+						></img>
+					);
+				})}
+				<img src={f} style={{position: 'absolute'}}></img>
+			</div>
 		</div>
 	);
 };
