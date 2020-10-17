9310dd4d2e9e15a9c86d807a9413ce2a6896bcd3
Jonny Burger <jonathanburger11@gmail.com>
2020-10-17 13:26:44 +0200

    lit orchestra
    Former-commit-id: 57260bfbb99319e86586252f610448ad6480a877

--- packages/example/src/BigRotate/index.tsx
+++ packages/example/src/BigRotate/index.tsx
@@ -1,20 +1,68 @@
-import {registerVideo, useCurrentFrame} from '@remotion/core';
+import {
+	interpolate,
+	registerVideo,
+	spring2,
+	useCurrentFrame,
+	useVideoConfig,
+} from '@remotion/core';
 import React from 'react';
 
 export const BigRotate = () => {
 	const frame = useCurrentFrame();
-	const src = require('./assets/Rotato Frame ' + (frame + 1) + '.png').default;
+	const videoConfig = useVideoConfig();
+	const rotatoFrame = spring2({
+		fps: videoConfig.fps,
+		config: {
+			stiffness: 20,
+			damping: 5,
+			mass: 0.3,
+			overshootClamping: false,
+			restDisplacementThreshold: 0.01,
+			restSpeedThreshold: 0.01,
+		},
+		from: 1,
+		to: 0,
+		frame,
+	});
+	const scale = spring2({
+		fps: videoConfig.fps,
+		config: {
+			stiffness: 20,
+			damping: 5,
+			mass: 0.3,
+			overshootClamping: false,
+			restDisplacementThreshold: 0.01,
+			restSpeedThreshold: 0.01,
+		},
+		from: 0.5,
+		to: 1,
+		frame,
+	});
+	const frameInterpolated = Math.round(
+		interpolate({
+			input: rotatoFrame,
+			inputRange: [0, 1],
+			outputRange: [1, 250],
+		})
+	);
+	const src = require('./assets/Rotato Frame ' +
+		(frameInterpolated + 1) +
+		'.png').default;
 	return (
 		<div
 			style={{
-				background: 'white',
 				flex: 1,
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
 			}}
 		>
-			<img src={src}></img>
+			<img
+				src={src}
+				style={{
+					transform: `scale(${scale})`,
+				}}
+			></img>
 		</div>
 	);
 };
@@ -22,6 +70,6 @@ export const BigRotate = () => {
 registerVideo(BigRotate, {
 	width: 1080,
 	height: 1080,
-	fps: 30,
+	fps: 60,
 	durationInFrames: 3 * 30,
 });
