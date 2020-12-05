0e78f946bd747d4172693183b69709118d8955d4
Jonny Burger <jonathanburger11@gmail.com>
2020-12-05 23:24:34 +0100

    sequencing and solve performance
    Former-commit-id: 07e1ae82c96065b480dc49ea480b2d8d9b6d0160

--- packages/core/src/timeline-position-state.ts
+++ packages/core/src/timeline-position-state.ts
@@ -1,6 +1,4 @@
-import {useEffect} from 'react';
 import {createGlobalState} from 'react-hooks-global-state';
-import {useVideoConfig} from './use-video-config';
 
 const {useGlobalState} = createGlobalState({
 	frame: 0,
@@ -14,15 +12,6 @@ type TimelineReturnType = readonly [
 
 export const useTimelinePosition = (): TimelineReturnType => {
 	const state = useGlobalState('frame');
-	const videoConfig = useVideoConfig();
-	useEffect(() => {
-		if (state[0] < 0) {
-			state[1](0);
-		}
-		if (state[0] - 1 > videoConfig.durationInFrames) {
-			state[1](videoConfig.durationInFrames - 1);
-		}
-	}, [state, videoConfig.durationInFrames]);
 	return state;
 };
 
