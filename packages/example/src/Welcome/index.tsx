39b2cef4de7cac7b9f1ff347ec651b2ca37e4435
Jonny Burger <jonathanburger11@gmail.com>
2020-12-05 23:12:07 +0100

    duration

--- packages/example/src/Welcome/index.tsx
+++ packages/example/src/Welcome/index.tsx
@@ -1,18 +1,14 @@
 import {Audio, registerVideo, Sequence} from '@remotion/core';
 import React from 'react';
 import {Any} from '../Any';
-import {Devices} from '../Devices';
 
 export const Welcome: React.FC = () => {
 	const audio = require('./audio.mp4').default;
 
 	return (
 		<div style={{flex: 1, display: 'flex'}}>
-			<Sequence from={0}>
+			<Sequence from={0} durationInFrames={20}>
 				<Any />
-			</Sequence>
-			<Sequence from={80}>
-				<Devices />
 			</Sequence>
 			<Audio src={audio} />
 		</div>
