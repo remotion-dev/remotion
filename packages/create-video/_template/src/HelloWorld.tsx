80a2bd67f3b103eb5386a24157d6f4ad7ba092cd
Jonny Burger <jonathanburger11@gmail.com>
2020-12-13 21:47:52 +0100

    copy template

---
+++ packages/create-video/_template/src/HelloWorld.tsx
@@ -0,0 +1,40 @@
+import {
+	interpolate,
+	Sequence,
+	useCurrentFrame,
+	useVideoConfig,
+} from '@remotion/core';
+import React from 'react';
+import {DotGrid} from './HelloWorld/DotGrid';
+import {Logo} from './HelloWorld/Logo';
+import {Subtitle} from './HelloWorld/Subtitle';
+import {Title} from './HelloWorld/Title';
+
+export const HelloWorld: React.FC = () => {
+	const frame = useCurrentFrame();
+	const videoConfig = useVideoConfig();
+
+	const opacity = interpolate(
+		frame,
+		[videoConfig.durationInFrames - 50, videoConfig.durationInFrames - 30],
+		[1, 0]
+	);
+	const transitionStart = 50;
+
+	return (
+		<div style={{flex: 1, backgroundColor: 'white'}}>
+			<DotGrid />
+			<div style={{opacity}}>
+				<Sequence from={0} durationInFrames={videoConfig.durationInFrames}>
+					<Logo transitionStart={transitionStart} />
+				</Sequence>
+				<Sequence from={transitionStart + 10} durationInFrames={Infinity}>
+					<Title />
+				</Sequence>
+				<Sequence from={transitionStart + 100} durationInFrames={Infinity}>
+					<Subtitle />
+				</Sequence>
+			</div>
+		</div>
+	);
+};
