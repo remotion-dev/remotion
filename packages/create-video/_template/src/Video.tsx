2bd32dabf38baa174b075a9c938c16570ec34726
Jonny Burger <jonathanburger11@gmail.com>
2020-12-13 21:47:52 +0100

    copy template
    Former-commit-id: 80a2bd67f3b103eb5386a24157d6f4ad7ba092cd

---
+++ packages/create-video/_template/src/Video.tsx
@@ -0,0 +1,35 @@
+import {Composition} from '@remotion/core';
+import {HelloWorld} from './HelloWorld';
+import {Logo} from './HelloWorld/Logo';
+import {Subtitle} from './HelloWorld/Subtitle';
+
+export const RemotionVideo = () => {
+	return (
+		<>
+			<Composition
+				name="HelloWorld"
+				component={HelloWorld}
+				durationInFrames={400}
+				fps={60}
+				width={1920}
+				height={1080}
+			/>
+			<Composition
+				name="Logo"
+				component={Logo}
+				durationInFrames={400}
+				fps={60}
+				width={1920}
+				height={1080}
+			/>
+			<Composition
+				name="Title"
+				component={Subtitle}
+				durationInFrames={100}
+				fps={60}
+				width={1920}
+				height={1080}
+			/>
+		</>
+	);
+};
