2016f8b0e1dce83e96c6e03faa81ece602892abe
Jonny Burger <jonathanburger11@gmail.com>
2020-12-15 16:24:08 +0100

    replace recoil
    Former-commit-id: a23663436384ce576633ef9fea42dff37be88682

--- packages/cli/src/editor/state/preview-size.tsx
+++ packages/cli/src/editor/state/preview-size.tsx
@@ -1,6 +1,13 @@
-import {atom} from 'recoil';
+import {createContext} from 'react';
 
-export const previewSizeState = atom({
-	key: 'react-motion-preview-size',
-	default: 'auto',
+export type PreviewSize = 'auto' | number;
+
+type PreviewSizeCtx = {
+	size: PreviewSize;
+	setSize: (s: PreviewSize) => void;
+};
+
+export const PreviewSizeContext = createContext<PreviewSizeCtx>({
+	setSize: () => void 0,
+	size: 'auto',
 });
--- packages/cli/src/previewEntry.tsx
+++ packages/cli/src/previewEntry.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {RecoilRoot} from 'recoil';
 import {RemotionRoot} from 'remotion';
 import {Editor} from './editor/components/Editor';
 
 render(
 	<RemotionRoot>
-		<RecoilRoot>
-			<Editor />
-		</RecoilRoot>
+		<Editor />
 	</RemotionRoot>,
 	document.getElementById('container')
 );
