59b7f89a0c7205d42477b27e241b6b53e308a58e
Jonny Burger <jonathanburger11@gmail.com>
2020-12-15 19:19:48 +0100

    show current composition

--- packages/cli/src/editor/components/TimeValue.tsx
+++ packages/cli/src/editor/components/TimeValue.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import {useCurrentFrame, useVideoConfig} from 'remotion';
 import styled from 'styled-components';
+import {renderFrame} from '../state/render-frame';
 
 const Text = styled.div`
 	color: white;
 	font-size: 24px;
 	font-family: Arial, Helvetica, sans-serif;
 `;
-
-const renderFrame = (frame: number, fps: number): string => {
-	const minutes = Math.floor(frame / fps / 60);
-	const remainingSec = frame - minutes * fps * 60;
-	const seconds = Math.floor(remainingSec / fps);
-	const frameAfterSec = frame % fps;
-	if (fps === 0) {
-		return 'No video selected';
-	}
-	return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
-		2,
-		'0'
-	)}.${String(frameAfterSec).padStart(2, '0')}`;
-};
 
 export const TimeValue: React.FC = () => {
 	const frame = useCurrentFrame();
