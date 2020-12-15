81a638ee6068c19885b20c283ff45f9dd6e9aa34
Jonny Burger <jonathanburger11@gmail.com>
2020-12-15 14:22:36 +0100

    render command
    Former-commit-id: 73c0dd9c692ae56ba26eb466c43db4c293011c75

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,22 +1,16 @@
-import path from 'path';
 import xns from 'xns';
-import {bundleCommand} from './bundle';
 import {previewCommand} from './preview';
 
-export const bundle = xns(
-	async (
-		fullPath = path.join(process.cwd(), '..', 'example', 'src', 'index.tsx')
-	) => {
-		const args = process.argv;
-		const command = args[2];
+export const cli = xns(async () => {
+	const args = process.argv;
+	const command = args[2];
 
-		if (command === 'render') {
-			await bundleCommand(fullPath);
-		} else if (command === 'preview') {
-			await previewCommand();
-		} else {
-			console.log(`Command ${command} not found.`);
-			process.exit(1);
-		}
+	if (command === 'preview') {
+		await previewCommand();
+	} else {
+		console.log(`Command ${command} not found.`);
+		process.exit(1);
 	}
-);
+});
+
+export * from './render';
