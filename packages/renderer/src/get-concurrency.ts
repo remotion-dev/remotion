e7ba5c755f7153ff8ed84c890380c3146a3c8713
Jonny Burger <jonathanburger11@gmail.com>
2020-12-20 22:15:59 +0100

    hmm

--- packages/renderer/src/get-concurrency.ts
+++ packages/renderer/src/get-concurrency.ts
@@ -2,7 +2,7 @@ import os from 'os';
 
 export const getActualConcurrency = (userPreference: number | null) => {
 	if (userPreference === null) {
-		return Math.min(8, Math.max(1, os.cpus().length / 2));
+		return Math.round(Math.min(8, Math.max(1, os.cpus().length / 2)));
 	}
 	const rounded = Math.floor(userPreference);
 	const max = os.cpus().length;
