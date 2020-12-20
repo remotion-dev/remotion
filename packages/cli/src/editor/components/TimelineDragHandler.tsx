f1e3064242c57456ee0dd0683a4b07ead2735f42
Jonny Burger <jonathanburger11@gmail.com>
2020-12-20 13:29:56 +0100

    start eslint plugins

--- packages/cli/src/editor/components/TimelineDragHandler.tsx
+++ packages/cli/src/editor/components/TimelineDragHandler.tsx
@@ -33,7 +33,7 @@ const getFrameFromX = (
 		interpolate(
 			pos,
 			[0, width - TIMELINE_LEFT_PADDING - TIMELINE_RIGHT_PADDING],
-			[0, durationInFrames ?? 0],
+			[0, durationInFrames - 1 ?? 0],
 			{
 				extrapolateLeft: 'clamp',
 				extrapolateRight: 'clamp',
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,43 @@
+const xo = require("eslint-config-xo/esnext");
+const xoReact = require("eslint-config-xo-react");
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Text: "off",
+    StyleSheet: "off",
+  },
+  plugins: [
+    ...xoReact.plugins,
+    "@typescript-eslint/eslint-plugin",
+    "react",
+    "10x",
+  ],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "prettier",
+  ].filter(Boolean),
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    ...xo.rules,
+    ...xoReact.rules,
+    "no-console": "off",
+    "10x/react-in-scope": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-key": "off",
+    "react/jsx-no-target-blank": "off",
+  },
+};
