1bd96bb2b06d644a10112a5b45051c87cab45125
Jonny Burger <jonathanburger11@gmail.com>
2020-12-20 17:21:40 +0100

    update template
    Former-commit-id: a0c1a1b80e0548da1304fc9f4536402fb5e11b3a

--- packages/create-video/_template/src/HelloWorld/Subtitle.tsx
+++ packages/create-video/_template/src/HelloWorld/Subtitle.tsx
@@ -1,7 +1,7 @@
 import {interpolate, useCurrentFrame} from 'remotion';
 import {COLOR_1} from './config';
 
-export const Subtitle = () => {
+export const Subtitle: React.FC = () => {
 	const frame = useCurrentFrame();
 	const opacity = interpolate(frame, [0, 30], [0, 1]);
 	return (
