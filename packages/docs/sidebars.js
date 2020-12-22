95958a7026b1f01920a8b8a0f29886899b7f7949
Jonny Burger <jonathanburger11@gmail.com>
2020-12-22 17:33:31 +0100

    start writing documentation

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -1,6 +1,10 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    "Getting started": [
+      "getting-started",
+      "the-fundamentals",
+      "animating-properties",
+    ],
+    Techniques: ["anti-patterns"],
   },
 };
