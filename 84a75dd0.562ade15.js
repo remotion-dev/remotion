d76937b0c5a8e1d29e8c1efd19802b4614815cfe
JonnyBurger <jonathanburger11@gmail.com>
2020-12-24 16:58:46 +0000

    Deploying to gh-pages from  @ 4623c0d5068add0f9481f80c0ee9c5bf18f3dc16 🚀

--- 84a75dd0.562ade15.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{83:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
--- a80da1cf.e2475e74.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{88:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/blog/tags/docusaurus"}')}}]);
