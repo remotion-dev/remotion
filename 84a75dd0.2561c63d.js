42345ae1c5223280d20212ccb8b4de1d3819deaf
JonnyBurger <jonathanburger11@gmail.com>
2021-01-05 12:02:50 +0000

    Deploying to gh-pages from  @ 329aa0744216481513079a8422260280de502d20 🚀

---
+++ 84a75dd0.2561c63d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{83:function(n){n.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing Remotion","permalink":"/blog/introducing-remotion"}]}')}}]);
