018e8bc8e8ea3aecc3e16f5e52839883f0ba10fa
Jonny Burger <jonathanburger11@gmail.com>
2021-01-10 16:04:04 +0100

    clip namr
    Former-commit-id: c512452dae5d55d993a8b4efac10c6d1a58b54d8

--- packages/core/src/get-timeline-clip-name.ts
+++ packages/core/src/get-timeline-clip-name.ts
@@ -1,7 +1,8 @@
 export const getTimelineClipName = (children: any, i = 0): string => {
 	const arrays = Array.isArray(children) ? children : [children];
 	const tree = arrays.map((ch) => {
-		const root = ('  '.repeat(i) + ch?.type?.id) as string;
+		// Must be name, not ID
+		const root = ('  '.repeat(i) + ch?.type?.name) as string;
 		if (ch.props.children) {
 			const chName = getTimelineClipName(children.props.children, i + 1);
 			return [root, chName].join('\n');
