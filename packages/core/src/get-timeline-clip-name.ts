ddb970f9b2e2074f0304f4874fc42b40a20a87b0
Jonny Burger <jonathanburger11@gmail.com>
2021-01-28 20:56:44 +0100

    Update get-timeline-clip-name.ts

--- packages/core/src/get-timeline-clip-name.ts
+++ packages/core/src/get-timeline-clip-name.ts
@@ -2,13 +2,16 @@ export const getTimelineClipName = (children: any, i = 0): string => {
 	const arrays = Array.isArray(children) ? children : [children];
 	const tree = arrays.map((ch) => {
 		// Must be name, not ID
-		const root = ('  '.repeat(i) + ch?.type?.name) as string;
+		const name = ch?.type?.name;
+		if (!name) {
+			return null;
+		}
+		const root = '  '.repeat(i) + name;
 		if (ch.props.children) {
 			const chName = getTimelineClipName(children.props.children, i + 1);
 			return [root, chName].join('\n');
-		} else {
-			return root;
 		}
+		return root;
 	});
-	return tree.join('\n');
+	return tree.filter(Boolean).join('\n');
 };
