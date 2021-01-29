b1477f85dbc93fd6d10e30cbce492a4ae44fbfbd
Jonny Burger <jonathanburger11@gmail.com>
2021-01-29 19:09:10 +0100

    cleanup

--- packages/create-video/_template/render.ts
+++
@@ -1,9 +0,0 @@
-import {render} from '@remotion/cli';
-import {evaluateRootForCompositions} from 'remotion';
-import xns from 'xns';
-
-xns(async () => {
-	await import('./src/index');
-	const comps = await evaluateRootForCompositions();
-	return render(require.resolve('./src/index'), comps);
-});
