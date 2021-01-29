b887ca83d9cbb77d51bffe5968f0e225ffe92170
Jonny Burger <jonathanburger11@gmail.com>
2021-01-29 15:13:10 +0100

    eslint plugin

---
+++ packages/eslint-plugin/src/index.ts
@@ -0,0 +1,9 @@
+import nomp4Import from "./rules/no-mp4-import";
+
+const rules = {
+  "no-mp4-import": nomp4Import,
+};
+
+export = {
+  rules,
+};
---
+++ packages/eslint-plugin/src/rules/no-mp4-import.ts
@@ -0,0 +1,68 @@
+import { ESLintUtils } from "@typescript-eslint/experimental-utils";
+
+const createRule = ESLintUtils.RuleCreator((name) => {
+  return `https://github.com/JonnyBurger/remotion`;
+});
+
+type Options = [];
+
+type MessageIds = "NoMP4Import";
+
+const NoMP4Import =
+  "Importing MP4 will work while you are previewing the video, but will not work while rendering since Puppeteer does not include the codecs necessary for MP4 videos. Convert the video to WebM first.";
+
+export default createRule<Options, MessageIds>({
+  name: "no-mp4-import",
+  meta: {
+    type: "problem",
+    docs: {
+      description: NoMP4Import,
+      category: "Best Practices",
+      recommended: "warn",
+    },
+    fixable: undefined,
+    schema: [],
+    messages: {
+      NoMP4Import,
+    },
+  },
+  defaultOptions: [],
+  create: (context) => {
+    return {
+      ImportDeclaration: (node) => {
+        if (node.source.raw.includes(".mp4")) {
+          context.report({
+            messageId: "NoMP4Import",
+            node,
+          });
+        }
+      },
+      CallExpression: (node) => {
+        // @ts-expect-error
+        if (node.callee.name !== "require") {
+          return;
+        }
+        // @ts-expect-error
+        const firstArgument = node.callee?.parent?.arguments?.[0]?.raw as
+          | string
+          | undefined;
+        if (!firstArgument) {
+          const sourceCode = context.getSourceCode().lines.join("\n");
+          if (sourceCode.includes(".mp4")) {
+            context.report({
+              messageId: "NoMP4Import",
+              node,
+            });
+          }
+          return;
+        }
+        if (firstArgument.includes(".mp4")) {
+          context.report({
+            messageId: "NoMP4Import",
+            node,
+          });
+        }
+      },
+    };
+  },
+});
