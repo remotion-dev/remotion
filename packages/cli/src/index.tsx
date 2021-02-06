748c8db957ef7bb84c9750c42ef990c6e320c110
Jonny Burger <jonathanburger11@gmail.com>
2021-02-06 14:29:06 +0100

    upgrade command

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,6 +1,7 @@
 import xns from 'xns';
 import {previewCommand} from './preview';
 import {render} from './render';
+import {upgrade} from './upgrade';
 
 export const cli = xns(async () => {
 	const args = process.argv;
@@ -10,8 +11,14 @@ export const cli = xns(async () => {
 		await previewCommand();
 	} else if (command === 'render') {
 		await render();
+	} else if (command === 'upgrade') {
+		await upgrade();
 	} else {
 		console.log(`Command ${command} not found.`);
+		console.log('Available commands:');
+		console.log('  preview');
+		console.log('  render');
+		console.log('  upgrade');
 		process.exit(1);
 	}
 });
---
+++ packages/cli/src/upgrade.ts
@@ -0,0 +1,59 @@
+import execa from 'execa';
+import fs from 'fs';
+import path from 'path';
+
+const npmOrYarn = (): 'npm' | 'yarn' => {
+	const packageLockJsonFilePath = path.join(process.cwd(), 'package-lock.json');
+	const yarnLockFilePath = path.join(process.cwd(), 'yarn.lock');
+
+	const npmExists = fs.existsSync(packageLockJsonFilePath);
+	const yarnExists = fs.existsSync(yarnLockFilePath);
+
+	if (npmExists && !yarnExists) {
+		return 'npm';
+	}
+	if (!npmExists && yarnExists) {
+		return 'yarn';
+	}
+	if (npmExists && yarnExists) {
+		console.log(
+			'Found both a package-lock.json and a yarn.lock file in your project.'
+		);
+		console.log(
+			'This can lead to bugs, delete one of the two files and settle on 1 package manager.'
+		);
+		console.log('Afterwards, run this command again.');
+		process.exit(1);
+	}
+	console.log('Did not find a package-lock.json or yarn.lock file.');
+	console.log('Cannot determine how to update dependencies.');
+	console.log('Did you run `npm install` yet?');
+	console.log('Make sure either file exists and run this command again.');
+	process.exit(1);
+};
+
+export const upgrade = async () => {
+	const packageJsonFilePath = path.join(process.cwd(), 'package.json');
+	if (!fs.existsSync(packageJsonFilePath)) {
+		console.log(
+			'Could not upgrade because no package.json could be found in your project.'
+		);
+		process.exit(1);
+	}
+	// eslint-disable-next-line import/no-dynamic-require
+	const packageJson = require(packageJsonFilePath);
+	const dependencies = Object.keys(packageJson['dependencies']);
+
+	const tool = npmOrYarn();
+
+	const toUpgrade = [
+		'@remotion/bundler',
+		'@remotion/cli',
+		'@remotion/eslint-config',
+		'@remotion/renderer',
+		'remotion',
+	].filter((u) => dependencies.includes(u));
+
+	await execa(tool, ['upgrade', ...toUpgrade]);
+	console.log('⏫ Remotion has been upgraded!');
+};
