d703bfea58e365ec38020afbbd162b52c12fadb9
Jonny Burger <jonathanburger11@gmail.com>
2021-02-08 23:27:12 +0100

    Use move command on Windows
    Former-commit-id: 309eb8d6e7bd892394ca812bdacee080254f229b

--- packages/create-video/src/dotfiles.ts
+++ packages/create-video/src/dotfiles.ts
@@ -1,17 +1,23 @@
-import path from 'path';
 import execa from 'execa';
+import path from 'path';
 
 const files = ['gitignore', 'eslintrc', 'prettierrc'];
 
 export const turnIntoUnderscore = async (dir: string) => {
 	for (const file of files) {
-		await execa('mv', [path.join(dir, `.${file}`), path.join(dir, `_${file}`)]);
+		await execa(process.platform === 'win32' ? 'move' : 'mv', [
+			path.join(dir, `.${file}`),
+			path.join(dir, `_${file}`),
+		]);
 	}
 };
 
 export const turnIntoDot = async (dir: string) => {
 	for (const file of files) {
-		await execa('mv', [path.join(dir, `_${file}`), path.join(dir, `.${file}`)]);
+		await execa(process.platform === 'win32' ? 'move' : 'mv', [
+			path.join(dir, `_${file}`),
+			path.join(dir, `.${file}`),
+		]);
 	}
 };
 
