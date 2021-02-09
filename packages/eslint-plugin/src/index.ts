a32ed8f2ae63a43bd9db356435f2f09199c804d6
Jonny Burger <jonathanburger11@gmail.com>
2021-02-09 23:17:23 +0100

    add warn-native-media-tag eslint rule
    Former-commit-id: 2c8f929cdc9eae003ac9b9a6e78269219411fed6

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,7 +1,9 @@
 import nomp4Import from "./rules/no-mp4-import";
+import warnNativeMediaTag from "./rules/warn-native-media-tag";
 
 const rules = {
   "no-mp4-import": nomp4Import,
+  "warn-native-image-tag": warnNativeMediaTag,
 };
 
 export = {
