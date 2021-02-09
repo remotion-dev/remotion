e541566d6a256800286ce3507caaf5fd6217984c
Jonny Burger <jonathanburger11@gmail.com>
2021-02-09 16:12:36 +0100

    add braces to webpack override

--- packages/bundler/src/webpack-config.ts
+++ packages/bundler/src/webpack-config.ts
@@ -25,120 +25,122 @@ export const webpackConfig = ({
 		historyApiFallback: boolean;
 		hot: true;
 	};
-} => ({
-	optimization: {
-		minimize: false,
-	},
-	experiments: {
-		lazyCompilation: {
-			entries: false,
+} => {
+	return {
+		optimization: {
+			minimize: false,
 		},
-	},
-	devtool: 'cheap-module-source-map',
-	entry: [
-		environment === 'development'
-			? require.resolve('webpack-hot-middleware/client') + '?overlay=true'
-			: null,
-		environment === 'development'
-			? require.resolve('@webhotelier/webpack-fast-refresh/runtime.js')
-			: null,
-		userDefinedComponent,
-		entry,
-	].filter(Boolean) as [string, ...string[]],
-	mode: environment,
-	plugins:
-		environment === 'development'
-			? [
-					new ErrorOverlayPlugin(),
-					new ReactRefreshPlugin(),
-					new webpack.HotModuleReplacementPlugin(),
-			  ]
-			: [],
-	output: {
-		globalObject: 'this',
-		filename: 'bundle.js',
-		path: outDir,
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, '..', 'web'),
-		historyApiFallback: true,
-		hot: true,
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js'],
-		alias: {
-			// Only one version of react
-			'react/jsx-runtime': require.resolve('react/jsx-runtime'),
-			react: require.resolve('react'),
-			remotion: require.resolve('remotion'),
-			'styled-components': require.resolve('styled-components'),
-			'react-native$': 'react-native-web',
+		experiments: {
+			lazyCompilation: {
+				entries: false,
+			},
 		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(woff|woff2)$/,
-				use: {
-					loader: 'url-loader',
+		devtool: 'cheap-module-source-map',
+		entry: [
+			environment === 'development'
+				? require.resolve('webpack-hot-middleware/client') + '?overlay=true'
+				: null,
+			environment === 'development'
+				? require.resolve('@webhotelier/webpack-fast-refresh/runtime.js')
+				: null,
+			userDefinedComponent,
+			entry,
+		].filter(Boolean) as [string, ...string[]],
+		mode: environment,
+		plugins:
+			environment === 'development'
+				? [
+						new ErrorOverlayPlugin(),
+						new ReactRefreshPlugin(),
+						new webpack.HotModuleReplacementPlugin(),
+				  ]
+				: [],
+		output: {
+			globalObject: 'this',
+			filename: 'bundle.js',
+			path: outDir,
+		},
+		devServer: {
+			contentBase: path.resolve(__dirname, '..', 'web'),
+			historyApiFallback: true,
+			hot: true,
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js'],
+			alias: {
+				// Only one version of react
+				'react/jsx-runtime': require.resolve('react/jsx-runtime'),
+				react: require.resolve('react'),
+				remotion: require.resolve('remotion'),
+				'styled-components': require.resolve('styled-components'),
+				'react-native$': 'react-native-web',
+			},
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(woff|woff2)$/,
+					use: {
+						loader: 'url-loader',
+					},
 				},
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|webp|gif|bmp|webm|mp4|mp3|wav|aac)$/,
-				use: [
-					{
-						loader: require.resolve('file-loader'),
-						options: {
-							// So you can do require('hi.png')
-							// instead of require('hi.png').default
-							esModule: false,
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.(png|svg|jpg|jpeg|webp|gif|bmp|webm|mp4|mp3|wav|aac)$/,
+					use: [
+						{
+							loader: require.resolve('file-loader'),
+							options: {
+								// So you can do require('hi.png')
+								// instead of require('hi.png').default
+								esModule: false,
+							},
 						},
-					},
-				],
-			},
-			{
-				test: /\.tsx?$/,
-				use: [
-					{
-						loader: require.resolve('babel-loader'),
-						options: {
-							presets: [
-								require.resolve('@babel/preset-env'),
-								[
-									require.resolve('@babel/preset-react'),
-									{
-										runtime: 'automatic',
-									},
+					],
+				},
+				{
+					test: /\.tsx?$/,
+					use: [
+						{
+							loader: require.resolve('babel-loader'),
+							options: {
+								presets: [
+									require.resolve('@babel/preset-env'),
+									[
+										require.resolve('@babel/preset-react'),
+										{
+											runtime: 'automatic',
+										},
+									],
+									[
+										require.resolve('@babel/preset-typescript'),
+										{
+											runtime: 'automatic',
+											isTSX: true,
+											allExtensions: true,
+										},
+									],
 								],
-								[
-									require.resolve('@babel/preset-typescript'),
-									{
-										runtime: 'automatic',
-										isTSX: true,
-										allExtensions: true,
-									},
-								],
-							],
-							plugins: [
-								environment === 'development'
-									? require.resolve('react-refresh/babel')
-									: null,
-							].filter(truthy),
+								plugins: [
+									environment === 'development'
+										? require.resolve('react-refresh/babel')
+										: null,
+								].filter(truthy),
+							},
 						},
-					},
-					environment === 'development'
-						? {
-								loader: require.resolve(
-									'@webhotelier/webpack-fast-refresh/loader.js'
-								),
-						  }
-						: null,
-				].filter(truthy),
-			},
-		],
-	},
-});
+						environment === 'development'
+							? {
+									loader: require.resolve(
+										'@webhotelier/webpack-fast-refresh/loader.js'
+									),
+							  }
+							: null,
+					].filter(truthy),
+				},
+			],
+		},
+	};
+};
