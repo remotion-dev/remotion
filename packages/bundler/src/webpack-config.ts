6d2ae42ffe6c037af209418e85ffe4ef54e2f145
Jonny Burger <jonathanburger11@gmail.com>
2021-02-09 15:49:48 +0100

    whitespce

--- packages/bundler/src/webpack-config.ts
+++ packages/bundler/src/webpack-config.ts
@@ -72,7 +72,7 @@ export const webpackConfig = ({
 			react: require.resolve('react'),
 			remotion: require.resolve('remotion'),
 			'styled-components': require.resolve('styled-components'),
-      			'react-native$': 'react-native-web',
+			'react-native$': 'react-native-web',
 		},
 	},
 	module: {
