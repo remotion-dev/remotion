bdb6aeb158a2ac577f02d30ac75acc31af2175d2
jeetiss <jeetiss@yandex.ru>
2021-02-12 12:29:36 +0300

    feat(renderer): improve render algorithm

--- packages/core/src/use-frame.ts
+++ packages/core/src/use-frame.ts
@@ -5,10 +5,6 @@ import {useTimelinePosition} from './timeline-position-state';
 export const useAbsoluteCurrentFrame = (): number => {
 	const timelinePosition = useTimelinePosition();
 
-	const param = new URLSearchParams(window.location.search).get('frame');
-	if (param !== null) {
-		return Number(param);
-	}
 	return timelinePosition;
 };
 
