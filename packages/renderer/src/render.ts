5c9d418156baf6698ea7cda3e2d4b26f9ba2a9c6
Jonny Burger <jonathanburger11@gmail.com>
2021-02-13 23:52:59 +0100

    some puppeteer experimenting

--- packages/renderer/src/render.ts
+++ packages/renderer/src/render.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import {VideoConfig} from 'remotion';
 import {openBrowser, provideScreenshot} from '.';
+import {logPerf} from './benchmarking';
 import {getActualConcurrency} from './get-concurrency';
 import {DEFAULT_IMAGE_FORMAT, ImageFormat} from './image-format';
 import {Pool} from './pool';
@@ -84,4 +85,7 @@ export const renderFrames = async ({
 			})
 	);
 	await browser.close();
+	setTimeout(() => {
+		logPerf();
+	}, 300);
 };
