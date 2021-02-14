ce8c833615329155d625d506712526fd04f6d80f
Jonny Burger <jonathanburger11@gmail.com>
2021-02-14 16:20:40 +0100

    fix typo
    Former-commit-id: d0c780c458bde9c5c8d1fdc4367f535e1cdb2d93

--- packages/bundler/src/override-webpack.ts
+++ packages/bundler/src/override-webpack.ts
@@ -1,13 +1,13 @@
 import {Config, WebpackOverrideFn} from 'remotion';
 
 /**
- * Please use Config.Bundler.overrideWebpack() from now on.
+ * Please use Config.Bundling.overrideWebpack() from now on.
  * See: https://www.remotion.dev/docs/webpack/
  * @deprecated
  */
 export const overrideWebpackConfig = (fn: WebpackOverrideFn) => {
 	console.warn('bundler.overrideWebpackConfig has been deprecated.');
-	console.warn('Please migrate to Config.Bundler.overrideWebpack().');
+	console.warn('Please migrate to Config.Bundling.overrideWebpack().');
 	console.warn('See: https://www.remotion.dev/docs/webpack/');
 
 	Config.Bundling.overrideWebpackConfig(fn);
