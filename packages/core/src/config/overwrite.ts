bcc5293fd8357fd8d48400e3eddf9eb8c59944a1
Jonny Burger <jonathanburger11@gmail.com>
2021-02-14 14:35:09 +0100

    config options
    Former-commit-id: 64fd1147c64a56c07e9dce883e6c8af521b374e3

---
+++ packages/core/src/config/overwrite.ts
@@ -0,0 +1,14 @@
+let shouldOverwrite = false;
+
+export const setOverwriteOutput = (newOverwrite: boolean) => {
+	if (typeof newOverwrite !== 'boolean') {
+		throw new Error(
+			`overwriteExisting must be a boolean but got ${typeof newOverwrite} (${JSON.stringify(
+				newOverwrite
+			)})`
+		);
+	}
+	shouldOverwrite = newOverwrite;
+};
+
+export const getShouldOverwrite = () => shouldOverwrite;
