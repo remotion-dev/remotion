3153ef3d391848e639506aacef388a858c707ce7
Jonny Burger <jonathanburger11@gmail.com>
2021-02-14 21:14:06 +0100

    Better windows FFMPEG installation instructions

--- packages/renderer/src/validate-ffmpeg.ts
+++ packages/renderer/src/validate-ffmpeg.ts
@@ -23,12 +23,28 @@ export const validateFfmpeg = async (): Promise<void> => {
 		if (os.platform() === 'darwin' && (await isHomebrewInstalled())) {
 			console.error('Run `brew install ffmpeg` to install ffmpeg');
 		} else if (os.platform() === 'win32') {
-			console.log('Windows error message');
+			console.error('1. Install FFMPEG for Windows here:');
+			console.error(
+				'https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg#installing-ffmpeg-in-windows'
+			);
+			console.error('2. Add FFMPEG to your PATH');
+			console.error('  a. Go to the settings app.');
+			console.error('  b. Click System.');
+			console.error('  c. Click About.');
+			console.error('  d. Click Advanced system settings.');
+			console.error('  e. Click Environment variables.');
+			console.error(
+				'  f. Search for PATH environemnt variable, click edit and the folder where you installed FFMPEG.'
+			);
+			console.error(
+				'  g. Important: Restart your terminal completely to apply the new PATH.'
+			);
+			console.error('3. Re-run this command.');
 		} else {
 			console.error(
 				'See https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg on how to install FFMPEG.'
 			);
 		}
-		process.exit(1)
+		process.exit(1);
 	}
 };
