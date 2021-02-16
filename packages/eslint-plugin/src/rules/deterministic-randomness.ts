1abd4196a0fb00a6021ca871faeabccb53615e7c
Jonny Burger <jonathanburger11@gmail.com>
2021-02-16 20:10:37 +0100

    add eslint rule

---
+++ packages/eslint-plugin/src/rules/deterministic-randomness.ts
@@ -0,0 +1,56 @@
+import { ESLintUtils } from "@typescript-eslint/experimental-utils";
+
+const createRule = ESLintUtils.RuleCreator((name) => {
+  return `https://github.com/JonnyBurger/remotion`;
+});
+
+type Options = [];
+
+type MessageIds = "DeterministicRandomness";
+
+const DeterministicRandomness = [
+  "The result of Math.random() will change between frames while rendering because multiple threads are used.",
+  "Use the `random()` API from Remotion to get a deterministic pseudorandom value.",
+  "If you are sure you want a true random value, use `random(null)` to hide this warning.",
+].join("\n");
+
+export default createRule<Options, MessageIds>({
+  name: "determinstic-randomness",
+  meta: {
+    type: "problem",
+    docs: {
+      description: DeterministicRandomness,
+      category: "Best Practices",
+      recommended: "warn",
+    },
+    fixable: undefined,
+    schema: [],
+    messages: {
+      DeterministicRandomness: DeterministicRandomness,
+    },
+  },
+  defaultOptions: [],
+  create: (context) => {
+    return {
+      CallExpression: (node) => {
+        const callee = node.callee;
+        if (callee.type === "MemberExpression") {
+          if (
+            callee.object.type === "Identifier" &&
+            callee.object.name === "Math"
+          ) {
+            if (
+              callee.property.type === "Identifier" &&
+              callee.property.name === "random"
+            ) {
+              context.report({
+                messageId: "DeterministicRandomness",
+                node,
+              });
+            }
+          }
+        }
+      },
+    };
+  },
+});
