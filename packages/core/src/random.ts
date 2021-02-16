2e30f8ca43bdadd6ccb4be0ea9d00192720ae25d
Jonny Burger <jonathanburger11@gmail.com>
2021-02-16 16:17:49 +0100

    further enhance random function
    Former-commit-id: d582f6862905a3ac98dac4a3c66215ad31007788

--- packages/core/src/random.ts
+++ packages/core/src/random.ts
@@ -5,6 +5,28 @@ function mulberry32(a: number) {
 	return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
 }
 
-export const random = (num: number) => {
-	return mulberry32(num * 10000000000);
+function hashCode(str: string) {
+	let i = 0;
+	let chr = 0;
+	let hash = 0;
+
+	for (i = 0; i < str.length; i++) {
+		chr = str.charCodeAt(i);
+		hash = (hash << 5) - hash + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+}
+
+export const random = (input: number | string, dummy?: unknown) => {
+	if (dummy !== undefined) {
+		throw new TypeError('random() takes only one argument');
+	}
+	if (typeof input === 'string') {
+		return mulberry32(hashCode(input));
+	}
+	if (typeof input === 'number') {
+		return mulberry32(input * 10000000000);
+	}
+	throw new Error('random() argument must be a number or a string');
 };
