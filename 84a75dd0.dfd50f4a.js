1e80017ebc4a0351529ee4d3fd915de7aed462a8
JonnyBurger <jonathanburger11@gmail.com>
2021-02-16 20:26:09 +0000

    Deploying to gh-pages from  @ a943639c6f62c35cad8cc50a8839098019f1603d 🚀

---
+++ 84a75dd0.dfd50f4a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{96:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Remotion 1.3","permalink":"/blog/1-3"},{"title":"Remotion 1.2","permalink":"/blog/1-2"},{"title":"Remotion 1.1","permalink":"/blog/1-1"},{"title":"Introducing Remotion","permalink":"/blog/introducing-remotion"}]}')}}]);
