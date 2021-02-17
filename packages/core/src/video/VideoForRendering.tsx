1172edd39440abf5b04aa41199a89274d6b3e3de
Jonny Burger <jonathanburger11@gmail.com>
2021-02-17 22:36:44 +0100

    Update VideoForRendering.tsx
    Former-commit-id: a4dc5ac642cf5436b250642bb82eabac725afc3c

--- packages/core/src/video/VideoForRendering.tsx
+++ packages/core/src/video/VideoForRendering.tsx
@@ -1,17 +1,10 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useRef} from 'react';
 import {continueRender, delayRender} from '../ready-manager';
 import {useCurrentFrame} from '../use-frame';
 import {useUnsafeVideoConfig} from '../use-unsafe-video-config';
 import {RemotionVideoProps} from './props';
 
 export const VideoForRendering: React.FC<RemotionVideoProps> = (props) => {
-	const [metadataLoaded, setMetadataLoaded] = useState(false);
-	const [currentFrameSet, setCurrentFrameSet] = useState(false);
-
-	const [handle] = useState(() => {
-		return delayRender();
-	});
-
 	const currentFrame = useCurrentFrame();
 	const videoConfig = useUnsafeVideoConfig();
 	const videoRef = useRef<HTMLVideoElement>(null);
@@ -25,67 +18,31 @@ export const VideoForRendering: React.FC<RemotionVideoProps> = (props) => {
 		videoConfig.fps,
 	]);
 
-	const setFrame = useCallback(() => {
-		if (!videoRef.current) {
-			return;
-		}
-		videoRef.current.currentTime = frameInSeconds;
-		videoRef.current.addEventListener(
-			'seeked',
-			() => {
-				continueRender(handle);
-			},
-			{once: true}
-		);
-		setInterval(() => {
-			setCurrentFrameSet(true);
-		}, 0);
-	}, [frameInSeconds, handle]);
-
-	const onMetadataLoad = useCallback(() => {
-		setMetadataLoaded(true);
-	}, []);
-
-	useEffect(() => {
-		if (metadataLoaded) {
-			setFrame();
-		}
-	}, [metadataLoaded, currentFrameSet, setFrame]);
-
 	useEffect(() => {
 		if (!videoRef.current) {
 			return;
 		}
-		if (metadataLoaded) {
-			setFrame();
-			return;
+		console.log('delaying');
+		const handle = delayRender();
+		if (videoRef.current.currentTime === frameInSeconds) {
+			videoRef.current.addEventListener(
+				'loadeddata',
+				() => {
+					continueRender(handle);
+				},
+				{once: true}
+			);
+		} else {
+			videoRef.current.currentTime = frameInSeconds;
+			videoRef.current.addEventListener(
+				'seeked',
+				() => {
+					continueRender(handle);
+				},
+				{once: true}
+			);
 		}
-		const {current} = videoRef;
-		current.addEventListener('loadedmetadata', onMetadataLoad);
+	}, [currentFrame, frameInSeconds, videoConfig.fps]);
 
-		return (): void => {
-			current.removeEventListener('loadedmetadata', onMetadataLoad);
-		};
-	}, [currentFrame, metadataLoaded, onMetadataLoad, setFrame, videoConfig.fps]);
-
-	const onSetReadyState = useCallback(() => {
-		if (!videoRef.current) {
-			throw Error('No video ref');
-		}
-		if (videoRef.current.readyState === 4) {
-			setFrame();
-		}
-	}, [setFrame]);
-
-	useEffect(() => {
-		if (!videoRef.current) {
-			return;
-		}
-		const {current} = videoRef;
-		current.addEventListener('loadeddata', onSetReadyState);
-		return (): void => {
-			current.removeEventListener('loadeddata', onSetReadyState);
-		};
-	});
 	return <video ref={videoRef} {...props} />;
 };
