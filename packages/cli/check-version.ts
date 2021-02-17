bda0b76e08e7113b22236fb9a8a902fb93f4b7f7
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-02-18 01:51:25 +0530

    ported minimum node version checking to typescript
    Former-commit-id: 25e4e564da9ed7bedadaf7b91833a47aa7a2c756

--- packages/cli/check-version.ts
+++ packages/cli/check-version.ts
@@ -4,6 +4,8 @@ const current = process.version;
 const supported = require('./package.json').engines.node;
 
 if (!semver.satisfies(current, supported)) {
-  console.warn(`Required node version ${supported} not satisfied with current version ${current}.`);
-  console.warn(`Update your node version to ${supported}`);
+	console.warn(
+		`Required node version ${supported} not satisfied with current version ${current}.`
+	);
+	console.warn(`Update your node version to ${supported}`);
 }
