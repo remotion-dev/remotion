be53f97237393f598f42f8047baf73b7b303a07b
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-02-19 01:54:07 +0530

    Fixed showing floats when fps is not a Integer
    Former-commit-id: 8f926846619872549e037cd7bd24aa6361955fbf

--- packages/cli/src/editor/state/render-frame.ts
+++ packages/cli/src/editor/state/render-frame.ts
@@ -2,7 +2,7 @@ export const renderFrame = (frame: number, fps: number): string => {
 	const minutes = Math.floor(frame / fps / 60);
 	const remainingSec = frame - minutes * fps * 60;
 	const seconds = Math.floor(remainingSec / fps);
-	const frameAfterSec = frame % fps;
+	const frameAfterSec = Math.round(frame % fps);
 	return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
 		2,
 		'0'
