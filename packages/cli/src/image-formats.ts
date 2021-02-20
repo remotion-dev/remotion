15f97cc1d106013d923f01dfe36a96eabf19a509
Jonny Burger <jonathanburger11@gmail.com>
2021-02-20 09:51:11 +0100

    render webm in jpeg
    reason: looks good, just need to tweak the CRF to make it look good

--- packages/cli/src/image-formats.ts
+++ packages/cli/src/image-formats.ts
@@ -3,10 +3,16 @@ import {OutputFormat} from 'remotion';
 export type FrameFormat = 'png' | 'jpeg';
 
 export const getFrameFormat = (format: OutputFormat): FrameFormat => {
-	if (format === 'mp4' || format === 'mp4-h264' || format === 'mp4-h265') {
+	if (
+		format === 'mp4' ||
+		format === 'mp4-h264' ||
+		format === 'mp4-h265' ||
+		format === 'webm-v8' ||
+		format === 'webm-v9'
+	) {
 		return 'jpeg';
 	}
-	if (format === 'png' || format === 'webm-v8' || format === 'webm-v9') {
+	if (format === 'png') {
 		return 'png';
 	}
 	throw new Error('Unrecognized render mode ' + format);
