8adc06903ddbf277cf1bb5e2cee12c21c7a3ccc6
Jonny Burger <jonathanburger11@gmail.com>
2021-02-20 12:39:20 +0100

    update docs
    Former-commit-id: 034cc248ad849aa5a4f1981b784ed8ece642e541

--- packages/core/src/config/codec.ts
+++ packages/core/src/config/codec.ts
@@ -3,14 +3,14 @@ const validRenderModes = ['png', 'h264', 'h265', 'vp8', 'vp9'] as const;
 export type Codec = typeof validRenderModes[number];
 export type CodecOrUndefined = Codec | undefined;
 
-let format: CodecOrUndefined = undefined;
+let codec: CodecOrUndefined = undefined;
 
 export const getOutputCodecOrUndefined = (): CodecOrUndefined => {
-	return format;
+	return codec;
 };
 
 export const getFinalOutputCodec = ({
-	codec,
+	codec: inputCodec,
 	fileExtension,
 	emitWarning,
 }: {
@@ -18,7 +18,7 @@ export const getFinalOutputCodec = ({
 	fileExtension: string | null;
 	emitWarning: boolean;
 }): Codec => {
-	if (codec === undefined && fileExtension === 'webm') {
+	if (inputCodec === undefined && fileExtension === 'webm') {
 		if (emitWarning) {
 			console.info(
 				'You have specified a .webm extension, using the VP8 encoder. Use --codec=vp9 to use the Vp9 encoder.'
@@ -26,7 +26,7 @@ export const getFinalOutputCodec = ({
 		}
 		return 'vp8';
 	}
-	if (codec === undefined && fileExtension === 'hevc') {
+	if (inputCodec === undefined && fileExtension === 'hevc') {
 		if (emitWarning) {
 			console.info(
 				'You have specified a .hevc extension, using it using H265 encoder.'
@@ -34,12 +34,12 @@ export const getFinalOutputCodec = ({
 		}
 		return 'h265';
 	}
-	return codec ?? 'h264';
+	return inputCodec ?? 'h264';
 };
 
 export const setOutputFormat = (newRenderMode: CodecOrUndefined) => {
 	if (newRenderMode === undefined) {
-		format = undefined;
+		codec = undefined;
 		return;
 	}
 	if (!validRenderModes.includes(newRenderMode)) {
@@ -49,5 +49,5 @@ export const setOutputFormat = (newRenderMode: CodecOrUndefined) => {
 			)}, but got ${newRenderMode}`
 		);
 	}
-	format = newRenderMode;
+	codec = newRenderMode;
 };
