13452816e240be3fea1765ec0e5c00d75c51fec8
Jonny Burger <jonathanburger11@gmail.com>
2021-02-20 15:29:58 +0100

    parse ffmpeg progress

---
+++ packages/renderer/src/test/parse-ffmpeg-progress.test.ts
@@ -0,0 +1,11 @@
+import {parseFfmpegProgress} from '../parse-ffmpeg-progress';
+
+test('Should be able to parse FFMPEG progress', () => {
+	const result = parseFfmpegProgress('anything');
+	expect(result).toBe(undefined);
+	expect(
+		parseFfmpegProgress(
+			'frame=   34 fps=5.7 q=0.0 size=       0kB time=00:00:00.15 bitrate=  27.0kbits/s speed=0.0253x'
+		)
+	).toBe(34);
+});
