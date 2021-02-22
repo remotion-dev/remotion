72f55728cc246b595db6f0b61009d0a6ed3b159f
yuta0801 <yuta030801@gmail.com>
2021-02-22 17:24:20 +0900

    make useVideoConfig safe
    Former-commit-id: 919d64930d53aa5bdf43512779b3d083a5bbe9fa

--- packages/core/src/use-video-config.ts
+++ packages/core/src/use-video-config.ts
@@ -2,5 +2,11 @@ import {useUnsafeVideoConfig} from './use-unsafe-video-config';
 import {VideoConfig} from './video-config';
 
 export const useVideoConfig = (): VideoConfig => {
-	return useUnsafeVideoConfig() as VideoConfig;
+	const videoConfig = useUnsafeVideoConfig();
+
+	if (!videoConfig) {
+		throw new Error('No video config found');
+	}
+
+	return videoConfig;
 };
