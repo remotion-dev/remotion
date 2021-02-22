cffba1b68184d3ea6b14c32d3a166fb5ad4e2e40
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-02-22 20:13:05 +0530

    Implemented `puppeteer-core` over `puppeteer`

--- packages/renderer/src/get-compositions.ts
+++ packages/renderer/src/get-compositions.ts
@@ -1,10 +1,12 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 import {TCompMetadata} from 'remotion';
 
 export const getCompositions = async (
 	webpackBundle: string
 ): Promise<TCompMetadata[]> => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		executablePath: '/usr/bin/google-chrome-stable',
+	});
 	const page = await browser.newPage();
 
 	await page.goto(`file://${webpackBundle}/index.html?evaluation=true`);
