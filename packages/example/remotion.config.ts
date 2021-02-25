7d65fefccf2db2c5d007d722453ea9028ef5117b
Jonny Burger <jonathanburger11@gmail.com>
2021-02-25 11:12:54 +0100

    inline transparent example
    Former-commit-id: eec7ed512e0a7722da3c50d5fb16a137186ee5d8

--- packages/example/remotion.config.ts
+++ packages/example/remotion.config.ts
@@ -2,10 +2,7 @@ import os from 'os';
 import {Config} from 'remotion';
 
 Config.Rendering.setConcurrency(os.cpus().length);
-Config.Rendering.setImageFormat('png');
 Config.Output.setOverwriteOutput(true);
-Config.Output.setPixelFormat('yuva420p');
-Config.Output.setCodec('vp8');
 
 Config.Bundling.overrideWebpackConfig((currentConfiguration) => {
 	return {
