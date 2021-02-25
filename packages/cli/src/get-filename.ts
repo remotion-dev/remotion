f026b8f4b6f29fa2708c34618bc0547c87f0a18f
Jonny Burger <jonathanburger11@gmail.com>
2021-02-25 09:29:11 +0100

    better error message

--- packages/cli/src/get-filename.ts
+++ packages/cli/src/get-filename.ts
@@ -34,19 +34,25 @@ export const getOutputFilename = (
 	}
 	if (codec === 'h264') {
 		if (extension !== 'mp4') {
-			console.error('The output filename must end in .mp4.');
+			console.error(
+				'When using the H264 codec, the output filename must end in .mp4.'
+			);
 			process.exit(1);
 		}
 	}
 	if (codec === 'h265') {
 		if (extension !== 'mp4' && extension !== 'hevc') {
-			console.error('The output filename must end in .mp4 or .hevc.');
+			console.error(
+				'When using H265 codec, the output filename must end in .mp4 or .hevc.'
+			);
 			process.exit(1);
 		}
 	}
 	if (codec === 'vp8' || codec === 'vp9') {
 		if (extension !== 'webm') {
-			console.error('The output filename must end in .webm.');
+			console.error(
+				`When using the ${codec.toUpperCase()} codec, the output filename must end in .webm.`
+			);
 			process.exit(1);
 		}
 	}
