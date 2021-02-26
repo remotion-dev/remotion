772e0aac7393127ad01afa973fe0d3bf548c7e94
Jonny Burger <jonathanburger11@gmail.com>
2021-02-26 19:26:34 +0100

    Revert "Update index.tsx"
    This reverts commit aadaf89141814b2d35df8f2a450b0248bcbbc50f [formerly dc3635a1ef03a4d4503219b0aa67d4342c7954cc].
    
    
    Former-commit-id: cfe4aaeb8441e30f17d77ac82d3a139e340f17cd

--- packages/example/src/MissingImg/index.tsx
+++ packages/example/src/MissingImg/index.tsx
@@ -1,11 +1,5 @@
 import {Img} from 'remotion';
-import img from './img.png';
 
 export const MissingImg: React.FC = () => {
-	return (
-		<>
-			<Img src={img} />
-			<Img src="./img.png" />
-		</>
-	);
+	return <Img src="doesnotexist" />;
 };
