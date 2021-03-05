f2c65e2e530349cc275eacb416006faf076bb844
Jonny Burger <jonathanburger11@gmail.com>
2021-03-05 15:19:50 +0100

    Update get-frame-range.ts

--- packages/renderer/src/get-frame-range.ts
+++ packages/renderer/src/get-frame-range.ts
@@ -11,9 +11,24 @@ export const getFrameCount = (
 	}
 
 	if (typeof frameRange === 'number') {
+		if (frameRange < 0 || frameRange >= totalDuration) {
+			throw new Error(
+				`Frame number is out of range, must be between 0 and ${
+					totalDuration - 1
+				} but got ${frameRange}`
+			);
+		}
 		return 1;
 	}
 
 	const [start, end] = frameRange;
+
+	if (frameRange[1] >= totalDuration || frameRange[0] < 0) {
+		throw new Error(
+			`Frame range ${frameRange.join('-')} is not in between 0-${
+				totalDuration - 1
+			}`
+		);
+	}
 	return end - start + 1;
 };
