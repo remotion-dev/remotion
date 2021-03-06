6556388df4a97b88cff6f4f847d9c3f2668757e4
Jonny Burger <jonathanburger11@gmail.com>
2021-03-06 10:02:34 +0100

    add more tests, some failing
    Former-commit-id: 1c85e9db77db185cb86af3993ba8cc9660a6f83f

--- packages/core/src/test/interpolate.test.ts
+++ packages/core/src/test/interpolate.test.ts
@@ -1,25 +1,109 @@
+import {Easing} from '../easing';
 import {interpolate} from '../interpolate';
 import {expectToThrow} from './expect-to-throw';
 
-test('Interpolate test', () => {
+test('Basic interpolations', () => {
+	expect(interpolate(1, [0, 1], [0, 2])).toEqual(2);
+	expect(interpolate(Math.PI, [0, 1, 4, 9], [0, 2, 1000, -1000])).toEqual(
+		714.4364894275378
+	);
+});
+
+test('Must be the same length', () => {
 	expectToThrow(() => {
 		interpolate(1, [0, 2], [0, 1, 2]);
-	}, /must have the same length/);
-	expectToThrow(() => {
-		interpolate(1, [0], [0, 2]);
-	}, /must have at least 2 elements/);
+	}, /inputRange \(2\) and outputRange \(3\) must have the same length/);
+});
+
+test('Test against Infinity values', () => {
 	expectToThrow(() => {
 		interpolate(1, [-Infinity, Infinity], [0, 2]);
-	}, /cannot be \]-infinity;\+infinity\[/);
+	}, /inputRange must contain only finite numbers, but got \[-Infinity,Infinity\]/);
+});
+
+test('Must pass at least 2 elements', () => {
 	expectToThrow(() => {
-		interpolate(1, [0, 1], [9]);
-	}, /outputRange must have at least 2 elements/);
-	expectToThrow(() => {
-		interpolate(1, [0], [9, 2]);
+		interpolate(1, [0], [9]);
 	}, /inputRange must have at least 2 elements/);
+});
+
+test('Input range must be monotonically non-decreasing', () => {
 	expectToThrow(() => {
-		interpolate(1, [0, 1, 0.5], [0, 2]);
+		interpolate(1, [0, 1, 0.5], [0, 2, 0.2]);
 	}, /inputRange must be monotonically non-decreasing/);
-	expect(interpolate(1, [0, 1], [0, 2])).toEqual(2);
-	expect(interpolate(0.5, [0, 0.5, 1], [0, 0.1, 2])).toEqual(0.1);
+});
+
+test('Output range can be monotonically decreasing', () => {
+	expect(interpolate(0.75, [0, 0.5, 1], [0, 2, 0])).toEqual(1);
+});
+
+test('Cannot have Infinity in output Range', () => {
+	expectToThrow(
+		() => interpolate(1, [0, 1], [Infinity, 2]),
+		/outputRange must contain only finite numbers, but got [Infinity,2]/
+	);
+});
+
+test('Should throw if passing 2x infinity', () => {
+	expectToThrow(
+		() => interpolate(1, [Infinity, Infinity], [0, 2]),
+		/inputRange must contain only finite numbers, but got [Infinity,Infinity]/
+	);
+});
+
+test('Should throw on Infinity as third argument', () => {
+	expectToThrow(
+		() => interpolate(1, [0, 1, Infinity], [0, 2, 3]),
+		/inputRange must contain only finite numbers, but got [0,1,Infinity]/
+	);
+});
+
+test('Easing test', () => {
+	expect(
+		interpolate(0.5, [0, 1], [0, 1], {
+			easing: Easing.sin,
+		})
+	).toEqual(1 - Math.cos((0.5 * Math.PI) / 2));
+});
+
+test('Extrapolation left test', () => {
+	expect(
+		interpolate(-3, [0, 1, 2], [0, 0.5, 1], {
+			extrapolateRight: 'extend',
+		})
+	).toEqual(-1.5);
+	expect(interpolate(-3, [0, 1, 2], [0, 0.5, 1])).toEqual(-1.5);
+});
+
+test('Extrapolation right test', () => {
+	expect(
+		interpolate(3, [0, 1, 2], [0, 0.5, 1], {
+			extrapolateRight: 'extend',
+		})
+	).toEqual(1.5);
+	expect(interpolate(3, [0, 1, 2], [0, 0.5, 1])).toEqual(1.5);
+});
+
+test('Extrapolation identity', () => {
+	expect(
+		interpolate(1000, [0, 1, 2], [0, 2, 4], {
+			extrapolateRight: 'identity',
+		})
+	).toBe(1000);
+});
+
+test('Clamp right test', () => {
+	expect(
+		interpolate(2000, [0, 1, 1000], [0, 1, -1000], {
+			extrapolateRight: 'clamp',
+		})
+	).toEqual(-1000);
+});
+
+test('Clamp left test', () => {
+	expect(
+		interpolate(-2000, [0, 1, 1000], [Math.PI, 1, -1000], {
+			extrapolateLeft: 'clamp',
+		})
+	).toEqual(Math.PI);
 });
