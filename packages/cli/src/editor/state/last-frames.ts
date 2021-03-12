eb56639f7d30cbdfdb985e7e6c7418bf3660c8dd
jeetiss <jeetiss@yandex.ru>
2021-03-12 12:56:59 +0300

    isolate FpsCounter
    Former-commit-id: 86e2c62d41c88c96b603b8d36500fd5ff4351201

--- packages/cli/src/editor/state/last-frames.ts
+++
@@ -1,9 +0,0 @@
-let lastFrames: number[] = [];
-
-export const setLastFrames = (n: number[]) => {
-	lastFrames = n;
-	lastFrames = lastFrames.slice(Math.max(lastFrames.length - 15, 0));
-};
-export const getLastFrames = () => {
-	return lastFrames;
-};
