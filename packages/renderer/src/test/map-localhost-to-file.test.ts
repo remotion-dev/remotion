127a28ace9881507d7b388a4358988506597e454
Jonny Burger <jonathanburger11@gmail.com>
2021-03-15 22:23:20 +0100

    handle assets correctly

---
+++ packages/renderer/src/test/map-localhost-to-file.test.ts
@@ -0,0 +1,22 @@
+import {mapLocalhostAssetToFile} from '../assets/map-localhost-to-file';
+
+test('Should correctly map a localhost URL to an asset', () => {
+	expect(
+		mapLocalhostAssetToFile({
+			localhostAsset: {
+				type: 'video',
+				src: 'http://localhost:3000/5f25ba62771d1f8195f858ec5ff8e8d6.mp4',
+				id: '0.8331499681195862',
+				sequenceFrame: 2045,
+			},
+			webpackBundle:
+				'/var/folders/hl/p8pg9kw15dbg3l7dbpn0scc80000gn/T/react-motion-graphicstoSTC7',
+		})
+	).toEqual({
+		type: 'video',
+		src:
+			'file:///var/folders/hl/p8pg9kw15dbg3l7dbpn0scc80000gn/T/react-motion-graphicstoSTC7/5f25ba62771d1f8195f858ec5ff8e8d6.mp4',
+		id: '0.8331499681195862',
+		sequenceFrame: 2045,
+	});
+});
