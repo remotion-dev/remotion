c45e13b882564430510102edeb451083da0f99e7
Jonny Burger <jonathanburger11@gmail.com>
2021-03-16 18:44:09 +0100

    handle remote videos
    Former-commit-id: 1c1bf3b4ffc4f405bae8c61aac334f7da95c962d

--- packages/example/src/RemoteVideo/index.tsx
+++ packages/example/src/RemoteVideo/index.tsx
@@ -2,9 +2,12 @@ import {Video} from 'remotion';
 
 const RemoteVideo: React.FC = () => {
 	return (
-		<div>
-			<Video src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4" />
-		</div>
+		<Video
+			style={{
+				transform: `rotate(180deg)`,
+			}}
+			src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+		/>
 	);
 };
 
