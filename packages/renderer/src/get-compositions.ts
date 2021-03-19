04c62a51939a0c6816de72567f3605cc21ee7f4b
Felippe Chemello <felippechemello@gmail.com>
2021-03-18 21:57:36 -0300

    Testing with getUserProps at Root component

--- packages/renderer/src/get-compositions.ts
+++ packages/renderer/src/get-compositions.ts
@@ -4,14 +4,19 @@ import {serveStatic} from './serve-static';
 
 export const getCompositions = async (
 	webpackBundle: string,
-	browser: Browser = Internals.DEFAULT_BROWSER
+	browser: Browser = Internals.DEFAULT_BROWSER,
+	userProps: unknown
 ): Promise<TCompMetadata[]> => {
 	const browserInstance = await openBrowser(browser);
 	const page = await browserInstance.newPage();
 
 	const {port, close} = await serveStatic(webpackBundle);
 
-	await page.goto(`http://localhost:${port}/index.html?evaluation=true`);
+	await page.goto(
+		`http://localhost:${port}/index.html?evaluation=true&props=${encodeURIComponent(
+			JSON.stringify(userProps)
+		)}`
+	);
 	await page.waitForFunction('window.ready === true');
 	const result = await page.evaluate('window.getStaticCompositions()');
 	await close();
