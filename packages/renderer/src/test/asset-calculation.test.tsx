ad9c2e47fcb4caa68c8e48710ba7265b56374bcb
Jonny Burger <jonathanburger11@gmail.com>
2021-03-20 14:34:56 +0100

    refine
    Former-commit-id: 12f194cb496a2403564a56294050e71b4003c864

--- packages/renderer/src/test/asset-calculation.test.tsx
+++ packages/renderer/src/test/asset-calculation.test.tsx
@@ -1,28 +1,60 @@
 import React from 'react';
-import {Video} from 'remotion';
+import {Audio, TAsset, Video} from 'remotion';
 import {calculateAssetPositions} from '../assets/calculate-asset-positions';
 import {getAssetsForMarkup} from './get-assets-for-markup';
 
+const basicConfig = {
+	width: 1080,
+	height: 1080,
+	fps: 30,
+	durationInFrames: 60,
+};
+
+const withoutId = (asset: TAsset) => {
+	const {id, ...position} = asset;
+	return position;
+};
+
+const getPositions = async (Markup: React.FC) => {
+	const assets = await getAssetsForMarkup(Markup, basicConfig);
+	return calculateAssetPositions(assets);
+};
+
 test('Should be able to collect assets', async () => {
-	const Markup: React.FC = () => (
-		<div>
-			<Video src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4" />
-		</div>
-	);
-	const assets = await getAssetsForMarkup(Markup, {
-		width: 1080,
-		height: 1080,
-		fps: 30,
-		durationInFrames: 60,
-	});
-	const assetPositions = calculateAssetPositions(assets);
+	const assetPositions = await getPositions(() => (
+		<Video src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4" />
+	));
 	expect(assetPositions.length).toBe(1);
-	const firstPosition = assetPositions[0];
-	const {id, ...position} = firstPosition;
-	expect(position).toEqual({
+	expect(withoutId(assetPositions[0])).toEqual({
 		type: 'video',
 		src:
 			'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4',
+		sequenceFrame: 0,
+		duration: 60,
+		startInVideo: 0,
+	});
+});
+
+test('Should get multiple assets', async () => {
+	const assetPositions = await getPositions(() => (
+		<div>
+			<Video src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4" />
+			<Audio src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp3" />
+		</div>
+	));
+	expect(assetPositions.length).toBe(2);
+	expect(withoutId(assetPositions[0])).toEqual({
+		type: 'video',
+		src:
+			'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4',
+		sequenceFrame: 0,
+		duration: 60,
+		startInVideo: 0,
+	});
+	expect(withoutId(assetPositions[1])).toEqual({
+		type: 'audio',
+		src:
+			'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp3',
 		sequenceFrame: 0,
 		duration: 60,
 		startInVideo: 0,
