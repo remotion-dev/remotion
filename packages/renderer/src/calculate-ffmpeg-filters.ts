2aa4ff2069259de4e48388697db698f6b7db0d78
Jonny Burger <jonathanburger11@gmail.com>
2021-03-20 14:53:02 +0100

    split up filter logic
    Former-commit-id: 3275ee48e250c45d9e0cdca315b8d8e575bb3756

---
+++ packages/renderer/src/calculate-ffmpeg-filters.ts
@@ -0,0 +1,43 @@
+import {Internals} from 'remotion';
+import {AssetAudioDetails, Assets} from './assets/types';
+import {resolveAssetSrc} from './resolve-asset-src';
+
+export const calculateFfmpegFilters = ({
+	assetPositions,
+	assetAudioDetails,
+	fps,
+}: {
+	assetPositions: Assets;
+	assetAudioDetails: Map<string, AssetAudioDetails>;
+	fps: number;
+}) => {
+	return assetPositions
+		.map((asset, i) => {
+			const duration = (asset.duration / fps).toFixed(3); // in seconds with milliseconds level precision
+			const assetTrimLeft = (asset.trimLeft / fps).toFixed(3);
+			const assetTrimRight = ((asset.trimLeft + asset.duration) / fps).toFixed(
+				3
+			);
+			const startInVideo = ((asset.startInVideo / fps) * 1000).toFixed(); // in milliseconds
+			const audioDetails = assetAudioDetails.get(
+				resolveAssetSrc(asset.src)
+			) as AssetAudioDetails;
+
+			if (audioDetails.channels === 0) {
+				return null;
+			}
+			const streamIndex = i + 1;
+			return {
+				filter: [
+					`[${streamIndex}:a]`,
+					duration ? `atrim=${assetTrimLeft}:${assetTrimRight},` : '',
+					`adelay=${new Array(audioDetails.channels)
+						.fill(startInVideo)
+						.join('|')}`,
+					`[a${streamIndex}]`,
+				].join(''),
+				streamIndex,
+			};
+		})
+		.filter(Internals.truthy);
+};
