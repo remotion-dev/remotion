9af2c8a86dc976916a08475e581323e3bd70e95d
Jonny Burger <jonathanburger11@gmail.com>
2021-03-21 12:58:46 +0100

    flatten volume array tests

--- packages/renderer/src/assets/flatten-volume-array.ts
+++ packages/renderer/src/assets/flatten-volume-array.ts
@@ -4,9 +4,12 @@
 
 import {AssetVolume, MediaAsset} from './types';
 
-const flattenVolumeArray = (volume: AssetVolume): AssetVolume => {
+export const flattenVolumeArray = (volume: AssetVolume): AssetVolume => {
 	if (typeof volume === 'number') {
 		return volume;
+	}
+	if (volume.length === 0) {
+		throw new TypeError('Volume array must have at least 1 number');
 	}
 	if (new Set(volume).size === 1) {
 		return volume[0];
