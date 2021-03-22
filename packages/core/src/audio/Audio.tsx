81730488cfe51248598667e50607646987063e80
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-03-23 01:11:04 +0530

    add runtime validation for startAt and endAt prop
    Former-commit-id: a380882a85841c43923615881ea020afda889470

--- packages/core/src/audio/Audio.tsx
+++ packages/core/src/audio/Audio.tsx
@@ -8,6 +8,41 @@ export const Audio: React.FC<RemotionAudioProps & RemotionMainAudioProps> = (
 ) => {
 	const {startAt, endAt, ...otherProps} = props;
 	if (typeof startAt !== 'undefined' || typeof endAt !== 'undefined') {
+		if (typeof startAt !== 'undefined') {
+			if (typeof startAt !== 'number') {
+				throw new TypeError(
+					`type of startAt prop is a number, instead got type ${typeof startAt}.`
+				);
+			}
+			if (startAt < 0) {
+				throw new TypeError(
+					`startAt must be greater than equal to 0 instead got ${startAt}.`
+				);
+			}
+			if (isNaN(startAt) || startAt === Infinity) {
+				throw new TypeError('startAt prop can not be NaN or Infinity.');
+			}
+		}
+		if (typeof endAt !== 'undefined') {
+			if (typeof endAt !== 'number') {
+				throw new TypeError(
+					`type of endAt prop is a number, instead got type ${typeof endAt}.`
+				);
+			}
+			if (endAt <= 0) {
+				throw new TypeError(
+					`endAt must be a positive number, instead got ${endAt}.`
+				);
+			}
+			if (isNaN(endAt)) {
+				throw new TypeError('startAt propcan not be NaN.');
+			}
+		}
+		if (typeof startAt !== 'undefined' && typeof endAt !== 'undefined') {
+			if (endAt < startAt) {
+				throw new TypeError('endAt prop must be greater than startAt prop');
+			}
+		}
 		const startAtFrameNo = startAt ? startAt : 0;
 		const endAtFrameNO = endAt ? endAt : Infinity;
 		return (
