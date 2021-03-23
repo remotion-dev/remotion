1958fa24f580dc08b49d0d366e1527ecd23cda8a
Jonny Burger <jonathanburger11@gmail.com>
2021-03-23 10:36:42 +0100

    removed way to start with ts-node, now always use JS file
    Former-commit-id: f25d0ce6676b5b7ff5120ee3ee8b6e864ef2af47

--- packages/cli/src/preview.ts
+++ packages/cli/src/preview.ts
@@ -2,7 +2,6 @@ import {startServer} from '@remotion/bundler';
 // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
 // @ts-ignore
 import betterOpn from 'better-opn';
-import fs from 'fs';
 import path from 'path';
 import xns from 'xns';
 import {getConfigFileName} from './get-config-file-name';
@@ -13,13 +12,12 @@ export const previewCommand = xns(async () => {
 	const file = args[3];
 	const fullPath = path.join(process.cwd(), file);
 
-	const tsxFile = path.resolve(__dirname, 'previewEntry.tsx');
-	const jsFile = path.resolve(__dirname, 'previewEntry.js');
 	loadConfigFile(getConfigFileName());
 
-	const fileChosen = fs.existsSync(tsxFile) ? tsxFile : jsFile;
-
-	const port = await startServer(fileChosen, fullPath);
+	const port = await startServer(
+		path.resolve(__dirname, 'previewEntry.js'),
+		fullPath
+	);
 	betterOpn(`http://localhost:${port}`);
 	await new Promise(() => void 0);
 });
