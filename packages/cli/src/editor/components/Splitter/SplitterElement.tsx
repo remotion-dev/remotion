8408eed225a3b5987cd323d34579273806397926
Jonny Burger <jonathanburger11@gmail.com>
2021-03-23 14:11:01 +0100

    custom splitter

---
+++ packages/cli/src/editor/components/Splitter/SplitterElement.tsx
@@ -0,0 +1,16 @@
+import React, {useContext, useMemo} from 'react';
+import {SplitterContext} from './SplitterContext';
+
+export const SplitterElement: React.FC<{
+	type: 'flexer' | 'anti-flexer';
+}> = ({children, type}) => {
+	const context = useContext(SplitterContext);
+
+	const style: React.CSSProperties = useMemo(() => {
+		return {
+			flex: type === 'flexer' ? context.flexValue : 1 - context.flexValue,
+		};
+	}, [context.flexValue, type]);
+
+	return <div style={style}>{children}</div>;
+};
--- packages/cli/src/editor/state/timeline.ts
+++ packages/cli/src/editor/state/timeline.ts
@@ -1,15 +1,15 @@
 const localStorageKey = 'remotion.editor.timelineFlex';
 
-const persistTimelineFlex = (value = 0.2) => {
+const persistTimelineFlex = (value: number) => {
 	localStorage.setItem(localStorageKey, String(value));
 };
 
-const loadTimelineFlex = (): number => {
+const loadTimelineFlex = (): number | null => {
 	const item = localStorage.getItem(localStorageKey);
 
-	return item ? parseFloat(item) : 0.2;
+	return item ? parseFloat(item) : null;
 };
 
-export const useTimelineFlex = (): [number, (value?: number) => void] => {
+export const useTimelineFlex = (): [number | null, (value: number) => void] => {
 	return [loadTimelineFlex(), persistTimelineFlex];
 };
