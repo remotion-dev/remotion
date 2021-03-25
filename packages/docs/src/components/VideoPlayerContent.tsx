63730203d692febb5a39619489187dfd46499385
Jonny Burger <jonathanburger11@gmail.com>
2021-03-25 13:31:58 +0100

    sidebar
    Former-commit-id: 2f9e133ca02a4052e7f8a0e9de538d15d10230c1

--- packages/docs/src/components/VideoPlayerContent.tsx
+++ packages/docs/src/components/VideoPlayerContent.tsx
@@ -3,15 +3,24 @@ import React, { useRef } from "react";
 import styled from "styled-components";
 import { ShowcaseVideo } from "../data/showcase-videos";
 import { useElementSize } from "../helpers/use-el-size";
+import { VideoSidebar } from "./VideoSidebar";
 
 export const Container = styled.div`
   background-color: white;
   margin-bottom: 0;
+  display: flex;
+  flex-direction: row;
 `;
 
 const Video = styled.video`
   margin-bottom: 0;
   background-color: white;
+`;
+
+const RESERVED_FOR_SIDEBAR = 300;
+
+const Sidebar = styled.div`
+  width: ${RESERVED_FOR_SIDEBAR}px;
 `;
 
 export const VideoPlayerContent: React.FC<{ video: ShowcaseVideo }> = ({
@@ -22,7 +31,7 @@ export const VideoPlayerContent: React.FC<{ video: ShowcaseVideo }> = ({
 
   const containerSize = useElementSize(document.body);
 
-  const containerWidth = Math.min(containerSize?.width ?? 0, 1200);
+  const containerWidth = Math.min(containerSize?.width ?? 0, 1200) - 200;
   const containerHeight = Math.min(containerSize?.height ?? 0, 800);
 
   const heightRatio = (containerHeight ?? 0) / video.height;
@@ -36,6 +45,9 @@ export const VideoPlayerContent: React.FC<{ video: ShowcaseVideo }> = ({
   return (
     <Container ref={container}>
       <Video src={vidUrl} height={height} width={width} autoPlay></Video>
+      <Sidebar>
+        <VideoSidebar video={video}></VideoSidebar>
+      </Sidebar>
     </Container>
   );
 };
