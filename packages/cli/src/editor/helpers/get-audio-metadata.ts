7fb36e8c331d4b116f88af3e25e89d29e2c1d642
Jonny Burger <jonathanburger11@gmail.com>
2021-03-25 19:33:58 +0100

    nice
    Former-commit-id: c48f0863b8d4be6c777d41059be3bd2e8dc89137

--- packages/cli/src/editor/helpers/get-audio-metadata.ts
+++ packages/cli/src/editor/helpers/get-audio-metadata.ts
@@ -1,32 +1,10 @@
-type AudioContextMetadata = {
-	waveform: Float32Array;
+export type AudioContextMetadata = {
+	channelWaveforms: Float32Array[];
 	sampleRate: number;
 	duration: number;
 	numberOfChannels: number;
 };
-
-export const getAudioDuration = (src: string) => {
-	const audio = document.createElement('audio');
-	audio.src = src;
-
-	return new Promise<number>((resolve, reject) => {
-		const onError = (ev: ErrorEvent) => {
-			reject(ev.error);
-			cleanup();
-		};
-		const onLoadedMetadata = () => {
-			resolve(audio.duration);
-			cleanup();
-		};
-		const cleanup = () => {
-			audio.removeEventListener('loadedmetadata', onLoadedMetadata);
-			audio.removeEventListener('error', onError);
-		};
-
-		audio.addEventListener('loadedmetadata', onLoadedMetadata, {once: true});
-		audio.addEventListener('error', onError, {once: true});
-	});
-};
+const metadataCache: {[key: string]: AudioContextMetadata} = {};
 
 export const getWaveform = async (
 	arrayBuffer: ArrayBuffer
@@ -38,21 +16,43 @@ export const getWaveform = async (
 	return channelData;
 };
 
+export const combineWaveforms = (waveforms: Float32Array[]) => {
+	if (waveforms.length === 0) {
+		return null;
+	}
+	const combinedWaveform = Float32Array.of(waveforms[0].length);
+	for (let i = 0; i < waveforms[0].length; i++) {
+		combinedWaveform[i] = waveforms.map((w) => w[i]).reduce((a, b) => a + b, 0);
+	}
+
+	return combinedWaveform;
+};
+
 export const getAudioMetadata = async (
 	src: string
 ): Promise<AudioContextMetadata> => {
+	if (metadataCache[src]) {
+		return metadataCache[src];
+	}
 	const audioContext = new AudioContext();
 
 	const response = await fetch(src);
 	const arrayBuffer = await response.arrayBuffer();
 
 	const wave = await audioContext.decodeAudioData(arrayBuffer);
-	const channelData = wave.getChannelData(0);
 
-	return {
-		waveform: channelData,
+	const channelWaveforms = new Array(wave.numberOfChannels)
+		.fill(true)
+		.map((_, channel) => {
+			return wave.getChannelData(channel);
+		});
+
+	const metadata: AudioContextMetadata = {
+		channelWaveforms,
 		sampleRate: audioContext.sampleRate,
 		duration: wave.duration,
 		numberOfChannels: wave.numberOfChannels,
 	};
+	metadataCache[src] = metadata;
+	return metadata;
 };
