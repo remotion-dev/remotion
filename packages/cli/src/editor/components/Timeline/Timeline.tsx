cad38d87d869e57434f06289ad47f952fe3a8339
Jonny Burger <jonathanburger11@gmail.com>
2021-03-26 17:53:14 +0100

    fix layout bugs
    Former-commit-id: 5c825f246166c8088b148c62884f1a108128a122

--- packages/cli/src/editor/components/Timeline/Timeline.tsx
+++ packages/cli/src/editor/components/Timeline/Timeline.tsx
@@ -2,6 +2,7 @@ import React, {useContext, useMemo} from 'react';
 import {Internals} from 'remotion';
 import styled from 'styled-components';
 import {calculateTimeline, Track} from '../../helpers/calculate-timeline';
+import {TIMELINE_LAYER_HEIGHT} from '../../helpers/timeline-layout';
 import {SplitterContainer} from '../Splitter/SplitterContainer';
 import {SplitterElement} from '../Splitter/SplitterElement';
 import {SplitterHandle} from '../Splitter/SplitterHandle';
@@ -14,6 +15,8 @@ const Container = styled.div`
 	min-height: 100%;
 	flex: 1;
 	display: flex;
+	height: 0;
+	overflow: auto;
 `;
 
 export const Timeline: React.FC = () => {
@@ -30,29 +33,39 @@ export const Timeline: React.FC = () => {
 		});
 	}, [sequences, videoConfig]);
 
+	const inner: React.CSSProperties = useMemo(() => {
+		return {
+			height: timeline.length * (TIMELINE_LAYER_HEIGHT + 2),
+			display: 'flex',
+			flex: 1,
+		};
+	}, [timeline.length]);
+
 	if (!videoConfig) {
 		return null;
 	}
 
 	return (
 		<Container>
-			<SplitterContainer
-				orientation="vertical"
-				defaultFlex={0.2}
-				id="names-to-timeline"
-				maxFlex={0.5}
-				minFlex={0.15}
-			>
-				<SplitterElement type="flexer">
-					<TimelineList timeline={timeline} />
-				</SplitterElement>
-				<SplitterHandle />
-				<SplitterElement type="anti-flexer">
-					<TimelineTracks timeline={timeline} fps={videoConfig.fps} />
-					<TimelineSlider />
-					<TimelineDragHandler />
-				</SplitterElement>
-			</SplitterContainer>
+			<div style={inner}>
+				<SplitterContainer
+					orientation="vertical"
+					defaultFlex={0.2}
+					id="names-to-timeline"
+					maxFlex={0.5}
+					minFlex={0.15}
+				>
+					<SplitterElement type="flexer">
+						<TimelineList timeline={timeline} />
+					</SplitterElement>
+					<SplitterHandle />
+					<SplitterElement type="anti-flexer">
+						<TimelineTracks timeline={timeline} fps={videoConfig.fps} />
+						<TimelineSlider />
+						<TimelineDragHandler />
+					</SplitterElement>
+				</SplitterContainer>
+			</div>
 		</Container>
 	);
 };
