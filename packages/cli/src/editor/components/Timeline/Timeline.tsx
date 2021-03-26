8fe852dbffcba22736a2acdb1d1fd098acd092dc
Jonny Burger <jonathanburger11@gmail.com>
2021-03-26 17:39:13 +0100

    can drag everywhere
    Former-commit-id: eaf80e850152641d5277cf2db21204bdc1944a91

--- packages/cli/src/editor/components/Timeline/Timeline.tsx
+++ packages/cli/src/editor/components/Timeline/Timeline.tsx
@@ -2,14 +2,13 @@ import React, {useContext, useMemo} from 'react';
 import {Internals} from 'remotion';
 import styled from 'styled-components';
 import {calculateTimeline, Track} from '../../helpers/calculate-timeline';
-import {TIMELINE_LAYER_HEIGHT} from '../../helpers/timeline-layout';
 import {SplitterContainer} from '../Splitter/SplitterContainer';
 import {SplitterElement} from '../Splitter/SplitterElement';
 import {SplitterHandle} from '../Splitter/SplitterHandle';
 import {TimelineDragHandler} from './TimelineDragHandler';
 import {TimelineList} from './TimelineList';
-import {TimelineSequence} from './TimelineSequence';
 import {TimelineSlider} from './TimelineSlider';
+import {TimelineTracks} from './TimelineTracks';
 
 const Container = styled.div`
 	min-height: 100%;
@@ -31,12 +30,6 @@ export const Timeline: React.FC = () => {
 		});
 	}, [sequences, videoConfig]);
 
-	const inner: React.CSSProperties = useMemo(() => {
-		return {
-			height: TIMELINE_LAYER_HEIGHT + 2,
-		};
-	}, []);
-
 	if (!videoConfig) {
 		return null;
 	}
@@ -55,14 +48,9 @@ export const Timeline: React.FC = () => {
 				</SplitterElement>
 				<SplitterHandle />
 				<SplitterElement type="anti-flexer">
-					<TimelineDragHandler>
-						{timeline.map((track) => (
-							<div key={track.sequence.id} style={inner}>
-								<TimelineSequence fps={videoConfig.fps} s={track.sequence} />
-							</div>
-						))}
-					</TimelineDragHandler>
+					<TimelineTracks timeline={timeline} fps={videoConfig.fps} />
 					<TimelineSlider />
+					<TimelineDragHandler />
 				</SplitterElement>
 			</SplitterContainer>
 		</Container>
