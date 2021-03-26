27b926fb53a969d86dfaa039c752320ebe56c359
Jonny Burger <jonathanburger11@gmail.com>
2021-03-26 16:08:34 +0100

    hook

--- packages/cli/src/editor/components/Timeline/TimelineListItem.tsx
+++ packages/cli/src/editor/components/Timeline/TimelineListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {TSequence} from 'remotion';
 import {
 	TIMELINE_LAYER_HEIGHT,
@@ -10,14 +10,51 @@ const outer: React.CSSProperties = {
 	color: 'white',
 	fontFamily: 'Arial, Helvetica, sans-serif',
 	display: 'flex',
-	flexDirection: 'column',
-	justifyContent: 'center',
+	flexDirection: 'row',
+	alignItems: 'center',
 	fontSize: 14,
 	paddingLeft: TIMELINE_PADDING,
+};
+
+const hookContainer: React.CSSProperties = {
+	height: TIMELINE_LAYER_HEIGHT,
+	width: 10,
+	position: 'relative',
+};
+
+const hook: React.CSSProperties = {
+	borderLeft: '1px solid white',
+	borderBottom: '1px solid white',
+	borderBottomLeftRadius: 7,
+	height: TIMELINE_LAYER_HEIGHT,
+	width: 10,
+	position: 'absolute',
+	bottom: TIMELINE_LAYER_HEIGHT / 2,
+};
+
+const space: React.CSSProperties = {
+	width: 5,
 };
 
 export const TimelineListItem: React.FC<{
 	sequence: TSequence;
 }> = ({sequence}) => {
-	return <div style={outer}>{sequence.displayName}</div>;
+	const leftOffset = sequence.parent ? 20 : 0;
+
+	const padder = useMemo((): React.CSSProperties => {
+		return {
+			width: leftOffset,
+		};
+	}, [leftOffset]);
+
+	return (
+		<div style={outer}>
+			<div style={padder} />
+			<div style={hookContainer}>
+				<div style={hook} />
+			</div>
+			<div style={space} />
+			{sequence.displayName}
+		</div>
+	);
 };
