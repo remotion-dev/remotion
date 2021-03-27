55fffad3ccaa10f4787adf10cfac7b860140c1bd
Tobias Boner <tobiasboner@hispeed.ch>
2021-03-27 23:43:30 +0100

    improved tests - removed test smells
    Former-commit-id: ba4be8e57140ab9096fd24c6be39b39e83851cb7

--- packages/core/src/test/concurrency.test.ts
+++ packages/core/src/test/concurrency.test.ts
@@ -1,22 +1,32 @@
-import {getConcurrency, setConcurrency} from '../config/concurrency';
+import {
+	Concurrency,
+	getConcurrency,
+	setConcurrency,
+} from '../config/concurrency';
 import {expectToThrow} from './expect-to-throw';
 
-test('setConcurrency should throw if concurrency is not a number or null', () => {
+const invalidConcurrency: String = 'invalidConcurrency';
+let defaultConcurrency: Concurrency;
+
+beforeAll(() => {
+	defaultConcurrency = getConcurrency();
+});
+
+test('setConcurrency should throw if concurrency is not a number', () => {
 	expectToThrow(
 		() =>
-			setConcurrency(
-				// @ts-expect-error
-				'invalidConcurrency'
-			),
+			// @ts-expect-error
+			setConcurrency(invalidConcurrency),
 		/--concurrency flag must be a number./
 	);
 });
-
-test('getConcurrency should return null by default', () => {
-	expect(getConcurrency()).toEqual(null);
-});
-
-test('setConcurrency should NOT throw if concurrency is a boolean value', () => {
+test('setConcurrency should NOT throw if concurrency is a number', () => {
 	expect(() => setConcurrency(50)).not.toThrow();
-	expect(getConcurrency()).toEqual(50);
+});
+test('getConcurrency should return null by default', () => {
+	expect(defaultConcurrency === null);
+});
+test('getConcurrency should return number', () => {
+	setConcurrency(100);
+	expect(getConcurrency()).toEqual(100);
 });
