a1e462525a282ab37ae09c390d4f9e5a5f2b3cd6
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-03-28 01:46:21 +0530

    removed the play pause logic from cli

--- packages/cli/src/editor/components/PlayPause.tsx
+++ packages/cli/src/editor/components/PlayPause.tsx
@@ -1,4 +1,5 @@
-import React, {useCallback, useEffect, useRef} from 'react';
+import {usePlaybackTime} from '@remotion/player';
+import React, {useEffect} from 'react';
 import {Internals} from 'remotion';
 import {Pause} from '../icons/pause';
 import {Play} from '../icons/play';
@@ -7,69 +8,17 @@ import {StepForward} from '../icons/step-forward';
 import {ControlButton} from './ControlButton';
 
 export const PlayPause: React.FC = () => {
-	const [playing, setPlaying] = Internals.Timeline.usePlayingState();
+	const [playing] = Internals.Timeline.usePlayingState();
 	const frame = Internals.Timeline.useTimelinePosition();
-	const setFrame = Internals.Timeline.useTimelineSetFrame();
 	const video = Internals.useVideo();
-	const config = Internals.useUnsafeVideoConfig();
 
-	const toggle = useCallback(() => {
-		if (!video) {
-			return null;
-		}
-		setPlaying((p) => {
-			return !p;
-		});
-	}, [video, setPlaying]);
-
-	const frameBack = useCallback(() => {
-		if (!video) {
-			return null;
-		}
-		if (playing) {
-			return;
-		}
-		if (frame === 0) {
-			return;
-		}
-
-		setFrame((f) => f - 1);
-	}, [frame, playing, setFrame, video]);
-
-	const isLastFrame = frame === (config?.durationInFrames ?? 1) - 1;
-
-	const frameForward = useCallback(() => {
-		if (!video) {
-			return null;
-		}
-		if (playing) {
-			return;
-		}
-
-		if (isLastFrame) {
-			return;
-		}
-
-		setFrame((f) => f + 1);
-	}, [isLastFrame, playing, setFrame, video]);
-
-	const onKeyPress = useCallback(
-		(e: KeyboardEvent) => {
-			if (e.code === 'Space') {
-				toggle();
-				e.preventDefault();
-			}
-			if (e.code === 'ArrowLeft') {
-				frameBack();
-				e.preventDefault();
-			}
-			if (e.code === 'ArrowRight') {
-				frameForward();
-				e.preventDefault();
-			}
-		},
-		[frameBack, frameForward, toggle]
-	);
+	const [
+		toggle,
+		frameBack,
+		frameForward,
+		onKeyPress,
+		isLastFrame,
+	] = usePlaybackTime();
 
 	useEffect(() => {
 		window.addEventListener('keydown', onKeyPress);
@@ -77,44 +26,6 @@ export const PlayPause: React.FC = () => {
 			window.removeEventListener('keydown', onKeyPress);
 		};
 	}, [onKeyPress]);
-
-	const frameRef = useRef(frame);
-	frameRef.current = frame;
-	useEffect(() => {
-		if (!config) {
-			return;
-		}
-		if (!playing) {
-			return;
-		}
-
-		let hasBeenStopped = false;
-		let reqAnimFrameCall: number | null = null;
-		const startedTime = performance.now();
-		const startedFrame = frameRef.current;
-
-		const callback = () => {
-			const time = performance.now() - startedTime;
-			const calculatedFrame =
-				(Math.round(time / (1000 / config.fps)) + startedFrame) %
-				config.durationInFrames;
-			if (calculatedFrame !== frameRef.current) {
-				setFrame(calculatedFrame);
-			}
-			if (!hasBeenStopped) {
-				reqAnimFrameCall = requestAnimationFrame(callback);
-			}
-		};
-
-		reqAnimFrameCall = requestAnimationFrame(callback);
-
-		return () => {
-			hasBeenStopped = true;
-			if (reqAnimFrameCall !== null) {
-				cancelAnimationFrame(reqAnimFrameCall);
-			}
-		};
-	}, [config, setFrame, playing]);
 
 	return (
 		<>
@@ -131,7 +42,7 @@ export const PlayPause: React.FC = () => {
 					}}
 				/>
 			</ControlButton>
-			
+
 			<ControlButton
 				aria-label={playing ? 'Pause' : 'Play'}
 				disabled={!video}
