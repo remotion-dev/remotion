824f54ceb7314755dc306324708778d8830132f9
Jonas Brunner <jonas.brunner@outlook.com>
2021-03-29 15:49:00 +0200

    better naming of tests
    Former-commit-id: a5ea747ec3a21b28823eec9bc168a1e29e764684

--- packages/core/src/test/Img.test.tsx
+++ packages/core/src/test/Img.test.tsx
@@ -2,19 +2,17 @@ import {render} from '@testing-library/react';
 import React from 'react';
 import {Img} from '../Img';
 
-describe('Img test', () => {
-	const ref = React.createRef<HTMLImageElement>();
-	const testImgUrl = 'https://source.unsplash.com/random/50x50';
+const ref = React.createRef<HTMLImageElement>();
+const testImgUrl = 'https://source.unsplash.com/random/50x50';
 
-	beforeEach(() => {
-		render(<Img ref={ref} src={testImgUrl} />);
-	});
+beforeEach(() => {
+	render(<Img ref={ref} src={testImgUrl} />);
+});
 
-	test('renders img tag', () => {
-		expect(ref.current?.tagName).toBe('IMG');
-	});
+test('Img component renders img tag', () => {
+	expect(ref.current?.tagName).toBe('IMG');
+});
 
-	test('forwards src', () => {
-		expect(ref.current).toHaveProperty('src', testImgUrl);
-	});
+test('Src attribute is forwarded to img tag', () => {
+	expect(ref.current).toHaveProperty('src', testImgUrl);
 });
