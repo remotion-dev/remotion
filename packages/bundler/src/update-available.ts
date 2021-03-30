90cfd578fb4e704a96cf6545af1c71e427500df0
Jonny Burger <jonathanburger11@gmail.com>
2021-03-30 18:51:28 +0200

    3 second time out for update endpotin
    Former-commit-id: cd376fab2caf810e74fcb2cd0c2f9781f9c36ec1

--- packages/bundler/src/update-available.ts
+++ packages/bundler/src/update-available.ts
@@ -5,15 +5,31 @@ export type Info = {
 	currentVersion: string;
 	latestVersion: string;
 	updateAvailable: boolean;
+	timedOut: boolean;
 };
 
-export const isUpdateAvailable = async (): Promise<Info> => {
-	const packageJson = require('../package.json');
+const isUpdateAvailable = async (currentVersion: string): Promise<Info> => {
 	const latest = await latestVersion('@remotion/bundler');
-	const {version} = packageJson;
 	return {
-		updateAvailable: semver.lt(version, latest),
-		currentVersion: version,
+		updateAvailable: semver.lt(currentVersion, latest),
+		currentVersion,
 		latestVersion: latest,
+		timedOut: false,
 	};
+};
+
+export const isUpdateAvailableWithTimeout = () => {
+	const packageJson = require('../package.json');
+	const {version} = packageJson;
+	const threeSecTimeout = new Promise<Info>((resolve) => {
+		setTimeout(() => {
+			resolve({
+				currentVersion: version,
+				latestVersion: version,
+				updateAvailable: false,
+				timedOut: true,
+			});
+		}, 3000);
+	});
+	return Promise.race([threeSecTimeout, isUpdateAvailable(version)]);
 };
