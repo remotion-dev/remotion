2abb5cd9c9377864623fc3c4ebfcd6a499cfd854
jeetiss <jeetiss@yandex.ru>
2021-03-30 15:40:31 +0300

    add onLoad, onError callbacks and forwardRef

--- packages/gif/src/props.ts
+++ packages/gif/src/props.ts
@@ -2,6 +2,14 @@ export type RemotionGifProps = {
 	src: string;
 	width?: number;
 	height?: number;
+	onLoad?: (info: {
+		loaded: true;
+		width: number;
+		height: number;
+		delays: number[];
+		frames: ImageData[];
+	}) => void;
+	onError?: (error: Error) => void;
 	fit?: 'contain' | 'fill' | 'cover';
 	style?: React.CSSProperties;
 };
