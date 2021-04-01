9f5c579bbe26365e4d40551a55920c7cd58bb75e
Jonny Burger <jonathanburger11@gmail.com>
2021-04-01 16:01:55 +0200

    audio corrections

--- packages/example/src/AudioTesting/index.tsx
+++ packages/example/src/AudioTesting/index.tsx
@@ -1,22 +1,20 @@
-import {Audio, interpolate, Sequence} from 'remotion';
+import {Audio, interpolate} from 'remotion';
 import music from './music.mp3';
 
 const AudioTesting: React.FC = () => {
 	return (
 		<div>
-			<Sequence from={100} durationInFrames={Infinity}>
-				<Audio
-					startAt={100}
-					endAt={200}
-					src={music}
-					volume={(f) =>
-						interpolate(f, [0, 50, 100], [0, 1, 0], {
-							extrapolateLeft: 'clamp',
-							extrapolateRight: 'clamp',
-						})
-					}
-				/>
-			</Sequence>
+			<Audio
+				startAt={100}
+				endAt={200}
+				src={music}
+				volume={(f) =>
+					interpolate(f, [0, 50, 100], [0, 1, 0], {
+						extrapolateLeft: 'clamp',
+						extrapolateRight: 'clamp',
+					})
+				}
+			/>
 		</div>
 	);
 };
