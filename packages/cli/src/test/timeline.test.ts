35792547e016ea6fea798704425fef20c53181d7
Jonny Burger <jonathanburger11@gmail.com>
2021-04-02 20:07:06 +0200

    start testing timlein
    Former-commit-id: 186f59797a070ba7414039df9489e6a62765ffc9

---
+++ packages/cli/src/test/timeline.test.ts
@@ -0,0 +1,56 @@
+import {calculateTimeline} from '../editor/helpers/calculate-timeline';
+
+test('Should calculate timeline with no sequences', () => {
+	const calculated = calculateTimeline({
+		sequences: [],
+		sequenceDuration: 100,
+	});
+	expect(calculated).toEqual([
+		{
+			depth: 0,
+			sequence: {
+				displayName: '',
+				duration: 100,
+				from: 0,
+				id: 'seq',
+				parent: null,
+				rootId: '-',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+		},
+	]);
+});
+
+test('Should calculate a basic timline', () => {
+	const calculated = calculateTimeline({
+		sequences: [
+			{
+				displayName: 'Audio',
+				duration: 100,
+				from: 0,
+				id: '0.1',
+				parent: null,
+				rootId: '0.1',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+		],
+		sequenceDuration: 100,
+	});
+	expect(calculated).toEqual([
+		{
+			depth: 0,
+			sequence: {
+				displayName: 'Audio',
+				duration: 100,
+				from: 0,
+				id: '0.1',
+				parent: null,
+				rootId: '0.1',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+		},
+	]);
+});
