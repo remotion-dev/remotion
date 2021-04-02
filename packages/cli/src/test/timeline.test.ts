17b5d0baaf2c0510785a51c7233bf0d61cc17cb3
Jonny Burger <jonathanburger11@gmail.com>
2021-04-02 20:09:24 +0200

    timeline order test (failing)

--- packages/cli/src/test/timeline.test.ts
+++ packages/cli/src/test/timeline.test.ts
@@ -54,3 +54,59 @@ test('Should calculate a basic timline', () => {
 		},
 	]);
 });
+
+test('Should follow order of nesting', () => {
+	const calculated = calculateTimeline({
+		sequences: [
+			{
+				displayName: 'Audio',
+				duration: 100,
+				from: 0,
+				id: '0.2',
+				parent: '0.1',
+				rootId: '0.1',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+			{
+				displayName: 'Audio',
+				duration: 100,
+				from: 0,
+				id: '0.1',
+				parent: null,
+				rootId: '0.1',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+		],
+		sequenceDuration: 100,
+	});
+	expect(calculated).toEqual([
+		{
+			depth: 0,
+			sequence: {
+				displayName: 'Audio',
+				duration: 100,
+				from: 0,
+				id: '0.1',
+				parent: null,
+				rootId: '0.1',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+		},
+		{
+			depth: 1,
+			sequence: {
+				displayName: 'Audio',
+				duration: 100,
+				from: 0,
+				id: '0.2',
+				parent: '0.1',
+				rootId: '0.1',
+				showInTimeline: true,
+				type: 'sequence',
+			},
+		},
+	]);
+});
