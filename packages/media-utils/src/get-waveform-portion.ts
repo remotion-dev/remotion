109517d9a58fc6fd099afde635931747f91f27a0
Jonny Burger <jonathanburger11@gmail.com>
2021-04-03 13:13:26 +0200

    media utils package

---
+++ packages/media-utils/src/get-waveform-portion.ts
@@ -0,0 +1,40 @@
+import {getWaveformSamples} from './get-wave-form-samples';
+import {AudioContextMetadata} from './types';
+
+type Bar = {
+	index: number;
+	amplitude: number;
+};
+
+export const getWaveformPortion = ({
+	metadata,
+	startFrom,
+	fps,
+	durationInFrames,
+	numberOfSamples,
+}: {
+	metadata: AudioContextMetadata;
+	startFrom: number;
+	fps: number;
+	durationInFrames: number;
+	numberOfSamples: number;
+}): Bar[] => {
+	const startSample = Math.floor(
+		(startFrom / (metadata.duration / fps)) *
+			metadata.channelWaveforms[0].length
+	);
+	const endSample = Math.floor(
+		((startFrom + durationInFrames) / (metadata.duration * fps)) *
+			metadata.channelWaveforms[0].length
+	);
+
+	return getWaveformSamples(
+		metadata.channelWaveforms[0].slice(startSample, endSample),
+		numberOfSamples
+	).map((w, i) => {
+		return {
+			index: i,
+			amplitude: w,
+		};
+	});
+};
---
+++ packages/media-utils/src/index.ts
@@ -0,0 +1,5 @@
+export {getAudioDuration} from './get-audio-duration';
+export {getAudioMetadata} from './get-audio-metadata';
+export {getWaveformSamples} from './get-wave-form-samples';
+export {getWaveformPortion} from './get-waveform-portion';
+export type {AudioContextMetadata} from './types';
