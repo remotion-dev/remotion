520b97ccb331967e713ae5529af0db114393368c
Jonny Burger <jonathanburger11@gmail.com>
2021-04-03 17:37:12 +0200

    handle component calling useAudioData not mounted anymore
    Former-commit-id: 705bf0577418c1c37435e18da96ac057b673e3d5

--- packages/media-utils/src/use-audio-metadata.ts
+++ packages/media-utils/src/use-audio-metadata.ts
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 import {continueRender, delayRender} from 'remotion';
 import {getAudioData} from '.';
 import {AudioData} from './types';
@@ -7,12 +7,24 @@ export const useAudioData = (src: string): AudioData | null => {
 	if (!src) {
 		throw new TypeError("useAudioMetadata requires a 'src' parameter");
 	}
+	const mountState = useRef({isMounted: true});
+
+	useEffect(() => {
+		const {current} = mountState;
+		current.isMounted = true;
+		return () => {
+			current.isMounted = false;
+		};
+	}, []);
+
 	const [metadata, setMetadata] = useState<AudioData | null>(null);
 
 	const fetchMetadata = useCallback(async () => {
 		const handle = delayRender();
 		const data = await getAudioData(src);
-		setMetadata(data);
+		if (mountState.current.isMounted) {
+			setMetadata(data);
+		}
 		continueRender(handle);
 	}, [src]);
 
