37e61083e68a6642570f49576d47e6027b58ee3e
Jonny Burger <jonathanburger11@gmail.com>
2021-04-04 13:01:01 +0200

    visualize audio
    Former-commit-id: 8e61a0caecc6cec9cabd10f96f6f0ff3e88f38ab

--- packages/media-utils/src/fft/get-visualization.ts
+++ packages/media-utils/src/fft/get-visualization.ts
@@ -27,10 +27,19 @@ export const getVisualization = ({
 	if (!fps) {
 		throw new TypeError('The argument "fps" was not provided');
 	}
+	if (data.length < sampleSize) {
+		throw new TypeError(
+			'Audio data is not big enough to provide ' + sampleSize + ' bars.'
+		);
+	}
 	const start = Math.floor((frame / fps) * sampleRate);
 
 	const actualStart = Math.max(0, start - sampleSize / 2);
-	const ints = Int16Array.from(
+
+	const ints = new Int16Array({
+		length: sampleSize,
+	});
+	ints.set(
 		data.subarray(actualStart, actualStart + sampleSize).map((x) => toInt16(x))
 	);
 	const phasors = fft(ints);
