89c6a9ab762a0a0c5f412299f337a4e74cc798c7
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-04-13 00:14:28 +0530

    add test, change variable name `value` to `input`
    Former-commit-id: dcfe41b1d89d3e8395fd66ac71c46390261cea29

--- packages/core/src/test/interpolateColors.test.ts
+++ packages/core/src/test/interpolateColors.test.ts
@@ -6,4 +6,19 @@ test('Throws if color space is not right', () => {
 		//@ts-expect-error
 		interpolateColors(1, [0, 1], ['#ffaadd', '#fabgdf'], 'GFR');
 	}, /invalid color space provided: GFR. Supported values are: \['RGB', 'HSV'\]/);
+
+	expectToThrow(() => {
+		//@ts-expect-error
+		interpolateColors(undefined, undefined, undefined);
+	}, /input or inputRange or outputRange can not be undefined/);
+
+	expectToThrow(() => {
+		interpolateColors(1, [1, 2, 3], ['#ffffff', '#aaaaaa']);
+	}, /inputRange \(3\) and outputRange \(2\) must have the same length/);
+});
+
+test('Basic interpolate Colors', () => {
+	expect(interpolateColors(1, [0, 1], ['#ffaadd', '#fabfdf'])).toBe(
+		'rgba(250, 191, 223, 1)'
+	);
 });
