ef3718b09a570da49cd4faefd07a1cf9eefce1b4
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-04-13 00:24:13 +0530

    updated test descriptions

--- packages/core/src/test/interpolateColors.test.ts
+++ packages/core/src/test/interpolateColors.test.ts
@@ -6,12 +6,16 @@ test('Throws if color space is not right', () => {
 		//@ts-expect-error
 		interpolateColors(1, [0, 1], ['#ffaadd', '#fabgdf'], 'GFR');
 	}, /invalid color space provided: GFR. Supported values are: \['RGB', 'HSV'\]/);
+});
 
+test('Throws error if input, inputRange or outputRange is undefined', () => {
 	expectToThrow(() => {
 		//@ts-expect-error
 		interpolateColors(undefined, undefined, undefined);
 	}, /input or inputRange or outputRange can not be undefined/);
+});
 
+test('inputRange and outputRange must be of same length', () => {
 	expectToThrow(() => {
 		interpolateColors(1, [1, 2, 3], ['#ffffff', '#aaaaaa']);
 	}, /inputRange \(3\) and outputRange \(2\) must have the same length/);
