6bed117cce4ae5c230dbd3b2148113979ce434d7
Jonny Burger <jonathanburger11@gmail.com>
2021-04-13 19:34:28 +0200

    limit amount of tracks

--- packages/cli/src/editor/components/Timeline/TimelineTracks.tsx
+++ packages/cli/src/editor/components/Timeline/TimelineTracks.tsx
@@ -7,13 +7,17 @@ import {
 	TIMELINE_PADDING,
 } from '../../helpers/timeline-layout';
 import {isTrackHidden} from './is-collapsed';
+import {MaxTimelineTracksReached} from './MaxTimelineTracks';
 import {TimelineViewState} from './timeline-state-reducer';
 import {TimelineSequence} from './TimelineSequence';
 
-const TimelineContent = styled.div`
-	flex: 1;
+const Content = styled.div`
 	padding-left: ${TIMELINE_PADDING}px;
 	padding-right: ${TIMELINE_PADDING}px;
+`;
+
+const TimelineContent = styled.div`
+	flex: 1;
 	background-color: #111111;
 	width: 100%;
 `;
@@ -22,7 +26,8 @@ export const TimelineTracks: React.FC<{
 	timeline: TrackWithHash[];
 	fps: number;
 	viewState: TimelineViewState;
-}> = ({timeline, fps, viewState}) => {
+	hasBeenCut: boolean;
+}> = ({timeline, fps, viewState, hasBeenCut}) => {
 	const inner: React.CSSProperties = useMemo(() => {
 		return {
 			height: TIMELINE_LAYER_HEIGHT + TIMELINE_BORDER * 2,
@@ -30,16 +35,19 @@ export const TimelineTracks: React.FC<{
 	}, []);
 	return (
 		<TimelineContent>
-			{timeline.map((track) => {
-				if (isTrackHidden(track, timeline, viewState)) {
-					return null;
-				}
-				return (
-					<div key={track.sequence.id} style={inner}>
-						<TimelineSequence fps={fps} s={track.sequence} />
-					</div>
-				);
-			})}
+			<Content>
+				{timeline.map((track) => {
+					if (isTrackHidden(track, timeline, viewState)) {
+						return null;
+					}
+					return (
+						<div key={track.sequence.id} style={inner}>
+							<TimelineSequence fps={fps} s={track.sequence} />
+						</div>
+					);
+				})}
+			</Content>
+			{hasBeenCut ? <MaxTimelineTracksReached /> : null}
 		</TimelineContent>
 	);
 };
