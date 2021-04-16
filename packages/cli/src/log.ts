c08f2a7344a2807471bdec4177520dbaa0016cfb
Jonny Burger <jonathanburger11@gmail.com>
2021-04-16 16:11:41 +0200

    only log if specified log level
    Former-commit-id: 97db51b969b37b8f0a4327e45ef9f1c6833d0636

--- packages/cli/src/log.ts
+++ packages/cli/src/log.ts
@@ -2,11 +2,38 @@
 
 import chalk from 'chalk';
 
+export enum LogLevel {
+	VERBOSE = 0,
+	INFO = 1,
+	WARN = 2,
+	ERROR = 3,
+}
+
+let logLevel = LogLevel.INFO;
+
+export const setLogLevel = (newLogLevel: LogLevel) => {
+	logLevel = newLogLevel;
+};
+
 export const Log = {
-	Verbose: (...args: Parameters<typeof console.log>) => console.log(...args),
-	Info: (...args: Parameters<typeof console.log>) => console.log(...args),
-	Warn: (...args: Parameters<typeof console.log>) =>
-		console.log(chalk.yellow(...args)),
-	Error: (...args: Parameters<typeof console.log>) =>
-		console.log(chalk.red(...args)),
+	Verbose: (...args: Parameters<typeof console.log>) => {
+		if (logLevel <= LogLevel.VERBOSE) {
+			return console.log(chalk.blueBright(...args));
+		}
+	},
+	Info: (...args: Parameters<typeof console.log>) => {
+		if (logLevel <= LogLevel.INFO) {
+			return console.log(...args);
+		}
+	},
+	Warn: (...args: Parameters<typeof console.log>) => {
+		if (logLevel <= LogLevel.WARN) {
+			return console.log(chalk.yellow(...args));
+		}
+	},
+	Error: (...args: Parameters<typeof console.log>) => {
+		if (logLevel <= LogLevel.ERROR) {
+			return console.log(chalk.red(...args));
+		}
+	},
 };
