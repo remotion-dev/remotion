4419c4cea9e11efa2c9c374431e1390a455403ae
Jonny Burger <jonathanburger11@gmail.com>
2021-04-16 22:16:26 +0200

    Update get-composition-id.ts
    Former-commit-id: f18945772ccafacbf8275e0a105c1dcd94826daf

--- packages/cli/src/get-composition-id.ts
+++ packages/cli/src/get-composition-id.ts
@@ -5,10 +5,10 @@ import {parsedCli} from './parse-command-line';
 export const getCompositionId = (comps: TCompMetadata[]) => {
 	if (!parsedCli._[2]) {
 		Log.Error('Composition ID not passed.');
-		Log.Info(
+		Log.Error(
 			'Pass an extra argument <composition-id>. The following video names are available:'
 		);
-		Log.Info(`${comps.map((c) => c.id).join(', ')}`);
+		Log.Error(`${comps.map((c) => c.id).join(', ')}`);
 		process.exit(1);
 	}
 	return parsedCli._[2];
