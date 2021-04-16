3f9768af5fe92bd2054960c327005bb74c776b77
Jonny Burger <jonathanburger11@gmail.com>
2021-04-16 16:22:34 +0200

    updated log levels

--- packages/cli/src/log.ts
+++ packages/cli/src/log.ts
@@ -2,37 +2,42 @@
 
 import chalk from 'chalk';
 
-export enum LogLevel {
-	VERBOSE = 0,
-	INFO = 1,
-	WARN = 2,
-	ERROR = 3,
-}
+const logLevels = ['verbose', 'info', 'warn', 'error'] as const;
 
-let logLevel = LogLevel.INFO;
+export type LogLevel = typeof logLevels[number];
+
+let logLevel: LogLevel = 'info';
 
 export const setLogLevel = (newLogLevel: LogLevel) => {
 	logLevel = newLogLevel;
 };
 
+const getNumberForLogLevel = (level: LogLevel) => {
+	return logLevels.indexOf(level);
+};
+
+export const isValidLogLevel = (level: string) => {
+	return getNumberForLogLevel(level as LogLevel) >= -1;
+};
+
 export const Log = {
 	Verbose: (...args: Parameters<typeof console.log>) => {
-		if (logLevel <= LogLevel.VERBOSE) {
+		if (getNumberForLogLevel(logLevel) <= getNumberForLogLevel('verbose')) {
 			return console.log(chalk.blueBright(...args));
 		}
 	},
 	Info: (...args: Parameters<typeof console.log>) => {
-		if (logLevel <= LogLevel.INFO) {
+		if (getNumberForLogLevel(logLevel) <= getNumberForLogLevel('info')) {
 			return console.log(...args);
 		}
 	},
 	Warn: (...args: Parameters<typeof console.log>) => {
-		if (logLevel <= LogLevel.WARN) {
+		if (getNumberForLogLevel(logLevel) <= getNumberForLogLevel('warn')) {
 			return console.log(chalk.yellow(...args));
 		}
 	},
 	Error: (...args: Parameters<typeof console.log>) => {
-		if (logLevel <= LogLevel.ERROR) {
+		if (getNumberForLogLevel(logLevel) <= getNumberForLogLevel('error')) {
 			return console.log(chalk.red(...args));
 		}
 	},
