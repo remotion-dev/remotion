cd51b813c66a5d064c5d99935efc65964fde50fb
Calvin Falter <calvin.falter@gmail.com>
2021-04-17 18:19:39 +0200

    add tests for hooks

---
+++ packages/core/src/test/use-sync-volume-with-media-tag.test.ts
@@ -0,0 +1,49 @@
+import {renderHook} from '@testing-library/react-hooks';
+import {RefObject} from 'react';
+import {useSyncVolumeWithMediaTag} from '../use-sync-volume-with-media-tag';
+import {VolumeProp} from '../volume-prop';
+
+test('has the volume been adapted', async () => {
+	const addEventListener = jest.fn();
+	const removeEventListener = jest.fn();
+	const audioRef = ({
+		current: {volume: 0.5, addEventListener, removeEventListener},
+	} as unknown) as RefObject<HTMLAudioElement>;
+	const volume = 0.6 as VolumeProp;
+	const volumePropFrame = 1;
+	const actualVolume = 0.4;
+
+	const initialProps =  {volumePropFrame, actualVolume, volume, mediaRef: audioRef};
+
+	const {rerender} = renderHook(
+		(hookProps) => useSyncVolumeWithMediaTag(hookProps),
+		{
+			initialProps,
+		}
+	);
+	expect(audioRef.current?.volume).toEqual(initialProps.volume)
+    initialProps.volume = 0.5
+    rerender(initialProps)
+    expect(audioRef.current?.volume).toEqual(initialProps.volume)
+});
+
+test('volume should not be adapted', async () => {
+	const addEventListener = jest.fn();
+	const removeEventListener = jest.fn();
+	const audioRef = ({
+		current: {volume: 0.4, addEventListener, removeEventListener},
+	} as unknown) as RefObject<HTMLAudioElement>;
+	const volume = 0.4 as VolumeProp;
+	const volumePropFrame = 1;
+	const actualVolume = 0.4;
+
+	const initialProps =  {volumePropFrame, actualVolume, volume, mediaRef: audioRef};
+
+	renderHook(
+		(hookProps) => useSyncVolumeWithMediaTag(hookProps),
+		{
+			initialProps,
+		}
+	);
+	expect(audioRef.current?.volume).toEqual(initialProps.volume)
+});
