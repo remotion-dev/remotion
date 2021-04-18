851b415b1972d6073e5fa1321e31f6dac7f60e35
Jonas Brunner <jonas.brunner@outlook.com>
2021-04-18 16:29:07 +0200

    test validation hook directly
    Former-commit-id: 13cafdb1741ca9613a15772b1a305b2aa993754c

--- packages/core/src/test/media-validation.test.tsx
+++ packages/core/src/test/media-validation.test.tsx
@@ -1,101 +1,49 @@
-import {render} from '@testing-library/react';
-import React, {useContext} from 'react';
-import {Audio} from '../audio';
-import {Internals} from '../internals';
-import {Video} from '../video';
-import {expectToThrow} from './expect-to-throw';
-
-const Wrapper: React.FC = ({children}) => {
-	const compositions = useContext(Internals.CompositionManager);
-	return (
-		<Internals.RemotionRoot>
-			<Internals.CompositionManager.Provider
-				value={{
-					...compositions,
-					compositions: [
-						{
-							height: 1080,
-							width: 1080,
-							fps: 30,
-							durationInFrames: 30,
-							id: 'markup',
-							nonce: 0,
-							component: React.lazy(() =>
-								Promise.resolve({
-									default: (() => null) as React.ComponentType<unknown>,
-								})
-							),
-						},
-					],
-					currentComposition: 'markup',
-				}}
-			>
-				{children}
-			</Internals.CompositionManager.Provider>
-		</Internals.RemotionRoot>
-	);
-};
+import {renderHook} from '@testing-library/react-hooks';
+import {validateMediaProps} from '../validate-media-props';
 
 test('It should not allow a video to have a negative volume', () => {
-	expectToThrow(
-		() =>
-			render(
-				<Wrapper>
-					<Video src="hi" volume={-1} />
-				</Wrapper>
-			),
-		/You have passed a volume below 0 to your <Video \/> component./
+	const {result} = renderHook(() => validateMediaProps({volume: -1}, 'Video'));
+	expect(result.error).toEqual(
+		TypeError(
+			'You have passed a volume below 0 to your <Video /> component. Volume must be between 0 and 1'
+		)
 	);
 });
 
 test('It should not allow an audio element to have a negative volume', () => {
-	expectToThrow(
-		() =>
-			render(
-				<Wrapper>
-					<Audio src="hi" volume={-1} />
-				</Wrapper>
-			),
-		/You have passed a volume below 0 to your <Audio \/> component./
+	const {result} = renderHook(() => validateMediaProps({volume: -1}, 'Audio'));
+	expect(result.error).toEqual(
+		TypeError(
+			'You have passed a volume below 0 to your <Audio /> component. Volume must be between 0 and 1'
+		)
 	);
 });
 
 test('It should not allow an audio element to have a wrong type', () => {
-	expectToThrow(
-		() =>
-			render(
-				<Wrapper>
-					{/**
-           *
-           // @ts-expect-error */}
-					<Audio src="hi" volume="wrong" />
-				</Wrapper>
-			),
-		/You have passed a volume of type string to your <Audio \/> component. Volume must be a number or a function/
+	const {result} = renderHook(() =>
+		//@ts-expect-error
+		validateMediaProps({volume: 'wrong'}, 'Audio')
+	);
+	expect(result.error).toEqual(
+		TypeError(
+			"You have passed a volume of type string to your <Audio /> component. Volume must be a number or a function with the signature '(frame: number) => number' undefined."
+		)
 	);
 });
 
-test('It should not allow an video element to have a wrong type', () => {
-	expectToThrow(
-		() =>
-			render(
-				<Wrapper>
-					{/**
-           *
-           // @ts-expect-error */}
-					<Video src="hi" volume="wrong" />
-				</Wrapper>
-			),
-		/You have passed a volume of type string to your <Video \/> component. Volume must be a number or a function/
+test('It should not allow a video element to have a wrong type', () => {
+	const {result} = renderHook(() =>
+		//@ts-expect-error
+		validateMediaProps({volume: 'wrong'}, 'Video')
+	);
+	expect(result.error).toEqual(
+		TypeError(
+			"You have passed a volume of type string to your <Video /> component. Volume must be a number or a function with the signature '(frame: number) => number' undefined."
+		)
 	);
 });
 
 test('It should allow a valid volume', () => {
-	expect(() =>
-		render(
-			<Wrapper>
-				<Video src="hi" volume={1} />
-			</Wrapper>
-		)
-	).not.toThrow();
+	const {result} = renderHook(() => validateMediaProps({volume: 1}, 'Video'));
+	expect(result.error).toEqual(undefined);
 });
