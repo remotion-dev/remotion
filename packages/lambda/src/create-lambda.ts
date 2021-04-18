d6862c8795ae133bc4545303529df53b9904558b
Jonny Burger <jonathanburger11@gmail.com>
2021-04-18 20:50:16 +0200

    lambda package

---
+++ packages/lambda/src/create-lambda.ts
@@ -0,0 +1,30 @@
+import {CreateFunctionCommand, LambdaClient} from '@aws-sdk/client-lambda';
+
+const client = new LambdaClient({
+	region: 'eu-central-1',
+});
+
+// https://jonnytv.s3.eu-central-1.amazonaws.com/test-lambda-91cf079b-ae80-4994-8a3a-1d2edd33ae5b.zip
+// Set the parameters.
+const params = {
+	Code: {
+		S3Bucket: 'jonnytv', // BUCKET_NAME
+		S3Key: 'test-lambda-91cf079b-ae80-4994-8a3a-1d2edd33ae5b.zip', // ZIP_FILE_NAME
+	},
+	FunctionName: 'remotion-test',
+	Handler: 'index.handler',
+	Role: 'arn:aws:iam::976210361945:role/lambda-admin', // IAM_ROLE_ARN; e.g., arn:aws:iam::650138640062:role/v3-lambda-tutorial-lambda-role
+	Runtime: 'nodejs12.x',
+	Description: 'Renders a Remotion video.',
+};
+
+const run = async () => {
+	try {
+		const data = await client.send(new CreateFunctionCommand(params));
+		console.log('Success', data); // successful response
+	} catch (err) {
+		console.log('Error', err); // an error occurred
+	}
+};
+
+run();
