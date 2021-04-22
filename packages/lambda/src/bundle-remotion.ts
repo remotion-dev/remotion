7a637dfd9f5883774c92c99422d593497fe269cc
Jonny Burger <jonathanburger11@gmail.com>
2021-04-22 17:58:14 +0200

    it works!

--- packages/lambda/src/bundle-remotion.ts
+++ packages/lambda/src/bundle-remotion.ts
@@ -2,5 +2,11 @@ import {bundle} from '@remotion/bundler';
 import path from 'path';
 
 export const bundleRemotion = () => {
-	return bundle(path.join(__dirname, '..', 'remotion-project', 'index.ts'));
+	return bundle(
+		path.join(__dirname, '..', 'remotion-project', 'index.ts'),
+		() => void 0,
+		{
+			enableCaching: false,
+		}
+	);
 };
---
+++ packages/lambda/src/upload-dir.ts
@@ -0,0 +1,39 @@
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import {createReadStream, promises as fs} from 'fs';
+import path from 'path';
+
+export const uploadDir = async ({
+	bucket,
+	client,
+	dir,
+}: {
+	bucket: string;
+	client: S3Client;
+	dir: string;
+}) => {
+	async function getFiles(directory: string): Promise<string | string[]> {
+		const dirents = await fs.readdir(directory, {withFileTypes: true});
+		const _files = await Promise.all(
+			dirents.map((dirent) => {
+				const res = path.resolve(directory, dirent.name);
+				return dirent.isDirectory() ? getFiles(res) : res;
+			})
+		);
+		return Array.prototype.concat(..._files);
+	}
+
+	const files = (await getFiles(dir)) as string[];
+	const uploads = files.map((filePath) => {
+		console.log(path.relative(dir, filePath));
+		return client.send(
+			new PutObjectCommand({
+				Key: path.relative(dir, filePath),
+				Bucket: bucket,
+				Body: createReadStream(filePath),
+				ACL: 'public-read',
+				ContentType: filePath.includes('index.html') ? 'text/html' : undefined,
+			})
+		);
+	});
+	return Promise.all(uploads);
+};
