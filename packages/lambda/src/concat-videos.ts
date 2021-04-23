d864d9dbfe6630f247deb9533f45b698f6dfbec4
Jonny Burger <jonathanburger11@gmail.com>
2021-04-23 22:12:05 +0200

    getting there kinda

--- packages/lambda/src/concat-videos.ts
+++ packages/lambda/src/concat-videos.ts
@@ -9,17 +9,21 @@ import {join} from 'path';
 import {Readable} from 'stream';
 import xns from 'xns';
 import {REGION} from './constants';
+import {timer} from './timer';
 
 export const concatVideos = xns(
 	async (
 		s3Client: S3Client = new S3Client({region: REGION}),
-		bucket = 'remotion-renders-0.7182592846197402'
+		bucket = 'remotion-renders-0.7182592846197402',
+		expectedFiles = 20
 	) => {
+		const lsTimer = timer('Listing files');
 		const filesInBucket = await s3Client.send(
 			new ListObjectsCommand({
 				Bucket: bucket,
 			})
 		);
+		lsTimer();
 		if (!filesInBucket.Contents) {
 			throw new Error('No contents');
 		}
@@ -31,6 +35,7 @@ export const concatVideos = xns(
 		}
 		mkdirSync(outdir);
 		for (const file of filesInBucket.Contents) {
+			const end = timer(`Download ${file.Key}`);
 			const {Body} = await s3Client.send(
 				new GetObjectCommand({
 					Bucket: bucket,
@@ -44,6 +49,7 @@ export const concatVideos = xns(
 					.on('error', (err) => reject(err))
 					.on('close', () => resolve());
 			});
+			end();
 		}
 
 		const files = filesInBucket.Contents.map((file) =>
@@ -51,7 +57,9 @@ export const concatVideos = xns(
 		);
 
 		const outfile = join(__dirname, 'concat.mp4');
+		const combine = timer('Combine videos');
 		await combineVideos(files, outfile);
+		combine();
 
 		console.log(outfile);
 	}
