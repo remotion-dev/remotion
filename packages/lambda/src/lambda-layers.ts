575512d9e82efd72229be0e45034f75837ba7843
Jonny Burger <jonathanburger11@gmail.com>
2021-04-23 14:14:11 +0200

    simplify lambdas
    Former-commit-id: 8eb58307fc4235a2e9b75cded8bd9777258be60a

---
+++ packages/lambda/src/lambda-layers.ts
@@ -0,0 +1,83 @@
+import {
+	LambdaClient,
+	LayersListItem,
+	ListLayersCommand,
+	PublishLayerVersionCommand,
+} from '@aws-sdk/client-lambda';
+
+const runtimes: string[] = ['nodejs14.x', 'nodejs12.x', 'nodejs10.x'];
+
+const FFMPEG_LAYER_NAME = 'remotion-ffmpeg-binaries';
+const CHROMIUM_LAYER_NAME = 'remotion-chromium-binaries';
+
+export const createLayer = (
+	lambdaClient: LambdaClient,
+	name: string,
+	key: string
+) => {
+	return lambdaClient.send(
+		new PublishLayerVersionCommand({
+			Content: {
+				S3Bucket: 'remotion-binaries',
+				S3Key: key,
+			},
+			LayerName: name,
+			LicenseInfo: 'https://ffmpeg.org/legal.html',
+			CompatibleRuntimes: runtimes,
+			Description: 'Adds ffmpeg and ffprobe binaries in PATH',
+		})
+	);
+};
+
+export const getLayers = async (lambdaClient: LambdaClient) => {
+	const data = await lambdaClient.send(
+		new ListLayersCommand({
+			CompatibleRuntime: runtimes[0],
+		})
+	);
+	return (data.Layers || []) as LayersListItem[];
+};
+
+const hasLayer = (name: string, layers: LayersListItem[]) => {
+	return layers.find((l) => l.LayerName === name);
+};
+
+export const ensureFffmpegLayer = async (
+	layers: LayersListItem[],
+	lambdaClient: LambdaClient
+): Promise<string> => {
+	const existingLayer = hasLayer(FFMPEG_LAYER_NAME, layers);
+	if (existingLayer) {
+		return existingLayer.LatestMatchingVersion?.LayerVersionArn as string;
+	}
+	const layer = await createLayer(
+		lambdaClient,
+		FFMPEG_LAYER_NAME,
+		'ffmpeg.zip'
+	);
+	return layer.LayerVersionArn as string;
+};
+
+export const ensurePuppeteerLayer = async (
+	layers: LayersListItem[],
+	lambdaClient: LambdaClient
+): Promise<string> => {
+	const existingLayer = hasLayer(CHROMIUM_LAYER_NAME, layers);
+	if (existingLayer) {
+		return existingLayer.LatestMatchingVersion?.LayerVersionArn as string;
+	}
+	const layer = await createLayer(
+		lambdaClient,
+		CHROMIUM_LAYER_NAME,
+		'chromium.zip'
+	);
+	return layer.LayerVersionArn as string;
+};
+
+export const ensureLayers = async (lambdaClient: LambdaClient) => {
+	const layers = await getLayers(lambdaClient);
+	const ffmpegArn = await ensureFffmpegLayer(layers, lambdaClient);
+
+	const chromeArn = await ensurePuppeteerLayer(layers, lambdaClient);
+	return {ffmpegArn, chromeArn};
+};
