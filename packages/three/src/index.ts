4af271946e4a9664317d8fe1c0fb7a0bf34d9ccf
Jonny Burger <jonathanburger11@gmail.com>
2021-04-24 09:14:26 +0200

    adapt to coding style
    Former-commit-id: 4b6814bed602b57b1b9f704a6e476cdf92866ac1

--- packages/three/src/index.ts
+++ packages/three/src/index.ts
@@ -1,17 +1,11 @@
-
-export * from './RemotionThreeCanvas';
-export { default as RemotionThreeCanvas } from './RemotionThreeCanvas';
-
-export { useRemotionThreeDomNode } from './RemotionThreeContext';
-
-export * from './ThreeVideo';
-export { default as ThreeVideo } from './ThreeVideo';
-
-export * from './useVideoTexture';
-export { default as useVideoTexture } from './useVideoTexture';
-
 export * from './effects/SwirlEffect';
 export { default as SwirlEffect } from './effects/SwirlEffect';
-
+export * from './RemotionThreeCanvas';
+export { RemotionThreeCanvas } from './RemotionThreeCanvas';
+export { useRemotionThreeDomNode } from './RemotionThreeContext';
+export * from './ThreeVideo';
+export { default as ThreeVideo } from './ThreeVideo';
+export * from './useVideoTexture';
+export { default as useVideoTexture } from './useVideoTexture';
 export * from './utils/TexturedShaderMaterial';
 export { default as TexturedShaderMaterial } from './utils/TexturedShaderMaterial';
