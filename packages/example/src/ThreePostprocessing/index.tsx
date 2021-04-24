bcf7e615843a5818153b388469d139d7fc1ee42d
Jonny Burger <jonathanburger11@gmail.com>
2021-04-24 10:45:27 +0200

    move swirl into example
    Former-commit-id: 68d7af845e1ab83255b5f19fd8126b1467a553e6

--- packages/example/src/ThreePostprocessing/index.tsx
+++ packages/example/src/ThreePostprocessing/index.tsx
@@ -1,70 +1,31 @@
-import {EffectComposer, Scanline, Vignette} from '@react-three/postprocessing';
-import {SwirlEffect, ThreeCanvas, ThreeVideo} from '@remotion/three';
+import {
+	DotScreen,
+	EffectComposer,
+	Scanline,
+	Sepia,
+	Vignette,
+} from '@react-three/postprocessing';
+import {ThreeCanvas, ThreeVideo} from '@remotion/three';
 import React from 'react';
-import {useCurrentFrame} from 'remotion';
+import {Sequence, useCurrentFrame} from 'remotion';
+import {SwirlEffect} from './SwirlEffect';
 
 const videoSrc =
 	'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 
 const ThreePostprocessing = (): React.ReactElement => {
 	const frame = useCurrentFrame();
-	const [enableEffects, setEnableEffects] = React.useState(true); // eslint-disable-line @typescript-eslint/no-unused-vars
 	return (
-		<div
-			style={{display: 'flex', width: '100%'}}
-			// onClick={() => setEnableEffects(x => !x)}
-		>
-			<ThreeCanvas
-				orthographic={false}
-				camera={{fov: 75, position: [0, 0, 470]}}
-			>
-				<ambientLight intensity={0.15} />
-				<pointLight args={[undefined, 0.4]} position={[200, 200, 0]} />
-
+		<div style={{flex: 1}}>
+			<ThreeCanvas orthographic={false}>
 				<ThreeVideo ignoreDepth fullViewport src={videoSrc} startFrom={460} />
-
-				<mesh
-					position={[-300, -200, 0]}
-					rotation={[
-						frame * 0.06 * 0.5,
-						frame * 0.07 * 0.5,
-						frame * 0.08 * 0.5,
-					]}
+				<EffectComposer
+					multisampling={0} // TODO: Remove with three.js v128
 				>
-					<boxGeometry args={[100, 100, 100]} />
-					<meshStandardMaterial
-						color={[
-							Math.sin(frame * 0.12) * 0.5 + 0.5,
-							Math.cos(frame * 0.11) * 0.5 + 0.5,
-							Math.sin(frame * 0.08) * 0.5 + 0.5,
-						]}
-					/>
-				</mesh>
-
-				<mesh
-					position={[300, -200, 0]}
-					rotation={[
-						frame * 0.06 * 0.5,
-						frame * 0.07 * 0.5,
-						frame * 0.08 * 0.5,
-					]}
-				>
-					<cylinderGeometry args={[50, 50, 180, 24]} />
-					<meshStandardMaterial color="red" />
-				</mesh>
-
-				{enableEffects && (
-					<EffectComposer
-						multisampling={0} // TODO: Remove with three.js v128
-					>
-						{/* <Bloom
-							kernelSize={4}
-							intensity={1.1}
-							luminanceSmoothing={0.04}
-							luminanceThreshold={0.7}
-						/> */}
-						{/* <DotScreen scale={0.5} /> */}
-						{/* <Sepia intensity={1} /> */}
+					<Sequence durationInFrames={100} from={0} layout="none">
+						<Scanline density={0.9} opacity={0.2} />
+					</Sequence>
+					<Sequence durationInFrames={100} from={100} layout="none">
 						<SwirlEffect
 							radius={400}
 							offset={[
@@ -73,10 +34,17 @@ const ThreePostprocessing = (): React.ReactElement => {
 							]}
 							angle={Math.cos(frame * 0.013) * 0.6}
 						/>
-						<Scanline density={0.9} opacity={0.2} />
+					</Sequence>
+					<Sequence durationInFrames={100} from={200} layout="none">
 						<Vignette darkness={0.55} />
-					</EffectComposer>
-				)}
+					</Sequence>
+					<Sequence durationInFrames={100} from={300} layout="none">
+						<Sepia intensity={1} />
+					</Sequence>
+					<Sequence durationInFrames={100} from={400} layout="none">
+						<DotScreen scale={0.5} />
+					</Sequence>
+				</EffectComposer>
 			</ThreeCanvas>
 		</div>
 	);
