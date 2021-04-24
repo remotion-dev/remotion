43bf9270fd7a0fb72e3e7c3712f7a0aa2a8ac0df
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-04-24 17:18:23 +0530

    added 2nd remotion video

--- packages/player-example/src/App.tsx
+++ packages/player-example/src/App.tsx
@@ -1,11 +1,14 @@
 import {Player, PlayerMethods} from '@remotion/player';
 import {useRef, useState} from 'react';
 import CarSlideshow from './CarSlideshow';
+import CarSlideshowNew from './CarSlideshowNew';
 
 export default function App() {
 	const [title, setTitle] = useState('This is my title');
 
 	const ref = useRef<PlayerMethods>(null);
+
+	const ref2 = useRef<PlayerMethods>(null);
 
 	return (
 		<div>
@@ -16,6 +19,18 @@ export default function App() {
 				fps={30}
 				durationInFrames={500}
 				component={CarSlideshow}
+				controls
+				props={{
+					title,
+				}}
+			/>
+			<Player
+				ref={ref2}
+				width={768}
+				height={432}
+				fps={30}
+				durationInFrames={500}
+				component={CarSlideshowNew}
 				controls
 				props={{
 					title,
@@ -33,6 +48,12 @@ export default function App() {
 			</button>
 			<button type="button" onClick={() => ref.current?.pause()}>
 				Pause
+			</button>
+			<button type="button" onClick={() => ref2.current?.play()}>
+				Play2
+			</button>
+			<button type="button" onClick={() => ref2.current?.pause()}>
+				Pause2
 			</button>
 		</div>
 	);
