9c75f9636f25b88647c00ded678b64e638cfb361
Jonny Burger <jonathanburger11@gmail.com>
2021-04-24 09:32:39 +0200

    fix sequence not showing
    Former-commit-id: 64aa005d06800b26bac2fbbe3f5bdf3a51de51b0

--- packages/example/src/ThreeBasic/index.tsx
+++ packages/example/src/ThreeBasic/index.tsx
@@ -1,35 +1,39 @@
+import {RemotionThreeCanvas, ThreeVideo} from '@remotion/three';
 import React from 'react';
-import { useCurrentFrame } from 'remotion';
-import { RemotionThreeCanvas, ThreeVideo } from '@remotion/three';
+import {Sequence, useCurrentFrame} from 'remotion';
 
-const videoSrc = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+const videoSrc =
+	'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 
 const ThreeBasic = (): React.ReactElement => {
 	const frame = useCurrentFrame();
 	return (
-		<div style={{ display: 'flex', width: '100%' }}>
+		<div style={{display: 'flex', width: '100%'}}>
 			<RemotionThreeCanvas
 				orthographic={false}
-				camera={{ fov: 75, position: [0, 0, 470] }}
+				camera={{fov: 75, position: [0, 0, 470]}}
 			>
 				<ambientLight intensity={0.15} />
 				<pointLight args={[undefined, 0.4]} position={[200, 200, 0]} />
 
 				<ThreeVideo ignoreDepth fullViewport src={videoSrc} startFrom={460} />
 
-				{/* TODO: Using Sequence directly here still crashes for some reason */}
-				{/* <Sequence
-					layout='none'
-					from={-460}
+				<Sequence
 					showInTimeline={false}
-					durationInFrames={Number.POSITIVE_INFINITY}
+					layout="none"
+					from={-460}
+					durationInFrames={Infinity}
 				>
 					<ThreeVideo ignoreDepth src={videoSrc} />
-				</Sequence> */}
+				</Sequence>
 
 				<mesh
 					position={[-300, -200, 0]}
-					rotation={[frame * 0.06 * 0.5, frame * 0.07 * 0.5, frame * 0.08 * 0.5]}
+					rotation={[
+						frame * 0.06 * 0.5,
+						frame * 0.07 * 0.5,
+						frame * 0.08 * 0.5,
+					]}
 				>
 					<boxGeometry args={[100, 100, 100]} />
 					<meshStandardMaterial
@@ -43,10 +47,14 @@ const ThreeBasic = (): React.ReactElement => {
 
 				<mesh
 					position={[300, -200, 0]}
-					rotation={[frame * 0.06 * 0.5, frame * 0.07 * 0.5, frame * 0.08 * 0.5]}
+					rotation={[
+						frame * 0.06 * 0.5,
+						frame * 0.07 * 0.5,
+						frame * 0.08 * 0.5,
+					]}
 				>
 					<cylinderGeometry args={[50, 50, 180, 24]} />
-					<meshStandardMaterial color='red' />
+					<meshStandardMaterial color="red" />
 				</mesh>
 			</RemotionThreeCanvas>
 		</div>
