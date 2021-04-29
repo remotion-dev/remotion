7d521c396633023f482d969f6efb46c2b613194d
Jonny Burger <jonathanburger11@gmail.com>
2021-04-29 17:19:04 +0200

    hide controls when leaving cursor

---
+++ packages/player/src/use-hover-state.ts
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+
+export const useHoverState = (ref: React.RefObject<HTMLDivElement>) => {
+	const [hovered, stetHovered] = useState(false);
+
+	useEffect(() => {
+		const {current} = ref;
+		if (!current) {
+			return;
+		}
+		const onHover = () => {
+			stetHovered(true);
+		};
+		const onLeave = () => {
+			stetHovered(false);
+		};
+
+		current.addEventListener('mouseenter', onHover);
+		current.addEventListener('mouseleave', onLeave);
+		return () => {
+			current.removeEventListener('mouseenter', onHover);
+			current.removeEventListener('mouseenter', onLeave);
+		};
+	}, [ref]);
+
+	return hovered;
+};
