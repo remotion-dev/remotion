ff81c5beba32543677b21713f0c1489fbcd2ea26
Jonny Burger <jonathanburger11@gmail.com>
2021-04-29 10:17:56 +0200

    add some spring tests

---
+++ packages/core/src/test/spring.test.ts
@@ -0,0 +1,38 @@
+import {isApproximatelyTheSame} from '../is-approximately-the-same';
+import {spring} from '../spring';
+
+test('Basic spring should work', () => {
+	expect(
+		spring({
+			fps: 30,
+			frame: 0,
+		})
+	).toEqual(0);
+
+	expect(
+		spring({
+			fps: 30,
+			frame: 0,
+			from: 1,
+			to: 0,
+		})
+	).toEqual(1);
+});
+
+test('Spring should go to 1', () => {
+	expect(
+		isApproximatelyTheSame(
+			spring({
+				fps: 30,
+				frame: 1,
+			}),
+			0.04941510804510185
+		)
+	).toBe(true);
+	expect(
+		spring({
+			fps: 30,
+			frame: 100,
+		})
+	).toBe(1);
+});
