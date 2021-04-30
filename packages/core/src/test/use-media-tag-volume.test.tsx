6891fad6b240d35305dd2702cd6f86c5594e73a6
Calvin Falter <calvin.falter@gmail.com>
2021-04-30 16:27:52 +0200

    add check for kind of eventlistener that is set
    Former-commit-id: 9bcb5af71f154855fc0e73f9b67d5c7a47b490af

--- packages/core/src/test/use-media-tag-volume.test.tsx
+++ packages/core/src/test/use-media-tag-volume.test.tsx
@@ -1,6 +1,7 @@
 import {renderHook} from '@testing-library/react-hooks';
 import React, {RefObject} from 'react';
 import {useMediaTagVolume} from '../use-media-tag-volume';
+import anything = jasmine.anything;
 
 describe('Should update state when volume changes', () => {
 	const setState = jest.fn();
@@ -30,6 +31,8 @@ describe('Should update state when volume changes', () => {
 		} as RefObject<HTMLAudioElement>;
 		rerender({mediaRef: audioRef});
 		expect(setState).toHaveBeenCalledWith(0.75);
+		expect(addEventListener).toHaveBeenCalledWith('volumechange', anything());
+		expect(removeEventListener).toHaveBeenCalledWith('volumechange', anything());
 	});
 });
 
