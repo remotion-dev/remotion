7eab7a1892ea830cbee003192feba020869cd48e
Jonny Burger <jonathanburger11@gmail.com>
2021-05-01 15:41:22 +0200

    should fix :)
    Former-commit-id: 7a77b30e55ec52d1253d7104a891844f3bea1265

--- packages/cli/remotion-cli.js
+++ packages/cli/remotion-cli.js
@@ -1,4 +1,10 @@
 #! /usr/bin/env node
 const {cli} = require('./dist/index');
 
-cli();
+cli()
+	.then(() => process.exit(0))
+	.catch((err) => {
+		// eslint-disable-next-line no-console
+		console.error(err);
+		process.exit(1);
+	});
