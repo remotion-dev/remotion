4635c77a8cf3f214203e5213297e5d97e8d7dc14
Jonny Burger <jonathanburger11@gmail.com>
2021-05-04 17:58:29 +0200

    fix ssr
    Former-commit-id: 47114b55b31a35fcda575ee6cb3931e570e15ce1

--- packages/core/src/use-lazy-component.ts
+++ packages/core/src/use-lazy-component.ts
@@ -2,14 +2,25 @@ import React, {useMemo} from 'react';
 import {CompProps} from './internals';
 
 // Expected, it can be any component props
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useLazyComponent = (compProps: CompProps<any>) => {
+export const useLazyComponent = (
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	compProps: CompProps<any>
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+): React.LazyExoticComponent<React.ComponentType<any>> => {
 	const lazy = useMemo(() => {
 		if ('lazyComponent' in compProps) {
 			return React.lazy(compProps.lazyComponent);
 		}
 
 		if ('component' in compProps) {
+			// In SSR, suspense is not yet supported, we cannot use React.lazy
+			if (typeof document === 'undefined') {
+				return (compProps.component as unknown) as React.LazyExoticComponent<
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+					React.ComponentType<any>
+				>;
+			}
+
 			return React.lazy(() => Promise.resolve({default: compProps.component}));
 		}
 
