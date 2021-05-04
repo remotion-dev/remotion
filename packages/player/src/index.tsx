083369f8df89e74677a17acb33c347a83435dc09
Jonny Burger <jonathanburger11@gmail.com>
2021-05-04 16:31:08 +0200

    share some common ground between player and CLI

--- packages/player/src/index.tsx
+++ packages/player/src/index.tsx
@@ -1,14 +1,20 @@
+import {calculateScale} from './calculate-scale';
 import {PlayerEventEmitterContext} from './emitter-context';
 import {PlayerEmitter} from './event-emitter';
 import {usePlayback} from './use-playback';
 import {usePlayer} from './use-player';
+import {useElementSize} from './utils/use-element-size';
 
 export {Player} from './Player';
 export {PlayerMethods, PlayerRef} from './player-methods';
+export {PreviewSize} from './utils/preview-size';
+export {Size} from './utils/use-element-size';
 
 export const PlayerInternals = {
 	PlayerEventEmitterContext,
 	PlayerEmitter,
 	usePlayer,
 	usePlayback,
+	useElementSize,
+	calculateScale,
 };
