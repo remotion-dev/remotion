7d6d1abc3158de77432d191cf789b331b95993a1
Jonny Burger <jonathanburger11@gmail.com>
2021-05-07 15:41:40 +0200

    Update index.ts
    Former-commit-id: dd5d3a9ff7a8e8bcc2e81c24c10d5104361f9a60

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -29,7 +29,6 @@ export * from './ready-manager';
 export {registerRoot} from './register-root';
 export {Sequence} from './sequencing';
 export * from './spring';
-export * from './use-env';
 export * from './use-frame';
 export * from './use-video-config';
 export * from './video';
