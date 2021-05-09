f578f0ea65a4ef8ef198280908b8774a379ccf4e
Jonny Burger <jonathanburger11@gmail.com>
2021-05-09 21:28:16 +0200

    can set undefined for preview port

--- packages/core/src/config/preview-server.ts
+++ packages/core/src/config/preview-server.ts
@@ -1,12 +1,17 @@
-let previewServerPort: number;
+let previewServerPort: number | undefined;
 
-export const setPreviewServerPort = (port: number) => {
+export const setPreviewServerPort = (port: number | undefined) => {
 	if (!['number', 'undefined'].includes(typeof port)) {
 		throw new Error(
 			`Preview server port should be a number. Got ${typeof port} (${JSON.stringify(
 				port
 			)})`
 		);
+	}
+
+	if (port === undefined) {
+		previewServerPort = undefined;
+		return;
 	}
 
 	if (port < 1 || port > 65535) {
