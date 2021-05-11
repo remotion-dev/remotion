662f74c9ff77976f9d12db24f3d9c2ab57b631d4
Jonny Burger <jonathanburger11@gmail.com>
2021-05-11 11:28:33 +0200

    try to perfect the design

--- packages/player/src/icons.tsx
+++ packages/player/src/icons.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const size = 25;
+const fullscreenIconSize = 16;
 
 const rotate: React.CSSProperties = {
 	transform: `rotate(90deg)`,
@@ -9,12 +10,13 @@ const rotate: React.CSSProperties = {
 export const PlayIcon: React.FC = () => {
 	return (
 		<svg width={size} height={size} viewBox="-100 -100 400 400" style={rotate}>
-			<g stroke="#fff" strokeWidth="100" strokeLinejoin="round">
-				<path
-					fill="#fff"
-					d="M 2 172 a 196 100 0 0 0 195 5 A 196 240 0 0 0 100 2.259 A 196 240 0 0 0 2 172 z"
-				/>
-			</g>
+			<path
+				fill="#fff"
+				stroke="#fff"
+				strokeWidth="100"
+				strokeLinejoin="round"
+				d="M 2 172 a 196 100 0 0 0 195 5 A 196 240 0 0 0 100 2.259 A 196 240 0 0 0 2 172 z"
+			/>
 		</svg>
 	);
 };
@@ -28,30 +30,59 @@ export const PauseIcon: React.FC = () => {
 	);
 };
 
-export const FullscreenIcon: React.FC = () => {
-	return (
-		<svg height={size} viewBox="0 0 24 24" width={size}>
-			<path d="M0 0h24v24H0z" fill="none" />
-			<path
-				d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"
-				fill="#fff"
-			/>
-		</svg>
-	);
-};
+export const FullscreenIcon: React.FC<{minimized: boolean}> = ({minimized}) => {
+	const strokeWidth = 6;
+	const viewSize = 32;
 
-export const FullscreenExitIcon: React.FC = () => {
+	const out = minimized ? strokeWidth / 2 : 0;
+	const middleInset = minimized ? strokeWidth / 2 : strokeWidth * 1.6;
+	const inset = minimized ? strokeWidth * 2 : strokeWidth * 1.6;
+
 	return (
 		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			height={size}
-			width={size}
-			viewBox="0 0 24 24"
+			viewBox={`0 0 ${viewSize} ${viewSize}`}
+			height={fullscreenIconSize}
+			width={fullscreenIconSize}
 		>
-			<path fill="none" d="M0 0h24v24H0z" />
 			<path
-				d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"
-				fill="#fff"
+				d={`
+				M ${out} ${inset}
+				L ${middleInset} ${middleInset}
+				L ${inset} ${out}
+				`}
+				stroke="#fff"
+				strokeWidth={strokeWidth}
+				fill="none"
+			/>
+			<path
+				d={`
+				M ${viewSize - out} ${inset}
+				L ${viewSize - middleInset} ${middleInset}
+				L ${viewSize - inset} ${out}
+				`}
+				stroke="#fff"
+				strokeWidth={strokeWidth}
+				fill="none"
+			/>
+			<path
+				d={`
+				M ${out} ${viewSize - inset}
+				L ${middleInset} ${viewSize - middleInset}
+				L ${inset} ${viewSize - out}
+				`}
+				stroke="#fff"
+				strokeWidth={strokeWidth}
+				fill="none"
+			/>
+			<path
+				d={`
+				M ${viewSize - out} ${viewSize - inset}
+				L ${viewSize - middleInset} ${viewSize - middleInset}
+				L ${viewSize - inset} ${viewSize - out}
+				`}
+				stroke="#fff"
+				strokeWidth={strokeWidth}
+				fill="none"
 			/>
 		</svg>
 	);
