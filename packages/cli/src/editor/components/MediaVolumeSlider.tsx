30761eaf4d7b34df69c003f9accd0ea0cd0f9819
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-13 01:08:53 +0530

    media volume toggle

--- packages/cli/src/editor/components/MediaVolumeSlider.tsx
+++ packages/cli/src/editor/components/MediaVolumeSlider.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import {VolumeOffIcon} from '../icons/media-volume';
+import React, {useCallback} from 'react';
+import {Internals} from 'remotion';
+import {VolumeOffIcon, VolumeOnIcon} from '../icons/media-volume';
 import {ControlButton} from './ControlButton';
 
 export const MediaVolumeSlider: React.FC = () => {
+	const [mediaMuted, setMediaMuted] = Internals.useMediaMutedState();
+	const [mediaVolume, setMediaVolume] = Internals.useMediaVolumeState();
+	console.log(mediaVolume, 'media volume');
+
+	const onClick = useCallback(() => {
+		setMediaMuted(!mediaMuted);
+	}, [setMediaMuted, mediaMuted]);
+
 	return (
-		<ControlButton>
-			<VolumeOffIcon />
+		<ControlButton onClick={onClick}>
+			{mediaMuted ? <VolumeOffIcon /> : <VolumeOnIcon />}
 		</ControlButton>
 	);
 };
