39e48b3a2d884af9decda65e438bdb865cdea2d1
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-13 01:53:11 +0530

    audio for developmentworks
    Former-commit-id: 96e4afc4326e46441d1d46d2a1121dc310ca083a

--- packages/cli/src/editor/components/MediaVolumeSlider.tsx
+++ packages/cli/src/editor/components/MediaVolumeSlider.tsx
@@ -6,12 +6,10 @@ import {ControlButton} from './ControlButton';
 export const MediaVolumeSlider: React.FC = () => {
 	const [mediaMuted, setMediaMuted] = Internals.useMediaMutedState();
 	const [mediaVolume, setMediaVolume] = Internals.useMediaVolumeState();
-	console.log(mediaMuted, mediaVolume, 'media');
 
 	const onClick = useCallback(() => {
 		setMediaMuted(!mediaMuted);
 		setMediaVolume(Number(mediaMuted));
-		console.log(Number(mediaMuted), 'media volume');
 	}, [setMediaMuted, mediaMuted, setMediaVolume]);
 
 	return (
