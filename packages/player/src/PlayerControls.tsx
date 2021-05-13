3eb3a3bae916840c895bdba92e0a0d35e0c0e3d7
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-14 00:38:34 +0530

    working slider on @remotion/player

--- packages/player/src/PlayerControls.tsx
+++ packages/player/src/PlayerControls.tsx
@@ -1,13 +1,8 @@
 import React, {MouseEventHandler, useMemo} from 'react';
 import {Internals} from 'remotion';
 import {formatTime} from './format-time';
-import {
-	FullscreenIcon,
-	PauseIcon,
-	PlayIcon,
-	VolumeOffIcon,
-	VolumeOnIcon,
-} from './icons';
+import {FullscreenIcon, PauseIcon, PlayIcon} from './icons';
+import {MediaVolumeSlider} from './MediaVolumeSlider';
 import {PlayerSeekBar} from './PlayerSeekBar';
 import {usePlayer} from './use-player';
 import {browserSupportsFullscreen} from './utils/browser-supports-fullscreen';
@@ -119,19 +114,11 @@ export const Controls: React.FC<{
 						{player.playing ? <PauseIcon /> : <PlayIcon />}
 					</button>
 					<div style={xSpacer} />
-					<button
-						type="button"
-						style={buttonStyle}
-						onClick={undefined}
-						aria-label={player.playing ? 'Mute video' : 'Play video'}
-						title={player.playing ? 'Pause video' : 'Play video'}
-					>
-						{player.playing ? <VolumeOffIcon /> : <VolumeOnIcon />}
-					</button>
-					<div style={xSpacer} />
 					<div style={timeLabel}>
 						{formatTime(frame / fps)} / {formatTime(durationInFrames / fps)}
 					</div>
+					<div style={xSpacer} />
+					<MediaVolumeSlider />
 				</div>
 				<div style={flex1} />
 				{browserSupportsFullscreen && allowFullscreen ? (
