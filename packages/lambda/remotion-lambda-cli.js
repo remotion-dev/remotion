d2bdb9412aa28db7be55980d46f01434ee95f3d6
Jonny Burger <jonathanburger11@gmail.com>
2021-05-13 11:37:04 +0200

    scaffold out a cli

---
+++ packages/lambda/remotion-lambda-cli.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const {cli} = require('./dist/cli/index');
+
+cli()
+	.then(() => process.exit(0))
+	.catch((err) => {
+		// eslint-disable-next-line no-console
+		console.error(err);
+		process.exit(1);
+	});
