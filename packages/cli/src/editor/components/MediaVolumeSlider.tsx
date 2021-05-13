b6a670c89436085a2e365180a0b96760d03cb009
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-13 18:25:13 +0530

    volume slider
    Former-commit-id: 9ddbc28632e487dc593a3b1412314e05abc4bd0a

--- packages/cli/src/editor/components/MediaVolumeSlider.tsx
+++ packages/cli/src/editor/components/MediaVolumeSlider.tsx
@@ -1,11 +1,21 @@
-import React, {useCallback} from 'react';
+import {PlayerInternals} from '@remotion/player';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {Internals} from 'remotion';
 import {VolumeOffIcon, VolumeOnIcon} from '../icons/media-volume';
-import {ControlButton} from './ControlButton';
+import {ControlDiv} from './ControlButton';
+
+const xSpacer: React.CSSProperties = {
+	width: 5,
+};
 
 export const MediaVolumeSlider: React.FC = () => {
 	const [mediaMuted, setMediaMuted] = Internals.useMediaMutedState();
 	const [mediaVolume, setMediaVolume] = Internals.useMediaVolumeState();
+	const [dragging, setDragging] = useState<boolean>(false);
+	const currentRef = useRef<HTMLDivElement>(null);
+	const iconDivRef = useRef<HTMLDivElement>(null);
+	const parentDivRef = useRef<HTMLDivElement>(null);
+	const hover = PlayerInternals.useHoverState(parentDivRef);
 	console.log(mediaVolume);
 
 	const onClick = useCallback(() => {
@@ -13,9 +23,43 @@ export const MediaVolumeSlider: React.FC = () => {
 		setMediaVolume(Number(mediaMuted));
 	}, [setMediaMuted, mediaMuted, setMediaVolume]);
 
+	const onPointerDown = useCallback((e: React.PointerEvent<HTMLDivElement>) => {
+		console.log(e.clientX);
+		setDragging(true);
+	}, []);
+
+	const onPointerMove = useCallback((e: PointerEvent) => {
+		console.log(e.clientX);
+	}, []);
+
+	const onPointerUp = useCallback(() => {
+		console.log('poiter up');
+		setDragging(false);
+	}, []);
+	useEffect(() => {
+		currentRef?.current?.addEventListener('pointermove', onPointerMove);
+		currentRef?.current?.addEventListener('pointerup', onPointerUp);
+		return () => {
+			currentRef?.current?.removeEventListener('pointermove', onPointerMove);
+			currentRef?.current?.removeEventListener('pointerup', onPointerUp);
+		};
+	}, [currentRef, onPointerMove, onPointerUp, onPointerDown]);
+
 	return (
-		<ControlButton onClick={onClick}>
-			{mediaMuted ? <VolumeOffIcon /> : <VolumeOnIcon />}
-		</ControlButton>
+		<ControlDiv ref={parentDivRef}>
+			<div ref={iconDivRef} onClick={onClick}>
+				{mediaMuted ? <VolumeOffIcon /> : <VolumeOnIcon />}
+			</div>
+			<div style={xSpacer} />
+			{hover ? (
+				<div
+					ref={currentRef}
+					onPointerDown={onPointerDown}
+					style={{width: 100, height: 10, backgroundColor: '#fff'}}
+				/>
+			) : (
+				<div style={{width: 100}} />
+			)}
+		</ControlDiv>
 	);
 };
