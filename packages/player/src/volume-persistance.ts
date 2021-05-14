244e9adca14357c6f876d673b69cdcfaa92438d1
Jonny Burger <jonathanburger11@gmail.com>
2021-05-14 18:16:10 +0200

    Update volume-persistance.ts

--- packages/player/src/volume-persistance.ts
+++ packages/player/src/volume-persistance.ts
@@ -1,10 +1,18 @@
 const VOLUME_PERSISTANCE_KEY = 'remotion.volumePreference';
 
 export const persistVolume = (volume: number) => {
+	if (typeof window === 'undefined') {
+		return;
+	}
+
 	window.localStorage.setItem(VOLUME_PERSISTANCE_KEY, String(volume));
 };
 
-export const getPreferredVolume = () => {
+export const getPreferredVolume = (): number => {
+	if (typeof window === 'undefined') {
+		return 1;
+	}
+
 	const val = window.localStorage.getItem(VOLUME_PERSISTANCE_KEY);
 	return val ? Number(val) : 1;
 };
