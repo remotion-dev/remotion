70d13da2d157475b3de068b8c9ee6ee1f44e98f1
Jonny Burger <jonathanburger11@gmail.com>
2021-05-15 12:02:04 +0200

    add startfrom
    Former-commit-id: 5af14ca9586419cd7954865ffa8a8f4672cc68a9

--- packages/core/src/test/get-media-time.ts
+++ packages/core/src/test/get-media-time.ts
@@ -7,6 +7,32 @@ test('Should correctly calculate the media time of a video element', () => {
 			frame: 30,
 			playbackRate: 1,
 			src: 'video.mp4',
+			startFrom: 0,
 		})
 	).toBe(1);
+});
+
+test('Should correctly calculate the media time of a video element with faster framerate', () => {
+	expect(
+		getMediaTime({
+			fps: 30,
+			frame: 30,
+			playbackRate: 2,
+			src: 'video.mp4',
+			startFrom: 0,
+		})
+	).toBe(2);
+});
+
+test('Should correctly calculate the media time of a video element with faster framerate and a startFrom', () => {
+	// If playbackrate is 2, but the video only starts after 1 second, at 2sec, the video position should be 3sec
+	expect(
+		getMediaTime({
+			fps: 30,
+			frame: 60,
+			playbackRate: 2,
+			src: 'video.mp4',
+			startFrom: 30,
+		})
+	).toBe(3);
 });
