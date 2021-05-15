a716982a248ffc56df02498df6ad9deda2a3bc55
Jonny Burger <jonathanburger11@gmail.com>
2021-05-15 18:27:37 +0200

    works 🥵
    Former-commit-id: dcd9a085f56496376e7e949bb754d4d198762644

--- packages/example/src/ThreeBasic/index.tsx
+++ packages/example/src/ThreeBasic/index.tsx
@@ -1,36 +1,15 @@
 import {ThreeCanvas} from '@remotion/three';
 import React from 'react';
-import {interpolate, useCurrentFrame} from 'remotion';
+import {Mesh} from './Mesh';
 
 const ThreeBasic: React.FC = () => {
-	const frame = useCurrentFrame();
 	return (
 		<div style={{display: 'flex', width: '100%', backgroundColor: 'white'}}>
 			<ThreeCanvas
 				orthographic={false}
 				camera={{fov: 75, position: [0, 0, 470]}}
 			>
-				<ambientLight intensity={0.15} />
-				<pointLight args={[undefined, 0.4]} position={[200, 200, 0]} />
-
-				<mesh
-					position={[0, 0, 0]}
-					rotation={[
-						frame * 0.06 * 0.5,
-						frame * 0.07 * 0.5,
-						frame * 0.08 * 0.5,
-					]}
-					scale={interpolate(Math.sin(frame / 10), [-1, 1], [0.8, 1.2])}
-				>
-					<boxGeometry args={[100, 100, 100]} />
-					<meshStandardMaterial
-						color={[
-							Math.sin(frame * 0.12) * 0.5 + 0.5,
-							Math.cos(frame * 0.11) * 0.5 + 0.5,
-							Math.sin(frame * 0.08) * 0.5 + 0.5,
-						]}
-					/>
-				</mesh>
+				<Mesh />
 			</ThreeCanvas>
 		</div>
 	);
--- packages/three/src/ThreeCanvas.tsx
+++ packages/three/src/ThreeCanvas.tsx
@@ -1,144 +1,17 @@
-import { Canvas, render, unmountComponentAtNode } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Internals } from 'remotion';
-import { RemotionThreeContext, RemotionThreeContextType } from './ThreeContext';
-
-const block: React.CSSProperties = {
-	display: 'block',
-};
 
 export type ThreeCanvasProps = React.ComponentProps<typeof Canvas>;
 
-export const ThreeCanvas = ({
-	children,
-	tabIndex,
-	resize,
-	id,
-	style,
-	className,
-	events,
-	...rest
-}: ThreeCanvasProps) => {
-	const wrapperRef = React.useRef<HTMLDivElement>(null);
-	const canvasRef = React.useRef<HTMLCanvasElement>(null);
-
-	const remotionContexts = Internals.useRemotionContexts();
-
-	const [
-		threeDomNodesWrapper,
-		remotionThreeContext,
-	] = useCreateRemotionThreeContext();
-
-	const width = wrapperRef.current?.clientWidth;
-	const height = wrapperRef.current?.clientHeight;
-	const size = React.useMemo(
-		() => (!width || !height ? undefined : { width, height }),
-		[width, height]
-	);
-
-	React.useLayoutEffect(() => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const threeFiberContent = (
-			<RemotionThreeContext.Provider value={remotionThreeContext}>
-				<Internals.RemotionContextProvider contexts={remotionContexts}>
-					{children}
-				</Internals.RemotionContextProvider>
-			</RemotionThreeContext.Provider>
-		);
-		render(threeFiberContent, canvasRef.current, {
-			frameloop: 'demand',
-			orthographic: true,
-			...rest,
-			size,
-		});
-	});
-
-	React.useEffect(() => {
-		const canvas = canvasRef.current as HTMLCanvasElement;
-		return () => {
-			unmountComponentAtNode(canvas);
-		};
-	}, []);
-
+export const ThreeCanvas = (props: ThreeCanvasProps) => {
+	const { children, ...rest } = props;
+	const contexts = Internals.useRemotionContexts();
 	return (
-		<div
-			ref={wrapperRef}
-			id={id}
-			className={className}
-			tabIndex={tabIndex}
-			style={{
-				position: 'relative',
-				width: '100%',
-				height: '100%',
-				overflow: 'hidden',
-				...style,
-			}}
-		>
-			<canvas ref={canvasRef} style={block} />
-			{threeDomNodesWrapper}
-		</div>
+		<Canvas {...rest}>
+			<Internals.RemotionContextProvider contexts={contexts}>
+				{children}
+			</Internals.RemotionContextProvider>
+		</Canvas>
 	);
 };
-
-function useCreateRemotionThreeContext() {
-	const isMountedRef = React.useRef(true);
-	React.useEffect(
-		() => () => {
-			isMountedRef.current = false;
-		},
-		[]
-	);
-
-	const portalRef = React.useRef<HTMLDivElement>(null);
-
-	const remotionThreeContext = React.useState<RemotionThreeContextType>(() => ({
-		useDomNode: (node) => {
-			const _remotionContexts = Internals.useRemotionContexts();
-
-			const [containerNode] = React.useState(() =>
-				document.createElement('div')
-			);
-			React.useEffect(() => {
-				if (!portalRef.current) {
-					return;
-				}
-				(portalRef.current as HTMLDivElement).appendChild(containerNode);
-				return () => {
-					ReactDOM.unmountComponentAtNode(containerNode);
-					containerNode.remove();
-				};
-			}, [containerNode]);
-
-			React.useEffect(() => {
-				// Updating threeDomNodes has to happen outside the react-three render loop, so use a timeout
-				setTimeout(() => {
-					if (isMountedRef.current) {
-						const finalNode = (
-							<Internals.RemotionContextProvider contexts={_remotionContexts}>
-								{node}
-							</Internals.RemotionContextProvider>
-						);
-						ReactDOM.render(finalNode, containerNode);
-					}
-				});
-			});
-		},
-	}))[0];
-
-	const threeDomNodesWrapper = (
-		<div
-			ref={portalRef}
-			style={{
-				position: 'absolute',
-				top: 0,
-				left: 0,
-				overflow: 'hidden',
-				visibility: 'hidden',
-			}}
-		/>
-	);
-	return [threeDomNodesWrapper, remotionThreeContext] as const;
-}
