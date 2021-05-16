ac7e63508885a1e7f30c0375e7d97f2aa87cf8ba
Jonny Burger <jonathanburger11@gmail.com>
2021-05-16 12:07:33 +0200

    tweak asset names
    Former-commit-id: 9899f2d41052db7db83ee46f855eacc10a4e5370

--- packages/renderer/src/test/handle-weird-file-names.test.ts
+++ packages/renderer/src/test/handle-weird-file-names.test.ts
@@ -18,7 +18,54 @@ test('Should sanitize weird file names when downloading', async () => {
 	});
 	expect(newAsset.src).toBe(
 		process.platform === 'win32'
-			? '\\var\\tmp\\aWHv6qn3/X'
-			: '/var/tmp/aWHv6qn3/X'
+			? '\\var\\tmp\\06788239371962845'
+			: '/var/tmp/06788239371962845'
 	);
+});
+
+test('Should give different file names based on different url query parameters', async () => {
+	const asset1 = await downloadAndMapAssetsToFileUrl({
+		localhostAsset: {
+			src: 'https://gtts-api.miniggiodev.fr/Ici+Japon+Corp.mp4?hi=1',
+			frame: 0,
+			id: 'japon',
+			isRemote: false,
+			mediaFrame: 1,
+			type: 'video',
+			volume: 1,
+			playbackRate: 1,
+		},
+		webpackBundle: '/var/tmp',
+		onDownload: () => undefined,
+	});
+	const sameAgain = await downloadAndMapAssetsToFileUrl({
+		localhostAsset: {
+			src: 'https://gtts-api.miniggiodev.fr/Ici+Japon+Corp.mp4?hi=1',
+			frame: 0,
+			id: 'japon',
+			isRemote: false,
+			mediaFrame: 1,
+			type: 'video',
+			volume: 1,
+			playbackRate: 1,
+		},
+		webpackBundle: '/var/tmp',
+		onDownload: () => undefined,
+	});
+	const differentAsset = await downloadAndMapAssetsToFileUrl({
+		localhostAsset: {
+			src: 'https://gtts-api.miniggiodev.fr/Ici+Japon+Corp.mp4?hi=2',
+			frame: 0,
+			id: 'japon',
+			isRemote: false,
+			mediaFrame: 1,
+			type: 'video',
+			volume: 1,
+			playbackRate: 1,
+		},
+		webpackBundle: '/var/tmp',
+		onDownload: () => undefined,
+	});
+	expect(asset1.src).toEqual(sameAgain.src);
+	expect(asset1.src).not.toEqual(differentAsset.src);
 });
