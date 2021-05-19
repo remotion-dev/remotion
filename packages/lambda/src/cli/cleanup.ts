c055a4aa9a4d0b91f1e584dd6456a2f4da2f1240
Jonny Burger <jonathanburger11@gmail.com>
2021-05-19 12:51:01 +0200

    cleanup command
    Former-commit-id: 50b17b24fe730e0b3d1ff729c2f45325257f7e3c

--- packages/lambda/src/cli/cleanup.ts
+++ packages/lambda/src/cli/cleanup.ts
@@ -1,12 +1,53 @@
+import {LambdaClient} from '@aws-sdk/client-lambda';
+import {S3Client} from '@aws-sdk/client-s3';
 import {CliInternals} from '@remotion/cli';
 import {lambdaClient, s3Client} from '../aws-clients';
+import {BINARY_NAME} from '../bundle-remotion';
 import {chunk} from '../chunk';
-import {getRemotionLambdas} from '../cleanup/cleanup-lambdas';
-import {getRemotionS3Buckets} from '../cleanup/s3-buckets';
+import {cleanupLambdas, getRemotionLambdas} from '../cleanup/cleanup-lambdas';
+import {cleanUpBuckets, getRemotionS3Buckets} from '../cleanup/s3-buckets';
 import {REGION} from '../constants';
 import {Log} from './log';
 
-export const cleanupCommand = async () => {
+export const CLEANUP_COMMAND = 'cleanup';
+const LAMBDA_SUBCOMMAND = 'lambda';
+const S3_BUCKETS_SUBCOMMAND = 'buckets';
+
+const cleanupLambdaCommand = async (client: LambdaClient) => {
+	await cleanupLambdas({
+		lambdaClient: client,
+		onAfterDelete: (lambdaName: string) =>
+			Log.info(CliInternals.chalk.blue(`Deleted ${lambdaName}`)),
+	});
+	Log.info('All Remotion-related lambdas deleted.');
+};
+
+const cleanupBucketsCommand = async (client: S3Client) => {
+	await cleanUpBuckets({
+		s3client: client,
+		onBeforeBucketDeleted: (bucketName) => {
+			Log.info(`Deleting items of ${bucketName}`);
+		},
+		onAfterItemDeleted: ({itemName}) => {
+			Log.info(CliInternals.chalk.gray(`  Deleting item ${itemName}`));
+		},
+		onAfterBucketDeleted: (bucketName: string) =>
+			Log.info(CliInternals.chalk.blue(`Deleted bucket ${bucketName}.`)),
+	});
+	Log.info('All Remotion-related buckets deleted.');
+};
+
+export const cleanupCommand = async (args: string[]) => {
+	if (args[0] === LAMBDA_SUBCOMMAND) {
+		await cleanupLambdaCommand(lambdaClient);
+		return;
+	}
+
+	if (args[0] === S3_BUCKETS_SUBCOMMAND) {
+		await cleanupBucketsCommand(s3Client);
+		return;
+	}
+
 	const fetching = CliInternals.createOverwriteableCliOutput();
 	fetching.update('Fetching AWS account...');
 
@@ -14,16 +55,22 @@ export const cleanupCommand = async () => {
 		getRemotionS3Buckets(s3Client),
 		getRemotionLambdas(lambdaClient),
 	]);
+	if (remotionBuckets.length === 0 && lambdas.length === 0) {
+		fetching.update(
+			`Your AWS account in ${REGION} contains no following Remotion-related resources.\n`
+		);
+		return;
+	}
 
 	fetching.update(
 		`Your AWS account in ${REGION} contains the following Remotion-related resources:\n\n`
 	);
-	Log.info(
-		`${remotionBuckets.length} ${
-			remotionBuckets.length === 1 ? 'bucket' : 'buckets'
-		}:`
-	);
 	if (remotionBuckets.length !== 0) {
+		Log.info(
+			`${remotionBuckets.length} ${
+				remotionBuckets.length === 1 ? 'bucket' : 'buckets'
+			}:`
+		);
 		Log.info(
 			`${chunk(remotionBuckets, 2)
 				.map((b) =>
@@ -32,17 +79,32 @@ export const cleanupCommand = async () => {
 				.join('\n')}`
 		);
 
+		Log.info(
+			CliInternals.chalk.gray(
+				`Run \`${BINARY_NAME} ${CLEANUP_COMMAND} ${S3_BUCKETS_SUBCOMMAND}\` to permanently delete S3 buckets`
+			)
+		);
 		Log.info();
 	}
 
-	Log.info(`${lambdas.length} ${lambdas.length === 1 ? 'lambda' : 'lambdas'}:`);
-	Log.info(
-		`${chunk(lambdas, 2)
-			.map((b) =>
-				CliInternals.chalk.blue(
-					b.map((a) => (a.FunctionName ?? '').padEnd(40, ' ')).join(' ')
+	if (lambdas.length > 0) {
+		Log.info(
+			`${lambdas.length} ${lambdas.length === 1 ? 'lambda' : 'lambdas'}:`
+		);
+		Log.info(
+			`${chunk(lambdas, 2)
+				.map((b) =>
+					CliInternals.chalk.blue(
+						b.map((a) => (a.FunctionName ?? '').padEnd(40, ' ')).join(' ')
+					)
 				)
+				.join('\n')}`
+		);
+		Log.info(
+			CliInternals.chalk.gray(
+				`Run \`${BINARY_NAME} ${CLEANUP_COMMAND} ${LAMBDA_SUBCOMMAND}\` to permanently delete lambdas`
 			)
-			.join('\n')}`
-	);
+		);
+		Log.info();
+	}
 };
