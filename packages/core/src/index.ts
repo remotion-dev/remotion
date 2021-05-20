bc1dfbda0888e4a756683c4e6d22e80ca5eac673
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-21 01:58:33 +0530

    added stagger component
    Former-commit-id: 7e5e1851a771cc545fa97d56b3a87fbf4c4d243a

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -28,6 +28,7 @@ export * from './ready-manager';
 export {registerRoot} from './register-root';
 export {Sequence} from './sequencing';
 export * from './spring';
+export {Stagger} from './stagger';
 export * from './use-frame';
 export * from './use-video-config';
 export * from './video';
