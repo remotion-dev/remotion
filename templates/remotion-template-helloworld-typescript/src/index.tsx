3ad4b13fe30e050ba6442efe8a8c3467c883c1ab
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-27 14:33:35 +0530

    create 2 template, typescript and javascript

---
+++ templates/remotion-template-helloworld-typescript/src/index.tsx
@@ -0,0 +1,4 @@
+import {registerRoot} from 'remotion';
+import {RemotionVideo} from './Video';
+
+registerRoot(RemotionVideo);
---
+++ templates/remotion-template-helloworld/src/HelloWorld/Logo.jsx
@@ -0,0 +1,83 @@
+import {interpolate, spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {Arc} from './Arc';
+import {Atom} from './Atom';
+
+export const Logo = ({transitionStart}) => {
+	const videoConfig = useVideoConfig();
+	const frame = useCurrentFrame();
+
+	const development = spring({
+		config: {
+			damping: 100,
+			mass: 0.5,
+		},
+		fps: videoConfig.fps,
+		frame,
+	});
+
+	const rotationDevelopment = spring({
+		config: {
+			damping: 100,
+			mass: 0.5,
+		},
+		fps: videoConfig.fps,
+		frame,
+	});
+
+	const scaleIn = spring({
+		frame,
+		config: {
+			mass: 0.5,
+		},
+		fps: videoConfig.fps,
+	});
+
+	const translation = interpolate(
+		spring({
+			frame: frame - transitionStart,
+			fps: videoConfig.fps,
+			config: {
+				damping: 100,
+				mass: 0.5,
+			},
+		}),
+		[0, 1],
+		[0, -150]
+	);
+
+	const scale = frame < 50 ? scaleIn : 1;
+
+	const logoRotation = interpolate(
+		frame,
+		[0, videoConfig.durationInFrames],
+		[0, 360]
+	);
+
+	return (
+		<div
+			style={{
+				position: 'absolute',
+				width: videoConfig.width,
+				height: videoConfig.height,
+				transform: `scale(${scale}) translateY(${translation}px) rotate(${logoRotation}deg)`,
+			}}
+		>
+			<Arc
+				rotateProgress={rotationDevelopment}
+				progress={development}
+				rotation={30}
+			/>
+			<Arc
+				rotateProgress={rotationDevelopment}
+				rotation={90}
+				progress={development}
+			/>
+			<Arc
+				rotateProgress={rotationDevelopment}
+				rotation={-30}
+				progress={development}
+			/>
+			<Atom scale={rotationDevelopment} />
+		</div>
+	);
+};
