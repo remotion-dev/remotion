418bff8d17e132cca6193c8ba6556812203d80d1
Jonny Burger <jonathanburger11@gmail.com>
2021-06-01 18:14:43 +0200

    better validation in renderFrames()

--- packages/core/src/validation/validate-fps.ts
+++ packages/core/src/validation/validate-fps.ts
@@ -1,19 +1,21 @@
-export const validateFps = (fps: number) => {
+export const validateFps = (fps: number, location: string) => {
 	if (typeof fps !== 'number') {
 		throw new Error(
-			`"fps" must be a number, but you passed a value of type ${typeof fps}`
+			`"fps" must be a number, but you passed a value of type ${typeof fps} ${location}`
 		);
 	}
 
 	if (!Number.isFinite(fps)) {
-		throw new Error(`"fps" must be a finite, but you passed ${fps}`);
+		throw new Error(
+			`"fps" must be a finite, but you passed ${fps} ${location}`
+		);
 	}
 
 	if (isNaN(fps)) {
-		throw new Error(`"fps" must not be NaN, but got ${fps}`);
+		throw new Error(`"fps" must not be NaN, but got ${fps} ${location}`);
 	}
 
 	if (fps <= 0) {
-		throw new TypeError(`"fps" must be positive, but got ${fps}.`);
+		throw new TypeError(`"fps" must be positive, but got ${fps} ${location}`);
 	}
 };
