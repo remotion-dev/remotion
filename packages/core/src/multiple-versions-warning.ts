f22e71671ea1739163b1e928fe97a7646c7a421c
Jonny Burger <jonathanburger11@gmail.com>
2021-06-01 17:04:46 +0200

    expand on error message
    Former-commit-id: e4582a64d66b9b8de8fb5a1298837a4ff03ce8a5

--- packages/core/src/multiple-versions-warning.ts
+++ packages/core/src/multiple-versions-warning.ts
@@ -6,6 +6,9 @@ export const checkMultipleRemotionVersions = () => {
 	if (window.remotion_imported) {
 		console.error('🚨 Multiple versions of Remotion detected.');
 		console.error(
+			'Multiple versions will cause conflicting React contexts and things may break in an unexpected way.'
+		);
+		console.error(
 			'Please check your dependency tree and make sure only one version of Remotion is on the page.'
 		);
 	}
