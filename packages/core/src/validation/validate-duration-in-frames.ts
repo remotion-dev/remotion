f0cd5c4627bc0a53f8e9dedb6d654957609aa5ed
Jonny Burger <jonathanburger11@gmail.com>
2021-06-01 16:12:22 +0200

    fix tests
    Former-commit-id: c4087c52a4d314d5a40bebca17669ba07521e6ab

--- packages/core/src/validation/validate-duration-in-frames.ts
+++ packages/core/src/validation/validate-duration-in-frames.ts
@@ -1,19 +1,19 @@
 export const validateDurationInFrames = (durationInFrames: number) => {
 	if (typeof durationInFrames !== 'number') {
 		throw new Error(
-			`The "durationInFrames" of a composition must be a number, but you passed a value of type ${typeof durationInFrames}`
+			`The "durationInFrames" prop of the <Composition/> component must be a number, but you passed a value of type ${typeof durationInFrames}`
 		);
 	}
 
 	if (durationInFrames <= 0) {
 		throw new TypeError(
-			`The "durationInFrames" of a composition must be positive, but got ${durationInFrames}.`
+			`The "durationInFrames" prop of the <Composition/> component must be positive, but got ${durationInFrames}.`
 		);
 	}
 
 	if (durationInFrames % 1 !== 0) {
 		throw new TypeError(
-			`The "durationInFrames" of a composition must be an integer, but got ${durationInFrames}.`
+			`The "durationInFrames" prop of the <Composition/> component must be an integer, but got ${durationInFrames}.`
 		);
 	}
 };
