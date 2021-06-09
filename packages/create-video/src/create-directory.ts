8e75df20d9a804412d2276dfc04a8f214a2086ce
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-06-09 15:50:05 +0530

    add more validation
    Former-commit-id: b005ee071ad1e220176b1afae1d232740d0a8996

--- packages/create-video/src/create-directory.ts
+++ packages/create-video/src/create-directory.ts
@@ -1,9 +1,79 @@
+import fs from 'fs-extra';
+import * as path from 'path';
+import {Log} from './log';
+
+const TOLERABLE_FILES = [
+	// System
+	'.DS_Store',
+	'Thumbs.db',
+	// Git
+	'.git',
+	'.gitattributes',
+	'.gitignore',
+	// Project
+	'.npmignore',
+	'.travis.yml',
+	'LICENSE',
+	'docs',
+	'.idea',
+	// Package manager
+	'npm-debug.log',
+	'yarn-debug.log',
+	'yarn-error.log',
+];
+
 export function validateName(name?: string): string | true {
 	if (typeof name !== 'string' || name === '') {
 		return 'The project name can not be empty.';
 	}
 	if (!/^[a-z0-9@.\-_]+$/i.test(name)) {
 		return 'The project name can only contain URL-friendly characters (alphanumeric and @ . -  _)';
+	}
+	return true;
+}
+
+export function getConflictsForDirectory(
+	projectRoot: string,
+	tolerableFiles: string[] = TOLERABLE_FILES
+): string[] {
+	return fs
+		.readdirSync(projectRoot)
+		.filter(
+			(file: string) => !(/\.iml$/.test(file) || tolerableFiles.includes(file))
+		);
+}
+
+export async function assertFolderEmptyAsync({
+	projectRoot,
+	folderName = path.dirname(projectRoot),
+	overwrite,
+}: {
+	projectRoot: string;
+	folderName?: string;
+	overwrite: boolean;
+}): Promise<boolean> {
+	const conflicts = getConflictsForDirectory(projectRoot);
+	if (conflicts.length) {
+		Log.info(
+			`The directory ${Log.chalk.green(
+				folderName
+			)} has files that might be overwritten:`
+		);
+		Log.newLine();
+		for (const file of conflicts) {
+			Log.info(`  ${file}`);
+		}
+
+		if (overwrite) {
+			Log.newLine();
+			Log.info(`Removing existing files from ${Log.chalk.green(folderName)}`);
+			await Promise.all(
+				conflicts.map((conflict) => fs.remove(path.join(projectRoot, conflict)))
+			);
+			return true;
+		}
+
+		return false;
 	}
 	return true;
 }
