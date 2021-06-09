b005ee071ad1e220176b1afae1d232740d0a8996
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-06-09 15:50:05 +0530

    add more validation

--- packages/create-video/src/init-v2.ts
+++ packages/create-video/src/init-v2.ts
@@ -1,7 +1,39 @@
 import chalk from 'chalk';
+import fs from 'fs-extra';
 import path from 'path';
 import prompts from 'prompts';
-import {validateName} from './create-directory';
+import * as CreateDirectory from './create-directory';
+import {Log} from './log';
+
+function assertValidName(folderName: string) {
+	const validation = CreateDirectory.validateName(folderName);
+	if (typeof validation === 'string') {
+		throw new Error(
+			`Cannot create an app named ${chalk.red(
+				`"${folderName}"`
+			)}. ${validation}`
+		);
+	}
+}
+
+async function assertFolderEmptyAsync(
+	projectRoot: string,
+	folderName?: string
+) {
+	if (
+		!(await CreateDirectory.assertFolderEmptyAsync({
+			projectRoot,
+			folderName,
+			overwrite: false,
+		}))
+	) {
+		const message = 'Try using a new directory name, or moving these files.';
+		Log.newLine();
+		Log.info(message);
+		Log.newLine();
+		throw new Error(message);
+	}
+}
 
 export const init = async () => {
 	let projectName = process.argv[2];
@@ -15,7 +47,9 @@ export const init = async () => {
 				message: 'What would you like to name your app?',
 				initial: 'my-app',
 				validate: (name) => {
-					const validation = validateName(path.basename(path.resolve(name)));
+					const validation = CreateDirectory.validateName(
+						path.basename(path.resolve(name))
+					);
 					if (typeof validation === 'string') {
 						return 'Invalid project name: ' + validation;
 					}
@@ -28,18 +62,18 @@ export const init = async () => {
 			}
 		} catch (error) {
 			// Handle the aborted message in a custom way.
-			console.log('nice');
 			if (error.code !== 'ABORTED') {
 				throw error;
 			}
 		}
-		const validation = validateName(projectName);
-		if (typeof validation === 'string') {
-			throw new Error(
-				`Cannot create an app named ${chalk.red(
-					`"${projectName}"`
-				)}. ${validation}`
-			);
-		}
+
+		const projectRoot = path.resolve(projectName);
+		const folderName = path.basename(projectRoot);
+
+		assertValidName(folderName);
+
+		await fs.ensureDir(projectRoot);
+
+		await assertFolderEmptyAsync(projectRoot, folderName);
 	}
 };
