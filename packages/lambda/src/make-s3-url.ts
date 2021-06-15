553c67343639ab6c8b3036d8534dd518f5b609a2
Jonny Burger <jonathanburger11@gmail.com>
2021-06-15 19:11:23 +0200

    site ID parsing
    Former-commit-id: 3411bacd04f77db0fb0c6904c6fcccc8036362ce

--- packages/lambda/src/make-s3-url.ts
+++ packages/lambda/src/make-s3-url.ts
@@ -1,7 +1,27 @@
-// TODO: Do it with HTTPS, but wait for certificate
+// TODO: wait for certificate
 
 import {REGION} from './constants';
 
 export const makeS3Url = (bucketName: string, subFolder: string): string => {
 	return `https://${bucketName}.s3.${REGION}.amazonaws.com/${subFolder}`;
+};
+
+// TODO: Instead of site ID, could also just make a hash
+export const getSiteId = (url: string) => {
+	const match = url.match(/https:\/\/(.*)\.s3\.(.*)\.com\/sites\/(.*)/);
+
+	if (!match) {
+		throw new Error('invalid aws url ' + url);
+	}
+
+	const lastPart = match[3];
+
+	const siteId = lastPart.substr(0, lastPart.indexOf('/'));
+
+	return {
+		bucketName: match[1],
+		// TODO: is wrong
+		region: match[2],
+		siteId,
+	};
 };
