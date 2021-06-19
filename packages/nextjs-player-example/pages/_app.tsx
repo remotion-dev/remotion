27ca64b6e08b330a90bfa1131ff78fca54edccb4
Jonny Burger <jonathanburger11@gmail.com>
2021-06-19 21:52:16 +0200

    just one player

--- packages/nextjs-player-example/pages/_app.tsx
+++ packages/nextjs-player-example/pages/_app.tsx
@@ -1,16 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <h1 style={{textAlign:"center", color:"rgb(74,149,245)", textDecoration:"underline"}}> Remotion Player with Next.js</h1>
-  <div style={{display: "flex", justifyContent:"center"}}>
-    
-    <Component {...pageProps} />
-    <Component {...pageProps} />
-  </div>
-  </div>)
+      <h1
+        style={{
+          textAlign: "center",
+          color: "rgb(74,149,245)",
+          textDecoration: "underline",
+        }}
+      >
+        {" "}
+        Remotion Player with Next.js
+      </h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
