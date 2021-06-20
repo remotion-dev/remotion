0cbed590082a07701394136f724d7270d6b5ad6e
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-06-20 20:50:48 +0530

    added jsx on example folder

---
+++ packages/example/src/video-js.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Composition} from 'remotion';
+import {Framer} from './FramerJs';
+
+export const Index = () => {
+	return (
+		<>
+			<Composition
+				id="framer"
+				component={Framer}
+				width={1080}
+				height={1080}
+				fps={30}
+				durationInFrames={100}
+			/>
+		</>
+	);
+};
