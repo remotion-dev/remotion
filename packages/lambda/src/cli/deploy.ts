ce6393cfca2dc2e8b37a1c7ece0642e7e3bcb98e
Jonny Burger <jonathanburger11@gmail.com>
2021-06-22 20:56:48 +0200

    update deployLambda API

--- packages/lambda/src/cli/deploy.ts
+++ packages/lambda/src/cli/deploy.ts
@@ -1,10 +1,18 @@
 import {deployLambda} from '../api/deploy-lambda';
+import {ensureLambdaBinaries} from '../api/ensure-lambda-binaries';
 import {getAwsRegion} from './get-aws-region';
 import {Log} from './log';
 
 export const DEPLOY_COMMAND = 'deploy';
 
 export const deployCommand = async () => {
-	const {functionName} = await deployLambda({region: getAwsRegion()});
+	// TODO: Unhardcode timeout
+	const TIMEOUT_HARDCODED = 120;
+	const {layerArn} = await ensureLambdaBinaries(getAwsRegion());
+	const {functionName} = await deployLambda({
+		region: getAwsRegion(),
+		timeoutInSeconds: TIMEOUT_HARDCODED,
+		layerArn,
+	});
 	Log.info(`Deployed to ${functionName}`);
 };
