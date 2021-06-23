d4d4d8fa826d57486af571d60d18cf1e8b5f17b1
Jonny Burger <jonathanburger11@gmail.com>
2021-06-23 18:48:09 +0200

    Update help.ts

--- packages/lambda/src/cli/help.ts
+++ packages/lambda/src/cli/help.ts
@@ -1,6 +1,7 @@
 import {CliInternals} from '@remotion/cli';
 import {BINARY_NAME} from '../shared/constants';
 import {CLEANUP_COMMAND} from './cleanup';
+import {FUNCTIONS_COMMAND} from './commands/functions';
 import {Log} from './log';
 import {RENDER_COMMAND} from './render';
 import {UPLOAD_COMMAND} from './upload';
@@ -30,4 +31,8 @@ export const printHelp = () => {
 			'Delete Remotion-related infrastructure from your AWS account.'
 		)
 	);
+
+	Log.info();
+	Log.info(`${BINARY_NAME} ${FUNCTIONS_COMMAND}`);
+	Log.info(CliInternals.chalk.gray('Deploy and manage AWS Lambda functions.'));
 };
