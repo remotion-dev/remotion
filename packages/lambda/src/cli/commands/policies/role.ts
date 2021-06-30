565cdd2022e6c6e3d1721213e732aea3b7951011
Jonny Burger <jonathanburger11@gmail.com>
2021-06-30 18:13:47 +0200

    progress

--- packages/lambda/src/cli/commands/policies/role.ts
+++ packages/lambda/src/cli/commands/policies/role.ts
@@ -3,6 +3,5 @@ import {Log} from '../../log';
 export const ROLE_SUBCOMMAND = 'role';
 
 export const roleSubcommand = () => {
-	Log.info('Policy for role:');
 	Log.info(JSON.stringify(getRolePolicy(), null, 2));
 };
