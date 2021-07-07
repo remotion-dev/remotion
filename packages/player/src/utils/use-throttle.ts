36951e371292a2056d160a7a4a8afe6aba42e150
Ashik Meerankutty <ashik9591@gmail.com>
2021-07-07 18:22:43 +0530

    Formatting using prettier

--- packages/player/src/utils/use-throttle.ts
+++ packages/player/src/utils/use-throttle.ts
@@ -1,23 +1,22 @@
-
-import { useEffect, useRef, useState } from 'react'
+import {useEffect, useRef, useState} from 'react';
 
 export const useThrottle = <T>(value: T, interval = 250): T => {
-  const [throttledValue, setThrottledValue] = useState<T>(value)
-  const lastExecuted = useRef<number>(Date.now())
+	const [throttledValue, setThrottledValue] = useState<T>(value);
+	const lastExecuted = useRef<number>(Date.now());
 
-  useEffect(() => {
-    if (Date.now() >= lastExecuted.current + interval) {
-      lastExecuted.current = Date.now()
-      setThrottledValue(value)
-    } else {
-      const timerId = setTimeout(() => {
-        lastExecuted.current = Date.now()
-        setThrottledValue(value)
-      }, interval)
+	useEffect(() => {
+		if (Date.now() >= lastExecuted.current + interval) {
+			lastExecuted.current = Date.now();
+			setThrottledValue(value);
+		} else {
+			const timerId = setTimeout(() => {
+				lastExecuted.current = Date.now();
+				setThrottledValue(value);
+			}, interval);
 
-      return () => clearTimeout(timerId)
-    }
-  }, [value, interval])
+			return () => clearTimeout(timerId);
+		}
+	}, [value, interval]);
 
-  return throttledValue
-}
+	return throttledValue;
+};
