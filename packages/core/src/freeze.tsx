3a97a48557f5141ff0caf143a9dc99ac230f777c
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-07-09 00:10:10 +0530

    added `<Freeze />` component
    Former-commit-id: 050822bbe79979feab2e56a068fc2072c124ef49

--- packages/core/src/freeze.tsx
+++ packages/core/src/freeze.tsx
@@ -1,16 +1,22 @@
 import React, {useContext, useMemo} from 'react';
-import {TimelineContext,TimelineContextValue} from './timeline-position-state';
-
+import {TimelineContext, TimelineContextValue} from './timeline-position-state';
 
 type FreezeProps = {
 	frame: number;
 };
 
-const Freeze: React.FC<FreezeProps> = ({frame,children}) => {
+const Freeze: React.FC<FreezeProps> = ({frame, children}) => {
+	if (typeof frame !== 'number') {
+		throw new Error(
+			`You passed to 'frame' an argument of type ${typeof frame}, but it must be a number.`
+		);
+	}
+
 	const context = useContext(TimelineContext);
 	const value: TimelineContextValue = useMemo(() => {
 		return {
 			...context,
+			playing: false,
 			frame,
 		};
 	}, [context, frame]);
@@ -22,4 +28,4 @@ const Freeze: React.FC<FreezeProps> = ({frame,children}) => {
 	);
 };
 
-export {Freeze}
+export {Freeze};
