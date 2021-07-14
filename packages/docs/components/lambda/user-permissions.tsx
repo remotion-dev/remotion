015502a254dcafd0f629ac7f9fefd318cb3a7c46
Jonny Burger <jonathanburger11@gmail.com>
2021-07-14 16:57:00 +0200

    document getUserPolicy and getRolePolicy

--- packages/docs/components/lambda/user-permissions.tsx
+++ packages/docs/components/lambda/user-permissions.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const LambdaRegionList: React.FC = () => {
   return (
     <div>
-      <pre>{JSON.stringify(getUserPolicy(), null, 2)}</pre>
+      <pre>{getUserPolicy()}</pre>
     </div>
   );
 };
