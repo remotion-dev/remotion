e14cf1dd8e322dd307e834c75eedf15283b830e9
Jonny Burger <jonathanburger11@gmail.com>
2021-07-19 16:37:56 +0200

    load config file from `remotion lambda` cli command

--- packages/cli/src/lambda-command.ts
+++ packages/cli/src/lambda-command.ts
@@ -1,5 +1,6 @@
+import {loadConfig} from './get-config-file-name';
 import {Log} from './log';
-import {parsedCli} from './parse-command-line';
+import {parseCommandLine, parsedCli} from './parse-command-line';
 
 export const lambdaCommand = async () => {
 	try {
@@ -7,6 +8,14 @@ export const lambdaCommand = async () => {
 			paths: [process.cwd()],
 		});
 		const {LambdaInternals} = require(path);
+		parseCommandLine();
+		const appliedName = loadConfig();
+		if (appliedName) {
+			Log.verbose(`Applied configuration from ${appliedName}.`);
+		} else {
+			Log.verbose('No config file loaded.');
+		}
+
 		await LambdaInternals.executeCommand(parsedCli._.slice(1));
 		process.exit(0);
 	} catch (err) {
