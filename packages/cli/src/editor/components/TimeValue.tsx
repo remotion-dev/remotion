a67ebf46bbd8198e02277164e2708e3e11dcdecb
Jonny Burger <jonathanburger11@gmail.com>
2021-08-11 15:25:56 +0200

    editor update
    Former-commit-id: db76068f16a79635d89c2ce0f9b27ff1b39b266c

--- packages/cli/src/editor/components/TimeValue.tsx
+++ packages/cli/src/editor/components/TimeValue.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Internals, useCurrentFrame} from 'remotion';
 import styled from 'styled-components';
+import {useIsStill} from '../helpers/is-current-selected-still';
 import {renderFrame} from '../state/render-frame';
 
 const Text = styled.div`
@@ -27,14 +28,19 @@ const Frame = styled.span`
 export const TimeValue: React.FC = () => {
 	const frame = useCurrentFrame();
 	const config = Internals.useUnsafeVideoConfig();
+	const isStill = useIsStill();
 
 	if (!config) {
 		return null;
 	}
 
+	if (isStill) {
+		return null;
+	}
+
 	return (
 		<Text>
-			<Time> {renderFrame(frame, config.fps)}</Time>{' '}
+			<Time>{renderFrame(frame, config.fps)}</Time>{' '}
 			<Frame>
 				{frame} <span>({config.fps} fps)</span>
 			</Frame>
