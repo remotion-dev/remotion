9a1ae1a77d0d4758ff5b21347200983e1694de38
Jonny Burger <jonathanburger11@gmail.com>
2021-08-25 15:36:52 +0200

    write tests
    Former-commit-id: a7739aef95289c8a2d9cadcacbca1688341d46e3

--- packages/core/src/stagger/index.tsx
+++ packages/core/src/stagger/index.tsx
@@ -5,6 +5,7 @@ import {validateDurationInFrames} from '../validation/validate-duration-in-frame
 type StaggerChildProps = PropsWithChildren<
 	{
 		durationInFrames: number;
+		offset?: number;
 	} & Pick<SequenceProps, 'layout' | 'name'>
 >;
 
@@ -16,18 +17,34 @@ const StaggerChild = ({children}: StaggerChildProps) => {
 const Stagger: FC = ({children}) => {
 	const childrenValue = useMemo(() => {
 		let startFrame = 0;
-		return Children.map(children, (child) => {
-			const castedChild = (child as unknown) as {
-				props: StaggerChildProps;
-				type: typeof StaggerChild;
-			};
-			if (!castedChild || !castedChild.props.children) {
-				throw new TypeError(`The StaggerChild component must have children.`);
+		return Children.map(children, (child, i) => {
+			const castedChild = (child as unknown) as
+				| {
+						props: StaggerChildProps;
+						type: typeof StaggerChild;
+				  }
+				| string;
+			if (typeof castedChild === 'string') {
+				if (castedChild.trim() === '') {
+					return null;
+				}
+
+				throw new TypeError(
+					`The <Stagger> component only accepts a list of <StaggerChild /> components as it's children, but you passed a string "${castedChild}"`
+				);
 			}
 
 			if (castedChild.type !== StaggerChild) {
 				throw new TypeError(
 					"The <Stagger> component only accepts a list of <StaggerChild /> components as it's children"
+				);
+			}
+
+			const debugInfo = `index = ${i}, duration = ${castedChild.props.durationInFrames}`;
+
+			if (!castedChild || !castedChild.props.children) {
+				throw new TypeError(
+					`A <StaggerChild /> component (${debugInfo}) doesn't have any children.`
 				);
 			}
 
@@ -38,8 +55,27 @@ const Stagger: FC = ({children}) => {
 				...passedProps
 			} = castedChild.props;
 			validateDurationInFrames(durationInFramesProp, `<StaggerChild />`);
-			const currentStartFrame = startFrame;
-			startFrame += durationInFramesProp;
+			const offset = castedChild.props.offset ?? 0;
+			if (Number.isNaN(offset)) {
+				throw new TypeError(
+					`The "offset" property of a <StaggerChild /> must not be NaN, but got NaN (${debugInfo}).`
+				);
+			}
+
+			if (!Number.isFinite(offset)) {
+				throw new TypeError(
+					`The "offset" property of a <StaggerChild /> must be finite, but got ${offset} (${debugInfo}).`
+				);
+			}
+
+			if (offset % 1 !== 0) {
+				throw new TypeError(
+					`The "offset" property of a <StaggerChild /> must be finite, but got ${offset} (${debugInfo}).`
+				);
+			}
+
+			const currentStartFrame = startFrame + offset;
+			startFrame += durationInFramesProp + offset;
 			return (
 				<Sequence
 					from={currentStartFrame}
