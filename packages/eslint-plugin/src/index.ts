37cda9c09258848cc6ae9756df8c4f0f39a2fff4
Jonny Burger <jonathanburger11@gmail.com>
2021-08-27 16:44:24 +0200

    even dimensions
    Former-commit-id: 316bb4b1352910dfeffe2b5b1070077a6e6867ec

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,4 +1,5 @@
 import deterministicRandomness from "./rules/deterministic-randomness";
+import evenDimensions from "./rules/even-dimensions";
 import nomp4Import from "./rules/no-mp4-import";
 import noStringAssets from "./rules/no-string-assets";
 import warnNativeMediaTag from "./rules/warn-native-media-tag";
@@ -8,6 +9,7 @@ const rules = {
   "warn-native-media-tag": warnNativeMediaTag,
   "deterministic-randomness": deterministicRandomness,
   "no-string-assets": noStringAssets,
+  "even-dimensions": evenDimensions,
 };
 
 export = {
