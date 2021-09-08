5930a59d32d176868f7a3e4dd22fa52d2e6bbf7c
Jonny Burger <jonathanburger11@gmail.com>
2021-09-08 16:25:59 +0200

    abstract modal
    Former-commit-id: 4915c0f12e3023fb016032c564d4f811a41b910f

---
+++ packages/cli/src/editor/components/ModalContainer.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect} from 'react';
+import {BACKGROUND} from '../helpers/colors';
+import {FONT_FAMILY} from '../helpers/font';
+import {useKeybinding} from '../helpers/use-keybinding';
+
+const backgroundOverlay: React.CSSProperties = {
+	backgroundColor: 'rgba(255, 255, 255, 0.2)',
+	backdropFilter: `blur(1px)`,
+	position: 'fixed',
+	height: '100%',
+	width: '100%',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+};
+
+const panel: React.CSSProperties = {
+	backgroundColor: BACKGROUND,
+	boxShadow: '0 0 4px black',
+	color: 'white',
+	fontFamily: FONT_FAMILY,
+};
+
+export const ModalContainer: React.FC<{
+	onEscape: () => void;
+}> = ({children, onEscape}) => {
+	const keybindings = useKeybinding();
+
+	useEffect(() => {
+		keybindings.stashOther();
+		const escape = keybindings.registerKeybinding(
+			'keydown',
+			'Escape',
+			onEscape
+		);
+
+		return () => {
+			keybindings.unstashOther();
+			escape.unregister();
+		};
+	}, [keybindings, onEscape]);
+
+	return (
+		<div style={backgroundOverlay} role="dialog" aria-modal="true">
+			<div style={panel}>{children}</div>
+		</div>
+	);
+};
