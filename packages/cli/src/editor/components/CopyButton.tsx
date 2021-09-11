99040ee7a86ca7e441ad4fa85c767b7fe2155f07
Jonny Burger <jonathanburger11@gmail.com>
2021-09-11 16:21:34 +0200

    finish update command
    Former-commit-id: 67a792b429c7da643086279a2e7669732526ae1c

---
+++ packages/cli/src/editor/components/CopyButton.tsx
@@ -0,0 +1,60 @@
+import React, {useCallback, useState} from 'react';
+import {copyCmd} from '../helpers/copy-text';
+import {Row, Spacing} from './layout';
+
+const copyIcon = (
+	<svg
+		aria-hidden="true"
+		focusable="false"
+		data-prefix="far"
+		data-icon="clipboard"
+		className="svg-inline--fa fa-clipboard fa-w-12"
+		role="img"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 384 512"
+		style={{width: 16, height: 16, color: 'white'}}
+	>
+		<path
+			fill="currentColor"
+			d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm144 418c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V118c0-3.3 2.7-6 6-6h42v36c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-36h42c3.3 0 6 2.7 6 6z"
+		/>
+	</svg>
+);
+
+const container: React.CSSProperties = {
+	border: `2px solid rgba(255, 255, 255, 0.2)`,
+	borderRadius: 4,
+	padding: 10,
+	cursor: 'pointer',
+};
+
+const button: React.CSSProperties = {
+	backgroundColor: 'transparent',
+	appearance: 'none',
+	border: 'none',
+	fontFamily: 'inherit',
+	fontSize: 16,
+	color: 'white',
+};
+
+export const CopyButton: React.FC<{
+	textToCopy: string;
+	label: string;
+	labelWhenCopied: string;
+}> = ({textToCopy, label, labelWhenCopied}) => {
+	const [copied, setCopied] = useState(false);
+
+	const onClick = useCallback(() => {
+		copyCmd(textToCopy);
+		setCopied(true);
+	}, [textToCopy]);
+
+	return (
+		<button onClick={onClick} style={button} type="button">
+			<Row style={container}>
+				{copyIcon}
+				<Spacing x={1} /> {copied ? labelWhenCopied : label}
+			</Row>
+		</button>
+	);
+};
--- packages/cli/src/editor/components/UpdateCheck.tsx
+++ packages/cli/src/editor/components/UpdateCheck.tsx
@@ -1,30 +1,5 @@
 import React, {useCallback, useContext, useEffect, useState} from 'react';
-import styled from 'styled-components';
-import {copyCmd} from '../helpers/copy-text';
 import {ModalsContext} from '../state/modals';
-
-export const Container = styled.div`
-	background: linear-gradient(to right, #4290f5, #42e9f5);
-	font-family: ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	color: white;
-	text-align: center;
-	font-weight: bold;
-	font-size: 14px;
-	a {
-		color: white;
-		text-decoration: underline;
-		cursor: pointer;
-	}
-	code {
-		background-color: rgba(255, 255, 255, 0.3);
-		border-radius: 4px;
-		padding: 3px 8px;
-		&:active {
-			color: black;
-		}
-	}
-`;
 
 type PackageManager = 'npm' | 'yarn' | 'unknown';
 
@@ -36,14 +11,13 @@ export type UpdateInfo = {
 	packageManager: PackageManager;
 };
 
-const makeLocalStorageKey = (version: string) => `update-dismiss-${version}`;
-
-const dismissVersion = (version: string) => {
-	window.localStorage.setItem(makeLocalStorageKey(version), 'true');
-};
-
-const isVersionDismissed = (version: string) => {
-	return window.localStorage.getItem(makeLocalStorageKey(version)) === 'true';
+const buttonStyle: React.CSSProperties = {
+	appearance: 'none',
+	color: '#4290f5',
+	border: 'none',
+	fontWeight: 'bold',
+	backgroundColor: 'transparent',
+	cursor: 'pointer',
 };
 
 export const UpdateCheck = () => {
@@ -57,19 +31,6 @@ export const UpdateCheck = () => {
 			.catch((err) => {
 				console.log('Could not check for updates', err);
 			});
-	}, []);
-
-	const dismiss = useCallback(() => {
-		if (info === null) {
-			return;
-		}
-
-		dismissVersion(info.latestVersion);
-		setInfo(null);
-	}, [info]);
-
-	const remindLater = useCallback(() => {
-		setInfo(null);
 	}, []);
 
 	useEffect(() => {
@@ -91,43 +52,9 @@ export const UpdateCheck = () => {
 		return null;
 	}
 
-	if (isVersionDismissed(info.latestVersion)) {
-		return null;
-	}
-
 	return (
-		<Container>
-			Update available! {info.currentVersion} ➡️{' '}
-			<span style={{width: 8, display: 'inline-block'}} />
-			{info.latestVersion}. Run{' '}
-			{info.packageManager === 'yarn' ? (
-				<code
-					onClick={() => copyCmd('yarn upgrade')}
-					style={{cursor: 'pointer'}}
-				>
-					yarn upgrade
-				</code>
-			) : (
-				<code
-					onClick={() => copyCmd('npm run upgrade')}
-					style={{cursor: 'pointer'}}
-				>
-					npm run upgrade
-				</code>
-			)}{' '}
-			to get it. <br />
-			<a
-				href="https://github.com/remotion-dev/remotion/releases"
-				target="_blank"
-			>
-				Release notes
-			</a>
-			<span style={{width: 8, display: 'inline-block'}} />
-			<a onClick={remindLater}>Remind me next time</a>
-			<span style={{width: 8, display: 'inline-block'}} />
-			<a onClick={dismiss}>Skip this version</a>
-			<span style={{width: 8, display: 'inline-block'}} />
-			<a onClick={openModal}>Open modal</a>
-		</Container>
+		<button style={buttonStyle} onClick={openModal} type="button">
+			Update available!
+		</button>
 	);
 };
