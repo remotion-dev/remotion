634ffa5193a329b55d7b36abbe04a1617ac30515
辉宇 <zhongliang.yzl@alibaba-inc.com>
2021-09-13 17:58:02 +0800

    feat: concurrentMode

--- packages/core/src/config/concurrent-mode.ts
+++ packages/core/src/config/concurrent-mode.ts
@@ -1,10 +1,10 @@
 const validOptions = ['tab','browser'] as const;
 
-export type concurrentMode = typeof validOptions[number];
+export type ConcurrentMode = typeof validOptions[number];
 
-let currentConcurrentMode: concurrentMode = 'tab';
+let currentConcurrentMode: ConcurrentMode = 'tab';
 
-export const setConcurrentMode = (mode: concurrentMode) => {
+export const setConcurrentMode = (mode: ConcurrentMode) => {
 	if (!validOptions.includes(mode)) {
 		throw new TypeError(`Value ${mode} is not valid as a concurrent mode.`);
 	}
