d3efed28d9cc9dcbe9eaf2ca8e1a1f5b8554020d
Jonny Burger <jonathanburger11@gmail.com>
2021-09-15 18:54:57 +0200

    fix frames per lambda

--- packages/lambda/src/shared/get-function-version.ts
+++ packages/lambda/src/shared/get-function-version.ts
@@ -21,7 +21,12 @@ export const getFunctionVersion = async ({
 		return result.version;
 	} catch (err) {
 		// Prerelease versions had no info command
-		if (err.message.includes(COMMAND_NOT_FOUND)) {
+		if ((err as Error).message.includes(COMMAND_NOT_FOUND)) {
+			return 'n/a';
+		}
+
+		// Bug in certain version of AWS S3 kit
+		if ((err as Error).message.includes('AWS CRT binary not present ')) {
 			return 'n/a';
 		}
 
