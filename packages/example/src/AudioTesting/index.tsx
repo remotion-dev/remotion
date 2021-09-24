80b638c575e96d34d000fb71e8456aa489aacdaf
Arthur Denner <arthurdenner7@gmail.com>
2021-09-24 13:40:06 +0200

    chore: remove static files from example folder
    Former-commit-id: f451cac704524f0361f015df449fb250036cfdb1

--- packages/example/src/AudioTesting/index.tsx
+++ packages/example/src/AudioTesting/index.tsx
@@ -1,10 +1,12 @@
 import {Audio, interpolate, Sequence} from 'remotion';
+import music from './music.mp3';
 
 const AudioTesting: React.FC = () => {
 	return (
 		<div>
 			<Sequence from={50} durationInFrames={100}>
 				<Audio
+					src={music}
 					volume={(f) =>
 						interpolate(f, [0, 50, 100], [0, 1, 0], {
 							extrapolateLeft: 'clamp',
@@ -12,9 +14,14 @@ const AudioTesting: React.FC = () => {
 						})
 					}
 				>
-					{/* Serve this assets with `npm run serve` in the `example` folder */}
-					<source src="http://127.0.0.1:8080/cow-moo.ogg" type="audio/ogg" />
-					<source src="http://127.0.0.1:8080/kid-laugh.mp3" type="audio/mpeg" />
+					<source
+						src="https://res.cloudinary.com/arthurdenner/video/upload/v1632482233/Remotion/kid-laugh.mp3"
+						type="audio/mpeg"
+					/>
+					<source
+						src="https://res.cloudinary.com/arthurdenner/video/upload/v1632482233/Remotion/cow-moo.ogg"
+						type="audio/ogg"
+					/>
 				</Audio>
 			</Sequence>
 		</div>
