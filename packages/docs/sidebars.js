151dcf41034c5b6aa315666f80b67a7aaf1720ef
Jonny Burger <jonathanburger11@gmail.com>
2021-10-01 19:14:24 +0200

    downloadVideo API
    Former-commit-id: b52a746f06a68ecb178b377bcc3317e8dd4a592e

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -24,6 +24,7 @@ module.exports = {
         "getfunctions",
         "deletesite",
         "deploysite",
+        "downloadvideo",
         "ensurelambdabinaries",
         "getuserpolicy",
         "getrolepolicy",
