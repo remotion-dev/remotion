ab75d89d4950e36c9f85fe38136bf204c1fcbf68
Jonny Burger <jonathanburger11@gmail.com>
2021-10-03 14:15:36 +0200

    Update help.ts

--- packages/lambda/src/cli/help.ts
+++ packages/lambda/src/cli/help.ts
@@ -21,7 +21,7 @@ export const printHelp = () => {
 	Log.info(`${BINARY_NAME} ${RENDER_COMMAND} <s3-url> <composition-id>`);
 	Log.info(CliInternals.chalk.gray('Render a video in the cloud.'));
 
-	Log.info(`${BINARY_NAME} ${STILL_COMMAND} <s3-url> <composiiton-id>`);
+	Log.info(`${BINARY_NAME} ${STILL_COMMAND} <s3-url> <composition-id>`);
 	Log.info(CliInternals.chalk.gray('Render a still image in the cloud.'));
 
 	Log.info();
