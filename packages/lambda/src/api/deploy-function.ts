ed9044ac5d21ea8763a8375d8b14d096f9b97a30
Jonny Burger <jonathanburger11@gmail.com>
2021-10-03 15:12:19 +0200

    prebuild lambda function
    Former-commit-id: 795406f93aa534d6e4b9fdb7dcd502dd89c8b040

--- packages/lambda/src/api/deploy-function.ts
+++ packages/lambda/src/api/deploy-function.ts
@@ -1,11 +1,10 @@
 import {AwsRegion} from '../pricing/aws-regions';
-import {RENDER_FN_PREFIX} from '../shared/constants';
+import {FUNCTION_ZIP, RENDER_FN_PREFIX} from '../shared/constants';
 import {getAccountId} from '../shared/get-account-id';
 import {randomHash} from '../shared/random-hash';
 import {validateAwsRegion} from '../shared/validate-aws-region';
 import {validateMemorySize} from '../shared/validate-memory-size';
 import {validateTimeout} from '../shared/validate-timeout';
-import {bundleLambda} from './bundle-lambda';
 import {createFunction} from './create-function';
 
 /**
@@ -26,14 +25,11 @@ export const deployFunction = async (options: {
 	validateAwsRegion(options.region);
 
 	const fnNameRender = RENDER_FN_PREFIX + randomHash();
-	const [renderOut, accountId] = await Promise.all([
-		bundleLambda(),
-		getAccountId({region: options.region}),
-	]);
+	const accountId = await getAccountId({region: options.region});
 
 	const created = await createFunction({
 		region: options.region,
-		zipFile: renderOut,
+		zipFile: FUNCTION_ZIP,
 		functionName: fnNameRender,
 		accountId,
 		memorySizeInMb: options.memorySizeInMb,
