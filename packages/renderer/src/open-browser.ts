7851dcd56950aebdaa491416f40d43bf5c4f0dd4
Jonny Burger <jonathanburger11@gmail.com>
2021-10-04 16:43:55 +0200

    fallback for macOS
    Former-commit-id: f207ab8cd4a89be4b579c0f986c84342cb92a5f9

--- packages/renderer/src/open-browser.ts
+++ packages/renderer/src/open-browser.ts
@@ -32,7 +32,7 @@ export const openBrowser = async (
 			'--no-sandbox',
 			'--disable-setuid-sandbox',
 			'--disable-dev-shm-usage',
-			'--use-gl=swiftshader',
+			process.platform === 'darwin' ? null : '--use-gl=egl',
 			process.platform === 'linux' ? '--single-process' : null,
 		].filter(Boolean) as string[],
 	});
