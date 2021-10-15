1f9901edd2ff1f00db7d726fbd50586900739c1b
Jonny Burger <jonathanburger11@gmail.com>
2021-10-15 11:58:22 +0200

    drag and drio
    Former-commit-id: 86704bd309ee9ddbc5ae05805f8334f054f7ebda

--- packages/cli/src/editor/components/Timeline/TimelineInOutPointerHandle.tsx
+++ packages/cli/src/editor/components/Timeline/TimelineInOutPointerHandle.tsx
@@ -1,4 +1,5 @@
-import React, {createRef} from 'react';
+import React, {createRef, useMemo} from 'react';
+import {useGetXPositionOfItemInTimeline} from '../../helpers/get-left-of-timeline-slider';
 
 const line: React.CSSProperties = {
 	height: '100%',
@@ -13,10 +14,26 @@ const line: React.CSSProperties = {
 export const inPointerHandle = createRef<HTMLDivElement>();
 export const outPointerHandle = createRef<HTMLDivElement>();
 
-export const TimelineInPointerHandle: React.FC = () => {
-	return <div ref={inPointerHandle} style={line} />;
-};
+export const TimelineInOutPointerHandle: React.FC<{
+	dragging: boolean;
+	type: 'in' | 'out';
+	atFrame: number;
+}> = ({dragging, type, atFrame}) => {
+	const {get} = useGetXPositionOfItemInTimeline();
+	const style: React.CSSProperties = useMemo(() => {
+		return {
+			...line,
+			backgroundColor: dragging
+				? 'rgba(255, 255, 255, 0.7)'
+				: 'rgba(255, 255, 255, 0.1)',
+			transform: `translateX(${get(atFrame)}px)`,
+		};
+	}, [atFrame, dragging, get]);
 
-export const TimelineOutPointerHandle: React.FC = () => {
-	return <div ref={outPointerHandle} style={line} />;
+	return (
+		<div
+			ref={type === 'in' ? inPointerHandle : outPointerHandle}
+			style={style}
+		/>
+	);
 };
