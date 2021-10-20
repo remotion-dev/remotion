597b19831a6f96ecd74448cc27323ce8e5f6cac5
Jonny Burger <jonathanburger11@gmail.com>
2021-10-20 20:41:59 +0200

    revert this

--- packages/lambda/src/shared/bundle-site.ts
+++ packages/lambda/src/shared/bundle-site.ts
@@ -1,11 +1,3 @@
-import type {bundle} from '@remotion/bundler';
+import {bundle} from '@remotion/bundler';
 
-// don't import @remotion/bundle at top so the bundler module can
-// be omitted if you are using esbuild to bundle.
-// eventually we could make a lambda-client that only exports browser-supported functions
-
-// or we export it as an ECMAscript module and allow a treeshake.
-export const bundleSite: typeof bundle = async (...args) => {
-	const bundler = require('@remotion/bundler').bundle as typeof bundle;
-	return bundler(...args);
-};
+export const bundleSite = bundle;
