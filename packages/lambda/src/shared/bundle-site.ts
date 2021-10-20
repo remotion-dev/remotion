c7fff219b8350cdc20e21b3554d3646794ed079a
Jonny Burger <jonathanburger11@gmail.com>
2021-10-20 20:19:39 +0200

    allow to exclude bundler

--- packages/lambda/src/shared/bundle-site.ts
+++ packages/lambda/src/shared/bundle-site.ts
@@ -1,3 +1,11 @@
-import {bundle} from '@remotion/bundler';
+import type {bundle} from '@remotion/bundler';
 
-export const bundleSite = bundle;
+// don't import @remotion/bundle at top so the bundler module can
+// be omitted if you are using esbuild to bundle.
+// eventually we could make a lambda-client that only exports browser-supported functions
+
+// or we export it as an ECMAscript module and allow a treeshake.
+export const bundleSite: typeof bundle = async (...args) => {
+	const bundler = require('@remotion/bundler').bundle as typeof bundle;
+	return bundler(...args);
+};
