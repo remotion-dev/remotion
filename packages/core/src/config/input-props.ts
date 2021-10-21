3a32f0e4c6d4c5186879a1e9acf1c8c2178c1897
Viren Mohindra <viren.mohindra@gmail.com>
2021-10-21 22:45:21 +0800

    return an empty object
    Former-commit-id: 764f02f5a22e81230f3de4bd9b91dc79ee4e11a0

--- packages/core/src/config/input-props.ts
+++ packages/core/src/config/input-props.ts
@@ -5,7 +5,7 @@ export const INPUT_PROPS_KEY = 'remotion.inputProps';
 export const getInputProps = () => {
 	if (getRemotionEnvironment() === 'rendering') {
 		if (typeof window === 'undefined') {
-			return;
+			return {};
 		}
 
 		const param = localStorage.getItem(INPUT_PROPS_KEY);
