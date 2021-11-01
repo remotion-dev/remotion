1f8eedf3963a4079f2f5cc4ac1ecf206613f9d4c
Jonny Burger <jonathanburger11@gmail.com>
2021-11-01 18:09:38 +0100

    fix changelog

--- packages/example/src/VideoTesting/index.tsx
+++ packages/example/src/VideoTesting/index.tsx
@@ -9,7 +9,7 @@ export const VideoTesting: React.FC<{
 	return (
 		<div>
 			<Sequence from={0} durationInFrames={durationInFrames}>
-				<Video src="https://s3.amazonaws.com/combo-compositions-development/uploads/797b04ee-74af-424f-8547-2b9000bcd5e4/AT-cm%7Cch9nFtZAH-OZeNh-cAfCZQ.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWRLLRLXPRY6O2UOD%2F20211029%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20211029T125551Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=d8c951750d237796187356938ad9ef15f1dad323dc2b2f2689bc3c71c6376c67" />
+				<Video src={codec === 'mp4' ? videoMp4 : videoWebm} />
 			</Sequence>
 		</div>
 	);
