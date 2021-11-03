23037eb0f06556d72629a329fe191028bf1c2fdf
Jonny Burger <jonathanburger11@gmail.com>
2021-11-03 14:36:19 +0100

    EGL renderer

--- packages/renderer/src/open-browser.ts
+++ packages/renderer/src/open-browser.ts
@@ -32,7 +32,7 @@ export const openBrowser = async (
 			'--no-sandbox',
 			'--disable-setuid-sandbox',
 			'--disable-dev-shm-usage',
-			'--use-gl=angle',
+			'--use-gl=egl',
 			'--disable-background-media-suspend',
 			process.platform === 'linux' ? '--single-process' : null,
 		].filter(Boolean) as string[],
