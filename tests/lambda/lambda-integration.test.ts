2b29cdb78f42fabb8baf3d3d84bbd7558b7d2798
Jonny Burger <jonathanburger11@gmail.com>
2021-11-12 11:53:00 +0100

    Update lambda-integration.test.ts

--- tests/lambda/lambda-integration.test.ts
+++ tests/lambda/lambda-integration.test.ts
@@ -11,5 +11,12 @@ test("Should be able to get user policy without authentication", async () => {
   const task = await execa("npx", ["remotion", "lambda", "policies", "user"], {
     cwd: "packages/example",
   });
-  expect(task.stdout).toContain("Available commands");
+  expect(task.stdout).toContain("iam:GetUser");
+});
+
+test("Should be able to get role policy without authentication", async () => {
+  const task = await execa("npx", ["remotion", "lambda", "policies", "role"], {
+    cwd: "packages/example",
+  });
+  expect(task.stdout).toContain("s3:ListAllMyBuckets");
 });
