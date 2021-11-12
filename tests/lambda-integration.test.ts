566267e5e0a897bb1fe611def469e95409d648b7
Jonny Burger <jonathanburger11@gmail.com>
2021-11-12 11:44:59 +0100

    Update lambda-integration.test.ts
    Former-commit-id: 392169710f85334b1b436df4c17cfeab16a76d1e

--- tests/lambda-integration.test.ts
+++ tests/lambda-integration.test.ts
@@ -6,3 +6,10 @@ test("Should be able to call npx remotion lambda", async () => {
   });
   expect(task.stdout).toContain("Available commands");
 });
+
+test("Should be able to get user policy without authentication", async () => {
+  const task = await execa("npx", ["remotion", "lambda", "policies", "user"], {
+    cwd: "packages/example",
+  });
+  expect(task.stdout).toContain("Available commands");
+});
