6695996dc5136a134752847c2e32d9cbd4019a32
Jonny Burger <jonathanburger11@gmail.com>
2021-11-12 16:42:13 +0100

    fix tests

--- tests/lambda/cli.test.ts
+++ tests/lambda/cli.test.ts
@@ -1,37 +1,62 @@
 import { cleanFnStore } from "../../packages/lambda/src/api/mock-functions";
 import { LambdaInternals } from "../../packages/lambda/src/index";
 
-const getConsoleOutput = () => {
-  // @ts-expect-error
-  return process.stdout.write.mock.calls.map((c) => c[0].toString()).join("\n");
+let stdoutOutput: string[] = [];
+let stderrOutput: string[] = [];
+
+const getProcessWriteOutput = () => {
+  return stdoutOutput.map((c) => c.toString()).join("\n");
 };
 
+const originalStdout = process.stdout.write;
+const originalStderr = process.stderr.write;
+
+beforeEach(() => {
+  stdoutOutput = [];
+  stderrOutput = [];
+  cleanFnStore();
+  // @ts-expect-error
+  process.stdout.write = (str: string) => {
+    // originalStdout(str);
+    stdoutOutput.push(str);
+  };
+
+  // @ts-expect-error
+  process.stderr.write = (str: string) => {
+    // originalStderr(str);
+    stderrOutput.push(str);
+  };
+});
+
+afterEach(() => {
+  process.stdout.write = originalStdout;
+  process.stderr.write = originalStderr;
+});
+
 test("Deploy function", async () => {
-  process.stdout.write = jest.fn();
   await LambdaInternals.executeCommand(["functions", "deploy"]);
-  expect(getConsoleOutput()).toContain("Deployed as remotion-render-abcdef\n");
+  expect(getProcessWriteOutput()).toContain(
+    "Deployed as remotion-render-abcdef\n"
+  );
 });
 
 test("Deploy function and list it", async () => {
-  cleanFnStore();
   await LambdaInternals.executeCommand(["functions", "deploy"]);
-  process.stdout.write = jest.fn();
   await LambdaInternals.executeCommand(["functions", "ls"]);
-  expect(getConsoleOutput()).toContain("Getting functions...");
-  expect(getConsoleOutput()).toContain("Memory (MB)");
-  expect(getConsoleOutput()).toMatch(
+  expect(getProcessWriteOutput()).toContain("Getting functions...");
+  expect(getProcessWriteOutput()).toContain("Memory (MB)");
+  expect(getProcessWriteOutput()).toMatch(
     /remotion-render-abcdef\s+(.*)\s+1024\s+120/g
   );
 });
 
 test("Deploy function and it already exists should fail", async () => {
-  cleanFnStore();
   await LambdaInternals.executeCommand(["functions", "deploy"]);
   await expect(() =>
     LambdaInternals.executeCommand(["functions", "deploy"])
   ).rejects.toThrow(/Exited process with code 1/);
 
-  expect(getConsoleOutput()).toMatch(
+  expect(getProcessWriteOutput()).toMatch(
     /A function with version (.*) is already deployed in region us-east-1, it is called remotion-render-abcdef/
   );
 });
