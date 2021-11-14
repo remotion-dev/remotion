837793777f6514eb83693ce9e5d2c4bdd77cc2a6
Jonny Burger <jonathanburger11@gmail.com>
2021-11-14 16:27:22 +0100

    :eyes:
    Former-commit-id: be7247c597fbfb6b603aaecaa918abf846a0bed0

--- tests/lambda/console-hooks.ts
+++ tests/lambda/console-hooks.ts
@@ -1,17 +1,25 @@
+import { Internals } from "../../packages/babel-loader/node_modules/remotion/dist";
 import { cleanFnStore } from "../../packages/lambda/src/api/mock-functions";
 
 let stdoutOutput: string[] = [];
 let stderrOutput: string[] = [];
 
 export const getProcessWriteOutput = () => {
-  return stdoutOutput.map((c) => c.toString()).join("\n");
+  return stdoutOutput
+    .filter(Internals.truthy)
+    .map((c) => c.toString())
+    .join("\n");
 };
 export const getProcessStdErrOutput = () => {
-  return stderrOutput.map((c) => c.toString()).join("\n");
+  return stderrOutput
+    .filter(Internals.truthy)
+    .map((c) => c.toString())
+    .join("\n");
 };
 
 const originalStdout = process.stdout.write;
 const originalConsoleLog = console.log;
+const originalConsoleError = console.error;
 const originalStderr = process.stderr.write;
 
 beforeEach(() => {
@@ -23,10 +31,13 @@ beforeEach(() => {
     // originalStdout(str);
     stdoutOutput.push(str);
   };
-  // @ts-expect-error
-  console.log.write = (str: string) => {
+  console.log = (str: string) => {
     // originalStdout(str);
     stdoutOutput.push(str);
+  };
+  console.error = (str: string) => {
+    // originalStdErr(str);
+    stderrOutput.push(str);
   };
 
   // @ts-expect-error
@@ -40,4 +51,5 @@ afterEach(() => {
   process.stdout.write = originalStdout;
   process.stderr.write = originalStderr;
   console.log = originalConsoleLog;
+  console.error = originalConsoleError;
 });
