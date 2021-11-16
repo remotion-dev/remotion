efaf2c8ae255fa3c3a124bdf47b6db6d2b6960eb
Jonny Burger <jonathanburger11@gmail.com>
2021-11-16 22:25:56 +0100

    simplify
    Former-commit-id: 1fdcf87629e0b8034dc1d8cd4805e11998717bc8

--- packages/cli/src/render.tsx
+++ packages/cli/src/render.tsx
@@ -152,10 +152,23 @@ export const render = async () => {
 			config,
 			imageFormat,
 			inputProps,
-			// TODO
-			onFrameUpdate: () => void 0,
-			// TODO
-			onStart: () => void 0,
+			onFrameUpdate: (renderedFrames) => {
+				updateRenderProgress({
+					encodedDoneIn: null,
+					encodedFrames: 0,
+					renderedDoneIn: null,
+					renderedFrames,
+					stitchStage: 'encoding',
+				});
+			},
+			onStart: () =>
+				updateRenderProgress({
+					encodedDoneIn: null,
+					encodedFrames: 0,
+					renderedDoneIn: null,
+					renderedFrames: 0,
+					stitchStage: 'encoding',
+				}),
 			outputDir,
 			serveUrl,
 			browser,
@@ -166,75 +179,83 @@ export const render = async () => {
 			puppeteerInstance: openedBrowser,
 			quality,
 		});
-	} else {
-		await renderMedia({
-			absoluteOutputFile,
-			browser,
-			codec,
-			config,
-			crf,
-			envVariables,
-			ffmpegExecutable,
-			frameRange,
-			imageFormat,
-			inputProps,
-			onProgress: updateRenderProgress,
-			openedBrowser,
-			outputDir,
-			overwrite,
-			parallelEncoding,
-			parallelism,
-			pixelFormat,
-			proResProfile,
-			quality,
-			serveUrl,
-			fileExtension: getUserPassedFileExtension(),
-			bundled,
-			onDownload: (src) => {
-				Log.info('Downloading asset... ', src);
-			},
-			dumpBrowserLogs: Internals.Logging.isEqualOrBelowLogLevel('verbose'),
+		const doneIn = Date.now() - startTime;
+		updateRenderProgress({
+			encodedDoneIn: doneIn,
+			encodedFrames: 0,
+			renderedDoneIn: null,
+			renderedFrames: config.durationInFrames,
+			stitchStage: 'encoding',
 		});
+		Log.info();
+		Log.info();
+		Log.info(chalk.green('\nYour image sequence is ready!'));
+		return;
 	}
+
+	await renderMedia({
+		absoluteOutputFile,
+		browser,
+		codec,
+		config,
+		crf,
+		envVariables,
+		ffmpegExecutable,
+		frameRange,
+		imageFormat,
+		inputProps,
+		onProgress: updateRenderProgress,
+		openedBrowser,
+		outputDir,
+		overwrite,
+		parallelEncoding,
+		parallelism,
+		pixelFormat,
+		proResProfile,
+		quality,
+		serveUrl,
+		fileExtension: getUserPassedFileExtension(),
+		bundled,
+		onDownload: (src) => {
+			Log.info('Downloading asset... ', src);
+		},
+		dumpBrowserLogs: Internals.Logging.isEqualOrBelowLogLevel('verbose'),
+	});
 
 	Log.info();
 	Log.info();
 	const seconds = Math.round((Date.now() - startTime) / 1000);
-	if (shouldOutputImageSequence) {
-		Log.info(chalk.green('\nYour image sequence is ready!'));
-	} else {
-		Log.info(
-			[
-				'- Total render time:',
-				seconds,
-				seconds === 1 ? 'second' : 'seconds',
-			].join(' ')
-		);
-		Log.info('-', 'Output can be found at:');
-		Log.info(chalk.cyan(`▶️ ${absoluteOutputFile}`));
-		close();
-		Log.verbose('Cleaning up...');
-		try {
-			if (process.platform === 'win32') {
-				// Properly delete directories because Windows doesn't seem to like fs.
-				await deleteDirectory(outputDir);
-				await deleteDirectory(bundled);
-			} else {
-				await Promise.all([
-					(fs.promises.rm ?? fs.promises.rmdir)(outputDir, {
-						recursive: true,
-					}),
-					(fs.promises.rm ?? fs.promises.rmdir)(bundled, {
-						recursive: true,
-					}),
-				]);
-			}
-		} catch (err) {
-			Log.warn('Could not clean up directory.');
-			Log.warn(err);
-			Log.warn('Do you have minimum required Node.js version?');
+	Log.info(
+		[
+			'- Total render time:',
+			seconds,
+			seconds === 1 ? 'second' : 'seconds',
+		].join(' ')
+	);
+	Log.info('-', 'Output can be found at:');
+	Log.info(chalk.cyan(`▶️ ${absoluteOutputFile}`));
+	close();
+	Log.verbose('Cleaning up...');
+	try {
+		if (process.platform === 'win32') {
+			// Properly delete directories because Windows doesn't seem to like fs.
+			await deleteDirectory(outputDir);
+			await deleteDirectory(bundled);
+		} else {
+			await Promise.all([
+				(fs.promises.rm ?? fs.promises.rmdir)(outputDir, {
+					recursive: true,
+				}),
+				(fs.promises.rm ?? fs.promises.rmdir)(bundled, {
+					recursive: true,
+				}),
+			]);
 		}
-
-		Log.info(chalk.green('\nYour video is ready!'));
+	} catch (err) {
+		Log.warn('Could not clean up directory.');
+		Log.warn(err);
+		Log.warn('Do you have minimum required Node.js version?');
 	}
+
+	Log.info(chalk.green('\nYour video is ready!'));
 };
