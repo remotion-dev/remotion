7582fb805c735d1a5f9541330330fc570537f2ec
Jonny Burger <jonathanburger11@gmail.com>
2021-11-19 14:33:56 +0100

    Update client.ts

--- packages/lambda/src/client.ts
+++ packages/lambda/src/client.ts
@@ -3,7 +3,13 @@ import {renderStillOnLambda} from './api/render-still-on-lambda';
 import {getRenderProgress} from './api/get-render-progress';
 import type {AwsRegion} from './pricing/aws-regions';
 import type {RenderProgress} from './shared/constants';
+import {getFunctions} from './api/get-functions';
 
-export {renderVideoOnLambda, renderStillOnLambda, getRenderProgress};
+export {
+	renderVideoOnLambda,
+	renderStillOnLambda,
+	getRenderProgress,
+	getFunctions,
+};
 
 export type {AwsRegion, RenderProgress};
