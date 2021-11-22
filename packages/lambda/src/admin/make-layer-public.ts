b09aa01e615b04facf54f9e29cbe3d177f566947
Jonny Burger <jonathanburger11@gmail.com>
2021-11-22 11:00:23 +0100

    delete obsolete regions
    Former-commit-id: 9bc1d32c750320fd19f3bb6213b6aae39c61fbf9

--- packages/lambda/src/admin/make-layer-public.ts
+++ packages/lambda/src/admin/make-layer-public.ts
@@ -14,26 +14,15 @@ const runtimes: string[] = ['nodejs14.x'];
 const layerInfo: {
 	[region in AwsRegion]: {layerArn: string; version: number}[];
 } = {
-	'af-south-1': [],
-	'ap-east-1': [],
 	'ap-northeast-1': [],
-	'ap-northeast-2': [],
-	'ap-northeast-3': [],
 	'ap-south-1': [],
 	'ap-southeast-1': [],
 	'ap-southeast-2': [],
-	'ca-central-1': [],
 	'eu-central-1': [],
-	'eu-north-1': [],
-	'eu-south-1': [],
 	'eu-west-1': [],
 	'eu-west-2': [],
-	'eu-west-3': [],
-	'me-south-1': [],
-	'sa-east-1': [],
 	'us-east-1': [],
 	'us-east-2': [],
-	'us-west-1': [],
 	'us-west-2': [],
 };
 
