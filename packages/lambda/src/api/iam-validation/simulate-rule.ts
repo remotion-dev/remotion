2a62fc61649e85aec7d1dfd1bcd6f4630a25c905
Jonny Burger <jonathanburger11@gmail.com>
2021-12-02 17:44:21 +0100

    add retries to validation

--- packages/lambda/src/api/iam-validation/simulate-rule.ts
+++ packages/lambda/src/api/iam-validation/simulate-rule.ts
@@ -18,40 +18,54 @@ export const simulateRule = async (options: {
 	actionNames: string[];
 	arn: string;
 	resource: string[];
+	retries: number;
 }): Promise<SimulationResult[]> => {
-	if (options.actionNames.includes(iam.GetUser)) {
-		try {
-			await getIamClient(options.region).send(new GetUserCommand({}));
-			const result: SimulationResult[] = [
-				{
-					decision: 'allowed',
-					name: iam.GetUser,
-				},
-			];
-			return result;
-		} catch (err) {
-			const result: SimulationResult[] = [
-				{
-					decision: 'explicitDeny',
-					name: iam.GetUser,
-				},
-			];
-			return result;
+	try {
+		if (options.actionNames.includes(iam.GetUser)) {
+			try {
+				await getIamClient(options.region).send(new GetUserCommand({}));
+				const result: SimulationResult[] = [
+					{
+						decision: 'allowed',
+						name: iam.GetUser,
+					},
+				];
+				return result;
+			} catch (err) {
+				const result: SimulationResult[] = [
+					{
+						decision: 'explicitDeny',
+						name: iam.GetUser,
+					},
+				];
+				return result;
+			}
 		}
+
+		const res = await getIamClient(options.region).send(
+			new SimulatePrincipalPolicyCommand({
+				ActionNames: options.actionNames,
+				PolicySourceArn: options.arn,
+				ResourceArns: options.resource,
+			})
+		);
+
+		return (res.EvaluationResults ?? []).map((pol): SimulationResult => {
+			return {
+				decision: pol.EvalDecision as EvalDecision,
+				name: pol.EvalActionName as string,
+			};
+		});
+	} catch (err) {
+		// Sometimes the AWS Rate limit hits. In that case we retry up to two times
+		// after waiting for 2 seconds.
+		if (options.retries <= 0) {
+			throw err;
+		}
+
+		await new Promise((resolve) => {
+			setTimeout(() => resolve, 2000);
+		});
+		return simulateRule({...options, retries: options.retries - 1});
 	}
-
-	const res = await getIamClient(options.region).send(
-		new SimulatePrincipalPolicyCommand({
-			ActionNames: options.actionNames,
-			PolicySourceArn: options.arn,
-			ResourceArns: options.resource,
-		})
-	);
-
-	return (res.EvaluationResults ?? []).map((pol): SimulationResult => {
-		return {
-			decision: pol.EvalDecision as EvalDecision,
-			name: pol.EvalActionName as string,
-		};
-	});
 };
