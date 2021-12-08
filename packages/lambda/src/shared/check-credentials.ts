b4233da1258df69309091ee3a7e92d1be665e34a
Jonny Burger <jonathanburger11@gmail.com>
2021-12-08 22:26:39 +0100

    allow REMOTION_ prefix

--- packages/lambda/src/shared/check-credentials.ts
+++ packages/lambda/src/shared/check-credentials.ts
@@ -5,11 +5,17 @@ const messageForVariable = (variable: string) =>
 
 // TODO: Could better differentiate between CLI and Node.JS acccess.
 export const checkCredentials = () => {
-	if (!process.env.AWS_ACCESS_KEY_ID) {
+	if (
+		!process.env.AWS_ACCESS_KEY_ID &&
+		!process.env.REMOTION_AWS_ACCESS_KEY_ID
+	) {
 		throw new Error(messageForVariable('AWS_ACCESS_KEY_ID'));
 	}
 
-	if (!process.env.AWS_SECRET_ACCESS_KEY) {
+	if (
+		!process.env.AWS_SECRET_ACCESS_KEY &&
+		!process.env.REMOTION_AWS_SECRET_ACCESS_KEY
+	) {
 		throw new Error(messageForVariable('AWS_SECRET_ACCESS_KEY'));
 	}
 };
