6aea5da55ec0969a07608ec58ffa7f66fb24ac41
Jonny Burger <jonathanburger11@gmail.com>
2021-12-15 12:27:08 +0100

    fix tests

--- tests/lambda/cli.test.ts
+++ tests/lambda/cli.test.ts
@@ -1,3 +1,9 @@
+import {
+  CURRENT_VERSION,
+  DEFAULT_MEMORY_SIZE,
+  DEFAULT_TIMEOUT,
+} from "@remotion/lambda/dist/defaults";
+import { CliInternals } from "../../packages/lambda/node_modules/@remotion/cli/dist";
 import { parsedLambdaCli } from "../../packages/lambda/src/cli/args";
 import { LambdaInternals } from "../../packages/lambda/src/index";
 import { getProcessStdErrOutput, getProcessWriteOutput } from "./console-hooks";
@@ -5,7 +11,7 @@ import { getProcessStdErrOutput, getProcessWriteOutput } from "./console-hooks";
 test("Deploy function", async () => {
   await LambdaInternals.executeCommand(["functions", "deploy"]);
   expect(getProcessWriteOutput()).toContain(
-    "Deployed as remotion-render-abcdef\n"
+    `Deployed as remotion-render-${CURRENT_VERSION}-${DEFAULT_MEMORY_SIZE}mb-${DEFAULT_TIMEOUT}sec\n`
   );
 });
 
@@ -14,24 +20,18 @@ test("Deploy function and list it", async () => {
   await LambdaInternals.executeCommand(["functions", "ls"]);
   expect(getProcessWriteOutput()).toContain("Getting functions...");
   expect(getProcessWriteOutput()).toContain("Memory (MB)");
-  expect(getProcessWriteOutput()).toMatch(
-    /remotion-render-abcdef\s+(.*)\s+1024\s+120/g
-  );
+  expect(getProcessWriteOutput()).toMatch(/remotion-render-(.*)\s+2048\s+120/g);
 });
 
 test("Deploy function and it already exists should fail", async () => {
   await LambdaInternals.executeCommand(["functions", "deploy"]);
-  await expect(() =>
-    LambdaInternals.executeCommand(["functions", "deploy"])
-  ).rejects.toThrow(/Exited process with code 1/);
+  await LambdaInternals.executeCommand(["functions", "deploy"]);
 
-  expect(getProcessStdErrOutput()).toMatch(
-    /Error: Already found a function \(remotion-render-abcdef\) with version (.*) deployed in region us-east-1/
-  );
+  expect(getProcessWriteOutput()).toMatch(/Already exists as remotion-render/);
 });
 
 test('If no functions are there and is quiet, should return "()"', async () => {
-  parsedLambdaCli.q = true;
+  CliInternals.parsedCli.q = true;
   await LambdaInternals.executeCommand(["functions", "ls"]);
   expect(getProcessWriteOutput()).toBe("()");
 });
