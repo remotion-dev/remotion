6d62992597b63da4314722f5ba222dd19700d610
Pablo Cuadrado <pablocuadrado@gmail.com>
2021-12-21 20:11:09 -0300

    Remove artifacts by restoring these files.
    Former-commit-id: a5a8c44e8eabd2d5861ebbc6cc2026676dc478a2

--- packages/cli/jest.config.d.ts
+++
@@ -1,4 +0,0 @@
-export const preset: string;
-export const testEnvironment: string;
-export const testPathIgnorePatterns: string[];
-//# sourceMappingURL=jest.config.d.ts.map
--- packages/cli/remotion-cli.d.ts
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env node
-export {};
-//# sourceMappingURL=remotion-cli.d.ts.map
