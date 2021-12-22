d7ecdf946f0d303281bd7d69c505ac9955eea733
Pablo Cuadrado <pablocuadrado@gmail.com>
2021-12-22 06:43:14 -0300

    Remove unnecessary delays.

--- packages/example/src/OfflineAudioBuffer/index.tsx
+++ packages/example/src/OfflineAudioBuffer/index.tsx
@@ -1,15 +1,12 @@
-import {useCurrentFrame, Sequence} from 'remotion';
+import {Sequence} from 'remotion';
 
 import {OfflineAudioBufferExample} from './OfflineAudioBufferExample';
 
 const OfflineAudioBuffer: React.FC = () => {
-	const frame = useCurrentFrame();
-	const transitionStart = 5;
-
 	return (
 		<div style={{background: '#444456', width: '100%', height: '100%'}}>
-			<Sequence from={1} durationInFrames={149}>
-				{frame > transitionStart && <OfflineAudioBufferExample />}
+			<Sequence from={0} durationInFrames={150}>
+				<OfflineAudioBufferExample />
 			</Sequence>
 		</div>
 	);
