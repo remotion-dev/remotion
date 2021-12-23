9ae15023fa7d981cdc265675954e33db966362c7
Jonny Burger <jonathanburger11@gmail.com>
2021-12-23 12:09:45 +0100

    credit original library
    Former-commit-id: 8f4b90eb6f35433da5653f295978f916a6ef4638

--- packages/media-utils/src/audio-buffer/audio-buffer-to-wav.ts
+++ packages/media-utils/src/audio-buffer/audio-buffer-to-wav.ts
@@ -1,3 +1,7 @@
+/**
+ * Inlined from https://github.com/Jam3/audiobuffer-to-wav/commit/2272eb09bd46a05e50a6d684d908aa6f13c58f63#diff-e727e4bdf3657fd1d798edcd6b099d6e092f8573cba266154583a746bba0f346
+ */
+
 export function audioBufferToWav(
 	buffer: AudioBuffer,
 	opt: {
