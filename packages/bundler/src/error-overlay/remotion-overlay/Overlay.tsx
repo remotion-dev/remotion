9b2b22e4aa9b9ee3e5f22c803b57193d6207e931
Jonny Burger <jonathanburger11@gmail.com>
2021-12-28 21:18:57 +0100

    finish

--- packages/bundler/src/error-overlay/remotion-overlay/Overlay.tsx
+++ packages/bundler/src/error-overlay/remotion-overlay/Overlay.tsx
@@ -1,12 +1,6 @@
-import React, {
-	createRef,
-	useCallback,
-	useImperativeHandle,
-	useState,
-} from 'react';
+import React, {createRef, useImperativeHandle, useState} from 'react';
 import {AbsoluteFill} from 'remotion';
 import {ErrorRecord} from '../react-overlay/listen-to-runtime-errors';
-import {Button} from './Button';
 import {ErrorDisplay} from './ErrorDisplay';
 
 type SetErrors = {
@@ -28,10 +22,6 @@ type State =
 	  };
 
 const BACKGROUND_COLOR = '#1f2428';
-const ICON_SIZE = 40;
-const rotate: React.CSSProperties = {
-	transform: `rotate(90deg)`,
-};
 export const Overlay: React.FC = () => {
 	const [errors, setErrors] = useState<State>({type: 'clear'});
 
@@ -52,26 +42,8 @@ export const Overlay: React.FC = () => {
 			<AbsoluteFill
 				style={{
 					backgroundColor: BACKGROUND_COLOR,
-					overflow: 'auto',
-					justifyContent: 'center',
-					alignItems: 'center',
 				}}
-			>
-				<svg
-					width={ICON_SIZE}
-					height={ICON_SIZE}
-					viewBox="-100 -100 400 400"
-					style={rotate}
-				>
-					<path
-						fill="#555"
-						stroke="#555"
-						strokeWidth="100"
-						strokeLinejoin="round"
-						d="M 2 172 a 196 100 0 0 0 195 5 A 196 240 0 0 0 100 2.259 A 196 240 0 0 0 2 172 z"
-					/>
-				</svg>{' '}
-			</AbsoluteFill>
+			/>
 		);
 	}
 
