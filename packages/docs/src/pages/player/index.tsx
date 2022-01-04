61e965f4969ae325147b0246229661338a62d3c3
Jonny Burger <jonathanburger11@gmail.com>
2022-01-04 16:59:55 +0100

    preview card

--- packages/docs/src/pages/player/index.tsx
+++ packages/docs/src/pages/player/index.tsx
@@ -4,6 +4,7 @@ import { LandingHeader } from "../../../components/Player/LandingHeader";
 import { PlayerFeatures } from "../../../components/Player/features";
 import { PoweredByRemotion } from "../../../components/Player/PoweredByRemotion";
 import { PlayerPageFooter } from "../../../components/Player/Footer";
+import Head from "@docusaurus/Head";
 
 const container: React.CSSProperties = {
   maxWidth: 1000,
@@ -18,6 +19,12 @@ export default () => {
       title="@remotion/player"
       description="Embed videos that are written in React, and change them at runtime. Connect it to server-side rendering to turn them into real MP4 videos."
     >
+      <Head>
+        <meta name="og:image" content="/img/player-og.png" />
+        <meta name="twitter:image" content="/img/player-og.png" />
+        <meta property="og:image" content="/img/player-og.png" />
+        <meta property="twitter:image" content="/img/player-og.png" />
+      </Head>
       <div style={container}>
         <LandingHeader />
       </div>
