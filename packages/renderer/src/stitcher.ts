8d4db1cc3c9da5841763da78ec2e45bb72a48695
Jonny Burger <jonathanburger11@gmail.com>
2022-01-04 14:54:52 +0100

    Update stitcher.ts
    Former-commit-id: c7cd386eeca1ce078c0f2e10686e846792c0934f

--- packages/renderer/src/stitcher.ts
+++ packages/renderer/src/stitcher.ts
@@ -13,7 +13,6 @@ import {calculateAssetPositions} from './assets/calculate-asset-positions';
 import {convertAssetsToFileUrls} from './assets/convert-assets-to-file-urls';
 import {markAllAssetsAsDownloaded} from './assets/download-and-map-assets-to-file';
 import {getAssetAudioDetails} from './assets/get-asset-audio-details';
-import {uncompressMediaAsset} from './assets/types';
 import {calculateFfmpegFilters} from './calculate-ffmpeg-filters';
 import {createFfmpegComplexFilter} from './create-ffmpeg-complex-filter';
 import {getAudioCodecName} from './get-audio-codec-name';
@@ -22,7 +21,6 @@ import {getFrameInfo} from './get-frame-number-length';
 import {getProResProfileName} from './get-prores-profile-name';
 import {DEFAULT_IMAGE_FORMAT} from './image-format';
 import {parseFfmpegProgress} from './parse-ffmpeg-progress';
-import {resolveAssetSrc} from './resolve-asset-src';
 import {validateEvenDimensionsWithCodec} from './validate-even-dimensions-with-codec';
 import {validateFfmpeg} from './validate-ffmpeg';
 
