9a6583bc9c03f7c8c969fbf33b479f4dcfc2da8c
Jonny Burger <jonathanburger11@gmail.com>
2022-01-10 09:33:35 +0100

    don't read config inside bundler
    Former-commit-id: 5f6d85caf57876914e4ae612164be13b39f9bfd8

--- packages/cli/src/setup-cache.ts
+++ packages/cli/src/setup-cache.ts
@@ -1,5 +1,6 @@
 import {bundle, BundlerInternals} from '@remotion/bundler';
 import {Internals} from 'remotion';
+import {defaultOverrideFunction} from 'remotion/src/config/override-webpack';
 import {Log} from './log';
 import {quietFlagProvided} from './parse-command-line';
 import {
@@ -31,6 +32,8 @@ export const bundleOnCli = async (fullPath: string, steps: RenderStep[]) => {
 		},
 		{
 			enableCaching: shouldCache,
+			webpackOverride:
+				Internals.getWebpackOverrideFn() ?? defaultOverrideFunction,
 		}
 	);
 	bundlingProgress.update(
