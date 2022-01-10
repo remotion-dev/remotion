2cd0058e4ac3f3e4cc5d6d257e1dfa2114b77f66
Jonny Burger <jonathanburger11@gmail.com>
2022-01-10 10:23:52 +0100

    delete colors

--- packages/lambda/src/cli/helpers/confirm.ts
+++ packages/lambda/src/cli/helpers/confirm.ts
@@ -1,6 +1,6 @@
-import prompt from 'prompt';
 import {forceFlagProvided} from '../args';
 import {quit} from './quit';
+import {yesOrNo} from './yes-or-no';
 
 export const confirmCli = async ({
 	delMessage,
@@ -9,27 +9,13 @@ export const confirmCli = async ({
 	delMessage: string;
 	allowForceFlag: boolean;
 }) => {
-	prompt.message = '';
-	prompt.start();
-
 	if (allowForceFlag && forceFlagProvided) {
 		return;
 	}
 
-	let didAnswer = false;
-	while (!didAnswer) {
-		const result = await prompt.get([delMessage]);
+	const result = await yesOrNo({question: delMessage, defaultValue: true});
 
-		if ((result[delMessage] as string).toLowerCase() === 'y') {
-			didAnswer = true;
-		}
-
-		if ((result[delMessage] as string).trim() === '') {
-			didAnswer = true;
-		}
-
-		if ((result[delMessage] as string).trim() === 'n') {
-			quit(1);
-		}
+	if (result === false) {
+		quit(1);
 	}
 };
