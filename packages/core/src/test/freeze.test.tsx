b0c62ecb6ff1f5b05d8ee092897f988d67072eda
Jonny Burger <jonathanburger11@gmail.com>
2022-01-23 11:12:04 +0100

    Fix freeze component

--- packages/core/src/test/freeze.test.tsx
+++ packages/core/src/test/freeze.test.tsx
@@ -18,3 +18,69 @@ describe('Throw with invalid duration props', () => {
 		);
 	});
 });
+
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React from 'react';
+import {Sequence} from '../index';
+import {
+	TimelineContext,
+	TimelineContextValue,
+} from '../timeline-position-state';
+import {useCurrentFrame} from '../use-frame';
+
+const timelineCtxValue = (frame: number): TimelineContextValue => ({
+	rootId: '',
+	frame,
+	playing: false,
+	imperativePlaying: {
+		current: false,
+	},
+	playbackRate: 1,
+	setPlaybackRate: () => {
+		throw new Error('playback rate');
+	},
+	audioAndVideoTags: {current: []},
+});
+
+const renderForFrame = (frame: number, markup: React.ReactNode) => {
+	return render(
+		<TimelineContext.Provider value={timelineCtxValue(frame)}>
+			{markup}
+		</TimelineContext.Provider>
+	);
+};
+
+const Basic: React.FC = () => {
+	return (
+		<Freeze frame={300}>
+			<TestComponent />
+		</Freeze>
+	);
+};
+
+const WithSequence: React.FC = () => {
+	const SequenceFrom = 200;
+	const FreezeFrame = 100;
+	return (
+		<Sequence from={SequenceFrom} layout="none">
+			<Freeze frame={FreezeFrame}>
+				<TestComponent />
+			</Freeze>
+		</Sequence>
+	);
+};
+
+const TestComponent: React.FC = () => {
+	const frame = useCurrentFrame();
+	return <div>{frame}</div>;
+};
+
+test('Basic test', () => {
+	const {container} = renderForFrame(0, <Basic />);
+	expect(container.innerHTML).toBe('<div>300</div>');
+});
+
+test('Should ignore a Sequence', () => {
+	const {container} = renderForFrame(300, <WithSequence />);
+	expect(container.innerHTML).toBe('<div>100</div>');
+});
