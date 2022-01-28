9c1403f6c97c39c0655c738134efa0b9213a023c
Jonny Burger <jonathanburger11@gmail.com>
2022-01-28 15:17:49 +0100

    update with 2.6.6 and got removal
    Former-commit-id: c47ac136b3ef08e6e0d792442fa870a6f977c141

--- packages/lambda/src/functions/helpers/read-with-progress.ts
+++ packages/lambda/src/functions/helpers/read-with-progress.ts
@@ -1,7 +1,6 @@
 import {GetObjectCommand} from '@aws-sdk/client-s3';
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
 import {RenderInternals} from '@remotion/renderer';
-import {Readable} from 'stream';
 import {AwsRegion} from '../../pricing/aws-regions';
 import {getS3Client} from '../../shared/aws-clients';
 
@@ -11,19 +10,21 @@ export type LambdaReadFileProgress = (progress: {
 	progress: number;
 }) => unknown;
 
-export const lambdaReadFileWithProgress = async ({
+export const lambdaDownloadFileWithProgress = async ({
 	bucketName,
 	key,
 	region,
 	expectedBucketOwner,
+	outputPath,
 	onProgress,
 }: {
 	bucketName: string;
 	key: string;
 	region: AwsRegion;
 	expectedBucketOwner: string;
+	outputPath: string;
 	onProgress: LambdaReadFileProgress;
-}): Promise<Readable> => {
+}): Promise<{sizeInBytes: number}> => {
 	const client = getS3Client(region);
 	const command = new GetObjectCommand({
 		Bucket: bucketName,
@@ -33,14 +34,5 @@ export const lambdaReadFileWithProgress = async ({
 
 	const presigned = await getSignedUrl(client, command);
 
-	const stream = RenderInternals.gotStream(presigned);
-	stream.on('downloadProgress', ({percent, transferred, total}) => {
-		onProgress({
-			downloaded: transferred,
-			progress: percent,
-			totalSize: total,
-		});
-	});
-
-	return stream;
+	return RenderInternals.downloadFile(presigned, outputPath, onProgress);
 };
--- packages/renderer/src/assets/download-file.ts
+++ packages/renderer/src/assets/download-file.ts
@@ -1,22 +1,41 @@
-import https from 'https';
 import {createWriteStream} from 'fs';
+import {readFile} from './read-file';
 
-export const downloadFile = (url: string, to: string) => {
-	return new Promise<void>((resolve, reject) => {
-		const writeStream = createWriteStream(to);
+export const downloadFile = (
+	url: string,
+	to: string,
+	onProgress:
+		| ((progress: {
+				progress: number;
+				downloaded: number;
+				totalSize: number;
+		  }) => void)
+		| undefined
+) => {
+	return new Promise<{sizeInBytes: number}>((resolve, reject) => {
+		readFile(url)
+			.then((res) => {
+				const totalSize = Number(res.headers['content-length']);
+				const writeStream = createWriteStream(to);
 
-		// Listen to 'close' event instead of more
-		// concise method to avoid this problem
-		// https://github.com/remotion-dev/remotion/issues/384#issuecomment-844398183
-		writeStream.on('close', () => resolve());
-		writeStream.on('error', (err) => reject(err));
-
-		https
-			.get(url, (res) => {
+				// Listen to 'close' event instead of more
+				// concise method to avoid this problem
+				// https://github.com/remotion-dev/remotion/issues/384#issuecomment-844398183
+				writeStream.on('close', () => resolve({sizeInBytes: totalSize}));
+				writeStream.on('error', (err) => reject(err));
+				let downloaded = 0;
 				res.pipe(writeStream).on('error', (err) => reject(err));
+				res.on('data', (d) => {
+					downloaded += d.length;
+					onProgress?.({
+						downloaded,
+						progress: downloaded / totalSize,
+						totalSize,
+					});
+				});
 			})
-			.on('error', (err) => {
-				return reject(err);
+			.catch((err) => {
+				reject(err);
 			});
 	});
 };
