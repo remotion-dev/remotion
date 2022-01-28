e3d9e99c640df33dc9737460b3ea25354ed25154
SkyezerFox <actuallyori@gmail.com>
2022-01-28 10:41:27 +0000

    recursively discover lockfiles
    Former-commit-id: d044907b74cc548d9d84ca17bc628669bedc2e5b

--- packages/bundler/src/get-package-manager.ts
+++ packages/bundler/src/get-package-manager.ts
@@ -18,27 +18,35 @@ export const lockFilePaths: LockfilePath[] = [
 	},
 ];
 
-export const getPackageManager = (): PackageManager | 'unknown' => {
+const discoverPkgManager = (currentPath: string): PackageManager | "unknown" => {
+	if (currentPath === "/") {
+		return "unknown";
+	}
+
 	const existingPkgManagers = lockFilePaths.filter((p) =>
 		fs.existsSync(path.join(process.cwd(), p.path))
 	);
 
-	if (existingPkgManagers.length === 0) {
-		return 'unknown';
-	}
-
 	if (existingPkgManagers.length > 1) {
 		const error = [
-			`Found multiple lockfiles:`,
-			...existingPkgManagers.map((m) => {
-				return `- ${m.path}`;
-			}),
-			'',
-			'This can lead to bugs, delete all but one of these files and run this command again.',
-		].join('\n');
-
+				`Found multiple lockfiles:`,
+				...existingPkgManagers.map((m) => {
+					return `- ${m.path}`;
+				}),
+				'',
+				'This can lead to bugs, delete all but one of these files and run this command again.',
+			].join('\n');
+	
 		throw new Error(error);
 	}
 
-	return existingPkgManagers[0].manager;
+	if (existingPkgManagers.length === 1) {
+		return existingPkgManagers[0].manager;
+	}
+
+	return discoverPkgManager(path.dirname(currentPath));
+}
+
+export const getPackageManager = (): PackageManager | 'unknown' => {
+	return discoverPkgManager(process.cwd());
 };
