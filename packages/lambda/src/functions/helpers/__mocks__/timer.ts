b0128103687e64c606d6fd442dc4838ff1e9b468
Jonny Burger <jonathanburger11@gmail.com>
2022-02-03 17:11:51 +0100

    cleanups
    Former-commit-id: be6668d0ca68c1cfea6f193d0413bbff31574d21

--- packages/lambda/src/functions/helpers/__mocks__/timer.ts
+++ packages/lambda/src/functions/helpers/__mocks__/timer.ts
@@ -1,6 +1,6 @@
 import {timer as original} from '../../../functions/helpers/timer';
 
-// Turn off timers in debug. rather slow
+// Turn off timers while in testing. very noisy.
 export const timer: typeof original = () => {
 	return {
 		end: () => undefined,
