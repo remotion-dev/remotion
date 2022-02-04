91bf2679396fb207dfdfc695d2d5aab694e3a2e6
Jonny Burger <jonathanburger11@gmail.com>
2022-02-04 15:38:01 +0100

    fix
    Former-commit-id: e145d0e3bcda5f50504a32b95505486fabff7b4a

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -285,6 +285,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-02-07'
 	| '2022-02-06'
 	| '2022-02-05'
 	| '2022-02-04'
@@ -331,7 +332,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-02-06';
+export const CURRENT_VERSION: LambdaVersions = '2022-02-07';
 
 export type PostRenderData = {
 	cost: {
