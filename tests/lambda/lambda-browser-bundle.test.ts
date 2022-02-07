1ba62c48dbbf2208cab404c19a310a0b7eba32d9
Jonny Burger <jonathanburger11@gmail.com>
2022-02-07 14:43:39 +0100

    remove need for Internals.isInLambda()

--- tests/lambda/lambda-browser-bundle.test.ts
+++ tests/lambda/lambda-browser-bundle.test.ts
@@ -1,8 +1,7 @@
 import path from "path";
-import esbuild = require("esbuild");
 import { tmpdir } from "os";
 import fs from "fs";
-
+import esbuild = require("esbuild");
 test("Should not be able to bundle @remotion/lambda directly", async () => {
   expect(() =>
     esbuild.build({
@@ -15,16 +14,38 @@ test("Should not be able to bundle @remotion/lambda directly", async () => {
   ).rejects.toThrow(/Could not resolve "pnpapi"/);
 });
 
-test("Should be able to bundle @remotion/lambda/client with ESBuild", async () => {
+describe("Should be able to bundle @remotion/lambda/client with ESBuild", () => {
   const outfile = path.join(tmpdir(), "esbuild-test.js");
-  const { errors } = await esbuild.build({
-    platform: "node",
-    target: "node14",
-    bundle: true,
-    logLevel: "silent",
-    outfile,
-    entryPoints: [require.resolve("@remotion/lambda/client")],
+
+  test("Should build without errors", async () => {
+    const { errors } = await esbuild.build({
+      platform: "node",
+      target: "node14",
+      bundle: true,
+      logLevel: "silent",
+      outfile,
+      entryPoints: [require.resolve("@remotion/lambda/client")],
+    });
+    expect(errors.length).toBe(0);
   });
-  expect(errors.length).toBe(0);
-  fs.unlinkSync(outfile);
+
+  test("Bundle should be below 6MB", async () => {
+    const file = await fs.promises.readFile(outfile, "utf-8");
+    expect(file.length).toBeGreaterThan(10000);
+    expect(file.length).toBeLessThanOrEqual(6000000);
+  });
+
+  test("Bundle should not include core Remotion", async () => {
+    const file = await fs.promises.readFile(outfile, "utf-8");
+    expect(file).not.toContain("AbsoluteFill");
+  });
+
+  test("Bundle should not include Renderer", async () => {
+    const file = await fs.promises.readFile(outfile, "utf-8");
+    expect(file).not.toContain("@remotion/renderer");
+  });
+
+  test("Should be able to delete it", () => {
+    fs.unlinkSync(outfile);
+  });
 });
