abd9fe76c9c01d5c0238bd1e5ae25460843653d9
Jonny Burger <jonathanburger11@gmail.com>
2022-02-09 20:27:39 +0100

    update to turborepo
    Former-commit-id: 66c6728f266cca525bd0c7baa7dabe29e35c387c

--- packages/it-tests/src/lambda/lambda-integration.test.ts
+++ packages/it-tests/src/lambda/lambda-integration.test.ts
@@ -1,22 +1,23 @@
 import execa from "execa";
+import path from "path";
 
 test("Should be able to call npx remotion lambda", async () => {
   const task = await execa("npx", ["remotion", "lambda"], {
-    cwd: "packages/example",
+    cwd: path.join(process.cwd(), "..", "example"),
   });
   expect(task.stdout).toContain("Available commands");
 });
 
 test("Should be able to get user policy without authentication", async () => {
   const task = await execa("npx", ["remotion", "lambda", "policies", "user"], {
-    cwd: "packages/example",
+    cwd: path.join(process.cwd(), "..", "example"),
   });
   expect(task.stdout).toContain("iam:GetUser");
 });
 
 test("Should be able to get role policy without authentication", async () => {
   const task = await execa("npx", ["remotion", "lambda", "policies", "role"], {
-    cwd: "packages/example",
+    cwd: path.join(process.cwd(), "..", "example"),
   });
   expect(task.stdout).toContain("s3:ListAllMyBuckets");
 });
