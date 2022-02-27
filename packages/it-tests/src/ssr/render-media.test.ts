263e6a0423c59f3411ee172bd5a5348e4146211f
Jonny Burger <jonathanburger11@gmail.com>
2022-02-27 21:02:01 +0100

    allow redirects
    Former-commit-id: b7f24c6678a7aacdb9eb5482f7d61a42aa773e88

--- packages/it-tests/src/ssr/render-media.test.ts
+++ packages/it-tests/src/ssr/render-media.test.ts
@@ -6,7 +6,7 @@ import { existsSync } from "fs";
 test("Render video with browser instance open", async () => {
   const puppeteerInstance = await openBrowser("chrome");
   const compositions = await getCompositions(
-    "https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/testbed/index.html",
+    "https://quizzical-jackson-ad3285.netlify.app/",
     {
       puppeteerInstance,
     }
@@ -25,8 +25,7 @@ test("Render video with browser instance open", async () => {
   await renderMedia({
     outputLocation: outPath,
     codec: "h264",
-    serveUrl:
-      "https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/testbed/index.html",
+    serveUrl: "https://quizzical-jackson-ad3285.netlify.app/",
     composition: reactSvg,
     frameRange: [0, 2],
     puppeteerInstance,
@@ -37,7 +36,7 @@ test("Render video with browser instance open", async () => {
 
 test("Render video with browser instance not open", async () => {
   const compositions = await getCompositions(
-    "https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/testbed/index.html"
+    "https://quizzical-jackson-ad3285.netlify.app/"
   );
 
   const reactSvg = compositions.find((c) => c.id === "react-svg");
@@ -53,8 +52,7 @@ test("Render video with browser instance not open", async () => {
   await renderMedia({
     outputLocation: outPath,
     codec: "h264",
-    serveUrl:
-      "https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/testbed/index.html",
+    serveUrl: "https://quizzical-jackson-ad3285.netlify.app/",
     composition: reactSvg,
     frameRange: [0, 2],
   });
@@ -71,8 +69,7 @@ test("should fail on invalid CRF", async () => {
     return renderMedia({
       outputLocation: outPath,
       codec: "h264",
-      serveUrl:
-        "https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/testbed/index.html",
+      serveUrl: "https://quizzical-jackson-ad3285.netlify.app/",
       // @ts-expect-error
       crf: "wrong",
       config: {
