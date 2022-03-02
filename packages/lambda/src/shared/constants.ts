70a7d3fec770754acbc5920721f75e19cda9056c
Jonny Burger <jonathanburger11@gmail.com>
2022-03-02 11:53:35 +0100

    bump

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -307,6 +307,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-03-02'
 	| '2022-03-01'
 	| '2022-02-27'
 	| '2022-02-14'
@@ -360,7 +361,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-03-01';
+export const CURRENT_VERSION: LambdaVersions = '2022-03-02';
 
 export type PostRenderData = {
 	cost: {
