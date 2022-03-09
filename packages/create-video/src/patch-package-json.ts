795f1b17d90559702f4b19e75cd12f1f498b31fe
Jonny Burger <jonathanburger11@gmail.com>
2022-03-09 14:50:04 +0100

    patch package json
    Former-commit-id: fe6f32af830f989d3bdd265739a4accd2091688a

--- packages/create-video/src/patch-package-json.ts
+++ packages/create-video/src/patch-package-json.ts
@@ -1,13 +1,19 @@
 import path from 'path';
 import fs from 'fs';
 
-export const patchPackageJson = (projectRoot: string, projectName) => {
+export const patchPackageJson = (projectRoot: string, projectName: string) => {
 	const fileName = path.join(projectRoot, 'package.json');
 
 	const contents = fs.readFileSync(fileName, 'utf-8');
 	const packageJson = JSON.parse(contents);
-	const newPackageJson = JSON.stringify({
-		...packageJson,
-		name: projectName,
-	});
+	const newPackageJson = JSON.stringify(
+		{
+			...packageJson,
+			name: projectName,
+		},
+		undefined,
+		2
+	);
+
+	fs.writeFileSync(fileName, newPackageJson);
 };
