82e528a9c592ddf1b8899d6c82e293b82bb23fde
Antoine Caron <antoine.caron@bedrockstreaming.com>
2022-03-15 15:50:54 +0100

    docs: add jsdoc on some core API methods

--- packages/core/src/spring/index.ts
+++ packages/core/src/spring/index.ts
@@ -1,5 +1,18 @@
 import {springCalculation, SpringConfig} from './spring-utils';
 
+/**
+ * Delightful and smooth animation primitive. Calculates a position based on physical parameters, start and end value, and time
+ * @link https://www.remotion.dev/docs/spring
+ * @param {number} frame The current time value. Most of the time you want to pass in the return value of useCurrentFrame.
+ * @param {number} fps For how many frames per second the spring animation should be calculated.
+ * @param {?Object} config optional object that allows you to customize the physical properties of the animation.
+ * @param {number} [config.mass=1] The weight of the spring. If you reduce the mass, the animation becomes faster!
+ * @param {number} [config.damping=10] How hard the animation decelerates.
+ * @param {number} [config.stiffness=100] How hard the animation decelerates.
+ * @param {boolean} [config.overshootClamping=false] How hard the animation decelerates.
+ * @param {?number} from The initial value of the animation
+ * @param {?number} to The end value of the animation. Note that depending on the parameters, spring animations may overshoot the target a bit, before they bounce back to their final target.
+ */
 export function spring({
 	frame,
 	fps,
