ceb29af2e932ca88ff13126906aa5ef8acfa73af
Jonny Burger <jonathanburger11@gmail.com>
2022-03-16 10:57:38 +0100

    adjust JSDoc

--- packages/core/src/interpolate.ts
+++ packages/core/src/interpolate.ts
@@ -100,15 +100,14 @@ function checkInfiniteRange(name: string, arr: readonly number[]) {
 }
 
 /**
- * Compute value based on an input number based on an interpolation function between input range and output range
- * Some options could be defined in order to handle extrapolations and easings.
+ * Map a value from an input range to an output range.
  * @link https://www.remotion.dev/docs/interpolate
- * @param {!number} input entry value used in interpolation function (example: currentFrame index)
+ * @param {!number} input value to interpolate
  * @param {!number[]} inputRange range of values that you expect the input to assume.
  * @param {!number[]} outputRange range of output values that you want the input to map to.
  * @param {?object} options
  * @param {?Function} options.easing easing function which allows you to customize the input, for example to apply a certain easing function. By default, the input is left unmodified, resulting in a pure linear interpolation {@link https://www.remotion.dev/docs/easing}
- * @param {string=} [options.extrapolateLeft="extend"] What should happen if the input value is outside left the input range {@link https://www.remotion.dev/docs/interpolate#extrapolateleft}
+ * @param {string=} [options.extrapolateLeft="extend"] What should happen if the input value is outside left the input range, default: "extend" {@link https://www.remotion.dev/docs/interpolate#extrapolateleft}
  * @param {string=} [options.extrapolateRight="extend"] Same as extrapolateLeft, except for values outside right the input range {@link https://www.remotion.dev/docs/interpolate#extrapolateright}
  */
 export function interpolate(
