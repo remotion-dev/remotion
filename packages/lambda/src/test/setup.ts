b9f9e4f2bade813d0e8992cd0ff2c1fc43bb9a24
Jonny Burger <jonathanburger11@gmail.com>
2022-03-29 21:53:42 +0200

    get current architecture

--- packages/lambda/src/test/setup.ts
+++ packages/lambda/src/test/setup.ts
@@ -16,6 +16,9 @@ jest.mock('../functions/helpers/print-cloudwatch-helper', () =>
 jest.mock('../functions/helpers/get-current-region', () =>
 	jest.requireActual('../functions/helpers/__mocks__/get-current-region')
 );
+jest.mock('../functions/helpers/get-current-architecture', () =>
+	jest.requireActual('../functions/helpers/__mocks__/get-current-architecture')
+);
 jest.mock('../functions/helpers/get-browser-instance', () =>
 	jest.requireActual('../functions/helpers/__mocks__/get-browser-instance')
 );
