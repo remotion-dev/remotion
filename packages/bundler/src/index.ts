b2506ac5ed473b119df1c2bd71b526a0bf134e24
Jonny Burger <jonathanburger11@gmail.com>
2022-04-05 21:25:36 +0200

    remove overrideWebpackConfig

--- packages/bundler/src/index.ts
+++ packages/bundler/src/index.ts
@@ -1,6 +1,5 @@
 import {getLatestRemotionVersion} from './get-latest-remotion-version';
 import {getPackageManager, lockFilePaths} from './get-package-manager';
-import {overrideWebpackConfig} from './override-webpack';
 import {startServer} from './start-server';
 import {cacheExists, clearCache} from './webpack-cache';
 import esbuild = require('esbuild');
@@ -18,5 +17,4 @@ export const BundlerInternals = {
 export type {ProjectInfo} from './project-info';
 
 export {bundle} from './bundler';
-export {overrideWebpackConfig};
 export {PackageManager} from './get-package-manager';
