e7b66ea2c29af16b4199333a73385a5d8cd426aa
Jonny Burger <jonathanburger11@gmail.com>
2022-04-05 11:34:54 +0200

    revert

--- packages/example/src/StaticServer/index.tsx
+++ packages/example/src/StaticServer/index.tsx
@@ -4,7 +4,6 @@ import {Audio, continueRender, delayRender, Img, staticFile} from 'remotion';
 export const StaticDemo: React.FC = () => {
 	const [handle1] = useState(() => delayRender());
 	const [handle2] = useState(() => delayRender());
-	const [handle3] = useState(() => delayRender('rendering of audio'));
 	return (
 		<>
 			<Img
@@ -14,10 +13,6 @@ export const StaticDemo: React.FC = () => {
 			<Img
 				src={staticFile('/nested/mp4.png')}
 				onLoad={() => continueRender(handle2)}
-			/>
-			<Audio
-				src={staticFile('/framer.mp4')}
-				onLoad={() => continueRender(handle3)}
 			/>
 		</>
 	);
