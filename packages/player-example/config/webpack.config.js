e677448b122b7e1cf91ee53e42aae1c653be5e34
Jonny Burger <jonathanburger11@gmail.com>
2022-04-13 13:41:38 +0200

    remove even more stuff
    Former-commit-id: 74d52c0aab5fa4de535adc520881d1268f2b237a

--- packages/player-example/config/webpack.config.js
+++ packages/player-example/config/webpack.config.js
@@ -462,39 +462,6 @@ module.exports = function (webpackEnv) {
 					filename: 'static/css/[name].[contenthash:8].css',
 					chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
 				}),
-			// Generate an asset manifest file with the following content:
-			// - "files" key: Mapping of all asset filenames to their corresponding
-			//   output file so that tools can pick it up without having to parse
-			//   `index.html`
-			// - "entrypoints" key: Array of files which are included in `index.html`,
-			//   can be used to reconstruct the HTML if necessary
-			new WebpackManifestPlugin({
-				fileName: 'asset-manifest.json',
-				publicPath: paths.publicUrlOrPath,
-				generate: (seed, files, entrypoints) => {
-					const manifestFiles = files.reduce((manifest, file) => {
-						manifest[file.name] = file.path;
-						return manifest;
-					}, seed);
-					const entrypointFiles = entrypoints.main.filter(
-						(fileName) => !fileName.endsWith('.map')
-					);
-
-					return {
-						files: manifestFiles,
-						entrypoints: entrypointFiles,
-					};
-				},
-			}),
-			// Moment.js is an extremely popular library that bundles large locale files
-			// by default due to how webpack interprets its code. This is a practical
-			// solution that requires the user to opt into importing specific locales.
-			// https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-			// You can remove this if you don't use Moment.js:
-			new webpack.IgnorePlugin({
-				resourceRegExp: /^\.\/locale$/,
-				contextRegExp: /moment$/,
-			}),
 			// TypeScript type checking
 			useTypeScript &&
 				new ForkTsCheckerWebpackPlugin({
