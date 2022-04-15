52468b89af82ae374731a48f4a0766e273f6a782
Jonny Burger <jonathanburger11@gmail.com>
2022-04-15 15:31:43 +0200

    stackblitz compatibility
    Former-commit-id: ab82b3df7760444be35dbffcd44a3f96c8f840e3

--- packages/create-video/cli.js
+++ packages/create-video/cli.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const paths = [path.join(__dirname, 'dist', 'init.js')];
 
-const prefix = '#! /usr/bin/env node\n';
+const prefix = '#!/usr/bin/env node\n';
 
 paths.forEach((p) => {
 	const contents = fs.readFileSync(p, 'utf8');
