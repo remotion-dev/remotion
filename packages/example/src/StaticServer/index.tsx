eeb2346274037709aa2635d7a86516b9d656f71e
Jonny Burger <jonathanburger11@gmail.com>
2022-04-17 22:52:37 +0200

    Update index.tsx
    Former-commit-id: 22fa670e5b75c1b75b73b47598337e7ba7d5ebd1

--- packages/example/src/StaticServer/index.tsx
+++ packages/example/src/StaticServer/index.tsx
@@ -5,15 +5,15 @@ export const StaticDemo: React.FC = () => {
 	const [handle1] = useState(() => delayRender('handle1'));
 	const [handle2] = useState(() => delayRender('handle2'));
 
-	useEffect(() => {
-		throw new Error('oops');
-	}, []);
-
 	return (
 		<>
 			<Img
 				src={staticFile('logo.png')}
 				onLoad={() => continueRender(handle1)}
+			/>
+			<Img
+				src={staticFile('/nested/mp4.png')}
+				onLoad={() => continueRender(handle2)}
 			/>
 		</>
 	);
