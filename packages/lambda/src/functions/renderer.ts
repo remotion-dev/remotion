59163112b68ee72f06560515f2735c1d9ba3290d
Jonny Burger <jonathanburger11@gmail.com>
2022-04-19 09:47:58 +0200

    correct jest detection
    Former-commit-id: a50b63b2ee1406edae802eb9fe4d8018f0384340

--- packages/lambda/src/functions/renderer.ts
+++ packages/lambda/src/functions/renderer.ts
@@ -129,7 +129,8 @@ const renderHandler = async (
 		outputLocation,
 		codec: params.codec,
 		crf: params.crf ?? undefined,
-		ffmpegExecutable: process.env.TEST ? null : '/opt/bin/ffmpeg',
+		ffmpegExecutable:
+			process.env.NODE_ENV === 'test' ? null : '/opt/bin/ffmpeg',
 		pixelFormat: params.pixelFormat,
 		proResProfile: params.proResProfile,
 		onDownload: (src: string) => {
