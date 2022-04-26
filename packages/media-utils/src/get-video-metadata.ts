7dacc14d0d3459928e40b1916afbf3b83a7de36f
Jonny Burger <jonathanburger11@gmail.com>
2022-04-26 16:54:08 +0200

    limit concurrency of getVideoMetadata to 3

--- packages/media-utils/src/get-video-metadata.ts
+++ packages/media-utils/src/get-video-metadata.ts
@@ -1,11 +1,14 @@
 import {isRemoteAsset} from './is-remote-asset';
+import {pLimit} from './p-limit';
 import {VideoMetadata} from './types';
 
 const cache: {[key: string]: VideoMetadata} = {};
 
-export const getVideoMetadata = async (src: string): Promise<VideoMetadata> => {
+const limit = pLimit(3);
+
+const fn = (src: string): Promise<VideoMetadata> => {
 	if (cache[src]) {
-		return cache[src];
+		return Promise.resolve(cache[src]);
 	}
 
 	const video = document.createElement('video');
@@ -33,9 +36,14 @@ export const getVideoMetadata = async (src: string): Promise<VideoMetadata> => {
 		const cleanup = () => {
 			video.removeEventListener('loadedmetadata', onLoadedMetadata);
 			video.removeEventListener('error', onError);
+			video.remove();
 		};
 
 		video.addEventListener('loadedmetadata', onLoadedMetadata, {once: true});
 		video.addEventListener('error', onError, {once: true});
 	});
+};
+
+export const getVideoMetadata = (src: string): Promise<VideoMetadata> => {
+	return limit(fn, src);
 };
