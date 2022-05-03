bae4def1250466d93851408db255412f61b877ae
Jonny Burger <jonathanburger11@gmail.com>
2022-05-03 16:57:14 +0200

    calculate duration of video from frame range consistently

--- packages/lambda/src/functions/chunk-optimization/is-valid-profile.ts
+++ packages/lambda/src/functions/chunk-optimization/is-valid-profile.ts
@@ -1,8 +1,12 @@
+import {RenderInternals} from '@remotion/renderer';
 import {TimingProfile} from './types';
 
 export const isValidOptimizationProfile = (profile: TimingProfile) => {
 	return profile.every((timing) => {
-		const frames = timing.frameRange[1] - timing.frameRange[0] + 1;
+		const frames = RenderInternals.getDurationFromFrameRange(
+			timing.frameRange,
+			0
+		);
 		const values = Object.values(timing.timings);
 		return frames === values.length && values.every((v) => v > 0);
 	});
