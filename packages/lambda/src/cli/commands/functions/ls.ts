1a3be3a8776381a5cf59a6b24b60f98f2b7e0a99
Jonny Burger <jonathanburger11@gmail.com>
2022-05-17 14:55:20 +0200

    increase NAME_COLS for function name

--- packages/lambda/src/cli/commands/functions/ls.ts
+++ packages/lambda/src/cli/commands/functions/ls.ts
@@ -3,7 +3,7 @@ import {Log} from '@remotion/cli/dist/log';
 import {getFunctions} from '../../../api/get-functions';
 import {getAwsRegion} from '../../get-aws-region';
 
-const NAME_COLS = 50;
+const NAME_COLS = 70;
 const MEMORY_COLS = 15;
 const DISK_COLS = 15;
 const TIMEOUT_COLS = 15;
