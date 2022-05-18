2927dff9b38bd2e96b32e28d606c157afda6bf4c
Jonny Burger <jonathanburger11@gmail.com>
2022-05-18 12:27:30 +0200

    bump
    Former-commit-id: 64cb756734b43bfdfae52544d40255c31a4a51c3

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -302,6 +302,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-05-18'
 	| '2022-05-16'
 	| '2022-05-11'
 	| '2022-05-07'
@@ -370,7 +371,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-05-16';
+export const CURRENT_VERSION: LambdaVersions = '2022-05-18';
 
 export type PostRenderData = {
 	cost: {
