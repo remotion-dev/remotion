808449e3cb0d6643c3abc5a4e8b9381a35d0f035
Jonny Burger <jonathanburger11@gmail.com>
2022-05-19 19:47:06 +0200

    update version

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -302,8 +302,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
-	| '2100-05-22'
-	| '2100-05-21'
+	| '2022-05-19'
 	| '2022-05-16'
 	| '2022-05-11'
 	| '2022-05-07'
@@ -372,7 +371,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2100-05-22';
+export const CURRENT_VERSION: LambdaVersions = '2022-05-19';
 
 export type PostRenderData = {
 	cost: {
