b0422577615ef8cd94fd6bbd14cbd5497e70055d
Jonny Burger <jonathanburger11@gmail.com>
2022-05-24 15:19:26 +0200

    add -faststart flag

--- packages/renderer/src/stitch-frames-to-video.ts
+++ packages/renderer/src/stitch-frames-to-video.ts
@@ -261,6 +261,7 @@ export const spawnFfmpeg = async (
 					// transparent WebM generation doesn't work
 					pixelFormat === 'yuva420p' ? ['-auto-alt-ref', '0'] : null,
 					['-b:v', '1M'],
+					codec === 'h264' ? ['-movflags', 'faststart'] : null,
 			  ]),
 		audioCodecName ? ['-c:a', audioCodecName] : null,
 		// Ignore metadata that may come from remote media
