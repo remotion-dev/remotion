9162aee00a4ae5fa99ad610aa264f45bbb4bc2df
Jonny Burger <jonathanburger11@gmail.com>
2022-05-26 21:13:50 +0200

    Update constants.ts
    Former-commit-id: 5888bc369ebc9edf6ecb60a7214f01ca4844b3a0

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -302,6 +302,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-05-30'
 	| '2022-05-27'
 	| '2022-05-26'
 	| '2022-05-19'
@@ -373,7 +374,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-05-27';
+export const CURRENT_VERSION: LambdaVersions = '2022-05-30';
 
 export type PostRenderData = {
 	cost: {
