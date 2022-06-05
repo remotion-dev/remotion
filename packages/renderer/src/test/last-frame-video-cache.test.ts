74268d3f1bd53f919fdef44575596b9005e112ab
Jonny Burger <jonathanburger11@gmail.com>
2022-06-05 16:00:26 +0200

    Update last-frame-video-cache.test.ts

--- packages/renderer/src/test/last-frame-video-cache.test.ts
+++ packages/renderer/src/test/last-frame-video-cache.test.ts
@@ -2,27 +2,48 @@ import crypto from 'crypto';
 import {
 	clearLastFileCache,
 	getLastFrameFromCache,
+	LastFrameOptions,
 	setLastFrameInCache,
 } from '../last-frame-from-video-cache';
 
+const makeKey = (id: string): LastFrameOptions => {
+	return {
+		ffmpegExecutable: null,
+		offset: 10,
+		src: id,
+	};
+};
+
 test('Last frame video cache', () => {
-	expect(getLastFrameFromCache('1')).toBe(null);
+	expect(getLastFrameFromCache(makeKey('1'))).toBe(null);
 
 	const buf = crypto.randomBytes(10 * 1024 * 1024);
-	setLastFrameInCache('1', buf);
-	expect(getLastFrameFromCache('1')?.byteLength).toBe(10 * 1024 * 1024);
-	setLastFrameInCache('2', buf);
-	setLastFrameInCache('3', buf);
-	setLastFrameInCache('4', buf);
-	setLastFrameInCache('5', buf);
-	setLastFrameInCache('6', buf);
+	setLastFrameInCache(makeKey('1'), buf);
+	expect(getLastFrameFromCache(makeKey('1'))?.byteLength).toBe(
+		10 * 1024 * 1024
+	);
+	setLastFrameInCache(makeKey('2'), buf);
+	setLastFrameInCache(makeKey('3'), buf);
+	setLastFrameInCache(makeKey('4'), buf);
+	setLastFrameInCache(makeKey('5'), buf);
+	setLastFrameInCache(makeKey('6'), buf);
 
-	expect(getLastFrameFromCache('1')?.byteLength ?? 0).toBe(0);
-	expect(getLastFrameFromCache('2')?.byteLength).toBe(10 * 1024 * 1024);
-	expect(getLastFrameFromCache('3')?.byteLength).toBe(10 * 1024 * 1024);
-	expect(getLastFrameFromCache('4')?.byteLength).toBe(10 * 1024 * 1024);
-	expect(getLastFrameFromCache('5')?.byteLength).toBe(10 * 1024 * 1024);
-	expect(getLastFrameFromCache('6')?.byteLength).toBe(10 * 1024 * 1024);
+	expect(getLastFrameFromCache(makeKey('1'))?.byteLength ?? 0).toBe(0);
+	expect(getLastFrameFromCache(makeKey('2'))?.byteLength).toBe(
+		10 * 1024 * 1024
+	);
+	expect(getLastFrameFromCache(makeKey('3'))?.byteLength).toBe(
+		10 * 1024 * 1024
+	);
+	expect(getLastFrameFromCache(makeKey('4'))?.byteLength).toBe(
+		10 * 1024 * 1024
+	);
+	expect(getLastFrameFromCache(makeKey('5'))?.byteLength).toBe(
+		10 * 1024 * 1024
+	);
+	expect(getLastFrameFromCache(makeKey('6'))?.byteLength).toBe(
+		10 * 1024 * 1024
+	);
 
 	clearLastFileCache();
 });
