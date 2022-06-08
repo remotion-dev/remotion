2c4f9d986cd5b40cc339bf52b8a2a3cf9707733e
Jonny Burger <jonathanburger11@gmail.com>
2022-06-08 17:36:20 +0200

    gif test
    Former-commit-id: 799a3dcfe5bfea33c6049b45236c4c8b7ce37982

---
+++ packages/lambda/src/test/integration/renders/gif.test.ts
@@ -0,0 +1,82 @@
+import {RenderInternals} from '@remotion/renderer';
+import execa from 'execa';
+import {createWriteStream} from 'fs';
+import {LambdaRoutines} from '../../../defaults';
+import {handler} from '../../../functions';
+import {lambdaReadFile} from '../../../functions/helpers/io';
+import {LambdaReturnValues} from '../../../shared/return-values';
+
+jest.setTimeout(30000);
+
+const extraContext = {
+	invokedFunctionArn: 'arn:fake',
+	getRemainingTimeInMillis: () => 12000,
+};
+
+type Await<T> = T extends PromiseLike<infer U> ? U : T;
+
+beforeAll(() => {
+	//	disableLogs();
+});
+
+afterAll(async () => {
+	//	enableLogs();
+
+	await RenderInternals.killAllBrowsers();
+});
+
+test('Should make a distributed GIF', async () => {
+	process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE = '2048';
+
+	const res = await handler(
+		{
+			type: LambdaRoutines.start,
+			serveUrl:
+				'https://6297949544e290044cecb257--cute-kitsune-214ea5.netlify.app/',
+			chromiumOptions: {},
+			codec: 'gif',
+			composition: 'framer',
+			crf: 9,
+			envVariables: {},
+			frameRange: [0, 12],
+			framesPerLambda: 8,
+			imageFormat: 'png',
+			inputProps: {},
+			logLevel: 'warn',
+			maxRetries: 3,
+			outName: 'out.mp4',
+			pixelFormat: 'yuv420p',
+			privacy: 'public',
+			proResProfile: undefined,
+			quality: undefined,
+			scale: 1,
+			timeoutInMilliseconds: 12000,
+			loop: null,
+			everyNthFrame: 1,
+		},
+		extraContext
+	);
+	const startRes = res as Await<LambdaReturnValues[LambdaRoutines.start]>;
+
+	const progress = (await handler(
+		{
+			type: LambdaRoutines.status,
+			bucketName: startRes.bucketName,
+			renderId: startRes.renderId,
+		},
+		extraContext
+	)) as Await<LambdaReturnValues[LambdaRoutines.status]>;
+
+	const file = await lambdaReadFile({
+		bucketName: progress.outBucket as string,
+		key: progress.outKey as string,
+		expectedBucketOwner: 'abc',
+		region: 'eu-central-1',
+	});
+	await new Promise<void>((resolve) => {
+		file.pipe(createWriteStream('gif.gif')).on('close', () => resolve());
+	});
+	//	console.log({file: await streamToString(file)});
+	const probe = await execa('ffprobe', ['gif.gif']);
+	expect(probe.stderr).toMatch(/Video: gif, bgra, 1080x1080/);
+});
