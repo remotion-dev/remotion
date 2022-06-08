b5274ea73de4d0e9757b2fce6dd5aeac0d46ad84
Jonny Burger <jonathanburger11@gmail.com>
2022-06-08 14:01:24 +0200

    example for TS aliases

--- packages/example/remotion.config.ts
+++ packages/example/remotion.config.ts
@@ -1,5 +1,21 @@
+import path from 'path';
 import {Config} from 'remotion';
 import {webpackOverride} from './webpack-override';
 
 Config.Output.setOverwriteOutput(true);
 Config.Bundling.overrideWebpackConfig(webpackOverride);
+Config.Bundling.overrideWebpackConfig((config) => {
+	const newConfig = webpackOverride(config);
+	return {
+		...newConfig,
+		resolve: {
+			...newConfig.resolve,
+			alias: {
+				...newConfig.resolve.alias,
+				'lib/alias': require.resolve(
+					path.join(process.cwd(), 'src', 'lib', 'alias.ts')
+				),
+			},
+		},
+	};
+});
