c6f581ce0e0bc9bdbbea571294af52c161f0b127
Jonny Burger <jonathanburger11@gmail.com>
2022-06-08 14:21:39 +0200

    add docs

--- packages/example/remotion.config.ts
+++ packages/example/remotion.config.ts
@@ -1,21 +1,5 @@
-import path from 'path';
 import {Config} from 'remotion';
 import {webpackOverride} from './webpack-override';
 
 Config.Output.setOverwriteOutput(true);
 Config.Bundling.overrideWebpackConfig(webpackOverride);
-Config.Bundling.overrideWebpackConfig((config) => {
-	const newConfig = webpackOverride(config);
-	return {
-		...newConfig,
-		resolve: {
-			...newConfig.resolve,
-			alias: {
-				...newConfig.resolve.alias,
-				'lib/alias': require.resolve(
-					path.join(process.cwd(), 'src', 'lib', 'alias.ts')
-				),
-			},
-		},
-	};
-});
