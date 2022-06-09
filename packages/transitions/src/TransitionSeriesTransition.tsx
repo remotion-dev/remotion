478e41c57eefce876b53470f1cc5cb646c115760
Jonny Burger <jonathanburger11@gmail.com>
2022-06-09 17:53:33 +0200

    transitions
    Former-commit-id: 6160f3ddad977e3f3748f0c0c6a0e0de1051502d

--- packages/transitions/src/TransitionSeriesTransition.tsx
+++ packages/transitions/src/TransitionSeriesTransition.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import {SpringConfig} from 'remotion';
 
-export const TransitionSeriesTransition: React.FC = () => {
-	return <></>;
+export type TransitionTiming = {
+	type: 'spring';
+	config: Partial<SpringConfig>;
+};
+
+export type TransitionSeriesTransitionProps = {
+	timing: TransitionTiming;
+};
+
+export const TransitionSeriesTransition: React.FC<
+	TransitionSeriesTransitionProps
+> = () => {
+	return null;
 };
