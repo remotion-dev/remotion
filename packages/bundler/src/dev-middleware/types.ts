d730ea911b61ee00a04ca409b38fb690fb77671f
Jonny Burger <jonathanburger11@gmail.com>
2022-06-10 15:11:12 +0200

    simplify further

--- packages/bundler/src/dev-middleware/types.ts
+++ packages/bundler/src/dev-middleware/types.ts
@@ -1,12 +1,11 @@
 import type memfs from 'memfs';
-import webpack, {Watching} from 'webpack';
+import webpack from 'webpack';
 
 export type DevMiddlewareContext = {
 	state: boolean;
 	stats: webpack.Stats | undefined;
 	callbacks: ((stats: webpack.Stats) => undefined | Promise<void>)[];
 	compiler: webpack.Compiler;
-	watching: Watching | undefined;
 	logger: ReturnType<webpack.Compiler['getInfrastructureLogger']>;
 	outputFileSystem: memfs.IFs | undefined;
 };
