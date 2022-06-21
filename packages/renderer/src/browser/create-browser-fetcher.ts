f8db59196b441bcaf256afbb133080c81cac22cb
Jonny Burger <jonathanburger11@gmail.com>
2022-06-21 21:15:41 +0200

    remove proxy code

--- packages/renderer/src/browser/create-browser-fetcher.ts
+++ packages/renderer/src/browser/create-browser-fetcher.ts
@@ -88,25 +88,6 @@ export async function downloadBrowser(): Promise<void> {
 			return;
 		}
 
-		// Override current environment proxy settings with npm configuration, if any.
-		const NPM_HTTPS_PROXY =
-			process.env.npm_config_https_proxy || process.env.npm_config_proxy;
-		const NPM_HTTP_PROXY =
-			process.env.npm_config_http_proxy || process.env.npm_config_proxy;
-		const NPM_NO_PROXY = process.env.npm_config_no_proxy;
-
-		if (NPM_HTTPS_PROXY) {
-			process.env.HTTPS_PROXY = NPM_HTTPS_PROXY;
-		}
-
-		if (NPM_HTTP_PROXY) {
-			process.env.HTTP_PROXY = NPM_HTTP_PROXY;
-		}
-
-		if (NPM_NO_PROXY) {
-			process.env.NO_PROXY = NPM_NO_PROXY;
-		}
-
 		function onSuccess(localRevisions: string[]): void {
 			logPolitely(
 				`${supportedProducts[product]} (${revisionInfo.revision}) downloaded to ${revisionInfo.folderPath}`
