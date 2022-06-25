20f8165a74cc05e0f3bd680e478e669c93ed6d76
Jonny Burger <jonathanburger11@gmail.com>
2022-06-25 14:41:01 +0200

    bump lambda version to 2022-06-25
    Former-commit-id: 46f5ff7f040d7304939c5337f82b519e13ad1344

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -303,6 +303,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-06-25'
 	| '2022-06-22'
 	| '2022-06-21'
 	| '2022-06-14'
@@ -381,7 +382,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-06-22';
+export const CURRENT_VERSION: LambdaVersions = '2022-06-25';
 
 export type PostRenderData = {
 	cost: {
