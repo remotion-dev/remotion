10988c034a5549ccf5c1a342f1bcd84a327c2962
Jonny Burger <jonathanburger11@gmail.com>
2022-07-04 16:48:54 +0200

    make lambda types more strict
    Former-commit-id: 2bce6ed2df3f961076cb2a7f7632e609809a3377

--- packages/lambda/src/api/render-media-on-lambda.ts
+++ packages/lambda/src/api/render-media-on-lambda.ts
@@ -4,16 +4,17 @@ import type {
 	ImageFormat,
 	LogLevel,
 	PixelFormat,
-	ProResProfile} from 'remotion';
-import {
-	Internals
+	ProResProfile,
 } from 'remotion';
+import {Internals} from 'remotion';
 import type {AwsRegion} from '../pricing/aws-regions';
 import {callLambda} from '../shared/call-lambda';
-import type { Privacy} from '../shared/constants';
+import type {Privacy} from '../shared/constants';
 import {LambdaRoutines} from '../shared/constants';
 import {convertToServeUrl} from '../shared/convert-to-serve-url';
 import {validateFramesPerLambda} from '../shared/validate-frames-per-lambda';
+import type {LambdaCodec} from '../shared/validate-lambda-codec';
+import {validateLambdaCodec} from '../shared/validate-lambda-codec';
 import {validateServeUrl} from '../shared/validate-serveurl';
 
 export type RenderMediaOnLambdaInput = {
@@ -22,7 +23,7 @@ export type RenderMediaOnLambdaInput = {
 	serveUrl: string;
 	composition: string;
 	inputProps: unknown;
-	codec: 'h264-mkv' | 'mp3' | 'aac' | 'wav';
+	codec: LambdaCodec;
 	imageFormat: ImageFormat;
 	crf?: number | undefined;
 	envVariables?: Record<string, string>;
@@ -87,6 +88,7 @@ export const renderMediaOnLambda = async ({
 	chromiumOptions,
 	scale,
 }: RenderMediaOnLambdaInput): Promise<RenderMediaOnLambdaOutput> => {
+	const actualCodec = validateLambdaCodec(codec);
 	validateServeUrl(serveUrl);
 	validateFramesPerLambda(framesPerLambda ?? null);
 	const realServeUrl = await convertToServeUrl(serveUrl, region);
@@ -98,7 +100,7 @@ export const renderMediaOnLambda = async ({
 			composition,
 			serveUrl: realServeUrl,
 			inputProps,
-			codec,
+			codec: actualCodec,
 			imageFormat,
 			crf,
 			envVariables,
