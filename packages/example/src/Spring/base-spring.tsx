d16c459ee33983afeb7b3e51bc8d85842a921981
Jonny Burger <jonathanburger11@gmail.com>
2022-07-05 14:48:28 +0200

    some polish
    Former-commit-id: a475790e55c3ef9d9e9d3421e865001285ea7f25

--- packages/example/src/Spring/base-spring.tsx
+++ packages/example/src/Spring/base-spring.tsx
@@ -21,6 +21,33 @@ export const BaseSpring: React.FC = () => {
 						frame,
 						fps,
 						config: {},
+					})})`,
+				}}
+			/>
+		</AbsoluteFill>
+	);
+};
+
+export const SpringWithDuration: React.FC = () => {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+	return (
+		<AbsoluteFill
+			style={{
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<div
+				style={{
+					height: 200,
+					width: 200,
+					backgroundColor: 'red',
+					borderRadius: 100,
+					transform: `scale(${spring({
+						frame,
+						fps,
+						config: {},
 						durationInFrames: 90,
 					})})`,
 				}}
