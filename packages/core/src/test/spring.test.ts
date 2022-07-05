8f9c629fe794511b615bc2e8ecde93d72ca31e75
Jonny Burger <jonathanburger11@gmail.com>
2022-07-05 14:29:36 +0200

    fix test
    Former-commit-id: 346a1b25b9dc5227a5874507e6997ff16f6ddf47

--- packages/core/src/test/spring.test.ts
+++ packages/core/src/test/spring.test.ts
@@ -1,56 +1,53 @@
 import {isApproximatelyTheSame} from '../is-approximately-the-same';
 import {spring} from '../spring';
 
-describe('Basic spring should work', () => {
-	test('Basic spring to equal 0', () => {
-		expect(
-			spring({
-				fps: 30,
-				frame: 0,
-			})
-		).toEqual(0);
-	});
-
-	test('Basic spring to equal 1', () => {
-		expect(
-			spring({
-				fps: 30,
-				frame: 0,
-				from: 1,
-				to: 0,
-			})
-		).toEqual(1);
-	});
+test('Basic spring to equal 0', () => {
+	expect(
+		spring({
+			fps: 30,
+			frame: 0,
+		})
+	).toEqual(0);
 });
 
-describe('Spring should go to 1', () => {
-	test('Should be approxmiately the same', () => {
-		expect(
-			isApproximatelyTheSame(
-				spring({
-					fps: 30,
-					frame: 1,
-				}),
-				0.04941510804510185
-			)
-		).toBe(true);
-	});
-	test('Should be close to 1', () => {
-		expect(
-			spring({
-				fps: 30,
-				frame: 100,
-			})
-		).toBeCloseTo(1);
-	});
+test('Basic spring to equal 1', () => {
+	expect(
+		spring({
+			fps: 30,
+			frame: 0,
+			from: 1,
+			to: 0,
+		})
+	).toEqual(1);
 });
 
-describe('Should be able to set duration for spring', () => {
+test('Should be approxmiately the same', () => {
+	expect(
+		isApproximatelyTheSame(
+			spring({
+				fps: 30,
+				frame: 1,
+			}),
+			0.04941510804510185
+		)
+	).toBe(true);
+});
+
+test('Should be close to 1', () => {
+	expect(
+		spring({
+			fps: 30,
+			frame: 100,
+		})
+	).toBeCloseTo(1);
+});
+
+test('Should be able to set duration for spring', () => {
 	expect(
 		spring({
 			fps: 30,
 			frame: 5,
 			durationInFrames: 5,
 		})
-	).toBe(1);
+	).toBeCloseTo(1);
 });
