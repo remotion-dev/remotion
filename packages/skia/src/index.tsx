cea1b853405af521ff4443799caf33a4be1455ea
Jonny Burger <jonathanburger11@gmail.com>
2022-07-08 20:25:12 +0200

    remove assetmanager

--- packages/skia/src/index.tsx
+++ packages/skia/src/index.tsx
@@ -1,8 +1,2 @@
-export {
-	AssetManagerContext,
-	TAssetManagerContext,
-	useImages,
-	useTypefaces,
-} from './asset-manager';
 export {enableSkia} from './enable-skia';
 export {SkiaCanvas} from './SkiaCanvas';
