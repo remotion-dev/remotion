926b410346e943efee290d627b82a24843dfc623
Jonny Burger <jonathanburger11@gmail.com>
2022-07-08 23:10:55 +0200

    skia on homepage

--- packages/create-video/src/templates.ts
+++ packages/create-video/src/templates.ts
@@ -119,4 +119,18 @@ export const FEATURED_TEMPLATES: Template[] = [
 		},
 		cliId: 'audiogram',
 	},
+	{
+		homePageLabel: 'Skia',
+		shortName: 'Skia',
+		org: 'remotion-dev',
+		repoName: 'template-skia',
+		description: 'React Native Skia starter',
+		longerDescription: 'A template with React Native Skia already setup.',
+		promoVideo: {
+			muxId: 'A3ei3bzeT00Z1JCXmykOXD9v01V02HWZkc4jmmDaBiW01rE',
+			height: 1080,
+			width: 1920,
+		},
+		cliId: 'skia',
+	},
 ];
