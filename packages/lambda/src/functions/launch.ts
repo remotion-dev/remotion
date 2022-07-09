81751dc81c39c6db38be37292a434056f2375667
Jonny Burger <jonathanburger11@gmail.com>
2022-07-09 20:06:33 +0200

    avoid bundling @remotion/renderer to be in @remotion/lambda/client
    Former-commit-id: 53ad0837735dd77fe03fd39c0c66ca91f28f4ccf

--- packages/lambda/src/functions/launch.ts
+++ packages/lambda/src/functions/launch.ts
@@ -100,6 +100,11 @@ const innerLaunchHandler = async (params: LambdaPayload, options: Options) => {
 	Internals.validateDimension(comp.height, 'height', 'passed to <Component />');
 	Internals.validateDimension(comp.width, 'width', 'passed to <Component />');
 
+	RenderInternals.validateConcurrency(
+		params.concurrencyPerLambda,
+		'concurrencyPerLambda'
+	);
+
 	const realFrameRange = RenderInternals.getRealFrameRange(
 		comp.durationInFrames,
 		params.frameRange
