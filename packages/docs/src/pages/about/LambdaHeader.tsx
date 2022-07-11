cd67ad4b575fa149ce222a37dc9bc1f483f58b1e
MehmetAdemi <mehmet@remotion.dev>
2022-07-11 16:47:59 +0200

    about page 4th draft

--- packages/docs/src/pages/about/LambdaHeader.tsx
+++ packages/docs/src/pages/about/LambdaHeader.tsx
@@ -1,38 +1,27 @@
 import React from "react";
-import { PlayerPreview } from "./PlayerPreview";
 import styles from "./lambdaheader.module.css";
-import { BlueButton } from "./Button";
 import { LambdaLogo } from "./LambdaLogo";
 
 export const LambdaHeader: React.FC = () => {
   return (
-    <div>
-      <div className={styles.writeincss}>
-        <div style={{ flex: 1 }}>
-          <LambdaLogo />
-          <h1 className={styles.writeincsstitle}>
-            Remotion. <br /> In Zurich <br />
-          </h1>
-          <p>Remotion is from developers for developers.</p>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-            }}
-          ></div>
-        </div>
-        <div className={styles.spacer} />
-        <div>
-          <div className={styles.spacer} />
-          <div className={styles.writeright} />
-          <PlayerPreview />
-          <br />
-          <div className={styles.spacer} />
-          <PlayerPreview />
-        </div>
+    <div className={styles.writeincss}>
+      <div style={{ flex: 1 }}>
+        <LambdaLogo />
+        <h1 className={styles.writeincsstitle}>
+          Remotion. The programmatic video dream.
+        </h1>
+        <p>
+          Remotion is from developers for developers. While the company was
+          founded in 2022, the idea of programmatic videos in React was born a
+          year before. Remotion combines qualities of programming and essential
+          features of traditional video editing programs. Our software is
+          source-available, meaning everyone can look at our code and
+          contribute. This allowed us to create a community of hundreds of
+          developers to fulfill the programmatic video dream. Remotion is free
+          for individuals and small teams, bigger companies will have to acquire
+          a company license to use Remotion.
+        </p>
       </div>
-      <div className={styles.spacer} />
-      <PlayerPreview />
     </div>
   );
 };
