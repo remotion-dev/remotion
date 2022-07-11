44367dc4e870672fd2ccd1cee605a45f0c2e6435
Jonny Burger <jonathanburger11@gmail.com>
2022-07-11 20:16:13 +0200

    Update get-help-link.ts

--- packages/cli/src/preview-server/error-overlay/remotion-overlay/get-help-link.ts
+++ packages/cli/src/preview-server/error-overlay/remotion-overlay/get-help-link.ts
@@ -4,6 +4,17 @@ export type THelpLink = {
 };
 
 export const getHelpLink = (message: string): THelpLink | null => {
+	if (
+		message.includes(
+			'See https://www.remotion.dev/docs/the-fundamentals#defining-compositions'
+		)
+	) {
+		return {
+			title: 'Defining compositions',
+			url: 'See https://www.remotion.dev/docs/the-fundamentals#defining-compositions',
+		};
+	}
+
 	if (message.includes('https://remotion.dev/docs/wrong-composition-mount')) {
 		return {
 			title: 'Wrongly mounted <Composition>',
