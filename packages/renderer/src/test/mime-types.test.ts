f67856399892f518f64f8ef1d06a0c1343958dc9
Jonny Burger <jonathanburger11@gmail.com>
2022-07-14 13:59:21 +0200

    Update mime-types.test.ts

--- packages/renderer/src/test/mime-types.test.ts
+++ packages/renderer/src/test/mime-types.test.ts
@@ -3,5 +3,4 @@ import {mimeLookup} from '../mime-types';
 test('Should get mime types', () => {
 	expect(mimeLookup('hi.png')).toBe('image/png');
 	expect(mimeLookup('hi.svg')).toBe('image/svg+xml');
-	expect(mimeLookup('svg')).toBe('image/svg+xml');
 });
