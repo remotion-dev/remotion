e0ff74997a8b9e881109be389febe4cc27bb81c5
Jonny Burger <jonathanburger11@gmail.com>
2022-07-20 09:37:32 +0200

    Update constants.ts

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -316,6 +316,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-07-20'
 	| '2022-07-18'
 	| '2022-07-15'
 	| '2022-07-14'
@@ -405,7 +406,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-07-18';
+export const CURRENT_VERSION: LambdaVersions = '2022-07-20';
 
 export type PostRenderData = {
 	cost: {
