9bc221d0c4f44a7020fef9a93da6b64bef7fb080
himmelio <safari.sepehr@gmail.com>
2022-07-22 03:09:55 +0430

    refactor: add setOutputLocation to config
    Former-commit-id: 1f0b0cfaa296b7242a4396d4a57d90519b423b0b

--- packages/example/remotion.config.ts
+++ packages/example/remotion.config.ts
@@ -1,5 +1,6 @@
 import {Config} from 'remotion';
 import {webpackOverride} from './src/webpack-override';
 
+Config.Output.setOutputLocation('out/video.mp4');
 Config.Output.setOverwriteOutput(true);
 Config.Bundling.overrideWebpackConfig(webpackOverride);
