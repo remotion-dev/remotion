baeb60a8ea22c50066deb35d576ccc9945bada0f
himmelio <safari.sepehr@gmail.com>
2022-07-22 03:07:32 +0430

    feat: add output location support in config file
    Former-commit-id: 1a04fec1542b5b32ff6f734e8d2ca2924459aba7

--- packages/cli/src/user-passed-output-location.ts
+++ packages/cli/src/user-passed-output-location.ts
@@ -1,12 +1,9 @@
-import {Log} from './log';
+import {Internals} from 'remotion';
 import {parsedCli} from './parse-command-line';
 
 export const getUserPassedOutputLocation = () => {
-	if (!parsedCli._[3]) {
-		Log.error('Pass an extra argument <output-filename>.');
-		process.exit(1);
-	}
-
-	const filename = parsedCli._[3];
+	const filename = parsedCli._[3]
+		? parsedCli._[3]
+		: Internals.getOutputLocation();
 	return filename;
 };
