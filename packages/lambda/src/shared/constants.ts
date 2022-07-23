aa71d1d8eb6a05085ff82dec0261e7c950f3c916
Jonny Burger <jonathanburger11@gmail.com>
2022-07-23 15:45:36 +0200

    bump lambda version

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -316,6 +316,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-07-23'
 	| '2022-07-20'
 	| '2022-07-18'
 	| '2022-07-15'
@@ -406,7 +407,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-07-20';
+export const CURRENT_VERSION: LambdaVersions = '2022-07-23';
 
 export type PostRenderData = {
 	cost: {
