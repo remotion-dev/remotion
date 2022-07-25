c79f4f46cfd9165ce64de8475e1e9289e78029ae
Jonny Burger <jonathanburger11@gmail.com>
2022-07-25 18:00:17 +0200

    make back button work in remotion preview
    Former-commit-id: aa75efc3f22425655dd85c029ae3801a146973c7

--- packages/cli/src/editor/components/InitialCompositionLoader.tsx
+++ packages/cli/src/editor/components/InitialCompositionLoader.tsx
@@ -1,6 +1,6 @@
 import type React from 'react';
 import {useContext, useEffect} from 'react';
-import type { TComposition} from 'remotion';
+import type {TComposition} from 'remotion';
 import {Internals} from 'remotion';
 import type {ExpandedFoldersState} from '../helpers/persist-open-folders';
 import {FolderContext} from '../state/folders';
@@ -16,9 +16,12 @@ export const useSelectComposition = () => {
 	const setCurrentFrame = Internals.Timeline.useTimelineSetFrame();
 	const {setCurrentComposition} = useContext(Internals.CompositionManager);
 	const {setFoldersExpanded} = useContext(FolderContext);
-	return (c: TComposition) => {
+	return (c: TComposition, push: boolean) => {
 		inOutHandles.current?.setMarks(loadMarks(c.id, c.durationInFrames));
-		window.history.pushState({}, 'Preview', `/${c.id}`);
+		if (push) {
+			window.history.pushState({}, 'Preview', `/${c.id}`);
+		}
+
 		const frame = getFrameForComposition(c.id);
 		const frameInBounds = Math.min(c.durationInFrames - 1, frame);
 		setCurrentFrame(frameInBounds);
@@ -55,15 +58,29 @@ export const InitialCompositionLoader: React.FC = () => {
 		if (compositionFromUrl) {
 			const exists = compositions.find((c) => c.id === compositionFromUrl);
 			if (exists) {
-				selectComposition(exists);
+				selectComposition(exists, true);
 				return;
 			}
 		}
 
 		if (compositions.length > 0) {
-			selectComposition(compositions[0]);
+			selectComposition(compositions[0], true);
 		}
 	}, [compositions, currentComposition, selectComposition]);
+
+	useEffect(() => {
+		const onchange = () => {
+			const newComp = window.location.pathname.substring(1);
+			const exists = compositions.find((c) => c.id === newComp);
+			if (exists) {
+				selectComposition(exists, false);
+			}
+		};
+
+		window.addEventListener('popstate', onchange);
+
+		return () => window.removeEventListener('popstate', onchange);
+	}, [compositions, selectComposition]);
 
 	return null;
 };
