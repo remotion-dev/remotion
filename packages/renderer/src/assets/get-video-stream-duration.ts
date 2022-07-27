678d4fa6927fdc6df2f4406ee6e1252c1f3ec046
Jonny Burger <jonathanburger11@gmail.com>
2022-07-27 11:41:17 +0200

    decrement last frame by 1 frame always
    Former-commit-id: f9c5412ff0184f5e9b41c8aea92eeb2bec5118ba

--- packages/renderer/src/assets/get-video-stream-duration.ts
+++ packages/renderer/src/assets/get-video-stream-duration.ts
@@ -17,7 +17,7 @@ async function getVideoStreamDurationUnlimited(
 	const args = [
 		['-v', 'error'],
 		['-select_streams', 'v:0'],
-		['-show_entries', 'stream=duration'],
+		['-show_entries', 'stream=duration,r_frame_rate'],
 		[src],
 	]
 		.reduce<(string | null)[]>((acc, val) => acc.concat(val), [])
@@ -26,9 +26,11 @@ async function getVideoStreamDurationUnlimited(
 	const task = await execa(ffprobeExecutable ?? 'ffprobe', args);
 
 	const duration = task.stdout.match(/duration=([0-9.]+)/);
+	const fps = task.stdout.match(/r_frame_rate=([0-9.]+)\/([0-9.]+)/);
 
 	const result: VideoDurationResult = {
 		duration: duration ? parseFloat(duration[1]) : null,
+		fps: fps ? parseInt(fps[1], 10) / parseInt(fps[2], 10) : null,
 	};
 
 	downloadMap.videoDurationResultCache[src] = result;
