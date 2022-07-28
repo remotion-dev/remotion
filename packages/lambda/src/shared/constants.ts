51c8dee2e6df4304464a8733cf2332067656a8a8
Jonny Burger <jonathanburger11@gmail.com>
2022-07-28 19:31:32 +0200

    bump lambda + fix lockfile
    Former-commit-id: bd41f4969b0097411cfeac58d7a2621004c82d39

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -316,6 +316,7 @@ export type RenderMetadata = {
 };
 
 export type LambdaVersions =
+	| '2022-07-28'
 	| '2022-07-27'
 	| '2022-07-25'
 	| '2022-07-23'
@@ -409,7 +410,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-07-27';
+export const CURRENT_VERSION: LambdaVersions = '2022-07-28';
 
 export type PostRenderData = {
 	cost: {
