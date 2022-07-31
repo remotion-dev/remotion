03acdbfbc24caecc5231059cdacc192c6e71ddca
Jonny Burger <jonathanburger11@gmail.com>
2022-07-31 13:00:55 +0200

    handle weird css better

--- packages/cli/src/editor/components/CompositionSelectorItem.tsx
+++ packages/cli/src/editor/components/CompositionSelectorItem.tsx
@@ -1,7 +1,12 @@
 import type {MouseEventHandler} from 'react';
 import React, {useCallback, useMemo, useState} from 'react';
 import type {TComposition} from 'remotion';
-import {CLEAR_HOVER, LIGHT_TEXT, SELECTED_BACKGROUND} from '../helpers/colors';
+import {
+	BACKGROUND,
+	CLEAR_HOVER,
+	LIGHT_TEXT,
+	SELECTED_BACKGROUND,
+} from '../helpers/colors';
 import {isCompositionStill} from '../helpers/is-composition-still';
 import {FilmIcon} from '../icons/film';
 import {CollapsedFolderIcon, ExpandedFolderIcon} from '../icons/folder';
@@ -22,6 +27,7 @@ const itemStyle: React.CSSProperties = {
 	border: 'none',
 	width: '100%',
 	textAlign: 'left',
+	backgroundColor: BACKGROUND,
 };
 
 const iconStyle: React.CSSProperties = {
