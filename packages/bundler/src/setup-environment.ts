fe917f5ffdbdd23e2d652338ed0ed88dba56134f
Jonny Burger <jonathanburger11@gmail.com>
2022-07-31 13:00:55 +0200

    handle weird css better
    Former-commit-id: 03acdbfbc24caecc5231059cdacc192c6e71ddca

--- packages/bundler/src/setup-environment.ts
+++ packages/bundler/src/setup-environment.ts
@@ -3,10 +3,11 @@ import {Internals} from 'remotion';
 Internals.setupEnvVariables();
 
 Internals.CSSUtils.injectCSS(`
-  .css-reset * {
+  .css-reset, .css-reset * {
     font-size: 16px;
     line-height: 1;
     color: white;
     font-family: Arial, Helvetica, sans-serif;
+    background-color: #1f2428;
   }
 `);
