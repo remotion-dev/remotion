bdbf8ef5c9b1e4ad698425ae098d4bce60d8e9a9
Arthur Denner <arthurdenner7@gmail.com>
2022-08-04 08:42:56 +0200

    test(lottie): move getNextFrame to utils and test it

---
+++ packages/lottie/src/test/utils.test.ts
@@ -0,0 +1,23 @@
+import {getNextFrame} from '../utils';
+
+describe('getNextFrame', () => {
+	describe('when loop is falsy', () => {
+		it('returns the current frame if smaller than total frames', () => {
+			expect(getNextFrame(23, 56)).toBe(23);
+		});
+
+		it('returns the last frame if current frame is bigger than total frames', () => {
+			expect(getNextFrame(23, 20)).toBe(20);
+		});
+	});
+
+	describe('when loop is truthy', () => {
+		it('returns the current frame if smaller than total frames', () => {
+			expect(getNextFrame(23, 56, true)).toBe(23);
+		});
+
+		it('returns the last frame if current frame is bigger than total frames', () => {
+			expect(getNextFrame(23, 20, true)).toBe(3);
+		});
+	});
+});
