cdced9a26921be61ef9fffaf42b0e140f4ddea4d
Jonny Burger <jonathanburger11@gmail.com>
2022-08-05 17:15:47 +0200

    export type
    Former-commit-id: 58a1390aab9cb1650b5c41b298d154015c93c8bf

--- packages/lottie/src/index.ts
+++ packages/lottie/src/index.ts
@@ -1,3 +1,4 @@
 export {getLottieMetadata} from './get-lottie-metadata';
-export {Lottie, LottieProps} from './Lottie';
+export {Lottie} from './Lottie';
+export type {LottieProps} from './Lottie';
 export type {LottieAnimationData} from './types';
