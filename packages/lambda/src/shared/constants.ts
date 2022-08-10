b05921d158817c4315f4b9cbd47665276643f591
Jonny Burger <jonathanburger11@gmail.com>
2022-08-10 11:38:23 +0200

    update lambda version

--- packages/lambda/src/shared/constants.ts
+++ packages/lambda/src/shared/constants.ts
@@ -416,7 +416,7 @@ export type LambdaVersions =
 	| '2021-06-23'
 	| 'n/a';
 
-export const CURRENT_VERSION: LambdaVersions = '2022-08-04';
+export const CURRENT_VERSION: LambdaVersions = '2022-08-10';
 
 export type PostRenderData = {
 	cost: {
