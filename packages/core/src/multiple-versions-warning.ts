2fc0987b861b04e20e9161ab99f4f538ff07d5ed
Jonny Burger <jonathanburger11@gmail.com>
2022-08-22 17:54:20 +0200

    catch multiple versions of remotion also on server
    Former-commit-id: 4e370c741821f1accc0aaa0b6a7c15e34cfbfc9e

--- packages/core/src/multiple-versions-warning.ts
+++ packages/core/src/multiple-versions-warning.ts
@@ -1,13 +1,13 @@
 export const checkMultipleRemotionVersions = () => {
-	if (typeof window === 'undefined') {
+	if (typeof globalThis === 'undefined') {
 		return;
 	}
 
-	if (window.remotion_imported) {
+	if ((globalThis as unknown as Window).remotion_imported) {
 		console.error(
 			'🚨 Multiple versions of Remotion detected. Multiple versions will cause conflicting React contexts and things may break in an unexpected way. Please check your dependency tree and make sure only one version of Remotion is on the page.'
 		);
 	}
 
-	window.remotion_imported = true;
+	(globalThis as unknown as Window).remotion_imported = true;
 };
