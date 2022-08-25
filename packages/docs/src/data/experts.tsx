2a872d0fb016f057e595f1875e64e3019d94204c
MehmetAdemi <mehmet@remotion.dev>
2022-08-25 15:10:10 +0200

    first-draft
    Former-commit-id: d0be658f9212f11e1a079f7af06fec92c8c9d68c

--- packages/docs/src/data/experts.tsx
+++ packages/docs/src/data/experts.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { BlueButton } from "../../components/layout/Button";
+import { Spacer } from "../../components/layout/Spacer";
+import {
+  TwitterLogo,
+  LinkedInLogo,
+  GitHubLogo,
+  EmailLogo,
+} from "../components/icons";
 
 type Expert = {
   name: string;
@@ -6,6 +14,9 @@ type Expert = {
   website: string | null;
   description: React.ReactNode;
   twitter: React.ReactNode;
+  github: React.ReactNode;
+  linkedin: React.ReactNode;
+  email: React.ReactNode;
 };
 
 export const experts: Expert[] = [
@@ -13,8 +24,13 @@ export const experts: Expert[] = [
     name: "Marcus Stenbeck",
     image: "/img/freelancers/marcus.jpeg",
     twitter: "marcusstenbeck",
+    github: "marcusstenbeck",
+    linkedin: "in/mstenbeck/",
+    email: null,
     description: (
       <div>
+        About me: Animation Technology Engineer specialized in customizable
+        videos and templates.
         <p>
           I made:{" "}
           <a target={"_blank"} href="https://superclip.io">
@@ -37,10 +53,13 @@ export const experts: Expert[] = [
     website: null,
   },
   {
-    name: "UmungoBongo",
+    name: "Matt McGillivray",
     image: "/img/freelancers/umungo.png",
     website: null,
     twitter: "UmunugoBongo1",
+    github: "UmungoBungo",
+    linkedin: "in/matthew-mcgillivray-68295a55",
+    email: null,
     description: (
       <div>
         <p>
@@ -62,6 +81,9 @@ export const experts: Expert[] = [
     image: "/img/freelancers/florent.jpeg",
     website: null,
     twitter: "florentpergoud",
+    github: "florentpergoud",
+    linkedin: "in/florent-pergoud/",
+    email: "florentpergoud@gmail.com",
     description: (
       <div>
         I made:{" "}
@@ -73,10 +95,13 @@ export const experts: Expert[] = [
     ),
   },
   {
-    name: "Middy",
-    image: "/img/freelancers/default.png",
+    name: "Stephen Sullivan",
+    image: "/img/freelancers/stephen.png",
     website: null,
     twitter: null,
+    github: null,
+    linkedin: "in/sterv/",
+    email: "stephen@middy.com",
     description: (
       <div>
         I made: <a href="https://middy.com">https://middy.com</a>!
@@ -84,21 +109,31 @@ export const experts: Expert[] = [
     ),
   },
   {
-    name: "Mohit",
+    name: "Mohit Yadav",
     image: "/img/freelancers/mohit.jpeg",
     website: null,
     twitter: "Just_Moh_it",
+    github: "Just-Moh-it",
+    linkedin: "in/just-moh-it/",
+    email: "yo@mohitya.dev",
     description: (
       <div>
-        I made: <a href="https://mockoops.mohitya.dev">Mockoops</a>!
+        I made: <a href="https://mockoops.mohitya.dev">Mockoops</a>! <br />
+        Able to help you with: SaaS platform from scratch including SSR,
+        creating individual videos and templates, and creating integrations for
+        remotion with existing infrastructure. <br />
+        Availability: 4 to 5 hours/day on weekdays, 5 to 6 hours/day on weekends
       </div>
     ),
   },
   {
-    name: "karel",
+    name: "Karel Nagel",
     image: "/img/freelancers/karel.png",
     website: "https://karel.wtf",
     twitter: "KarelETH",
+    github: "karelnagel",
+    linkedin: "in/karelnagel/",
+    email: null,
     description: (
       <div>
         I made: <a href="https://www.tiktok.com/@elommusd">TikTok automation</a>
