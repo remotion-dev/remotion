cb6f943261699faec572cb1238e62fca6cf15ec6
Jonny Burger <jonathanburger11@gmail.com>
2022-08-27 16:02:54 +0200

    add JSDoc comments
    Former-commit-id: 60b08c18bdb0ff02284426c8eaaa0fff851607c1

--- packages/paths/src/get-point-at-length.ts
+++ packages/paths/src/get-point-at-length.ts
@@ -1,9 +1,15 @@
 import {construct} from './helpers/construct';
 import {getPartAtLength} from './helpers/get-part-at-length';
 
-export const getPointAtLength = (p: string, fractionLength: number) => {
-	const constructed = construct(p);
-	const fractionPart = getPartAtLength(p, fractionLength);
+/**
+ * Gets the coordinates of a point which is on an SVG path.
+ * @param {string} path A valid SVG path
+ * @param {number} length The length at which the point should be sampled
+ * @link https://remotion.dev/docs/paths/get-point-at-length
+ */
+export const getPointAtLength = (path: string, length: number) => {
+	const constructed = construct(path);
+	const fractionPart = getPartAtLength(path, length);
 	const functionAtPart = constructed.functions[fractionPart.i];
 
 	if (functionAtPart) {
