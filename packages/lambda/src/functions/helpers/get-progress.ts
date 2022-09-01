facca3c645273e15739563edf60c35425f33a50d
Jonny Burger <jonathanburger11@gmail.com>
2022-09-01 12:59:57 +0200

    Update get-progress.ts
    Former-commit-id: b5a7f0961f39824ab887e1ec41b2ec749b27214b

--- packages/lambda/src/functions/helpers/get-progress.ts
+++ packages/lambda/src/functions/helpers/get-progress.ts
@@ -4,7 +4,7 @@ import type {RenderProgress} from '../../shared/constants';
 import {
 	chunkKey,
 	encodingProgressKey,
-	lambdaChunkInitializedPrefix,
+	initalizedMetadataKey,
 	MAX_EPHEMERAL_STORAGE_IN_MB,
 	renderMetadataKey,
 	rendersPrefix,
@@ -107,7 +107,7 @@ export const getProgress = async ({
 
 	const initializedExists = Boolean(
 		contents.find((c) => {
-			return c.Key?.startsWith(lambdaChunkInitializedPrefix(renderId));
+			return c.Key?.startsWith(initalizedMetadataKey(renderId));
 		})
 	);
 
