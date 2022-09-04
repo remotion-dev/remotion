295cef031a0f8a152091653449439e4132bb0af9
Elihu Munguía <47308963+z16th@users.noreply.github.com>
2022-09-04 14:13:35 -0500

    add: still zoom capabilities*
    *first try

---
+++ packages/example/src/StillZoom/index.tsx
@@ -0,0 +1,16 @@
+export const StillZoom: React.FC = () => {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				background: 'white',
+				flex: 1,
+				paddingBottom: 100,
+			}}
+		>
+			Hello
+		</div>
+	);
+};
