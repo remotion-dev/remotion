fc9ad0acfd5737304fa409c247d1fcb5c7ce604b
Jonny Burger <jonathanburger11@gmail.com>
2022-09-13 13:51:46 +0200

    keyboard shortcuts

--- packages/cli/src/editor/helpers/use-keybinding.ts
+++ packages/cli/src/editor/helpers/use-keybinding.ts
@@ -41,6 +41,7 @@ export const useKeybinding = () => {
 				const commandKey = window.navigator.platform.startsWith('Mac')
 					? e.metaKey
 					: e.ctrlKey;
+				console.log({e});
 				if (
 					e.key.toLowerCase() === options.key.toLowerCase() &&
 					options.commandCtrlKey === commandKey
