d2e95d3d39a91deca9649e697782d857b902f5a1
Jonny Burger <jonathanburger11@gmail.com>
2022-09-23 14:43:54 +0200

    color managemetn

--- packages/renderer/src/prespawn-ffmpeg.ts
+++ packages/renderer/src/prespawn-ffmpeg.ts
@@ -109,7 +109,7 @@ export const prespawnFfmpeg = async (options: PreSticherOptions) => {
 		proResProfileName ? ['-profile:v', proResProfileName] : null,
 		supportsCrf ? ['-crf', String(crf)] : null,
 		['-pix_fmt', pixelFormat],
-
+		['-vf', 'colorspace=bt709:iall=bt601-6-625:fast=1'],
 		// Without explicitly disabling auto-alt-ref,
 		// transparent WebM generation doesn't work
 		pixelFormat === 'yuva420p' ? ['-auto-alt-ref', '0'] : null,
