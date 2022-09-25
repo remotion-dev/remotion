6b0b57bbfe1c00842f9f9d7c16fe96479605ae69
Jonny Burger <jonathanburger11@gmail.com>
2022-09-25 15:25:23 +0200

    detect initial hover

--- packages/player/src/use-hover-state.ts
+++ packages/player/src/use-hover-state.ts
@@ -1,7 +1,13 @@
 import {useEffect, useState} from 'react';
 
 export const useHoverState = (ref: React.RefObject<HTMLDivElement>) => {
-	const [hovered, stetHovered] = useState(false);
+	const [hovered, stetHovered] = useState(() => {
+		if (ref.current) {
+			return ref.current.matches(':hover');
+		}
+
+		return false;
+	});
 
 	useEffect(() => {
 		const {current} = ref;
