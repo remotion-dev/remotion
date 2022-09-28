adc45148eecba877c1c4cd91c2596cf6df4a1171
Jonny Burger <jonathanburger11@gmail.com>
2022-09-28 17:58:06 +0200

    mock head command

--- packages/lambda/src/functions/helpers/__mocks__/io.ts
+++ packages/lambda/src/functions/helpers/__mocks__/io.ts
@@ -8,6 +8,7 @@ import {
 } from '../../../api/__mocks__/mock-s3';
 import type {
 	lambdaDeleteFile as deleteOriginal,
+	lambdaHeadCommand as headOriginal,
 	lambdaLs as lsOriginal,
 	lambdaReadFile as readOriginal,
 	lambdaWriteFile as writeOriginal,
@@ -59,6 +60,22 @@ export const lambdaDeleteFile: typeof deleteOriginal = ({
 		region,
 	});
 	return Promise.resolve(undefined);
+};
+
+export const lambdaHeadCommand: typeof headOriginal = ({
+	bucketName,
+	key,
+	region,
+}) => {
+	const read = readMockS3File({
+		bucketName,
+		key,
+		region,
+	});
+	return Promise.resolve({
+		ContentLength: read?.content.toString().length,
+		LastModified: new Date(),
+	});
 };
 
 export const lambdaLs: typeof lsOriginal = (
