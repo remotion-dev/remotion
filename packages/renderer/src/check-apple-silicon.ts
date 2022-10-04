74a9347551a01d5c1364bd705e482fd0d12c20c3
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2022-10-04 14:31:38 +0200

    feat: warn if Apple Silicon is not using arm64 architecture
    Former-commit-id: d96f516e4d7a9f13e25699e461b2037b33587778

---
+++ packages/renderer/src/check-apple-silicon.ts
@@ -0,0 +1,27 @@
+import * as os from 'os';
+import semver from 'semver';
+
+export const warnIfAppleSiliconIsNotUsingArm64Architecture = () => {
+	// see https://github.com/nodejs/node/issues/41900#issuecomment-1113511254
+	const cpus = os.cpus();
+	const isAppleSilicon = cpus[0].model.includes('Apple');
+	const isArm64 = os.arch() === 'arm64';
+
+	if (isAppleSilicon && !isArm64) {
+		const recommendedNodeVersion = '16.0.0';
+		const recommendNodeUpgrade = semver.lt(
+			process.version,
+			recommendedNodeVersion
+		);
+
+		console.warn(
+			`Apple Silicon detected, but running under Rosetta (not arm64 architecture). This will cause performance issues.\n` +
+				`Recommended actions:\n` +
+				(recommendNodeUpgrade
+					? ` - Upgrade to Node ${recommendedNodeVersion} or later\n`
+					: '') +
+				' - Run Node using `arch -arm64` architecture\n' +
+				'See https://emotion.dev/docs/troubleshooting/rosetta for more information.'
+		);
+	}
+};
