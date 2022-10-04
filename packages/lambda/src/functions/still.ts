908931633f76494c4344101cbe99b2f72d9b532c
Jonny Burger <jonathanburger11@gmail.com>
2022-10-04 10:10:53 +0200

    use convertToPositiveFrameIndex

--- packages/lambda/src/functions/still.ts
+++ packages/lambda/src/functions/still.ts
@@ -137,7 +137,10 @@ const innerStillHandler = async (
 		serveUrl: lambdaParams.serveUrl,
 		dumpBrowserLogs: false,
 		envVariables: lambdaParams.envVariables,
-		frame: lambdaParams.frame < 0 ? composition.durationInFrames - lambdaParams.frame : lambdaParams.frame,
+		frame: RenderInternals.convertToPositiveFrameIndex({
+			frame: lambdaParams.frame,
+			durationInFrames: composition.durationInFrames,
+		}),
 		imageFormat: lambdaParams.imageFormat as StillImageFormat,
 		inputProps: lambdaParams.inputProps,
 		overwrite: false,
