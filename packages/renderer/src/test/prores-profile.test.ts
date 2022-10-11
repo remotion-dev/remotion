e28daaf9d613cdb9bfce182edf6de9bf06dc0dc6
Jonny Burger <jonathanburger11@gmail.com>
2022-10-11 21:39:55 +0200

    Update prores-profile.test.ts
    Former-commit-id: 703d0e7f024be84ed70eabdf5b58629a2e3fc63b

--- packages/renderer/src/test/prores-profile.test.ts
+++ packages/renderer/src/test/prores-profile.test.ts
@@ -4,22 +4,31 @@ import {validateSelectedCodecAndProResCombination} from '../prores-profile';
 describe('Pro Res Profile', () => {
 	test("Just a ProRes profile is not enough, because codec doesn't default to ProRes", () => {
 		expect(() => {
-			validateSelectedCodecAndProResCombination('aac', '4444-xq');
+			validateSelectedCodecAndProResCombination({
+				codec: 'aac',
+				proResProfile: '4444-xq',
+			});
 		}).toThrow(
 			/You have set a ProRes profile but the codec is not "prores". Set the codec to "prores" or remove the ProRes profile./
 		);
 	});
 
 	test('Should accept a valid codec and ProRes combination', () => {
-		expect(validateSelectedCodecAndProResCombination('prores', '4444')).toBe(
-			undefined
-		);
+		expect(
+			validateSelectedCodecAndProResCombination({
+				codec: 'prores',
+				proResProfile: '4444',
+			})
+		).toBe(undefined);
 	});
 
 	test('Should throw on invalid ProRes name', () => {
 		expect(() =>
-			// @ts-expect-error
-			validateSelectedCodecAndProResCombination('prores', 'typoed')
+			validateSelectedCodecAndProResCombination({
+				codec: 'prores',
+				// @ts-expect-error
+				proResProfile: 'typoed',
+			})
 		).toThrow(
 			/The ProRes profile "typoed" is not valid. Valid options are "4444-xq", "4444", "hq", "standard", "light", "proxy"/
 		);
