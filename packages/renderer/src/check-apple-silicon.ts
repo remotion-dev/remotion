2dde719ce70a00ae42c236e9ad347b20fe58549d
Jonny Burger <jonathanburger11@gmail.com>
2022-10-19 11:19:51 +0200

    move node version into same check as apple silicon

--- packages/renderer/src/check-apple-silicon.ts
+++ packages/renderer/src/check-apple-silicon.ts
@@ -5,11 +5,18 @@ export const warnIfAppleSiliconIsNotUsingArm64Architecture = () => {
 	const cpus = os.cpus();
 	const isAppleSilicon = cpus[0].model.includes('Apple');
 	const isArm64 = os.arch() === 'arm64';
+	const version = process.version.replace('v', '').split('.');
+	const majorVersion = Number(version[0]);
+	const requiredNodeVersion = 14;
+
+	if (majorVersion < 13) {
+		throw new Error(
+			`Remotion requires at least Node ${requiredNodeVersion}. You currently have ${process.version}. Update your node version to ${requiredNodeVersion} to use Remotion.`
+		);
+	}
 
 	if (isAppleSilicon && !isArm64) {
 		const recommendedNodeVersion = 16;
-		const version = process.version.replace('v', '').split('.');
-		const majorVersion = Number(version[0]);
 		const recommendNodeUpgrade = majorVersion < recommendedNodeVersion;
 
 		console.warn(
