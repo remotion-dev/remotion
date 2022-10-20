01dc59a14775de96c5bdfa41fe1c636879b0ac62
Jonny Burger <jonathanburger11@gmail.com>
2022-10-20 18:01:21 +0200

    design changes

---
+++ packages/docs/src/theme/Footer/index.tsx
@@ -0,0 +1,23 @@
+import { useThemeConfig } from "@docusaurus/theme-common";
+import React from "react";
+import FooterLayout from "./Layout";
+import FooterLinks from "./Links";
+
+const Footer = () => {
+  const { footer } = useThemeConfig();
+  if (!footer) {
+    return null;
+  }
+
+  const { links, style } = footer;
+  return (
+    <FooterLayout
+      style={style}
+      links={
+        links && links.length > 0 && <FooterLinks style={style} links={links} />
+      }
+    />
+  );
+};
+
+export default React.memo(Footer);
