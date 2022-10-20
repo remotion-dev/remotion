a9fc5c1e4c230d8d1064b76494d825a5a6a4cb9a
Jonny Burger <jonathanburger11@gmail.com>
2022-10-20 17:25:38 +0200

    Newsletter API route

--- packages/docs/components/LandingPage/NewsletterButton.tsx
+++ packages/docs/components/LandingPage/NewsletterButton.tsx
@@ -1,37 +1,87 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { BlueButton } from "../layout/Button";
+import { Spacer } from "../layout/Spacer";
+import { CoolInput } from "../TextInput";
 import styles from "./newsletter.module.css";
 
 export const NewsletterButton: React.FC<{}> = () => {
+  const [email, setEmail] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const [subscribed, setSubscribed] = useState(false);
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      try {
+        setSubmitting(true);
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:3001/api/newsletter", {
+          method: "POST",
+          body: JSON.stringify({ email }),
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+        const json = await response.json();
+        if (json.success) {
+          setSubscribed(true);
+        } else {
+          // eslint-disable-next-line no-alert
+          alert("Something went wrong. Please try again later.");
+        }
+
+        setSubmitting(false);
+      } catch (err) {
+        // eslint-disable-next-line no-alert
+        alert("Something went wrong. Please try again later.");
+      }
+    },
+    [email]
+  );
+
   return (
     <div>
       <div className={styles.newslettergrow}>
         <div className={styles.portion}>
           <div className={styles.panel}>
-            <div
-              className={[styles.tablenewsletter, styles.titlecolor].join(" ")}
+            <div className={[styles.tablenewsletter].join(" ")}>Newsletter</div>
+            <form
+              onSubmit={handleSubmit}
+              style={{
+                width: "100%",
+              }}
             >
-              Remotion Newsletter
-            </div>
-            <ul>
-              <li>
-                In our newsletter, we announce and summarize every once in a
-                while new features and all noteworthy updates we have made on
-                Remotion. Stay in the loop and join our newsletter.
-              </li>
-            </ul>
-            <div style={{ flex: 1 }} />
-            <a
-              className={styles.newslettera}
-              href="https://www.getrevue.co/profile/Remotion?via=twitter-profile"
-              target="_blank"
-            >
+              <p>
+                Read about new features and all noteworthy updates we have made
+                on Remotion once in a while.
+              </p>
+
+              <CoolInput
+                style={{
+                  width: "100%",
+                }}
+                disabled={submitting}
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type={"email"}
+                required
+                placeholder="animator@gmail.com"
+              />
+              <Spacer />
+              <Spacer />
               <div>
-                <BlueButton fullWidth loading={false} size="bg">
-                  Subscribe
+                <BlueButton
+                  type="submit"
+                  fullWidth
+                  loading={submitting}
+                  disabled={submitting || subscribed}
+                  size="bg"
+                >
+                  {subscribed ? "Subscribed!" : "Subscribe"}
                 </BlueButton>
               </div>
-            </a>
+            </form>
           </div>
         </div>
       </div>
