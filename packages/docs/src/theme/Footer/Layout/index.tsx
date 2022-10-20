2ca72434cec84cd3364a986127f41e73dc2637e4
Jonny Burger <jonathanburger11@gmail.com>
2022-10-20 22:02:12 +0200

    fix css

--- packages/docs/src/theme/Footer/Layout/index.tsx
+++ packages/docs/src/theme/Footer/Layout/index.tsx
@@ -1,17 +1,15 @@
-import { useColorMode } from "@docusaurus/theme-common";
 import clsx from "clsx";
 import React from "react";
 
 export default ({ style, links }) => {
-  const { isDarkTheme } = useColorMode();
   return (
     <footer
       className={clsx("footer", {
         "footer--dark": style === "dark",
       })}
       style={{
-        backgroundColor: isDarkTheme ? "#1f1f1f" : "#fcfcfc",
-        borderTop: isDarkTheme ? "1px solid #2f2f2f" : "1px solid #eaeaea",
+        backgroundColor: "var(--footer-background)",
+        borderTop: "var(--footer-border)",
       }}
     >
       <div className="container container-fluid">{links}</div>
