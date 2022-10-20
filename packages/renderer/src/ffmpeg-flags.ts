44c1364566279a964ad9a57c60dc91ce3ea9a994
Jonny Burger <jonathanburger11@gmail.com>
2022-10-20 18:13:35 +0200

    fix
    Former-commit-id: 3200ef554bc2b4b2305ddd17fd9da9f18b1dddb3

--- packages/renderer/src/ffmpeg-flags.ts
+++ packages/renderer/src/ffmpeg-flags.ts
@@ -21,7 +21,11 @@ export const getFfmpegBuildInfo = async (options: {
 	}
 
 	const data = await execa(
-		await getExecutableBinary(options.ffmpegExecutable, remotionRoot, 'ffmpeg'),
+		await getExecutableBinary(
+			options.ffmpegExecutable,
+			options.remotionRoot,
+			'ffmpeg'
+		),
 		['-buildconf'],
 		{
 			reject: false,
