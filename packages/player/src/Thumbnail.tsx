60ae60a18647928845bc65218bc8dc17fa4e8178
Jonny Burger <jonathanburger11@gmail.com>
2022-10-23 13:52:35 +0200

    fix rendering

--- packages/player/src/Thumbnail.tsx
+++ packages/player/src/Thumbnail.tsx
@@ -1,63 +1,134 @@
-import {useState} from 'react'
-import type { CSSProperties} from 'react';
-import {useMemo, Suspense} from 'react'
-import type {TComposition, TimelineContextValue} from "remotion";
-import {random, Internals} from "remotion";
+import type {ComponentType, CSSProperties, LazyExoticComponent} from 'react';
+import {Suspense, useMemo, useState} from 'react';
+import type {
+	CompositionManagerContext,
+	CompProps,
+	TimelineContextValue,
+} from 'remotion';
+import {Internals, random} from 'remotion';
+import type {PropsIfHasProps} from './utils/props-if-has-props';
 
-export const Thumbnail:React.FC<{
-    composition: TComposition<unknown>;
-    targetHeight: number;
-    targetWidth: number;
-    frameToDisplay: number;
-    style?: CSSProperties
-}> = ({
-    composition,
-    targetWidth,
-    targetHeight,
-    frameToDisplay,
-    style,
-                             }) => {
-    const [thumbnailId] = useState(() => String(random(null)));
+type ThumbnailProps<T> = PropsIfHasProps<T> &
+	CompProps<T> & {
+		targetHeight: number;
+		targetWidth: number;
+		frameToDisplay: number;
+		style?: CSSProperties;
+		durationInFrames: number;
+		compositionWidth: number;
+		compositionHeight: number;
+		inputProps: number;
+		fps: number;
+	};
 
-    const container: CSSProperties = useMemo(() => {
-        return {
-            width: targetWidth,
-            height: targetHeight,
-            backgroundColor: 'rgba(0, 0, 0, 0.4)',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            ...style,
-        };
-    }, [targetHeight, targetWidth, style]);
+export const Thumbnail = <T,>({
+	targetWidth,
+	targetHeight,
+	frameToDisplay,
+	style,
+	inputProps,
+	compositionHeight,
+	compositionWidth,
+	durationInFrames,
+	fps,
+	...componentProps
+}: ThumbnailProps<T>) => {
+	const [thumbnailId] = useState(() => String(random(null)));
 
-    const timelineState: TimelineContextValue = useMemo(() => {
-        return {
-            playing: false,
-            frame: frameToDisplay,
-            rootId: thumbnailId,
-            imperativePlaying: {
-                current: false,
-            },
-            playbackRate: 1,
-            setPlaybackRate: () => {
-                throw new Error('thumbnail');
-            },
-            audioAndVideoTags: {current: []},
-        };
-    }, [frameToDisplay, thumbnailId]);
+	const container: CSSProperties = useMemo(() => {
+		return {
+			width: targetWidth,
+			height: targetHeight,
+			backgroundColor: 'rgba(0, 0, 0, 0.4)',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			...style,
+		};
+	}, [targetHeight, targetWidth, style]);
 
-    const props = useMemo(() => {
-        return (composition.defaultProps as unknown as {}) ?? {};
-    }, [composition.defaultProps]);
+	const timelineState: TimelineContextValue = useMemo(() => {
+		return {
+			playing: false,
+			frame: frameToDisplay,
+			rootId: thumbnailId,
+			imperativePlaying: {
+				current: false,
+			},
+			playbackRate: 1,
+			setPlaybackRate: () => {
+				throw new Error('thumbnail');
+			},
+			audioAndVideoTags: {current: []},
+		};
+	}, [frameToDisplay, thumbnailId]);
 
-    const ThumbnailComponent = composition.component;
-    
-    return <div style={container}>
-        <Suspense fallback={null}>
-            <Internals.Timeline.TimelineContext.Provider value={timelineState}>
-                <ThumbnailComponent {...props} />
-            </Internals.Timeline.TimelineContext.Provider>
-        </Suspense>
-    </div>
-}
+	const props = useMemo(() => {
+		return (inputProps ?? {}) as unknown as {};
+	}, [inputProps]);
+
+	const Component = Internals.useLazyComponent(
+		componentProps
+	) as LazyExoticComponent<ComponentType<unknown>>;
+
+	const compositionManagerContext: CompositionManagerContext = useMemo(() => {
+		return {
+			compositions: [
+				{
+					component: Component as React.LazyExoticComponent<
+						ComponentType<unknown>
+					>,
+					durationInFrames,
+					height: compositionHeight,
+					width: compositionWidth,
+					fps,
+					id: 'player-comp',
+					props: inputProps as unknown,
+					nonce: 777,
+					scale: 1,
+					folderName: null,
+					defaultProps: undefined,
+					parentFolderName: null,
+				},
+			],
+			folders: [],
+			registerFolder: () => undefined,
+			unregisterFolder: () => undefined,
+			currentComposition: 'player-comp',
+			registerComposition: () => undefined,
+			registerSequence: () => undefined,
+			sequences: [],
+			setCurrentComposition: () => undefined,
+			unregisterComposition: () => undefined,
+			unregisterSequence: () => undefined,
+			registerAsset: () => undefined,
+			unregisterAsset: () => undefined,
+			currentCompositionMetadata: null,
+			setCurrentCompositionMetadata: () => undefined,
+			assets: [],
+		};
+	}, [
+		Component,
+		durationInFrames,
+		compositionHeight,
+		compositionWidth,
+		fps,
+		inputProps,
+	]);
+
+	return (
+		<Internals.CanUseRemotionHooksProvider>
+			<div style={container}>
+				<Suspense fallback={null}>
+					<Internals.Timeline.TimelineContext.Provider value={timelineState}>
+						<Internals.CompositionManager.Provider
+							value={compositionManagerContext}
+						>
+							<Component {...props} />
+						</Internals.CompositionManager.Provider>
+					</Internals.Timeline.TimelineContext.Provider>
+				</Suspense>
+			</div>
+		</Internals.CanUseRemotionHooksProvider>
+	);
+};
