6386b6f952b3f49ad8e32925d2421be340930031
Stefan Uzunov <stefan.uzunov@codechem.com>
2022-10-23 12:38:39 +0200

    Added composition-promts and select-composition

---
+++ packages/cli/src/composition-prompts.ts
@@ -0,0 +1,139 @@
+import type {Choice, Options, PromptObject} from 'prompts';
+import prompts from 'prompts';
+
+export type Question<V extends string = string> = PromptObject<V> & {
+	optionsPerPage?: number;
+};
+export type NamelessQuestion = Omit<Question<'value'>, 'name' | 'type'>;
+type PromptOptions = {nonInteractiveHelp?: string} & Options;
+
+export default function prompt(
+	questions: Question | Question[],
+	{nonInteractiveHelp, ...options}: PromptOptions = {}
+) {
+	questions = Array.isArray(questions) ? questions : [questions];
+	return prompts(questions, {
+		onCancel() {
+			throw new Error();
+		},
+		...options,
+	});
+}
+
+prompt.separator = (title: string) => ({
+	title,
+	disabled: true,
+	value: undefined,
+});
+
+export async function selectComposition(
+	questions: NamelessQuestion,
+	options?: PromptOptions
+): Promise<unknown> {
+	const {value} = await prompt(
+		{
+			limit: 11,
+			...questions,
+			// @ts-expect-error: onRender not in the types
+			onRender(this: {
+				cursor: number;
+				firstRender: boolean;
+				choices: (Omit<prompts.Choice, 'disable'> & {disabled?: boolean})[];
+				value: string;
+				render: () => void;
+				moveCursor: (n: number) => void;
+				fire: () => void;
+				up: () => void;
+				down: () => void;
+				bell: () => void;
+				next: () => void;
+			}) {
+				if (this.firstRender) {
+					while ((this.choices[this.cursor] as Choice).disabled) {
+						this.cursor++;
+						if (this.cursor > this.choices.length - 1) {
+							break;
+						}
+					}
+
+					this.fire();
+					// eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+					this.value = (this.choices[this.cursor] || {}).value;
+
+					this.up = () => {
+						let next = this.cursor;
+						// eslint-disable-next-line no-constant-condition
+						while (true) {
+							if (next <= 0) {
+								break;
+							}
+
+							next--;
+							if (!(this.choices[next] as Choice).disabled) {
+								break;
+							}
+						}
+
+						if (
+							!(this.choices[next] as Choice).disabled &&
+							next !== this.cursor
+						) {
+							this.moveCursor(next);
+							this.render();
+						} else {
+							this.bell();
+						}
+					};
+
+					this.down = () => {
+						let next = this.cursor;
+						// eslint-disable-next-line no-constant-condition
+						while (true) {
+							if (next >= this.choices.length - 1) {
+								break;
+							}
+
+							next++;
+							if (!(this.choices[next] as Choice).disabled) {
+								break;
+							}
+						}
+
+						if (
+							!(this.choices[next] as Choice).disabled &&
+							next !== this.cursor
+						) {
+							this.moveCursor(next);
+							this.render();
+						} else {
+							this.bell();
+						}
+					};
+
+					this.next = () => {
+						let next = this.cursor;
+						let i = 0;
+						while (i < this.choices.length) {
+							i++;
+							next = (next + 1) % this.choices.length;
+							if ((this.choices[next] as Choice).disabled) {
+								break;
+							}
+						}
+
+						if ((this.choices[next] as Choice).disabled) {
+							this.bell();
+						} else {
+							this.moveCursor(next);
+							this.render();
+						}
+					};
+				}
+			},
+			name: 'value',
+			type: 'select',
+		},
+		options
+	);
+	return value ?? null;
+}
