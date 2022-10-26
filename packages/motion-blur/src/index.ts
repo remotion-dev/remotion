ada1e8ce2c1b0ca470c05a90096f378723d30342
Jonny Burger <jonathanburger11@gmail.com>
2022-10-26 14:50:58 +0200

    deprecation notice on symbol itself
    Former-commit-id: f3bdf175b4f69d5d44a5183858d64062d35773e6

--- packages/motion-blur/src/index.ts
+++ packages/motion-blur/src/index.ts
@@ -1,6 +1,3 @@
 export {CameraMotionBlur, CameraMotionBlurProps} from './CameraMotionBlur';
-/**
- * @deprecated `<MotionBlur/>` component is renamed to `<Trail/>`
- */
 export {MotionBlur, MotionBlurProps} from './MotionBlur';
 export {Trail, TrailProps} from './Trail';
