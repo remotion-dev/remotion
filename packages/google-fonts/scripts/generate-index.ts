d53bca02d831f956085c9cc54bdf1664a4fae0fb
Jonny Burger <jonathanburger11@gmail.com>
2022-10-27 16:39:36 +0200

    migrate scripts to typescript
    Former-commit-id: dfe58ac4ba71b6775fa3c9cc84a65465763061d5

--- packages/google-fonts/scripts/generate-index.ts
+++ packages/google-fonts/scripts/generate-index.ts
@@ -1,23 +1,17 @@
 import fs from "node:fs";
 import path from "path";
-import axios from "axios";
-import postcss from "postcss";
 import prettier from "prettier";
-import PQueue from "p-queue";
-
-import { getCssLink, unqoute, quote } from "./utils.mjs";
+import { googleFonts } from "./google-fonts";
 
 const OUTDIR = "./src";
-const FONTDATA_FILE = "./google-fonts.json";
 
 const generate = async () => {
-  const fonts = JSON.parse(fs.readFileSync(FONTDATA_FILE, "utf-8"));
   // Prepare filename
   const filename = `index.ts`;
 
   console.log(`- Generating ${filename}`);
   let output = `export const availableFonts = ${JSON.stringify(
-    fonts.items.map((f) => ({
+    googleFonts.map((f) => ({
       fontFamily: f.family.replace(/^['"]/g, "").replace(/['"]$/g, ""),
       importName: f.family
         .replace(/^['"]/g, "")
