d9cc919f16dbbbb6ed883cea8707a08680e249f3
Jonny Burger <jonathanburger11@gmail.com>
2022-10-28 13:36:38 +0200

    don't need fontinfo
    Former-commit-id: 57d8c5c99150863da3dda58788ef4f2f2964bb83

--- packages/google-fonts/scripts/utils.ts
+++ packages/google-fonts/scripts/utils.ts
@@ -1,14 +1,5 @@
 import { Font } from "./google-fonts";
 
-export type FontInfo = {
-  fontFamily: string;
-  importName: string;
-  version: string;
-  url: string;
-  unicodeRanges: Record<string, string>;
-  fonts: Record<string, Record<string, Record<string, string>>>;
-};
-
 export const unqoute = (str: string) =>
   str.replace(/^['"]/g, "").replace(/['"]$/g, "");
 
