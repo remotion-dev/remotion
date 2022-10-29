8382cdfc4c02d732f8dc38f5ffb88a5195850661
satelllte <lunaerxs@gmail.com>
2022-10-29 22:40:44 +0300

    satelllte: remove redundant linkedin slash

--- packages/docs/src/data/experts.tsx
+++ packages/docs/src/data/experts.tsx
@@ -125,7 +125,7 @@ export const experts: Expert[] = [
     website: null,
     twitter: "isatelllte",
     github: "satelllte",
-    linkedin: "/in/satelllte/",
+    linkedin: "in/satelllte/",
     email: "lunaerxs@gmail.com",
     description: (
       <div>
