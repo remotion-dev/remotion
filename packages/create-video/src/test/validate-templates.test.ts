5ecea9554ec61fe1f9e4b3b5a74194a8162b2314
Jonny Burger <jonathanburger11@gmail.com>
2022-11-11 10:25:25 +0100

    validate templates
    Former-commit-id: d111fe2f5cf90a24bfd8e00b5653c28c55259748

--- packages/create-video/src/test/validate-templates.test.ts
+++ packages/create-video/src/test/validate-templates.test.ts
@@ -8,13 +8,30 @@ const getFileForTemplate = (template: Template, file: string) => {
 };
 
 for (const template of FEATURED_TEMPLATES) {
-	test(template.shortName + ' should have a package.json', async () => {
+	test(template.shortName + ' should have a valid package.json', async () => {
 		const packageLockJson = getFileForTemplate(template, 'package.json');
 
 		const res = await got(packageLockJson, {
 			throwHttpErrors: false,
 		});
 		expect(res.statusCode).toBe(200);
+		const body = JSON.parse(res.body);
+		expect(body.dependencies.remotion).toMatch(/^\^3/);
+		expect(body.dependencies['@remotion/cli']).toMatch(/^\^3/);
+		expect(body.dependencies.react).toMatch(/^\^?18/);
+		expect(body.dependencies['react-dom']).toMatch(/^\^?18/);
+
+		expect(body.devDependencies.prettier).toMatch(/^\^?2/);
+		expect(body.devDependencies.eslint).toMatch(/^\^?8/);
+		const eitherPluginOrConfig =
+			body.devDependencies['@remotion/eslint-config']?.match(/^\^3/) ||
+			body.devDependencies['@remotion/eslint-plugin']?.match(/^\^3/);
+
+		expect(eitherPluginOrConfig).toBeTruthy();
+
+		if (!template.shortName.includes('JavaScript')) {
+			expect(body.devDependencies.typescript).toMatch(/^\^?4/);
+		}
 	});
 
 	test(
