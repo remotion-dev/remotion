2cc558d4abe13c847b2a7ebe1eb9a390d4c5893b
Jonny Burger <jonathanburger11@gmail.com>
2022-11-12 14:06:30 +0100

    expoert og images
    Former-commit-id: 31f0548b0af59916e022c1a50a367c74e1455646

---
+++ packages/docs/src/remotion/Root.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Folder, Still } from "remotion";
+import { experts } from "../data/experts";
+import { Expert } from "./Expert";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Folder name="experts">
+      {experts.map((e) => {
+        return (
+          <Still
+            key={e.slug}
+            component={Expert}
+            defaultProps={{
+              expertId: e.slug,
+            }}
+            height={630}
+            width={1200}
+            id={`${e.slug}`}
+          />
+        );
+      })}
+    </Folder>
+  );
+};
