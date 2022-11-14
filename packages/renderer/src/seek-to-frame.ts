0737e5c55393bfcc8dfefdea675b8f33a47a7426
Jonny Burger <jonathanburger11@gmail.com>
2022-11-14 13:23:45 +0100

    wait for function or browser crash

--- packages/renderer/src/seek-to-frame.ts
+++ packages/renderer/src/seek-to-frame.ts
@@ -1,6 +1,19 @@
 import type {Page} from './browser/BrowserPage';
 import {puppeteerEvaluateWithCatch} from './puppeteer-evaluate';
 
+export const waitForReady = (page: Page) => {
+	return Promise.race([
+		new Promise((_, reject) => {
+			page.browser.once(BrowserEmittedEvents.ClosedSilent, () => {
+				reject(new Error('Target closed'));
+			});
+		}),
+		page
+			.mainFrame()
+			._mainWorld.waitForFunction(page.browser, 'window.ready === true'),
+	]);
+};
+
 export const seekToFrame = async ({
 	frame,
 	page,
@@ -8,7 +21,7 @@ export const seekToFrame = async ({
 	frame: number;
 	page: Page;
 }) => {
-	await page.mainFrame().waitForFunction(page.browser, 'window.ready === true');
+	await waitForReady(page);
 	await puppeteerEvaluateWithCatch({
 		pageFunction: (f: number) => {
 			window.remotion_setFrame(f);
@@ -17,6 +30,6 @@ export const seekToFrame = async ({
 		frame,
 		page,
 	});
-	await page.mainFrame().waitForFunction(page.browser, 'window.ready === true');
+	await waitForReady(page);
 	await page.evaluateHandle('document.fonts.ready');
 };
