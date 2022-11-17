42fa21094912480664123b7d8ba89412ea057414
Jonny Burger <jonathanburger11@gmail.com>
2022-11-17 16:41:22 +0100

    version check
    Former-commit-id: 265565d0a0af9c34a66a8cb52daad12a3d2d61b5

--- packages/lambda/src/functions/compositions.ts
+++ packages/lambda/src/functions/compositions.ts
@@ -1,9 +1,22 @@
+import {VERSION} from 'remotion/version';
 import type {LambdaPayload} from '../defaults';
 import {LambdaRoutines} from '../defaults';
 
 export const compositionsHandler = (lambdaParams: LambdaPayload) => {
 	if (lambdaParams.type !== LambdaRoutines.compositions) {
 		throw new TypeError('Expected info compositions');
+	}
+
+	if (lambdaParams.version !== VERSION) {
+		if (!lambdaParams.version) {
+			throw new Error(
+				`Version mismatch: When calling getCompositionsOnLambda(), you called the function ${process.env.AWS_LAMBDA_FUNCTION_NAME} which has the version ${VERSION} but the @remotion/lambda package is an older version. Deploy a new function and use it to call getCompositionsOnLambda(). See: https://www.remotion.dev/docs/lambda/upgrading`
+			);
+		}
+
+		throw new Error(
+			`Version mismatch: When calling getCompositionsOnLambda(), you passed ${process.env.AWS_LAMBDA_FUNCTION_NAME} as the function, which has the version ${VERSION}, but the @remotion/lambda package you used to invoke the function has version ${lambdaParams.version}. Deploy a new function and use it to call getCompositionsOnLambda(). See: https://www.remotion.dev/docs/lambda/upgrading`
+		);
 	}
 
 	return Promise.resolve({});
