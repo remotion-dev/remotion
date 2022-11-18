6b083f2a848b0ee6af04c3688af8a916d66a05db
Jonny Burger <jonathanburger11@gmail.com>
2022-11-18 12:29:26 +0100

    show copy progress
    Former-commit-id: 07d4897fb44920b58dc85cf7465d990a0a139994

--- packages/bundler/src/copy-dir.ts
+++ packages/bundler/src/copy-dir.ts
@@ -5,26 +5,37 @@ export async function copyDir({
 	src,
 	dest,
 	onSymlinkDetected,
+	onProgress,
 }: {
 	src: string;
 	dest: string;
 	onSymlinkDetected: (entry: fs.Dirent, dir: string) => void;
+	onProgress: (bytes: number) => void;
 }) {
 	await fs.promises.mkdir(dest, {recursive: true});
 	const entries = await fs.promises.readdir(src, {withFileTypes: true});
+
+	let copied = 0;
 
 	for (const entry of entries) {
 		const srcPath = path.join(src, entry.name);
 		const destPath = path.join(dest, entry.name);
 
 		if (entry.isDirectory()) {
-			await copyDir({src: srcPath, dest: destPath, onSymlinkDetected});
+			await copyDir({
+				src: srcPath,
+				dest: destPath,
+				onSymlinkDetected,
+				onProgress,
+			});
 		} else if (entry.isSymbolicLink()) {
 			const realpath = await fs.promises.realpath(srcPath);
 			onSymlinkDetected(entry, src);
 			await fs.promises.symlink(realpath, destPath);
 		} else {
 			await fs.promises.copyFile(srcPath, destPath);
+			copied += fs.statSync(srcPath).size;
+			onProgress(copied);
 		}
 	}
 }
