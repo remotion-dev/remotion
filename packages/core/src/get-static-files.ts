489f3e9f4f02b5e33f052feb0165899eb945b4a8
Jonny Burger <jonathanburger11@gmail.com>
2022-12-04 17:29:01 +0100

    handle player and SSR cases
    Former-commit-id: 13262d701e1884b3ee591000d665c30151a5c41d

--- packages/core/src/get-static-files.ts
+++ packages/core/src/get-static-files.ts
@@ -1,5 +1,40 @@
 import type {StaticFile} from './static-file';
 
+let warnedServer = false;
+let warnedPlayer = false;
+
+const warnServerOnce = () => {
+	if (warnedServer) {
+		return;
+	}
+
+	warnedServer = true;
+	console.warn(
+		'Called getStaticFiles() on the server. The API is only available in the browser. An empty array was returned.'
+	);
+};
+
+const warnPlayerOnce = () => {
+	if (warnedPlayer) {
+		return;
+	}
+
+	warnedPlayer = true;
+	console.warn(
+		'Called getStaticFiles() while using the Remotion Player. The API is only available while using the Remotion Preview. An empty array was returned.'
+	);
+};
+
 export const getStaticFiles = (): StaticFile[] => {
+	if (typeof document === 'undefined') {
+		warnServerOnce();
+		return [];
+	}
+
+	if (window.remotion_isPlayer) {
+		warnPlayerOnce();
+		return [];
+	}
+
 	return window.remotion_staticFiles;
 };
