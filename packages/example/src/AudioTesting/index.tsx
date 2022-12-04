593d22ed128a3f92f40af159265c419938802070
Jonny Burger <jonathanburger11@gmail.com>
2022-12-04 17:36:17 +0100

    Update index.tsx

--- packages/example/src/AudioTesting/index.tsx
+++ packages/example/src/AudioTesting/index.tsx
@@ -1,6 +1,15 @@
-import {Audio, interpolate, Sequence, staticFile} from 'remotion';
+import {
+	Audio,
+	getStaticFiles,
+	interpolate,
+	Sequence,
+	staticFile,
+} from 'remotion';
 
 const AudioTesting: React.FC = () => {
+	const files = getStaticFiles();
+	const music = files.find((f) => f.path.startsWith('music.mp3'));
+
 	return (
 		<div>
 			<Sequence from={100} durationInFrames={100}>
@@ -8,7 +17,7 @@ const AudioTesting: React.FC = () => {
 					loop
 					startFrom={100}
 					endAt={200}
-					src={staticFile('music.mp3')}
+					src={staticFile(music?.path as string)}
 					volume={(f) =>
 						interpolate(f, [0, 50, 100], [0, 1, 0], {
 							extrapolateLeft: 'clamp',
