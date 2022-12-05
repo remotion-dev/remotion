0aaaaac8bb41945c31364c077985bfebbf4cf379
Jonny Burger <jonathanburger11@gmail.com>
2022-12-05 17:44:52 +0100

    don't reupload assets already on S3
    Former-commit-id: 79067b1a7fee5641de222b2879a77ad6dc35ab84

---
+++ packages/lambda/src/shared/get-s3-operations.ts
@@ -0,0 +1,34 @@
+import type {_Object} from '@aws-sdk/client-s3';
+import {readDirectory} from './read-dir';
+
+export const getS3DiffOperations = async ({
+	objects,
+	bundle,
+	prefix,
+}: {
+	objects: _Object[];
+	bundle: string;
+	prefix: string;
+}) => {
+	const dir = await readDirectory({
+		dir: bundle,
+		etags: {},
+		originalDir: bundle,
+	});
+
+	const filesOnS3ButNotLocal = objects.filter((fileOnS3) => {
+		const key = fileOnS3.Key?.substring(prefix.length + 1) as string;
+		return !dir[key];
+	});
+	const localFilesNotOnS3 = Object.keys(dir).filter((d) => {
+		return !objects.find((o) => {
+			const key = o.Key?.substring(prefix.length + 1) as string;
+			return key === d && o.ETag === dir[d];
+		});
+	});
+
+	return {
+		toDelete: filesOnS3ButNotLocal,
+		toUpload: localFilesNotOnS3,
+	};
+};
