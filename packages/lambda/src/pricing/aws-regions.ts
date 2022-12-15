979a86761405b83a471e3849990d8c9c34ff6204
Jonny Burger <jonathanburger11@gmail.com>
2022-12-15 13:54:13 +0100

    allow getRegions() to return only the regions that are enabled by default on AWS
    Former-commit-id: 49d2459eb672314b9e7396c3f1a0e7d0774866eb

--- packages/lambda/src/pricing/aws-regions.ts
+++ packages/lambda/src/pricing/aws-regions.ts
@@ -1,3 +1,23 @@
+export const DEFAULT_AWS_REGIONS = [
+	'eu-central-1',
+	'eu-west-1',
+	'eu-west-2',
+	'eu-west-3',
+	'eu-north-1',
+	'us-east-1', // N. Virginia
+	'us-east-2', // Ohio;
+	'us-west-1',
+	'us-west-2', // Oregon
+	'ap-south-1',
+	'ap-southeast-1',
+	'ap-southeast-2',
+	'ap-northeast-1',
+	'ap-northeast-2',
+	'ap-northeast-3',
+	'ca-central-1',
+	'sa-east-1',
+] as const;
+
 export const AWS_REGIONS = [
 	'eu-central-1',
 	'eu-west-1',
