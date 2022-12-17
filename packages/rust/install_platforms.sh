12450e6f0d1722be3eb1c30f9d8d11e3fafcb395
Jonny Burger <jonathanburger11@gmail.com>
2022-12-17 16:51:44 +0100

    add windows as a rust target

--- packages/rust/install_platforms.sh
+++ packages/rust/install_platforms.sh
@@ -2,3 +2,4 @@ rustup target add x86_64-unknown-linux-gnu
 rustup target add aarch64-unknown-linux-gnu
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-darwin
+rustup target add x86_64-pc-windows-gnu
