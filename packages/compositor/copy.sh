d210d899b1da90278e7e55a858b0050685998975
Jonny Burger <jonathanburger11@gmail.com>
2022-12-18 11:34:03 +0100

    add more scripts and distribute binaries
    Former-commit-id: 6825c9d64382d369eb0bb9ca58d9367a1f3b04bf

---
+++ packages/compositor/copy.sh
@@ -0,0 +1,7 @@
+cp target/aarch64-unknown-linux-gnu/release/compositor ../compositor-linux-arm64-gnu/compositor
+cp target/aarch64-unknown-linux-musl/release/compositor ../compositor-linux-arm64-musl/compositor
+cp target/x86_64-unknown-linux-gnu/release/compositor ../compositor-linux-x64-gnu/compositor
+cp target/x86_64-unknown-linux-musl/release/compositor ../compositor-linux-x64-musl/compositor
+cp target/x86_64-apple-darwin/release/compositor ../compositor-darwin-x64/compositor
+cp target/aarch64-apple-darwin/release/compositor ../compositor-darwin-arm64/compositor
+cp target/x86_64-pc-windows-gnu/release/compositor.exe ../compositor-win32-x64-msvc/compositor.exe
