6825c9d64382d369eb0bb9ca58d9367a1f3b04bf
Jonny Burger <jonathanburger11@gmail.com>
2022-12-18 11:34:03 +0100

    add more scripts and distribute binaries

--- packages/compositor/install_platforms.sh
+++ packages/compositor/install_platforms.sh
@@ -1,5 +1,7 @@
 rustup target add x86_64-unknown-linux-gnu
 rustup target add aarch64-unknown-linux-gnu
+rustup target add x86_64-unknown-linux-musl
+rustup target add aarch64-unknown-linux-musl
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-darwin
 rustup target add x86_64-pc-windows-gnu
