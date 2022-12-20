bb127a4dc142550e53f0d4d1c29e0dc485161837
Jonny Burger <jonathanburger11@gmail.com>
2022-12-20 18:07:13 +0100

    apple wow tutorial

--- packages/docs/src/components/AppleFireworksTutorial.tsx
+++ packages/docs/src/components/AppleFireworksTutorial.tsx
@@ -1,16 +1,21 @@
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import React from "react";
 import { VideoPlayerWithControls } from "./VideoPlayerWithControls";
 
 export const AppleFireworksTutorial = () => {
   return (
-    <VideoPlayerWithControls
-      playbackId="RDHz028sflhaMtuCm1ogQTdtq02aLTspWjLrnEIlDS1ZM"
-      poster="https://image.mux.com/RDHz028sflhaMtuCm1ogQTdtq02aLTspWjLrnEIlDS1ZM/thumbnail.png"
-      onError={(error) => {
-        console.log(error);
-      }}
-      onLoaded={() => undefined}
-      onSize={() => undefined}
-    />
+    <BrowserOnly>
+      {() => (
+        <VideoPlayerWithControls
+          playbackId="RDHz028sflhaMtuCm1ogQTdtq02aLTspWjLrnEIlDS1ZM"
+          poster="https://image.mux.com/RDHz028sflhaMtuCm1ogQTdtq02aLTspWjLrnEIlDS1ZM/thumbnail.png"
+          onError={(error) => {
+            console.log(error);
+          }}
+          onLoaded={() => undefined}
+          onSize={() => undefined}
+        />
+      )}
+    </BrowserOnly>
   );
 };
