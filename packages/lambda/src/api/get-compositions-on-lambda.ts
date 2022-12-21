8d4f040e01af3cff3a8a72fc42b16bf763f5c6b4
Jonny Burger <jonathanburger11@gmail.com>
2022-12-21 15:43:52 +0100

    remove one getOrCreateBucket() call

--- packages/lambda/src/api/get-compositions-on-lambda.ts
+++ packages/lambda/src/api/get-compositions-on-lambda.ts
@@ -5,7 +5,6 @@ import {VERSION} from 'remotion/version';
 import type {AwsRegion} from '../client';
 import {LambdaRoutines} from '../defaults';
 import {callLambda} from '../shared/call-lambda';
-import {convertToServeUrl} from '../shared/convert-to-serve-url';
 import {serializeInputProps} from '../shared/serialize-input-props';
 
 export type GetCompositionsOnLambdaInput = {
@@ -44,8 +43,6 @@ export const getCompositionsOnLambda = async ({
 	logLevel,
 	timeoutInMilliseconds,
 }: GetCompositionsOnLambdaInput): Promise<GetCompositionsOnLambdaOutput> => {
-	const realServeUrl = await convertToServeUrl(serveUrl, region);
-
 	const serializedInputProps = await serializeInputProps({
 		inputProps,
 		region,
@@ -58,7 +55,7 @@ export const getCompositionsOnLambda = async ({
 			type: LambdaRoutines.compositions,
 			payload: {
 				chromiumOptions: chromiumOptions ?? {},
-				serveUrl: realServeUrl,
+				serveUrl,
 				envVariables,
 				inputProps: serializedInputProps,
 				logLevel: logLevel ?? 'info',
