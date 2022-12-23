e1bcd6d6baacd396a03b5d4f796dcb12e6a2830d
Jonny Burger <jonathanburger11@gmail.com>
2022-12-23 14:37:19 +0100

    allow amplification during render

--- packages/renderer/src/stringify-ffmpeg-filter.ts
+++ packages/renderer/src/stringify-ffmpeg-filter.ts
@@ -14,6 +14,7 @@ export const stringifyFfmpegFilter = ({
 	playbackRate,
 	durationInFrames,
 	assetDuration,
+	allowAmplificationDuringRender,
 }: {
 	trimLeft: number;
 	trimRight: number;
@@ -24,6 +25,7 @@ export const stringifyFfmpegFilter = ({
 	durationInFrames: number;
 	playbackRate: number;
 	assetDuration: number | null;
+	allowAmplificationDuringRender: boolean;
 }): string | null => {
 	const startInVideoSeconds = startInVideo / fps;
 
@@ -35,6 +37,7 @@ export const stringifyFfmpegFilter = ({
 		volume,
 		fps,
 		trimLeft,
+		allowAmplificationDuringRender,
 	});
 
 	// Avoid setting filters if possible, as combining them can create noise
