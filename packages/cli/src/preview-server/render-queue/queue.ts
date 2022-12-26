d47cfcc764f93920e5f2c8c7d28cb3e7b3dd24fc
Jonny Burger <jonathanburger11@gmail.com>
2022-12-26 20:38:45 +0100

    shuffle things around
    Former-commit-id: 85735ddd58d8a6ebb87be99e2cbcf2cd8c38afd7

--- packages/cli/src/preview-server/render-queue/queue.ts
+++ packages/cli/src/preview-server/render-queue/queue.ts
@@ -1,16 +1,8 @@
-import {
-	getCompositions,
-	openBrowser,
-	RenderInternals,
-	renderStill,
-} from '@remotion/renderer';
-import {ConfigInternals} from '../../config';
 import {installFileWatcher} from '../../file-watcher';
-import {getCliOptions} from '../../get-cli-options';
 import {Log} from '../../log';
-import {bundleOnCli} from '../../setup-cache';
 import {waitForLiveEventsListener} from '../live-events';
 import type {RenderJob, RenderJobWithCleanup} from './job';
+import {processStill} from './process-still';
 
 let jobQueue: RenderJobWithCleanup[] = [];
 
@@ -54,85 +46,7 @@ export const processJob = async ({
 	entryPoint: string;
 }) => {
 	if (job.type === 'still') {
-		const {
-			publicDir,
-			browserExecutable,
-			ffmpegExecutable,
-			ffprobeExecutable,
-			chromiumOptions,
-			envVariables,
-			inputProps,
-			port,
-			scale,
-			browser,
-			puppeteerTimeout,
-		} = await getCliOptions({
-			isLambda: false,
-			type: 'get-compositions',
-			remotionRoot,
-		});
-
-		const browserInstance = openBrowser(browser, {
-			browserExecutable,
-			chromiumOptions,
-			shouldDumpIo: RenderInternals.isEqualOrBelowLogLevel(
-				ConfigInternals.Logging.getLogLevel(),
-				'verbose'
-			),
-			forceDeviceScaleFactor: scale,
-		});
-
-		const serveUrl = await bundleOnCli({
-			fullPath: entryPoint,
-			publicDir,
-			remotionRoot,
-			steps: ['bundling'],
-		});
-
-		const compositions = await getCompositions(serveUrl, {
-			browserExecutable,
-			chromiumOptions,
-			envVariables,
-			ffmpegExecutable,
-			ffprobeExecutable,
-			inputProps,
-			port,
-			puppeteerInstance: await browserInstance,
-			timeoutInMilliseconds: puppeteerTimeout,
-		});
-
-		const composition = compositions.find((c) => {
-			return c.id === job.compositionId;
-		});
-
-		if (!composition) {
-			throw new TypeError(`Composition ${job.compositionId} not found`);
-		}
-
-		await renderStill({
-			composition,
-			output: job.outputLocation,
-			serveUrl,
-			browserExecutable,
-			chromiumOptions,
-			envVariables,
-			ffmpegExecutable,
-			ffprobeExecutable,
-			scale: job.scale,
-			timeoutInMilliseconds: puppeteerTimeout,
-			// TODO: Write download progress to CLI
-			overwrite: false,
-			// TODO: Allow user to overwrite file
-			port,
-			inputProps,
-			puppeteerInstance: await browserInstance,
-			quality: job.quality ?? undefined,
-			imageFormat: job.imageFormat,
-			frame: job.frame,
-
-			// TODO: Allow cancel signal
-			// TODO: Accept CLI options
-		});
+		await processStill({job, remotionRoot, entryPoint});
 	}
 };
 
