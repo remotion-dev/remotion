6b17d51e23d26eb799bfa7ce47eabb520671311c
Jonny Burger <jonathanburger11@gmail.com>
2023-01-01 19:13:06 +0100

    Update get-compositions-on-server.tsx

--- packages/renderer/src/get-compositions-on-server.tsx
+++ packages/renderer/src/get-compositions-on-server.tsx
@@ -1,7 +1,18 @@
 import type {ComponentType} from 'react';
 import {renderToString} from 'react-dom/server';
+import type {TCompMetadata} from 'remotion/src';
 
 export const getCompositionsOnServer = (Comp: ComponentType) => {
 	process.env.REMOTION_SERVER_RENDERING = 'true';
-	console.log(renderToString(<Comp />));
+	const str = renderToString(<Comp />);
+
+	const matches = str.matchAll(/<div>(.*?)<\/div>/g);
+	const metadata: TCompMetadata[] = [];
+
+	for (const match of matches) {
+		const json = JSON.parse(match[1]);
+		metadata.push(json);
+	}
+
+	console.log(metadata);
 };
