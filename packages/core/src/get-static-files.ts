884a154ddf1950e74c40b35465ce968a381d4540
Jonny Burger <jonathanburger11@gmail.com>
2023-01-03 20:36:43 +0100

    get it working

--- packages/core/src/get-static-files.ts
+++ packages/core/src/get-static-files.ts
@@ -38,8 +38,18 @@ export const getStaticFiles = (): StaticFile[] => {
 };
 
 export type StaticFile = {
+	/**
+	 * A string that you can pass to the `src` attribute of an `<Audio>`, `<Img>` and `<Video>` element.
+	 */
 	src: string;
+	/**
+	 * The filepath of the file, relative to the public folder.
+	 * Example: `subfolder/image.png`
+	 */
 	name: string;
 	sizeInBytes: number;
+	/**
+	 * UNIX timestamp in milliseconds
+	 */
 	lastModified: number;
 };
