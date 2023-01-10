0f093e486c4fcec5e456be232d177f136d778c9e
Ravi Jain <jravi248@gmail.com>
2023-01-11 02:32:37 +0530

    Extend concurrency property to accept percentage
    Former-commit-id: a7629d44f8eb0293183f6e2d50cd90162d597d25

--- packages/renderer/src/get-concurrency.ts
+++ packages/renderer/src/get-concurrency.ts
@@ -1,13 +1,20 @@
 import os from 'os';
 
-export const getActualConcurrency = (userPreference: number | null) => {
+export const getActualConcurrency = (userPreference: number | string | null) => {
 	if (userPreference === null) {
 		return Math.round(Math.min(8, Math.max(1, os.cpus().length / 2)));
 	}
 
-	const rounded = Math.floor(userPreference);
 	const max = os.cpus().length;
 	const min = 1;
+
+	if (typeof userPreference === 'string') {
+		const percentage = parseInt(userPreference.slice(0, -1));
+		const rounded = Math.floor(percentage/100 * max);
+		return rounded;
+	}
+
+	const rounded = Math.floor(userPreference);
 	if (rounded > max) {
 		throw new Error(
 			`Maximum for --concurrency is ${max} (number of cores on this system)`
