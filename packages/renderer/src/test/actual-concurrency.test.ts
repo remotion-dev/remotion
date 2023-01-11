1657fa392fff3fb027bcf46c25946770e715b1a2
Ravi Jain <jravi248@gmail.com>
2023-01-11 19:39:57 +0530

    Update test
    Former-commit-id: cb6df933aef0762c66dac78a3bc5b42969cde60c

--- packages/renderer/src/test/actual-concurrency.test.ts
+++ packages/renderer/src/test/actual-concurrency.test.ts
@@ -4,7 +4,7 @@ import {getActualConcurrency} from '../get-concurrency';
 
 test('Actual concurrency null should choose a good result', () => {
 	expect(getActualConcurrency(null)).toBe(
-		Math.floor(Math.min(8, Math.max(1, os.cpus().length / 2)))
+		Math.round(Math.min(8, Math.max(1, os.cpus().length / 2)))
 	);
 });
 
