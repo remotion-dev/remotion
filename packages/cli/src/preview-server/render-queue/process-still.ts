d6784a1c3d0270b2765287becd44319adaf4c3ab
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-01-12 17:18:05 +0100

    slider decimal bug fix
    Former-commit-id: 14d641f484c018c577285ec8fbd8c3477bd417d3

--- packages/cli/src/preview-server/render-queue/process-still.ts
+++ packages/cli/src/preview-server/render-queue/process-still.ts
@@ -69,6 +69,5 @@ export const processStill = async ({
 		addCleanupCallback,
 		cancelSignal: job.cancelToken.cancelSignal,
 	});
-	// TODO: Cancel signal does not work
 	// TODO: Accept CLI options
 };
