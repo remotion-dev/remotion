00e4fc4e2a0d065cd3292bc8c5a66b395d7e43a2
Jonny Burger <jonathanburger11@gmail.com>
2023-01-15 16:02:59 +0100

    remove commas

--- packages/shapes/src/test/make-circle.test.ts
+++ packages/shapes/src/test/make-circle.test.ts
@@ -4,6 +4,6 @@ import {makeCircle} from '../make-circle';
 test('Should be able to make a circle path', () => {
 	const circlePath = makeCircle({cx: 50, cy: 50, radius: 50});
 	expect(circlePath).toEqual(
-		'M 50 50 m -50, 0 a 50,50 0 1,0 100,0  50,50 0 1,0 -100,0'
+		'M 50 50 m -50 0 a 50 50 0 1 0 100 0 50 50 0 1 0 -100 0'
 	);
 });
