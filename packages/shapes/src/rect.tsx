1ad0d8742c8fe57ccc55b499a3c7a7c167cde424
Jonny Burger <jonathanburger11@gmail.com>
2023-01-16 11:58:52 +0100

    generic renderSvg method
    Former-commit-id: c8a57a3688c78a3a70b976642f4b4cd2ea1305d4

--- packages/shapes/src/rect.tsx
+++ packages/shapes/src/rect.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import type React from 'react';
 import {makeRect} from './make-rect';
+import {renderSvg} from './render-svg';
 
 export type SquareProps = {
 	width: number;
@@ -9,22 +10,14 @@ export type SquareProps = {
 };
 
 export const Rect: React.FC<SquareProps> = ({width, height, fill, style}) => {
-	return (
-		<svg
-			width={width}
-			height={height}
-			viewBox={`0 0 ${width} ${height}`}
-			xmlns="http://www.w3.org/2000/svg"
-			style={style}
-			data-shape-type="rect"
-		>
-			<path
-				d={makeRect({
-					width,
-					height,
-				})}
-				fill={fill}
-			/>
-		</svg>
-	);
+	return renderSvg({
+		fill,
+		height,
+		path: makeRect({
+			width,
+			height,
+		}),
+		width,
+		style,
+	});
 };
