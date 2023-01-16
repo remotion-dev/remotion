b3c9ade525947b92643c52cc32c1641a2b168f55
Jonny Burger <jonathanburger11@gmail.com>
2023-01-16 14:52:02 +0100

    remove default values
    Former-commit-id: 3a92f021b9fa9623dc087320fddc18494d0a9859

--- packages/shapes/src/utils/make-rect.ts
+++ packages/shapes/src/utils/make-rect.ts
@@ -6,12 +6,9 @@ export type MakeRectOptions = {
 };
 
 export const makeRect = ({width, height}: MakeRectOptions): ShapeInfo => {
-	const x = 0;
-	const y = 0;
-
 	return {
 		width,
 		height,
-		path: `M ${x} ${y} l ${width} 0 l 0 ${height} l ${-width} 0 Z`,
+		path: `M 0 0 l ${width} 0 l 0 ${height} l ${-width} 0 Z`,
 	};
 };
