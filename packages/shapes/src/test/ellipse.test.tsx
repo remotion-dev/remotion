23ae28c9ccd120e9feee7cc7cba1f40a132a652a
Jonny Burger <jonathanburger11@gmail.com>
2023-01-16 13:00:13 +0100

    ellipse
    Former-commit-id: d042391f0d7b868642470484d5ef6ba8cb173b47

--- packages/shapes/src/test/ellipse.test.tsx
+++ packages/shapes/src/test/ellipse.test.tsx
@@ -1,5 +1,5 @@
 import {expect, test} from 'vitest';
-import {Ellipse} from '../components/_Ellipse';
+import {Ellipse} from '../components/ellipse';
 import {render} from './test-utils';
 
 test('Should be able to make a circle svg', () => {
