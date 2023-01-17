3742ea0a4b83bd4b664e6d3f897bb3d897dffd8c
Jonny Burger <jonathanburger11@gmail.com>
2023-01-17 12:03:29 +0100

    use SWC loader instead
    Former-commit-id: 03b666881a1fcdd3293fe827823f26a907c2b9f6

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -13,11 +13,18 @@ module.exports = {
   projectName: "remotion", // Usually your repo name.
   webpack: {
     jsLoader: (isServer) => ({
-      loader: require.resolve("esbuild-loader"),
+      loader: require.resolve("swc-loader"),
       options: {
-        loader: "tsx",
-        format: isServer ? "cjs" : undefined,
-        target: isServer ? "node16" : "es2017",
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          target: "es2017",
+        },
+        module: {
+          type: isServer ? "commonjs" : "es6",
+        },
       },
     }),
   },
