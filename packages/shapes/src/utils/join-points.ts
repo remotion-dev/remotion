e2ea58fef2759ed67939eaea91147b6b4584972b
Jonny Burger <jonathanburger11@gmail.com>
2023-01-17 22:42:24 +0100

    squircle factor, doesn't work yet

--- packages/shapes/src/utils/join-points.ts
+++ packages/shapes/src/utils/join-points.ts
@@ -1,6 +1,41 @@
-export const joinPoints = (points: [number, number][]) => {
+export const joinPoints = (
+	points: [number, number][],
+	{squircleFactor}: {squircleFactor: number}
+) => {
 	const pointsString = points
-		.map(([x, y], i) => `${i === 0 ? 'M' : 'L'} ${x} ${y}`)
+		.map(([x, y], i) => {
+			const prevPoint = i === 0 ? points[points.length - 2] : points[i - 1];
+			const nextPoint = i === points.length - 1 ? points[1] : points[i + 1];
+			const middleOfLine = [(x + prevPoint[0]) / 2, (y + prevPoint[1]) / 2];
+
+			if (i === 0) {
+				if (squircleFactor > 0) {
+					return `M ${middleOfLine[0]} ${middleOfLine[1]}`;
+				}
+
+				return `M ${x} ${y}`;
+			}
+
+			if (squircleFactor === 0) {
+				return `L ${x} ${y}`;
+			}
+
+			const prevVector = [x - prevPoint[0], y - prevPoint[1]] as const;
+			const nextVector = [nextPoint[0] - x, nextPoint[1] - y] as const;
+
+			const controlPoint1 = [
+				prevPoint[0] + prevVector[0] * squircleFactor * 0.5,
+				prevPoint[1] + prevVector[1] * squircleFactor * 0.5,
+			] as const;
+
+			const controlPoint2 = [
+				x - nextVector[0] * squircleFactor * 0.5,
+				y - nextVector[1] * squircleFactor * 0.5,
+			] as const;
+
+			return `C ${controlPoint1[0]} ${controlPoint1[1]} ${controlPoint2[0]} ${controlPoint2[1]} ${middleOfLine[0]} ${middleOfLine[1]}`;
+		})
 		.join(' ');
+
 	return pointsString;
 };
--- packages/shapes/src/utils/make-rect.ts
+++ packages/shapes/src/utils/make-rect.ts
@@ -7,17 +7,22 @@ export type MakeRectOptions = {
 };
 
 export const makeRect = ({width, height}: MakeRectOptions): ShapeInfo => {
-	const path = joinPoints([
-		[0, 0],
-		[width, 0],
-		[width, height],
-		[0, height],
-	]);
+	const transformOrigin: [number, number] = [width / 2, height / 2];
+	const path = joinPoints(
+		[
+			[0, 0],
+			[width, 0],
+			[width, height],
+			[0, height],
+			[0, 0],
+		],
+		{squircleFactor: 1}
+	);
 
 	return {
 		width,
 		height,
 		path,
-		transformOrigin: `${width / 2} ${height / 2}`,
+		transformOrigin: transformOrigin.join(' '),
 	};
 };
