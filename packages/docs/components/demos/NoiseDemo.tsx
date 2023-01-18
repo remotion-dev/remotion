430621f4251c0f07b6d157ea3e0170655969db4c
Jonny Burger <jonathanburger11@gmail.com>
2023-01-18 15:02:55 +0100

    generic demo component
    Former-commit-id: 23ee70d9a9ca55a5c706c2bd88bc1cbc37534ef5

--- packages/docs/components/demos/NoiseDemo.tsx
+++ packages/docs/components/demos/NoiseDemo.tsx
@@ -1,6 +1,5 @@
 import { noise3D } from "@remotion/noise";
-import { Player } from "@remotion/player";
-import React, { useMemo, useState } from "react";
+import React from "react";
 import { interpolate, useCurrentFrame, useVideoConfig } from "remotion";
 
 interface Props {
@@ -13,7 +12,11 @@ const OVERSCAN_MARGIN = 100;
 const ROWS = 10;
 const COLS = 15;
 
-const NoiseComp: React.FC<Props> = ({ speed, circleRadius, maxOffset }) => {
+export const NoiseComp: React.FC<Props> = ({
+  speed,
+  circleRadius,
+  maxOffset,
+}) => {
   const frame = useCurrentFrame();
   const { height, width } = useVideoConfig();
 
@@ -48,90 +51,5 @@ const NoiseComp: React.FC<Props> = ({ speed, circleRadius, maxOffset }) => {
         })
       )}
     </svg>
-  );
-};
-
-export const NoiseDemo = () => {
-  const [maxOffset, setMaxOffset] = useState(50);
-  const [speed, setSpeed] = useState(0.01);
-  const [circleRadius, setCircleRadius] = useState(5);
-
-  const inputStyle = useMemo<React.CSSProperties>(
-    () => ({
-      width: 90,
-      marginRight: 8,
-    }),
-    []
-  );
-  const labelStyle = useMemo<React.CSSProperties>(
-    () => ({
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      margin: 8,
-    }),
-    []
-  );
-
-  return (
-    <>
-      <Player
-        component={NoiseComp}
-        compositionWidth={1280}
-        compositionHeight={720}
-        durationInFrames={150}
-        fps={30}
-        style={{
-          width: "100%",
-          border: "1px solid var(--ifm-color-emphasis-300)",
-          borderRadius: "var(--ifm-pre-border-radius)",
-        }}
-        inputProps={{
-          maxOffset,
-          speed,
-          circleRadius,
-        }}
-        autoPlay
-        loop
-      />
-      <div>
-        <label style={labelStyle}>
-          <input
-            type="range"
-            min={0.001}
-            max={0.025}
-            step={0.001}
-            value={speed}
-            style={inputStyle}
-            onChange={(e) => setSpeed(Number(e.target.value))}
-          />
-          <code>{`speed={${speed}}`}</code>
-        </label>
-        <label style={labelStyle}>
-          <input
-            type="range"
-            min={0}
-            max={100}
-            step={1}
-            value={maxOffset}
-            style={inputStyle}
-            onChange={(e) => setMaxOffset(Number(e.target.value))}
-          />
-          <code>{`maxOffset={${maxOffset}}`}</code>
-        </label>
-        <label style={labelStyle}>
-          <input
-            type="range"
-            min={2}
-            max={20}
-            step={1}
-            value={circleRadius}
-            style={inputStyle}
-            onChange={(e) => setCircleRadius(Number(e.target.value))}
-          />
-          <code>{`circleRadius={${circleRadius}}`}</code>
-        </label>
-      </div>
-    </>
   );
 };
--- packages/docs/components/demos/index.tsx
+++ packages/docs/components/demos/index.tsx
@@ -1,12 +1,122 @@
-import React from "react";
-import { NoiseDemo } from "./NoiseDemo";
+import { Player } from "@remotion/player";
+import React, { useMemo, useState } from "react";
+import { NoiseComp } from "./NoiseDemo";
+
+type DemoType = {
+  id: string;
+  comp: React.FC;
+  compWidth: number;
+  compHeight: number;
+  fps: number;
+  durationInFrames: number;
+};
+
+const noiseDemo: DemoType = {
+  comp: NoiseComp,
+  compHeight: 720,
+  compWidth: 1280,
+  durationInFrames: 150,
+  fps: 30,
+  id: "noise",
+};
+
+const container: React.CSSProperties = {
+  overflow: "hidden",
+  width: "100%",
+  border: "1px solid var(--ifm-color-emphasis-300)",
+  borderRadius: "var(--ifm-pre-border-radius)",
+};
+
+const demos: DemoType[] = [noiseDemo];
 
 export const Demo: React.FC<{
-  type: "noise";
+  type: string;
 }> = ({ type }) => {
-  if (type === "noise") {
-    return <NoiseDemo />;
+  const [maxOffset, setMaxOffset] = useState(50);
+  const [speed, setSpeed] = useState(0.01);
+  const [circleRadius, setCircleRadius] = useState(5);
+
+  const demo = demos.find((d) => d.id === type);
+
+  if (!demo) {
+    throw new Error("Demo not found");
   }
 
-  throw new Error(`Unknown demo type: ${type}`);
+  const inputStyle = useMemo<React.CSSProperties>(
+    () => ({
+      width: 90,
+      marginRight: 8,
+    }),
+    []
+  );
+  const labelStyle = useMemo<React.CSSProperties>(
+    () => ({
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      margin: 8,
+    }),
+    []
+  );
+
+  return (
+    <div style={container}>
+      <Player
+        component={demo.comp}
+        compositionWidth={demo.compWidth}
+        compositionHeight={demo.compHeight}
+        durationInFrames={demo.durationInFrames}
+        fps={demo.fps}
+        style={{
+          width: "100%",
+          aspectRatio: demo.compWidth / demo.compHeight,
+        }}
+        inputProps={{
+          maxOffset,
+          speed,
+          circleRadius,
+        }}
+        autoPlay
+        loop
+      />
+      <div>
+        <label style={labelStyle}>
+          <input
+            type="range"
+            min={0.001}
+            max={0.025}
+            step={0.001}
+            value={speed}
+            style={inputStyle}
+            onChange={(e) => setSpeed(Number(e.target.value))}
+          />
+          <code>{`speed={${speed}}`}</code>
+        </label>
+        <label style={labelStyle}>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            step={1}
+            value={maxOffset}
+            style={inputStyle}
+            onChange={(e) => setMaxOffset(Number(e.target.value))}
+          />
+          <code>{`maxOffset={${maxOffset}}`}</code>
+        </label>
+        <label style={labelStyle}>
+          <input
+            type="range"
+            min={2}
+            max={20}
+            step={1}
+            value={circleRadius}
+            style={inputStyle}
+            onChange={(e) => setCircleRadius(Number(e.target.value))}
+          />
+          <code>{`circleRadius={${circleRadius}}`}</code>
+        </label>
+      </div>
+    </div>
+  );
 };
