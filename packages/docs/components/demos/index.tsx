9a30390bacd79cf79dfb6702ba58cec942b9aad5
Jonny Burger <jonathanburger11@gmail.com>
2023-01-18 15:20:48 +0100

    real generic demo
    Former-commit-id: 9a927df5095735abf956a5b1c04e3de839349079

--- packages/docs/components/demos/index.tsx
+++ packages/docs/components/demos/index.tsx
@@ -1,24 +1,8 @@
 import { Player } from "@remotion/player";
-import React, { useMemo, useState } from "react";
-import { NoiseComp } from "./NoiseDemo";
-
-type DemoType = {
-  id: string;
-  comp: React.FC;
-  compWidth: number;
-  compHeight: number;
-  fps: number;
-  durationInFrames: number;
-};
-
-const noiseDemo: DemoType = {
-  comp: NoiseComp,
-  compHeight: 720,
-  compWidth: 1280,
-  durationInFrames: 150,
-  fps: 30,
-  id: "noise",
-};
+import React, { useState } from "react";
+import { Control } from "./control";
+import type { DemoType } from "./types";
+import { noiseDemo } from "./types";
 
 const container: React.CSSProperties = {
   overflow: "hidden",
@@ -32,33 +16,20 @@ const demos: DemoType[] = [noiseDemo];
 export const Demo: React.FC<{
   type: string;
 }> = ({ type }) => {
-  const [maxOffset, setMaxOffset] = useState(50);
-  const [speed, setSpeed] = useState(0.01);
-  const [circleRadius, setCircleRadius] = useState(5);
-
   const demo = demos.find((d) => d.id === type);
 
   if (!demo) {
     throw new Error("Demo not found");
   }
 
-  const inputStyle = useMemo<React.CSSProperties>(
-    () => ({
-      width: 90,
-      marginRight: 8,
-    }),
-    []
+  const [state, setState] = useState(() =>
+    demo.options
+      .map((o) => [o.name, o.default] as const)
+      .reduce((a, b) => {
+        a[b[0]] = b[1];
+        return a;
+      }, {})
   );
-  const labelStyle = useMemo<React.CSSProperties>(
-    () => ({
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      margin: 8,
-    }),
-    []
-  );
-
   return (
     <div style={container}>
       <Player
@@ -71,51 +42,26 @@ export const Demo: React.FC<{
           width: "100%",
           aspectRatio: demo.compWidth / demo.compHeight,
         }}
-        inputProps={{
-          maxOffset,
-          speed,
-          circleRadius,
-        }}
+        inputProps={state}
         autoPlay
         loop
       />
       <div>
-        <label style={labelStyle}>
-          <input
-            type="range"
-            min={0.001}
-            max={0.025}
-            step={0.001}
-            value={speed}
-            style={inputStyle}
-            onChange={(e) => setSpeed(Number(e.target.value))}
-          />
-          <code>{`speed={${speed}}`}</code>
-        </label>
-        <label style={labelStyle}>
-          <input
-            type="range"
-            min={0}
-            max={100}
-            step={1}
-            value={maxOffset}
-            style={inputStyle}
-            onChange={(e) => setMaxOffset(Number(e.target.value))}
-          />
-          <code>{`maxOffset={${maxOffset}}`}</code>
-        </label>
-        <label style={labelStyle}>
-          <input
-            type="range"
-            min={2}
-            max={20}
-            step={1}
-            value={circleRadius}
-            style={inputStyle}
-            onChange={(e) => setCircleRadius(Number(e.target.value))}
-          />
-          <code>{`circleRadius={${circleRadius}}`}</code>
-        </label>
+        {demo.options.map((option) => {
+          return (
+            <Control
+              key={option.name}
+              option={option}
+              value={state[option.name]}
+              setValue={(value) => {
+                setState((s) => ({
+                  ...s,
+                  [option.name]: value,
+                }));
+              }}
+            />
+          );
+        })}
       </div>
     </div>
   );
