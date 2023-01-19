78e013612ca40304ffcf5092b212202f1488db16
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-01-20 02:50:27 +1100

    feat: include example render execution
    Former-commit-id: f7a6c7ff23aa6f2c59ddb6b88580ba80914b8d9c

--- packages/example-lambda/resources/render_function.ts
+++ packages/example-lambda/resources/render_function.ts
@@ -1,14 +1,22 @@
 import { Handler } from "aws-lambda";
+import { renderMediaOnLambda } from "@remotion/lambda/client";
 
 export const handler: Handler = async (event: any = {}): Promise<any> => {
-  //console.log(event);
-
-  //return { statusCode: 201, body: "Hello world!" };
+  const { bucketName, renderId } = await renderMediaOnLambda({
+    region: "us-east-1",
+    functionName: "remotion-render-bds9aab",
+    composition: "MyVideo",
+    serveUrl:
+      "https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/bf2jrbfkw",
+    codec: "h264",
+  });
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "success",
+      isSuccess: true,
+      bucketName,
+      renderId,
     }),
   };
 };
