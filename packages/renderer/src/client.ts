3838b0b6b58b4bdd5c9831fecefccbb28bcda060
Jonny Burger <jonathanburger11@gmail.com>
2023-01-19 16:22:52 +0100

    as codecs
    Former-commit-id: ea44c4bcea4054669596404a64d8210216f5bc3f

--- packages/renderer/src/client.ts
+++ packages/renderer/src/client.ts
@@ -1,5 +1,7 @@
+import {validCodecs} from './codec';
 import {getFileExtensionFromCodec} from './get-extension-from-codec';
 
 export const BrowserSafeApis = {
 	getFileExtensionFromCodec,
+	validCodecs,
 };
