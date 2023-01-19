0bc43ec0938d09877e9495f1c29ea2c5dc4abd84
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-01-19 19:26:25 +1100

    prioritise credentials that has AWS Session token

--- packages/lambda/src/shared/aws-clients.ts
+++ packages/lambda/src/shared/aws-clients.ts
@@ -45,23 +45,24 @@ const getCredentials = ():
 
 	if (
 		process.env.REMOTION_AWS_ACCESS_KEY_ID &&
-		process.env.REMOTION_AWS_SECRET_ACCESS_KEY
+		process.env.REMOTION_AWS_SECRET_ACCESS_KEY &&
+		process.env.REMOTION_AWS_SESSION_TOKEN
 	) {
+		console.log('Using credentials from Remotion assumed role.');
 		return {
 			accessKeyId: process.env.REMOTION_AWS_ACCESS_KEY_ID,
 			secretAccessKey: process.env.REMOTION_AWS_SECRET_ACCESS_KEY,
+			sessionToken: process.env.REMOTION_AWS_SESSION_TOKEN,
 		};
 	}
 
 	if (
 		process.env.REMOTION_AWS_ACCESS_KEY_ID &&
-		process.env.REMOTION_AWS_SECRET_ACCESS_KEY &&
-		process.env.REMOTION_AWS_SESSION_TOKEN
+		process.env.REMOTION_AWS_SECRET_ACCESS_KEY
 	) {
 		return {
 			accessKeyId: process.env.REMOTION_AWS_ACCESS_KEY_ID,
 			secretAccessKey: process.env.REMOTION_AWS_SECRET_ACCESS_KEY,
-			sessionToken: process.env.REMOTION_AWS_SESSION_TOKEN,
 		};
 	}
 
@@ -71,22 +72,23 @@ const getCredentials = ():
 		});
 	}
 
-	if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
-		return {
-			accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-		};
-	}
-
 	if (
 		process.env.AWS_ACCESS_KEY_ID &&
 		process.env.AWS_SECRET_ACCESS_KEY &&
 		process.env.AWS_SESSION_TOKEN
 	) {
+		console.log('Using credentials from AWS STS');
 		return {
 			accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
 			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
 			sessionToken: process.env.AWS_SESSION_TOKEN as string,
+		};
+	}
+
+	if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+		return {
+			accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
 		};
 	}
 
