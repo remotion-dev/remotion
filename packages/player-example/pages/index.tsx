02585238db429930beb4b2f0db0eec3227a1afb7
Jonny Burger <jonathanburger11@gmail.com>
2023-01-20 15:44:55 +0100

    use next.js for player-example
    Former-commit-id: 136d99248556dd2a9944780fcb329cd26b10ceb5

---
+++ packages/player-example/pages/index.tsx
@@ -0,0 +1,51 @@
+import {Thumbnail} from '@remotion/player';
+import React, {ComponentType} from 'react';
+import App from '../src/App';
+import CarSlideshow from '../src/CarSlideshow';
+import {ThumbnailDemo} from '../src/ThumbnailDemo';
+import {VideoautoplayDemo} from '../src/VideoAutoplay';
+
+const Car = () =>
+	new Promise<{default: ComponentType<unknown>}>((resolve) => {
+		setTimeout(
+			() =>
+				resolve({
+					default: CarSlideshow as ComponentType<unknown>,
+				}),
+			3000
+		);
+	});
+
+export default function () {
+	return (
+		<React.StrictMode>
+			<h2>Player</h2>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+				}}
+			>
+				<App lazyComponent={Car} durationInFrames={500} />
+				<App component={VideoautoplayDemo} durationInFrames={2700} />
+			</div>
+			<h2>Thumbnail</h2>
+			<Thumbnail
+				lazyComponent={Car}
+				frameToDisplay={480}
+				compositionHeight={200}
+				compositionWidth={500}
+				durationInFrames={5000}
+				fps={30}
+				style={{
+					border: '4px solid red',
+				}}
+				inputProps={{
+					title: 'Hi there',
+				}}
+			/>
+			<ThumbnailDemo />
+		</React.StrictMode>
+	);
+}
--- packages/player-example/src/index.tsx
+++ packages/player-example/src/index.tsx
@@ -8,47 +8,4 @@ import {ThumbnailDemo} from './ThumbnailDemo';
 
 const rootElement = document.getElementById('root');
 
-const Car = () =>
-	new Promise<{default: ComponentType<unknown>}>((resolve) => {
-		setTimeout(
-			() =>
-				resolve({
-					default: CarSlideshow as ComponentType<unknown>,
-				}),
-			3000
-		);
-	});
-
-createRoot(rootElement as HTMLElement).render(
-	<div style={{}}>
-		<React.StrictMode>
-			<h2>Player</h2>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'center',
-				}}
-			>
-				<App lazyComponent={Car} durationInFrames={500} />
-				<App component={VideoautoplayDemo} durationInFrames={2700} />
-			</div>
-			<h2>Thumbnail</h2>
-			<Thumbnail
-				lazyComponent={Car}
-				frameToDisplay={480}
-				compositionHeight={200}
-				compositionWidth={500}
-				durationInFrames={5000}
-				fps={30}
-				style={{
-					border: '4px solid red',
-				}}
-				inputProps={{
-					title: 'Hi there',
-				}}
-			/>
-			<ThumbnailDemo />
-		</React.StrictMode>
-	</div>
-);
+createRoot(rootElement as HTMLElement).render(<div style={{}}></div>);
