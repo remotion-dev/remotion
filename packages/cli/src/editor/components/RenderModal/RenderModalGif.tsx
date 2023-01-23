45e3e5c79efc64aad948553442c158b98e923924
Jonny Burger <jonathanburger11@gmail.com>
2023-01-23 17:32:21 +0100

    gif tab

---
+++ packages/cli/src/editor/components/RenderModal/RenderModalGif.tsx
@@ -0,0 +1,56 @@
+import type {ChangeEvent} from 'react';
+import React, {useCallback} from 'react';
+import {Checkbox} from '../Checkbox';
+import {label, optionRow, rightRow} from './layout';
+import {NumberOfLoopsSetting} from './NumberOfLoopsSetting';
+import {NumberSetting} from './NumberSetting';
+
+export const RenderModalGif: React.FC<{
+	limitNumberOfGifLoops: boolean;
+	setLimitNumberOfGifLoops: (value: React.SetStateAction<boolean>) => void;
+	numberOfGifLoopsSetting: number;
+	setNumberOfGifLoopsSetting: React.Dispatch<React.SetStateAction<number>>;
+	everyNthFrame: number;
+	setEveryNthFrameSetting: React.Dispatch<React.SetStateAction<number>>;
+}> = ({
+	everyNthFrame,
+	limitNumberOfGifLoops,
+	numberOfGifLoopsSetting,
+	setEveryNthFrameSetting,
+	setLimitNumberOfGifLoops,
+	setNumberOfGifLoopsSetting,
+}) => {
+	const onShouldLimitNumberOfGifLoops = useCallback(
+		(e: ChangeEvent<HTMLInputElement>) => {
+			setLimitNumberOfGifLoops(e.target.checked);
+		},
+		[setLimitNumberOfGifLoops]
+	);
+
+	return (
+		<div>
+			<NumberSetting
+				name="Every nth frame"
+				min={1}
+				onValueChanged={setEveryNthFrameSetting}
+				value={everyNthFrame}
+				step={1}
+			/>
+			<div style={optionRow}>
+				<div style={label}>Limit GIF loops</div>
+				<div style={rightRow}>
+					<Checkbox
+						checked={limitNumberOfGifLoops}
+						onChange={onShouldLimitNumberOfGifLoops}
+					/>
+				</div>
+			</div>
+			{limitNumberOfGifLoops ? (
+				<NumberOfLoopsSetting
+					numberOfGifLoops={numberOfGifLoopsSetting}
+					setNumberOfGifLoops={setNumberOfGifLoopsSetting}
+				/>
+			) : null}
+		</div>
+	);
+};
