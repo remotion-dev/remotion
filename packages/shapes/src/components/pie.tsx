4b01e0444210167f43cf7ca5767d8ddb19b22c26
Jonny Burger <jonathanburger11@gmail.com>
2023-01-28 10:58:40 +0100

    sketch out pie component

---
+++ packages/shapes/src/components/pie.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import type {MakePieProps} from '../utils/make-pie';
+import {makePie} from '../utils/make-pie';
+import type {AllShapesProps} from './render-svg';
+import {RenderSvg} from './render-svg';
+
+export type PieProps = MakePieProps & AllShapesProps;
+
+export const Pie: React.FC<PieProps> = ({radius, fillAmount, ...props}) => {
+	return <RenderSvg {...makePie({radius, fillAmount})} {...props} />;
+};
