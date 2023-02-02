22d376653bf7d2ee6eb40bfc5827f633936fd768
Jonny Burger <jonathanburger11@gmail.com>
2023-02-02 10:46:07 +0100

    mix references for better testing
    Former-commit-id: d9f2de801c81cb7d3c2863a9dffd02122e9a6fec

--- packages/example/remotion.config.js
+++ packages/example/remotion.config.js
@@ -1,4 +1,4 @@
 import {Config} from 'remotion';
 
-Config.Rendering.setConcurrency(require('os').cpus().length);
+Config.setConcurrency(require('os').cpus().length);
 Config.Output.setOverwriteOutput(true);
