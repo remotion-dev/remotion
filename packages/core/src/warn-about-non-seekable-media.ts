e4a99a55be1f427453b419efc58c41df8673c53d
Jonny Burger <jonathanburger11@gmail.com>
2023-02-03 10:37:59 +0100

    ramp up the docs

--- packages/core/src/warn-about-non-seekable-media.ts
+++ packages/core/src/warn-about-non-seekable-media.ts
@@ -25,7 +25,7 @@ export const warnAboutNonSeekableMedia = (
 	const range = {start: ref.seekable.start(0), end: ref.seekable.end(0)};
 
 	if (range.start === 0 && range.end === 0) {
-		const msg = `The media does not support seeking. Remotion cannot properly render it. Please see https://remotion.dev/docs/non-seekable-media for assistance. Source: ${ref.src}`;
+		const msg = `The media ${ref.src} cannot be seeked. This could be one of two reasons: 1) The media resource was replaced while the video is playing but it was not loaded yet. 2) The media does not support seeking. Please see https://remotion.dev/docs/non-seekable-media for assistance.`;
 
 		if (type === 'console-error') {
 			console.error(msg);
