f0eaa324ae9a13993dbf5903a2bbfda620a3adca
Jonny Burger <jonathanburger11@gmail.com>
2023-02-06 11:36:25 +0100

    fix passing a serve URL to `npx remotion render` breaking

---
+++ packages/cli/src/test/find-entry-point.test.ts
@@ -0,0 +1,38 @@
+import path from 'path';
+import {expect, test} from 'vitest';
+import {findEntryPoint} from '../entry-point';
+
+test('Should accept URL as entry point', () => {
+	const entryPoint = findEntryPoint(
+		['https://www.example.com'],
+		path.resolve(process.cwd(), '..', 'example')
+	);
+
+	expect(entryPoint.file).toBe('https://www.example.com');
+	expect(entryPoint.remainingArgs).toEqual([]);
+	expect(entryPoint.reason).toEqual('argument passed');
+});
+
+test('Should find entry point automatically', () => {
+	const entryPoint = findEntryPoint(
+		[],
+		path.resolve(process.cwd(), '..', 'example')
+	);
+
+	expect(entryPoint.file).toBe('src/index.ts');
+	expect(entryPoint.remainingArgs).toEqual([]);
+	expect(entryPoint.reason).toEqual('common paths');
+});
+
+test('Should use explicit entry point', () => {
+	const entryPoint = findEntryPoint(
+		['src/ts-entry.tsx'],
+		path.resolve(process.cwd(), '..', 'example')
+	);
+
+	expect(entryPoint.file).toBe(
+		'/Users/jonathanburger/remotion/packages/example/src/ts-entry.tsx'
+	);
+	expect(entryPoint.remainingArgs).toEqual([]);
+	expect(entryPoint.reason).toEqual('argument passed - found in root');
+});
