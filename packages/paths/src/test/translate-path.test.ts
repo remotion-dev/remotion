e5eb2833c1937172cd01321379f453f505049862
Jonny Burger <jonathanburger11@gmail.com>
2023-02-06 15:12:46 +0100

    fix some tests
    Former-commit-id: f538bf1de0ef8d45de268d005f67d08bcbc32e73

--- packages/paths/src/test/translate-path.test.ts
+++ packages/paths/src/test/translate-path.test.ts
@@ -4,13 +4,13 @@ import {translatePath} from '../translate-path';
 test('Should be able to translate simple path, update x', () => {
 	const path = translatePath('M10 10 L15 15', 10, 0);
 
-	expect(path).toEqual('M 20,10 L 25,15');
+	expect(path).toEqual('M 20 10 L 25 15');
 });
 
 test('Should be able to translate simple path, update x and y', () => {
 	const path = translatePath('M10 10 L15 15', 10, 10);
 
-	expect(path).toEqual('M 20,20 L 25,25');
+	expect(path).toEqual('M 20 20 L 25 25');
 });
 
 test('Should be able to translate circle path, update x', () => {
@@ -20,7 +20,7 @@ test('Should be able to translate circle path, update x', () => {
 		0
 	);
 
-	expect(path).toEqual('M 45,50 a 25,25,0,1,1,50,0 a 25,25,0,1,1,-50,0');
+	expect(path).toEqual('M 45 50 a 25 25 0 1 1 50 0 a 25 25 0 1 1 -50 0');
 });
 
 test('Should be able to translate circle path, update x and y', () => {
@@ -30,13 +30,13 @@ test('Should be able to translate circle path, update x and y', () => {
 		20
 	);
 
-	expect(path).toEqual('M 45,70 a 25,25,0,1,1,50,0 a 25,25,0,1,1,-50,0');
+	expect(path).toEqual('M 45 70 a 25 25 0 1 1 50 0 a 25 25 0 1 1 -50 0');
 });
 
 test('Should be able to translate example path, update x', () => {
 	const path = translatePath('M 50 50 L 150 50', 10, 0);
 
-	expect(path).toEqual('M 60,50 L 160,50');
+	expect(path).toEqual('M 60 50 L 160 50');
 });
 
 test('Translation should throw error', () => {
