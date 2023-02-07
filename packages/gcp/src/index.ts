ad9e012d01e2109bab65b2c5a86371364c1f83c9
Matt Mcgillivray <m.mcgillivray@outlook.com>
2023-02-07 11:26:22 +0800

    move functions into one container
    Former-commit-id: a46df68fec2215abc50a5d1c343fb02b8092e8c8

--- packages/gcp/src/index.ts
+++ packages/gcp/src/index.ts
@@ -1,4 +1,18 @@
+import * as ff from '@google-cloud/functions-framework';
 import { renderMediaSingleThread } from "./functions/render-media-single-thread";
 import { renderStillSingleThread } from "./functions/render-still-single-thread";
 
-export { renderMediaSingleThread, renderStillSingleThread}
+export const renderOnCloudRun = async (req: ff.Request, res: ff.Response) => {
+  const renderType = req.body.type;
+
+  switch (renderType) {
+    case 'media':
+      await renderMediaSingleThread(req, res);
+      break;
+    case 'still':
+      await renderStillSingleThread(req, res);
+      break;
+    default:
+      res.status(400).send('Invalid render type, must be either "media" or "still"');
+  }
+};
