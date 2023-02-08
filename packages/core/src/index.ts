2f544affd95ce06b212810f03c0f31b436cf2656
kaf-lamed-beyt <belac335@gmail.com>
2023-02-08 14:06:50 +0100

    feat: add cancelRender demo in test-bed
    Former-commit-id: 3006633b1ed07095723e7e61d03e8264bf992c65

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -89,6 +89,7 @@ export * from './use-video-config';
 export * from './version';
 export * from './video';
 export * from './video-config';
+export * from "./cancel-render";
 
 export const Experimental = {
 	Clipper,
