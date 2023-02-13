e02013fd54618e4e3869cc26cca92f722dafe3ea
Jonny Burger <jonathanburger11@gmail.com>
2023-02-13 18:44:21 +0100

    fix text label flickering

--- packages/player/src/use-video-controls-resize.ts
+++ packages/player/src/use-video-controls-resize.ts
@@ -3,20 +3,19 @@ import {fullscreenIconSize, ICON_SIZE} from './icons';
 import {VOLUME_SLIDER_WIDTH} from './MediaVolumeSlider';
 import {X_PADDING, X_SPACER} from './PlayerControls';
 
+type Info = {
+	maxTimeLabelWidth: number | null;
+	displayVerticalVolumeSlider: boolean;
+};
+
 export const useVideoControlsResize = ({
 	allowFullscreen: allowFullScreen,
 	playerWidth,
 }: {
 	allowFullscreen: boolean;
 	playerWidth: number;
-}): {
-	maxTimeLabelWidth: number;
-	displayVerticalVolumeSlider: boolean;
-} => {
-	const resizeInfo = useMemo((): {
-		maxTimeLabelWidth: number;
-		displayVerticalVolumeSlider: boolean;
-	} => {
+}): Info => {
+	const resizeInfo = useMemo((): Info => {
 		const playPauseIconSize = ICON_SIZE;
 		const volumeIconSize = ICON_SIZE;
 		const _fullscreenIconSize = allowFullScreen ? fullscreenIconSize : 0;
@@ -51,7 +50,10 @@ export const useVideoControlsResize = ({
 			playerWidth < minWidthForHorizontalDisplay;
 
 		return {
-			maxTimeLabelWidth: maxTimeLabelWidthWithoutNegativeValue,
+			maxTimeLabelWidth:
+				maxTimeLabelWidthWithoutNegativeValue === 0
+					? null
+					: maxTimeLabelWidthWithoutNegativeValue,
 			displayVerticalVolumeSlider,
 		};
 	}, [allowFullScreen, playerWidth]);
