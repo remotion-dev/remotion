b61fc6f3e4e642f2d1632621334ce56b00d03f28
Jonny Burger <jonathanburger11@gmail.com>
2023-02-13 10:57:49 +0100

    accept error or string

--- packages/core/src/cancel-render.ts
+++ packages/core/src/cancel-render.ts
@@ -1,3 +1,22 @@
-export function cancelRender(): never {
-  throw new Error("Rendering cancelled")
+const isErrorLink = (err: unknown) => {
+	return (
+		typeof err === 'object' &&
+		err !== null &&
+		'stack' in err &&
+		typeof err.stack === 'string' &&
+		'message' in err &&
+		typeof err.message === 'string'
+	);
+};
+
+export function cancelRender(err: unknown): never {
+	if (isErrorLink(err)) {
+		throw err;
+	}
+
+	if (typeof err === 'string') {
+		throw new Error(err);
+	}
+
+	throw new Error('Rendering was cancelled');
 }
