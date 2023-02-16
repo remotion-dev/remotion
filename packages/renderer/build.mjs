1631198563db15f55c47d8b1383024f0093e8ba6
Jonny Burger <jonathanburger11@gmail.com>
2023-02-16 17:25:53 +0100

    add libavdevice and get it to work on macos
    Former-commit-id: 5c72024bbbb42d6bf15280fb2d98d08df585d1ba

--- packages/renderer/build.mjs
+++ packages/renderer/build.mjs
@@ -12,13 +12,13 @@ function isMusl() {
 }
 
 const targets = [
-	'x86_64-pc-windows-gnu',
-	'x86_64-apple-darwin',
 	'aarch64-apple-darwin',
+	'x86_64-apple-darwin',
 	'x86_64-unknown-linux-musl',
 	'x86_64-unknown-linux-gnu',
 	'aarch64-unknown-linux-musl',
 	'aarch64-unknown-linux-gnu',
+	'x86_64-pc-windows-gnu',
 ];
 
 export const getTarget = () => {
