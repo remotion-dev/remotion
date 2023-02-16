4da1550b614b191f1153d8bee5b47183e5093452
Jonny Burger <jonathanburger11@gmail.com>
2023-02-16 10:14:11 +0100

    Update renderer.ts
    Former-commit-id: 976d4a8c6e499fe71b82a02eb18ec189a488ae06

--- packages/lambda/src/functions/renderer.ts
+++ packages/lambda/src/functions/renderer.ts
@@ -217,13 +217,6 @@ const renderHandler = async (
 			enforceAudioTrack: true,
 			audioBitrate: params.audioBitrate,
 			videoBitrate: params.videoBitrate,
-			onSlowestFrames: (slowestFrames) => {
-				console.log();
-				console.log(`Slowest frames:`);
-				slowestFrames.forEach(({frame, time}) => {
-					console.log(`Frame ${frame} (${time.toFixed(3)}ms)`);
-				});
-			},
 			// Lossless flag takes priority over audio codec
 			// https://github.com/remotion-dev/remotion/issues/1647
 			// Special flag only in Lambda renderer which improves the audio quality
@@ -231,7 +224,14 @@ const renderHandler = async (
 			preferLossless: true,
 			browserExecutable: executablePath(),
 		})
-			.then(() => resolve())
+			.then(({slowestFrames}) => {
+				console.log();
+				console.log(`Slowest frames:`);
+				slowestFrames.forEach(({frame, time}) => {
+					console.log(`Frame ${frame} (${time.toFixed(3)}ms)`);
+				});
+				resolve();
+			})
 			.catch((err) => reject(err));
 	});
 
