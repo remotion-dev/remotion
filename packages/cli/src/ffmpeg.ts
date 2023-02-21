49f04c46e7c68bfd8418c31d1330e62253ccbbd2
Jonny Burger <jonathanburger11@gmail.com>
2023-02-21 19:40:18 +0100

    align ffmpeg command
    Former-commit-id: 40a712c67f6b1675049be4407b74a8d26211cef9

--- packages/cli/src/ffmpeg.ts
+++ packages/cli/src/ffmpeg.ts
@@ -2,10 +2,11 @@ import {RenderInternals} from '@remotion/renderer';
 import {spawnSync} from 'child_process';
 
 export const ffmpegCommand = (_root: string, args: string[]) => {
-	spawnSync(RenderInternals.getExecutablePath('ffmpeg'), args, {
+	const done = spawnSync(RenderInternals.getExecutablePath('ffmpeg'), args, {
 		...RenderInternals.callFfExtraOptions(),
 		stdio: 'inherit',
 	});
+	process.exit(done.status as number);
 };
 
 export const ffprobeCommand = (_root: string, args: string[]) => {
