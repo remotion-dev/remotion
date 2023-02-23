278ba73be56fea52187e385a611ba823c9aeb528
kaf-lamed-beyt <belac335@gmail.com>
2023-02-23 14:02:53 +0100

    chore: ref doc links from @remotion/path: getLength() through @remotion/shapes

--- packages/paths/src/serialize-instructions.ts
+++ packages/paths/src/serialize-instructions.ts
@@ -1,5 +1,11 @@
 import type {Instruction} from './helpers/types';
 
+/**
+ * @description Takes an array of Instruction's and serializes it into an SVG path string.
+ * @param {Array} instruction 
+ * @returns a serialized SVG path string
+ * @see [Documentation](https://www.remotion.dev/docs/paths/serialize-instructions)
+ */
 const serializeInstruction = (instruction: Instruction): string => {
 	if (instruction.type === 'A') {
 		return `A ${instruction.rx} ${instruction.ry} ${
--- packages/shapes/src/utils/make-star.ts
+++ packages/shapes/src/utils/make-star.ts
@@ -23,6 +23,15 @@ export type StarProps = {
 	cornerRadius: number;
 };
 
+/**
+ * @description Generates a star SVG path.
+ * @param {Number} innerRadius The inner radius of the star.
+ * @param {Number} outerRadius The outer radius of the star.
+ * @param {Number} points The amount of points of the star.
+ * @param {Number} cornerRadius Rounds the corner using an arc. Similar to CSS's border-radius. Cannot be used together with edgeRoundness.
+ * @param {null|Number} edgeRoundness Allows to modify the shape by rounding the edges using bezier curves. Default null.
+ * @see [Documentation](https://www.remotion.dev/docs/shapes/make-star)
+ */
 const star = ({
 	centerX,
 	centerY,
