8eccfe9c749748abecd0f805400c87637fc66148
Jonny Burger <jonathanburger11@gmail.com>
2023-02-24 09:12:19 +0100

    add eslint
    Former-commit-id: 9c2a82c6e50cf2ab360e3b291bede2c5b7b8c72b

--- packages/gcp/src/shared/make-storage-url.ts
+++ packages/gcp/src/shared/make-storage-url.ts
@@ -1,11 +1,9 @@
-import crypto from 'crypto';
-
 export const makeStorageServeUrl = ({
-	bucketName,
-	subFolder,
+  bucketName,
+  subFolder,
 }: {
-	bucketName: string;
-	subFolder: string;
+  bucketName: string;
+  subFolder: string;
 }): string => {
-	return `https://storage.googleapis.com/${bucketName}/${subFolder}/index.html`;
+  return `https://storage.googleapis.com/${bucketName}/${subFolder}/index.html`;
 };
