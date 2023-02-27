84823e6a6b1a657aadf0aaaed492c17761819650
Jonny Burger <jonathanburger11@gmail.com>
2023-02-27 14:05:50 +0100

    works with ESM wallaby
    Former-commit-id: 8887e99b6dc9bd4c856267ac3027acc1b9c4097a

--- packages/astro-example/test.mjs
+++ packages/astro-example/test.mjs
@@ -1,10 +1,12 @@
 import assert from "assert";
 import { interpolate } from "remotion";
 import { Player } from "@remotion/player";
+import { VERSION } from "remotion/version";
 
 const val = interpolate(1, [0, 1], [0, 100]);
 
 assert(val === 100);
 assert(Boolean(Player));
+assert(typeof VERSION === "string");
 
 console.log("ESM works!");
