9c9f73d097d4e3c7ef58a1e89013e50280ccac42
Jonny Burger <jonathanburger11@gmail.com>
2023-02-28 15:39:27 +0100

    rename the multiarchitecture hosted layers to __internal_doNotUsehostedLayers

--- packages/lambda/src/test/unit/hosted-layers-match.test.ts
+++ packages/lambda/src/test/unit/hosted-layers-match.test.ts
@@ -1,13 +1,15 @@
 import {expect, test} from 'vitest';
 import type {LambdaArchitecture} from '../..';
 import {
-	hostedLayers,
 	REMOTION_HOSTED_LAYER_ARN,
+	__internal_doNotUsehostedLayers,
 } from '../../shared/hosted-layers';
 
 test('All hosted layers should match ARN', () => {
-	Object.keys(hostedLayers).forEach((arch) => {
-		Object.values(hostedLayers[arch as LambdaArchitecture]).forEach((h) => {
+	Object.keys(__internal_doNotUsehostedLayers).forEach((arch) => {
+		Object.values(
+			__internal_doNotUsehostedLayers[arch as LambdaArchitecture]
+		).forEach((h) => {
 			h.forEach(({layerArn}) => {
 				expect(layerArn).toMatch(
 					new RegExp(REMOTION_HOSTED_LAYER_ARN.replace(/\*/g, '(.*)'))
