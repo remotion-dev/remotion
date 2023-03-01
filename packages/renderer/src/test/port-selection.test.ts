f18c369dbd4ce15a0f19b24300b8966f39aa9910
Jonny Burger <jonathanburger11@gmail.com>
2023-03-01 11:25:38 +0100

    clean lambda bundles
    Former-commit-id: 1d74cfb1493d7bd4e5716c23374ea19e25ec17bd

--- packages/renderer/src/test/port-selection.test.ts
+++ packages/renderer/src/test/port-selection.test.ts
@@ -4,8 +4,8 @@ import {getDesiredPort} from '../get-port';
 test('Port selection should only be freed once the previous result has been used', async () => {
 	let ports = 0;
 
-	const {port, didUsePort} = await getDesiredPort(undefined, 3000, 3100);
-	const secondPort = getDesiredPort(port, 3000, 3100).then(() => ports++);
+	const {port, didUsePort} = await getDesiredPort(undefined, 3100, 3200);
+	const secondPort = getDesiredPort(port, 3100, 3200).then(() => ports++);
 
 	await new Promise<void>((resolve) => {
 		setTimeout(resolve, 2000);
