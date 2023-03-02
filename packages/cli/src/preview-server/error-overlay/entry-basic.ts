a67d22b985b45f7b4d741977da4058d966a063b7
Jonny Burger <jonathanburger11@gmail.com>
2023-03-02 14:13:22 +0100

    JS HMR
    Former-commit-id: 16ccddf6311cd3d6da55799e3a26917a0b60bf1d

--- packages/cli/src/preview-server/error-overlay/entry-basic.ts
+++ packages/cli/src/preview-server/error-overlay/entry-basic.ts
@@ -3,8 +3,8 @@ import {mountRemotionOverlay} from './remotion-overlay';
 import {setErrorsRef} from './remotion-overlay/Overlay';
 
 startReportingRuntimeErrors(() => {
-	if (module.hot) {
-		module.hot.addStatusHandler((status) => {
+	if (__webpack_module__.hot) {
+		__webpack_module__.hot.addStatusHandler((status) => {
 			if (status === 'apply') {
 				if (didUnmountReactApp()) {
 					return window.location.reload();
