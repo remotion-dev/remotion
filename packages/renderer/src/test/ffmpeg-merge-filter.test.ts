077efad7e6c314be880cb37ff63ab48f8531221b
Jonny Burger <jonathanburger11@gmail.com>
2023-03-02 21:14:24 +0100

    fix videos having no audio

--- packages/renderer/src/test/ffmpeg-merge-filter.test.ts
+++ packages/renderer/src/test/ffmpeg-merge-filter.test.ts
@@ -2,13 +2,7 @@ import {expect, test} from 'vitest';
 import {createFfmpegMergeFilter} from '../create-ffmpeg-merge-filter';
 
 test('FFMPEG merge filters', () => {
-	expect(createFfmpegMergeFilter(2)).toBe(
-		'[0:a][1:a]amix=inputs=2,pan=stereo[a]'
-	);
-	expect(createFfmpegMergeFilter(3)).toBe(
-		'[0:a][1:a]amix=inputs=3,pan=stereo[a]'
-	);
-	expect(createFfmpegMergeFilter(4)).toBe(
-		'[0:a][1:a]amix=inputs=4,pan=stereo[a]'
-	);
+	expect(createFfmpegMergeFilter(2)).toBe('[0:a][1:a]amix=inputs=2[a]');
+	expect(createFfmpegMergeFilter(3)).toBe('[0:a][1:a]amix=inputs=3[a]');
+	expect(createFfmpegMergeFilter(4)).toBe('[0:a][1:a]amix=inputs=4[a]');
 });
