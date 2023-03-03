55b8e6238a5fcd35e1427fe0c7810032346d1267
Jonny Burger <jonathanburger11@gmail.com>
2023-03-03 09:21:38 +0100

    Update create-ffmpeg-merge-filter.ts
    Former-commit-id: 051336c30cefd670043bae8226c3738d3efaf397

--- packages/renderer/src/create-ffmpeg-merge-filter.ts
+++ packages/renderer/src/create-ffmpeg-merge-filter.ts
@@ -1,3 +1,3 @@
 export const createFfmpegMergeFilter = (inputs: number) => {
-	return `[0:a][1:a]amix=inputs=${inputs}[a]`;
+	return `[0:a][1:a]amix=inputs=${inputs},dropout_transition=0,normalize=0[a]`;
 };
