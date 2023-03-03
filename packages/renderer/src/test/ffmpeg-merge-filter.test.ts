b81f18e4e6d5796411ae315938575c6d537c0f8f
Jonny Burger <jonathanburger11@gmail.com>
2023-03-03 17:59:30 +0100

    Update ffmpeg-merge-filter.test.ts

--- packages/renderer/src/test/ffmpeg-merge-filter.test.ts
+++ packages/renderer/src/test/ffmpeg-merge-filter.test.ts
@@ -26,6 +26,6 @@ test('FFMPEG merge filters', async () => {
 		filter ===
 			'[0:a]300,acopy[padded0];[1:a]600,acopy[padded1];[padded0][padded1]amix=inputs=2:dropout_transition=0:normalize=0[outputaudio]' ||
 			filter ===
-				'[0:a]300,acopy[padded0];[1:a]600,acopy[padded1];[padded0][padded1]amix=inputs=2:dropout_transition=0:sum=1[outputaudio]'
+				'[0:a]300,acopy[padded0];[1:a]600,acopy[padded1];[padded0][padded1]amix=inputs=2:dropout_transition=0[outputaudio]'
 	).toBeTruthy();
 });
