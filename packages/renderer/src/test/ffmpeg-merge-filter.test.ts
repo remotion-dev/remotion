938081586253d261054a4324bcde1ba8cdf23941
Jonny Burger <jonathanburger11@gmail.com>
2023-03-03 16:40:41 +0100

    Update ffmpeg-merge-filter.test.ts

--- packages/renderer/src/test/ffmpeg-merge-filter.test.ts
+++ packages/renderer/src/test/ffmpeg-merge-filter.test.ts
@@ -2,24 +2,26 @@ import {expect, test} from 'vitest';
 import {createFfmpegMergeFilter} from '../create-ffmpeg-merge-filter';
 
 test('FFMPEG merge filters', () => {
+	const filter = createFfmpegMergeFilter([
+		{
+			filter: {
+				pad_end: '300',
+				pad_start: null,
+			},
+			outName: '0.wav',
+		},
+		{
+			filter: {
+				pad_end: '600',
+				pad_start: null,
+			},
+			outName: '1.wav',
+		},
+	]);
 	expect(
-		createFfmpegMergeFilter([
-			{
-				filter: {
-					pad_end: '300',
-					pad_start: null,
-				},
-				outName: '0.wav',
-			},
-			{
-				filter: {
-					pad_end: '600',
-					pad_start: null,
-				},
-				outName: '1.wav',
-			},
-		])
-	).toBe(
-		'[0:a]300,acopy[padded0];[1:a]600,acopy[padded1];[padded0][padded1]amix=inputs=2:dropout_transition=0:normalize=0[outputaudio]'
+		filter ===
+			'[0:a]300,acopy[padded0];[1:a]600,acopy[padded1];[padded0][padded1]amix=inputs=2:dropout_transition=0:normalize=0[outputaudio]' ||
+			filter ===
+				'[0:a]300,acopy[padded0];[1:a]600,acopy[padded1];[padded0][padded1]amix=inputs=2:dropout_transition=0:sum=1[outputaudio]'
 	);
 });
