118f0f32b61fc04966b582d7bfc54ffbf7e77afd
Jonny Burger <jonathanburger11@gmail.com>
2023-03-05 20:50:07 +0100

    :(

--- packages/compositor-win32-x64-msvc/index.js
+++ packages/compositor-win32-x64-msvc/index.js
@@ -1,5 +1,17 @@
 const path = require("path");
 exports.binaryPath = path.resolve(__dirname, "compositor.exe");
-exports.ffmpegPath = path.resolve(__dirname "ffmpeg","remotion","bin","ffmpeg.exe");
-exports.ffprobePath = path.resolve(__dirname, "ffmpeg", "remotion", "bin", "ffprobe.exe");
+exports.ffmpegPath = path.resolve(
+  __dirname,
+  "ffmpeg",
+  "remotion",
+  "bin",
+  "ffmpeg.exe"
+);
+exports.ffprobePath = path.resolve(
+  __dirname,
+  "ffmpeg",
+  "remotion",
+  "bin",
+  "ffprobe.exe"
+);
 exports.ffmpegCwd = path.resolve(__dirname, "ffmpeg");
