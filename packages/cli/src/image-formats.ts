ef6499083c1901dfd2fc0edfc9feef18278a2f06
Jonny Burger <jonathanburger11@gmail.com>
2023-03-08 15:03:25 +0100

    fix --image-format while rendering

--- packages/cli/src/image-formats.ts
+++ packages/cli/src/image-formats.ts
@@ -1,14 +1,27 @@
 import type {VideoImageFormat} from '@remotion/renderer';
 import {RenderInternals} from '@remotion/renderer';
 import {ConfigInternals} from './config';
+import {parsedCli} from './parse-command-line';
 
 export const getVideoImageFormat = (
 	codec: ReturnType<typeof ConfigInternals.getOutputCodecOrUndefined>
 ): VideoImageFormat => {
-	const userPreferred = ConfigInternals.getUserPreferredVideoImageFormat();
+	const configFileOption = ConfigInternals.getUserPreferredVideoImageFormat();
 
-	if (typeof userPreferred !== 'undefined') {
-		return userPreferred;
+	if (typeof configFileOption !== 'undefined') {
+		return configFileOption;
+	}
+
+	if (typeof parsedCli['image-format'] !== 'undefined') {
+		if (
+			!(RenderInternals.validVideoImageFormats as readonly string[]).includes(
+				parsedCli['image-format'] as string
+			)
+		) {
+			throw new Error(`Invalid image format: ${parsedCli['image-format']}`);
+		}
+
+		return parsedCli['image-format'] as VideoImageFormat;
 	}
 
 	if (RenderInternals.isAudioCodec(codec)) {
