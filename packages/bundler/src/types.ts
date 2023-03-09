ccd5faeb807041b8d3f4791d26e0aea60a75d192
Jonny Burger <jonathanburger11@gmail.com>
2023-03-09 11:06:41 +0100

    revert moving webpackoverridefn to bundler

--- packages/bundler/src/types.ts
+++
@@ -1,7 +0,0 @@
-import webpack = require('webpack');
-
-export type WebpackConfiguration = webpack.Configuration;
-
-export type WebpackOverrideFn = (
-	currentConfiguration: WebpackConfiguration
-) => WebpackConfiguration;
