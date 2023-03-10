7fd1e2eda0f02bc680b9655c6ea5e9eaa7a6aa1a
Jonny Burger <jonathanburger11@gmail.com>
2023-03-10 12:54:55 +0100

    add test for monorepo exports field and make tests actually run
    Former-commit-id: 4b67bc4b424909560bdfd2bcadca8a6336cedb03

---
+++ packages/it-tests/src/monorepo/exports-field.test.ts
@@ -0,0 +1,31 @@
+import { existsSync, lstatSync, readdirSync, readFileSync } from "fs";
+import path from "path";
+import { expect, test } from "vitest";
+
+test("All packages require the same remotion version", () => {
+  const packages = readdirSync(path.join(process.cwd(), ".."));
+  const folders = packages.filter((p) =>
+    lstatSync(path.join(process.cwd(), "..", p)).isDirectory()
+  );
+
+  for (const folder of folders) {
+    const packageJsonPath = path.join(
+      process.cwd(),
+      "..",
+      folder,
+      "package.json"
+    );
+    if (!existsSync(packageJsonPath)) {
+      continue;
+    }
+    const json = readFileSync(packageJsonPath, "utf-8");
+    const packageJson = JSON.parse(json);
+    const { exports } = packageJson;
+    if (exports === undefined) {
+      continue;
+    }
+    if (!exports["./package.json"]) {
+      throw new Error("No package.json export in " + folder);
+    }
+  }
+});
