4c5d7979593c56f1c9d3eb0e0c65e373afe764ef
Jonny Burger <jonathanburger11@gmail.com>
2023-03-13 10:01:38 +0100

    run all tests again

--- packages/shapes/src/test/make-triangle.test.ts
+++ packages/shapes/src/test/make-triangle.test.ts
@@ -22,37 +22,34 @@ test('Should be able to make a triangle path', () => {
 	});
 });
 
-test.only('Should handle triangle path with radius', () => {
+test('Should handle triangle path with radius', () => {
 	const rightTriangle = makeTriangle({
 		length: 100,
 		direction: 'right',
-		cornerRadius: 60
+		cornerRadius: 60,
 	});
 
-	expect(rightTriangle).toEqual(
-		{
-			height: 100,
-			path: "M 0 40 L 0 40 C 0 100 0 100 51.96152422706631 70 L 34.64101615137755 80 C 86.60254037844386 50 86.60254037844386 50 34.64101615137755 20 L 51.96152422706631 30 C 0 0 0 0 0 60 Z",
-			transformOrigin: "28.867513459481287 50",
-			instructions: expect.any(Array),
-			width: 86.60254037844386,
-		});
-})
+	expect(rightTriangle).toEqual({
+		height: 100,
+		path: 'M 0 40 L 0 40 C 0 100 0 100 51.96152422706631 70 L 34.64101615137755 80 C 86.60254037844386 50 86.60254037844386 50 34.64101615137755 20 L 51.96152422706631 30 C 0 0 0 0 0 60 Z',
+		transformOrigin: '28.867513459481287 50',
+		instructions: expect.any(Array),
+		width: 86.60254037844386,
+	});
+});
 
-
-test.only('Should handle triangle path with radius with direction up', () => {
+test('Should handle triangle path with radius with direction up', () => {
 	const upTriangle = makeTriangle({
 		length: 100,
 		direction: 'up',
-		cornerRadius: 60
+		cornerRadius: 60,
 	});
 
-	expect(upTriangle).toEqual(
-		{
-			height: 86.60254037844386,
-			instructions: expect.any(Array),
-			path: "M 30 34.64101615137755 L 30 34.64101615137755 C 0 86.60254037844386 0 86.60254037844386 60 86.60254037844386 L 40 86.60254037844386 C 100 86.60254037844386 100 86.60254037844386 70 34.64101615137755 L 80 51.96152422706631 C 50 0 50 0 20 51.96152422706631 Z",
-			transformOrigin: "50 57.735026918962575",
-			width: 100,
-		});
-})
+	expect(upTriangle).toEqual({
+		height: 86.60254037844386,
+		instructions: expect.any(Array),
+		path: 'M 30 34.64101615137755 L 30 34.64101615137755 C 0 86.60254037844386 0 86.60254037844386 60 86.60254037844386 L 40 86.60254037844386 C 100 86.60254037844386 100 86.60254037844386 70 34.64101615137755 L 80 51.96152422706631 C 50 0 50 0 20 51.96152422706631 Z',
+		transformOrigin: '50 57.735026918962575',
+		width: 100,
+	});
+});
