a1a58b5adff080eba5c99bd1e3b44bbbad6e5dce
Jonny Burger <jonathanburger11@gmail.com>
2023-03-13 10:28:06 +0100

    fix google fonts script

--- packages/google-fonts/ensure-generation.js
+++ packages/google-fonts/ensure-generation.js
@@ -1,8 +1,14 @@
 const fs = require("node:fs");
 
-const files = fs.readdirSync("./dist");
+const filesCjs = fs.readdirSync("./dist/cjs");
 
-if (files.length < 2200) {
+if (filesCjs.length < 2200) {
+  console.error("Not publishing, Google fonts have not been generated");
+  process.exit(1);
+}
+const filesEsm = fs.readdirSync("./dist/esm");
+
+if (filesEsm.length < 2200) {
   console.error("Not publishing, Google fonts have not been generated");
   process.exit(1);
 }
