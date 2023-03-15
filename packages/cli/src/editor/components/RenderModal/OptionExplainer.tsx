dc15f6b331a6fa4a1c45f6eae005b3700909f7e1
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-03-15 15:50:12 +0100

    copy button
    Former-commit-id: 419b5e757b1a768bf6c0b70f7b4bfbad8585a102

--- packages/cli/src/editor/components/RenderModal/OptionExplainer.tsx
+++ packages/cli/src/editor/components/RenderModal/OptionExplainer.tsx
@@ -1,6 +1,7 @@
 import type {RemotionOption} from '@remotion/renderer';
 import {Spacing} from '../layout';
 import {MenuDivider} from '../Menu/MenuDivider';
+import {CliCopyButton} from './CliCopyButton';
 
 const container: React.CSSProperties = {
 	fontSize: 14,
@@ -34,7 +35,12 @@ export const OptionExplainer: React.FC<{
 			<div style={description}>{option.description}</div>
 			<Spacing block y={0.5} />
 			<MenuDivider />
-			<div style={description}>CLI flag: {option.cliFlag}</div>
+			<div style={{...description, display: 'flex', alignItems: 'center'}}>
+				CLI flag: {option.cliFlag}
+				<div style={{display: 'flex', justifyContent: 'flex-end', flex: 1}}>
+					<CliCopyButton valueToCopy={option.cliFlag} />
+				</div>
+			</div>
 			<div style={description}>Node.JS option: {option.ssrName}</div>
 			<div style={description}>
 				Docs:{' '}
