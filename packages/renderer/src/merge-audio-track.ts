db762c9619f101e94e12075bb336956d8b384afe
Jonny Burger <jonathanburger11@gmail.com>
2023-03-16 10:38:26 +0100

    fix audio bug where 1 audio track that is delayed does not correctly delay

--- packages/renderer/src/merge-audio-track.ts
+++ packages/renderer/src/merge-audio-track.ts
@@ -41,10 +41,7 @@ const mergeAudioTrackUnlimited = async ({
 		return;
 	}
 
-	if (files.length === 1) {
-		copyFileSync(files[0].outName, outName);
-		return;
-	}
+	// Previously a bug: We cannot optimize for files.length === 1 because we need to pad the audio
 
 	// In FFMPEG, the total number of left and right tracks that can be merged at one time is limited to 64
 	if (files.length >= 32) {
