107e04c8f6ddfd6a04561e638c5ae2e8d6829932
Jonny Burger <jonathanburger11@gmail.com>
2023-03-23 16:37:11 +0100

    fixes
    Former-commit-id: 8126cd82cf73ac31f327c626fc27e9eca0729d0f

--- packages/cli/src/editor/components/RenderModal/GuiRenderStatus.tsx
+++ packages/cli/src/editor/components/RenderModal/GuiRenderStatus.tsx
@@ -3,7 +3,9 @@ import type {BaseRenderProgress} from '../../../preview-server/render-queue/job'
 import type {
 	AggregateRenderProgress,
 	RenderingProgressInput,
+	StitchingProgressInput,
 } from '../../../progress-types';
+import {LIGHT_TEXT} from '../../helpers/colors';
 import {Spacing} from '../layout';
 import {CircularProgress} from '../RenderQueue/CircularProgress';
 import {SuccessIcon} from '../RenderQueue/SuccessIcon';
@@ -17,6 +19,15 @@ const progressItem: React.CSSProperties = {
 
 const label: React.CSSProperties = {
 	fontSize: 14,
+	width: 400,
+	color: 'white',
+};
+
+const right: React.CSSProperties = {
+	fontSize: 14,
+	color: LIGHT_TEXT,
+	width: 100,
+	textAlign: 'right',
 };
 
 const BundlingProgress: React.FC<{
@@ -34,7 +45,7 @@ const BundlingProgress: React.FC<{
 			<div style={label}>
 				{progress === 1 ? 'Bundled' : `Bundling ${progress * 100}%`}
 			</div>
-			{doneIn ? <div style={label}>{doneIn}ms</div> : null}
+			{doneIn ? <div style={right}>{doneIn}ms</div> : null}
 		</div>
 	);
 };
@@ -51,11 +62,32 @@ const RenderingProgress: React.FC<{
 			)}
 			<Spacing x={1} />
 			<div style={label}>
-				{progress.doneIn === 1
+				{progress.doneIn
 					? `Rendered ${progress.totalFrames} frames`
 					: `Rendering ${progress.frames} / ${progress.totalFrames} frames`}
 			</div>
-			{progress.doneIn ? <div style={label}>{progress.doneIn}ms</div> : null}
+			{progress.doneIn ? <div style={right}>{progress.doneIn}ms</div> : null}
+		</div>
+	);
+};
+
+const StitchingProgress: React.FC<{
+	progress: StitchingProgressInput;
+}> = ({progress}) => {
+	return (
+		<div style={progressItem}>
+			{progress.frames === progress.totalFrames ? (
+				<SuccessIcon />
+			) : (
+				<CircularProgress progress={progress.frames / progress.totalFrames} />
+			)}
+			<Spacing x={1} />
+			<div style={label}>
+				{progress.doneIn
+					? `Stitched ${progress.totalFrames} frames`
+					: `Stitching ${progress.frames} / ${progress.totalFrames} frames`}
+			</div>
+			{progress.doneIn ? <div style={right}>{progress.doneIn}ms</div> : null}
 		</div>
 	);
 };
@@ -65,12 +97,16 @@ export const GuiRenderStatus: React.FC<{
 }> = ({progress}) => {
 	return (
 		<div>
+			<Spacing y={0.5} />
 			<BundlingProgress
 				progress={progress.bundling.progress}
 				doneIn={progress.bundling.doneIn}
 			/>
 			{progress.rendering ? (
 				<RenderingProgress progress={progress.rendering} />
+			) : null}
+			{progress.stitching ? (
+				<StitchingProgress progress={progress.stitching} />
 			) : null}
 			<Spacing y={2} />
 		</div>
