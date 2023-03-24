b6daa0f2c6a0a92ce637e16dbdd5a46150f9dcf2
Jonny Burger <jonathanburger11@gmail.com>
2023-03-24 18:20:15 +0100

    array editor
    Former-commit-id: 02bd220208e49f99a2e25539b78a4b46cd174eea

--- packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodArrayEditor.tsx
+++ packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodArrayEditor.tsx
@@ -32,10 +32,29 @@ const row: React.CSSProperties = {
 	display: 'flex',
 	flexDirection: 'row',
 	width: '100%',
+	alignItems: 'center',
 };
 
 const flex: React.CSSProperties = {
 	flex: 1,
+};
+
+// TODO: Hover effect would be nice
+const clearButton: React.CSSProperties = {
+	border: '1px solid ' + LIGHT_TEXT,
+	display: 'inline-flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	appearance: 'none',
+	width: 20,
+	height: 20,
+	borderRadius: 14,
+	cursor: 'pointer',
+};
+
+const clearIcon: React.CSSProperties = {
+	height: 12,
+	color: LIGHT_TEXT,
 };
 
 type LocalState = {
@@ -120,6 +139,7 @@ export const ZodArrayEditor: React.FC<{
 									style={row}
 								>
 									<button
+										style={clearButton}
 										onClick={(e) => {
 											e.preventDefault();
 											onChange(
@@ -129,7 +149,16 @@ export const ZodArrayEditor: React.FC<{
 										}}
 										type="button"
 									>
-										remove
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 320 512"
+											style={clearIcon}
+										>
+											<path
+												d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
+												fill={LIGHT_TEXT}
+											/>
+										</svg>
 									</button>
 									<div style={flex}>
 										<ZodSwitch
