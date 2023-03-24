02d1b9675ac6ad20e66c33a63859ac1574ae12e6
Matt McGillivray <m.mcgillivray@outlook.com>
2023-03-24 19:39:03 +0800

    using project .env variable
    Former-commit-id: 3eb1413c1002fa74e804b89a6053e5d80002d372

--- packages/gcp/src/api/helpers/get-cloud-run-client.ts
+++ packages/gcp/src/api/helpers/get-cloud-run-client.ts
@@ -4,6 +4,7 @@ const {ServicesClient} = v2;
 
 export const getCloudRunClient = () => {
 	return new ServicesClient({
+		projectId: process.env.REMOTION_GCP_PROJECT_ID,
 		credentials: {
 			client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
 			private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
