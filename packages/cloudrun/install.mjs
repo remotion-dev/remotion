6fb736ea08d110717fb7ddeef38e5b46ebe098e5
Matt McGillivray <m.mcgillivray@outlook.com>
2023-04-03 16:25:41 +0800

    tshooting sa-permissions.json access

---
+++ packages/cloudrun/install.mjs
@@ -0,0 +1,75 @@
+// this will be executed with node
+import { execSync } from "child_process";
+
+// execSync("gcloud config get-value project", (error, stdout, stderr, {stdio: 'inherit'}) => {
+//   if (error) {
+//     console.log(`error: ${error.message}`);
+//     return;
+//   }
+
+//   if (stderr) {
+//     console.log(`stderr: ${stderr}`);
+//     return;
+//   }
+
+//   console.log('setting TF_VAR_project_id');
+//   execSync(`export TF_VAR_project_id=${stdout}`)
+//   console.log('stdout');
+// })
+
+
+console.log('setting project_id in Terraform, based on current project.')
+
+execSync(
+  'export TF_VAR_project_id=$(gcloud config get-value project)',
+  {stdio: 'inherit'}
+);
+
+console.log('Initialising Terraform.')
+
+execSync(
+  'terraform init',
+  {stdio: 'inherit'}
+);
+
+
+// try {
+//   const cmd = 'gcloud config get-value project';
+//   execSync(cmd, {
+//     encoding: "utf8"
+//   })
+// } catch (error) {
+//   console.log(`Status Code: ${error.status} with '${error.message}'`);
+// }
+
+// exec("gcloud config get-value project", (error, stdout, stderr) => {
+//   if (error) {
+//     console.log(`error: ${error.message}`);
+//     return;
+//   }
+
+//   if (stderr) {
+//     console.log(`stderr: ${stderr}`);
+//     return;
+//   }
+
+//   console.log('setting TF_VAR_project_id');
+//   exec(`export TF_VAR_project_id=${stdout}`)
+//   console.log('stdout');
+// });
+
+// exec("terraform init", (error, stdout, stderr) => {
+//   if (error) {
+//     console.log(`error: ${error.message}`);
+//     return;
+//   }
+
+//   if (stderr) {
+//     console.log(`stderr: ${stderr}`);
+//     return;
+//   }
+
+//   console.log(stdout)
+
+//   console.log('Terraform initialised');
+// })
