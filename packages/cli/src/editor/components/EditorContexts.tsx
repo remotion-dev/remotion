f292c0265a68e0af8488abe6f4d1e3aedeb55c14
Jonny Burger <jonathanburger11@gmail.com>
2023-04-04 15:21:52 +0200

    separate contexts
    Former-commit-id: 0ad2a8a0d27440ccd099e46bb6507c4604c5c19b

--- packages/cli/src/editor/components/EditorContexts.tsx
+++ packages/cli/src/editor/components/EditorContexts.tsx
@@ -1,138 +1,47 @@
-import {PlayerInternals} from '@remotion/player';
-import React, {useCallback, useMemo, useState} from 'react';
-import type {
-	MediaVolumeContextValue,
-	SetMediaVolumeContextValue,
-} from 'remotion';
-import {Internals} from 'remotion';
+import React from 'react';
 import {PreviewServerConnection} from '../helpers/client-id';
-import {
-	CheckerboardContext,
-	loadCheckerboardOption,
-	persistCheckerboardOption,
-} from '../state/checkerboard';
-import {
-	EditorZoomGesturesContext,
-	loadEditorZoomGesturesOption,
-	persistEditorZoomGesturesOption,
-} from '../state/editor-zoom-gestures';
 import {FolderContextProvider} from '../state/folders';
 import {HighestZIndexProvider} from '../state/highest-z-index';
 import {KeybindingContextProvider} from '../state/keybindings';
-import type {ModalContextType, ModalState} from '../state/modals';
-import {ModalsContext} from '../state/modals';
-import {loadMuteOption} from '../state/mute';
 import {PreviewSizeProvider} from '../state/preview-size';
 
 import {SidebarContextProvider} from '../state/sidebar';
+import {CheckerboardProvider} from './CheckerboardProvider';
+import {MediaVolumeProvider} from './MediaVolumeProvider';
+import {ModalsProvider} from './ModalsProvider';
+import {PlayerEmitterContext} from './PlayerEmitterContext';
 import {RenderQueueContextProvider} from './RenderQueue/context';
 import {SetTimelineInOutProvider} from './SetTimelineInOutProvider';
+import {ZoomGesturesProvider} from './ZoomGesturesProvider';
 
 export const EditorContexts: React.FC<{
 	children: React.ReactNode;
 }> = ({children}) => {
-	const [emitter] = useState(() => new PlayerInternals.PlayerEmitter());
-
-	const [checkerboard, setCheckerboardState] = useState(() =>
-		loadCheckerboardOption()
-	);
-	const setCheckerboard = useCallback(
-		(newValue: (prevState: boolean) => boolean) => {
-			setCheckerboardState((prevState) => {
-				const newVal = newValue(prevState);
-				persistCheckerboardOption(newVal);
-				return newVal;
-			});
-		},
-		[]
-	);
-	const [editorZoomGestures, setEditorZoomGesturesState] = useState(() =>
-		loadEditorZoomGesturesOption()
-	);
-	const setEditorZoomGestures = useCallback(
-		(newValue: (prevState: boolean) => boolean) => {
-			setEditorZoomGesturesState((prevState) => {
-				const newVal = newValue(prevState);
-				persistEditorZoomGesturesOption(newVal);
-				return newVal;
-			});
-		},
-		[]
-	);
-
-	const [mediaMuted, setMediaMuted] = useState<boolean>(() => loadMuteOption());
-	const [mediaVolume, setMediaVolume] = useState<number>(1);
-	const [modalContextType, setModalContextType] = useState<ModalState | null>(
-		null
-	);
-
-	const checkerboardCtx = useMemo(() => {
-		return {
-			checkerboard,
-			setCheckerboard,
-		};
-	}, [checkerboard, setCheckerboard]);
-	const editorZoomGesturesCtx = useMemo(() => {
-		return {
-			editorZoomGestures,
-			setEditorZoomGestures,
-		};
-	}, [editorZoomGestures, setEditorZoomGestures]);
-
-	const mediaVolumeContextValue = useMemo((): MediaVolumeContextValue => {
-		return {
-			mediaMuted,
-			mediaVolume,
-		};
-	}, [mediaMuted, mediaVolume]);
-
-	const setMediaVolumeContextValue = useMemo((): SetMediaVolumeContextValue => {
-		return {
-			setMediaMuted,
-			setMediaVolume,
-		};
-	}, []);
-
-	const modalsContext = useMemo((): ModalContextType => {
-		return {
-			selectedModal: modalContextType,
-			setSelectedModal: setModalContextType,
-		};
-	}, [modalContextType]);
-
 	return (
 		<PreviewServerConnection>
 			<RenderQueueContextProvider>
 				<KeybindingContextProvider>
-					<CheckerboardContext.Provider value={checkerboardCtx}>
-						<EditorZoomGesturesContext.Provider value={editorZoomGesturesCtx}>
+					<CheckerboardProvider>
+						<ZoomGesturesProvider>
 							<PreviewSizeProvider>
-								<ModalsContext.Provider value={modalsContext}>
-									<Internals.MediaVolumeContext.Provider
-										value={mediaVolumeContextValue}
-									>
-										<Internals.SetMediaVolumeContext.Provider
-											value={setMediaVolumeContextValue}
-										>
-											<PlayerInternals.PlayerEventEmitterContext.Provider
-												value={emitter}
-											>
-												<SidebarContextProvider>
-													<FolderContextProvider>
-														<HighestZIndexProvider>
-															<SetTimelineInOutProvider>
-																{children}
-															</SetTimelineInOutProvider>
-														</HighestZIndexProvider>
-													</FolderContextProvider>
-												</SidebarContextProvider>
-											</PlayerInternals.PlayerEventEmitterContext.Provider>
-										</Internals.SetMediaVolumeContext.Provider>
-									</Internals.MediaVolumeContext.Provider>
-								</ModalsContext.Provider>
+								<ModalsProvider>
+									<MediaVolumeProvider>
+										<PlayerEmitterContext>
+											<SidebarContextProvider>
+												<FolderContextProvider>
+													<HighestZIndexProvider>
+														<SetTimelineInOutProvider>
+															{children}
+														</SetTimelineInOutProvider>
+													</HighestZIndexProvider>
+												</FolderContextProvider>
+											</SidebarContextProvider>
+										</PlayerEmitterContext>
+									</MediaVolumeProvider>
+								</ModalsProvider>
 							</PreviewSizeProvider>
-						</EditorZoomGesturesContext.Provider>
-					</CheckerboardContext.Provider>
+						</ZoomGesturesProvider>
+					</CheckerboardProvider>
 				</KeybindingContextProvider>
 			</RenderQueueContextProvider>
 		</PreviewServerConnection>
