305fa2d5345f932e69bb2f11f39bb5ce042cc6d4
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-04-05 17:42:05 +0200

    shortcut explainer scrollable
    Former-commit-id: be647d6cfd0e37a188a9714ab6a28948749f3cd9

--- packages/cli/src/editor/components/RenderToolbarIcon.tsx
+++ packages/cli/src/editor/components/RenderToolbarIcon.tsx
@@ -12,6 +12,7 @@ import {Internals, useCurrentFrame} from 'remotion';
 import {getDefaultOutLocation} from '../../get-default-out-name';
 import {getDefaultCodecs} from '../../preview-server/render-queue/get-default-video-contexts';
 import {PreviewServerConnectionCtx} from '../helpers/client-id';
+import {areKeyboardShortcutsDisabled} from '../helpers/use-keybinding';
 import {RenderIcon} from '../icons/render';
 import {ModalsContext} from '../state/modals';
 import {ControlButton} from './ControlButton';
@@ -27,9 +28,10 @@ export const RenderButton: React.FC = () => {
 			},
 		};
 	}, []);
+	const shortcut = areKeyboardShortcutsDisabled() ? '' : '(R)';
 	const tooltip =
 		type === 'connected'
-			? 'Export the current composition R'
+			? 'Export the current composition ' + shortcut
 			: 'Connect to the preview server to render';
 
 	const video = Internals.useVideo();
