0e681fa937506d98f2e2d53095a8b2e7621bed95
Matt McGillivray <m.mcgillivray@outlook.com>
2023-04-05 14:51:09 +0800

    working on install.mjs

--- packages/cloudrun/install.mjs
+++ packages/cloudrun/install.mjs
@@ -18,7 +18,31 @@ import { execSync } from "child_process";
 // })
 
 execSync(
-  'echo "\u001bsetting project_id in Terraform, based on current project."',
+  'echo "\u001b[32;1mThis will setup the GCP project for Remotion Cloud Run.\n\u001b[0m"',
+  {stdio: 'inherit'}
+);
+
+// Read input from the user
+process.stdin.setEncoding('utf8');
+console.log('What version of Remotion do you want to use? (format: 1.0.0)');
+process.stdin.on('data', (input) => {
+  // Remove newlines and carriage returns from the input
+  input = input.trim();
+
+  // Run a bash command with the input as an argument
+  exec(`echo "You entered: ${input}"`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return;
+    }
+
+    console.log(`Output: ${stdout}`);
+    console.error(`Errors: ${stderr}`);
+  });
+});
+
+execSync(
+  'echo "setting project_id in Terraform, based on current project."',
   {stdio: 'inherit'}
 );
 
