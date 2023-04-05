5e09d5409697d768f30835e456a1de59fd8f5fb4
Jonny Burger <jonathanburger11@gmail.com>
2023-04-05 13:26:12 +0200

    start
    Former-commit-id: 78cd620e28008c8b2b106c8498c18f0a63e673f5

---
+++ packages/cli/src/test/create-zod-values.test.ts
@@ -0,0 +1,28 @@
+import {z} from 'remotion';
+import {expect, test} from 'vitest';
+import {createZodValues} from '../editor/components/RenderModal/SchemaEditor/create-zod-values';
+
+test('Should be able to create a string', () => {
+	const hi = createZodValues(z.string());
+	expect(hi).toBe('');
+});
+
+test('Should be able to create a number', () => {
+	const hi = createZodValues(z.number());
+	expect(hi).toBe(0);
+});
+
+test('Should be able to create an object', () => {
+	const hi = createZodValues(
+		z.object({
+			a: z.string(),
+			b: z.number(),
+		})
+	);
+	expect(hi).toEqual({a: '', b: 0});
+});
+
+test('Should be able to create an array', () => {
+	expect(createZodValues(z.array(z.string()))).toEqual(['']);
+	expect(createZodValues(z.array(z.number()))).toEqual([0]);
+});
