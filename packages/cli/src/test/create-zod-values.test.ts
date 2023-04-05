1bc195bf884254b238507f57816413cf86c1f37e
Jonny Burger <jonathanburger11@gmail.com>
2023-04-05 13:28:00 +0200

    union

--- packages/cli/src/test/create-zod-values.test.ts
+++ packages/cli/src/test/create-zod-values.test.ts
@@ -26,3 +26,10 @@ test('Should be able to create an array', () => {
 	expect(createZodValues(z.array(z.string()))).toEqual(['']);
 	expect(createZodValues(z.array(z.number()))).toEqual([0]);
 });
+
+test('Should be able to create a union', () => {
+	expect(createZodValues(z.union([z.string(), z.number()]))).toBe('');
+	expect(createZodValues(z.union([z.number(), z.string()]))).toBe(0);
+	// @ts-expect-error union
+	expect(createZodValues(z.union([]))).toBe(undefined);
+});
