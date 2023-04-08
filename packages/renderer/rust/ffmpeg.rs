cb4bc7c3ced66b92d60f8e799adabf2b3823f66f
Jonny Burger <jonathanburger11@gmail.com>
2023-04-08 17:47:15 +0200

    not clean but it works

--- packages/renderer/rust/ffmpeg.rs
+++ packages/renderer/rust/ffmpeg.rs
@@ -16,7 +16,7 @@ pub fn extract_frame(src: String) -> Result<Vec<u8>, std::io::Error> {
     let mut stream_input = remotionffmepg::format::input(&src)?;
 
     // TODO: Hardcoded
-    let first_frame = 0;
+    let time: f64 = 5.0;
 
     let stream = match stream_input
         .streams_mut()
@@ -27,7 +27,7 @@ pub fn extract_frame(src: String) -> Result<Vec<u8>, std::io::Error> {
     };
 
     let time_base = stream.time_base();
-    let position = (first_frame as f64 * time_base.1 as f64 / time_base.0 as f64) as i64;
+    let position = (time as f64 * time_base.1 as f64 / time_base.0 as f64) as i64;
 
     input.seek(position, ..position)?;
 
@@ -52,6 +52,7 @@ pub fn extract_frame(src: String) -> Result<Vec<u8>, std::io::Error> {
         |decoder: &mut remotionffmepg::decoder::Video| -> Result<Vec<u8>, remotionffmepg::Error> {
             let mut input = Video::empty();
             decoder.receive_frame(&mut input)?;
+            input.data(0);
             let mut rgb_frame = Video::empty();
             scaler.run(&input, &mut rgb_frame)?;
 
@@ -74,11 +75,16 @@ pub fn extract_frame(src: String) -> Result<Vec<u8>, std::io::Error> {
                     let err = rgb_frame.err().unwrap();
                     if err.to_string().contains("Resource temporarily unavailable") {
                         // Need to send another packet
+                        println!("Resource temporarily unavailable");
                     } else {
                         errors::handle_error(&err);
                     }
                 } else {
-                    return Ok(rgb_frame.unwrap());
+                    return Ok(create_bmp_image(
+                        rgb_frame.unwrap(),
+                        decoder.width(),
+                        decoder.height(),
+                    ));
                 }
             }
         }
@@ -95,8 +101,58 @@ fn turn_frame_into_bitmap(rgb_frame: Video) -> Result<Vec<u8>, remotionffmepg::E
     for line in 0..height {
         let begin = line * stride;
         let end = begin + byte_width;
-        new_data.append(&mut rgb_frame.data(0)[begin..end].to_vec());
+        new_data.extend_from_slice(&rgb_frame.data(0)[begin..end]);
     }
 
     Ok(new_data)
+}
+
+fn create_bmp_image(rgb_data: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+    let row_size = (width * 3 + 3) & !3; // Each row is 4-byte aligned
+    let row_padding = row_size - width * 3;
+    let image_size = row_size * height;
+    let header_size = 54;
+
+    let mut bmp_data: Vec<u8> = Vec::new();
+
+    // BMP file header
+    bmp_data.extend_from_slice(b"BM"); // Magic identifier (2 bytes)
+    bmp_data.extend(&((header_size + image_size) as u32).to_le_bytes()); // File size (4 bytes)
+    bmp_data.extend(&0u16.to_le_bytes()); // Reserved (2 bytes)
+    bmp_data.extend(&0u16.to_le_bytes()); // Reserved (2 bytes)
+    bmp_data.extend(&(header_size as u32).to_le_bytes()); // Offset to pixel array (4 bytes)
+
+    // DIB header
+    bmp_data.extend(&(40u32.to_le_bytes())); // Header size (4 bytes)
+    bmp_data.extend(&width.to_le_bytes()); // Image width (4 bytes)
+    bmp_data.extend(&height.to_le_bytes()); // Image height (4 bytes)
+    bmp_data.extend(&1u16.to_le_bytes()); // Color planes (2 bytes)
+    bmp_data.extend(&24u16.to_le_bytes()); // Bits per pixel (2 bytes)
+    bmp_data.extend(&0u32.to_le_bytes()); // Compression method (4 bytes)
+    bmp_data.extend(&image_size.to_le_bytes()); // Image data size (4 bytes)
+    bmp_data.extend(&2835u32.to_le_bytes()); // Horizontal resolution (4 bytes, 72 DPI * 39.3701)
+    bmp_data.extend(&2835u32.to_le_bytes()); // Vertical resolution (4 bytes, 72 DPI * 39.3701)
+    bmp_data.extend(&0u32.to_le_bytes()); // Number of colors (4 bytes)
+    bmp_data.extend(&0u32.to_le_bytes()); // Number of important colors (4 bytes)
+
+    // Image data
+    for y in (0..height).rev() {
+        let row_start = y * width * 3;
+        let row_end = row_start + width * 3;
+        let row = &rgb_data[row_start as usize..row_end as usize];
+
+        // Reverse the order of RGB values to BGR
+        for i in (0..row.len()).step_by(3) {
+            bmp_data.push(row[i + 2]);
+            bmp_data.push(row[i + 1]);
+            bmp_data.push(row[i]);
+        }
+
+        // Add padding to the row if necessary
+        for _ in 0..row_padding {
+            bmp_data.push(0);
+        }
+    }
+
+    bmp_data
 }
