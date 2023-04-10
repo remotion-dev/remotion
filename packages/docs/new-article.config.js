2f566b98d2c3f0f1402eacef626f8363c74b2829
Jonny Burger <jonathanburger11@gmail.com>
2023-04-10 18:40:47 +0200

    custom fields

--- packages/docs/new-article.config.js
+++ packages/docs/new-article.config.js
@@ -1,6 +1,6 @@
 // A minimal config that can be used to write a new article
 // without having to copile all files.
 
-const { config } = require("./docusaurus.config.js");
+const { customFields } = require("./docusaurus.config.js");
 
-module.exports = config("new-doc");
+module.exports = customFields.config("new-doc");
