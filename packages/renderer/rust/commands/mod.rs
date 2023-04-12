ed7de797ef2e8006aac4cb3cf622fac509bac0ad
Jonny Burger <jonathanburger11@gmail.com>
2023-04-12 18:25:41 +0200

    cache scaler
    Former-commit-id: 779b5d81209759d79065dcd812bc3179fc17add6

--- packages/renderer/rust/commands/mod.rs
+++ packages/renderer/rust/commands/mod.rs
@@ -1,6 +1,5 @@
 use crate::compositor::draw_layer;
 use crate::errors::PossibleErrors;
-use crate::global_printer::_print_debug;
 use crate::image::{save_as_jpeg, save_as_png};
 use crate::payloads::payloads::CliInputCommand;
 use crate::{ffmpeg, global_printer};
@@ -9,7 +8,6 @@ use std::io::ErrorKind;
 pub fn execute_command(opts: CliInputCommand) -> Result<(), PossibleErrors> {
     match opts {
         CliInputCommand::ExtractFrame(command) => {
-            let start = std::time::Instant::now();
             let _result = ffmpeg::extract_frame(command.input, command.time)?;
             global_printer::synchronized_write_buf(&command.nonce, &_result)?;
         }
