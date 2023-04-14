d1357d3a31e8635a47316a0d06d9e040319e7f1e
Jonny Burger <jonathanburger11@gmail.com>
2023-04-14 13:34:50 +0200

    moving debug logs to only be called in test
    Former-commit-id: 96a5532dc0be98eb313f96382b837f220977dc0f

--- packages/lambda/src/functions/start.ts
+++ packages/lambda/src/functions/start.ts
@@ -94,7 +94,11 @@ export const startHandler = async (params: LambdaPayload, options: Options) => {
 		rendererFunctionName: params.rendererFunctionName,
 		audioCodec: params.audioCodec,
 	};
-	console.log('invoking lambda', payload);
+
+	if (process.env.NODE_ENV === 'test') {
+		console.log('invoking lambda', payload);
+	}
+
 	await getLambdaClient(getCurrentRegionInFunction()).send(
 		new InvokeCommand({
 			FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME,
