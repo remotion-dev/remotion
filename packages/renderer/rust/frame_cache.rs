34120f9a156b9ff5549e2f599cdca1e939b23731
Jonny Burger <jonathanburger11@gmail.com>
2023-04-14 17:31:37 +0200

    frame cache

---
+++ packages/renderer/rust/frame_cache.rs
@@ -0,0 +1,29 @@
+use remotionffmpeg::frame::Video;
+extern crate ffmpeg_next as remotionffmpeg;
+
+
+pub struct FrameCacheItem {
+  pub frame: Video,
+  pub time: i64,
+}
+
+pub struct FrameCache {
+  pub items: Vec<FrameCacheItem>,
+}
+
+
+impl FrameCache {
+  pub fn new() -> Self {
+    Self {
+      items: Vec::new(),
+    }
+  }
+  pub fn add_item(&mut self, item: FrameCacheItem) {
+    let exists = self.items.iter().any(|i| i.time == item.time);
+    if exists {
+      return;
+    }
+
+    self.items.push(item);
+  }
+}
