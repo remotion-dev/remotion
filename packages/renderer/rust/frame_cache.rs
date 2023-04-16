cd4d076164c4ac75cef9a1c4ed9ba140cb2ece24
Jonny Burger <jonathanburger11@gmail.com>
2023-04-16 12:00:41 +0200

    revert to working
    Former-commit-id: 724a9ebe6440baba676af523b14c808424b06c51

--- packages/renderer/rust/frame_cache.rs
+++ packages/renderer/rust/frame_cache.rs
@@ -4,8 +4,6 @@ extern crate ffmpeg_next as remotionffmpeg;
 pub struct FrameCacheItem {
     pub frame: Video,
     pub time: i64,
-    pub next_time: i64,
-    pub has_next: bool,
 }
 
 pub struct FrameCache {
@@ -16,7 +14,6 @@ impl FrameCache {
     pub fn new() -> Self {
         Self { items: Vec::new() }
     }
-
     pub fn add_item(&mut self, item: FrameCacheItem) {
         let exists = self.items.iter().any(|i| i.time == item.time);
         if exists {
@@ -24,11 +21,5 @@ impl FrameCache {
         }
 
         self.items.push(item);
-    }
-
-    pub fn get_item(&self, time: i64) -> Option<&FrameCacheItem> {
-        self.items
-            .iter()
-            .find(|i| time >= i.time && time < i.next_time)
     }
 }
