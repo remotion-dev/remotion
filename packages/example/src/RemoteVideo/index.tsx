7b91298053d8f0d20305373aeca27c3b88082f72
Jonny Burger <jonathanburger11@gmail.com>
2023-04-17 10:26:52 +0200

    keep PTS, DTS and asked time
    Former-commit-id: 75432f0d11b5c3df835d4101da04ec0bb9ec224b

--- packages/example/src/RemoteVideo/index.tsx
+++ packages/example/src/RemoteVideo/index.tsx
@@ -1,14 +1,7 @@
-import {interpolate, Video} from 'remotion';
+import {OffthreadVideo, staticFile} from 'remotion';
 
 const RemoteVideo: React.FC = () => {
-	return (
-		<Video
-			volume={(f) =>
-				interpolate(f, [0, 500], [1, 0], {extrapolateRight: 'clamp'})
-			}
-			src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-		/>
-	);
+	return <OffthreadVideo src={staticFile('bigbuckbunny.mp4')} />;
 };
 
 export default RemoteVideo;
