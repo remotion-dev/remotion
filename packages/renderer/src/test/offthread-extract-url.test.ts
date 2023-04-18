3781d68171ff243ac6ac938649c794375ce22c00
Jonny Burger <jonathanburger11@gmail.com>
2023-04-18 17:09:53 +0200

    changelog + test
    Former-commit-id: 63363bf4560aba5da79d66a27182bf7f7b8f6c40

--- packages/renderer/src/test/offthread-extract-url.test.ts
+++ packages/renderer/src/test/offthread-extract-url.test.ts
@@ -4,11 +4,10 @@ import {extractUrlAndSourceFromUrl} from '../offthread-video-server';
 test('Extract URL correctly', () => {
 	expect(
 		extractUrlAndSourceFromUrl(
-			'/proxy?src=http%3A%2F%2Flocalhost%3A3000%2Fpublic%2Fframermp4withoutfileextension&time=1.3&imageFormat=png'
+			'/proxy?src=http%3A%2F%2Flocalhost%3A3000%2Fpublic%2Fframermp4withoutfileextension&time=1.3'
 		)
 	).toEqual({
 		src: 'http://localhost:3000/public/framermp4withoutfileextension',
 		time: 1.3,
-		imageFormat: 'png',
 	});
 });
