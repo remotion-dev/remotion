9a29612ce72a2bb65a3cb76d0a70e54d006ddb71
Matt McGillivray <m.mcgillivray@outlook.com>
2023-04-20 15:06:00 +0800

    working on setup page for cloudrun

--- packages/cloudrun/src/client.ts
+++ packages/cloudrun/src/client.ts
@@ -1,3 +1,10 @@
+import {getCompositionsOnGcp} from './api/get-compositions-on-gcp';
+// import {getFunctions} from './api/get-functions';
+// import {getRenderProgress} from './api/get-render-progress';
+// import {getSites} from './api/get-sites';
+import {renderMediaOnCloudrun} from './api/render-media-on-cloudrun';
+import {renderStillOnCloudrun} from './api/render-still-on-cloudrun';
 import type {GcpRegion} from './pricing/gcp-regions';
 
+export {renderMediaOnCloudrun, renderStillOnCloudrun, getCompositionsOnGcp};
 export type {GcpRegion};
