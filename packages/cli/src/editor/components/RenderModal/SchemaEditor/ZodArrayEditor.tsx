8ccf935c0c17f30a4da4db26b7261aa9323bf295
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-04-21 14:20:46 +0200

    Alignment changed from right to left
    ...since error message for number looked odd on the right side -> now everything on left side for consistency
    - close #2064
    
    
    Former-commit-id: b890b4950ec21b2fafbe88500f7701319ef0caf3

--- packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodArrayEditor.tsx
+++ packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodArrayEditor.tsx
@@ -142,7 +142,7 @@ export const ZodArrayEditor: React.FC<{
 						<>
 							<Spacing x={1} />
 							<ValidationMessage
-								align="flex-end"
+								align="flex-start"
 								message={localValue.zodValidation.error.format()._errors[0]}
 								type="error"
 							/>
