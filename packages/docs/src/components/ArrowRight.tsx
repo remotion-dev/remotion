5a76b32d2b4dca7e92e2bd2e56854060b09d0be1
hp_bisle <harshavardhap.1602>
2023-04-24 13:09:56 +0530

    changes template link to button

--- packages/docs/src/components/ArrowRight.tsx
+++ packages/docs/src/components/ArrowRight.tsx
@@ -1,4 +1,3 @@
-import Link from "@docusaurus/Link";
 import React from "react";
 import { useMobileLayout } from "../helpers/mobile-layout";
 
@@ -20,6 +19,7 @@ const commonStyle: React.CSSProperties = {
 const rightStyle: React.CSSProperties = {
   ...commonStyle,
   right: 0,
+  border: "none",
   textDecoration: "none",
 };
 
@@ -31,8 +31,12 @@ const icon: React.CSSProperties = {
 export const MoreTemplatesButton: React.FC = () => {
   const mobileLayout = useMobileLayout();
 
+  const handleClick = () => {
+    window.location.href = "/templates";
+  };
+
   return (
-    <Link to={"/templates"} style={rightStyle}>
+    <button type="button" onClick={handleClick} style={rightStyle} >
       {mobileLayout ? "Templates" : "Find a template"}
       <svg
         style={icon}
@@ -44,6 +48,6 @@ export const MoreTemplatesButton: React.FC = () => {
           d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"
         />
       </svg>
-    </Link>
+    </button>
   );
 };
