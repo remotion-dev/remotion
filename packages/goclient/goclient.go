2761f67cdd77a62a91f78ae5c55ff465c8ed41cb
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-04-25 06:51:46 +1000

    initial work on models and request
    Former-commit-id: cac756a6a5b1c4fbf989537c0d3f831ee3512d8c

---
+++ packages/goclient/goclient.go
@@ -0,0 +1,52 @@
+package goclient
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+)
+
+func invokeLambda(options *RemotionOptions) error {
+	// Create a new AWS session
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create a new Lambda client
+	svc := lambda.New(sess)
+
+	// Create input for Lambda function
+	payload, err := json.Marshal(options)
+	if err != nil {
+		return err
+	}
+	params := &lambda.InvokeInput{
+		FunctionName: aws.String(options.FunctionName),
+		Payload:      payload,
+	}
+
+	// Invoke Lambda function
+	resp, err := svc.Invoke(params)
+	if err != nil {
+		return err
+	}
+
+	// Handle response from Lambda function
+	var output RemotionRenderResponse
+	err = json.Unmarshal(resp.Payload, &output)
+	if err != nil {
+		return err
+	}
+
+	// Do something with the output from Lambda function
+	fmt.Println(output)
+
+	return nil
+}
+
+func Render(input *RemotionOptions) error {
+	return invokeLambda(input)
+}
---
+++ packages/goclient/models.go
@@ -0,0 +1,44 @@
+package goclient
+
+type RemotionOptions struct {
+	ServeUrl              string                 `json:"serveUrl"`
+	FunctionName          string                 `json:"functionName"`
+	InputProps            interface{}            `json:"inputProps"`
+	Composition           string                 `json:"composition"`
+	Type                  string                 `json:"type"`
+	Codec                 string                 `json:"codec"`
+	Version               string                 `json:"version"`
+	ImageFormat           string                 `json:"imageFormat"`
+	Crf                   int                    `json:"crf"`
+	EnvVariables          []interface{}          `json:"envVariables"`
+	Quality               int                    `json:"quality"`
+	MaxRetries            int                    `json:"maxRetries"`
+	Privacy               string                 `json:"privacy"`
+	LogLevel              string                 `json:"logLevel"`
+	FrameRange            interface{}            `json:"frameRange"`
+	OutName               interface{}            `json:"outName"`
+	TimeoutInMilliseconds int                    `json:"timeoutInMilliseconds"`
+	ChromiumOptions       []interface{}          `json:"chromiumOptions"`
+	Scale                 int                    `json:"scale"`
+	EveryNthFrame         int                    `json:"everyNthFrame"`
+	NumberOfGifLoops      int                    `json:"numberOfGifLoops"`
+	ConcurrencyPerLambda  int                    `json:"concurrencyPerLambda"`
+	DownloadBehavior      map[string]interface{} `json:"downloadBehavior"`
+	Muted                 bool                   `json:"muted"`
+	Overwrite             bool                   `json:"overwrite"`
+	AudioBitrate          interface{}            `json:"audioBitrate"`
+	VideoBitrate          interface{}            `json:"videoBitrate"`
+	Webhook               interface{}            `json:"webhook"`
+	ForceHeight           interface{}            `json:"forceHeight"`
+	ForceWidth            interface{}            `json:"forceWidth"`
+	BucketName            interface{}            `json:"bucketName"`
+	AudioCodec            interface{}            `json:"audioCodec"`
+	ForceBucketName       string                 `json:"forceBucketName"`
+}
+
+type RemotionRenderResponse struct {
+	RenderId          string `json:"renderId"`
+	BucketName        string `json:"bucketName"`
+	CloudWatchLogs    string `json:"cloudWatchLogs"`
+	FolderInS3Console string `json:"folderInS3Console"`
+}
