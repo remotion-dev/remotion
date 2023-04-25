c81077532e3d98ea663f11089db0e4c09b5ffea9
Jonny Burger <jonathanburger11@gmail.com>
2023-04-25 16:49:48 +0200

    ok

--- packages/renderer/rust/opened_video.rs
+++ packages/renderer/rust/opened_video.rs
@@ -1,8 +1,12 @@
 use std::sync::{Arc, Mutex};
 
+use ffmpeg_next::Rational;
+
 use crate::{frame_cache::FrameCache, opened_stream::OpenedStream};
 
 pub struct OpenedVideo {
     pub opened_streams: Vec<Arc<Mutex<OpenedStream>>>,
     pub frame_cache: Arc<Mutex<FrameCache>>,
+    pub fps: Rational,
+    pub time_base: Rational,
 }
