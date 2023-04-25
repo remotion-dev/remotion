e7325b21ba15468728365c267daa63d8f795d74c
Jonny Burger <jonathanburger11@gmail.com>
2023-04-25 18:42:26 +0200

    Update create-bucket.ts
    Former-commit-id: 16581746accc10a5e7b07cd83deeda5b6a79b1f9

--- packages/lambda/src/api/create-bucket.ts
+++ packages/lambda/src/api/create-bucket.ts
@@ -1,6 +1,8 @@
 import {
 	CreateBucketCommand,
+	DeleteBucketOwnershipControlsCommand,
 	DeletePublicAccessBlockCommand,
+	PutBucketAclCommand,
 } from '@aws-sdk/client-s3';
 import type {AwsRegion} from '../pricing/aws-regions';
 import {getS3Client} from '../shared/aws-clients';
@@ -12,17 +14,49 @@ export const createBucket = async ({
 	region: AwsRegion;
 	bucketName: string;
 }) => {
+	console.log('creating', bucketName);
 	await getS3Client(region, null).send(
 		new CreateBucketCommand({
 			Bucket: bucketName,
-			ACL: 'public-read',
-			ObjectOwnership: 'ObjectWriter',
 		})
 	);
 
+	try {
+		await getS3Client(region, null).send(
+			new DeleteBucketOwnershipControlsCommand({
+				Bucket: bucketName,
+			})
+		);
+	} catch (err) {
+		if ((err as Error).message.includes('Access Denied')) {
+			throw new Error(
+				'Since April 2023, more AWS permissions are required to create an S3 bucket. You need to update your user policy to continue.'
+			);
+		}
+
+		throw err;
+	}
+
+	try {
+		await getS3Client(region, null).send(
+			new DeletePublicAccessBlockCommand({
+				Bucket: bucketName,
+			})
+		);
+	} catch (err) {
+		if ((err as Error).message.includes('Access Denied')) {
+			throw new Error(
+				'PARTIAL SUCCESS: Since April 2023, more AWS permissions are required to create an S3 bucket. You need to update your user policy to continue.'
+			);
+		}
+
+		throw err;
+	}
+
 	await getS3Client(region, null).send(
-		new DeletePublicAccessBlockCommand({
+		new PutBucketAclCommand({
 			Bucket: bucketName,
+			ACL: 'public-read',
 		})
 	);
 };
