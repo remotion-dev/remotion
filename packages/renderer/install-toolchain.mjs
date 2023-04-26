859c7c01518a30f12330bacc5953891a2cb7f943
Jonny Burger <jonathanburger11@gmail.com>
2023-04-26 13:41:50 +0200

    make it work

--- packages/renderer/install-toolchain.mjs
+++ packages/renderer/install-toolchain.mjs
@@ -3,7 +3,6 @@ import {existsSync, mkdirSync, unlinkSync} from 'fs';
 
 const toolchains = [
 	'x86_64_gnu_toolchain',
-	'aarch_gnu_toolchain',
 	'x86_64_musl_toolchain',
 	'aarch64-musl-toolchain',
 ];
@@ -12,6 +11,7 @@ const unpatched = [
 	'x86_64-apple-darwin',
 	'aarch64-apple-darwin',
 	'x86_64-pc-windows-gnu',
+	'aarch64-unknown-linux-gnu',
 ];
 
 for (const toolchain of toolchains) {
@@ -40,3 +40,9 @@ for (const toolchain of toolchains) {
 for (const target of unpatched) {
 	execSync(`rustup target add ${target}`);
 }
+
+// Aarch64 sysroot is missing libz.so.1
+cpSync(
+	'libz.so.1',
+	'/opt/homebrew/Cellar/aarch64-unknown-linux-gnu/0.1.0/aarch64-unknown-linux-gnu/sysroot/lib64/libz.so.1'
+);
