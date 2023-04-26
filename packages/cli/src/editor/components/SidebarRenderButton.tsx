1c9cbe61f2a03b27253cfb5e48924253767870fb
Jonny Burger <jonathanburger11@gmail.com>
2023-04-26 19:25:33 +0200

    Update SidebarRenderButton.tsx
    Former-commit-id: 7f23e5cbde53cdd3b333babfa99170ad3d665f50

--- packages/cli/src/editor/components/SidebarRenderButton.tsx
+++ packages/cli/src/editor/components/SidebarRenderButton.tsx
@@ -36,7 +36,7 @@ export const SidebarRenderButton: React.FC<{
 	const {props} = useContext(Internals.EditorPropsContext);
 
 	const isVideo = composition.durationInFrames > 1;
-	const onClick: React.MouseEventHandler<HTMLAnchorElement> = useCallback(
+	const onClick: React.MouseEventHandler<HTMLButtonElement> = useCallback(
 		(e) => {
 			const defaults = window.remotion_renderDefaults;
 			if (!defaults) {
