a74caa49425eff89cca51a1df3d2609dae065aef
Jonny Burger <jonathanburger11@gmail.com>
2023-04-27 17:33:10 +0200

    make it work

--- packages/cli/src/editor/components/RenderQueue/RenderQueueRemoveItem.tsx
+++ packages/cli/src/editor/components/RenderQueue/RenderQueueRemoveItem.tsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useMemo} from 'react';
 import type {RenderJob} from '../../../preview-server/render-queue/job';
+import type {RenderInlineAction} from '../InlineAction';
 import {InlineAction} from '../InlineAction';
 import {
 	notificationCenter,
@@ -31,18 +32,23 @@ export const RenderQueueRemoveItem: React.FC<{job: RenderJob}> = ({job}) => {
 		};
 	}, []);
 
-	return (
-		<InlineAction onClick={onClick}>
-			<svg
-				style={icon}
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 320 512"
-			>
-				<path
-					fill="currentColor"
-					d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
-				/>
-			</svg>
-		</InlineAction>
+	const renderAction: RenderInlineAction = useCallback(
+		(color) => {
+			return (
+				<svg
+					style={icon}
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 320 512"
+				>
+					<path
+						fill={color}
+						d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
+					/>
+				</svg>
+			);
+		},
+		[icon]
 	);
+
+	return <InlineAction renderAction={renderAction} onClick={onClick} />;
 };
