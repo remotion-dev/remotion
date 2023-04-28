481e21506816dc7bbead1b21b90b3ead3bf640d8
Matt McGillivray <m.mcgillivray@outlook.com>
2023-04-28 12:27:15 +0800

    docs for rendering cloudrun

--- packages/cloudrun/src/index.ts
+++ packages/cloudrun/src/index.ts
@@ -1,6 +1,20 @@
+import type {
+	DeployServiceInput,
+	DeployServiceOutput,
+} from './api/deploy-service';
 import {deployService} from './api/deploy-service';
 import {deploySite} from './api/deploy-site';
 import {getOrCreateBucket} from './api/get-or-create-bucket';
+import {renderMediaOnCloudrun} from './api/render-media-on-cloudrun';
+import {renderStillOnCloudrun} from './api/render-still-on-cloudrun';
 import {CloudrunInternals} from './internals';
 
-export {CloudrunInternals, deployService, deploySite, getOrCreateBucket};
+export {
+	CloudrunInternals,
+	deployService,
+	deploySite,
+	getOrCreateBucket,
+	renderMediaOnCloudrun,
+	renderStillOnCloudrun,
+};
+export type {DeployServiceInput, DeployServiceOutput};
