b948d92b1d7d4be2f2aa98e1e0c59bab278382d4
Jonny Burger <jonathanburger11@gmail.com>
2023-04-28 16:50:49 +0200

    Revert "demonstrate that close video works"
    This reverts commit a08371ba79f3840ecfe8ff2c7820740333ff921b [formerly 21a9bf1aaffbd811d73f7b84e0ba8620224562d5].
    
    
    Former-commit-id: 1f77afc641a9d407b3acb82b6954ca1fbb571302

--- packages/renderer/src/compositor/compositor.ts
+++ packages/renderer/src/compositor/compositor.ts
@@ -17,7 +17,6 @@ export type Compositor = {
 		payload: CompositorCommand[T]
 	) => Promise<Buffer>;
 	waitForDone: () => Promise<void>;
-	pid: number | null;
 };
 
 type Waiter = {
@@ -272,6 +271,5 @@ export const startCompositor = <T extends keyof CompositorCommand>(
 				});
 			});
 		},
-		pid: child.pid ?? null,
 	};
 };
