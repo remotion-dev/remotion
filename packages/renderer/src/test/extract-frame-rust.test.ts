966f4a8ec832f9a9a6fa1b43dc89d4e84964ab2d
Jonny Burger <jonathanburger11@gmail.com>
2023-04-28 16:51:14 +0200

    Revert "Revert "blueprint for a test""
    This reverts commit 6d8980a0be46511ef5b34aa65a73be7e48505ea8.

--- packages/renderer/src/test/extract-frame-rust.test.ts
+++ packages/renderer/src/test/extract-frame-rust.test.ts
@@ -337,7 +337,7 @@ test('Two different starting times should not result in big seeking', async () =
 	await compositor.waitForDone();
 });
 
-test('Memory usage should be determined ', async () => {
+test.only('Memory usage should be determined ', async () => {
 	const compositor = startLongRunningCompositor();
 
 	const memoryUsageBefore = await compositor.executeCommand('MemoryStats', {});
@@ -354,11 +354,21 @@ test('Memory usage should be determined ', async () => {
 	const parsed = JSON.parse(memoryUsageAfter.toString('utf-8'));
 	expect(parsed.physical_mem).toBeGreaterThan(100 * 1024 * 1024);
 
+	await new Promise((resolve) => {
+		setTimeout(() => resolve(), 10000);
+	});
+
+	await compositor.executeCommand('CloseAllVideos', {});
+
+	await new Promise((resolve) => {
+		setTimeout(() => resolve(), 10000);
+	});
+
 	const memoryUsageAfterClean = await compositor.executeCommand(
 		'MemoryStats',
 		{}
 	);
 
 	const afterClosed = JSON.parse(memoryUsageAfterClean.toString('utf-8'));
-	expect(afterClosed.physical_mem).toBeGreaterThan(100 * 1024 * 1024);
+	expect(afterClosed.physical_mem).toBeLessThan(50 * 1024 * 1024);
 });
