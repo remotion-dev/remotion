08aeab2e6d1edef53a266e11ea897eb0c90151fa
Jonny Burger <jonathanburger11@gmail.com>
2023-05-02 13:29:12 +0200

    skip unnecessary log messages

--- packages/renderer/rust/logger.rs
+++ packages/renderer/rust/logger.rs
@@ -10,11 +10,21 @@ pub unsafe extern "C" fn log_callback(
     fmt: *const c_char,
     list: VaListLoggerArg,
 ) {
-    let message = ffmpeg_next::log::make_log_message(fmt, list);
+    let message = ffmpeg_next::log::make_log_message(fmt, list).unwrap();
+
+    // Warning that we cannot do much about
+    if message.contains("No accelerated colorspace conversion found") {
+        return;
+    }
+
+    // libvpx logs this
+    if message.trim() == "v1.12.0" {
+        return;
+    }
 
     if level <= ffmpeg_next::log::Level::Info.into() {
-        _print_debug(&format!("[FFmpeg] {}", message.unwrap())).unwrap();
+        _print_debug(&format!("[FFmpeg] {}", message)).unwrap();
     } else if level <= ffmpeg_next::log::Level::Verbose.into() {
-        _print_verbose(&format!("[FFmpeg] {}", message.unwrap())).unwrap();
+        _print_verbose(&format!("[FFmpeg] {}", message)).unwrap();
     }
 }
