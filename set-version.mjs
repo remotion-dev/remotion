e671e2549010c2ee0b2a97f565f699cdcea31040
Jonny Burger <jonathanburger11@gmail.com>
2023-05-03 15:43:00 +0200

    custom scripts

---
+++ set-version.mjs
@@ -0,0 +1,51 @@
+import { execSync } from "child_process";
+import {
+  existsSync,
+  lstatSync,
+  readFileSync,
+  readdirSync,
+  writeFileSync,
+} from "fs";
+import path from "path";
+
+let version = process.argv[2];
+if (!version) {
+  throw new Error("Please specify a version");
+}
+
+if (version.startsWith("v")) {
+  version = version.slice(1);
+}
+
+const dirs = readdirSync("packages")
+  .filter((dir) =>
+    lstatSync(path.join(process.cwd(), "packages", dir)).isDirectory()
+  )
+  .filter((dir) =>
+    existsSync(path.join(process.cwd(), "packages", dir, "package.json"))
+  );
+
+for (const dir of dirs) {
+  const packageJsonPath = path.join(
+    process.cwd(),
+    "packages",
+    dir,
+    "package.json"
+  );
+  const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
+  packageJson.version = version;
+  writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + "\n");
+  try {
+    execSync("pnpm exec prettier --write package.json", {
+      cwd: path.join(process.cwd(), "packages", dir),
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+console.log(dirs);
+
+execSync("pnpm exec vitest src/monorepo --run", {
+  cwd: "packages/it-tests",
+  stdio: "inherit",
+});
