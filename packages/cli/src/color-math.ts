0a7b26855afe8628e9ee92218453096f482f82a4
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-05-05 16:00:09 +0200

    fixed

--- packages/cli/src/color-math.ts
+++ packages/cli/src/color-math.ts
@@ -7,7 +7,9 @@ export const colorWithNewOpacity = (
 ) => {
 	const {r, g, b} = zodTypes.ZodZypesInternals.parseColor(color);
 	if (opacity >= 255) {
-		return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+		return `#${r.toString(16).padStart(2, '0')}${g
+			.toString(16)
+			.padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
 	}
 
 	return `rgba(${r}, ${g}, ${b}, ${(opacity / 255).toFixed(2)})`;
