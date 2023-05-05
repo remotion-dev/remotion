160811de4c14f9f2fed2aa6bfa271d0f5e1b2a9d
Jonny Burger <jonathanburger11@gmail.com>
2023-05-05 13:07:03 +0200

    fix warnings + remove unused

--- packages/goclient/utils.go
+++ packages/goclient/utils.go
@@ -4,16 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
-	"math/rand"
-	"time"
 )
 
 func serializeInputProps(inputProps interface{}, region string, inputType string,
 	userSpecifiedBucketName *string) (map[string]interface{}, error) {
 	payload, err := json.Marshal(inputProps)
 	if err != nil {
-		return nil, errors.New("error serializing inputProps. Check it has no circular references or reduce the size if the object is big.")
+		return nil, errors.New("error serializing inputProps. Check it has no circular references or reduce the size if the object is big")
 	}
 
 	// Set maximum inline payload size based on input type
@@ -31,33 +28,4 @@ func serializeInputProps(inputProps interface{}, region string, inputType string
 		"type":    "payload",
 		"payload": string(payload),
 	}, nil
-}
-
-func randomHash(options ...bool) string {
-	var randomInTests bool
-	if len(options) > 0 {
-		randomInTests = options[0]
-	}
-
-	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	length := 10
-
-	if randomInTests {
-		rand.Seed(1234)
-	} else {
-		rand.Seed(time.Now().UnixNano())
-	}
-
-	hash := make([]byte, length)
-	for i := 0; i < length; i++ {
-		hash[i] = alphabet[rand.Intn(len(alphabet))]
-	}
-
-	return string(hash)
-}
-
-func logInfo(message string) {
-	log.SetPrefix("INFO ")
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	log.Println(message)
 }
