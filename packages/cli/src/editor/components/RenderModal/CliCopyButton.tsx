2acc4eef5bdc013e9648a5fe6fc45554e7327ada
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-05-10 15:51:11 +0200

    check insted of "copied"
    Former-commit-id: 7707d22e90c9c77e7d11c40b71febacb7395664e

--- packages/cli/src/editor/components/RenderModal/CliCopyButton.tsx
+++ packages/cli/src/editor/components/RenderModal/CliCopyButton.tsx
@@ -1,6 +1,6 @@
 import {useCallback, useEffect, useMemo, useState} from 'react';
 import {LIGHT_TEXT} from '../../helpers/colors';
-const iconStyle: React.CSSProperties = {
+const svgStyle: React.CSSProperties = {
 	width: 16,
 	height: 16,
 	verticalAlign: 'sub',
@@ -9,8 +9,10 @@ const iconStyle: React.CSSProperties = {
 const copiedStyle: React.CSSProperties = {
 	fontSize: '14px',
 	minHeight: '30px',
+	minWidth: '30px',
 	display: 'flex',
 	alignItems: 'center',
+	justifyContent: 'center',
 };
 
 export const CliCopyButton: React.FC<{valueToCopy: string}> = ({
@@ -33,11 +35,24 @@ export const CliCopyButton: React.FC<{valueToCopy: string}> = ({
 			role="img"
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 384 512"
-			style={iconStyle}
+			style={svgStyle}
 		>
 			<path
 				fill={fillColor}
 				d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm144 418c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V118c0-3.3 2.7-6 6-6h42v36c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-36h42c3.3 0 6 2.7 6 6z"
+			/>
+		</svg>
+	);
+
+	const checkSvg = (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 448 512"
+			style={svgStyle}
+		>
+			<path
+				fill={fillColor}
+				d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
 			/>
 		</svg>
 	);
@@ -84,6 +99,6 @@ export const CliCopyButton: React.FC<{valueToCopy: string}> = ({
 			{clipboardIcon}
 		</button>
 	) : (
-		<span style={copiedStyle}>copied</span>
+		<span style={copiedStyle}>{checkSvg}</span>
 	);
 };
