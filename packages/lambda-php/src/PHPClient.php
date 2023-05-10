5b8934e622341b774cd070d75df8e7548b526405
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-05-10 19:13:49 +1000

    initial php client
    Former-commit-id: b684591a7c30c02f741cf26c587fc45773c17ac1

---
+++ packages/lambda-php/src/PHPClient.php
@@ -0,0 +1,124 @@
+<?php
+
+use Aws\Lambda\LambdaClient;
+
+class PHPClient
+{
+    private $client;
+
+    public function __construct(
+        string $region,
+        string $serveUrl,
+        string $functionName,
+        string $bucketName,
+        array $credential) {
+        $this->client = LambdaClient::factory([
+            'version' => 'latest',
+            'region' => $region,
+            'credentials' => $credential,
+        ]);
+    }
+
+    public function invokeLambda(
+        array $data,
+        string $composition = 'main',
+        string $type = 'start',
+        string $codec = 'h264',
+        string $version = '3.3.78',
+        string $imageFormat = 'jpeg',
+        int $crf = 1,
+        array $envVariables = [],
+        int $quality = 80,
+        int $maxRetries = 1,
+        string $privacy = 'public',
+        string $logLevel = 'info',
+        ? string $frameRange = null,
+        ? string $outName = null,
+        int $timeoutInMilliseconds = 30000,
+        array $chromiumOptions = [],
+        float $scale = 1,
+        int $everyNthFrame = 1,
+        int $numberOfGifLoops = 0,
+        int $concurrencyPerLambda = 1,
+        array $downloadBehavior = [
+            'type' => 'play-in-browser',
+        ],
+        bool $muted = false,
+        bool $overwrite = false,
+        ? int $audioBitrate = null,
+        ? int $videoBitrate = null,
+        ? string $webhook = null,
+        ? int $forceHeight = null,
+        ? int $forceWidth = null,
+        ? string $audioCodec = null,
+    ) :  ? string{
+        $input = serializeInputProps(
+            $data,
+            $region,
+            "video-or-audio",
+            null
+        );
+
+        $params = [
+            "serveUrl" => $serveUrl,
+            "inputProps" => $input,
+            "composition" => $composition,
+            "type" => $type,
+            "codec" => $codec,
+            "version" => $version,
+            "imageFormat" => $imageFormat,
+            "crf" => $crf,
+            "envVariables" => $envVariables,
+            "quality" => $quality,
+            "maxRetries" => $maxRetries,
+            "privacy" => $privacy,
+            "logLevel" => $logLevel,
+            "frameRange" => $frameRange,
+            "outName" => $outName,
+            "timeoutInMilliseconds" => $timeoutInMilliseconds,
+            "chromiumOptions" => $chromiumOptions,
+            "scale" => $scale,
+            "everyNthFrame" => $everyNthFrame,
+            "numberOfGifLoops" => $numberOfGifLoops,
+            "concurrencyPerLambda" => $concurrencyPerLambda,
+            "downloadBehavior" => $downloadBehavior,
+            "muted" => $muted,
+            "overwrite" => $overwrite,
+            "audioBitrate" => $audioBitrate,
+            "videoBitrate" => $videoBitrate,
+            "webhook" => $webhook,
+            "forceHeight" => $forceHeight,
+            "forceWidth" => $forceWidth,
+            "bucketName" => $bucketName,
+            "audioCodec" => $audioCodec,
+            "forceBucketName" => $bucketName,
+        ];
+
+        // Remove null values
+        $data = array_filter($params, function ($value) {
+            return !is_null($value);
+        });
+
+        $result = $this->client->invoke([
+            'InvocationType' => 'RequestResponse',
+            'FunctionName' => $functionName,
+            'Payload' => json_encode($data),
+        ]);
+
+        // Check if the invocation was successful
+        if ($result->getStatusCode() == 200) {
+            // Get the response from the invocation
+            $response = json_decode($result->get('Payload'));
+            if (isset($response->errorMessage)) {
+                // The Lambda function encountered an error
+                throw new Exception($response->errorMessage);
+            } else {
+                // The Lambda function was invoked successfully
+                return $response->output;
+            }
+        } else {
+            // The Lambda function encountered an error
+            throw new Exception("Failed to invoke Lambda function");
+        }
+    }
+}
---
+++ packages/lambda-php/src/PHPClientTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class PHPClientTest extends PHPUnit\Framework\TestCase
+
+{
+    public function testAdd()
+    {
+        //  $calculator = new Calculator();
+        //$result = $calculator->add(2, 3);
+        $this->assertEquals(5, 5);
+    }
+}
