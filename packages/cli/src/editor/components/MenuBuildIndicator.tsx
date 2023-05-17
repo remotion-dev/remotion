d29e4dc4a306b87d71c47f91e444509bd717e6f2
Jonny Burger <jonathanburger11@gmail.com>
2023-05-17 16:47:27 +0200

    add a spinner!

--- packages/cli/src/editor/components/MenuBuildIndicator.tsx
+++ packages/cli/src/editor/components/MenuBuildIndicator.tsx
@@ -2,12 +2,27 @@ import React, {useContext, useEffect, useState} from 'react';
 import {PreviewServerConnectionCtx} from '../helpers/client-id';
 import {Spacing} from './layout';
 import {OpenEditorButton} from './OpenEditorButton';
+import {Spinner} from './Spinner';
 
 const cwd: React.CSSProperties = {
 	fontSize: 13,
 	opacity: 0.8,
 	display: 'flex',
 	alignItems: 'center',
+	justifyContent: 'center',
+};
+
+const spinnerSize = 14;
+
+const spinner: React.CSSProperties = {
+	position: 'relative',
+	width: spinnerSize,
+	marginTop: 4,
+};
+
+const noSpinner: React.CSSProperties = {
+	position: 'relative',
+	width: spinnerSize,
 };
 
 export const MenuBuildIndicator: React.FC = () => {
@@ -32,8 +47,16 @@ export const MenuBuildIndicator: React.FC = () => {
 
 	return (
 		<div style={cwd} title={window.remotion_cwd}>
-			{showButton ? <Spacing x={5} /> : null}
-			{isBuilding ? 'Building...' : window.remotion_projectName}
+			{showButton ? <Spacing x={2} /> : null}
+			{isBuilding ? (
+				<div style={spinner}>
+					<Spinner size={spinnerSize} />
+				</div>
+			) : (
+				<div style={noSpinner} />
+			)}
+			{showButton ? <Spacing x={0.5} /> : null}
+			{window.remotion_projectName}
 			{showButton ? <OpenEditorButton /> : null}
 		</div>
 	);
