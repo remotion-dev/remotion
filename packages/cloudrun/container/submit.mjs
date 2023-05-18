d3561aea4fd43a0c6eb518e9eedc939ada7d5c58
Jonny Burger <jonathanburger11@gmail.com>
2023-05-18 22:34:13 +0200

    cache docker builds
    Former-commit-id: 58b06943328ff43abf95517fa521f00eed847291

--- packages/cloudrun/container/submit.mjs
+++ packages/cloudrun/container/submit.mjs
@@ -1,8 +1,41 @@
 import {execSync} from 'child_process';
+import {writeFileSync} from 'fs';
 import {VERSION} from 'remotion/version';
 
-execSync(
-	'gcloud builds submit --tag us-docker.pkg.dev/remotion-dev/cloud-run/render:' +
+// Make an image with tag called `cachebase` - this contains all the layers until the JS gets copied
+// If an earlier layer is changed, need to rebuild `cachebase` again
+
+const cloudbuildJSON = {
+	steps: [
+		{
+			name: 'gcr.io/cloud-builders/docker',
+			entrypoint: 'bash',
+			args: [
+				'-c',
+				`docker pull us-docker.pkg.dev/remotion-dev/cloud-run/render:cachebase || exit 0`,
+			],
+		},
+		{
+			name: 'gcr.io/cloud-builders/docker',
+			args: [
+				'build',
+				'-t',
+				`us-docker.pkg.dev/remotion-dev/cloud-run/render:${VERSION}`,
+				'--cache-from',
+				`us-docker.pkg.dev/remotion-dev/cloud-run/render:cachebase`,
+				'.',
+			],
+		},
+	],
+	images: [`us-docker.pkg.dev/remotion-dev/cloud-run/render:${VERSION}`],
+};
+
+writeFileSync('cloudbuild.json', JSON.stringify(cloudbuildJSON, null, 2));
+
+execSync(`gcloud builds submit --config cloudbuild.json .`, {
+	stdio: 'inherit',
+	env: {
+		...process.env,
 		VERSION,
-	{stdio: 'inherit'}
-);
+	},
+});
