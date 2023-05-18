a458cdf4ccc13a15353d6b0241e37ccd5b1e0dbb
Jonny Burger <jonathanburger11@gmail.com>
2023-05-18 19:51:18 +0200

    deploy site + no need to specify service name

--- packages/example/testcloudrun.sh
+++ packages/example/testcloudrun.sh
@@ -10,5 +10,6 @@ cd ..
 cd example
 pnpm exec remotion cloudrun services rmall -f
 sleep 3
+pnpm exec remotion cloudrun sites create --site-name=testbed
 pnpm exec remotion cloudrun services deploy
-pnpm exec remotion cloudrun render still zsq2ehnz1v react-svg --service-name=remotion--3-3-94--mem2gi--cpu1-0--t-300
+pnpm exec remotion cloudrun render still testbed react-svg
