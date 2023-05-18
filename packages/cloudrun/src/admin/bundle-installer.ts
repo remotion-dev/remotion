3990e160c2ff57f2a344e9316f60c7bdf0ff4297
Jonny Burger <jonathanburger11@gmail.com>
2023-05-18 17:43:18 +0200

    bundle the installer
    Former-commit-id: 0670e3d00f86145df69460a60fe645c008823e0b

---
+++ packages/cloudrun/src/admin/bundle-installer.ts
@@ -0,0 +1,39 @@
+import {BundlerInternals} from '@remotion/bundler';
+import {execSync} from 'child_process';
+import {copyFileSync, mkdirSync, rmSync} from 'fs';
+import path from 'path';
+
+const bundleInstaller = async () => {
+	const outdir = path.join(__dirname, '..', `build-installer`);
+	mkdirSync(outdir, {recursive: true});
+
+	const bundlemjs = path.join(outdir, 'bundled.mjs');
+	await BundlerInternals.esbuild.build({
+		platform: 'node',
+		target: 'node18',
+		bundle: true,
+		outfile: bundlemjs,
+		format: 'esm',
+		entryPoints: [path.resolve(__dirname, '../../gcpInstaller/install.mjs')],
+	});
+
+	const tfoutfile = path.join(outdir, 'main.tf');
+	copyFileSync(
+		path.resolve(__dirname, '../../gcpInstaller/main.tf'),
+		tfoutfile
+	);
+
+	execSync(
+		`tar -cf ../../gcpInstaller/gcpInstaller.tar -C . ${path.relative(
+			outdir,
+			bundlemjs
+		)} ${path.relative(outdir, tfoutfile)}`,
+		{
+			stdio: 'inherit',
+			cwd: outdir,
+		}
+	);
+	rmSync(outdir, {recursive: true});
+};
+
+bundleInstaller();
