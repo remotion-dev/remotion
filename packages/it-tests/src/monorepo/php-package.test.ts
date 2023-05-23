39686a164cddbb15a799e9b840416ed02e0b76e7
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-05-23 11:44:51 +1000

    remove console logs
    Former-commit-id: 82212bf082d268265ee8d92a533b0ad80fb96bee

--- packages/it-tests/src/monorepo/php-package.test.ts
+++ packages/it-tests/src/monorepo/php-package.test.ts
@@ -25,27 +25,8 @@ test("PHP package should create the same payload as normal Lambda package", asyn
   const phpOutput = execSync("phpunit ./src/PHPClientTest.php", {
     cwd: path.join(process.cwd(), "..", "lambda-php"),
   });
-  console.log("show php output");
-  console.log(phpOutput.toString());
-
   const output = phpOutput.toString().split("\n");
-
-  console.log("first Line");
-  console.log(output[0]);
-  console.log("second Line");
-  console.log(output[1]);
-  console.log("third Line");
-  console.log(output[2]);
-  console.log("fourth Line");
-  console.log(output[3]);
-  console.log("Fifth Line");
-  console.log(output[4]);
-
-  console.log("whole output");
-  console.log(output);
-
   const toParse = output[4];
-
   const nativeVersion = await LambdaInternals.makeLambdaPayload({
     region: "us-east-1",
     composition: "react-svg",
@@ -53,13 +34,8 @@ test("PHP package should create the same payload as normal Lambda package", asyn
     serveUrl: "testbed",
     codec: "h264",
   });
-
-  //const startMarker = "(100%)";
   const jsonOutput = toParse.substring(0, toParse.lastIndexOf("}") + 1);
-  console.log("The element to parse");
-  console.log(jsonOutput);
   const parsedJson = JSON.parse(jsonOutput);
-
   expect(JSON.stringify(parsedJson, null, 3)).toEqual(
     JSON.stringify(nativeVersion, null, 3)
   );
