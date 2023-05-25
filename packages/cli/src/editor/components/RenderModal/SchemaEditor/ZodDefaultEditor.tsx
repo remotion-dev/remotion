e8d8dbf553bddbe7aa313a8391843f57d0d40599
Jonny Burger <jonathanburger11@gmail.com>
2023-05-25 15:36:43 +0200

    default editor
    Former-commit-id: aa9823fe0ce2f009622dffabdce7adf3d92a8b75

---
+++ packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodDefaultEditor.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import type {z} from 'zod';
+import type {JSONPath} from './zod-types';
+import type {UpdaterFunction} from './ZodSwitch';
+import {ZodSwitch} from './ZodSwitch';
+
+export const ZodDefaultEditor: React.FC<{
+	showSaveButton: boolean;
+	jsonPath: JSONPath;
+	compact: boolean;
+	value: unknown;
+	defaultValue: unknown;
+	schema: z.ZodTypeAny;
+	setValue: UpdaterFunction<unknown>;
+	onSave: UpdaterFunction<unknown>;
+	onRemove: null | (() => void);
+	saving: boolean;
+}> = ({
+	jsonPath,
+	compact,
+	schema,
+	setValue,
+	onSave,
+	defaultValue,
+	value,
+	showSaveButton,
+	onRemove,
+	saving,
+}) => {
+	const {innerType} = schema._def as z.ZodDefaultDef;
+
+	return (
+		<ZodSwitch
+			compact={compact}
+			defaultValue={defaultValue}
+			jsonPath={jsonPath}
+			onRemove={onRemove}
+			onSave={onSave}
+			schema={innerType}
+			setValue={setValue}
+			showSaveButton={showSaveButton}
+			value={value}
+			saving={saving}
+		/>
+	);
+};
