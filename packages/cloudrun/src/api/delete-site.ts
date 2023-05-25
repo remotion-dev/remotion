dc2dc61bdf0a72d7f8c1ab121549fffc8d7f1028
Matt McGillivray <m.mcgillivray@outlook.com>
2023-05-25 14:59:13 +0800

    jsdoc updates for cloudurn
    Former-commit-id: 9706f202563b2c57901f40ca1d8f556d7aea3187

--- packages/cloudrun/src/api/delete-site.ts
+++ packages/cloudrun/src/api/delete-site.ts
@@ -3,18 +3,15 @@ import {getCloudStorageClient} from './helpers/get-cloud-storage-client';
 export type DeleteSiteInput = {
 	bucketName: string;
 	siteName: string;
-	onAfterItemDeleted?: (data: {bucketName: string; itemName: string}) => void;
 };
 
 /**
  *
  * @description Deletes a deployed site from your S3 bucket. The opposite of deploySite().
- * @see [Documentation](https://remotion.dev/docs/lambda/deletesite)
- * @param options.bucketName The S3 bucket name where the site resides in.
- * @param options.siteName The ID of the site that you want to delete.
- * @param {AwsRegion} options.region The region in where the S3 bucket resides in.
- * @param options.onAfterItemDeleted Function that gets called after each file that gets deleted, useful for showing progress.
- * @returns {Promise<void>} Object containing info about how much space was freed.
+ * @see [Documentation](https://remotion.dev/docs/cloudrun/deletesite)
+ * @param options.bucketName The name of the Cloud Storage bucket where the site resides in.
+ * @param options.siteName The ID of the site to delete.
+ * @returns {Promise<void>} Nothing. Throws if the site failed to delete.
  */
 export const deleteSite = ({
 	bucketName,
