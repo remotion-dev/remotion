d37091f04c2ea59bc0fb08aa3cd68bce5d7a5e41
Jonny Burger <jonathanburger11@gmail.com>
2023-05-25 18:12:13 +0200

    Update CanvasOrLoading.tsx
    Former-commit-id: 4318334b0b86a16a445996a449cbe39f0a036351

--- packages/cli/src/editor/components/CanvasOrLoading.tsx
+++ packages/cli/src/editor/components/CanvasOrLoading.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {Internals} from 'remotion';
-import {BACKGROUND} from '../helpers/colors';
+import {BACKGROUND, LIGHT_TEXT} from '../helpers/colors';
 import {Canvas} from './Canvas';
+import {Spacing} from './layout';
+import {inlineCodeSnippet} from './Menu/styles';
 import {Spinner} from './Spinner';
 
 const container: React.CSSProperties = {
@@ -11,18 +13,48 @@ const container: React.CSSProperties = {
 	alignItems: 'center',
 	display: 'flex',
 	backgroundColor: BACKGROUND,
+	flexDirection: 'column',
 };
 
 export const CanvasOrLoading: React.FC = () => {
 	const resolved = Internals.useResolvedVideoConfig(null);
+	const [takesALongTime, setTakesALongTime] = useState(false);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setTakesALongTime(true);
+		}, 500);
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, []);
+
+	const style = useMemo(() => {
+		return {
+			...loaderLabel,
+			opacity: takesALongTime ? 1 : 0,
+			transition: 'opacity 0.3s',
+		};
+	}, [takesALongTime]);
 
 	if (!resolved) {
 		return (
 			<div style={container} className="css-reset">
 				<Spinner size={30} duration={1} />
+				<Spacing y={2} />
+				<div style={style}>
+					Running <code style={inlineCodeSnippet}>calculateMetadata()</code>...
+				</div>
 			</div>
 		);
 	}
 
 	return <Canvas />;
+};
+
+const loaderLabel: React.CSSProperties = {
+	fontSize: 14,
+	color: LIGHT_TEXT,
+	fontFamily: 'sans-serif',
+	lineHeight: 1.5,
 };
