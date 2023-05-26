5a26ad9cdd36167e6cb8ddff12df915084427c62
Jonny Burger <jonathanburger11@gmail.com>
2023-05-26 18:12:10 +0200

    fix bugs in PHP framework
    Former-commit-id: e6bfc869127addec3f2295cbdbc849a8824e41e9

--- packages/it-tests/src/monorepo/go-package.test.ts
+++ packages/it-tests/src/monorepo/go-package.test.ts
@@ -28,7 +28,7 @@ test("Go package should create the same payload as normal Lambda package", async
   const firstLine = goOutput.toString().split("\n")[0];
   const parsed = JSON.parse(firstLine);
 
-  const nativeVersion = await LambdaInternals.makeLambdaPayload({
+  const nativeVersion = await LambdaInternals.makeLambdaRenderMediaPayload({
     region: "us-east-1",
     composition: "react-svg",
     functionName: "remotion-render",
