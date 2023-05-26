92c2af73b1f75d70f42946b9ad68b180db42365b
Jonny Burger <jonathanburger11@gmail.com>
2023-05-26 09:34:02 +0200

    lambda: rename options to params
    Former-commit-id: af1cdb816956149dc7a02c504835d6c9563f8b3d

--- packages/lambda/src/api/delete-function.ts
+++ packages/lambda/src/api/delete-function.ts
@@ -10,8 +10,8 @@ export type DeleteFunctionInput = {
 /**
  * @description Deletes a function from AWS Lambda.
  * @see [Documentation](https://remotion.dev/docs/lambda/deletefunction)
- * @param options.region The region the function was deployed to.
- * @param options.functionName The name of the function.
+ * @param params.region The region the function was deployed to.
+ * @param params.functionName The name of the function.
  * @returns {Promise<void>} Nothing. Throws if the function failed to delete.
  */
 export const deleteFunction = async ({
