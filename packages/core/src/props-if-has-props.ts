f09006bb86f42a53245544b37d1daddc0e13ec29
Jonny Burger <jonathanburger11@gmail.com>
2023-05-30 17:54:36 +0200

    🤔
    Former-commit-id: 1ebc2b09b98ff7f15f3c765e9d059a569cdc156c

--- packages/core/src/props-if-has-props.ts
+++ packages/core/src/props-if-has-props.ts
@@ -1,24 +1,28 @@
 import type {AnyZodObject, z} from 'zod';
 
+type And<A extends boolean, B extends boolean> = A extends true
+	? B extends true
+		? true
+		: false
+	: false;
+
 export type PropsIfHasProps<
 	Schema extends AnyZodObject,
 	Props extends Record<string, unknown> | undefined
-> = AnyZodObject extends Schema
-	? {} extends Props
-		? {
-				// Neither props nor schema specified
-				defaultProps?: Props;
-		  }
-		: {
-				// Only props specified
-				defaultProps: Props;
-		  }
-	: {
-			// Only schema specified
-			defaultProps: RenamePropsIfHasProps<Schema, Props>;
+> = And<
+	AnyZodObject extends Schema ? true : false,
+	{} extends Props ? true : false
+> extends true
+	? {
+			// Neither props nor schema specified
+			defaultProps?: {};
+	  }
+	: // All the other cases
+	  {
+			defaultProps: InferProps<Schema, Props>;
 	  };
 
-export type RenamePropsIfHasProps<
+export type InferProps<
 	Schema extends AnyZodObject,
 	Props
 > = AnyZodObject extends Schema
