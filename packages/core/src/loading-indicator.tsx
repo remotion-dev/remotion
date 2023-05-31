ecbee447e33b1db2c9a9cacf7d90895cb62edaec
Jonny Burger <jonathanburger11@gmail.com>
2023-05-31 17:01:07 +0200

    Update loading-indicator.tsx
    Former-commit-id: 1d0d7aa8d51d0e35ae14f68b1c2cbf7c3fdd589d

--- packages/core/src/loading-indicator.tsx
+++ packages/core/src/loading-indicator.tsx
@@ -48,7 +48,7 @@ export const Loading: React.FC = () => {
 					d="M 2 172 a 196 100 0 0 0 195 5 A 196 240 0 0 0 100 2.259 A 196 240 0 0 0 2 172 z"
 				/>
 			</svg>
-			<p style={label}>Loading...</p>
+			<p style={label}>Resolving {'<Suspense>'}...</p>
 		</AbsoluteFill>
 	);
 };
