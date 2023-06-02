bc52a3f45c04bef94101b05bb46e898d46adc43e
Jonny Burger <jonathanburger11@gmail.com>
2023-06-02 17:30:25 +0200

    fix color picker not working

--- packages/example/src/SchemaTest/index.tsx
+++ packages/example/src/SchemaTest/index.tsx
@@ -1,3 +1,4 @@
+import {zColor} from '@remotion/zod-types';
 import React from 'react';
 import {AbsoluteFill, Sequence} from 'remotion';
 import {z} from 'zod';
@@ -5,6 +6,7 @@ import {z} from 'zod';
 export const schemaTestSchema = z.object({
 	title: z.string().nullable(),
 	delay: z.number().min(0).max(1000).step(0.1),
+	color: zColor(),
 });
 
 export const schemaArrayTestSchema = z.array(z.number());
@@ -24,6 +26,7 @@ export const ArrayTest: React.FC = () => {
 export const SchemaTest: React.FC<z.infer<typeof schemaTestSchema>> = ({
 	delay,
 	title,
+	color,
 }) => {
 	return (
 		<AbsoluteFill
@@ -33,7 +36,9 @@ export const SchemaTest: React.FC<z.infer<typeof schemaTestSchema>> = ({
 				fontSize: 80,
 			}}
 		>
-			<Sequence from={delay}>{title}</Sequence>
+			<Sequence from={delay}>
+				<span style={{color}}>{title}</span>
+			</Sequence>
 		</AbsoluteFill>
 	);
 };
