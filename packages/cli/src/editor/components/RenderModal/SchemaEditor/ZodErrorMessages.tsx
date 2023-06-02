dd92ac7009263c64d9a78a8f31f90e9dfdb4f794
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-06-02 08:51:54 +0200

    uniform error styling

--- packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodErrorMessages.tsx
+++ packages/cli/src/editor/components/RenderModal/SchemaEditor/ZodErrorMessages.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import type {z} from 'zod';
-import {LIGHT_TEXT} from '../../../helpers/colors';
+import {FAIL_COLOR} from '../../../helpers/colors';
+import {Spacing} from '../../layout';
+import {WarningTriangle} from '../../NewComposition/ValidationMessage';
 
 const label: React.CSSProperties = {
-	fontSize: 14,
-	color: LIGHT_TEXT,
+	fontSize: 13,
+	color: 'white',
 	fontFamily: 'sans-serif',
+	display: 'flex',
+	alignItems: 'center',
 };
 
-const code: React.CSSProperties = {
-	...label,
-	fontFamily: 'monospace',
+const triangleStyle: React.CSSProperties = {
+	width: 12,
+	height: 12,
+	flexShrink: 0,
+	fill: FAIL_COLOR,
 };
 
 export const ZodErrorMessages: React.FC<{
@@ -25,11 +31,10 @@ export const ZodErrorMessages: React.FC<{
 			{zodValidationResult.error.errors.map((error) => {
 				return (
 					<div key={error.path.join('.')} style={label}>
-						-{' '}
-						<code style={code}>
-							{error.path.length === 0 ? 'Root' : error.path.join('.')}
-						</code>
-						: {error.message}
+						<WarningTriangle style={triangleStyle} />
+						<Spacing x={1} />
+						{error.path.length === 0 ? 'Root' : error.path.join('.')}:{' '}
+						{error.message}
 					</div>
 				);
 			})}
