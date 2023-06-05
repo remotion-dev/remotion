f9ff33c82660dbeaeb3b1fb0ed67891625cdd064
Jonny Burger <jonathanburger11@gmail.com>
2023-06-05 15:24:20 +0200

    fix a test
    Former-commit-id: 93193fe897493933032ad04fd31b68cf440abdfa

--- packages/core/src/test/use-media-in-timeline.test.tsx
+++ packages/core/src/test/use-media-in-timeline.test.tsx
@@ -2,11 +2,12 @@
  * @vitest-environment jsdom
  */
 import type {RefObject} from 'react';
-import React from 'react';
+import React, {useMemo} from 'react';
 import {afterAll, beforeAll, expect, test, vitest} from 'vitest';
-import type {CompositionManagerContext} from '../CompositionManagerContext.js';
 import {CompositionManager} from '../CompositionManagerContext.js';
 import {ResolveCompositionConfig} from '../ResolveCompositionConfig.js';
+import type {SequenceManagerContext} from '../SequenceManager.js';
+import {SequenceManager} from '../SequenceManager.js';
 import {useMediaInTimeline} from '../use-media-in-timeline.js';
 import * as useVideoConfigModule from '../use-video-config.js';
 import {renderHook} from './render-hook.js';
@@ -33,20 +34,24 @@ test('useMediaInTimeline registers and unregisters new sequence', () => {
 	const unregisterSequence = vitest.fn();
 	const wrapper: React.FC<{
 		children: React.ReactNode;
-	}> = ({children}) => (
-		<CompositionManager.Provider
-			value={
-				// eslint-disable-next-line react/jsx-no-constructed-context-values
-				{
-					...mockCompositionContext,
-					registerSequence,
-					unregisterSequence,
-				} as unknown as CompositionManagerContext
-			}
-		>
-			<ResolveCompositionConfig>{children}</ResolveCompositionConfig>
-		</CompositionManager.Provider>
-	);
+	}> = ({children}) => {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const sequenceManagerContext: SequenceManagerContext = useMemo(() => {
+			return {
+				registerSequence,
+				unregisterSequence,
+				sequences: [],
+			};
+		}, []);
+
+		return (
+			<CompositionManager.Provider value={mockCompositionContext}>
+				<SequenceManager.Provider value={sequenceManagerContext}>
+					<ResolveCompositionConfig>{children}</ResolveCompositionConfig>
+				</SequenceManager.Provider>
+			</CompositionManager.Provider>
+		);
+	};
 
 	const audioRef = {
 		current: {volume: 0.5},
