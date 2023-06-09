443cc8a09131c1ae4c93c08cddaa44b31c2a0f72
Matt McGillivray <m.mcgillivray@outlook.com>
2023-06-09 17:25:21 +0800

    typo in cloudrun-permissions.list

--- packages/cloudrun/src/gcpInstaller/main.tf
+++ packages/cloudrun/src/gcpInstaller/main.tf
@@ -35,7 +35,7 @@ resource "google_project_iam_custom_role" "remotion_sa" {
   role_id     = "RemotionSA"
   title       = "Remotion API Service Account"
   description = "Allow the service account to manage necessary resources for Remotion Cloud Run rendering."
-  permissions = local.cloudrun-permissions.list
+  permissions = local.cloudrun-permissions
 }
 
 # Create a service account
