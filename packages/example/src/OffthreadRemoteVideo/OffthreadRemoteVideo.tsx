578ae0414c64107d668a376b2adb7aaf6fafd676
Jonny Burger <jonathanburger11@gmail.com>
2023-06-29 13:30:31 +0200

    Discard changes to packages/example/src/OffthreadRemoteVideo/OffthreadRemoteVideo.tsx

--- packages/example/src/OffthreadRemoteVideo/OffthreadRemoteVideo.tsx
+++ packages/example/src/OffthreadRemoteVideo/OffthreadRemoteVideo.tsx
@@ -1,9 +1,12 @@
-import {AbsoluteFill, OffthreadVideo, staticFile} from 'remotion';
+import {interpolate, OffthreadVideo} from 'remotion';
 
 export const OffthreadRemoteVideo: React.FC = () => {
 	return (
-		<AbsoluteFill>
-			<OffthreadVideo src={staticFile('bigbuckbunny.mp4')} />
-		</AbsoluteFill>
+		<OffthreadVideo
+			volume={(f) =>
+				interpolate(f, [0, 500], [1, 0], {extrapolateRight: 'clamp'})
+			}
+			src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+		/>
 	);
 };
