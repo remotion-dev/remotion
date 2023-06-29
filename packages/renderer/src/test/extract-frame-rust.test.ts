586e72d94a2cbf6f9e8e7a64bcf972e9d3de9cba
Jonny Burger <jonathanburger11@gmail.com>
2023-06-29 13:12:16 +0200

    remove other testcase

--- packages/renderer/src/test/extract-frame-rust.test.ts
+++ packages/renderer/src/test/extract-frame-rust.test.ts
@@ -328,6 +328,25 @@ test('Last frame should be fast', async () => {
 	await compositor.waitForDone();
 });
 
+test('Should get from a screen recording', async () => {
+	const compositor = startLongRunningCompositor(
+		getIdealMaximumFrameCacheItems(),
+		'info',
+		false
+	);
+
+	const data = await compositor.executeCommand('ExtractFrame', {
+		input: exampleVideos.screenrecording,
+		time: 0.5,
+		transparent: false,
+	});
+
+	expect(data.length).toBe(15230038);
+
+	compositor.finishCommands();
+	await compositor.waitForDone();
+});
+
 test('Two different starting times should not result in big seeking', async () => {
 	const compositor = startLongRunningCompositor(300, 'info', false);
 
