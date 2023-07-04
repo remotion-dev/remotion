65b7706e1cab92a42110e3c49b19dac0946358e9
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-07-04 14:28:03 +0200

    slashes no longer get encoded
    Former-commit-id: c3bf59939e077b06d83798dd52fa5aecc9bb0636

--- packages/core/src/test/static-file-safe-uri.test.ts
+++ packages/core/src/test/static-file-safe-uri.test.ts
@@ -3,10 +3,20 @@ import {staticFile} from '../static-file.js';
 
 test('staticFile() should convert # into %23', () => {
 	const unsafeName = 'test/example#music';
-	expect(staticFile(unsafeName)).toBe('/test%2Fexample%23music');
+	expect(staticFile(unsafeName)).toBe('/test/example%23music');
 });
 
 test('staticFile() passing already encoded path should give warning', () => {
 	const unsafeName = '/test%2Fexample%23music';
-	expect(staticFile(unsafeName)).toBe('/%2Ftest%252Fexample%2523music');
+	expect(staticFile(unsafeName)).toBe('/test%252Fexample%2523music');
+});
+
+test('staticFile() should not encode slashes', () => {
+	const nameWithSlashes = '/test/example/word';
+	expect(staticFile(nameWithSlashes)).toBe(nameWithSlashes);
+});
+
+test('staticFile() should encode all problematic characters except slashes', () => {
+	const nameWithSlashes = '/test/#example/$word';
+	expect(staticFile(nameWithSlashes)).toBe('/test/%23example/%24word');
 });
