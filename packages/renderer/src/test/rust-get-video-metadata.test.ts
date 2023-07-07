442fcbca83b8a98ca5e0122a6d0fb728525d2c19
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-07-08 09:53:46 +1000

    include test for audio
    Former-commit-id: e9030e4462ae3d94e589cdec59409fd172bc6c9c

--- packages/renderer/src/test/rust-get-video-metadata.test.ts
+++ packages/renderer/src/test/rust-get-video-metadata.test.ts
@@ -53,3 +53,33 @@ test('Should return an error due to non existing file', async () => {
 		);
 	}
 });
+
+test('Should return an error due to using a audio file', async () => {
+	const compositor = startLongRunningCompositor(
+		getIdealMaximumFrameCacheItems(),
+		'info',
+		false
+	);
+
+	const audioFile = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'example',
+		'src',
+		'resources',
+		'sound1.mp3'
+	);
+	expect(existsSync(audioFile)).toEqual(true);
+
+	try {
+		await compositor.executeCommand('GetVideoMetadata', {
+			src: audioFile,
+		});
+	} catch (err) {
+		expect((err as Error).message).toContain(
+			'Compositor error: No video stream found'
+		);
+	}
+});
