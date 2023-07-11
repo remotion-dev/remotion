c0db50d54ac65c4f05b409228f5724d05d45d289
Jonny Burger <jonathanburger11@gmail.com>
2023-07-11 11:24:34 +0200

    fix source maps

--- packages/renderer/src/get-bundle-url-from-serve-url.ts
+++ packages/renderer/src/get-bundle-url-from-serve-url.ts
@@ -5,3 +5,9 @@ export const getBundleUrlFromServeUrl = (serveUrl: string) => {
 	const url = serveUrl.substring(0, index + 1) + Internals.bundleName;
 	return url;
 };
+
+export const getBundleMapUrlFromServeUrl = (serveUrl: string) => {
+	const index = serveUrl.lastIndexOf('/');
+	const url = serveUrl.substring(0, index + 1) + Internals.bundleMapName;
+	return url;
+};
