c074f2b9111a1a144a1cff8e420c6b5cc8428139
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-07-16 11:39:09 +1000

    add test for progress request
    Former-commit-id: a0a6873e91e2f63e8a47df1fc1a194050645a2cb

--- packages/lambda-python/remotion_lambda_sdk/__init__.py
+++ packages/lambda-python/remotion_lambda_sdk/__init__.py
@@ -1,3 +1,4 @@
 # pylint: disable-all
 from .models import RenderParams, RenderProgress, RenderResponse, RenderProgressParams
 from .remotionclient import RemotionClient
+from .version import VERSION
---
+++ packages/lambda-python/setup.py
@@ -0,0 +1,20 @@
+import os
+import setuptools
+from remotion_lambda_sdk.version import VERSION
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+setuptools.setup(
+    name="remotion_lambda_sdk",
+    version=VERSION,
+    author="Johny Burger",
+    author_email="jonny@remotion.dev",
+    description="Remotion Lambda client",
+    long_description_content_type="text/markdown",
+    url="https://github.com/remotion-dev/remotion/tree/main/packages/lambda-python",
+    classifiers=[
+
+    ],
+    python_requires='>=3.6',
+)
