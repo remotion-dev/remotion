62e5db649c439961a60b354c9427395d82f24c90
Jonny Burger <jonathanburger11@gmail.com>
2023-07-17 13:13:59 +0200

    renderer function

--- packages/lambda/src/functions/helpers/make-timeout-message.ts
+++ packages/lambda/src/functions/helpers/make-timeout-message.ts
@@ -1,5 +1,7 @@
-import type {RenderMetadata} from '../../defaults';
+import {LambdaRoutines, type RenderMetadata} from '../../defaults';
 import {DOCS_URL} from '../../shared/docs-url';
+import { getCloudwatchStreamUrl } from '../../shared/get-aws-urls';
+import { getCurrentRegionInFunction } from './get-current-region';
 
 const makeChunkMissingMessage = ({
 	missingChunks,
@@ -33,18 +35,41 @@ export const makeTimeoutMessage = ({
 	timeoutInMilliseconds,
 	missingChunks,
 	renderMetadata,
+	renderId,
 }: {
 	timeoutInMilliseconds: number;
 	missingChunks: number[];
 	renderMetadata: RenderMetadata;
+	renderId: string;
 }) => {
+
+	const cloudWatchRendererUrl = getCloudwatchStreamUrl({
+		renderId,
+		functionName: process.env.AWS_LAMBDA_FUNCTION_NAME as string,
+		method: LambdaRoutines.renderer,
+		region: getCurrentRegionInFunction(),
+		rendererFunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME as string
+	})
+
+	const cloudWatchLaunchUrl = getCloudwatchStreamUrl({
+		renderId,
+		functionName: process.env.AWS_LAMBDA_FUNCTION_NAME as string,
+		method: LambdaRoutines.launch,
+		region: getCurrentRegionInFunction(),
+		rendererFunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME as string
+	})
+
 	const message = [
 		`The main function timed out after ${timeoutInMilliseconds}ms.`,
 		`Consider increasing the timeout of your function.`,
 		makeChunkMissingMessage({missingChunks, renderMetadata}),
-		`You can use the "--timeout" parameter when deploying a function via CLI, or the "timeoutInSeconds" parameter when using the deployFunction() API.`,
+		`▸ You can use the "--timeout" parameter when deploying a function via CLI, or the "timeoutInSeconds" parameter when using the deployFunction() API.`,
 		`${DOCS_URL}/docs/lambda/cli/functions#deploy`,
-		'Get help on debugging this error:',
+		'▸ Visit the logs for the main function:',
+		cloudWatchLaunchUrl,
+		'▸ Visit the logs for the renderer functions:',
+		cloudWatchRendererUrl,
+		'▸ Get help on debugging this error:',
 		`${DOCS_URL}/docs/lambda/troubleshooting/debug`,
 	].join('\n');
 
