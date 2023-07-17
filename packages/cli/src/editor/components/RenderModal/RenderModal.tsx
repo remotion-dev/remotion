0c3baa7ccc24d2ab3bce4f709d27c4b039a13142
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-07-17 10:24:06 +0200

    dot grey while saving
    Former-commit-id: 27bf3e52cd95dd67f8a87a0f55d99abcafc6797d

--- packages/cli/src/editor/components/RenderModal/RenderModal.tsx
+++ packages/cli/src/editor/components/RenderModal/RenderModal.tsx
@@ -265,7 +265,7 @@ const RenderModal: React.FC<
 
 	const [state, dispatch] = useReducer(reducer, initialState);
 	const [unclampedFrame, setFrame] = useState(() => initialFrame);
-
+	const [saving, setSaving] = useState<boolean>(false);
 	const [stillImageFormat, setStillImageFormat] = useState<StillImageFormat>(
 		() => initialStillImageFormat
 	);
@@ -1067,6 +1067,8 @@ const RenderModal: React.FC<
 							unresolvedComposition={unresolvedComposition}
 							mayShowSaveButton={false}
 							propsEditType="input-props"
+							saving={saving}
+							setSaving={setSaving}
 						/>
 					) : (
 						<RenderModalAdvanced
