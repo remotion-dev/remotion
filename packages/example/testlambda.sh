7be4296e8e38dc4f7a0a8d0b1266b985afd3922e
Jonny Burger <jonathanburger11@gmail.com>
2023-07-17 14:41:36 +0200

    revert to render

--- packages/example/testlambda.sh
+++ packages/example/testlambda.sh
@@ -5,6 +5,6 @@ npm run buildlambda
 cd ..
 cd example
 pnpm exec remotion lambda functions rmall -f
-pnpm exec remotion lambda functions deploy --memory=3000
+pnpm exec remotion lambda functions deploy --memory=3000 --timeout=30
 pnpm exec remotion lambda sites create --site-name=testbed-v6 --log=verbose
-pnpm exec remotion lambda still testbed-v6 react-svg --out-name=ggg.mp4 ggg.mp4 --log=verbose 
+pnpm exec remotion lambda render testbed-v6 react-svg --out-name=ggg.mp4 ggg.mp4 --log=verbose 
