588d4052ef16a45f23e488341c0721b9724c43a5
Jonny Burger <jonathanburger11@gmail.com>
2023-07-17 16:09:09 +0200

    v4.0.7
    Former-commit-id: 91a4bd0ea138c736ac2719ca9304a03d9243493e

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.6"
+const VERSION = "4.0.7"
--- packages/lambda-php/src/Version.php
+++ packages/lambda-php/src/Version.php
@@ -1,4 +1,4 @@
 <?php 
 namespace Remotion\LambdaPhp;
 
-const VERSION = "4.0.6";
+const VERSION = "4.0.7";
