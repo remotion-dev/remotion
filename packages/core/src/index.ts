fa745115d45ed43d88e9fc418a947f1ac8060496
Jonny Burger <jonathanburger11@gmail.com>
2023-07-24 18:03:30 +0200

    better error message instead of "waiting xms for"
    Former-commit-id: b7449f07e3d93305e7a25c92652d408d3cd0709f

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,6 +11,9 @@ import type {VideoConfig} from './video-config.js';
 declare global {
 	interface Window {
 		remotion_renderReady: boolean;
+		remotion_delayRenderTimeouts: {
+			[key: string]: {label: string | null; timeout: number | NodeJS.Timeout};
+		};
 		remotion_cancelledError: string | undefined;
 		remotion_getCompositionNames: () => string[];
 		getStaticCompositions: () => Promise<VideoConfig[]>;
