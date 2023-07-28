ede8a434fba0eb26554959e3e6b43ab2b02321ba
Jonny Burger <jonathanburger11@gmail.com>
2023-07-28 15:32:54 +0200

    live update when file system changes
    Former-commit-id: c68c4b828e2f5c35c4af29bc7940544046ed92dc

--- packages/cli/src/editor/components/AssetSelector.tsx
+++ packages/cli/src/editor/components/AssetSelector.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {getStaticFiles} from 'remotion';
+import {subscribeToEvent} from '../../event-source';
 import {BACKGROUND} from '../helpers/colors';
 import {useZIndex} from '../state/z-index';
 import {AssetSelectorItem} from './AssetSelectorItem';
@@ -19,7 +20,18 @@ const list: React.CSSProperties = {
 export const AssetSelector: React.FC = () => {
 	const {tabIndex} = useZIndex();
 
-	const staticFiles = React.useMemo(() => getStaticFiles(), []);
+	const [staticFiles, setStaticFiles] = React.useState(() => getStaticFiles());
+
+	useEffect(() => {
+		const onUpdate = () => {
+			setStaticFiles(getStaticFiles());
+		};
+
+		const unsub = subscribeToEvent('new-public-folder', onUpdate);
+		return () => {
+			unsub();
+		};
+	}, []);
 
 	return (
 		<div style={container}>
