19f2b5cd1317141f7e2c948f7467df87cb146c5b
Jonny Burger <jonathanburger11@gmail.com>
2023-07-31 18:16:46 +0200

    better logging on lambda
    Former-commit-id: 87f7f779005bdddde89410c9c0aaeeef5be36920

---
+++ packages/lambda/src/functions/helpers/on-downloads-logger.ts
@@ -0,0 +1,36 @@
+import type {RenderMediaOnDownload} from '@remotion/renderer';
+
+export const onDownloadsHelper = (): RenderMediaOnDownload => {
+	const downloads: Record<string, number> = {};
+
+	return (src: string) => {
+		console.log('Downloading', src);
+		downloads[src] = 0;
+		return ({percent, downloaded}) => {
+			if (percent === null) {
+				console.log(
+					`Download progress (${src}): ${downloaded} bytes. Don't know final size of download, no Content-Length header.`
+				);
+				return;
+			}
+
+			if (
+				// Only report every 10% change
+				downloads[src] > percent - 0.1 &&
+				percent !== 1
+			) {
+				return;
+			}
+
+			downloads[src] = percent;
+			console.log(
+				`Download progress (${src}): ${downloaded} bytes, ${(
+					percent * 100
+				).toFixed(1)}%`
+			);
+			if (percent === 1) {
+				console.log(`Download complete: ${src}`);
+			}
+		};
+	};
+};
