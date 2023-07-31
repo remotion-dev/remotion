7e45e0bc19972613a120d9f6b18931b8e3008038
Jonny Burger <jonathanburger11@gmail.com>
2023-07-31 16:07:31 +0200

    update clients

--- packages/example/testlambdaintegrations.mjs
+++ packages/example/testlambdaintegrations.mjs
@@ -1,23 +1,29 @@
 import {execSync} from 'child_process';
 
-const functionName = execSync(`pnpm exec remotion lambda functions ls -q`);
+const functionName = execSync(`pnpm exec remotion lambda functions ls -q`)
+	.toString('utf8')
+	.trim();
 
+console.log('=== PHP ===');
 execSync(`php src/render.php`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
-		REMOTION_APP_FUNCTION_NAME: functionName.toString('utf8'),
+		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
 	cwd: '../lambda-php-example',
 	stdio: 'inherit',
 });
+
+console.log('=== Python ===');
 execSync(`python testclient.py`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
 		REMOTION_APP_REGION: 'eu-central-1',
-		REMOTION_APP_FUNCTION_NAME: functionName.toString('utf8'),
+		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
 	cwd: '../lambda-python',
