cdc4993d7c4992a4aa2867c00f67226ed8992351
Jonny Burger <jonathanburger11@gmail.com>
2023-08-01 08:57:01 +0200

    v4.0.15

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.14"
+const VERSION = "4.0.15"
--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.14"
+VERSION = "4.0.15"
