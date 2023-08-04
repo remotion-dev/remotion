aba6ff7798ce860fd36a97e474e6719c74b6f6ce
Jonny Burger <jonathanburger11@gmail.com>
2023-08-04 15:57:42 +0200

    make it faster and simpler
    Former-commit-id: 95790b74e6964a9e363a3087992b5d08375eb719

--- packages/renderer/rust/frame_cache.rs
+++ packages/renderer/rust/frame_cache.rs
@@ -197,7 +197,7 @@ impl FrameCache {
                     (self.items[best_item.unwrap()].asked_time - time).abs();
                 if has_asked_time_before && self.items[best_item.unwrap()].asked_time <= time {
                     _print_debug(&format!(
-                        "Found timestamp with too much threshold, but will let it pass because no closer frames found {} {}",
+                        "Found timestamp with too much threshold, but will let it pass because no closer frames found. difference = {}, threshold = {}",
                         asked_time_difference, threshold
                     ))?;
                     // Fall through to success case
