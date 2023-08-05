70e02a7ea9c0417ee0602d466a0b32a894aceb09
Jonny Burger <jonathanburger11@gmail.com>
2023-08-05 21:47:31 +0200

    silences
    Former-commit-id: 6634b0c74542c080760ed05ef4bc200abf21b1dd

--- packages/renderer/rust/commands/mod.rs
+++ packages/renderer/rust/commands/mod.rs
@@ -55,11 +55,7 @@ pub fn execute_command(opts: CliInputCommandPayload) -> Result<Vec<u8>, ErrorWit
                 "silencedetect=n={}dB:d={}",
                 _command.noiseThresholdInDecibel, _command.minDuration
             );
-            let res = get_silences::get_silences(
-                _command.src,
-                "out.mp3".to_string(),
-                filter.to_string(),
-            )?;
+            let res = get_silences::get_silences(_command.src, filter.to_string())?;
             let str = serde_json::to_string(&res)?;
             Ok(str.as_bytes().to_vec())
         }
