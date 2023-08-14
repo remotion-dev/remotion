b72be0cc23597fe3ca7c9f005ec6df5c4f400798
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-08-14 14:26:28 +0200

    rm duplicate arg
    Former-commit-id: a8834b7c64e738e8b4cf76e0a2c578463698563a

--- packages/lambda/src/cli/commands/render/render.ts
+++ packages/lambda/src/cli/commands/render/render.ts
@@ -93,7 +93,6 @@ export const renderCommand = async (args: string[], remotionRoot: string) => {
 
 		const {compositionId} =
 			await CliInternals.getCompositionWithDimensionOverride({
-				args,
 				args: args.slice(1),
 				compositionIdFromUi: null,
 				browserExecutable,
