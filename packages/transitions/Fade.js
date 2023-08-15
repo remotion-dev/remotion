9497fa347d0d7e126c9f48544ef59339f4c89549
Jonny Burger <jonathanburger11@gmail.com>
2023-08-15 16:43:00 +0200

    ESM imports

---
+++ packages/transitions/Fade.js
@@ -0,0 +1,1 @@
+export * from './dist/presentations/Fade.js'
--- packages/transitions/src/index.ts
+++ packages/transitions/src/index.ts
@@ -1,7 +1,3 @@
-// Presentations
-export {makeFadePresentation} from './presentations/Fade.js';
-export {makeSlidePresentation} from './presentations/Slide.js';
-export {makeWipePresentation} from './presentations/Wipe.js';
 // Timings
 export {makeLinearTiming} from './timings/linear.js';
 export {makeSpringTiming} from './timings/spring.js';
