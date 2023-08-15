cd178e55006e57f145f2a62b67e17d24d091966c
Jonny Burger <jonathanburger11@gmail.com>
2023-08-15 11:25:44 +0200

    slide transition

--- packages/transitions/src/index.ts
+++ packages/transitions/src/index.ts
@@ -1,4 +1,6 @@
+export {SlideTransition} from './presentations/Slide';
 export * from './presentations/Triangle';
+export {TriangleTransition} from './presentations/Triangle';
 export {makeEasingTiming} from './timings/easing';
 export {makeSpringTiming} from './timings/spring';
 export {TransitionSeries} from './TransitionSeries';
