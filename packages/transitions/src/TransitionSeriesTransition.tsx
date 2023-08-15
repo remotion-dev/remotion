d78cbfe59824470eb1394c1b25eaa2650a4a7ed7
Jonny Burger <jonathanburger11@gmail.com>
2023-08-15 11:03:48 +0200

    transition series
    Former-commit-id: 10cc53d478d7c1b0a115cb9f419502e28f8652c0

--- packages/transitions/src/TransitionSeriesTransition.tsx
+++
@@ -1,8 +0,0 @@
-import type React from 'react';
-import type {TransitionSeriesTransitionProps} from './types';
-
-export const TransitionSeriesTransition: React.FC<
-	TransitionSeriesTransitionProps
-> = () => {
-	return null;
-};
