facf65057b2621a68f748275516ffbc63227f735
Jonny Burger <jonathanburger11@gmail.com>
2023-08-15 14:04:03 +0200

    allow props from sequence
    Former-commit-id: 3240af4f70e98825f11ea271ff4766e58a551f9b

--- packages/example/src/Transitions/index.tsx
+++ packages/example/src/Transitions/index.tsx
@@ -8,66 +8,64 @@ import {AbsoluteFill, Easing} from 'remotion';
 
 export const BasicTransition: React.FC = () => {
 	return (
-		<AbsoluteFill>
-			<TransitionSeries>
-				<TransitionSeries.Sequence durationInFrames={90}>
-					<AbsoluteFill
-						style={{
-							backgroundColor: 'orange',
-							opacity: 0.5,
-							justifyContent: 'center',
-							alignItems: 'center',
-							fontSize: 200,
-							color: 'white',
-						}}
-					>
-						A
-					</AbsoluteFill>
-				</TransitionSeries.Sequence>
-				<TransitionSeries.Transition
-					presentation={makeWipePresentation({origin: 'from-bottom-left'})}
-					timing={makeLinearTiming({
-						durationInFrames: 30,
-						easing: Easing.bounce,
-					})}
-				/>
-				<TransitionSeries.Sequence durationInFrames={90}>
-					<AbsoluteFill
-						style={{
-							backgroundColor: 'green',
-							opacity: 0.5,
-							justifyContent: 'center',
-							alignItems: 'center',
-							fontSize: 200,
-							color: 'white',
-						}}
-					>
-						B
-					</AbsoluteFill>
-				</TransitionSeries.Sequence>
-				<TransitionSeries.Transition
-					presentation={makeWipePresentation({origin: 'from-left'})}
-					timing={makeSpringTiming({
-						config: {
-							damping: 200,
-						},
-					})}
-				/>
-				<TransitionSeries.Sequence durationInFrames={90}>
-					<AbsoluteFill
-						style={{
-							backgroundColor: 'blue',
-							opacity: 0.5,
-							justifyContent: 'center',
-							alignItems: 'center',
-							fontSize: 200,
-							color: 'white',
-						}}
-					>
-						C
-					</AbsoluteFill>
-				</TransitionSeries.Sequence>
-			</TransitionSeries>
-		</AbsoluteFill>
+		<TransitionSeries from={30}>
+			<TransitionSeries.Sequence durationInFrames={90}>
+				<AbsoluteFill
+					style={{
+						backgroundColor: 'orange',
+						opacity: 0.5,
+						justifyContent: 'center',
+						alignItems: 'center',
+						fontSize: 200,
+						color: 'white',
+					}}
+				>
+					A
+				</AbsoluteFill>
+			</TransitionSeries.Sequence>
+			<TransitionSeries.Transition
+				presentation={makeWipePresentation({origin: 'from-bottom-left'})}
+				timing={makeLinearTiming({
+					durationInFrames: 30,
+					easing: Easing.bounce,
+				})}
+			/>
+			<TransitionSeries.Sequence durationInFrames={90}>
+				<AbsoluteFill
+					style={{
+						backgroundColor: 'green',
+						opacity: 0.5,
+						justifyContent: 'center',
+						alignItems: 'center',
+						fontSize: 200,
+						color: 'white',
+					}}
+				>
+					B
+				</AbsoluteFill>
+			</TransitionSeries.Sequence>
+			<TransitionSeries.Transition
+				presentation={makeWipePresentation({origin: 'from-left'})}
+				timing={makeSpringTiming({
+					config: {
+						damping: 200,
+					},
+				})}
+			/>
+			<TransitionSeries.Sequence durationInFrames={90}>
+				<AbsoluteFill
+					style={{
+						backgroundColor: 'blue',
+						opacity: 0.5,
+						justifyContent: 'center',
+						alignItems: 'center',
+						fontSize: 200,
+						color: 'white',
+					}}
+				>
+					C
+				</AbsoluteFill>
+			</TransitionSeries.Sequence>
+		</TransitionSeries>
 	);
 };
