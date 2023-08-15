1b7ce65b0c508d299dca5cd561eb80456bf88e36
Jonny Burger <jonathanburger11@gmail.com>
2023-08-15 15:50:52 +0200

    fade presentation

--- packages/transitions/src/index.ts
+++ packages/transitions/src/index.ts
@@ -1,4 +1,8 @@
+// Presentations
+export {makeFadePresentation} from './presentations/Fade';
 export {makeWipePresentation} from './presentations/Wipe';
+// Timings
 export {makeLinearTiming} from './timings/linear';
 export {makeSpringTiming} from './timings/spring';
+// Component
 export {TransitionSeries} from './TransitionSeries';
