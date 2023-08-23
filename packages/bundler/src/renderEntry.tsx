7bad84a2e09d6a0f22f0ec99556d34a31af00b1d
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-08-23 08:53:34 +0200

    replaced internals
    Former-commit-id: 487dabfd7b555c787647f7474c4fc30664c6e983

--- packages/bundler/src/renderEntry.tsx
+++ packages/bundler/src/renderEntry.tsx
@@ -14,6 +14,7 @@ import {
 	continueRender,
 	delayRender,
 	getInputProps,
+	getRemotionEnvironment,
 	Internals,
 	VERSION,
 } from 'remotion';
@@ -282,8 +283,7 @@ if (typeof window !== 'undefined') {
 		const compositions = getUnevaluatedComps();
 
 		const inputProps =
-			typeof window === 'undefined' ||
-			Internals.getRemotionEnvironment().isPlayer
+			typeof window === 'undefined' || getRemotionEnvironment().isPlayer
 				? {}
 				: getInputProps() ?? {};
 
@@ -340,8 +340,7 @@ if (typeof window !== 'undefined') {
 		);
 
 		const inputProps =
-			typeof window === 'undefined' ||
-			Internals.getRemotionEnvironment().isPlayer
+			typeof window === 'undefined' || getRemotionEnvironment().isPlayer
 				? {}
 				: getInputProps() ?? {};
 
