b2ea932c4a9f952d21a768a2cc1ec10959ebb0ef
Jonny Burger <jonathanburger11@gmail.com>
2023-08-24 10:30:17 +0200

    lambda
    Former-commit-id: 316c7e2a32d23765d0d9e92a267108d12f85d62e

--- packages/lambda/src/api/render-media-on-lambda.ts
+++ packages/lambda/src/api/render-media-on-lambda.ts
@@ -10,6 +10,7 @@ import type {
 	X264Preset,
 } from '@remotion/renderer';
 import type {BrowserSafeApis} from '@remotion/renderer/client';
+import {wrapWithErrorHandling} from '@remotion/renderer/src/wrap-with-error-handling';
 import type {AwsRegion} from '../pricing/aws-regions';
 import {callLambda} from '../shared/call-lambda';
 import type {OutNameInput, Privacy} from '../shared/constants';
@@ -76,28 +77,8 @@ export type RenderMediaOnLambdaOutput = {
 	folderInS3Console: string;
 };
 
-/**
- * @description Triggers a render on a lambda given a composition and a lambda function.
- * @see [Documentation](https://remotion.dev/docs/lambda/rendermediaonlambda)
- * @param params.functionName The name of the Lambda function that should be used
- * @param params.serveUrl The URL of the deployed project
- * @param params.composition The ID of the composition which should be rendered.
- * @param params.inputProps The input props that should be passed to the composition.
- * @param params.codec The media codec which should be used for encoding.
- * @param params.imageFormat In which image format the frames should be rendered. Default "jpeg"
- * @param params.crf The constant rate factor to be used during encoding.
- * @param params.envVariables Object containing environment variables to be inserted into the video environment
- * @param params.proResProfile The ProRes profile if rendering a ProRes video
- * @param params.jpegQuality JPEG quality if JPEG was selected as the image format.
- * @param params.region The AWS region in which the media should be rendered.
- * @param params.maxRetries How often rendering a chunk may fail before the media render gets aborted. Default "1"
- * @param params.logLevel Level of logging that Lambda function should perform. Default "info".
- * @param params.webhook Configuration for webhook called upon completion or timeout of the render.
- * @returns {Promise<RenderMediaOnLambdaOutput>} See documentation for detailed structure
- */
-
-export const renderMediaOnLambda = async (
-	input: RenderMediaOnLambdaInput,
+const renderMediaOnLambdaRaw = async (
+	input: RenderMediaOnLambdaInput
 ): Promise<RenderMediaOnLambdaOutput> => {
 	const {functionName, region, rendererFunctionName} = input;
 
@@ -130,13 +111,36 @@ export const renderMediaOnLambda = async (
 	} catch (err) {
 		if ((err as Error).stack?.includes('UnrecognizedClientException')) {
 			throw new Error(
-				'UnrecognizedClientException: The AWS credentials provided were probably mixed up. Learn how to fix this issue here: https://remotion.dev/docs/lambda/troubleshooting/unrecognizedclientexception',
+				'UnrecognizedClientException: The AWS credentials provided were probably mixed up. Learn how to fix this issue here: https://remotion.dev/docs/lambda/troubleshooting/unrecognizedclientexception'
 			);
 		}
 
 		throw err;
 	}
 };
+
+/**
+ * @description Triggers a render on a lambda given a composition and a lambda function.
+ * @see [Documentation](https://remotion.dev/docs/lambda/rendermediaonlambda)
+ * @param params.functionName The name of the Lambda function that should be used
+ * @param params.serveUrl The URL of the deployed project
+ * @param params.composition The ID of the composition which should be rendered.
+ * @param params.inputProps The input props that should be passed to the composition.
+ * @param params.codec The media codec which should be used for encoding.
+ * @param params.imageFormat In which image format the frames should be rendered. Default "jpeg"
+ * @param params.crf The constant rate factor to be used during encoding.
+ * @param params.envVariables Object containing environment variables to be inserted into the video environment
+ * @param params.proResProfile The ProRes profile if rendering a ProRes video
+ * @param params.jpegQuality JPEG quality if JPEG was selected as the image format.
+ * @param params.region The AWS region in which the media should be rendered.
+ * @param params.maxRetries How often rendering a chunk may fail before the media render gets aborted. Default "1"
+ * @param params.logLevel Level of logging that Lambda function should perform. Default "info".
+ * @param params.webhook Configuration for webhook called upon completion or timeout of the render.
+ * @returns {Promise<RenderMediaOnLambdaOutput>} See documentation for detailed structure
+ */
+export const renderMediaOnLambda = wrapWithErrorHandling(
+	renderMediaOnLambdaRaw
+) as typeof renderMediaOnLambdaRaw;
 
 /**
  * @deprecated Renamed to renderMediaOnLambda()
