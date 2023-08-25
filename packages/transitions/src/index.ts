e00b2fc325cdd71e59e10b0f9fa0bc1421ef8363
Jonny Burger <jonathanburger11@gmail.com>
2023-08-25 16:35:21 +0200

    file name
    Former-commit-id: 7977fb200e3a5c4a15c7c8b3fd38c54e145f909f

--- packages/transitions/src/index.ts
+++ packages/transitions/src/index.ts
@@ -1,6 +1,6 @@
 // Timings
 export { linearTiming } from "./timings/linear-timing.js";
-export { springTiming } from "./timings/spring.js";
+export { springTiming } from "./timings/spring-timing.js";
 // Component
 export { TransitionSeries } from "./TransitionSeries.js";
 export {
