ff827aa30a55bb44f75296f075d47753b7401370
Jonny Burger <jonathanburger11@gmail.com>
2023-09-04 16:18:24 +0200

    move around
    Former-commit-id: 6b8f72995a8fda7e2cb27c16a30d3bb98a8ccd49

--- packages/lambda/src/api/get-or-create-bucket.ts
+++ packages/lambda/src/api/get-or-create-bucket.ts
@@ -1,19 +1,14 @@
-import {
-	DeleteBucketLifecycleCommand,
-	PutBucketLifecycleConfigurationCommand,
-} from '@aws-sdk/client-s3';
-
-import {
-	createLifeCycleInput,
-	deleteLifeCycleInput,
-} from '../functions/helpers/apply-lifecyle';
-import {getLifeCycleRules} from '../functions/helpers/lifecycle';
 import type {AwsRegion} from '../pricing/aws-regions';
-import {getS3Client} from '../shared/aws-clients';
 import {REMOTION_BUCKET_PREFIX} from '../shared/constants';
+import {applyLifeCyleOperation} from '../shared/lifecycle-rules';
 import {makeBucketName} from '../shared/validate-bucketname';
 import {createBucket} from './create-bucket';
 import {getRemotionS3Buckets} from './get-buckets';
+
+type GetOrCreateBucketInputInner = {
+	region: AwsRegion;
+	enableFolderExpiry: boolean | null;
+};
 
 export type GetOrCreateBucketInput = {
 	region: AwsRegion;
@@ -24,14 +19,9 @@ export type GetOrCreateBucketOutput = {
 	bucketName: string;
 	alreadyExisted: boolean;
 };
-/**
- * @description Creates a bucket for Remotion Lambda in your S3 account. If one already exists, it will get returned instead.
- * @see [Documentation](https://remotion.dev/docs/lambda/getorcreatebucket)
- * @param params.region The region in which you want your S3 bucket to reside in.
- * @returns {Promise<GetOrCreateBucketOutput>} An object containing the `bucketName`.
- */
-export const getOrCreateBucket = async (
-	params: GetOrCreateBucketInput,
+
+const getOrCreateBucketInner = async (
+	params: GetOrCreateBucketInputInner,
 ): Promise<GetOrCreateBucketOutput> => {
 	const {remotionBuckets} = await getRemotionS3Buckets(params.region);
 	if (remotionBuckets.length > 1) {
@@ -48,11 +38,11 @@ export const getOrCreateBucket = async (
 	if (remotionBuckets.length === 1) {
 		const existingBucketName = remotionBuckets[0].name;
 		// apply to existing bucket
-		await applyLifeCyleOperation(
-			enableFolderExpiry,
-			existingBucketName,
+		await applyLifeCyleOperation({
+			enableFolderExpiry: enableFolderExpiry ?? null,
+			bucketName: existingBucketName,
 			region,
-		);
+		});
 
 		return {bucketName: remotionBuckets[0].name, alreadyExisted: true};
 	}
@@ -65,46 +55,24 @@ export const getOrCreateBucket = async (
 	});
 
 	// apply to newly created bucket
-	await applyLifeCyleOperation(enableFolderExpiry, bucketName, region);
+	await applyLifeCyleOperation({
+		enableFolderExpiry: enableFolderExpiry ?? null,
+		bucketName,
+		region,
+	});
 
 	return {bucketName, alreadyExisted: false};
 };
 
-const createLCRules = async (bucketName: string, region: AwsRegion) => {
-	const lcRules = getLifeCycleRules();
-	// create the lifecyle rules
-	const createCommandInput = createLifeCycleInput({
-		bucketName,
-		lcRules,
+/**
+ * @description Creates a bucket for Remotion Lambda in your S3 account. If one already exists, it will get returned instead.
+ * @see [Documentation](https://remotion.dev/docs/lambda/getorcreatebucket)
+ * @param params.region The region in which you want your S3 bucket to reside in.
+ * @returns {Promise<GetOrCreateBucketOutput>} An object containing the `bucketName`.
+ */
+export const getOrCreateBucket = (options: GetOrCreateBucketInput) => {
+	return getOrCreateBucketInner({
+		region: options.region,
+		enableFolderExpiry: options.enableFolderExpiry ?? null,
 	});
-	const createCommand = new PutBucketLifecycleConfigurationCommand(
-		createCommandInput,
-	);
-	await getS3Client(region, null).send(createCommand);
 };
-
-async function applyLifeCyleOperation(
-	enableFolderExpiry: boolean | undefined,
-	bucketName: string,
-	region: AwsRegion,
-) {
-	if (enableFolderExpiry !== undefined) {
-		if (enableFolderExpiry) {
-			console.log('Creating lifecycle rules!');
-			await createLCRules(bucketName, region);
-		} else {
-			console.log('Deleting existing lifecycle rules!');
-			await deleteLCRules(bucketName, region);
-		}
-	}
-}
-
-async function deleteLCRules(bucketName: string, region: AwsRegion) {
-	const lcRules = getLifeCycleRules();
-	const deleteCommandInput = deleteLifeCycleInput({
-		bucketName,
-		lcRules,
-	});
-	const deleteCommand = new DeleteBucketLifecycleCommand(deleteCommandInput);
-	await getS3Client(region, null).send(deleteCommand);
-}
---
+++ packages/lambda/src/shared/lifecycle-rules.ts
@@ -0,0 +1,66 @@
+import {
+	DeleteBucketLifecycleCommand,
+	PutBucketLifecycleConfigurationCommand,
+} from '@aws-sdk/client-s3';
+import type {AwsRegion} from '../client';
+import {
+	createLifeCycleInput,
+	deleteLifeCycleInput,
+} from '../functions/helpers/apply-lifecyle';
+import {getLifeCycleRules} from '../functions/helpers/lifecycle';
+import {getS3Client} from './aws-clients';
+
+export const createLCRules = async ({
+	bucketName,
+	region,
+}: {
+	bucketName: string;
+	region: AwsRegion;
+}) => {
+	const lcRules = getLifeCycleRules();
+	// create the lifecyle rules
+	const createCommandInput = createLifeCycleInput({
+		bucketName,
+		lcRules,
+	});
+	const createCommand = new PutBucketLifecycleConfigurationCommand(
+		createCommandInput,
+	);
+	await getS3Client(region, null).send(createCommand);
+};
+
+export const applyLifeCyleOperation = async ({
+	enableFolderExpiry,
+	bucketName,
+	region,
+}: {
+	enableFolderExpiry: boolean | null;
+	bucketName: string;
+	region: AwsRegion;
+}) => {
+	if (enableFolderExpiry === null) {
+		return;
+	}
+
+	if (enableFolderExpiry) {
+		await createLCRules({bucketName, region});
+	} else {
+		await deleteLCRules({bucketName, region});
+	}
+};
+
+export const deleteLCRules = async ({
+	bucketName,
+	region,
+}: {
+	bucketName: string;
+	region: AwsRegion;
+}) => {
+	const lcRules = getLifeCycleRules();
+	const deleteCommandInput = deleteLifeCycleInput({
+		bucketName,
+		lcRules,
+	});
+	const deleteCommand = new DeleteBucketLifecycleCommand(deleteCommandInput);
+	await getS3Client(region, null).send(deleteCommand);
+};
