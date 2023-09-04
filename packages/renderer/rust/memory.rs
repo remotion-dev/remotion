95e39bd018cadae131eee1795395fa8f302916c5
Jonny Burger <jonathanburger11@gmail.com>
2023-09-04 13:41:13 +0200

    free memory
    Former-commit-id: 7931ff3748e70ea0941842180897c14e904a10c3

--- packages/renderer/rust/memory.rs
+++ packages/renderer/rust/memory.rs
@@ -3,7 +3,7 @@ use sysinfo::{System, SystemExt};
 pub fn get_available_memory() -> u64 {
     let mut sys = System::new();
     sys.refresh_memory();
-    return sys.free_memory();
+    return sys.total_memory() - sys.used_memory();
 }
 
 // Is there less than 100MB of memory left?
@@ -11,6 +11,6 @@ pub fn is_about_to_run_out_of_memory() -> bool {
     let mut sys = System::new();
     sys.refresh_memory();
 
-    let free_memory_and_swap = sys.free_memory() + sys.free_swap();
+    let free_memory_and_swap = sys.total_memory() - sys.used_memory();
     return free_memory_and_swap < 100 * 1024 * 1024;
 }
