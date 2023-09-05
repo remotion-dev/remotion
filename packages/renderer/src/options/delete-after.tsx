b20a7c8d706832ad5598a36c184c9fe238c45054
Jonny Burger <jonathanburger11@gmail.com>
2023-09-05 11:39:28 +0200

    render expiry days

--- packages/renderer/src/options/delete-after.tsx
+++ packages/renderer/src/options/delete-after.tsx
@@ -1,17 +1,17 @@
 export const deleteAfterOption = {
-	name: 'Render expiry days',
+	name: 'Lambda render expiration',
 	cliFlag: 'delete-after' as const,
 	description: () => {
 		return (
 			<>
 				Automatically delete the render after a certain period. Accepted values
 				are <code>1-day</code>, <code>3-days</code>, <code>7-days</code> and{' '}
-				<code>30-days</code>. For this to work, your bucket needs to have
+				<code>30-days</code>.<br /> For this to work, your bucket needs to have{' '}
 				<a href="/docs/lambda/autodelete">lifecycles enabled</a>.
 			</>
 		);
 	},
 	ssrName: 'deleteAfter' as const,
-	docLink: 'https://www.remotion.dev/docs/autodelete',
-	type: 0 as number | null,
+	docLink: 'https://www.remotion.dev/docs/lambda/autodelete',
+	type: '' as string | null,
 };
