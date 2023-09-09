3f2df9f006beedd62313c373c8de2be22fe7ed30
Jonny Burger <jonathanburger11@gmail.com>
2023-09-09 13:35:39 +0200

    v4.0.35
    Former-commit-id: d49c41494fe736d2b762bb601834861a7bc06d9d

--- packages/lambda-php/src/Version.php
+++ packages/lambda-php/src/Version.php
@@ -1,4 +1,4 @@
 <?php 
 namespace Remotion\LambdaPhp;
 
-const VERSION = "4.0.34";
+const VERSION = "4.0.35";
--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.34"
+VERSION = "4.0.35"
