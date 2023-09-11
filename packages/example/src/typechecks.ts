28f022f3863368d0f735211fb83cf2e07bf8cfd2
Jonny Burger <jonathanburger11@gmail.com>
2023-09-11 16:10:00 +0200

    add type check in root

---
+++ packages/example/src/typechecks.ts
@@ -0,0 +1,10 @@
+// Just checking the types
+
+import {RemotionOffthreadVideoProps} from 'remotion';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const _props: RemotionOffthreadVideoProps = {
+	startFrom: 0,
+	endAt: 100,
+	src: 'hi there',
+};
