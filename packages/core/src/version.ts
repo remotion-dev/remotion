2a471347b660536c4549abbe0ccb777e526cf982
Jonny Burger <jonathanburger11@gmail.com>
2023-09-14 16:18:05 +0200

    v4.0.39

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.38';
+export const VERSION = '4.0.39';
--- packages/lambda-php/src/Version.php
+++ packages/lambda-php/src/Version.php
@@ -1,4 +1,4 @@
 <?php 
 namespace Remotion\LambdaPhp;
 
-const VERSION = "4.0.38";
+const VERSION = "4.0.39";
