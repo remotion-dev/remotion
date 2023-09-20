5a8a2d30fafedb319724aff306880e64ad256601
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-09-20 19:04:36 +1000

    add checking of environment
    Former-commit-id: 50e49c4255b302a0dc14d04520cb39012aa058e5

--- packages/cloudrun/src/api/helpers/get-cloud-logging-client.ts
+++ packages/cloudrun/src/api/helpers/get-cloud-logging-client.ts
@@ -3,11 +3,19 @@ import {v2} from '@google-cloud/logging';
 const {LoggingServiceV2Client} = v2;
 
 export const getCloudLoggingClient = () => {
-	return new LoggingServiceV2Client({
-		projectId: process.env.REMOTION_GCP_PROJECT_ID,
-		credentials: {
-			client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
-			private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
-		},
-	});
+	if (
+		process.env.REMOTION_GCP_PROJECT_ID &&
+		process.env.REMOTION_GCP_CLIENT_EMAIL &&
+		process.env.REMOTION_GCP_PRIVATE_KEY
+	) {
+		return new LoggingServiceV2Client({
+			projectId: process.env.REMOTION_GCP_PROJECT_ID,
+			credentials: {
+				client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
+				private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
+			},
+		});
+	}
+
+	return new LoggingServiceV2Client();
 };
