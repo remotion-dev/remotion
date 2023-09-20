50e49c4255b302a0dc14d04520cb39012aa058e5
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-09-20 19:04:36 +1000

    add checking of environment

--- packages/cloudrun/src/api/helpers/get-auth-client-for-url.ts
+++ packages/cloudrun/src/api/helpers/get-auth-client-for-url.ts
@@ -1,12 +1,21 @@
 import {GoogleAuth} from 'google-auth-library';
 
 export const getAuthClientForUrl = async (url: string) => {
-	const auth = new GoogleAuth({
-		credentials: {
-			client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
-			private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
-		},
-	});
+	let auth = null;
+	if (
+		process.env.REMOTION_GCP_CLIENT_EMAIL &&
+		process.env.REMOTION_GCP_PRIVATE_KEY
+	) {
+		auth = new GoogleAuth({
+			credentials: {
+				client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
+				private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
+			},
+		});
+	} else {
+		auth = new GoogleAuth();
+	}
+
 	const client = await auth.getIdTokenClient(url);
 	return client;
 };
--- packages/cloudrun/src/api/helpers/get-resource-manager-client.ts
+++ packages/cloudrun/src/api/helpers/get-resource-manager-client.ts
@@ -2,11 +2,19 @@ import {v3} from '@google-cloud/resource-manager';
 const {ProjectsClient} = v3;
 
 export const getResourceManagerClient = () => {
-	return new ProjectsClient({
-		projectId: process.env.REMOTION_GCP_PROJECT_ID,
-		credentials: {
-			client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
-			private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
-		},
-	});
+	if (
+		process.env.REMOTION_GCP_PROJECT_ID &&
+		process.env.REMOTION_GCP_CLIENT_EMAIL &&
+		process.env.REMOTION_GCP_PRIVATE_KEY
+	) {
+		return new ProjectsClient({
+			projectId: process.env.REMOTION_GCP_PROJECT_ID,
+			credentials: {
+				client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
+				private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
+			},
+		});
+	}
+
+	return new ProjectsClient();
 };
