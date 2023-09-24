42b2431acf8295f01509a087846634bb6c3657c6
Jonny Burger <jonathanburger11@gmail.com>
2023-09-24 17:23:30 +0200

    add mappings.wasm

--- packages/cloudrun/src/admin/bundle-renderLogic.ts
+++ packages/cloudrun/src/admin/bundle-renderLogic.ts
@@ -37,6 +37,21 @@ const bundleRenderLogic = async () => {
 	fs.copyFileSync(x64BinaryPath, compositorFile);
 	fs.cpSync(ffmpegCwd, `${outdir}/ffmpeg`, {recursive: true});
 
+	fs.cpSync(
+		path.join(
+			__dirname,
+			'..',
+			'..',
+			'..',
+			'renderer',
+			'node_modules',
+			'source-map',
+			'lib',
+			'mappings.wasm',
+		),
+		`${outdir}/mappings.wasm`,
+	);
+
 	console.log('distribution bundled.');
 };
 
