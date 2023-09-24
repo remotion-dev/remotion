779d992a5e54b62334210ff12e07107208481853
Jonny Burger <jonathanburger11@gmail.com>
2023-09-24 16:12:15 +0200

    docs and command line
    Former-commit-id: 4fbf84b581dc13936c688b68b742fe54c605849a

--- packages/cloudrun/src/api/render-media-on-cloudrun.ts
+++ packages/cloudrun/src/api/render-media-on-cloudrun.ts
@@ -184,10 +184,7 @@ const internalRenderMediaOnCloudrunRaw = async ({
 		numberOfGifLoops: numberOfGifLoops ?? null,
 		frameRange: frameRange ?? null,
 		envVariables: envVariables ?? {},
-		chromiumOptions:
-			{
-				...(chromiumOptions ?? {}),
-			} ?? null,
+		chromiumOptions: chromiumOptions ?? undefined,
 		muted: muted ?? false,
 		outputBucket,
 		privacy,
