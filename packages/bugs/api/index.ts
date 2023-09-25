17bd2e1dbd635e9ac483cb3153be9a80c98dc8fb
Jonny Burger <jonathanburger11@gmail.com>
2023-09-25 11:23:43 +0200

    bugs: main page and add some real bugs
    Former-commit-id: 075e01d50b287da593a5570c35f732c40c451822

--- packages/bugs/api/index.ts
+++ packages/bugs/api/index.ts
@@ -1,6 +1,6 @@
-export default async function handler(request: Request) {
+export default async function handler() {
   return new Response(
-    `<h1>Remotion Bugs</h1><p>Add a version to query bugs, e.g <a href="https://bugs.remotion.dev/v4.0.33"></a></p>`,
+    `<h1>Remotion Bugs</h1><p>Add a version to query bugs, e.g <a href="https://bugs.remotion.dev/v4.0.36">https://bugs.remotion.dev/v4.0.36</a></p>`,
     {
       status: 200,
       headers: {
