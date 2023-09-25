075e01d50b287da593a5570c35f732c40c451822
Jonny Burger <jonathanburger11@gmail.com>
2023-09-25 11:23:43 +0200

    bugs: main page and add some real bugs

--- packages/bugs/api/[v].ts
+++ packages/bugs/api/[v].ts
@@ -1,19 +1,46 @@
+type Bug = {
+  title: string;
+  description: string;
+  link: string;
+  versions: string[];
+};
+
+const bugs: Bug[] = [
+  {
+    title: "OffthreadVideo could crash",
+    description:
+      "On some videos, OffthreadVideo could crash without proper error handling.",
+    link: "https://github.com/remotion-dev/remotion/pull/2882",
+    versions: ["4.0.36", "4.0.37", "4.0.38"],
+  },
+  {
+    title: "Slow OffthreadVideo performance",
+    description:
+      "Without an explicit cache size, the OffthreadVideo component would run with no cache.",
+    link: "https://github.com/remotion-dev/remotion/pull/2882",
+    versions: ["4.0.33", "4.0.34", "4.0.35", "4.0.36"],
+  },
+];
+
+const getVersionBugs = (version: string) => {
+  const selectedVersionBugs = bugs.filter((bug) => {
+    return bug.versions.includes(version);
+  });
+  return selectedVersionBugs;
+};
+
 export default async function handler(request: Request) {
   const urlParams = new URL(request.url).searchParams;
+
   const query = Object.fromEntries(urlParams);
-  const cookies = request.headers.get("cookie");
-  let body;
-  try {
-    body = await request.json();
-  } catch (e) {
-    body = null;
-  }
+  const v = query["v"].replace("v", "") as string;
+
+  const bugs = getVersionBugs(v);
 
   return new Response(
     JSON.stringify({
-      body,
-      query,
-      cookies,
+      version: v,
+      bugs,
     }),
     {
       status: 200,
