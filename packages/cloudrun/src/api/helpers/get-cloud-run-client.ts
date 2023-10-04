f567cbda26d310d9883229549df22b82810bb88b
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-10-04 20:04:21 +1100

    use priority on which credential to use
    Former-commit-id: cf788e5efdf8661b7ab9f80be330404e9fdec01f

--- packages/cloudrun/src/api/helpers/get-cloud-run-client.ts
+++ packages/cloudrun/src/api/helpers/get-cloud-run-client.ts
@@ -1,21 +1,23 @@
 import {v2} from '@google-cloud/run';
+import {
+	getProjectId,
+	isInCloudTask,
+} from '../../functions/helpers/is-in-cloud-task';
 
 const {ServicesClient} = v2;
 
 export const getCloudRunClient = () => {
-	if (
-		process.env.REMOTION_GCP_PROJECT_ID &&
-		process.env.REMOTION_GCP_CLIENT_EMAIL &&
-		process.env.REMOTION_GCP_PRIVATE_KEY
-	) {
+	if (isInCloudTask()) {
 		return new ServicesClient({
-			projectId: process.env.REMOTION_GCP_PROJECT_ID,
-			credentials: {
-				client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
-				private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
-			},
+			projectId: getProjectId(),
 		});
 	}
 
-	return new ServicesClient();
+	return new ServicesClient({
+		projectId: process.env.REMOTION_GCP_PROJECT_ID,
+		credentials: {
+			client_email: process.env.REMOTION_GCP_CLIENT_EMAIL,
+			private_key: process.env.REMOTION_GCP_PRIVATE_KEY,
+		},
+	});
 };
