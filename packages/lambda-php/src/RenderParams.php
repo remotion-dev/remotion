5539a731c01407c63f573c1d07aa1e28fcc86552
Jonny Burger <jonathanburger11@gmail.com>
2023-10-06 09:59:12 +0200

    PHP: Extract GetRenderProgressResponse into separate file
    Former-commit-id: bd8ae9c4a2e268f4399e42f368598a097e38b14a

--- packages/lambda-php/src/RenderParams.php
+++ packages/lambda-php/src/RenderParams.php
@@ -12,22 +12,6 @@ class RenderMediaOnLambdaResponse
     public string $renderId;
 }
 
-class GetRenderProgressResponse
-{
-    public int $chunks;
-    public bool $done;
-    public float $overallProgress;
-    public string $type;
-    public ?string $outputFile;
-    public int $lambdasInvoked;
-    public int $renderSize;
-    public int $currentTime;
-    public bool $fatalErrorEncountered;
-    public ?int $timeToFinish;
-    public ?string $outBucket;
-    public ?string $outKey;
-    public ?string $bucket;
-}
 
 class RenderParams
 {
