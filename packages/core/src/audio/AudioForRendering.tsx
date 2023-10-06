bd018ab356e4d2f0272de4104556c12a8d38e856
evoxf1 <sahil2812@live.com>
2023-10-06 08:24:36 +0530

    Allow a toneFrequency prop to be passed to <Audio>
    Former-commit-id: 67b7ccd4abcd8d8232970bafe616ecc372f4640f

--- packages/core/src/audio/AudioForRendering.tsx
+++ packages/core/src/audio/AudioForRendering.tsx
@@ -95,10 +95,10 @@ const AudioForRenderingRefForwardingFunction: React.ForwardRefRenderFunction<
 			toneFrequency !== undefined &&
 			(toneFrequency < 0 || toneFrequency > 1)
 		) {
-			throw new Error('Tone frequency must be between 0 and 1');
+			throw new Error('Tone frequency must be between 0 and 1');// Stabilizing a min and max value for toneFrequecny
 		}
 
-		const ffmpegFilter = `asetrate=44100*${toneFrequency},aresample=44100,atempo=1/${toneFrequency}`;
+		const ffmpegFilter = `asetrate=44100*${toneFrequency},aresample=44100,atempo=1/${toneFrequency}`; // Logic to calculate the pitch
 
 		registerRenderAsset({
 			type: 'audio',
