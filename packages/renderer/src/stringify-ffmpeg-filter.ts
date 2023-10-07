123b8f841317dd835b577215054a5526e6b33778
Jonny Burger <jonathanburger11@gmail.com>
2023-10-07 14:27:48 +0200

    simplify

--- packages/renderer/src/stringify-ffmpeg-filter.ts
+++ packages/renderer/src/stringify-ffmpeg-filter.ts
@@ -44,19 +44,11 @@ export const stringifyFfmpegFilter = ({
 		return null;
 	}
 
-	if (
-		toneFrequency === null ||
-		toneFrequency <= 0 ||
-		toneFrequency > 2 ||
-		toneFrequency % 0.01 !== 0
-	) {
+	if (toneFrequency !== null && (toneFrequency <= 0 || toneFrequency > 2)) {
 		throw new Error(
-			'toneFrequency must be a positive number between 0.01 and 2 with up to two decimal places & if toneFrequency is null, it will be set to 1 (default pitch)',
+			'toneFrequency must be a positive number between 0.01 and 2',
 		);
 	}
-
-	// If toneFrequency is null, set it to 1 (default pitch)
-	toneFrequency = parseFloat(toneFrequency.toFixed(2)) || 1;
 
 	const volumeFilter = ffmpegVolumeExpression({
 		volume,
