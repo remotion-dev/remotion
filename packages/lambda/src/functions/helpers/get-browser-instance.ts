05bcac94f6be2600290fc71d56a1ae4a09ab79c4
Jonny Burger <jonathanburger11@gmail.com>
2023-10-09 15:19:41 +0200

    Discard changes to packages/lambda/src/functions/helpers/get-browser-instance.ts

--- packages/lambda/src/functions/helpers/get-browser-instance.ts
+++ packages/lambda/src/functions/helpers/get-browser-instance.ts
@@ -1,10 +1,48 @@
-import type {ChromiumOptions} from '@remotion/renderer';
-import {openBrowser} from '@remotion/renderer';
+import type {ChromiumOptions, LogLevel, openBrowser} from '@remotion/renderer';
+import {RenderInternals} from '@remotion/renderer';
+import type {Await} from '../../shared/await';
 import {executablePath} from './get-chromium-executable-path';
 
+let _browserInstance: Await<ReturnType<typeof openBrowser>> | null;
+
+let launching = false;
+
+const waitForLaunched = () => {
+	return new Promise<void>((resolve, reject) => {
+		const check = () =>
+			setTimeout(() => {
+				if (launching) {
+					resolve();
+				} else {
+					check();
+				}
+			}, 16);
+
+		setTimeout(() => reject(new Error('Timeout launching browser')), 5000);
+		check();
+	});
+};
+
 export const getBrowserInstance = async (
+	logLevel: LogLevel,
+	indent: boolean,
 	chromiumOptions: ChromiumOptions,
 ): ReturnType<typeof openBrowser> => {
+	if (launching) {
+		await waitForLaunched();
+		if (!_browserInstance) {
+			throw new Error('expected to launch');
+		}
+
+		return _browserInstance;
+	}
+
+	if (_browserInstance) {
+		return _browserInstance;
+	}
+
+	launching = true;
+
 	const execPath = executablePath();
 
 	const actualChromiumOptions: ChromiumOptions = {
@@ -13,11 +51,20 @@ export const getBrowserInstance = async (
 		gl: chromiumOptions.gl ?? 'swangle',
 	};
 
-	const _browserInstance = await openBrowser('chrome', {
+	_browserInstance = await RenderInternals.internalOpenBrowser({
+		browser: 'chrome',
 		browserExecutable: execPath,
 		chromiumOptions: actualChromiumOptions,
 		forceDeviceScaleFactor: undefined,
+		indent: false,
+		viewport: null,
+		logLevel,
 	});
-
+	_browserInstance.on('disconnected', () => {
+		console.log('Browser disconnected / crashed');
+		_browserInstance?.close(true, logLevel, indent).catch(() => undefined);
+		_browserInstance = null;
+	});
+	launching = false;
 	return _browserInstance;
 };
