53f30f8077da030f2a1d6c1a7e3ed4dfb31e2431
Jonny Burger <jonathanburger11@gmail.com>
2023-10-18 20:39:44 +0200

    add failing test

--- packages/paths/src/test/get-point-at-length.test.ts
+++ packages/paths/src/test/get-point-at-length.test.ts
@@ -6,3 +6,16 @@ test('Should be able to get parts of a path', () => {
 
 	expect(parts).toEqual({x: 50, y: 0});
 });
+
+test.only('Another more complex point', () => {
+	const path = `M2 129
+		
+		C48.6667 84.6666 186 2
+		 324 1.99997C476.135 1.99994 616
+		 89.9999 663 129`;
+
+	const point = getPointAtLength(path, 721.1239854719418 * 0.51);
+	expect(point.x).not.toBe(663);
+	expect(point.y).not.toBe(129);
+	console.log(point);
+});
