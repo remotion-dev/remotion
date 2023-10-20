28c6702bc7a6cb6940da59ae5827976509dd69c8
Jonny Burger <jonathanburger11@gmail.com>
2023-10-20 10:25:25 +0200

    Update validate-default-codec.ts
    Former-commit-id: 22229d8981ca886aa6e717ab82bb75d7107fb2b5

--- packages/core/src/validation/validate-default-codec.ts
+++ packages/core/src/validation/validate-default-codec.ts
@@ -1,15 +1,25 @@
-import { Codec, CodecOrUndefined, validCodecs } from '../codec';
+import type {Codec, CodecOrUndefined} from '../codec';
+import {validCodecs} from '../codec';
 
-export function validateDefaultCodec(defaultCodec: unknown, location: string): asserts defaultCodec is CodecOrUndefined {
+export function validateDefaultCodec(
+	defaultCodec: unknown,
+	location: string,
+): asserts defaultCodec is CodecOrUndefined {
 	if (typeof defaultCodec === 'undefined') {
-		return;	
+		return;
 	}
 
 	if (typeof defaultCodec !== 'string') {
-		throw new TypeError(`The "defaultCodec" prop ${location} must be a string, but you passed a value of type ${typeof defaultCodec}.`);
+		throw new TypeError(
+			`The "defaultCodec" prop ${location} must be a string, but you passed a value of type ${typeof defaultCodec}.`,
+		);
 	}
 
 	if (!validCodecs.includes(defaultCodec as Codec)) {
-		throw new Error(`The "defaultCodec" prop ${location} must be one of ${validCodecs.join(', ')}, but you passed ${defaultCodec}.`);
+		throw new Error(
+			`The "defaultCodec" prop ${location} must be one of ${validCodecs.join(
+				', ',
+			)}, but you passed ${defaultCodec}.`,
+		);
 	}
 }
