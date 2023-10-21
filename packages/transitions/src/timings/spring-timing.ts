bcd4eacae8a494c2cfaaa27f86e163dfd90b908e
Jonny Burger <jonathanburger11@gmail.com>
2023-10-21 17:49:39 +0200

    Update spring-timing.ts
    Former-commit-id: 5e3c2d7f73a662ab37609b39e8eec9c6882c09a6

--- packages/transitions/src/timings/spring-timing.ts
+++ packages/transitions/src/timings/spring-timing.ts
@@ -2,6 +2,16 @@ import type { SpringConfig } from "remotion";
 import { measureSpring, spring } from "remotion";
 import type { TransitionTiming } from "../types.js";
 
+const springWithInvalidArgumentRejection: typeof spring = (args) => {
+  if (args.to || args.from) {
+    throw new Error(
+      "to / from values are not supported by springWithRoundUpIfThreshold"
+    );
+  }
+
+  return spring(args);
+};
+
 export const springTiming = (
   options: {
     config?: Partial<SpringConfig>;
@@ -33,12 +43,3 @@ export const springTiming = (
   };
 };
 
-const springWithInvalidArgumentRejection: typeof spring = (args) => {
-  if (args.to || args.from) {
-    throw new Error(
-      "to / from values are not supported by springWithRoundUpIfThreshold"
-    );
-  }
-
-  return spring(args);
-};
