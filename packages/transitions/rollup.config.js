20f0b1d2012a8941164b869a9ddc98d3ac47fed7
Jonny Burger <jonathanburger11@gmail.com>
2023-10-23 17:41:57 +0200

    `@remotion/transitions`: flip transition

--- packages/transitions/rollup.config.js
+++ packages/transitions/rollup.config.js
@@ -1,6 +1,8 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
 
+const presentations = ['slide', 'flip', 'wipe', 'fade'];
+
 export default [
 	{
 		input: 'src/index.ts',
@@ -20,58 +22,24 @@ export default [
 			}),
 		],
 	},
-	{
-		input: 'src/presentations/slide.tsx',
-		output: [
-			{
-				file: 'dist/cjs/slide.js',
-				format: 'cjs',
-				sourcemap: false,
-			},
-		],
-		external: ['remotion', 'react', 'react/jsx-runtime'],
-		plugins: [
-			typescript({
-				tsconfig: 'tsconfig-cjs.json',
-				sourceMap: false,
-				outputToFilesystem: true,
-			}),
-		],
-	},
-	{
-		input: 'src/presentations/fade.tsx',
-		output: [
-			{
-				file: 'dist/cjs/fade.js',
-				format: 'cjs',
-				sourcemap: false,
-			},
-		],
-		external: ['remotion', 'react', 'react/jsx-runtime'],
-		plugins: [
-			typescript({
-				tsconfig: 'tsconfig-cjs.json',
-				sourceMap: false,
-				outputToFilesystem: true,
-			}),
-		],
-	},
-  {
-		input: 'src/presentations/wipe.tsx',
-		output: [
-			{
-				file: 'dist/cjs/wipe.js',
-				format: 'cjs',
-				sourcemap: false,
-			},
-		],
-		external: ['remotion', 'react', 'react/jsx-runtime'],
-		plugins: [
-			typescript({
-				tsconfig: 'tsconfig-cjs.json',
-				sourceMap: false,
-				outputToFilesystem: true,
-			}),
-		],
-	},
+	...presentations.map((p) => {
+		return {
+			input: `src/presentations/${p}.tsx`,
+			output: [
+				{
+					file: `dist/cjs/${p}.js`,
+					format: 'cjs',
+					sourcemap: false,
+				},
+			],
+			external: ['remotion', 'react', 'react/jsx-runtime'],
+			plugins: [
+				typescript({
+					tsconfig: 'tsconfig-cjs.json',
+					sourceMap: false,
+					outputToFilesystem: true,
+				}),
+			],
+		};
+	}),
 ];
