a0e3f847a858977f02446335c05900d0e80bf7c5
Jonny Burger <jonathanburger11@gmail.com>
2023-12-19 14:01:09 +0100

    Discard changes to packages/lambda/src/admin/generate-etag-list.ts

--- packages/lambda/src/admin/generate-etag-list.ts
+++ packages/lambda/src/admin/generate-etag-list.ts
@@ -1,12 +1,12 @@
-import hex from 'crypto-js/enc-hex';
-import md5 from 'crypto-js/md5';
+import crypto from 'node:crypto';
 import {writeFileSync} from 'node:fs';
 
-const md5Func = (data: string) => hex.stringify(md5(data));
+const md5 = (data: string) =>
+	crypto.createHash('md5').update(data).digest('hex');
 
 const chunks: string[] = [];
 for (let i = 0; i < 2000; i++) {
-	const etag = `"${md5Func(String(i))}"`;
+	const etag = `"${md5(String(i))}"`;
 	chunks.push(etag);
 }
 
