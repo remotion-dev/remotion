9cc132a1bfeb8fd273f36479641f7f928bf610f3
Jonny Burger <jonathanburger11@gmail.com>
2023-12-20 21:15:20 +0100

    Update webpack-config.ts

--- packages/bundler/src/webpack-config.ts
+++ packages/bundler/src/webpack-config.ts
@@ -90,7 +90,7 @@ export const webpackConfig = ({
 			aggregateTimeout: 0,
 			ignored: ['**/.git/**', '**/node_modules/**'],
 		},
-		devtool: 'cheap-module-source-map',
+		devtool: 'source-map',
 		entry: [
 			// Fast Refresh must come first,
 			// because setup-environment imports ReactDOM.
