9e97a5e086d43363b324c7e9cc11944f3ffd248a
Jonny Burger <jonathanburger11@gmail.com>
2024-01-06 15:12:37 +0100

    Update client.ts

--- packages/renderer/src/client.ts
+++ packages/renderer/src/client.ts
@@ -14,9 +14,12 @@ import {
 	defaultCodecsForFileExtension,
 	getFileExtensionFromCodec,
 } from './get-extension-from-codec';
+import { validStillImageFormats, validVideoImageFormats } from './image-format';
 import {isAudioCodec} from './is-audio-codec';
+import { logLevels } from './log-level';
 import {allOptions} from './options';
 import {validColorSpaces} from './options/color-space';
+import { validOpenGlRenderers } from './options/gl';
 import {optionsMap} from './options/options-map';
 import {
 	DEFAULT_PIXEL_FORMAT,
@@ -39,7 +42,10 @@ export const BrowserSafeApis = {
 	proResProfileOptions,
 	x264PresetOptions,
 	validPixelFormats,
+	validOpenGlRenderers,
 	validPixelFormatsForCodec,
+	validVideoImageFormats,
+	validStillImageFormats,
 	DEFAULT_PIXEL_FORMAT,
 	supportedAudioCodecs,
 	defaultFileExtensionMap,
@@ -51,4 +57,5 @@ export const BrowserSafeApis = {
 	optionsMap,
 	codecSupportsCrf,
 	codecSupportsVideoBitrate,
+	logLevels
 };
