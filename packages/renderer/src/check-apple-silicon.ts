f660608b73c7e560066bb0eb1185ccacebd74b05
Jonny Burger <jonathanburger11@gmail.com>
2024-01-08 15:32:03 +0100

    check if glibc is supported on linux, and throw error otherwise

--- packages/renderer/src/check-apple-silicon.ts
+++ packages/renderer/src/check-apple-silicon.ts
@@ -1,3 +1,36 @@
+export const gLibCErrorMessage = (libCString: string) => {
+	const split = libCString.split('.');
+	if (split.length !== 2) {
+		return null;
+	}
+
+	if (split[0] === '2' && Number(split[1]) >= 35) {
+		return null;
+	}
+
+	if (Number(split[0]) > 2) {
+		return null;
+	}
+
+	return `Remotion requires glibc 2.35 or higher. Your system has glibc ${libCString}.`;
+};
+
+const checkLibCRequirement = () => {
+	const {report} = process;
+	if (report) {
+		// @ts-expect-error no types
+		const {glibcVersionRuntime} = report.getReport().header;
+		if (!glibcVersionRuntime) {
+			return;
+		}
+
+		const error = gLibCErrorMessage(glibcVersionRuntime as string);
+		if (error) {
+			throw new Error(error);
+		}
+	}
+};
+
 export const checkNodeVersionAndWarnAboutRosetta = () => {
 	const version = process.version.replace('v', '').split('.');
 	const majorVersion = Number(version[0]);
@@ -8,4 +41,6 @@ export const checkNodeVersionAndWarnAboutRosetta = () => {
 			`Remotion requires at least Node ${requiredNodeVersion}. You currently have ${process.version}. Update your node version to ${requiredNodeVersion} to use Remotion.`,
 		);
 	}
+
+	checkLibCRequirement();
 };
---
+++ packages/renderer/src/test/libc-requirement.test.ts
@@ -0,0 +1,11 @@
+import {expect, test} from 'vitest';
+import {gLibCErrorMessage} from '../check-apple-silicon';
+
+test('Libc requirement', () => {
+	expect(gLibCErrorMessage('2.35')).toBe(null);
+
+	const message = gLibCErrorMessage('2.34');
+	expect(message).toBe(
+		'Remotion requires glibc 2.35 or higher. Your system has glibc 2.34.',
+	);
+});
