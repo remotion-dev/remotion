41b89532c08ab95d981f5bf381c1f3c19687c3a5
Jonny Burger <jonathanburger11@gmail.com>
2024-01-13 13:19:31 +0100

    cool

--- packages/docs/src/remotion/HomepageVideo/CurrentCountry.tsx
+++ packages/docs/src/remotion/HomepageVideo/CurrentCountry.tsx
@@ -4,7 +4,7 @@ import { AbsoluteFill } from "remotion";
 import { countriesPaths, ISO_3166_ALPHA_2_MAPPINGS } from "./paths";
 
 export const CurrentCountry: React.FC = () => {
-  const country = "US";
+  const country = "FR";
   const paths = countriesPaths.filter((c) => c.class === country);
   const joined = paths.map((p) => p.d).join(" ");
   const reset = resetPath(joined);
@@ -12,21 +12,43 @@ export const CurrentCountry: React.FC = () => {
 
   return (
     <AbsoluteFill>
-      <AbsoluteFill>
+      <AbsoluteFill style={{}}>
         <svg
           viewBox={boundingBox.viewBox}
           style={{
-            scale: "0.6",
+            scale: "0.8",
           }}
         >
-          <path fill="#222" d={reset} />
+          <path fill="#bbb" d={reset} />
         </svg>
       </AbsoluteFill>
-      <AbsoluteFill>
+      <AbsoluteFill />
+      <AbsoluteFill
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          paddingLeft: 20,
+          paddingRight: 20,
+        }}
+      >
         <div
           style={{
+            color: "#0b84f3",
+            fontFamily: "GT Planar",
+            fontWeight: "500",
+            fontSize: 13,
+            textAlign: "center",
+          }}
+        >
+          Your location
+        </div>
+        <div
+          style={{
+            lineHeight: 1.1,
             fontFamily: "GT Planar",
             textAlign: "center",
+            fontWeight: "500",
+            fontSize: 30,
           }}
         >
           {ISO_3166_ALPHA_2_MAPPINGS[country]}
