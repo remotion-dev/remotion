7a1c461192cb0e1a78ad500826f09112bb094b53
Jonny Burger <jonathanburger11@gmail.com>
2024-01-15 10:36:59 +0100

    prefix with node:

--- packages/compositor-linux-x64-gnu/index.js
+++ packages/compositor-linux-x64-gnu/index.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 exports.binaryPath = `${__dirname}/compositor`;
 exports.ffmpegPath = `${__dirname}/ffmpeg/remotion/bin/ffmpeg`;
 exports.ffprobePath = `${__dirname}/ffmpeg/remotion/bin/ffprobe`;
--- packages/compositor-win32-x64-msvc/index.mjs
+++ packages/compositor-win32-x64-msvc/index.mjs
@@ -1,5 +1,5 @@
-import path from "path";
-import * as url from "url";
+import path from "node:path";
+import * as url from "node:url";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 export const binaryPath = `${__dirname}${path.sep}compositor.exe`;
