721a0894aec01612e0bb47ce256a52a6c976d435
Jonny Burger <jonathanburger11@gmail.com>
2024-01-15 10:54:44 +0100

    test: allow bundling

---
+++ packages/it-tests/src/rendering/bundle-renderer.test.ts
@@ -0,0 +1,34 @@
+import { BundlerInternals } from "@remotion/bundler";
+import { execSync } from "node:child_process";
+import { ffmpegCwd, binaryPath } from "@remotion/compositor-darwin-arm64";
+import { tmpdir } from "node:os";
+import path from "node:path";
+import { expect, test } from "vitest";
+import { exampleVideos } from "./example-videos";
+import { copyFileSync, cpSync, rmSync } from "node:fs";
+
+test("Should be able to bundle the renderer", () => {
+  const outputdir = path.join(tmpdir(), `test-${Math.random()}`);
+  const outfile = path.join(outputdir, "esbuild-test.js");
+
+  const { errors, warnings } = BundlerInternals.esbuild.buildSync({
+    platform: "node",
+    target: "node16",
+    bundle: true,
+    outfile,
+    entryPoints: [`${__dirname}/test-index.ts`],
+  });
+  expect(errors.length).toBe(0);
+  expect(warnings.length).toBe(0);
+  copyFileSync(binaryPath, path.join(outputdir, path.basename(binaryPath)));
+  cpSync(ffmpegCwd, path.join(outputdir, path.basename(ffmpegCwd)), {
+    recursive: true,
+  });
+
+  const out = execSync("node " + outfile + " " + exampleVideos.bigBuckBunny);
+  expect(out.toString("utf8")).toBe("h264\n");
+
+  rmSync(outputdir, {
+    recursive: true,
+  });
+});
