f0e16a43d40f9e38ca1211d0ea0ae3d625a0e2be
Jonny Burger <jonathanburger11@gmail.com>
2024-01-21 21:53:01 +0100

    google fonts bun

--- packages/google-fonts/scripts/generate.ts
+++ packages/google-fonts/scripts/generate.ts
@@ -1,6 +1,5 @@
 import fs, { mkdirSync } from "node:fs";
 import path from "path";
-import got from "got";
 import postcss from "postcss";
 
 type FontInfo = {
@@ -40,19 +39,21 @@ const generate = async (font: Font) => {
   let css: null | string = null;
   let fontFamily: null | string = replaceDigitsWithWords(unquote(font.family));
   let cssFile = path.resolve(CSS_CACHE_DIR, cssname);
+
   if (!fs.existsSync(cssFile)) {
     //  Get from url with user agent that support woff2
-    const res = await got.get(url, {
+    const res = await fetch(url, {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
       },
     });
 
+    const body = await res.text();
     // Save to cache
-    await fs.promises.writeFile(cssFile, res.body);
+    await fs.promises.writeFile(cssFile, body);
 
-    css = res.body;
+    css = body;
   } else {
     // Read css from cache
     css = await fs.promises.readFile(cssFile, "utf-8");
