c38454218a4d2396d74d174c70a5e58164fde1f4
Jonny Burger <jonathanburger11@gmail.com>
2024-01-24 17:43:00 +0100

    v4.0.97

--- packages/lambda-php/src/Version.php
+++ packages/lambda-php/src/Version.php
@@ -1,4 +1,4 @@
 <?php 
 namespace Remotion\LambdaPhp;
 
-const VERSION = "4.0.96";
+const VERSION = "4.0.97";
--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.96"
+VERSION = "4.0.97"
