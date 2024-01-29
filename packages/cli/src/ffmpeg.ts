4149ef9139f39f927c275dea317de436df4d2bbf
Jonny Burger <jonathanburger11@gmail.com>
2024-01-29 12:08:51 +0100

    fix CWD, at least on macos

--- packages/cli/src/ffmpeg.ts
+++ packages/cli/src/ffmpeg.ts
@@ -1,8 +1,30 @@
+import type {LogLevel} from '@remotion/renderer';
 import {RenderInternals} from '@remotion/renderer';
 import {spawnSync} from 'node:child_process';
 import {chmodSync} from 'node:fs';
 import path from 'node:path';
 import {ConfigInternals} from './config';
+
+export const dynamicLibEnv = (indent: boolean, logLevel: LogLevel) => {
+	const lib = path.dirname(
+		RenderInternals.getExecutablePath('compositor', indent, logLevel),
+	);
+
+	return {
+		RUST_BACKTRACE: 'full',
+		...(process.platform === 'darwin'
+			? {
+					DYLD_LIBRARY_PATH: lib,
+				}
+			: process.platform === 'win32'
+				? {
+						PATH: `${lib};${process.env.PATH}`,
+					}
+				: {
+						LD_LIBRARY_PATH: lib,
+					}),
+	};
+};
 
 export const ffmpegCommand = (_root: string, args: string[]) => {
 	const logLevel = ConfigInternals.Logging.getLogLevel();
@@ -13,8 +35,8 @@ export const ffmpegCommand = (_root: string, args: string[]) => {
 	}
 
 	const done = spawnSync(binary, args, {
-		cwd: path.dirname(binary),
 		stdio: 'inherit',
+		env: dynamicLibEnv(false, logLevel),
 	});
 	process.exit(done.status as number);
 };
@@ -30,6 +52,7 @@ export const ffprobeCommand = (_root: string, args: string[]) => {
 	const done = spawnSync(binary, args, {
 		cwd: path.dirname(binary),
 		stdio: 'inherit',
+		env: dynamicLibEnv(false, logLevel),
 	});
 	process.exit(done.status as number);
 };
