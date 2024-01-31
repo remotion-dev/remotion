44202b245a0811bfdb7049dd432ca6f85bd975e4
Jonny Burger <jonathanburger11@gmail.com>
2024-01-31 15:15:12 +0100

    v4.0.104

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.103';
+export const VERSION = '4.0.104';
--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.103"
+VERSION = "4.0.104"
