bf7be2351cb8e7c1964be9715d796bfff1ad2486
Jonny Burger <jonathanburger11@gmail.com>
2024-02-05 11:36:14 +0100

    use native nodejs child process instead of execa

--- packages/renderer/src/call-ffmpeg.ts
+++ packages/renderer/src/call-ffmpeg.ts
@@ -1,4 +1,6 @@
 import execa from 'execa';
+import type {SpawnOptionsWithoutStdio} from 'node:child_process';
+import {spawn} from 'node:child_process';
 import {chmodSync} from 'node:fs';
 import path from 'path';
 import {getExecutablePath} from './compositor/get-executable-path';
@@ -24,6 +26,30 @@ export const callFf = ({
 	}
 
 	return execa(executablePath, args.filter(truthy), {
+		cwd: path.dirname(executablePath),
+		...options,
+	});
+};
+
+export const callFfNative = ({
+	args,
+	bin,
+	indent,
+	logLevel,
+	options,
+}: {
+	bin: 'ffmpeg' | 'ffprobe';
+	args: (string | null)[];
+	indent: boolean;
+	logLevel: LogLevel;
+	options?: SpawnOptionsWithoutStdio;
+}) => {
+	const executablePath = getExecutablePath(bin, indent, logLevel);
+	if (!process.env.READ_ONLY_FS) {
+		chmodSync(executablePath, 0o755);
+	}
+
+	return spawn(executablePath, args.filter(truthy), {
 		cwd: path.dirname(executablePath),
 		...options,
 	});
