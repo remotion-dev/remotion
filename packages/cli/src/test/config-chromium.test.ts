0002ba78cdf9a6ce792017b142627fb2677fc45c
Jonny Burger <jonathanburger11@gmail.com>
2024-02-18 12:57:08 +0100

    Update config-chromium.test.ts

--- packages/cli/src/test/config-chromium.test.ts
+++ packages/cli/src/test/config-chromium.test.ts
@@ -4,22 +4,22 @@ import {Config} from '../config';
 
 test('getChromiumOpenGlRenderer from Config - angle value', () => {
 	Config.setChromiumOpenGlRenderer('angle');
-	expect(BrowserSafeApis.options.glOption.getValue({commandLine: {}})).toEqual(
-		'angle',
-	);
+	expect(
+		BrowserSafeApis.options.glOption.getValue({commandLine: {}}).value,
+	).toEqual('angle');
 });
 
 test('getChromiumOpenGlRenderer from old Puppeter Config - egl value', () => {
 	Config.setChromiumOpenGlRenderer('egl');
-	expect(BrowserSafeApis.options.glOption.getValue({commandLine: {}})).toEqual(
-		'egl',
-	);
+	expect(
+		BrowserSafeApis.options.glOption.getValue({commandLine: {}}).value,
+	).toEqual('egl');
 });
 
 test('getChromiumOpenGlRenderer, override Puppeter Config - angle value', () => {
 	Config.setChromiumOpenGlRenderer('egl');
 	Config.setChromiumOpenGlRenderer('angle');
-	expect(BrowserSafeApis.options.glOption.getValue({commandLine: {}})).toEqual(
-		'angle',
-	);
+	expect(
+		BrowserSafeApis.options.glOption.getValue({commandLine: {}}).value,
+	).toEqual('angle');
 });
