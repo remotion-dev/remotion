ada1dc3f3b629da5f21ac8b6719587206e1fcbf9
Parsa Rahimi <parsa@bestever.io>
2024-02-19 14:23:47 -0800

    Fix formatting

--- packages/gif/src/resolve-gif-source.ts
+++ packages/gif/src/resolve-gif-source.ts
@@ -1,6 +1,4 @@
 export const resolveGifSource = (src: string): string => {
-	return new URL(
-		src,
-		typeof window === 'undefined' ? undefined : window.origin,
-	).href;
+	return new URL(src, typeof window === 'undefined' ? undefined : window.origin)
+		.href;
 };
