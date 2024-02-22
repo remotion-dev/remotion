9395618929e0a8c88c200870acbeb045cec658ce
Jonny Burger <jonathanburger11@gmail.com>
2024-02-22 15:55:14 +0100

    v4.0.118

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.117"
+const VERSION = "4.0.118"
--- packages/lambda-php/src/Version.php
+++ packages/lambda-php/src/Version.php
@@ -1,4 +1,4 @@
 <?php 
 namespace Remotion\LambdaPhp;
 
-const VERSION = "4.0.117";
+const VERSION = "4.0.118";
