ecad3e9ca06b17b449fbe30400e976a2b55ecfba
Jonny Burger <jonathanburger11@gmail.com>
2024-03-12 13:46:19 +0100

    `@remotion/cloudrun`: Point more prominently towards light client

--- packages/cloudrun/src/index.ts
+++ packages/cloudrun/src/index.ts
@@ -10,15 +10,30 @@ import {getOrCreateBucket} from './api/get-or-create-bucket';
 import {getRegions} from './api/get-regions';
 import {getServiceInfo} from './api/get-service-info';
 import {getServices} from './api/get-services';
-import {getSites} from './api/get-sites';
+import {getSites as deprecatedGetSites} from './api/get-sites';
 import {testPermissions} from './api/iam-validation/testPermissions';
 import type {RenderMediaOnCloudrunInput} from './api/render-media-on-cloudrun';
-import {renderMediaOnCloudrun} from './api/render-media-on-cloudrun';
-import {renderStillOnCloudrun} from './api/render-still-on-cloudrun';
+import {renderMediaOnCloudrun as deprecatedRenderMediaOnCloudrun} from './api/render-media-on-cloudrun';
+import {renderStillOnCloudrun as deprecatedRenderStillOnCloudrun} from './api/render-still-on-cloudrun';
 import {speculateServiceName} from './api/speculate-service-name';
 import type {RenderMediaOnCloudrunOutput} from './functions/helpers/payloads';
 import {CloudrunInternals} from './internals';
 import type {GcpRegion} from './pricing/gcp-regions';
+
+/**
+ * @deprecated  Import this from `@remotion/cloudrun/client` instead
+ */
+const renderMediaOnCloudrun = deprecatedRenderMediaOnCloudrun;
+
+/**
+ * @deprecated  Import this from `@remotion/cloudrun/client` instead
+ */
+const renderStillOnCloudrun = deprecatedRenderStillOnCloudrun;
+
+/**
+ * @deprecated Import this from `@remotion/lambda/client` instead
+ */
+const getSites = deprecatedGetSites;
 
 export {
 	CloudrunInternals,
