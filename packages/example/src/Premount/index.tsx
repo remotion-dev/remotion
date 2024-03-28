d786271d0cb4668fc62100ccd2f68d13b50bb985
Jonny Burger <jonathanburger11@gmail.com>
2024-03-28 16:43:51 +0100

    premount

---
+++ packages/example/src/Premount/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+	AbsoluteFill,
+	Freeze,
+	Sequence,
+	staticFile,
+	useCurrentFrame,
+	Video,
+} from 'remotion';
+
+const Premount: React.FC<{
+	children: React.ReactNode;
+	premountFor: number;
+}> = ({children, premountFor}) => {
+	const frame = useCurrentFrame();
+	const active = frame < premountFor;
+
+	return (
+		<Freeze frame={0} active={active}>
+			<Sequence
+				layout="none"
+				name={`<Premount premountFor={${premountFor}}>`}
+				from={active ? 0 : premountFor}
+			>
+				{children}
+			</Sequence>
+		</Freeze>
+	);
+};
+
+const ShouldNotUnmount: React.FC = () => {
+	return <Video src={staticFile('framer.webm')} />;
+};
+
+export const PremountedExample: React.FC = () => {
+	return (
+		<AbsoluteFill>
+			<Premount premountFor={10}>
+				<ShouldNotUnmount />
+			</Premount>
+		</AbsoluteFill>
+	);
+};
