9b825e83d5a286c1ef06d46329fff9d1dd1e27d6
Jonny Burger <jonathanburger11@gmail.com>
2024-03-28 15:58:46 +0100

    fix audio playback rates

--- packages/example/src/Root.tsx
+++ packages/example/src/Root.tsx
@@ -354,7 +354,7 @@ export const Index: React.FC = () => {
 					width={1080}
 					height={1080}
 					fps={30}
-					durationInFrames={500}
+					durationInFrames={10000}
 				/>
 				<Composition
 					id="skip-zero-frame"
