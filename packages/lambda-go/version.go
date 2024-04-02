5e4e3002ba46220ea7908f4b937655f981c4a4be
Jonny Burger <jonathanburger11@gmail.com>
2024-04-02 11:23:31 +0200

    v4.0.135

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.134"
+const VERSION = "4.0.135"
--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.134"
+VERSION = "4.0.135"
