6c6c02ceb643e0ce799b2a876f93c6fa43cb1190
Jonny Burger <jonathanburger11@gmail.com>
2024-04-05 18:10:48 +0200

    fix

--- packages/paths/src/test/serialize-instructions.test.ts
+++ packages/paths/src/test/serialize-instructions.test.ts
@@ -1,4 +1,4 @@
-import {expect, test} from 'vitest';
+import {expect, test} from 'bun:test';
 import {serializeInstructions} from '../serialize-instructions';
 
 test('Should serialize instructions', () => {
