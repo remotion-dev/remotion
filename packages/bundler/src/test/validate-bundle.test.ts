8067631f8c38742d3555ae2358fed9a76f205935
Jonny Burger <jonathanburger11@gmail.com>
2024-04-05 16:30:13 +0200

    vitest

--- packages/bundler/src/test/validate-bundle.test.ts
+++ packages/bundler/src/test/validate-bundle.test.ts
@@ -3,12 +3,10 @@ import {bundle} from '../bundle';
 
 test('Should reject bad bundle options', () => {
 	// @ts-expect-error
-	expect(() => bundle()).rejects.toThrow(
-		/bundle\(\) was called without arguments/,
-	);
+	expect(() => bundle()).toThrow(/bundle\(\) was called without arguments/);
 
 	// @ts-expect-error
-	expect(() => bundle({})).rejects.toThrow(
+	expect(() => bundle({})).toThrow(
 		/bundle\(\) was called without the `entryPoint` option/,
 	);
 });
