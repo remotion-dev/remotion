70899a144dc80b43e4763f13ffe24ffe372216f3
Jonny Burger <jonathanburger11@gmail.com>
2024-04-09 17:45:01 +0200

    writing docs

--- packages/example/src/RetryDelayRender.tsx
+++ packages/example/src/RetryDelayRender.tsx
@@ -1,10 +1,10 @@
 import {useEffect} from 'react';
-import {delayRender, useCurrentFrame} from 'remotion';
+import {delayRender, random, useCurrentFrame} from 'remotion';
 
 export const RetryDelayRender: React.FC = () => {
 	const frame = useCurrentFrame();
 	useEffect(() => {
-		if (Math.random() < 0.6) {
+		if (random(null) < 0.6) {
 			delayRender('Retrying twice', {
 				timeoutInMilliseconds: 7000,
 			});
