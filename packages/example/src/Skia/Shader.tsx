b758a6f960660282dfab3dd32e5feb6685776edd
Jonny Burger <jonathanburger11@gmail.com>
2024-04-19 14:47:16 +0200

    upgrade skia

--- packages/example/src/Skia/Shader.tsx
+++ packages/example/src/Skia/Shader.tsx
@@ -1,27 +1,41 @@
 import {
 	Canvas,
-	Circle,
-	Group,
-	RuntimeShader,
+	Fill,
+	ImageShader,
+	Shader,
 	Skia,
+	useImage,
 } from '@shopify/react-native-skia';
+import React from 'react';
+import {staticFile} from 'remotion';
 
 const source = Skia.RuntimeEffect.Make(`
 uniform shader image;
- 
-half4 main(float2 xy) {
+ 
+half4 main(float2 xy) {   
+  xy.x += sin(xy.y / 3) * 4;
   return image.eval(xy).rbga;
-}
-`)!;
+}`)!;
 
 export const RuntimeShaderDemo = () => {
-	const r = 128;
+	const s = React.useState(0);
+
+	const image = useImage(staticFile('nested/mp4.png'));
+	if (!image) {
+		return null;
+	}
+
 	return (
-		<Canvas style={{flex: 1}}>
-			<Group>
-				<RuntimeShader source={source} />
-				<Circle cx={r} cy={r} r={r} color="lightblue" />
-			</Group>
+		<Canvas style={{width: 1080, height: 1080}}>
+			<Fill>
+				<Shader source={source}>
+					<ImageShader
+						image={image}
+						fit="cover"
+						rect={{x: 0, y: 0, width: 1080, height: 1080}}
+					/>
+				</Shader>
+			</Fill>
 		</Canvas>
 	);
 };
