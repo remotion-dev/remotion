6fcfa94c99fe0ebbc8fe47adfe12e09744586be4
Jonny Burger <jonathanburger11@gmail.com>
2024-04-20 20:23:27 +0200

    bun bundle.ts

---
+++ packages/three/bundle.ts
@@ -0,0 +1,23 @@
+import {build} from 'bun';
+
+const output = await build({
+	entrypoints: ['src/index.ts'],
+	naming: '[name].mjs',
+	external: [
+		'react',
+		'remotion',
+		'@remotion/media-utils',
+		'remotion/no-react',
+		'react/jsx-runtime',
+		'@react-three/fiber',
+		'three/src/textures/VideoTexture.js',
+		'three/src/loaders/TextureLoader.js',
+	],
+});
+
+const [file] = output.outputs;
+const text = await file.text();
+
+await Bun.write('dist/esm/index.mjs', text);
+
+export {};
