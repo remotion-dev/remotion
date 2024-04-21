d0b9431138edbf4f220ff80ecb25427be0543196
Jonny Burger <jonathanburger11@gmail.com>
2024-04-21 20:44:08 +0200

    alright

--- packages/google-fonts/ensure-generation.ts
+++ packages/google-fonts/ensure-generation.ts
@@ -8,7 +8,7 @@ if (filesCjs.length < 2200) {
 }
 const filesEsm = fs.readdirSync("./dist/esm");
 
-if (filesEsm.length < 2200) {
+if (filesEsm.length < 1100) {
   console.error("Not publishing, Google fonts have not been generated");
   process.exit(1);
 }
