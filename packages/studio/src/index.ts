da861e86aacf6a6a90a9b7d7e905dee7967906e1
Jonny Burger <jonathanburger11@gmail.com>
2024-04-22 18:40:13 +0200

    Update index.ts

--- packages/studio/src/index.ts
+++ packages/studio/src/index.ts
@@ -1,3 +1,3 @@
 export {StaticFile, getStaticFiles} from './api/get-static-files';
-export {updateDefaultProps} from './api/use-update-default-props';
+export {updateDefaultProps} from './api/update-default-props';
 export {watchStaticFile} from './api/watch-static-file';
