69f39f5c617598a3ea448d3508f5c7b9e6c323e6
Jonny Burger <jonathanburger11@gmail.com>
2024-04-29 15:26:15 +0200

    `@remotion/cloudrun`: Implement ETag diffing when deploying a site

--- packages/cloudrun/src/shared/get-etag.ts
+++ packages/cloudrun/src/shared/get-etag.ts
@@ -1,23 +1,10 @@
 import crypto from 'crypto';
 import fs from 'fs';
 
-const chunk = 1024 * 1024 * 5; // 5MB
-
 const md5 = (data: Buffer) =>
 	crypto.createHash('md5').update(data).digest('hex');
 
 export const getEtagOfFile = async (filePath: string) => {
 	const stream = await fs.promises.readFile(filePath);
-	if (stream.length <= chunk) {
-		return `"${md5(stream)}"`;
-	}
-
-	const md5Chunks = [];
-	const chunksNumber = Math.ceil(stream.length / chunk);
-	for (let i = 0; i < chunksNumber; i++) {
-		const chunkStream = stream.slice(i * chunk, (i + 1) * chunk);
-		md5Chunks.push(md5(chunkStream));
-	}
-
-	return `"${md5(Buffer.from(md5Chunks.join(''), 'hex'))}-${chunksNumber}"`;
+	return `${md5(stream)}`;
 };
