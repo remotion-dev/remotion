b6fd76b3e2add43977adcd5413a50764bbb6a3aa
Jonny Burger <jonathanburger11@gmail.com>
2024-05-03 17:37:13 +0200

    Require Bun 1.1.7 for contributing

--- packages/skia/bundle.ts
+++ packages/skia/bundle.ts
@@ -1,16 +1,7 @@
-import {build, revision} from 'bun';
+import {build} from 'bun';
 
 if (process.env.NODE_ENV !== 'production') {
 	throw new Error('This script must be run using NODE_ENV=production');
-}
-
-if (!revision.startsWith('07ce')) {
-	// eslint-disable-next-line no-console
-	console.warn('warn: Remotion currently uses a fork of Bun to bundle.');
-	// eslint-disable-next-line no-console
-	console.log(
-		'You dont currently run the fork, this could lead to duplicate key warnings in React.',
-	);
 }
 
 const mainModule = await build({
