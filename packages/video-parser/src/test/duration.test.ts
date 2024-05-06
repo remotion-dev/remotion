aa77b84d5e0ed64a299a339d22ef7aecaa60d543
Jonny Burger <jonathanburger11@gmail.com>
2024-05-06 14:25:48 +0200

    duration test ts

---
+++ packages/video-parser/src/test/duration.test.ts
@@ -0,0 +1,26 @@
+import {RenderInternals} from '@remotion/renderer';
+import {expect, test} from 'bun:test';
+import {getDuration} from '../get-duration';
+import {parseVideo} from '../parse-video';
+
+const {exampleVideos} = RenderInternals;
+
+test('Should get duration of video', async () => {
+	const parsed = await parseVideo(exampleVideos.framer24fps, 128 * 1024);
+	if (!parsed) {
+		throw new Error('No parsed data');
+	}
+
+	const duration = getDuration(parsed);
+	expect(duration).toBe(4.167);
+});
+
+test('Should get duration of HEVC video', async () => {
+	const parsed = await parseVideo(exampleVideos.iphonehevc, Infinity);
+	if (!parsed) {
+		throw new Error('No parsed data');
+	}
+
+	const duration = getDuration(parsed);
+	expect(duration).toBe(3.4);
+});
--- packages/video-parser/src/test/parse-video.test.ts
+++ packages/video-parser/src/test/parse-video.test.ts
@@ -1,6 +1,5 @@
 import {RenderInternals} from '@remotion/renderer';
 import {expect, test} from 'bun:test';
-import {getDuration} from '../get-duration';
 import {parseVideo} from '../parse-video';
 
 const {exampleVideos} = RenderInternals;
@@ -132,24 +131,4 @@ test('Parse a full video', async () => {
 	if (!fourth) {
 		throw new Error('No extra data');
 	}
-});
-
-test('Should get duration of video', async () => {
-	const parsed = await parseVideo(exampleVideos.framer24fps, 128 * 1024);
-	if (!parsed) {
-		throw new Error('No parsed data');
-	}
-
-	const duration = getDuration(parsed);
-	expect(duration).toBe(4.167);
-});
-
-test('Should get duration of HEVC video', async () => {
-	const parsed = await parseVideo(exampleVideos.iphonehevc, Infinity);
-	if (!parsed) {
-		throw new Error('No parsed data');
-	}
-
-	const duration = getDuration(parsed);
-	expect(duration).toBe(3.4);
 });
