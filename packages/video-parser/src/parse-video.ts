1d92298b392b95a1e95142f171dc03788e5664ad
Jonny Burger <jonathanburger11@gmail.com>
2024-05-06 13:08:08 +0200

    parse video test

--- packages/video-parser/src/parse-video.ts
+++ packages/video-parser/src/parse-video.ts
@@ -1,147 +1,147 @@
-import { createReadStream } from "fs";
-import type { MvhdBox } from "./boxes/iso-base-media/mvhd";
-import { parseMvhd } from "./boxes/iso-base-media/mvhd";
+import {createReadStream} from 'fs';
+import type {MvhdBox} from './boxes/iso-base-media/mvhd';
+import {parseMvhd} from './boxes/iso-base-media/mvhd';
 
 const fourByteToNumber = (data: Buffer, from: number) => {
-  return (
-    (data[from + 0] << 24) |
-    (data[from + 1] << 16) |
-    (data[from + 2] << 8) |
-    data[from + 3]
-  );
+	return (
+		(data[from + 0] << 24) |
+		(data[from + 1] << 16) |
+		(data[from + 2] << 8) |
+		data[from + 3]
+	);
 };
 
 type ExtraData =
-  | {
-      type: "ftyp-box";
-      majorBrand: string;
-      minorVersion: number;
-      compatibleBrands: string[];
-    }
-  | {
-      type: "boxes";
-      boxes: Box[];
-    }
-  | {
-      type: "mvhd-box";
-      box: MvhdBox;
-    };
+	| {
+			type: 'ftyp-box';
+			majorBrand: string;
+			minorVersion: number;
+			compatibleBrands: string[];
+	  }
+	| {
+			type: 'boxes';
+			boxes: Box[];
+	  }
+	| {
+			type: 'mvhd-box';
+			box: MvhdBox;
+	  };
 
 export type Box = {
-  boxType: string;
-  boxSize: number;
-  extraData: ExtraData | undefined;
-  offset: number;
+	boxType: string;
+	boxSize: number;
+	extraData: ExtraData | undefined;
+	offset: number;
 };
 
 const getExtraDataFromBox = (
-  box: Buffer,
-  type: string
+	box: Buffer,
+	type: string,
 ): ExtraData | undefined => {
-  if (type === "ftyp") {
-    return parseFtyp(box);
-  }
+	if (type === 'ftyp') {
+		return parseFtyp(box);
+	}
 
-  if (type === "mvhd") {
-    return { type: "mvhd-box", box: parseMvhd(box) };
-  }
+	if (type === 'mvhd') {
+		return {type: 'mvhd-box', box: parseMvhd(box)};
+	}
 
-  if (
-    type === "moov" ||
-    type === "trak" ||
-    type === "mdia" ||
-    type === "minf" ||
-    type === "stbl" ||
-    type === "stsb"
-  ) {
-    return parseBoxes(box.subarray(8));
-  }
+	if (
+		type === 'moov' ||
+		type === 'trak' ||
+		type === 'mdia' ||
+		type === 'minf' ||
+		type === 'stbl' ||
+		type === 'stsb'
+	) {
+		return parseBoxes(box.subarray(8));
+	}
 };
 
 const processBoxAndSubtract = (data: Buffer, offset: number): BoxAndNext => {
-  const boxSize = fourByteToNumber(data, 0);
-  const boxType = data.subarray(4, 8).toString("utf-8");
+	const boxSize = fourByteToNumber(data, 0);
+	const boxType = data.subarray(4, 8).toString('utf-8');
 
-  const next = data.subarray(boxSize);
+	const next = data.subarray(boxSize);
 
-  return {
-    next,
-    boxType,
-    boxSize,
-    extraData: getExtraDataFromBox(data.subarray(0, boxSize), boxType),
-    offset: offset + boxSize,
-  };
+	return {
+		next,
+		boxType,
+		boxSize,
+		extraData: getExtraDataFromBox(data.subarray(0, boxSize), boxType),
+		offset: offset + boxSize,
+	};
 };
 
 type BoxAndNext = Box & {
-  next: Buffer;
+	next: Buffer;
 };
 
-const isoBaseMediaMp4Pattern = Buffer.from("ftyp");
+const isoBaseMediaMp4Pattern = Buffer.from('ftyp');
 
 const parseBoxes = (data: Buffer): ExtraData => {
-  const boxes: Box[] = [];
-  let remaining = data;
-  let bytesConsumed = 0;
-  while (remaining.length > 0) {
-    const { next, boxType, boxSize, extraData, offset } = processBoxAndSubtract(
-      remaining,
-      bytesConsumed
-    );
-    remaining = next;
-    boxes.push({ boxType, boxSize, extraData, offset: bytesConsumed });
-    bytesConsumed = offset;
-  }
+	const boxes: Box[] = [];
+	let remaining = data;
+	let bytesConsumed = 0;
+	while (remaining.length > 0) {
+		const {next, boxType, boxSize, extraData, offset} = processBoxAndSubtract(
+			remaining,
+			bytesConsumed,
+		);
+		remaining = next;
+		boxes.push({boxType, boxSize, extraData, offset: bytesConsumed});
+		bytesConsumed = offset;
+	}
 
-  return { boxes, type: "boxes" };
+	return {boxes, type: 'boxes'};
 };
 
 const parseFtyp = (data: Buffer): ExtraData => {
-  const majorBrand = data.subarray(8, 12).toString("utf-8").trim();
+	const majorBrand = data.subarray(8, 12).toString('utf-8').trim();
 
-  const minorVersion = fourByteToNumber(data, 12);
+	const minorVersion = fourByteToNumber(data, 12);
 
-  const rest = data.subarray(16);
-  const types = rest.length / 4;
-  const compatibleBrands: string[] = [];
-  for (let i = 0; i < types; i++) {
-    const fourBytes = rest.subarray(i * 4, i * 4 + 4);
-    compatibleBrands.push(fourBytes.toString("utf-8").trim());
-  }
+	const rest = data.subarray(16);
+	const types = rest.length / 4;
+	const compatibleBrands: string[] = [];
+	for (let i = 0; i < types; i++) {
+		const fourBytes = rest.subarray(i * 4, i * 4 + 4);
+		compatibleBrands.push(fourBytes.toString('utf-8').trim());
+	}
 
-  return {
-    type: "ftyp-box",
-    majorBrand,
-    minorVersion,
-    compatibleBrands,
-  };
+	return {
+		type: 'ftyp-box',
+		majorBrand,
+		minorVersion,
+		compatibleBrands,
+	};
 };
 
 const matchesPattern = (pattern: Buffer) => {
-  return (data: Buffer) => {
-    return pattern.every((value, index) => data[index] === value);
-  };
+	return (data: Buffer) => {
+		return pattern.every((value, index) => data[index] === value);
+	};
 };
 
 export const parseVideo = async (src: string, bytes: number) => {
-  const stream = createReadStream(
-    src,
-    Number.isFinite(bytes) ? { end: bytes - 1 } : {}
-  );
-  const data = await new Promise<Buffer>((resolve, reject) => {
-    stream.on("data", (chunk) => {
-      resolve(chunk as Buffer);
-    });
+	const stream = createReadStream(
+		src,
+		Number.isFinite(bytes) ? {end: bytes - 1} : {},
+	);
+	const data = await new Promise<Buffer>((resolve, reject) => {
+		stream.on('data', (chunk) => {
+			resolve(chunk as Buffer);
+		});
 
-    stream.on("error", (err) => {
-      reject(err);
-    });
-  });
+		stream.on('error', (err) => {
+			reject(err);
+		});
+	});
 
-  if (matchesPattern(isoBaseMediaMp4Pattern)(data.subarray(4, 8))) {
-    const boxes = parseBoxes(data);
-    if (boxes.type === "boxes") {
-      return boxes.boxes;
-    }
-  }
+	if (matchesPattern(isoBaseMediaMp4Pattern)(data.subarray(4, 8))) {
+		const boxes = parseBoxes(data);
+		if (boxes.type === 'boxes') {
+			return boxes.boxes;
+		}
+	}
 };
--- packages/video-parser/src/test/parse-video.test.ts
+++ packages/video-parser/src/test/parse-video.test.ts
@@ -1,138 +1,140 @@
-import { test, expect } from "bun:test";
-import { parseVideo } from "../parse-video";
-import { exampleVideos } from "./example-videos";
-import { getDuration } from "../get-duration";
+import {RenderInternals} from '@remotion/renderer';
+import {expect, test} from 'bun:test';
+import {getDuration} from '../get-duration';
+import {parseVideo} from '../parse-video';
 
-test("Parse Big Buck bunny", async () => {
-  const data = await parseVideo(exampleVideos.bigBuckBunny, 4 * 1024);
-  expect(data).toEqual([
-    {
-      offset: 0,
-      boxSize: 32,
-      boxType: "ftyp",
-      extraData: {
-        type: "ftyp-box",
-        majorBrand: "isom",
-        minorVersion: 512,
-        compatibleBrands: ["isom", "iso2", "avc1", "mp41"],
-      },
-    },
-    {
-      offset: 32,
-      boxSize: 8,
-      boxType: "free",
-      extraData: undefined,
-    },
-    {
-      boxSize: 14282275,
-      boxType: "mdat",
-      extraData: undefined,
-      offset: 40,
-    },
-  ]);
+const {exampleVideos} = RenderInternals;
+
+test('Parse Big Buck bunny', async () => {
+	const data = await parseVideo(exampleVideos.bigBuckBunny, 4 * 1024);
+	expect(data).toEqual([
+		{
+			offset: 0,
+			boxSize: 32,
+			boxType: 'ftyp',
+			extraData: {
+				type: 'ftyp-box',
+				majorBrand: 'isom',
+				minorVersion: 512,
+				compatibleBrands: ['isom', 'iso2', 'avc1', 'mp41'],
+			},
+		},
+		{
+			offset: 32,
+			boxSize: 8,
+			boxType: 'free',
+			extraData: undefined,
+		},
+		{
+			boxSize: 14282275,
+			boxType: 'mdat',
+			extraData: undefined,
+			offset: 40,
+		},
+	]);
 });
 
-test("Parse an iPhone video", async () => {
-  const data = await parseVideo(exampleVideos.iphonevideo, 4 * 1024);
-  expect(data).toEqual([
-    {
-      boxSize: 20,
-      boxType: "ftyp",
-      extraData: {
-        type: "ftyp-box",
-        majorBrand: "qt",
-        minorVersion: 0,
-        compatibleBrands: ["qt"],
-      },
-      offset: 0,
-    },
-    {
-      boxType: "wide",
-      boxSize: 8,
-      extraData: undefined,
-      offset: 20,
-    },
-    {
-      boxSize: 39048800,
-      boxType: "mdat",
-      extraData: undefined,
-      offset: 28,
-    },
-  ]);
+test('Parse an iPhone video', async () => {
+	const data = await parseVideo(exampleVideos.iphonevideo, 4 * 1024);
+	expect(data).toEqual([
+		{
+			boxSize: 20,
+			boxType: 'ftyp',
+			extraData: {
+				type: 'ftyp-box',
+				majorBrand: 'qt',
+				minorVersion: 0,
+				compatibleBrands: ['qt'],
+			},
+			offset: 0,
+		},
+		{
+			boxType: 'wide',
+			boxSize: 8,
+			extraData: undefined,
+			offset: 20,
+		},
+		{
+			boxSize: 39048800,
+			boxType: 'mdat',
+			extraData: undefined,
+			offset: 28,
+		},
+	]);
 });
 
-test("Parse framer", async () => {
-  const parsed = await parseVideo(
-    exampleVideos.framerWithoutFileExtension,
-    4 * 1024
-  );
-  expect(parsed).toEqual([
-    {
-      offset: 0,
-      boxSize: 32,
-      boxType: "ftyp",
-      extraData: {
-        compatibleBrands: ["isom", "iso2", "avc1", "mp41"],
-        majorBrand: "isom",
-        minorVersion: 512,
-        type: "ftyp-box",
-      },
-    },
-    {
-      offset: 32,
-      boxSize: 8,
-      boxType: "free",
-      extraData: undefined,
-    },
-    {
-      offset: 40,
-      boxSize: 73010,
-      boxType: "mdat",
-      extraData: undefined,
-    },
-  ]);
+test('Parse framer', async () => {
+	const parsed = await parseVideo(
+		exampleVideos.framerWithoutFileExtension,
+		4 * 1024,
+	);
+	expect(parsed).toEqual([
+		{
+			offset: 0,
+			boxSize: 32,
+			boxType: 'ftyp',
+			extraData: {
+				compatibleBrands: ['isom', 'iso2', 'avc1', 'mp41'],
+				majorBrand: 'isom',
+				minorVersion: 512,
+				type: 'ftyp-box',
+			},
+		},
+		{
+			offset: 32,
+			boxSize: 8,
+			boxType: 'free',
+			extraData: undefined,
+		},
+		{
+			offset: 40,
+			boxSize: 73010,
+			boxType: 'mdat',
+			extraData: undefined,
+		},
+	]);
 });
 
-test("Parse a full video", async () => {
-  const data = await parseVideo(exampleVideos.framer24fps, Infinity);
-  if (!data) throw new Error("No data");
+test('Parse a full video', async () => {
+	const data = await parseVideo(exampleVideos.framer24fps, Infinity);
+	if (!data) throw new Error('No data');
 
-  expect(data[0]).toEqual({
-    offset: 0,
-    boxSize: 32,
-    boxType: "ftyp",
-    extraData: {
-      type: "ftyp-box",
-      majorBrand: "isom",
-      minorVersion: 512,
-      compatibleBrands: ["isom", "iso2", "avc1", "mp41"],
-    },
-  });
-  expect(data[1]).toEqual({
-    offset: 32,
-    boxType: "free",
-    boxSize: 8,
-    extraData: undefined,
-  });
-  expect(data[2]).toEqual({
-    offset: 40,
-    boxSize: 57014,
-    boxType: "mdat",
-    extraData: undefined,
-  });
+	expect(data[0]).toEqual({
+		offset: 0,
+		boxSize: 32,
+		boxType: 'ftyp',
+		extraData: {
+			type: 'ftyp-box',
+			majorBrand: 'isom',
+			minorVersion: 512,
+			compatibleBrands: ['isom', 'iso2', 'avc1', 'mp41'],
+		},
+	});
+	expect(data[1]).toEqual({
+		offset: 32,
+		boxType: 'free',
+		boxSize: 8,
+		extraData: undefined,
+	});
+	expect(data[2]).toEqual({
+		offset: 40,
+		boxSize: 57014,
+		boxType: 'mdat',
+		extraData: undefined,
+	});
 
-  const moov = data[3].extraData;
-  if (!moov) {
-    throw new Error("No extra data");
-  }
+	const moov = data[3].extraData;
+	if (!moov) {
+		throw new Error('No extra data');
+	}
 });
 
-test("Should get duration of video", async () => {
-  const parsed = await parseVideo(exampleVideos.framer24fps, 128 * 1024);
-  if (!parsed) {
-    throw new Error("No parsed data");
-  }
+test('Should get duration of video', async () => {
+	const parsed = await parseVideo(exampleVideos.framer24fps, 128 * 1024);
+	if (!parsed) {
+		throw new Error('No parsed data');
+	}
 
-  const duration = getDuration(parsed);
-  expect(duration).toBe(4.167);
+	const duration = getDuration(parsed);
+	expect(duration).toBe(4.167);
 });
