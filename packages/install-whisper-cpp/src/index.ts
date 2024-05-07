dde83fcbd39580300f2ea88898b2bcfe323d832a
Jonny Burger <jonathanburger11@gmail.com>
2024-05-07 18:39:14 +0200

    `@remotion/install-whisper-cpp`: Make it install with Whisper.cpp on Windows

--- packages/install-whisper-cpp/src/index.ts
+++ packages/install-whisper-cpp/src/index.ts
@@ -1,9 +1,6 @@
 export {Caption, convertToCaptions} from './convert-to-captions';
-export {
-	OnProgress,
-	WhisperModel,
-	downloadWhisperModel,
-} from './download-whisper-model';
+export type {OnProgress} from './download';
+export {WhisperModel, downloadWhisperModel} from './download-whisper-model';
 export {installWhisperCpp} from './install-whisper-cpp';
 export type {Language} from './languages';
 export {TranscriptionJson, transcribe} from './transcribe';
