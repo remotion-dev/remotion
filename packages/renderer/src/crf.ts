0859c6ff9a412326f11d601232d8fe105ea4e2dc
Jonny Burger <jonathanburger11@gmail.com>
2024-05-07 15:11:05 +0200

    typesafe crf

--- packages/renderer/src/crf.ts
+++ packages/renderer/src/crf.ts
@@ -3,76 +3,42 @@ import {isAudioCodec} from './options/audio-codec';
 
 export type Crf = number | undefined;
 
+const defaultCrfMap: {[key in Codec]: number} = {
+	h264: 18,
+	h265: 23,
+	vp8: 9,
+	vp9: 28,
+	prores: 0,
+	gif: 0,
+	avi: 0,
+	'h264-mkv': 18,
+	'h264-ts': 18,
+	aac: 0,
+	mp3: 0,
+	wav: 0,
+};
+
 export const getDefaultCrfForCodec = (codec: Codec): number => {
-	if (isAudioCodec(codec)) {
-		return 0;
-	}
+	return defaultCrfMap[codec];
+};
 
-	if (codec === 'h264' || codec === 'h264-mkv' || codec === 'h264-ts') {
-		return 18; // FFMPEG default 23
-	}
-
-	if (codec === 'h265') {
-		return 23; // FFMPEG default 28
-	}
-
-	if (codec === 'vp8') {
-		return 9; // FFMPEG default 10
-	}
-
-	if (codec === 'vp9') {
-		return 28; // FFMPEG recommendation 31
-	}
-
-	if (codec === 'prores') {
-		return 0;
-	}
-
-	if (codec === 'gif') {
-		return 0;
-	}
-
-	if (codec === 'avi') {
-		return 0;
-	}
-
-	throw new TypeError(`Got unexpected codec "${codec}"`);
+const crfRanges: {[key in Codec]: [number, number]} = {
+	h264: [1, 51],
+	h265: [0, 51],
+	vp8: [4, 63],
+	vp9: [0, 63],
+	prores: [0, 0],
+	gif: [0, 0],
+	avi: [0, 0],
+	'h264-mkv': [1, 51],
+	'h264-ts': [1, 51],
+	aac: [0, 0],
+	mp3: [0, 0],
+	wav: [0, 0],
 };
 
 export const getValidCrfRanges = (codec: Codec): [number, number] => {
-	if (isAudioCodec(codec)) {
-		return [0, 0];
-	}
-
-	if (codec === 'prores') {
-		return [0, 0];
-	}
-
-	if (codec === 'gif') {
-		return [0, 0];
-	}
-
-	if (codec === 'avi') {
-		return [0, 0];
-	}
-
-	if (codec === 'h264' || codec === 'h264-mkv' || codec === 'h264-ts') {
-		return [1, 51];
-	}
-
-	if (codec === 'h265') {
-		return [0, 51];
-	}
-
-	if (codec === 'vp8') {
-		return [4, 63];
-	}
-
-	if (codec === 'vp9') {
-		return [0, 63];
-	}
-
-	throw new TypeError(`Got unexpected codec "${codec}"`);
+	return crfRanges[codec];
 };
 
 export const validateQualitySettings = ({
