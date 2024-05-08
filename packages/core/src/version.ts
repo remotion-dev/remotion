3f0cc316af1a2c54b6fdb3c67c0f8e867d22d174
Jonny Burger <jonathanburger11@gmail.com>
2024-05-08 08:45:13 +0200

    v4.0.151

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -5,4 +5,4 @@
  * @see [Documentation](https://remotion.dev/docs/version)
  * @returns {string} The current version of the remotion package
  */
-export const VERSION = '4.0.150';
+export const VERSION = '4.0.151';
