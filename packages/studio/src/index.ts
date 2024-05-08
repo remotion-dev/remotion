a1e06a267b6908824d084e92376fbb44e8d4d0e0
Jonny Burger <jonathanburger11@gmail.com>
2024-05-08 13:27:20 +0200

    start with updateDefaultProps() API

--- packages/studio/src/index.ts
+++ packages/studio/src/index.ts
@@ -1,4 +1,6 @@
 export {StaticFile, getStaticFiles} from './api/get-static-files';
+export {UpdateDefaultPropsFunction} from './api/helpers/calc-new-props';
 export {saveDefaultProps} from './api/save-default-props';
+export {updateDefaultProps} from './api/update-default-props';
 export {watchStaticFile} from './api/watch-static-file';
 export {writeStaticFile} from './api/write-static-file';
