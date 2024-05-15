9bc1dbdcb45b157c4b03be62d6bbc07e02e5a16c
MehmetAdemi <mehmet@remotion.dev>
2024-05-15 09:56:53 +0200

    Update showcase-videos.tsx

--- packages/docs/src/data/showcase-videos.tsx
+++ packages/docs/src/data/showcase-videos.tsx
@@ -356,12 +356,12 @@ export const showcaseVideos: ShowcaseVideo[] = [
     title: "Fluidmotion - Animated video backgrounds",
     type: "mux_video",
     time: "0",
-    muxId: "xXuyf3dfiUHcK2hYm8MkrmRZFRISHcP2E0201LYhm02e8k",
+    muxId: "eLyQruncRGvNahd3n9hB44OuziMYqGgS8R1xyhbz02s4",
     description:
       "Create beautiful animated background for apps, videos or presentations.",
     height: 1080,
-    width: 1080,
-    submittedOn: new Date("05-14-2024"),
+    width: 1920,
+    submittedOn: new Date("05-15-2024"),
     links: [
       {
         type: "website",
