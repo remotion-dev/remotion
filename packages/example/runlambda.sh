b7ae34c6786040e031ddde994651529fb7d0e352
Jonny Burger <jonathanburger11@gmail.com>
2024-05-23 16:13:46 -0400

    Discard changes to packages/example/runlambda.sh

--- packages/example/runlambda.sh
+++ packages/example/runlambda.sh
@@ -5,6 +5,6 @@ npm run buildlambda
 cd ..
 cd example
 bunx remotion lambda functions rmall -f
+bunx remotion lambda functions deploy --memory=3000 --disk=10000
 bunx remotion lambda sites create --site-name=testbed-v6 --log=verbose --enable-folder-expiry
-bunx remotion lambda functions deploy --memory=3000 --disk=10000 --log=verbose
 bunx remotion lambda render testbed-v6 OffthreadRemoteVideo --log=verbose --delete-after="1-day"
