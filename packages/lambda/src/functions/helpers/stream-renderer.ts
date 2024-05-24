661fbb26352da681f99a10ce96e3197078f617e1
Jonny Burger <jonathanburger11@gmail.com>
2024-05-24 16:12:34 -0400

    cleanup code

--- packages/lambda/src/functions/helpers/stream-renderer.ts
+++ packages/lambda/src/functions/helpers/stream-renderer.ts
@@ -3,6 +3,7 @@ import {join} from 'path';
 import type {LambdaPayload} from '../../defaults';
 import {LambdaRoutines, chunkKeyForIndex} from '../../defaults';
 import {callLambdaWithStreaming} from '../../shared/call-lambda';
+import type {OnMessage} from '../streaming/streaming';
 import {getCurrentRegionInFunction} from './get-current-region';
 import type {OverallProgressHelper} from './overall-render-progress';
 
@@ -23,6 +24,50 @@ export const streamRenderer = async ({
 		throw new Error('Expected renderer type');
 	}
 
+	const receivedStreamingPayload: OnMessage = ({message}) => {
+		if (message.type === 'frames-rendered') {
+			overallProgress.setFrames({
+				index: payload.chunk,
+				encoded: message.payload.encoded,
+				rendered: message.payload.rendered,
+			});
+			return;
+		}
+
+		if (message.type === 'video-chunk-rendered') {
+			const filename = join(
+				outdir,
+				chunkKeyForIndex({
+					index: payload.chunk,
+					type: 'video',
+				}),
+			);
+			writeFileSync(filename, message.payload);
+			files.push(filename);
+			return;
+		}
+
+		if (message.type === 'audio-chunk-rendered') {
+			const filename = join(
+				outdir,
+				chunkKeyForIndex({
+					index: payload.chunk,
+					type: 'audio',
+				}),
+			);
+			writeFileSync(filename, message.payload);
+			files.push(filename);
+			return;
+		}
+
+		if (message.type === 'chunk-complete') {
+			overallProgress.addChunkCompleted(payload.chunk);
+			return;
+		}
+
+		throw new Error(`Unknown message type ${message.type}`);
+	};
+
 	await callLambdaWithStreaming({
 		functionName,
 		payload,
@@ -30,38 +75,6 @@ export const streamRenderer = async ({
 		region: getCurrentRegionInFunction(),
 		timeoutInTest: 12000,
 		type: LambdaRoutines.renderer,
-		receivedStreamingPayload: ({message}) => {
-			if (message.type === 'frames-rendered') {
-				overallProgress.setFrames({
-					index: payload.chunk,
-					encoded: message.payload.encoded,
-					rendered: message.payload.rendered,
-				});
-			} else if (message.type === 'video-chunk-rendered') {
-				const filename = join(
-					outdir,
-					chunkKeyForIndex({
-						index: payload.chunk,
-						type: 'video',
-					}),
-				);
-				writeFileSync(filename, message.payload);
-				files.push(filename);
-			} else if (message.type === 'audio-chunk-rendered') {
-				const filename = join(
-					outdir,
-					chunkKeyForIndex({
-						index: payload.chunk,
-						type: 'audio',
-					}),
-				);
-				writeFileSync(filename, message.payload);
-				files.push(filename);
-			} else if (message.type === 'chunk-complete') {
-				overallProgress.addChunkCompleted(payload.chunk);
-			} else {
-				throw new Error(`Unknown message type ${message.type}`);
-			}
-		},
+		receivedStreamingPayload,
 	});
 };
