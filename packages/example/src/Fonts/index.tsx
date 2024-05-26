2152dc4a8cef897160d167571efc555cef799f48
benya7 <bitup.games@gmail.com>
2024-05-26 20:07:36 +0200

    add: test in Fonts composition

--- packages/example/src/Fonts/index.tsx
+++ packages/example/src/Fonts/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {AbsoluteFill} from 'remotion';
+import {loadFont} from '@remotion/fonts';
+
+loadFont({
+	family: 'Bangers',
+	url: 'bangers.woff2',
+	weight: '500'
+}).then(() => console.log("Font loaded!"))
 
 export const FontDemo: React.FC = () => {
 	return (
 		<AbsoluteFill
 			style={{
+				fontFamily: 'Bangers',
 				justifyContent: 'center',
 				alignItems: 'center',
-				fontSize: 200,
+				fontSize: 100,
 				backgroundColor: 'whitesmoke',
 			}}
 		>
