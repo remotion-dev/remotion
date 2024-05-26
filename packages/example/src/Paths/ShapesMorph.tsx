170b3cb712b30b48bc5debada9f8754657ee2751
Jonny Burger <jonathanburger11@gmail.com>
2024-05-26 10:57:15 -0400

    testbed

---
+++ packages/example/src/Paths/ShapesMorph.tsx
@@ -0,0 +1,31 @@
+import {interpolatePath} from '@remotion/paths';
+import {makeCircle, makePolygon} from '@remotion/shapes';
+import {AbsoluteFill, interpolate, useCurrentFrame} from 'remotion';
+
+export const ShapesMorph: React.FC = () => {
+	const frame = useCurrentFrame();
+
+	const {path: circlepath} = makeCircle({
+		radius: 200,
+	});
+
+	const {path: polygonpath} = makePolygon({
+		points: 5,
+		radius: 80,
+	});
+
+	const interpolation = interpolate(frame, [30, 60], [0, 1], {
+		extrapolateLeft: 'clamp',
+		extrapolateRight: 'clamp',
+	});
+
+	const interpolated = interpolatePath(interpolation, polygonpath, circlepath);
+
+	return (
+		<AbsoluteFill className="bg-gray-100 items-center justify-center">
+			<svg viewBox="0 0 720 720">
+				<path d={interpolated} />
+			</svg>
+		</AbsoluteFill>
+	);
+};
