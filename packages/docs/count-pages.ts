8c480fc7bcb033edd87608044f3507f58dfc9d1f
Jonny Burger <jonathanburger11@gmail.com>
2024-05-31 10:12:05 +0200

    again

--- packages/docs/count-pages.ts
+++ packages/docs/count-pages.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import path from "path";
-import { readDir } from "./get-pages.js";
+import { readDir } from "./get-pages.mjs";
 
 const root = path.join(process.cwd(), "docs");
 
