02bb518ef1c77fe227a41e04960751b25d8fa940
Jonny Burger <jonathanburger11@gmail.com>
2024-06-06 10:13:25 +0200

    v4.0.169

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.168"
+const VERSION = "4.0.169"
--- packages/lambda-php/src/Semantic.php
+++ packages/lambda-php/src/Semantic.php
@@ -4,5 +4,5 @@ namespace Remotion\LambdaPhp;
 
 class Semantic
 {
-    public const VERSION = "4.0.168";
+    public const VERSION = "4.0.169";
 }
