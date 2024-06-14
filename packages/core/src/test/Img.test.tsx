ef5bfc1d0fc2a80cc022f553bfb6735a609ed702
Jonny Burger <jonathanburger11@gmail.com>
2024-06-14 12:15:04 +0200

    hmm

--- packages/core/src/test/Img.test.tsx
+++ packages/core/src/test/Img.test.tsx
@@ -5,12 +5,17 @@ import {render} from '@testing-library/react';
 import React from 'react';
 import {beforeEach, expect, test} from 'vitest';
 import {Img} from '../Img.js';
+import {WrapSequenceContext} from './wrap-sequence-context.js';
 
 const ref = React.createRef<HTMLImageElement>();
 const testImgUrl = 'https://source.unsplash.com/random/50x50';
 
 beforeEach(() => {
-	render(<Img ref={ref} src={testImgUrl} />);
+	render(
+		<WrapSequenceContext>
+			<Img ref={ref} src={testImgUrl} />
+		</WrapSequenceContext>,
+	);
 });
 
 test('Img component renders img tag', () => {
