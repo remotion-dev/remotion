354b18ddcb3442b156ea350c2542ad95ec1b72d7
Jonny Burger <jonathanburger11@gmail.com>
2024-06-17 13:27:35 +0200

    add amazon linux 2023

--- packages/dockerfiles/run.sh
+++ packages/dockerfiles/run.sh
@@ -1,3 +1,4 @@
 set -e
+docker build --file Dockerfile.al2023 . --output type=tar,dest=/dev/null --progress=plain
 docker build --file Dockerfile.ubuntu . --output type=tar,dest=/dev/null --progress=plain
 docker build --file Dockerfile.debian . --output type=tar,dest=/dev/null --progress=plain
