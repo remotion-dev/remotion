ed8da7439b7d5787c21162876ea68c9b83948fe0
Jonny Burger <jonathanburger11@gmail.com>
2024-06-17 12:01:30 +0200

    add ubuntu way

--- packages/dockerfiles/run.sh
+++ packages/dockerfiles/run.sh
@@ -1,1 +1,3 @@
+set -e
+docker build --file Dockerfile.ubuntu . --output type=tar,dest=/dev/null --progress=plain
 docker build --file Dockerfile.debian . --output type=tar,dest=/dev/null --progress=plain
