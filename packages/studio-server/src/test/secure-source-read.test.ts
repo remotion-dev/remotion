d1307693401a72e3f3f79ccd80a20514c0e8370f
Jonny Burger <jonathanburger11@gmail.com>
2024-06-24 23:00:48 +0200

    `@remotion/studio-server`: bun test

--- packages/studio-server/src/test/secure-source-read.test.ts
+++ packages/studio-server/src/test/secure-source-read.test.ts
@@ -1,11 +1,11 @@
-import {expect, test} from 'vitest';
+import {expect, test} from 'bun:test';
 import {getFileSource} from '../helpers/get-file-source';
 
 test('Should not allow to read files outside of the project', () => {
-	expect(() => getFileSource(process.cwd(), '/etc/passwd')).rejects.toMatch(
+	expect(() => getFileSource(process.cwd(), '/etc/passwd')).toThrow(
 		/Not allowed to open/,
 	);
-	expect(() => getFileSource(process.cwd(), '.env')).rejects.toMatch(
+	expect(() => getFileSource(process.cwd(), '.env')).toThrow(
 		/Not allowed to open/,
 	);
 });
