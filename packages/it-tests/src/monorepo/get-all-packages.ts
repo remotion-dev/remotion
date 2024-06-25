503ccc818cad837e9f98e7df3bc4e5a5da17ca12
Jonny Burger <jonathanburger11@gmail.com>
2024-06-25 16:59:20 +0200

    Add description for every package

--- packages/it-tests/src/monorepo/get-all-packages.ts
+++ packages/it-tests/src/monorepo/get-all-packages.ts
@@ -2,12 +2,99 @@ import {existsSync, lstatSync, readdirSync, writeFileSync} from 'fs';
 import {readFileSync} from 'node:fs';
 import path from 'path';
 
+export const packages = [
+	'core',
+	'player',
+	'renderer',
+	'cli',
+	'cloudrun',
+	'lambda',
+	'bundler',
+	'studio-server',
+	'install-whisper-cpp',
+	'google-fonts',
+	'media-utils',
+	'lottie',
+	'layout-utils',
+	'noise',
+	'motion-blur',
+	'preload',
+	'shapes',
+	'zod-types',
+	'gif',
+	'eslint-plugin',
+	'eslint-config',
+	'compositor-linux-x64-gnu',
+	'compositor-linux-x64-musl',
+	'compositor-darwin-x64',
+	'compositor-darwin-arm64',
+	'compositor-linux-arm64-gnu',
+	'compositor-linux-arm64-musl',
+	'compositor-win32-x64-msvc',
+	'babel-loader',
+	'fonts',
+	'transitions',
+	'enable-scss',
+	'create-video',
+	'studio-shared',
+	'tailwind',
+	'streaming',
+	'video-parser',
+	'rive',
+	'paths',
+	'studio',
+	'skia',
+	'three',
+	'astro-example',
+	'lambda-go-example',
+	'test-utils',
+	'example-without-zod',
+	'lambda-go',
+	'animation-utils',
+	'example',
+	'lambda-php',
+	'bugs',
+	'docs',
+	'it-tests',
+	'lambda-python',
+	'player-example',
+	'ai-improvements',
+	'discord-poster',
+	'cli-autocomplete',
+];
+
+export type Pkgs = (typeof packages)[number];
+
 export const getAllPackages = () => {
 	const pkgDir = path.join(__dirname, '..', '..', '..');
-	return readdirSync(pkgDir)
+	const filePackages = readdirSync(pkgDir)
 		.filter((pkg) => lstatSync(path.join(pkgDir, pkg)).isDirectory())
 		.map((pkg) => ({pkg, path: path.join(pkgDir, pkg, 'package.json')}))
 		.filter(({path}) => existsSync(path));
+
+	const notInFile = packages.sort().map((pkg) => pkg);
+
+	if (
+		JSON.stringify(filePackages.map((pkg) => pkg.pkg).sort()) !==
+		JSON.stringify(notInFile)
+	) {
+		const diff = notInFile.filter(
+			(pkg) => !filePackages.map((pkg) => pkg.pkg).includes(pkg),
+		);
+		const diff2 = filePackages
+			.map((pkg) => pkg.pkg)
+			.filter((pkg) => !notInFile.includes(pkg));
+
+		throw new Error(
+			`Add the new package to 'get-all-packages.ts'. Diff: ${JSON.stringify(diff, null, 2)} ${JSON.stringify(
+				diff2,
+				null,
+				2,
+			)}`,
+		);
+	}
+
+	return filePackages;
 };
 
 export const updatePackageJson = (
--- packages/it-tests/src/monorepo/package-metadata.test.ts
+++ packages/it-tests/src/monorepo/package-metadata.test.ts
@@ -1,5 +1,70 @@
 import {expect, test} from 'bun:test';
-import {getAllPackages, updatePackageJson} from './get-all-packages';
+import {Pkgs, getAllPackages, updatePackageJson} from './get-all-packages';
+
+const descriptions: Record<Pkgs, string | null> = {
+	player: 'React component for embedding a Remotion preview into your app',
+	cloudrun: 'Render Remotion videos on Google Cloud Run',
+	renderer: 'Render Remotion videos using Node.js or Bun',
+	cli: 'Control Remotion features using the `npx remotion` command',
+	core: 'Make videos programmatically',
+	lambda: 'Render Remotion videos on AWS Lambda',
+	bundler: 'Bundle Remotion compositions using Webpack',
+	'studio-server': 'Run a Remotion Studio with a server backend',
+	'install-whisper-cpp': 'Helpers for installing and using Whisper.cpp',
+	'google-fonts': 'Use Google Fonts in Remotion',
+	'media-utils': 'Utilities for working with media files',
+	lottie: 'Include Lottie animations in Remotion',
+	'layout-utils': 'Utilities for working with layouts',
+	noise: 'Noise generation functions',
+	'motion-blur': 'Motion blur effect for Remotion',
+	preload: 'Preloads assets for use in Remotion',
+	shapes: 'Generate SVG shapes',
+	'zod-types': 'Zod types for Remotion',
+	gif: 'Embed GIFs in a Remotion video',
+	'eslint-plugin': 'Rules for writing Remotion code',
+	'eslint-config': 'Default configuration for Remotion templates',
+	'compositor-linux-x64-gnu': 'Linux x64 binary for the Remotion Rust code',
+	'compositor-linux-x64-musl': 'Linux x64 binary for the Remotion Rust code',
+	'compositor-darwin-x64': 'MacOS x64 binary for the Remotion Rust code',
+	'compositor-darwin-arm64':
+		'MacOS Apple Silicon binary for the Remotion Rust code',
+	'compositor-win32-x64': 'Windows x64 binary for the Remotion Rust code',
+	'compositor-linux-arm64-gnu': 'Linux ARM64 binary for the Remotion Rust code',
+	'compositor-linux-arm64-musl':
+		'Linux ARM64 binary for the Remotion Rust code',
+	'babel-loader': 'Babel loader for Remotion',
+	fonts: 'Helpers for loading local fonts into Remotion',
+	transitions: 'Library for creating transitions in Remotion',
+	'enable-scss': 'Enable SCSS support in Remotion',
+	'create-video': 'Create a new Remotion project',
+	'studio-shared':
+		'Internal package for shared objects between the Studio backend and frontend',
+	tailwind: 'Enable TailwindCSS support in Remotion',
+	streaming: 'Utilities for streaming data between programs',
+	'video-parser': 'A JavaScript parser for .mp4, .mov, .webm and .mkv files',
+	rive: 'Embed Rive animations in a Remotion video',
+	paths: 'Utilities for working with SVG paths',
+	studio: 'APIs for interacting with the Remotion Studio',
+	skia: 'Include React Native Skia components in a Remotion video',
+	three: 'Include React Three Fiber components in a Remotion video',
+	'astro-example': null,
+	'lambda-go-example': null,
+	'compositor-win32-x64-msvc': null,
+	'animation-utils': 'Helpers for animating CSS properties',
+	'test-utils': null,
+	'example-without-zod': null,
+	'lambda-go': null,
+	example: null,
+	'lambda-php': null,
+	bugs: null,
+	docs: null,
+	'it-tests': null,
+	'lambda-python': null,
+	'player-example': null,
+	'ai-improvements': null,
+	'discord-poster': null,
+	'cli-autocomplete': null,
+};
 
 test('All packages should have a repository field', () => {
 	const dirs = getAllPackages();
@@ -15,5 +80,26 @@ test('All packages should have a repository field', () => {
 				},
 			};
 		});
+	}
+});
+
+test('All packages should have a description field', () => {
+	const dirs = getAllPackages();
+
+	expect(dirs.length).toBeGreaterThan(0);
+
+	for (const {pkg, path} of dirs) {
+		const description = descriptions[pkg as Pkgs];
+		if (description === undefined) {
+			throw new Error(`No description for ${pkg}`);
+		}
+		if (description) {
+			updatePackageJson(path, (data) => {
+				return {
+					...data,
+					description,
+				};
+			});
+		}
 	}
 });
