70f8d58e1fcce33fba06e6b88ef60d19e2f7f87d
Jonny Burger <jonathanburger11@gmail.com>
2024-06-25 17:27:03 +0200

    move stats

--- packages/it-tests/src/monorepo/statistics-file.test.ts
+++ packages/it-tests/src/monorepo/statistics-file.test.ts
@@ -1,7 +1,7 @@
 import {expect, test} from 'bun:test';
 import {writeFileSync} from 'fs';
 import {join} from 'path';
-import {getAllPackages} from './get-all-packages';
+import {Pkgs, descriptions, getAllPackages} from './get-all-packages';
 
 test('All packages should have a README.md file', () => {
 	const dirs = getAllPackages();
@@ -14,11 +14,18 @@ test('All packages should have a README.md file', () => {
 		[
 			`# Download statistics`,
 			'Monthly downloads of Remotion packages',
-			dirs.map(({pkg}) => {
-				const name = pkg === 'core' ? 'remotion' : `@remotion/${pkg}`;
+			dirs
+				.map(({pkg}) => {
+					const name = pkg === 'core' ? 'remotion' : `@remotion/${pkg}`;
+					const description = descriptions[pkg as Pkgs];
+					if (!description) {
+						return null;
+					}
 
-				return `[![NPM Downloads](https://img.shields.io/npm/dm/${name}.svg?style=flat&color=black&label=${name})](https://npmcharts.com/compare/${name}?minimal=true)  `;
-			}),
+					return `[![NPM Downloads](https://img.shields.io/npm/dm/${name}.svg?style=flat&color=black&label=${name})](https://npmcharts.com/compare/${name}?minimal=true)  `;
+				})
+				.filter(Boolean)
+				.join('\n'),
 		]
 			.filter(Boolean)
 			.join('\n') + '\n';
