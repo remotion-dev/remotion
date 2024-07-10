4b4d495b89928a5b5a69205acee75b465f256d75
hunxjunedo <junaidhunain6@gmail.com>
2024-07-10 23:18:52 +0500

    fixed formatting

--- packages/cli/src/config/override-webpack.ts
+++ packages/cli/src/config/override-webpack.ts
@@ -4,7 +4,6 @@ export type WebpackOverrideFn = (
 	currentConfiguration: WebpackConfiguration,
 ) => WebpackConfiguration | Promise<WebpackConfiguration>;
 
-
 export const defaultOverrideFunction: WebpackOverrideFn = (config) => config;
 let overrideFn: WebpackOverrideFn = defaultOverrideFunction;
 
@@ -16,11 +15,12 @@ export const getWebpackOverrideFn = () => {
 let invocations = 0;
 
 export const overrideWebpackConfig = (fn: WebpackOverrideFn) => {
-	if(invocations > 0){
-		throw new Error('You specified the overrideWebpackConfig() multiple times which is incorrect, in such a case, only the function passed in the latest override will be considered. If you want to call more than one functions, pass a single function and then call other functions from inside. Read More: https://www.remotion.dev/docs/config#overridewebpackconfig   https://github.com/remotion-dev/remotion/issues/4063  \n')
-	
+	if (invocations > 0) {
+		throw new Error(
+			'You specified the overrideWebpackConfig() multiple times which is incorrect, in such a case, only the function passed in the latest override will be considered. If you want to call more than one functions, pass a single function and then call other functions from inside. Read More: https://www.remotion.dev/docs/config#overridewebpackconfig   https://github.com/remotion-dev/remotion/issues/4063  \n',
+		);
 	}
-	
-	invocations++
+
+	invocations++;
 	overrideFn = fn;
 };
