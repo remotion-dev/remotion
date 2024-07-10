4ce1705981c30a4019a3c0b39388a003f824824e
Jonny Burger <jonathanburger11@gmail.com>
2024-07-10 15:44:31 +0200

    add Nix to sample docker files

--- packages/dockerfiles/run.sh
+++ packages/dockerfiles/run.sh
@@ -6,3 +6,4 @@ cd ../dockerfiles
 docker build --file Dockerfile.al2023 . --output type=tar,dest=/dev/null --progress=plain
 docker build --file Dockerfile.ubuntu . --output type=tar,dest=/dev/null --progress=plain
 docker build --file Dockerfile.debian . --output type=tar,dest=/dev/null --progress=plain
+docker build --file Dockerfile.nix . --output type=tar,dest=/dev/null --progress=plain
