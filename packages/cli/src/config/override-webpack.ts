ffbf8fc39452f2db06b39e23282303ca3503f356
hunxjunedo <junaidhunain6@gmail.com>
2024-07-10 23:02:18 +0500

    fixed lint and removed console statement

--- packages/cli/src/config/override-webpack.ts
+++ packages/cli/src/config/override-webpack.ts
@@ -12,15 +12,15 @@ export const getWebpackOverrideFn = () => {
 	return overrideFn;
 };
 
-//to warn the user if overrideWebpackConfig is invoked more than once
+//	to warn the user if overrideWebpackConfig is invoked more than once
 let invocations = 0;
-let warned = false
 
 export const overrideWebpackConfig = (fn: WebpackOverrideFn) => {
-	if(invocations > 0 && !warned){
-		console.error('You specified the overrideWebpackConfig() multiple times which is incorrect, in such a case, only the function passed in the latest override will be considered. If you want to call more than one functions, pass a single function and then call other functions from inside. Read More: https://www.remotion.dev/docs/config#overridewebpackconfig   https://github.com/remotion-dev/remotion/issues/4063  \n')
-		warned = true
+	if(invocations > 0){
+		throw new Error('You specified the overrideWebpackConfig() multiple times which is incorrect, in such a case, only the function passed in the latest override will be considered. If you want to call more than one functions, pass a single function and then call other functions from inside. Read More: https://www.remotion.dev/docs/config#overridewebpackconfig   https://github.com/remotion-dev/remotion/issues/4063  \n')
+	
 	}
+	
 	invocations++
 	overrideFn = fn;
 };
