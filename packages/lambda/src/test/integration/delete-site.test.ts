ccbcea62ca7bfeefb66c398f6aecc93edb69e1cb
Jonny Burger <jonathanburger11@gmail.com>
2024-07-26 16:55:43 +0200

    delete

--- packages/lambda/src/test/integration/delete-site.test.ts
+++ packages/lambda/src/test/integration/delete-site.test.ts
@@ -1,10 +1,8 @@
 import {expect, test} from 'vitest';
-import {deleteSite} from '../../api/delete-site';
-import {deploySite} from '../../api/deploy-site';
-import {
-	getOrCreateBucket,
-	internalGetOrCreateBucket,
-} from '../../api/get-or-create-bucket';
+import {internalDeleteSite} from '../../api/delete-site';
+import {internalDeploySite} from '../../api/deploy-site';
+import {internalGetOrCreateBucket} from '../../api/get-or-create-bucket';
+import {randomHash} from '../../shared/random-hash';
 import {mockImplementation} from '../mock-implementation';
 
 test('Return 0 total size if site did not exist', async () => {
@@ -15,29 +13,41 @@ test('Return 0 total size if site did not exist', async () => {
 		enableFolderExpiry: null,
 	});
 	expect(
-		await deleteSite({
+		await internalDeleteSite({
 			bucketName,
 			region: 'eu-west-1',
 			siteName: 'non existent',
+			providerSpecifics: mockImplementation,
 		}),
 	).toEqual({totalSizeInBytes: 0});
 });
 test('Return more than 0 total size if site did not exist', async () => {
-	const {bucketName} = await getOrCreateBucket({
+	const {bucketName} = await internalGetOrCreateBucket({
 		region: 'eu-west-1',
+		providerSpecifics: mockImplementation,
+		customCredentials: null,
+		enableFolderExpiry: null,
 	});
-	const {siteName} = await deploySite({
+	const {siteName} = await internalDeploySite({
 		bucketName,
 		entryPoint: 'first',
 		region: 'eu-west-1',
 		gitSource: null,
+		providerSpecifics: mockImplementation,
+		indent: false,
+		logLevel: 'info',
+		options: {},
+		privacy: 'public',
+		throwIfSiteExists: false,
+		siteName: randomHash(),
 	});
 	expect(
 		(
-			await deleteSite({
+			await internalDeleteSite({
 				bucketName,
 				region: 'eu-west-1',
 				siteName,
+				providerSpecifics: mockImplementation,
 			})
 		).totalSizeInBytes,
 	).toBeGreaterThan(0);
