f014e83c12b0fcf7945329342979f04e453767c5
Jonny Burger <jonathanburger11@gmail.com>
2024-07-30 11:07:36 +0200

    Discard changes to packages/renderer/src/options/separate-audio.tsx

--- packages/renderer/src/options/separate-audio.tsx
+++ packages/renderer/src/options/separate-audio.tsx
@@ -6,14 +6,8 @@ const cliFlag = 'separate-audio-to';
 
 export const separateAudioOption = {
 	cliFlag,
-	description: () => (
-		<>
-			If set, the audio will not be included in the main output but rendered as
-			a separate file at the location you pass. It is recommended to use an
-			absolute path. If a relative path is passed, it is relative to the{' '}
-			<a href="/docs/terminology/remotion-root">Remotion Root</a>.
-		</>
-	),
+	description: () =>
+		`If set, the audio will not be included in the main output but rendered as a separate file at the location you pass. It is recommended to use an absolute path. If a relative path is passed, it is relative to the Remotion Root.`,
 	docLink: 'https://remotion.dev/docs/renderer/render-media',
 	getValue: ({commandLine}) => {
 		if (commandLine[cliFlag]) {
