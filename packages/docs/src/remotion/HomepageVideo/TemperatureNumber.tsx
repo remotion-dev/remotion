08c359fdb4114be288b03c8ea8dec420d5c4c0b1
Jonny Burger <jonathanburger11@gmail.com>
2024-08-04 14:06:35 +0200

    Update TemperatureNumber.tsx

--- packages/docs/src/remotion/HomepageVideo/TemperatureNumber.tsx
+++ packages/docs/src/remotion/HomepageVideo/TemperatureNumber.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const Digit: React.FC<{
+	digit: string;
+}> = ({digit}) => {
+	return <span style={{display: 'inline-block'}}>{digit}</span>;
+};
+
 export const TemperatureNumber: React.FC<{
 	readonly theme: 'dark' | 'light';
 	readonly num: number;
 }> = ({theme, num}) => {
+	const digits = String(num).split('');
+	console.log(digits);
+
 	return (
 		<div
 			style={{
@@ -17,7 +26,11 @@ export const TemperatureNumber: React.FC<{
 				fontVariantNumeric: 'tabular-nums',
 			}}
 		>
-			{num}°C
+			{digits.map((digit, i) => (
+				// eslint-disable-next-line react/no-array-index-key
+				<Digit key={i} digit={digit} />
+			))}
+			°C
 		</div>
 	);
 };
