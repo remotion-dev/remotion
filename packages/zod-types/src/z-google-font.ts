983d6c6c76d5c4aada5aa185cc6dd19b4f4a1201
Eray Erdin <eraygezer.94@gmail.com>
2024-09-05 21:22:28 +0300

    add comment to explain zod enums

--- packages/zod-types/src/z-google-font.ts
+++ packages/zod-types/src/z-google-font.ts
@@ -4,4 +4,6 @@ import {z} from 'zod';
 const fonts = getAvailableFonts();
 const fontImportNames = fonts.map(({importName}) => importName);
 
+// z.enum requires const arrays for compile time
+// below is a trick to set default to Roboto and add other fonts dynamically
 export const zGoogleFont = () => z.enum(['Roboto', ...fontImportNames]);
