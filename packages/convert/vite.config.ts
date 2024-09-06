64a8d9ede3389a0e0cf4aaef31d123eebe4f8022
Jonny Burger <jonathanburger11@gmail.com>
2024-09-06 21:55:36 +0200

    hmm

--- packages/convert/vite.config.ts
+++ packages/convert/vite.config.ts
@@ -1,18 +1,11 @@
 import {vitePlugin as remix} from '@remix-run/dev';
+import {installGlobals} from '@remix-run/node';
 import {vercelPreset} from '@vercel/remix/vite';
 import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+installGlobals();
+
 export default defineConfig({
-	plugins: [
-		remix({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-			},
-			presets: [vercelPreset()],
-		}),
-		tsconfigPaths(),
-	],
+	plugins: [remix({presets: [vercelPreset()]}), tsconfigPaths()],
 });
