aaa6b973fe07df77ae7761c9d0637bd673584066
Eray Erdin <eraygezer.94@gmail.com>
2024-09-06 13:26:25 +0300

    use font prop in example composition

--- packages/example/src/GoogleFontsPicker/index.tsx
+++ packages/example/src/GoogleFontsPicker/index.tsx
@@ -7,12 +7,18 @@ export const googleFontsPickerSchema = z.object({
 	font: zGoogleFont().optional(),
 });
 
-const GoogleFontsPicker: React.FC<
-	z.infer<typeof googleFontsPickerSchema>
-> = () => {
+const GoogleFontsPicker: React.FC<z.infer<typeof googleFontsPickerSchema>> = ({
+	font,
+}) => {
 	return (
 		<AbsoluteFill>
-			<h1>Text</h1>
+			<h1
+				style={{
+					fontFamily: font,
+				}}
+			>
+				Text
+			</h1>
 		</AbsoluteFill>
 	);
 };
