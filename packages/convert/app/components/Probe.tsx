cf31ec4c906fa206d5733b2f1fa5050e311ab223
Jonny Burger <jonathanburger11@gmail.com>
2024-09-07 14:52:40 +0200

    responsiveness

--- packages/convert/app/components/Probe.tsx
+++ packages/convert/app/components/Probe.tsx
@@ -8,6 +8,7 @@ import {parseMedia} from '@remotion/media-parser';
 import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {ContainerOverview} from './ContainerOverview';
 import {TrackSwitcher} from './TrackSwitcher';
+import {VideoTrackOverview} from './VideoTrackOverview';
 import {Button} from './ui/button';
 import {
 	Card,
@@ -17,6 +18,7 @@ import {
 	CardHeader,
 	CardTitle,
 } from './ui/card';
+import {ScrollArea} from './ui/scroll-area';
 import {Skeleton} from './ui/skeleton';
 
 export const Probe: React.FC<{
@@ -108,38 +110,46 @@ export const Probe: React.FC<{
 	}, [probeDetails, sortedTracks, trackDetails]);
 
 	return (
-		<Card className={probeDetails ? 'w-[800px]' : 'w-[350px]'}>
+		<Card
+			className={
+				(probeDetails ? 'w-[800px]' : 'w-[350px]') +
+				' h-5/6 max-h-[700px] flex flex-col max-w-[90vw]'
+			}
+		>
 			<CardHeader>
 				<CardTitle title={name ?? undefined}>
 					{name ? name : <Skeleton className="h-5 w-[220px] inline-block" />}
 				</CardTitle>
 				<CardDescription>From URL</CardDescription>
 			</CardHeader>
-			<CardContent>
-				{sortedTracks.length && probeDetails ? (
-					<>
-						<TrackSwitcher
-							selectedTrack={trackDetails}
-							onTrack={(track) => {
-								setTrackDetails(track);
-							}}
-							sortedTracks={sortedTracks}
-						/>
-						<div className="h-4" />
-					</>
-				) : null}
-				{selectedTrack === null ? (
-					<ContainerOverview
-						container="MP4"
-						dimensions={dimensions ?? null}
-						videoCodec={videoCodec ?? null}
-						size={size ?? null}
-						durationInSeconds={durationInSeconds}
-						audioCodec={audioCodec ?? null}
-						fps={fps}
+			{sortedTracks.length && probeDetails ? (
+				<div className="pl-6 pr-6">
+					<TrackSwitcher
+						selectedTrack={trackDetails}
+						onTrack={(track) => {
+							setTrackDetails(track);
+						}}
+						sortedTracks={sortedTracks}
 					/>
-				) : null}
-			</CardContent>
+				</div>
+			) : null}
+			<ScrollArea className="flex-1">
+				<CardContent className="flex flex-1 flex-col">
+					{selectedTrack === null ? (
+						<ContainerOverview
+							container="MP4"
+							dimensions={dimensions ?? null}
+							videoCodec={videoCodec ?? null}
+							size={size ?? null}
+							durationInSeconds={durationInSeconds}
+							audioCodec={audioCodec ?? null}
+							fps={fps}
+						/>
+					) : selectedTrack.type === 'video' ? (
+						<VideoTrackOverview track={selectedTrack} />
+					) : null}
+				</CardContent>
+			</ScrollArea>
 			<CardFooter className="flex justify-between">
 				<div className="flex-1" />
 				<Button disabled={!tracks} onClick={onClick} variant={'link'}>
