3bd06c2c6099f6c4a069b12c4850e42bea9ead1b
Jonny Burger <jonathanburger11@gmail.com>
2024-09-27 16:51:13 +0200

    add tailwind template

--- packages/create-video/src/templates.tsx
+++ packages/create-video/src/templates.tsx
@@ -97,7 +97,7 @@ export const FEATURED_TEMPLATES: Template[] = [
 		defaultBranch: 'main',
 		featuredOnHomePage: 'Next.js',
 		previewURL: null,
-		templateInMonorepo: null,
+		templateInMonorepo: 'template-next-app',
 		allowEnableTailwind: false,
 	},
 	{
