254257cacd0a8da8763bf431811d2304d80a2798
umungobungo <m.mcgillivray@outlook.com>
2024-09-30 14:19:44 +0800

    align colour of time with colour of seekbar

--- packages/docs/src/components/Demo/PlayerControls.tsx
+++ packages/docs/src/components/Demo/PlayerControls.tsx
@@ -1,56 +1,11 @@
 import type {PlayerRef} from '@remotion/player';
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './player.module.css';
 import {PlayerFullscreen} from './PlayerFullscreen';
 import {PlayerSeekBar} from './PlayerSeekBar';
 import {PlayerVolume} from './PlayerVolume';
 import {PlayPauseButton} from './PlayPauseButton';
-
-const formatTime = (timeInSeconds: number) => {
-	const minutes = Math.floor(timeInSeconds / 60);
-	const seconds = Math.floor(timeInSeconds - minutes * 60);
-	return `${String(minutes)}:${String(seconds).padStart(2, '0')}`;
-};
-
-const TimeDisplay: React.FC<{
-	durationInFrames: number;
-	fps: number;
-	playerRef: React.RefObject<PlayerRef>;
-}> = ({durationInFrames, fps, playerRef}) => {
-	const [time, setTime] = React.useState(0);
-
-	useEffect(() => {
-		const {current} = playerRef;
-		if (!current) {
-			return;
-		}
-
-		const onTimeUpdate = () => {
-			setTime(current.getCurrentFrame());
-		};
-
-		current.addEventListener('frameupdate', onTimeUpdate);
-
-		return () => {
-			current.removeEventListener('frameupdate', onTimeUpdate);
-		};
-	}, [playerRef]);
-
-	return (
-		<div
-			style={{
-				fontFamily: 'monospace',
-				fontSize: '14px',
-				display: 'flex',
-				gap: '8px',
-			}}
-		>
-			<span>{formatTime(time / fps)}</span>
-			<span style={{opacity: 0.6}}>/</span>
-			<span style={{opacity: 0.6}}>{formatTime(durationInFrames / fps)}</span>
-		</div>
-	);
-};
+import {TimeDisplay} from './TimeDisplay';
 
 export const PlayerControls: React.FC<{
 	playerRef: React.RefObject<PlayerRef>;
@@ -61,16 +16,14 @@ export const PlayerControls: React.FC<{
 		<div className={styles['controls-wrapper']}>
 			<div className={styles['start-controls']}>
 				<PlayPauseButton playerRef={playerRef} />
-				<div style={{flex: 1}}>
-					<PlayerSeekBar
-						durationInFrames={durationInFrames}
-						playerRef={playerRef}
-						inFrame={null}
-						outFrame={null}
-						onSeekEnd={() => undefined}
-						onSeekStart={() => undefined}
-					/>
-				</div>
+				<PlayerSeekBar
+					durationInFrames={durationInFrames}
+					playerRef={playerRef}
+					inFrame={null}
+					outFrame={null}
+					onSeekEnd={() => undefined}
+					onSeekStart={() => undefined}
+				/>
 				<TimeDisplay
 					playerRef={playerRef}
 					durationInFrames={durationInFrames}
