1caad1c78046ecd2e5f6acb90dccb98512062548
Jonny Burger <jonathanburger11@gmail.com>
2024-10-04 09:38:54 +0200

    Add `@remotion/captions` as installable package

--- packages/studio/src/components/InstallPackage.tsx
+++ packages/studio/src/components/InstallPackage.tsx
@@ -77,6 +77,7 @@ export const InstallPackageModal: React.FC<{
 		'@remotion/three': false,
 		'@remotion/transitions': false,
 		'@remotion/zod-types': false,
+		"@remotion/captions": false
 	});
 	const {previewServerState: ctx} = useContext(StudioServerConnectionCtx);
 
