126a218a3dac6e53d38645068a214ee5d8d348a5
Jonny Burger <jonathanburger11@gmail.com>
2024-10-31 14:09:31 +0100

    Update deploy-homepage-video.ts

--- packages/docs/deploy-homepage-video.ts
+++ packages/docs/deploy-homepage-video.ts
@@ -10,6 +10,10 @@ const {bucketName} = await getOrCreateBucket({
 	region,
 });
 
+if (!process.env.REMOTION_AWS_ACCESS_KEY_ID?.endsWith('KQHJ')) {
+	throw new Error('Please fill in your AWS credentials in .env');
+}
+
 // @ts-expect-error
 const {serveUrl} = await deploySite({
 	siteName: 'remotion-homepage',
