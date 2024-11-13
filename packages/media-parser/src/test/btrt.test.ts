fd825e50d4da2182a504146b5fa20149dd6c016a
Jonny Burger <jonathanburger11@gmail.com>
2024-11-13 15:49:09 +0100

    btrt box

---
+++ packages/media-parser/src/test/btrt.test.ts
@@ -0,0 +1,13 @@
+import {expect, test} from 'bun:test';
+import {createBtrt} from '../create/iso-base-media/create-btrt';
+
+const reference = new Uint8Array([
+	0x00, 0x00, 0x00, 0x14, 0x62, 0x74, 0x72, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x06, 0xae, 0x73, 0x00, 0x06, 0xae, 0x73,
+]);
+
+test('btrt', () => {
+	expect(createBtrt({maxBitrate: 437875, avgBitrate: 437875})).toEqual(
+		reference,
+	);
+});
