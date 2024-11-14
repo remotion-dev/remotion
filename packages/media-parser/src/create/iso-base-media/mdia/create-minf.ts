895551ba1613a26d13c6a79b1f6a91ed1dbc6234
Jonny Burger <jonathanburger11@gmail.com>
2024-11-14 10:27:52 +0100

    minf atom

---
+++ packages/media-parser/src/create/iso-base-media/mdia/create-minf.ts
@@ -0,0 +1,12 @@
+import {combineUint8Arrays} from '../../../boxes/webm/make-header';
+import {addSize} from '../primitives';
+
+export const createMinf = ({
+	vmhdAtom,
+	stblAtom,
+}: {
+	vmhdAtom: Uint8Array;
+	stblAtom: Uint8Array;
+}) => {
+	return addSize(combineUint8Arrays([vmhdAtom, stblAtom]));
+};
