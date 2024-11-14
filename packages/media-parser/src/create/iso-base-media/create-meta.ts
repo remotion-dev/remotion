e77dd712b10ad58507de188e112edf8b35ee809e
Jonny Burger <jonathanburger11@gmail.com>
2024-11-14 11:22:00 +0100

    hdlr

---
+++ packages/media-parser/src/create/iso-base-media/create-meta.ts
@@ -0,0 +1,19 @@
+import {combineUint8Arrays} from '../../boxes/webm/make-header';
+import {addSize, stringsToUint8Array} from './primitives';
+
+export const createMeta = (hdlr: Uint8Array, ilst: Uint8Array) => {
+	return addSize(
+		combineUint8Arrays([
+			// type
+			stringsToUint8Array('meta'),
+			// version
+			new Uint8Array([0]),
+			// flags
+			new Uint8Array([0, 0, 0]),
+			// hdlr
+			hdlr,
+			// ilst
+			ilst,
+		]),
+	);
+};
