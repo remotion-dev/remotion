69fd4fb1b84d749b40aacd9edce07399e80462a9
Jonny Burger <jonathanburger11@gmail.com>
2024-11-19 17:44:51 +0100

    Update format-seconds.ts

--- packages/convert/app/lib/format-seconds.ts
+++ packages/convert/app/lib/format-seconds.ts
@@ -1,6 +1,6 @@
 export const formatSeconds = (seconds: number) => {
 	const minutes = Math.floor(seconds / 60);
-	const secondsLeft = seconds % 60;
+	const secondsLeft = Math.round(seconds % 60);
 
-	return `${minutes}:${Math.round(secondsLeft) < 10 ? '0' : ''}${Math.round(secondsLeft)} min`;
+	return `${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft} min`;
 };
