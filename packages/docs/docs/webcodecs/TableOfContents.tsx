8d91e2642f1e8f478c092df4307360496692e172
Jonny Burger <jonathanburger11@gmail.com>
2024-11-22 15:50:48 +0100

    Update TableOfContents.tsx

--- packages/docs/docs/webcodecs/TableOfContents.tsx
+++ packages/docs/docs/webcodecs/TableOfContents.tsx
@@ -58,11 +58,11 @@ export const TableOfContents: React.FC = () => {
 					<strong>{'defaultOnVideoTrackHandler()'}</strong>
 					<div>The default track transformation function for video tracks.</div>
 				</TOCItem>
-				<TOCItem link="/docs/webcodecs/get-avaiable-audio-codecs">
+				<TOCItem link="/docs/webcodecs/get-available-audio-codecs">
 					<strong>{'getAvailableAudioCodecs()'}</strong>
 					<div>Get the audio codecs that can fit in a container.</div>
 				</TOCItem>
-				<TOCItem link="/docs/webcodecs/get-avaiable-video-codecs">
+				<TOCItem link="/docs/webcodecs/get-available-video-codecs">
 					<strong>{'getAvailableVideoCodecs()'}</strong>
 					<div>Get the video codecs that can fit in a container.</div>
 				</TOCItem>
