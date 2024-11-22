b5c084171976351f334b5cc64f51267abdb63456
Jonny Burger <jonathanburger11@gmail.com>
2024-11-22 18:01:19 +0100

    render progress

--- packages/it-tests/src/monorepo/ruby-package.test.ts
+++ packages/it-tests/src/monorepo/ruby-package.test.ts
@@ -1,5 +1,33 @@
+import {LambdaInternals} from '@remotion/lambda';
 import {expect, test} from 'bun:test';
+import {execSync} from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import {VERSION} from 'remotion';
 
+const rubySdk = path.join(__dirname, '..', '..', '..', 'lambda-ruby');
 test('Set the right version for Ruby', () => {
-	expect(true).toBe(true);
+	const versionPath = path.join(rubySdk, 'lib', 'version.rb');
+
+	fs.writeFileSync(versionPath, `VERSION = "${VERSION}"`);
+});
+
+test('Ruby package', () => {
+	const output = execSync('ruby lib/sdk_spec.rb', {
+		cwd: rubySdk,
+	}).toString();
+	const nativeVersion = LambdaInternals.getRenderProgressPayload({
+		region: 'us-east-1',
+		functionName: 'remotion-render',
+		bucketName: 'remotion-render',
+		renderId: 'abcdef',
+		logLevel: 'info',
+		s3OutputProvider: {
+			endpoint: 'https://s3.us-east-1.amazonaws.com',
+			accessKeyId: 'accessKeyId',
+			secretAccessKey: 'secretAccessKey',
+			region: 'us-east-1',
+		},
+	});
+	expect(JSON.parse(output)).toEqual(nativeVersion);
 });
--- packages/lambda-ruby/lib/sdk_spec.rb
+++ packages/lambda-ruby/lib/sdk_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'sdk'
+require_relative 's3_output_provider'
 require 'json'
 
 # Create a test instance of the RemotionLambda::Client
@@ -9,11 +10,20 @@ client = RemotionLambda::Client.new(
 )
 
 # Sample data
-render_id = "test-render-123"
-bucket_name = "my-test-bucket"
+render_id = "abcdef"
+bucket_name = "remotion-render"
 
 # Call get_render_progress_payload with sample data
-payload = client.get_render_progress_payload(render_id, bucket_name)
+payload = client.get_render_progress_payload(
+  render_id: render_id,
+  bucket_name: bucket_name,
+  s3_output_provider: RemotionLambda::S3OutputProvider.new(
+    access_key_id: 'accessKeyId',
+    endpoint: 'https://s3.us-east-1.amazonaws.com',
+    region: 'us-east-1',
+    secret_access_key: 'secretAccessKey'
+  )
+)
 
 # Print as JSON
 puts JSON.generate(payload)
