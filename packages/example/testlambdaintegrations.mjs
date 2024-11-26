4cbec3598266a92cd32f6c5124ba4cc8f89a5d06
Jonny Burger <jonathanburger11@gmail.com>
2024-11-26 16:02:28 +0100

    update lock

--- packages/example/testlambdaintegrations.mjs
+++ packages/example/testlambdaintegrations.mjs
@@ -8,6 +8,57 @@ const functionName = execSync(`pnpm exec remotion lambda functions ls -q`)
 	.trim()
 	.split(' ')[0];
 
+console.log('=== Golang ===');
+execSync(`go run main.go`, {
+	env: {
+		// eslint-disable-next-line no-undef
+		...process.env,
+		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_FUNCTION_NAME: functionName,
+		REMOTION_APP_SERVE_URL: 'testbed-v6',
+	},
+	cwd: '../lambda-go-example',
+	stdio: 'inherit',
+});
+console.log('=== PHP ===');
+execSync(`php src/render.php`, {
+	env: {
+		// eslint-disable-next-line no-undef
+		...process.env,
+		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_FUNCTION_NAME: functionName,
+		REMOTION_APP_SERVE_URL: 'testbed-v6',
+	},
+	cwd: '../lambda-php-example',
+	stdio: 'inherit',
+});
+
+console.log('=== Python(render media) ===');
+execSync(`python testclient_render_media.py`, {
+	env: {
+		// eslint-disable-next-line no-undef
+		...process.env,
+		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_FUNCTION_NAME: functionName,
+		REMOTION_APP_SERVE_URL: 'testbed-v6',
+	},
+	cwd: '../lambda-python',
+	stdio: 'inherit',
+});
+
+console.log('=== Python(render still) ===');
+execSync(`python testclient_render_still.py`, {
+	env: {
+		// eslint-disable-next-line no-undef
+		...process.env,
+		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_FUNCTION_NAME: functionName,
+		REMOTION_APP_SERVE_URL: 'testbed-v6',
+	},
+	cwd: '../lambda-python',
+	stdio: 'inherit',
+});
+
 console.log('=== Ruby (Still) ===');
 
 execSync(`ruby lib/test_render_spec_media.rb`, {
