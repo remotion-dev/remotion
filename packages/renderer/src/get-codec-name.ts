5d0cc6494f84cd5a857034b201821108d5244a2a
Jonny Burger <jonathanburger11@gmail.com>
2024-12-05 16:49:56 +0100

    `@remotion/renderer`: Support hardware accelerated encoding for H.264, H.265 and ProRes

--- packages/renderer/src/get-codec-name.ts
+++ packages/renderer/src/get-codec-name.ts
@@ -1,27 +1,119 @@
 import type {Codec} from './codec';
+import type {LogLevel} from './log-level';
+import {Log} from './logger';
+import type {HardwareAccelerationOption} from './options/hardware-acceleration';
 
 export type CodecSettings = {
 	encoderName: string;
 	hardwareAccelerated: boolean;
 };
 
-export const getCodecName = (
-	codec: Codec,
-	preferredHwAcceleration: boolean,
-): CodecSettings | null => {
+export const hasSpecifiedUnsupportedHardwareQualifySettings = ({
+	encodingMaxRate,
+	encodingBufferSize,
+	crf,
+}: {
+	encodingMaxRate: string | null;
+	encodingBufferSize: string | null;
+	crf: unknown;
+}) => {
+	if (encodingBufferSize !== null) {
+		return 'encodingBufferSize';
+	}
+
+	if (encodingMaxRate !== null) {
+		return 'encodingMaxRate';
+	}
+
+	if (crf !== null && typeof crf !== 'undefined') {
+		return 'crf';
+	}
+
+	return null;
+};
+
+export const getCodecName = ({
+	codec,
+	encodingMaxRate,
+	encodingBufferSize,
+	crf,
+	hardwareAcceleration,
+	logLevel,
+	indent,
+}: {
+	codec: Codec;
+	hardwareAcceleration: HardwareAccelerationOption;
+	encodingMaxRate: string | null;
+	encodingBufferSize: string | null;
+	crf: unknown;
+	logLevel: LogLevel;
+	indent: boolean;
+}): CodecSettings | null => {
+	const preferredHwAcceleration =
+		hardwareAcceleration === 'required' ||
+		hardwareAcceleration === 'if-possible';
+
+	const unsupportedQualityOption =
+		hasSpecifiedUnsupportedHardwareQualifySettings({
+			encodingMaxRate,
+			encodingBufferSize,
+			crf,
+		});
+
+	if (hardwareAcceleration === 'required' && unsupportedQualityOption) {
+		throw new Error(
+			`When using hardware accelerated encoding, the option "${unsupportedQualityOption}" with hardware acceleration is not supported. Disable hardware accelerated encoding or use "if-possible" instead.`,
+		);
+	}
+
+	const warnAboutDisabledHardwareAcceleration = () => {
+		if (hardwareAcceleration === 'if-possible' && unsupportedQualityOption) {
+			Log.warn(
+				{indent, logLevel},
+				`${indent ? '' : '\n'}Hardware accelerated encoding disabled - "${unsupportedQualityOption}" option is not supported with hardware acceleration`,
+			);
+		}
+	};
+
 	if (codec === 'prores') {
-		if (preferredHwAcceleration && process.platform === 'darwin') {
+		if (
+			preferredHwAcceleration &&
+			process.platform === 'darwin' &&
+			!unsupportedQualityOption
+		) {
 			return {encoderName: 'prores_videotoolbox', hardwareAccelerated: true};
 		}
+
+		warnAboutDisabledHardwareAcceleration();
 
 		return {encoderName: 'prores_ks', hardwareAccelerated: false};
 	}
 
 	if (codec === 'h264') {
+		if (
+			preferredHwAcceleration &&
+			process.platform === 'darwin' &&
+			!unsupportedQualityOption
+		) {
+			return {encoderName: 'h264_videotoolbox', hardwareAccelerated: true};
+		}
+
+		warnAboutDisabledHardwareAcceleration();
+
 		return {encoderName: 'libx264', hardwareAccelerated: false};
 	}
 
 	if (codec === 'h265') {
+		if (
+			preferredHwAcceleration &&
+			process.platform === 'darwin' &&
+			!unsupportedQualityOption
+		) {
+			return {encoderName: 'hevc_videotoolbox', hardwareAccelerated: true};
+		}
+
+		warnAboutDisabledHardwareAcceleration();
+
 		return {encoderName: 'libx265', hardwareAccelerated: false};
 	}
 
