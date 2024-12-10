0a10bd672a1fef0147c40e5b4c0eefed8d29a055
Johnpaul <polymenjohn1@gmail.com>
2024-12-10 15:37:50 +0100

    set correct placement for service worker

--- packages/convert/app/entry.client.tsx
+++ packages/convert/app/entry.client.tsx
@@ -1,15 +1,12 @@
 import {RemixBrowser} from '@remix-run/react';
-import {hydrateRoot} from 'react-dom/client';
 import {startTransition} from 'react';
+import {hydrateRoot} from 'react-dom/client';
 
 const registerServiceWorker = () => {
 	if ('serviceWorker' in navigator) {
 		window.addEventListener('load', () => {
 			navigator.serviceWorker
-				.register('/convert/service-worker.js')
-				.then((registration) => {
-					console.log('SW registered:', registration);
-				})
+				.register('/convert-service-worker.js')
 				.catch((error) => {
 					console.log('SW registration failed:', error);
 				});
