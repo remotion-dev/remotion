379f121ecfb673ecebaf9b866998defc860d093b
Jonny Burger <jonathanburger11@gmail.com>
2024-12-12 14:16:30 +0100

    broaden service worker scope

--- packages/convert/app/entry.client.tsx
+++ packages/convert/app/entry.client.tsx
@@ -1,20 +1,22 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
-import { startTransition } from "react";
+import {RemixBrowser} from '@remix-run/react';
+import {startTransition} from 'react';
+import {hydrateRoot} from 'react-dom/client';
 
 const registerServiceWorker = () => {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/convert/service-worker.js")
-        .catch((error) => {
-          console.log("SW registration failed:", error);
-        });
-    });
-  }
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker
+				.register('/convert/service-worker.js', {
+					scope: '/convert', // "/convert", not "/convert/"
+				})
+				.catch((error) => {
+					console.log('SW registration failed:', error);
+				});
+		});
+	}
 };
 
 startTransition(() => {
-  hydrateRoot(document, <RemixBrowser />);
-  registerServiceWorker();
+	hydrateRoot(document, <RemixBrowser />);
+	registerServiceWorker();
 });
