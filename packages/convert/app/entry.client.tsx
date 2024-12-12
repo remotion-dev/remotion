8b3e8ca4909a282a141791ab2f0fa11fb73aeab6
Johnpaul <polymenjohn1@gmail.com>
2024-12-12 11:51:54 +0100

    fix entry.client.tsx

--- packages/convert/app/entry.client.tsx
+++ packages/convert/app/entry.client.tsx
@@ -1,21 +1,20 @@
-import {RemixBrowser} from '@remix-run/react';
-import React from 'react';
-import {startTransition} from 'react';
-import {hydrateRoot} from 'react-dom/client';
+import { RemixBrowser } from "@remix-run/react";
+import { hydrateRoot } from "react-dom/client";
+import { startTransition } from "react";
 
 const registerServiceWorker = () => {
-	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', () => {
-			navigator.serviceWorker
-				.register('/convert/service-worker.js')
-				.catch((error) => {
-					console.log('SW registration failed:', error);
-				});
-		});
-	}
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/convert/service-worker.js")
+        .catch((error) => {
+          console.log("SW registration failed:", error);
+        });
+    });
+  }
 };
 
 startTransition(() => {
-	hydrateRoot(document, <RemixBrowser />);
-	registerServiceWorker();
+  hydrateRoot(document, <RemixBrowser />);
+  registerServiceWorker();
 });
