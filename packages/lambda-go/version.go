8548c5cdb6693e94eafdd93fc27ac91c25b2b76f
Jonny Burger <jonathanburger11@gmail.com>
2024-12-13 15:53:19 +0100

    v4.0.240

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.239"
+const VERSION = "4.0.240"
--- packages/media-parser/src/version.ts
+++ packages/media-parser/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.239';
+export const VERSION = '4.0.240';
