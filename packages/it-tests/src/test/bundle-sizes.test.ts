595f5534a6ba3e78c82ebb1af9818b30413b6fe3
Jonny Burger <jonathanburger11@gmail.com>
2024-12-20 15:34:22 +0100

    Add test for bundle sizes

---
+++ packages/it-tests/src/test/bundle-sizes.test.ts
@@ -0,0 +1,23 @@
+import {expect, test} from 'bun:test';
+import {unlinkSync} from 'fs';
+import path from 'node:path';
+
+test('Bundle size of media parser', async () => {
+	const content = `import {parseMedia} from "@remotion/media-parser"; console.log(parseMedia);`;
+	const entryPoint = path.join(__dirname, 'media-parser-index.ts');
+	await Bun.write(entryPoint, content);
+
+	const result = await Bun.build({
+		entrypoints: [entryPoint],
+		minify: true,
+		root: __dirname,
+		format: 'esm',
+	});
+	unlinkSync(entryPoint);
+	if (!result.success) {
+		throw new Error('Build failed');
+	}
+
+	const [output] = result.outputs;
+	expect(output.size).toBeLessThan(200_000);
+});
