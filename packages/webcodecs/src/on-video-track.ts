24c6c54ce6736fb526001db1903ef4d18725a4d4
Jonny Burger <jonathanburger11@gmail.com>
2024-12-22 11:01:32 +0100

    fix in firefox

--- packages/webcodecs/src/on-video-track.ts
+++ packages/webcodecs/src/on-video-track.ts
@@ -83,7 +83,7 @@ export const makeVideoTrackHandler =
 
 		if (videoOperation.type === 'fail') {
 			throw new Error(
-				`Video track with ID ${track.trackId} could with {"type": "fail"}. This could mean that this video track could neither be copied to the output container or re-encoded. You have the option to drop the track instead of failing it: https://remotion.dev/docs/webcodecs/track-transformation`,
+				`Video track with ID ${track.trackId} resolved with {"type": "fail"}. This could mean that this video track could neither be copied to the output container or re-encoded. You have the option to drop the track instead of failing it: https://remotion.dev/docs/webcodecs/track-transformation`,
 			);
 		}
 
