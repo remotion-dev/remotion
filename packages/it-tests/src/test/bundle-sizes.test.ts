e3983b700a258bc42df91d27ac635a40462d5753
Jonny Burger <jonathanburger11@gmail.com>
2024-12-29 17:19:38 +0100

    add another test

--- packages/it-tests/src/test/bundle-sizes.test.ts
+++ packages/it-tests/src/test/bundle-sizes.test.ts
@@ -21,3 +21,23 @@ test('Bundle size of media parser', async () => {
 	const [output] = result.outputs;
 	expect(output.size).toBeLessThan(200_000);
 });
+
+test('Bundle size of webcodecs', async () => {
+	const content = `import {parseMedia} from "@remotion/media-parser"; console.log(parseMedia); import {convertMedia} from "@remotion/webcodecs"; console.log(convertMedia);`;
+	const entryPoint = path.join(__dirname, 'media-parser-index.ts');
+	await Bun.write(entryPoint, content);
+
+	const result = await Bun.build({
+		entrypoints: [entryPoint],
+		minify: true,
+		root: __dirname,
+		format: 'esm',
+	});
+	unlinkSync(entryPoint);
+	if (!result.success) {
+		throw new Error(result.logs.toString());
+	}
+
+	const [output] = result.outputs;
+	expect(output.size).toBeLessThan(200_000);
+});
