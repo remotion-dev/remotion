cb9942e3e032beed88daf396e81983d1b380658a
Jonny Burger <jonathanburger11@gmail.com>
2025-01-11 11:11:13 +0100

    scope tailwind

--- packages/promo-pages/tailwind.config.ts
+++ packages/promo-pages/tailwind.config.ts
@@ -1,18 +1,30 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
+
+import {
+	isolateInsideOfContainer,
+	scopedPreflightStyles,
+} from 'tailwindcss-scoped-preflight';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [
+		scopedPreflightStyles({
+			isolationStrategy: isolateInsideOfContainer(
+				'.remotion-tailwind-landing',
+				{},
+			),
+		}),
+	],
 } satisfies Config;
