a8e58d0a3094be7a4c163f999e0f4462ca296a30
Jonny Burger <jonathanburger11@gmail.com>
2025-01-11 11:54:24 +0100

    shell

---
+++ packages/docs/src/pages/shell.tsx
@@ -0,0 +1,12 @@
+import Layout from '@theme/Layout';
+import React from 'react';
+
+const Shell: React.FC = () => {
+	return (
+		<Layout>
+			<div id="shell" />
+		</Layout>
+	);
+};
+
+export default Shell;
