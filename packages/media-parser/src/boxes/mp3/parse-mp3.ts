8005df9baa0b60b2421b450a66f0a89a1a709ee0
Jonny Burger <jonathanburger11@gmail.com>
2025-01-15 10:07:14 +0100

    parse ID3 v1

--- packages/media-parser/src/boxes/mp3/parse-mp3.ts
+++ packages/media-parser/src/boxes/mp3/parse-mp3.ts
@@ -2,6 +2,7 @@ import type {BufferIterator} from '../../buffer-iterator';
 import type {Mp3Structure, ParseResult} from '../../parse-result';
 import type {ParserState} from '../../state/parser-state';
 import {parseId3} from './id3';
+import {parseID3V1} from './id3-v1';
 import {parseMpegHeader} from './parse-mpeg-header';
 
 export const parseMp3 = async ({
@@ -35,6 +36,17 @@ export const parseMp3 = async ({
 	const bytes = iterator.getSlice(3);
 	returnToCheckpoint();
 
+	// ID3 v1
+	if (bytes[0] === 0x54 && bytes[1] === 0x41 && bytes[2] === 0x47) {
+		parseID3V1(iterator);
+		return {
+			status: 'incomplete',
+			continueParsing,
+			skipTo: null,
+		};
+	}
+
+	// ID3 v2 or v3
 	if (bytes[0] === 0x49 && bytes[1] === 0x44 && bytes[2] === 0x33) {
 		parseId3(iterator, structure);
 		return {
