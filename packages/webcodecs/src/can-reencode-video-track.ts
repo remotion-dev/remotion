2eb82b2a55e0657c9a00420c9e7f5cab709f54d3
Jonny Burger <jonathanburger11@gmail.com>
2025-01-17 16:09:17 +0100

    `@remotion/webcodecs`: Fix calculation whether a video can be re-encoded

--- packages/webcodecs/src/can-reencode-video-track.ts
+++ packages/webcodecs/src/can-reencode-video-track.ts
@@ -1,19 +1,32 @@
 import type {VideoTrack} from '@remotion/media-parser';
 import type {ConvertMediaVideoCodec} from './get-available-video-codecs';
+import type {ResizeOperation} from './resizing/mode';
+import {calculateNewDimensionsFromRotateAndScale} from './rotation';
 import {getVideoDecoderConfigWithHardwareAcceleration} from './video-decoder-config';
 import {getVideoEncoderConfig} from './video-encoder-config';
 
 export const canReencodeVideoTrack = async ({
 	videoCodec,
 	track,
+	resizeOperation,
+	rotate,
 }: {
 	videoCodec: ConvertMediaVideoCodec;
 	track: VideoTrack;
+	resizeOperation: ResizeOperation | null;
+	rotate: number | null;
 }) => {
+	const {height, width} = calculateNewDimensionsFromRotateAndScale({
+		height: track.displayAspectHeight,
+		resizeOperation,
+		rotation: rotate ?? 0,
+		videoCodec,
+		width: track.displayAspectWidth,
+	});
 	const videoEncoderConfig = await getVideoEncoderConfig({
 		codec: videoCodec,
-		height: track.displayAspectHeight,
-		width: track.displayAspectWidth,
+		height,
+		width,
 		fps: track.fps,
 	});
 	const videoDecoderConfig =
