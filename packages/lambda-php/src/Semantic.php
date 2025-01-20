d14f77d41bf3acd4dd8d4c6272b13f65ae4c45a7
Jonny Burger <jonathanburger11@gmail.com>
2025-01-20 18:12:53 +0100

    v4.0.251

--- packages/lambda-php/src/Semantic.php
+++ packages/lambda-php/src/Semantic.php
@@ -4,5 +4,5 @@ namespace Remotion\LambdaPhp;
 
 class Semantic
 {
-    public const VERSION = "4.0.250";
+    public const VERSION = "4.0.251";
 }
--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.250"
+VERSION = "4.0.251"
