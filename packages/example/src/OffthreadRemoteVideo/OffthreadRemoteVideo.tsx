3ab3d23031f1c3436373fcb8a43801e77e7c51eb
Jonny Burger <jonathanburger11@gmail.com>
2025-01-21 15:09:33 +0100

    Update OffthreadRemoteVideo.tsx

--- packages/example/src/OffthreadRemoteVideo/OffthreadRemoteVideo.tsx
+++ packages/example/src/OffthreadRemoteVideo/OffthreadRemoteVideo.tsx
@@ -1,4 +1,4 @@
-import {getVideoMetadata} from '@remotion/media-utils';
+import {parseMedia} from '@remotion/media-parser';
 import {StudioInternals} from '@remotion/studio';
 import {
 	AbsoluteFill,
@@ -14,13 +14,22 @@ const src = staticFile('bigbuckbunny.mp4');
 export const calculateMetadataFn: CalculateMetadataFunction<
 	Record<string, unknown>
 > = async () => {
-	const {durationInSeconds, width, height} = await getVideoMetadata(src);
+	const {slowDurationInSeconds, dimensions} = await parseMedia({
+		src,
+		fields: {
+			dimensions: true,
+			slowDurationInSeconds: true,
+		},
+	});
+	if (dimensions === null) {
+		throw new Error('No video track');
+	}
 
 	return {
-		durationInFrames: Math.round(durationInSeconds * fps),
+		durationInFrames: Math.round(slowDurationInSeconds * fps),
 		fps,
-		width: Math.floor(width / 2) * 2,
-		height: Math.floor(height / 2) * 2,
+		width: Math.floor(dimensions.width / 2) * 2,
+		height: Math.floor(dimensions.height / 2) * 2,
 	};
 };
 
