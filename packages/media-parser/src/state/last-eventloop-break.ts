c62eaf6e72d513d842be649e9f1f488f3a5734af
Jonny Burger <jonathanburger11@gmail.com>
2025-01-22 10:18:13 +0100

    Update last-eventloop-break.ts

--- packages/media-parser/src/state/last-eventloop-break.ts
+++ packages/media-parser/src/state/last-eventloop-break.ts
@@ -7,7 +7,7 @@ export const eventLoopState = (logLevel: LogLevel) => {
 	const eventLoopBreakIfNeeded = async () => {
 		if (Date.now() - lastEventLoopBreak > 2_000) {
 			await new Promise<void>((resolve) => {
-				setTimeout(() => resolve(), 10);
+				setTimeout(() => resolve(), 50);
 			});
 			Log.verbose(logLevel, '10ms event loop break');
 			lastEventLoopBreak = Date.now();
