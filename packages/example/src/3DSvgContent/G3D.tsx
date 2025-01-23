400d51614636be49a7ff94dcd90c373caf1c1da2
Jonny Burger <jonathanburger11@gmail.com>
2025-01-23 17:30:38 +0100

    svg content

--- packages/example/src/3DSvgContent/G3D.tsx
+++ packages/example/src/3DSvgContent/G3D.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {getBoundingBox, scalePath, translatePath} from '@remotion/paths';
+import React, {useMemo} from 'react';
 import {SvgExtrusion} from '../3DContext/Extrusion';
 import {FrontFaceG} from '../3DContext/FrontFaceG';
 import {PathProvider} from '../3DContext/path-context';
@@ -8,11 +9,28 @@ export const G3D: React.FC<
 		readonly d: string;
 		readonly children: React.ReactNode;
 		readonly depth: number;
+		readonly strokeWidth?: number;
 	} & React.SVGAttributes<SVGGElement>
-> = ({d, children, depth}) => {
+> = ({d, children, depth, strokeWidth}) => {
+	const scaledPath = useMemo(() => {
+		if (!strokeWidth) {
+			return d;
+		}
+
+		const sizeOfPath = getBoundingBox(d);
+		const scaleX = (sizeOfPath.width + (strokeWidth ?? 0)) / sizeOfPath.width;
+		const scaleY = (sizeOfPath.height + (strokeWidth ?? 0)) / sizeOfPath.height;
+
+		return translatePath(
+			scalePath(d, scaleX, scaleY),
+			-(strokeWidth ?? 0) / 2,
+			-(strokeWidth ?? 0) / 2,
+		);
+	}, [d, strokeWidth]);
+
 	return (
-		<PathProvider d={d}>
-			<SvgExtrusion depth={depth} />
+		<PathProvider d={scaledPath}>
+			{depth > 0 ? <SvgExtrusion depth={depth} /> : null}
 			<FrontFaceG depth={depth}>{children}</FrontFaceG>
 		</PathProvider>
 	);
--- packages/example/src/3DSvgContent/SpinEffect.tsx
+++ packages/example/src/3DSvgContent/SpinEffect.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import {useCurrentFrame} from 'remotion';
-import {RotateX, RotateY} from '../3DContext/transformation-context';
+import {interpolate, spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {RotateX, RotateY, Scale} from '../3DContext/transformation-context';
 
 export const SpinEffect: React.FC<{
 	readonly children: React.ReactNode;
 }> = ({children}) => {
 	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
 
-	const x = frame / 10;
-	const y = 0;
+	const jumpIn = spring({
+		fps,
+		frame,
+		config: {
+			damping: 200,
+		},
+		durationInFrames: 40,
+	});
+
+	const rotateDown = spring({
+		fps,
+		frame,
+		config: {
+			damping: 200,
+		},
+		delay: 15,
+		durationInFrames: 40,
+	});
+
+	const y = interpolate(jumpIn, [0, 1], [Math.PI * 2, 0]);
+	const x = interpolate(rotateDown, [0, 1], [-0.3, 0]);
 
 	return (
-		<RotateX radians={x}>
-			<RotateY radians={y}>{children}</RotateY>
-		</RotateX>
+		<Scale factor={jumpIn}>
+			<RotateX radians={x}>
+				<RotateY radians={y}>{children}</RotateY>
+			</RotateX>
+		</Scale>
 	);
 };
