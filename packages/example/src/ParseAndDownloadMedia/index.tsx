62d2653e63790558e96d8c8f4a1a6cb7e26a14f2
Jonny Burger <jonathanburger11@gmail.com>
2025-01-26 11:02:35 +0100

    coool

--- packages/example/src/ParseAndDownloadMedia/index.tsx
+++ packages/example/src/ParseAndDownloadMedia/index.tsx
@@ -1,72 +1,87 @@
 import React from 'react';
 import {
 	AbsoluteFill,
+	Easing,
 	interpolate,
+	Sequence,
 	spring,
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
 import {ExtrudeDiv, ThreeDiv} from '../3DContext/Div3D';
-import {RotateX, RotateY} from '../3DContext/transformation-context';
-
-const ENABLE_EXTRUDE = 1;
+import {RotateX, RotateY, Scale} from '../3DContext/transformation-context';
+import {Cover} from './Covert';
+import {FrontFace} from './FrontFace';
+import {TopFace} from './TopFace';
 
 export const ParseAndDownloadMedia: React.FC = () => {
 	const frame = useCurrentFrame();
-	const {fps, width, height} = useVideoConfig();
+	const {fps} = useVideoConfig();
+	const moveInDelay = 150;
 
-	const prog = spring({
+	const moveIn = spring({
 		fps,
 		frame,
-		durationInFrames: 100,
+		config: {
+			damping: 200,
+		},
+		durationInFrames: moveInDelay - 35,
+		delay: 80,
+	});
+
+	const rotateX =
+		interpolate(moveIn, [0, 1], [0, -10 / 100]) +
+		interpolate(frame, [80, 270], [0, 10 / 100], {
+			extrapolateLeft: 'clamp',
+		});
+	const rotateY = interpolate(moveIn, [0, 1], [Math.PI / 2, 10 / 20]);
+
+	const prog = interpolate(frame, [moveInDelay, 200 + moveInDelay], [0, 1], {
+		extrapolateLeft: 'clamp',
+		extrapolateRight: 'clamp',
+		easing: Easing.in(Easing.ease),
 	});
 
 	return (
 		<AbsoluteFill className="bg-white flex justify-center items-center">
-			<RotateY radians={frame / 100}>
-				<RotateX radians={frame / 20}>
-					<ThreeDiv
-						className="text-3xl mb-10"
-						style={{fontFamily: 'GT Planar', backgroundColor: 'red'}}
-					>
-						hi there
-					</ThreeDiv>
-					<ExtrudeDiv
-						depth={80}
-						width={800}
-						height={160}
-						cornerRadius={10}
-						topFace={
-							<div
-								style={{fontFamily: 'GT Planar'}}
-								className="w-full h-full flex items-center text-white pl-6"
-							>
-								Up top
-							</div>
-						}
-						bottomFace={
-							<div
-								style={{fontFamily: 'GT Planar'}}
-								className="w-full h-full flex items-center text-white pl-6 text-4xl"
-							>
-								Bottom top
-							</div>
-						}
-					>
+			<Scale
+				factor={interpolate(frame, [0, 20], [1.1, 1], {
+					extrapolateLeft: 'clamp',
+					extrapolateRight: 'clamp',
+					easing: Easing.out(Easing.ease),
+				})}
+			>
+				<RotateY radians={rotateX}>
+					<RotateX radians={rotateY}>
 						<div
-							className="h-full w-full overflow-hidden border-black border-solid border-2"
-							style={{borderRadius: 10, backgroundColor: 'white'}}
+							style={{
+								width: 800,
+								height: 800,
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}
 						>
-							<div
-								style={{
-									width: `${interpolate(prog, [0, 1], [0, 100])}%`,
-								}}
-								className="bg-[#0B84F3] h-full"
+							<ThreeDiv
+								className="text-3xl w-full h-full flex items-center justify-center absolute"
+								style={{fontFamily: 'GT Planar'}}
 							/>
+							<ExtrudeDiv
+								depth={90}
+								width={800}
+								height={160}
+								cornerRadius={40}
+								topFace={<TopFace delay={100} />}
+							>
+								<FrontFace delay={moveInDelay} prog={prog} />
+							</ExtrudeDiv>
 						</div>
-					</ExtrudeDiv>
-				</RotateX>
-			</RotateY>
+					</RotateX>
+				</RotateY>
+			</Scale>
+			<Sequence from={420}>
+				<Cover />
+			</Sequence>
 		</AbsoluteFill>
 	);
 };
