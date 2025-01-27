c75bed18e5f8f0aefd7b1c90df9a9ea1605d8f00
Jonny Burger <jonathanburger11@gmail.com>
2025-01-27 10:33:53 +0100

    Add `playsInline` to all videos in the docs

--- packages/docs/components/TransparentVideoDemo.tsx
+++ packages/docs/components/TransparentVideoDemo.tsx
@@ -1,23 +1,24 @@
-import React, { useCallback, useRef } from "react";
-import "./transparent-video.css";
+import React, {useCallback, useRef} from 'react';
+import './transparent-video.css';
 
 export const TransparentVideoDemo: React.FC = () => {
-  const ref = useRef<HTMLVideoElement>(null);
-  const onClick = useCallback(() => {
-    ref.current?.classList.toggle("transparent");
-  }, []);
-  return (
-    <div>
-      <video
-        ref={ref}
-        src="/img/transparent-video.webm"
-        controls
-        autoPlay
-        loop
-      />
-      <p className="tr-centered" onClick={onClick}>
-        <button type="button">Toggle transparency</button>
-      </p>
-    </div>
-  );
+	const ref = useRef<HTMLVideoElement>(null);
+	const onClick = useCallback(() => {
+		ref.current?.classList.toggle('transparent');
+	}, []);
+	return (
+		<div>
+			<video
+				ref={ref}
+				src="/img/transparent-video.webm"
+				controls
+				autoPlay
+				loop
+				playsInline
+			/>
+			<p className="tr-centered" onClick={onClick}>
+				<button type="button">Toggle transparency</button>
+			</p>
+		</div>
+	);
 };
