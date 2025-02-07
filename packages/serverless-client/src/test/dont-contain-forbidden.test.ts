9ec322f7016944cb207e734048b391310ccd26c1
Jonny Burger <jonathanburger11@gmail.com>
2025-02-07 18:10:37 +0100

    dont contain fprbidden

--- packages/serverless-client/src/test/dont-contain-forbidden.test.ts
+++ packages/serverless-client/src/test/dont-contain-forbidden.test.ts
@@ -4,7 +4,7 @@ import path from 'path';
 test('Should not contain forbidden imports', async () => {
 	const output = await Bun.build({
 		external: ['@remotion/renderer', 'react/jsx-runtime'],
-		entrypoints: [path.join(__filename, '..', '..', 'client.ts')],
+		entrypoints: [path.join(__filename, '..', '..', 'index.ts')],
 	});
 	expect(output.outputs.length).toBe(1);
 	const text = await output.outputs[0].text();
