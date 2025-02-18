e7b9dc6e0bf8ffef188a5441aef02ad534a21161
Jonny Burger <jonathanburger11@gmail.com>
2025-02-18 14:45:17 +0100

    add a test for pausability / resumability

---
+++ packages/media-parser/src/test/m3u8-pause-resume.test.ts
@@ -0,0 +1,36 @@
+import {expect, test} from 'bun:test';
+import {hasBeenAborted} from '../errors';
+import {mediaParserController} from '../media-parser-controller';
+import {parseMedia} from '../parse-media';
+
+test('should correctly pause and resume m3u8', async () => {
+	const controller = mediaParserController();
+	let samples = 0;
+
+	await parseMedia({
+		src: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+		controller,
+		acknowledgeRemotionLicense: true,
+		onVideoTrack: (track) => {
+			return async (sample) => {
+				samples++;
+				if (sample.timestamp === 21250000) {
+					controller.pause();
+					expect(samples).toBe(675);
+					await new Promise((r) => {
+						setTimeout(r, 100);
+					});
+					expect(samples).toBe(675);
+					controller.resume();
+				}
+
+				if (sample.timestamp === 21400000) {
+					controller.abort();
+				}
+			};
+		},
+	}).catch((e) => {
+		expect(hasBeenAborted(e)).toBe(true);
+	});
+	expect(samples).toBe(680);
+});
