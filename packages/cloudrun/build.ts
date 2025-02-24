40928b3f7352f91fb03d4d58b40fda8005453547
Jonny Burger <jonathanburger11@gmail.com>
2025-02-24 17:49:40 +0100

    build

--- packages/cloudrun/build.ts
+++ packages/cloudrun/build.ts
@@ -1,8 +1,5 @@
 import {bundleInstaller} from './src/admin/bundle-installer';
 import {bundleRenderLogic} from './src/admin/bundle-renderLogic';
 
-const permissions = Bun.file('src/shared/sa-permissions.json');
-await Bun.write('dist/shared/sa-permissions.json', permissions);
-
 await bundleRenderLogic();
 await bundleInstaller();
