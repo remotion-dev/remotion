3f5675353dedbbe845a0907b8f4b8cfa3f3bf380
Jonny Burger <jonathanburger11@gmail.com>
2025-02-27 18:16:32 +0100

    React Router template add a <title> tag

--- packages/template-react-router/app/root.tsx
+++ packages/template-react-router/app/root.tsx
@@ -7,9 +7,11 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-
 export const meta: MetaFunction = () => {
   return [
+    {
+      title: "Remotion Starter",
+    },
     { charset: "utf-8" },
     { name: "viewport", content: "width=device-width,initial-scale=1" },
     { property: "og:title", content: "Remotion + React Router" },
