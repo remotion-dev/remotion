deb21fb503c0775ffed59dd5475ce30d77836de0
Udit Kumar <uditkumarussa@gmail.com>
2025-03-02 14:15:48 +0530

    fix: update `whisperCppVersion` in `sub.mjs`+template docs: update documentation for `whisperCppVersion`+formatting: adjust code formatting

--- packages/it-tests/download-whisper.ts
+++ packages/it-tests/download-whisper.ts
@@ -1,32 +1,33 @@
 import {
-  downloadWhisperModel,
-  installWhisperCpp,
-  transcribe,
-} from "@remotion/install-whisper-cpp";
-import path from "path";
+	downloadWhisperModel,
+	installWhisperCpp,
+	transcribe,
+} from '@remotion/install-whisper-cpp';
+import path from 'path';
 
-const to = path.join(process.cwd(), "whisper.cpp");
+const to = path.join(process.cwd(), 'whisper.cpp');
 
 await installWhisperCpp({
-  to,
-  version: "1.5.5",
+	to,
+	version: '1.5.5',
 });
 
 await downloadWhisperModel({
-  model: "medium.en",
-  folder: to,
-  onProgress: (downloadedBytes, totalBytes) => {
-    const progress = downloadedBytes / totalBytes;
-  },
+	model: 'medium.en',
+	folder: to,
+	onProgress: (downloadedBytes, totalBytes) => {
+		const progress = downloadedBytes / totalBytes;
+	},
 });
 
 const transcription = await transcribe({
-  inputPath: path.join(process.cwd(), "totranscribe.wav"),
-  whisperPath: to,
-  model: "medium.en",
-  tokenLevelTimestamps: true,
-  printOutput: false,
-  onProgress: (progress) => {
-    console.log(progress);
-  },
+	inputPath: path.join(process.cwd(), 'totranscribe.wav'),
+	whisperPath: to,
+	whisperCppVersion: '1.5.5',
+	model: 'medium.en',
+	tokenLevelTimestamps: true,
+	printOutput: false,
+	onProgress: (progress) => {
+		console.log(progress);
+	},
 });
