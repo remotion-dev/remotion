0fad532a402298dc8994bff0254eac7a7b0ce72f
Jonny Burger <jonathanburger11@gmail.com>
2025-03-21 09:41:20 +0100

    `@remotion/media-utils`: Log status code if it is not 206 in getPartialWaveData)()

--- packages/media-utils/src/probe-wave-file.ts
+++ packages/media-utils/src/probe-wave-file.ts
@@ -55,7 +55,7 @@ export const probeWaveFile = async (src: string): Promise<WaveProbe> => {
 	});
 	if (response.status !== 206) {
 		throw new Error(
-			`Tried to read bytes 0-256 from ${src}, but the response status code was not 206. This means the server might not support returning a partial response.`,
+			`Tried to read bytes 0-256 from ${src}, but the response status code was ${response.status} (expected was 206). This means the server might not support returning a partial response.`,
 		);
 	}
 
