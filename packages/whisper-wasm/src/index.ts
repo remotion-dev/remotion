2a2f195cb3fc7f157f65e3f05f12a2acc4d6288a
Hunain Ahmed <junaidhunain6@gmail.com>
2025-03-25 17:23:49 +0500

    add types

--- packages/whisper-wasm/src/index.ts
+++ packages/whisper-wasm/src/index.ts
@@ -1,20 +1,39 @@
-export interface transcribe{
-	file: File,
-	onProgress?: (p:number) => void,
-	onTranscribeChunk? : (start:string, end: string, text:string) => void,
-	threads?: number
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export interface Transcribe {
+	file: File;
+	onProgress?: (p: number) => void;
+	onTranscribeChunk?: (start: string, end: string, text: string) => void;
+	threads?: number;
 }
 
-export const transcribe = (_args: transcribe) => {
+// Ensures transcribe() returns a Promise that resolves to a string
+export const transcribe = async (_args: Transcribe): Promise<string> => {
 	throw new Error('cjs not supported');
 };
 
-export interface downloadWhisperModel {
-	model: string,
-	onProgress?: (progress: number) => void
+export type WhisperModel =
+	| 'tiny'
+	| 'tiny.en'
+	| 'base'
+	| 'base.en'
+	| 'small'
+	| 'small.en'
+	| 'medium'
+	| 'medium.en'
+	| 'large-v1'
+	| 'large-v2'
+	| 'large-v3'
+	| 'large-v3-turbo';
+
+export interface DownloadWhisperModel {
+	model: WhisperModel;
+	onProgress?: (progress: number) => void;
 }
 
-export const downloadWhisperModel = async (_args:downloadWhisperModel) => {
-	throw new Error("cjs not supported")
-}
-
+// Ensures downloadWhisperModel() returns a Promise that resolves to a string
+export const downloadWhisperModel = async (
+	_args: DownloadWhisperModel,
+): Promise<string> => {
+	throw new Error('cjs not supported');
+};
