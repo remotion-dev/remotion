b16bc005dd96c6bc46cc2a65ee6436dc2cad6b29
Jonny Burger <jonathanburger11@gmail.com>
2025-04-01 08:46:01 +0200

    node v16 does not work

--- packages/it-tests/src/node-version/media-parser.mjs
+++ packages/it-tests/src/node-version/media-parser.mjs
@@ -4,15 +4,18 @@ import {nodeReader} from '@remotion/media-parser/node';
 import assert from 'node:assert';
 import {test} from 'node:test';
 
-test('parse media', async () => {
-	const result = await parseMedia({
-		src: exampleVideos.stretchedVp8,
-		fields: {
-			durationInSeconds: true,
-		},
-		acknowledgeRemotionLicense: true,
-		reader: nodeReader,
-	});
+const major = parseInt(process.version.split('.')[0].slice(1));
+if (major > 16) {
+	test('parse media', async () => {
+		const result = await parseMedia({
+			src: exampleVideos.stretchedVp8,
+			fields: {
+				durationInSeconds: true,
+			},
+			acknowledgeRemotionLicense: true,
+			reader: nodeReader,
+		});
 
-	assert(result.durationInSeconds === 12.043999999999999);
-});
+		assert(result.durationInSeconds === 12.043999999999999);
+	});
+}
