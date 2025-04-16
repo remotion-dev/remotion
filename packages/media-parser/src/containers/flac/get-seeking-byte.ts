d609320978a06ebcaeddd83e093c90500851c3e6
Jonny Burger <jonathanburger11@gmail.com>
2025-04-16 17:54:26 +0200

    Update get-seeking-byte.ts

--- packages/media-parser/src/containers/flac/get-seeking-byte.ts
+++ packages/media-parser/src/containers/flac/get-seeking-byte.ts
@@ -14,6 +14,9 @@ export const getSeekingByteForFlac = ({
 			continue;
 		}
 
+		// Everything is a keyframe in flac, so if this sample does not cover the time, it's not a good candidate.
+		// Let's go to the next one. Exception: If we already saw the last sample, we use it so we find can at least
+		// find the closest one.
 		if (
 			hint.timeInSeconds + hint.durationInSeconds < time &&
 			!seekingHints.lastSampleObserved
