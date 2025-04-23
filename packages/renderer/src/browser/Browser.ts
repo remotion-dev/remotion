2a7d8f818c789f056f35e1902eeb4940a3ee3c0a
Jonny Burger <jonathanburger11@gmail.com>
2025-04-23 10:29:48 +0200

    IT Tests: Investigate flaky tests

--- packages/renderer/src/browser/Browser.ts
+++ packages/renderer/src/browser/Browser.ts
@@ -81,6 +81,7 @@ export class HeadlessBrowser extends EventEmitter {
 	#defaultContext: BrowserContext;
 	#contexts: Map<string, BrowserContext>;
 	#targets: Map<string, Target>;
+	id: string;
 
 	runner: BrowserRunner;
 
@@ -100,6 +101,8 @@ export class HeadlessBrowser extends EventEmitter {
 		super();
 		this.#defaultViewport = defaultViewport;
 		this.connection = connection;
+
+		this.id = crypto.randomUUID();
 
 		this.#defaultContext = new BrowserContext(this);
 		this.#contexts = new Map();
