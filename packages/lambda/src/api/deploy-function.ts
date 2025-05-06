2b3215108f471cf779ddeb7c8aa0e611b1b980f9
Jonny Burger <jonathanburger11@gmail.com>
2025-05-06 18:13:02 +0200

    Revert "`@remotion/lambda`: Fix how .zip location is specified"
    This reverts commit 922c9e03c881d047cd819509a2fe72b996173097.

--- packages/lambda/src/api/deploy-function.ts
+++ packages/lambda/src/api/deploy-function.ts
@@ -14,6 +14,7 @@ import type {
 } from '@remotion/serverless';
 import {VERSION} from 'remotion/version';
 import {awsFullClientSpecifics} from '../functions/full-client-implementation';
+import {FUNCTION_ZIP_ARM64} from '../shared/function-zip-path';
 import {validateRuntimePreference} from '../shared/get-layers';
 import {validateCustomRoleArn} from '../shared/validate-custom-role-arn';
 import {validateCloudWatchRetentionPeriod} from '../shared/validate-retention-period';
@@ -86,12 +87,7 @@ export const internalDeployFunction = async <Provider extends CloudProvider>(
 	const created = await params.fullClientSpecifics.createFunction({
 		createCloudWatchLogGroup: params.createCloudWatchLogGroup,
 		region: params.region,
-		// Very delicate!
-		// this will output to
-		// - dist/esm/index.mjs
-		// - dist/api/deploy-function.js
-		// It only works because both are two levels deep
-		zipFile: '../../remotionlambda-arm64.zip',
+		zipFile: FUNCTION_ZIP_ARM64,
 		functionName,
 		accountId,
 		memorySizeInMb: params.memorySizeInMb,
