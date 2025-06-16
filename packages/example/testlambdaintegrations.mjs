6d9d98d84aeafe0cc5eae1cf2415526fdadc42ea
Jonny Burger <jonathanburger11@gmail.com>
2025-06-16 14:19:20 +0200

    nice

--- packages/example/testlambdaintegrations.mjs
+++ packages/example/testlambdaintegrations.mjs
@@ -10,12 +10,25 @@ const functionName = execSync(
 	.trim()
 	.split(' ')[0];
 
+console.log('=== Python(render media) ===');
+execSync(`python testclient_render_media.py`, {
+	env: {
+		// eslint-disable-next-line no-undef
+		...process.env,
+		REMOTION_APP_REGION: 'us-east-1',
+		REMOTION_APP_FUNCTION_NAME: functionName,
+		REMOTION_APP_SERVE_URL: 'testbed-v6',
+	},
+	cwd: '../lambda-python',
+	stdio: 'inherit',
+});
+
 console.log('=== Golang ===');
 execSync(`go run main.go`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
-		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_REGION: 'us-east-1',
 		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
@@ -31,7 +44,7 @@ execSync(`bundle exec ruby test_render_spec_still.rb`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
-		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_REGION: 'us-east-1',
 		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
@@ -44,7 +57,7 @@ execSync(`bundle exec ruby test_render_spec_media.rb`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
-		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_REGION: 'us-east-1',
 		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
@@ -57,24 +70,11 @@ execSync(`php src/render.php`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
-		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_REGION: 'us-east-1',
 		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
 	cwd: '../lambda-php-example',
-	stdio: 'inherit',
-});
-
-console.log('=== Python(render media) ===');
-execSync(`python testclient_render_media.py`, {
-	env: {
-		// eslint-disable-next-line no-undef
-		...process.env,
-		REMOTION_APP_REGION: 'eu-central-1',
-		REMOTION_APP_FUNCTION_NAME: functionName,
-		REMOTION_APP_SERVE_URL: 'testbed-v6',
-	},
-	cwd: '../lambda-python',
 	stdio: 'inherit',
 });
 
@@ -83,7 +83,7 @@ execSync(`python testclient_render_still.py`, {
 	env: {
 		// eslint-disable-next-line no-undef
 		...process.env,
-		REMOTION_APP_REGION: 'eu-central-1',
+		REMOTION_APP_REGION: 'us-east-1',
 		REMOTION_APP_FUNCTION_NAME: functionName,
 		REMOTION_APP_SERVE_URL: 'testbed-v6',
 	},
