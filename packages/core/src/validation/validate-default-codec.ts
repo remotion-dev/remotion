81c97366b9f4ddaa88e25445b3652bb7eeed25d2
JonnyBurger <jonathanburger11@gmail.com>
2025-07-24 11:38:37 +0200

    `@remotion/serverless`: Validate codec

--- packages/core/src/validation/validate-default-codec.ts
+++ packages/core/src/validation/validate-default-codec.ts
@@ -1,9 +1,10 @@
 import type {Codec, CodecOrUndefined} from '../codec';
 import {validCodecs} from '../codec';
 
-export function validateDefaultCodec(
+export function validateCodec(
 	defaultCodec: unknown,
 	location: string,
+	name: string,
 ): asserts defaultCodec is CodecOrUndefined {
 	if (typeof defaultCodec === 'undefined') {
 		return;
@@ -11,13 +12,13 @@ export function validateDefaultCodec(
 
 	if (typeof defaultCodec !== 'string') {
 		throw new TypeError(
-			`The "defaultCodec" prop ${location} must be a string, but you passed a value of type ${typeof defaultCodec}.`,
+			`The "${name}" prop ${location} must be a string, but you passed a value of type ${typeof defaultCodec}.`,
 		);
 	}
 
 	if (!validCodecs.includes(defaultCodec as Codec)) {
 		throw new Error(
-			`The "defaultCodec" prop ${location} must be one of ${validCodecs.join(
+			`The "${name}" prop ${location} must be one of ${validCodecs.join(
 				', ',
 			)}, but you passed ${defaultCodec}.`,
 		);
