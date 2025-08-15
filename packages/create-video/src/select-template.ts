c6931da513fa60b8c6d86cbfb9c4523fbd16fc0c
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2025-08-15 16:35:58 +0530

    feat: enhance template selection with detailed descriptions and hyperlinks

--- packages/create-video/src/select-template.ts
+++ packages/create-video/src/select-template.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import minimist from 'minimist';
+import {makeHyperlink} from './hyperlinks/make-link';
 import {selectAsync} from './prompts';
 import type {Template} from './templates';
 import {FEATURED_TEMPLATES} from './templates';
@@ -42,7 +43,15 @@ export const selectTemplate = async () => {
 					template.cliId === 'editor-starter'
 						? ' ' + chalk.yellow('(Paid)')
 						: ''
-				}`,
+				}${chalk.reset(
+					` ${chalk.gray(template.description.trim())} ${chalk.gray(
+						makeHyperlink({
+							text: '(?)',
+							url: `https://remotion.dev/templates/${template.cliId}`,
+							fallback: '',
+						}),
+					)}`,
+				)}`,
 			};
 		}),
 	})) as Template;
