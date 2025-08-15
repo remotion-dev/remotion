7641834d10eafc8132f51bc752863910eac0fb34
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2025-08-15 23:49:11 +0530

    style: format SVG elements in EditorIcon component for improved readability

--- packages/docs/src/components/icons/editor.tsx
+++ packages/docs/src/components/icons/editor.tsx
@@ -9,14 +9,46 @@ export const EditorIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
 			xmlns="http://www.w3.org/2000/svg"
 			{...props}
 		>
-			<rect x="4" y="4" width="52" height="52" rx="15" stroke="currentColor" strokeWidth="8"/>
-			<rect x="195" y="4" width="52" height="52" rx="15" stroke="currentColor" strokeWidth="8"/>
-			<rect x="4" y="195" width="52" height="52" rx="15" stroke="currentColor" strokeWidth="8"/>
-			<rect x="195" y="195" width="52" height="52" rx="15" stroke="currentColor" strokeWidth="8"/>
-			<path d="M55 222H196" stroke="currentColor" strokeWidth="8"/>
-			<path d="M30 60L30 191" stroke="currentColor" strokeWidth="8"/>
-			<path d="M221 60V191" stroke="currentColor" strokeWidth="8"/>
-			<path d="M55 30H196" stroke="currentColor" strokeWidth="8"/>
+			<rect
+				x="4"
+				y="4"
+				width="52"
+				height="52"
+				rx="15"
+				stroke="currentColor"
+				strokeWidth="8"
+			/>
+			<rect
+				x="195"
+				y="4"
+				width="52"
+				height="52"
+				rx="15"
+				stroke="currentColor"
+				strokeWidth="8"
+			/>
+			<rect
+				x="4"
+				y="195"
+				width="52"
+				height="52"
+				rx="15"
+				stroke="currentColor"
+				strokeWidth="8"
+			/>
+			<rect
+				x="195"
+				y="195"
+				width="52"
+				height="52"
+				rx="15"
+				stroke="currentColor"
+				strokeWidth="8"
+			/>
+			<path d="M55 222H196" stroke="currentColor" strokeWidth="8" />
+			<path d="M30 60L30 191" stroke="currentColor" strokeWidth="8" />
+			<path d="M221 60V191" stroke="currentColor" strokeWidth="8" />
+			<path d="M55 30H196" stroke="currentColor" strokeWidth="8" />
 		</svg>
 	);
 };
