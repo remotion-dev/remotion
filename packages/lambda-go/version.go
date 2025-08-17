984659c08c6c90d7146ba7b17a0b4c232ef35ea8
JonnyBurger <jonathanburger11@gmail.com>
2025-08-17 20:15:43 +0200

    v4.0.339

--- packages/lambda-go/version.go
+++ packages/lambda-go/version.go
@@ -1,3 +1,3 @@
 package lambda_go_sdk;
 
-const VERSION = "4.0.338"
+const VERSION = "4.0.339"
--- packages/media-parser/src/version.ts
+++ packages/media-parser/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.338';
+export const VERSION = '4.0.339';
