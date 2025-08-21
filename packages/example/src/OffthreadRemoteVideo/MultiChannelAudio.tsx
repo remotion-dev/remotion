cd801c6fde750da39c6f8719b2b88f4d73cb31c8
Jonny Burger <jonathanburger11@gmail.com>
2025-08-21 16:28:38 +0200

    create example channel

---
+++ packages/example/src/OffthreadRemoteVideo/MultiChannelAudio.tsx
@@ -0,0 +1,41 @@
+import {parseMedia} from '@remotion/media-parser';
+import {StudioInternals} from '@remotion/studio';
+import {Audio, CalculateMetadataFunction} from 'remotion';
+
+const fps = 30;
+const src = 'https://parser.media/multichannel-audio.mov';
+
+export const calculateMetadataFn: CalculateMetadataFunction<
+	Record<string, unknown>
+> = async () => {
+	const {slowDurationInSeconds, dimensions} = await parseMedia({
+		src,
+		acknowledgeRemotionLicense: true,
+		fields: {
+			slowDurationInSeconds: true,
+			dimensions: true,
+		},
+	});
+
+	return {
+		durationInFrames: Math.round(slowDurationInSeconds * fps),
+		fps,
+	};
+};
+
+const Component = () => {
+	return (
+		<>
+			<Audio src={src} />
+		</>
+	);
+};
+
+export const MultiChannelAudio = StudioInternals.createComposition({
+	component: Component,
+	id: 'MultiChannelAudio',
+	calculateMetadata: calculateMetadataFn,
+	fps,
+	width: 100,
+	height: 199,
+});
