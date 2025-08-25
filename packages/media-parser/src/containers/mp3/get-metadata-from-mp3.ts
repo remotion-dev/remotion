d38b612abd39e7a0ceef092d321dd6d6d78977f6
JonnyBurger <jonathanburger11@gmail.com>
2025-08-25 14:47:06 +0200

    correct fix

--- packages/media-parser/src/containers/mp3/get-metadata-from-mp3.ts
+++ packages/media-parser/src/containers/mp3/get-metadata-from-mp3.ts
@@ -3,9 +3,7 @@ import type {Mp3Structure} from '../../parse-result';
 
 export const getMetadataFromMp3 = (
 	mp3Structure: Mp3Structure,
-): MediaParserMetadataEntry[] => {
+): MediaParserMetadataEntry[] | null => {
 	const findHeader = mp3Structure.boxes.find((b) => b.type === 'id3-header');
-	// Not all MP3s file have this header.
-	// Internal link: https://discord.com/channels/809501355504959528/1001500302375125055/1408880907602890752
-	return findHeader ? findHeader.metatags : [];
+	return findHeader ? findHeader.metatags : null;
 };
