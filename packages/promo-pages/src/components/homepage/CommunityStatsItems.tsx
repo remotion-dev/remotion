63e9ccc09bc84a98632a958a020ee54f71cd33d6
MehmetAdemi <mehmet@remotion.dev>
2025-08-27 12:06:58 +0200

    update GitHub star count

--- packages/promo-pages/src/components/homepage/CommunityStatsItems.tsx
+++ packages/promo-pages/src/components/homepage/CommunityStatsItems.tsx
@@ -177,7 +177,7 @@ export const GitHubStars: React.FC = () => {
 					width="45px"
 				/>
 				<StatItemContent
-					content="23k"
+					content="24k"
 					width="80px"
 					fontSize="2.5rem"
 					fontWeight="bold"
