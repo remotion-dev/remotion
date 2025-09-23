0e180c200a635e0a0414da5f68e7246409d4eb0b
Jonny Burger <jonathanburger11@gmail.com>
2025-09-23 14:38:10 +0200

    parser.media -> remotion.media

--- packages/convert/app/lib/config.ts
+++ packages/convert/app/lib/config.ts
@@ -1,4 +1,4 @@
 export const TEST_FAST = false;
 export const SAMPLE_FILE_CONVERT =
 	'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
-export const SAMPLE_FILE_TRANSCRIBE = 'https://parser.media/audio.wav';
+export const SAMPLE_FILE_TRANSCRIBE = 'https://remotion.media/dialogue.wav';
