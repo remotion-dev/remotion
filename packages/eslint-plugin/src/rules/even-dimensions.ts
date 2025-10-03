13d72b4a4626853eda07bac712a6fd2fe7e20101
JonnyBurger <jonathanburger11@gmail.com>
2025-10-03 16:38:13 +0200

    add a test

--- packages/eslint-plugin/src/rules/even-dimensions.ts
+++ packages/eslint-plugin/src/rules/even-dimensions.ts
@@ -9,7 +9,7 @@ type Options = [];
 type MessageIds = 'EvenDimensions';
 
 const EvenDimensions =
-	"Videos rendered in H264/H265 codec do not support dimensions that are not divisible by 2. Make the number even to resolve this warning. Ignore this warning if you don't plan on rendering this video with a H264 or H265 codec.";
+	"Videos rendered in H264/H265 codec do not support dimensions that are not divisible by 2. Remotion will round down your dimensions to satisfy this constraint. Ignore this warning if you don't plan on rendering this video with a H264 or H265 codec.";
 
 export default createRule<Options, MessageIds>({
 	name: 'even-dimensions',
