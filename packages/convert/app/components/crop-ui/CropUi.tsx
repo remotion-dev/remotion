94a0846da5f9dfd185c9a3f6173e3154434bca0a
JonnyBurger <jonathanburger11@gmail.com>
2025-10-20 18:44:55 +0200

    uiuiuui

--- packages/convert/app/components/crop-ui/CropUi.tsx
+++ packages/convert/app/components/crop-ui/CropUi.tsx
@@ -1,6 +1,7 @@
 import type {MediaFox, PlayerStateData} from '@mediafox/core';
+import type {CropRectangle} from 'mediabunny';
 import type React from 'react';
-import {useEffect, useMemo, useState} from 'react';
+import {useEffect, useMemo, useRef, useState} from 'react';
 import {ResizeHandle} from './ResizeHandle';
 
 export const CropUI: React.FC<{
@@ -9,6 +10,17 @@ export const CropUI: React.FC<{
 	const [state, setState] = useState<PlayerStateData | null>(() =>
 		mediaFox.getState(),
 	);
+
+	const [unclampedRect, setUnclampedRect] = useState<CropRectangle>(() => {
+		return {
+			left: 0,
+			top: 0,
+			width: Infinity,
+			height: Infinity,
+		};
+	});
+
+	const ref = useRef<HTMLDivElement>(null);
 
 	const dimensions = useMemo(() => {
 		if (!state) {
@@ -46,18 +58,73 @@ export const CropUI: React.FC<{
 		return null;
 	}
 
+	const rect = (() => {
+		const width = Math.min(
+			(dimensions?.width ?? 0) - unclampedRect.left,
+			Number.isFinite(unclampedRect.width)
+				? unclampedRect.width
+				: (dimensions?.width ?? 0),
+		);
+		const height = Math.min(
+			(dimensions?.height ?? 0) - unclampedRect.top,
+			Number.isFinite(unclampedRect.height)
+				? unclampedRect.height
+				: (dimensions?.height ?? 0),
+		);
+
+		return {
+			left: unclampedRect.left,
+			top: unclampedRect.top,
+			width,
+			height,
+		};
+	})();
+
 	return (
 		<div
+			ref={ref}
 			className="absolute w-full"
 			style={{
 				aspectRatio: `${dimensions.width} / ${dimensions.height}`,
 			}}
 		>
-			<div className="border-brand absolute border-4 rounded-md w-full h-full" />
-			<ResizeHandle position="top-left" />
-			<ResizeHandle position="top-right" />
-			<ResizeHandle position="bottom-left" />
-			<ResizeHandle position="bottom-right" />
+			<div
+				className="border-brand absolute border-4 rounded-md "
+				style={{
+					width: (rect.width / dimensions.width) * 100 + '%',
+					height: (rect.height / dimensions.height) * 100 + '%',
+					left: (rect.left / dimensions.width) * 100 + '%',
+					top: (rect.top / dimensions.height) * 100 + '%',
+				}}
+			/>
+			<ResizeHandle
+				divRef={ref}
+				position="top-left"
+				dimensions={dimensions}
+				rect={rect}
+				updateRect={setUnclampedRect}
+			/>
+			<ResizeHandle
+				divRef={ref}
+				position="top-right"
+				dimensions={dimensions}
+				rect={rect}
+				updateRect={setUnclampedRect}
+			/>
+			<ResizeHandle
+				divRef={ref}
+				position="bottom-left"
+				dimensions={dimensions}
+				rect={rect}
+				updateRect={setUnclampedRect}
+			/>
+			<ResizeHandle
+				divRef={ref}
+				position="bottom-right"
+				dimensions={dimensions}
+				rect={rect}
+				updateRect={setUnclampedRect}
+			/>
 		</div>
 	);
 };
--- packages/convert/app/components/crop-ui/ResizeHandle.tsx
+++ packages/convert/app/components/crop-ui/ResizeHandle.tsx
@@ -1,4 +1,6 @@
-import {useMemo} from 'react';
+import type {CropRectangle} from 'mediabunny';
+import React, {useCallback, useMemo} from 'react';
+import type {Dimensions} from '~/lib/calculate-new-dimensions-from-dimensions';
 
 const baseStyle: React.CSSProperties = {
 	width: 12,
@@ -7,15 +9,23 @@ const baseStyle: React.CSSProperties = {
 
 type Position = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
 
-export const ResizeHandle: React.FC<{readonly position: Position}> = ({
-	position,
-}) => {
+const clamp = (val: number, min: number, max: number) => {
+	return Math.min(Math.max(val, min), max);
+};
+
+export const ResizeHandle: React.FC<{
+	readonly position: Position;
+	readonly divRef: React.RefObject<HTMLDivElement | null>;
+	readonly dimensions: Dimensions;
+	readonly rect: CropRectangle;
+	readonly updateRect: React.Dispatch<React.SetStateAction<CropRectangle>>;
+}> = ({position, divRef, dimensions, rect, updateRect}) => {
 	const style: React.CSSProperties = useMemo(() => {
 		if (position === 'top-left') {
 			return {
 				...baseStyle,
-				top: 0,
-				left: 0,
+				top: (rect.top / dimensions.height) * 100 + '%',
+				left: (rect.left / dimensions.width) * 100 + '%',
 				marginLeft: -3,
 				marginTop: -3,
 				cursor: 'nwse-resize',
@@ -25,8 +35,11 @@ export const ResizeHandle: React.FC<{readonly position: Position}> = ({
 		if (position === 'top-right') {
 			return {
 				...baseStyle,
-				top: 0,
-				right: 0,
+				top: (rect.top / dimensions.height) * 100 + '%',
+				right:
+					((dimensions.width - (rect.width + rect.left)) / dimensions.width) *
+						100 +
+					'%',
 				marginRight: -3,
 				marginTop: -3,
 				cursor: 'nesw-resize',
@@ -36,8 +49,11 @@ export const ResizeHandle: React.FC<{readonly position: Position}> = ({
 		if (position === 'bottom-left') {
 			return {
 				...baseStyle,
-				bottom: 0,
-				left: 0,
+				bottom:
+					((dimensions.height - (rect.height + rect.top)) / dimensions.height) *
+						100 +
+					'%',
+				left: (rect.left / dimensions.width) * 100 + '%',
 				marginLeft: -3,
 				marginBottom: -3,
 				cursor: 'nesw-resize',
@@ -47,8 +63,14 @@ export const ResizeHandle: React.FC<{readonly position: Position}> = ({
 		if (position === 'bottom-right') {
 			return {
 				...baseStyle,
-				bottom: 0,
-				right: 0,
+				bottom:
+					((dimensions.height - (rect.height + rect.top)) / dimensions.height) *
+						100 +
+					'%',
+				right:
+					((dimensions.width - (rect.width + rect.left)) / dimensions.width) *
+						100 +
+					'%',
 				marginRight: -3,
 				marginBottom: -3,
 				cursor: 'nwse-resize',
@@ -56,12 +78,112 @@ export const ResizeHandle: React.FC<{readonly position: Position}> = ({
 		}
 
 		throw new Error('Unknown position: ' + JSON.stringify(position));
-	}, [position]);
+	}, [position, rect, dimensions]);
+
+	const onPointerDown = useCallback(
+		(e: React.PointerEvent) => {
+			e.preventDefault();
+			e.stopPropagation();
+			if (e.button !== 0) {
+				return;
+			}
+
+			const move = (evt: PointerEvent) => {
+				evt.preventDefault();
+				evt.stopPropagation();
+
+				const boxOffset = divRef.current?.getBoundingClientRect()!;
+				const offsetX = evt.clientX - boxOffset.left;
+				const offsetY = evt.clientY - boxOffset.top;
+
+				const factor = boxOffset.width / dimensions.width;
+				const cropPixelsX = clamp(
+					Math.round(offsetX / factor),
+					0,
+					dimensions.width,
+				);
+				const cropPixelsY = clamp(
+					Math.round(offsetY / factor),
+					0,
+					dimensions.height,
+				);
+
+				if (position === 'top-left') {
+					updateRect((prev) => {
+						const diffX = cropPixelsX - prev.left;
+						const diffY = cropPixelsY - prev.top;
+						return {
+							...prev,
+							left: cropPixelsX,
+							top: cropPixelsY,
+							width: Math.max(2, prev.width - diffX),
+							height: Math.max(2, prev.height - diffY),
+						};
+					});
+				}
+
+				if (position === 'top-right') {
+					updateRect((prev) => {
+						const newHeight = prev.height - (cropPixelsY - prev.top);
+
+						return {
+							...prev,
+							width: Math.max(2, cropPixelsX - prev.left),
+							height: Math.max(2, newHeight),
+							top: cropPixelsY,
+						};
+					});
+				}
+
+				if (position === 'bottom-left') {
+					updateRect((prev) => {
+						const diffX = cropPixelsX - prev.left;
+
+						return {
+							...prev,
+							height: Math.max(2, cropPixelsY - prev.top),
+							left: cropPixelsX,
+							width: Math.max(2, prev.width - diffX),
+						};
+					});
+				}
+
+				if (position === 'bottom-right') {
+					updateRect((prev) => ({
+						...prev,
+						width: cropPixelsX - prev.left,
+						height: cropPixelsY - prev.top,
+					}));
+				}
+
+				throw new Error('Unknown position: ' + JSON.stringify(position));
+			};
+
+			const onPointerMove = (evt: PointerEvent) => {
+				evt.preventDefault();
+				evt.stopPropagation();
+				move(evt);
+			};
+
+			const onPointerUp = (evt: PointerEvent) => {
+				evt.preventDefault();
+				evt.stopPropagation();
+
+				window.removeEventListener('pointermove', onPointerMove);
+				window.removeEventListener('pointerup', onPointerUp);
+			};
+
+			window.addEventListener('pointermove', onPointerMove);
+			window.addEventListener('pointerup', onPointerUp);
+		},
+		[dimensions.height, dimensions.width, divRef, position, updateRect],
+	);
 
 	return (
 		<div
 			className="w-3 h-3 bg-white absolute rounded-full border-2 border-black"
 			style={style}
+			onPointerDown={onPointerDown}
 		/>
 	);
 };
