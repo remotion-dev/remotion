5f493b1e9ae84df8c5d8ecb2eaeb29cfa79a7e07
Igor Samokhovets <igor@remotion.dev>
2025-10-20 18:40:25 +0200

    copy page label fix

--- packages/docs/src/theme/DocBreadcrumbs/index.tsx
+++ packages/docs/src/theme/DocBreadcrumbs/index.tsx
@@ -198,7 +198,7 @@ export default function DocBreadcrumbsWrapper(props: Props): ReactNode {
 							d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z"
 						/>
 					</svg>
-					{copied ? 'Copied' : 'Ask AI'}
+					{copied ? 'Copied' : 'Copy page'}
 				</button>
 				<DropdownMenu.Root>
 					<DropdownMenu.Trigger asChild>
