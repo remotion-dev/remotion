157d679d1e3388ddea968fbabf7a5d62ef4ffe0b
JonnyBurger <jonathanburger11@gmail.com>
2025-10-21 09:37:24 +0200

    Update apply-crop.ts

--- packages/convert/app/lib/apply-crop.ts
+++ packages/convert/app/lib/apply-crop.ts
@@ -3,7 +3,7 @@ import type {Dimensions} from './calculate-new-dimensions-from-dimensions';
 
 export const applyCrop = (dimensions: Dimensions, cropRect: CropRectangle) => {
 	return {
-		width: Math.min(dimensions.width, cropRect.width - cropRect.left),
-		height: Math.min(dimensions.height, cropRect.height - cropRect.top),
+		width: Math.min(dimensions.width, cropRect.width),
+		height: Math.min(dimensions.height, cropRect.height),
 	};
 };
