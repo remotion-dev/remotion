8f587a95652122009be68e3924a765f14a341bd6
JonnyBurger <jonathanburger11@gmail.com>
2025-10-28 08:27:08 +0100

    Remove zip-lib dependency

--- packages/lambda/build.ts
+++ packages/lambda/build.ts
@@ -3,8 +3,7 @@ import {dir} from '@remotion/compositor-linux-arm64-gnu';
 import fs, {cpSync, readdirSync} from 'node:fs';
 import path from 'node:path';
 import {FUNCTION_ZIP_ARM64} from './src/shared/function-zip-path';
-
-import zl from 'zip-lib';
+import {$} from 'bun';
 
 const outdir = path.join(__dirname, `build-render`);
 fs.mkdirSync(outdir, {
@@ -57,7 +56,7 @@ fs.cpSync(
 	`${outdir}/mappings.wasm`,
 );
 
-await zl.archiveFolder(outdir, FUNCTION_ZIP_ARM64);
+await $`cd ${outdir} && zip -r ${path.resolve(FUNCTION_ZIP_ARM64)} .`;
 
 fs.rmSync(outdir, {recursive: true});
 console.log('Bundled Lambda');
