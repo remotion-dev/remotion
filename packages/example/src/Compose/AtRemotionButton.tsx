449bdbeb2e01cd9f286aa38358a5609f4c57ce31
JonnyBurger <jonathanburger11@gmail.com>
2025-11-03 17:17:35 +0100

    update what is remotion

--- packages/example/src/Compose/AtRemotionButton.tsx
+++ packages/example/src/Compose/AtRemotionButton.tsx
@@ -2,15 +2,14 @@ import {AbsoluteFill} from 'remotion';
 
 export const AtRemotionButton: React.FC<{progress: number}> = ({progress}) => {
 	return (
-		<AbsoluteFill className="bg-white justify-center items-center border-2 border-solid border-white rounded-full">
+		<AbsoluteFill className="bg-black justify-center items-center border-2 border-solid border-white rounded-full">
 			<div
-				className="text-black text-3xl font-brand"
+				className="text-white text-3xl font-brand"
 				style={{
 					fontFamily: 'GT Planar',
-					opacity: 1.1 - progress * 3,
 				}}
 			>
-				Follow @JNYBGR
+				@remotion
 			</div>
 		</AbsoluteFill>
 	);
