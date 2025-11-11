5119206ff55fe980d8f4a988b1e368c457b9f602
JonnyBurger <jonathanburger11@gmail.com>
2025-11-11 15:17:53 +0100

    add test for delayRender()

---
+++ packages/web-renderer/src/test/delay-render.test.tsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+import {useDelayRender} from 'remotion';
+import {test} from 'vitest';
+import {renderStillOnWeb} from '../render-still-on-web';
+import {testImage} from './utils';
+
+test('should be able to use delayRender()', async () => {
+	const blob = await renderStillOnWeb({
+		Component: () => {
+			const {delayRender, continueRender} = useDelayRender();
+			const [data, setData] = useState<boolean>(false);
+			const [handle] = useState(() => delayRender());
+
+			useEffect(() => {
+				setTimeout(() => {
+					setData(true);
+					continueRender(handle);
+				}, 50);
+			}, [continueRender, handle]);
+
+			return data ? (
+				<svg viewBox="0 0 100 100" style={{backgroundColor: 'green'}} />
+			) : (
+				<svg viewBox="0 0 100 100" style={{backgroundColor: 'red'}} />
+			);
+		},
+		durationInFrames: 100,
+		fps: 30,
+		width: 100,
+		height: 100,
+		frame: 0,
+	});
+
+	await testImage({blob, testId: 'delay-render'});
+});
