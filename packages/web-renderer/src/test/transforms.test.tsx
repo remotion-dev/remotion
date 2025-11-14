31d5faca95084464b59ff2a2f36918ad87e19887
JonnyBurger <jonathanburger11@gmail.com>
2025-11-14 16:57:53 +0100

    another test passing :)

--- packages/web-renderer/src/test/transforms.test.tsx
+++ packages/web-renderer/src/test/transforms.test.tsx
@@ -273,7 +273,6 @@ test('three-level nested transforms with varying origins', async () => {
 	await testImage({blob, testId: 'three-level-transform-origins'});
 });
 
-/*
 test('nested transforms with pixel-based transform-origins', async () => {
 	const Component: React.FC = () => {
 		return (
@@ -310,4 +309,3 @@ test('nested transforms with pixel-based transform-origins', async () => {
 
 	await testImage({blob, testId: 'pixel-transform-origins'});
 });
-*/
