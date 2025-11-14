c08abfdeea320456a83bd3ccf7778d097852ca13
JonnyBurger <jonathanburger11@gmail.com>
2025-11-14 11:57:39 +0100

    fix lints

--- packages/web-renderer/src/test/delay-render.test.tsx
+++ packages/web-renderer/src/test/delay-render.test.tsx
@@ -5,26 +5,27 @@ import {renderStillOnWeb} from '../render-still-on-web';
 import {testImage} from './utils';
 
 test('should be able to use delayRender()', async () => {
+	const Component: React.FC = () => {
+		const {delayRender, continueRender} = useDelayRender();
+		const [data, setData] = useState<boolean>(false);
+		const [handle] = useState(() => delayRender());
+
+		useEffect(() => {
+			setTimeout(() => {
+				setData(true);
+				continueRender(handle);
+			}, 50);
+		}, [continueRender, handle]);
+
+		return data ? (
+			<svg viewBox="0 0 100 100" style={{backgroundColor: 'green'}} />
+		) : (
+			<svg viewBox="0 0 100 100" style={{backgroundColor: 'red'}} />
+		);
+	};
+
 	const blob = await renderStillOnWeb({
-		component: () => {
-			const {delayRender, continueRender} = useDelayRender();
-			const [data, setData] = useState<boolean>(false);
-			const [handle] = useState(() => delayRender());
-
-			useEffect(() => {
-				setTimeout(() => {
-					setData(true);
-
-					continueRender(handle);
-				}, 50);
-			}, [continueRender, handle]);
-
-			return data ? (
-				<svg viewBox="0 0 100 100" style={{backgroundColor: 'green'}} />
-			) : (
-				<svg viewBox="0 0 100 100" style={{backgroundColor: 'red'}} />
-			);
-		},
+		component: Component,
 		durationInFrames: 100,
 		fps: 30,
 		width: 100,
