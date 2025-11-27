3866307d361879bd338fa93fa124c502faa5d988
JonnyBurger <jonathanburger11@gmail.com>
2025-11-27 21:43:42 +0100

    v4.0.379

--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.378"
+VERSION = "4.0.379"
--- packages/media-parser/src/version.ts
+++ packages/media-parser/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.378';
+export const VERSION = '4.0.379';
