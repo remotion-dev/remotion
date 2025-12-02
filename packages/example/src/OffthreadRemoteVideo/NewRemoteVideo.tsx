4c254686e40d2602813fc0c19624db37bf62c198
Jonny Burger <jonathanburger11@gmail.com>
2025-12-02 16:50:44 +0100

    oh shit

--- packages/example/src/OffthreadRemoteVideo/NewRemoteVideo.tsx
+++ packages/example/src/OffthreadRemoteVideo/NewRemoteVideo.tsx
@@ -19,7 +19,7 @@ export const calculateMetadataFn: CalculateMetadataFunction<
 };
 
 export const Component = () => {
-	return <Video src={src}  />;
+	return <Video src={src} />;
 };
 
 export const NewVideoComp = () => {
