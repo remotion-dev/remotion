0867ef9f940a49d5a639097b3f4b31a369f3ac1e
Jonny Burger <jonathanburger11@gmail.com>
2025-12-04 15:54:11 +0100

    remotion.dev/convert: Ensure that crop is divisible by 2

---
+++ packages/convert/app/components/make-crop.tsx
@@ -0,0 +1,32 @@
+import type {CropRectangle, VideoCodec} from 'mediabunny';
+import type {Dimensions} from '~/lib/calculate-new-dimensions-from-dimensions';
+
+export const makeCrop = ({
+	cropRect,
+	dimensions,
+	videoCodec,
+}: {
+	cropRect: CropRectangle;
+	dimensions: Dimensions;
+	videoCodec: VideoCodec;
+}) => {
+	let width = Number.isFinite(cropRect.width)
+		? cropRect.width
+		: dimensions!.width - cropRect.left;
+	let height = Number.isFinite(cropRect.height)
+		? cropRect.height
+		: dimensions!.height - cropRect.top;
+
+	// Round down to nearest even number if codec is avc or hevc
+	if (videoCodec === 'avc' || videoCodec === 'hevc') {
+		width = Math.floor(width / 2) * 2;
+		height = Math.floor(height / 2) * 2;
+	}
+
+	return {
+		width,
+		height,
+		left: cropRect.left,
+		top: cropRect.top,
+	};
+};
