2a331b6d2fb4d8a39f1dd4a9e46ba008a1925ced
Jonny Burger <jonathanburger11@gmail.com>
2025-12-10 18:06:34 +0100

    `@remotion/web-renderer`: Fix audio positioning

--- packages/web-renderer/src/audio.ts
+++ packages/web-renderer/src/audio.ts
@@ -11,7 +11,9 @@ function mixAudio(waves: Int16Array[], length: number) {
 	const mixed = new Int16Array(length);
 
 	for (let i = 0; i < length; i++) {
-		const sum = waves.reduce((acc, wave) => acc + wave[i], 0);
+		const sum = waves.reduce((acc, wave) => {
+			return acc + (wave[i] ?? 0);
+		}, 0);
 		// Clamp to Int16 range
 		mixed[i] = Math.max(-32768, Math.min(32767, sum));
 	}
@@ -19,44 +21,39 @@ function mixAudio(waves: Int16Array[], length: number) {
 	return mixed;
 }
 
-export const onlyInlineAudio = (assets: TRenderAsset[]): AudioData | null => {
+export const onlyInlineAudio = ({
+	assets,
+	fps,
+	frame,
+}: {
+	assets: TRenderAsset[];
+	fps: number;
+	frame: number;
+}): AudioData | null => {
 	const inlineAudio = assets.filter((asset) => asset.type === 'inline-audio');
+	const expectedLength = Math.round(
+		(TARGET_NUMBER_OF_CHANNELS * TARGET_SAMPLE_RATE) / fps,
+	);
 
-	let length: number | null = null;
 	for (const asset of inlineAudio) {
 		if (asset.toneFrequency !== 1) {
 			throw new Error(
 				'Setting the toneFrequency is not supported yet in web rendering.',
 			);
 		}
-
-		if (length === null) {
-			length = asset.audio.length;
-			// 1 frame offset may happen due to rounding, it is safe to truncate the last sample
-		} else if (
-			Math.abs(length - asset.audio.length) > TARGET_NUMBER_OF_CHANNELS
-		) {
-			throw new Error('All inline audio must have the same length');
-		} else {
-			length = Math.min(length, asset.audio.length);
-		}
-	}
-
-	if (length === null) {
-		return null;
 	}
 
 	const mixedAudio = mixAudio(
 		inlineAudio.map((asset) => asset.audio as Int16Array),
-		length,
+		expectedLength,
 	) as Int16Array<ArrayBuffer>;
 
 	return new AudioData({
 		data: mixedAudio,
 		format: 's16',
 		numberOfChannels: TARGET_NUMBER_OF_CHANNELS,
-		numberOfFrames: length / TARGET_NUMBER_OF_CHANNELS,
+		numberOfFrames: expectedLength / TARGET_NUMBER_OF_CHANNELS,
 		sampleRate: TARGET_SAMPLE_RATE,
-		timestamp: inlineAudio[0].timestamp,
+		timestamp: (frame / fps) * 1_000_000,
 	});
 };
