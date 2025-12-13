51034dca37766d62a73836867aa27fa7cfa7a904
JonnyBurger <jonathanburger11@gmail.com>
2025-12-13 08:46:41 +0100

    Make text fixture more complicated

--- packages/web-renderer/src/test/fixtures/text.tsx
+++ packages/web-renderer/src/test/fixtures/text.tsx
@@ -8,9 +8,26 @@ const Component: React.FC = () => {
 				backgroundColor: 'white',
 			}}
 		>
-			<p>
-				<strong>Hello</strong> world
-			</p>
+			<div style={{width: 550}}>
+				Hello from{' '}
+				<a href="https://github.com/WICG/html-in-canvas">html-in-canvas</a>!
+				<br />I{"'"}m multi-line, <b>formatted</b>, rotated text with emoji
+				(&#128512;), RTL text
+				<span dir="rtl">من فارسی صحبت میکنم</span>
+				{','}
+				vertical text,
+				<p style={{writingMode: 'vertical-rl'}}>这是垂直文本</p>
+				an inline image (
+				<img width={150} src="wolf.jpg" alt="Wolf" />
+				), and
+				<svg width={50} height={50}>
+					<circle cx={25} cy={25} r={20} fill="green" />
+					<text x={25} y={30} fontSize={15} textAnchor="middle" fill="#fff">
+						SVG
+					</text>
+				</svg>
+				!
+			</div>
 		</AbsoluteFill>
 	);
 };
