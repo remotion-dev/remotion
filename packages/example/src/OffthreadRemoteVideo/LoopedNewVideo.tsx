bcab7cd9f8c56eefc31b12d7829b19646010993c
Jonny Burger <jonathanburger11@gmail.com>
2025-12-15 15:37:04 +0100

    add example

---
+++ packages/example/src/OffthreadRemoteVideo/LoopedNewVideo.tsx
@@ -0,0 +1,36 @@
+import {Video} from '@remotion/media';
+import {CalculateMetadataFunction, Composition} from 'remotion';
+// https://www.remotion.dev/docs/mediabunny/metadata
+import {getMediaMetadata} from '../get-media-metadata';
+
+const src = 'https://remotion.media/video-1m.mp4';
+
+export const calculateMetadataFn: CalculateMetadataFunction<
+	Record<string, unknown>
+> = async () => {
+	const {dimensions, fps} = await getMediaMetadata(src);
+
+	return {
+		durationInFrames: fps! * 180,
+		fps: fps!,
+		width: dimensions!.width,
+		height: dimensions!.height,
+	};
+};
+
+export const Component = () => {
+	return <Video src={src} trimBefore={90} trimAfter={60 * 30} loop muted />;
+};
+
+export const LoopedNewVideo = () => {
+	return (
+		<Composition
+			component={Component}
+			id="LoopedNewVideo"
+			calculateMetadata={calculateMetadataFn}
+		/>
+	);
+};
+
+// In Root.tsx:
+// <LoopedNewVideo />
