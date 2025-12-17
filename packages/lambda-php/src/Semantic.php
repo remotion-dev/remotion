db3db807825364976fbf5fc996d18ede2727c946
JonnyBurger <jonathanburger11@gmail.com>
2025-12-17 17:37:51 +0100

    v4.0.393

--- packages/lambda-php/src/Semantic.php
+++ packages/lambda-php/src/Semantic.php
@@ -4,5 +4,5 @@ namespace Remotion\LambdaPhp;
 
 class Semantic
 {
-    public const VERSION = "4.0.392";
+    public const VERSION = "4.0.393";
 }
--- packages/media-parser/src/version.ts
+++ packages/media-parser/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.392';
+export const VERSION = '4.0.393';
