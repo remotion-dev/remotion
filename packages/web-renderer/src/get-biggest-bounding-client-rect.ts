c6c48f7a1927ad1717dfd855912e51def3deb61a
JonnyBurger <jonathanburger11@gmail.com>
2025-12-19 20:25:40 +0100

    Update get-biggest-bounding-client-rect.ts

--- packages/web-renderer/src/get-biggest-bounding-client-rect.ts
+++ packages/web-renderer/src/get-biggest-bounding-client-rect.ts
@@ -10,12 +10,15 @@ export const getBiggestBoundingClientRect = (
 	let mostRight = -Infinity;
 	let mostBottom = -Infinity;
 
-	while (treeWalker.nextNode()) {
+	while (true) {
 		const rect = (treeWalker.currentNode as Element).getBoundingClientRect();
 		mostLeft = Math.min(mostLeft, rect.left);
 		mostTop = Math.min(mostTop, rect.top);
 		mostRight = Math.max(mostRight, rect.right);
 		mostBottom = Math.max(mostBottom, rect.bottom);
+		if (!treeWalker.nextNode()) {
+			break;
+		}
 	}
 
 	return new DOMRect(
