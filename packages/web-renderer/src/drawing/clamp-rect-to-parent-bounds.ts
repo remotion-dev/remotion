3a658503ac88af94c92973805dc20729dd21d47f
Jonny Burger <jonathanburger11@gmail.com>
2025-12-27 14:48:15 +0100

    Update clamp-rect-to-parent-bounds.ts

--- packages/web-renderer/src/drawing/clamp-rect-to-parent-bounds.ts
+++ packages/web-renderer/src/drawing/clamp-rect-to-parent-bounds.ts
@@ -8,7 +8,15 @@ export const clampRectToParentBounds = ({
 	return new DOMRect(
 		Math.max(rect.left, parentRect.left),
 		Math.max(rect.top, parentRect.top),
-		Math.min(rect.width, parentRect.width),
-		Math.min(rect.height, parentRect.height),
+		Math.max(
+			0,
+			Math.min(rect.right, parentRect.right) -
+				Math.max(rect.left, parentRect.left),
+		),
+		Math.max(
+			0,
+			Math.min(rect.bottom, parentRect.bottom) -
+				Math.max(rect.top, parentRect.top),
+		),
 	);
 };
