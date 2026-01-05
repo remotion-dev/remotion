5ce50994515c3bcc63676eca712368f8b4f74538
Jonny Burger <jonathanburger11@gmail.com>
2026-01-05 12:30:26 +0100

    `@remotion/web-renderer`: Use explicit resource management for Mediabunny resources

---
+++ packages/web-renderer/src/get-audio-sample-source.ts
@@ -0,0 +1,33 @@
+import {
+	AudioSampleSource,
+	type BufferTarget,
+	type Output,
+	type OutputFormat,
+	type StreamTarget,
+} from 'mediabunny';
+import {getDefaultAudioEncodingConfig} from './get-audio-encoding-config';
+
+export const addAudioSampleSource = async ({
+	muted,
+	output,
+}: {
+	muted: boolean;
+	output: Output<OutputFormat, StreamTarget | BufferTarget>;
+}) => {
+	if (muted) {
+		return null;
+	}
+
+	// TODO: Should be able to customize
+	const defaultAudioEncodingConfig = await getDefaultAudioEncodingConfig();
+
+	if (!defaultAudioEncodingConfig) {
+		return Promise.reject(new Error('No default audio encoding config found'));
+	}
+
+	const audioSampleSource = new AudioSampleSource(defaultAudioEncodingConfig);
+
+	output.addAudioTrack(audioSampleSource);
+
+	return {audioSampleSource, [Symbol.dispose]: () => audioSampleSource.close()};
+};
