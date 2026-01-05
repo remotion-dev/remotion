8c427e52140bd4b10044e7d4dc57d6be12a65774
JonnyBurger <jonathanburger11@gmail.com>
2026-01-05 08:38:09 +0100

    Update ffmpeg-args.test.ts

--- packages/renderer/src/test/ffmpeg-args.test.ts
+++ packages/renderer/src/test/ffmpeg-args.test.ts
@@ -43,12 +43,9 @@ test('Should add premultiply filter for ProRes with yuva420p', () => {
 		logLevel: 'info',
 	});
 
-	const flatArgs = args.flat();
-	const vfIndex = flatArgs.indexOf('-vf');
-	expect(vfIndex).toBeGreaterThan(-1);
-
-	const vfValue = flatArgs[vfIndex + 1];
-	expect(vfValue).toContain('premultiply=inplace=1');
+	expect(args.flat().join(' ')).toBe(
+		'-c:v prores_videotoolbox -vf premultiply=inplace=1 -profile:v 4444 -pix_fmt yuva420p -auto-alt-ref 0 -video_track_timescale 90000',
+	);
 });
 
 test('Should NOT add premultiply filter for ProRes without alpha', () => {
@@ -69,12 +66,9 @@ test('Should NOT add premultiply filter for ProRes without alpha', () => {
 	});
 
 	const flatArgs = args.flat();
-	const vfIndex = flatArgs.indexOf('-vf');
-
-	if (vfIndex > -1) {
-		const vfValue = flatArgs[vfIndex + 1];
-		expect(vfValue).not.toContain('premultiply');
-	}
+	expect(flatArgs.join(' ')).toBe(
+		'-c:v prores_videotoolbox -profile:v 4444 -pix_fmt yuv444p10le -video_track_timescale 90000',
+	);
 });
 
 test('Should NOT add premultiply filter for h264 with alpha pixel format', () => {
@@ -95,12 +89,9 @@ test('Should NOT add premultiply filter for h264 with alpha pixel format', () =>
 	});
 
 	const flatArgs = args.flat();
-	const vfIndex = flatArgs.indexOf('-vf');
-
-	if (vfIndex > -1) {
-		const vfValue = flatArgs[vfIndex + 1];
-		expect(vfValue).not.toContain('premultiply');
-	}
+	expect(flatArgs.join(' ')).toBe(
+		'-c:v libx264 -pix_fmt yuva420p -auto-alt-ref 0 -video_track_timescale 90000 -crf 18',
+	);
 });
 
 test('Should chain premultiply with colorspace filter for bt709', () => {
@@ -121,19 +112,9 @@ test('Should chain premultiply with colorspace filter for bt709', () => {
 	});
 
 	const flatArgs = args.flat();
-	const vfIndex = flatArgs.indexOf('-vf');
-	expect(vfIndex).toBeGreaterThan(-1);
-
-	const vfValue = flatArgs[vfIndex + 1];
-	// Should have both filters chained with comma
-	expect(vfValue).toContain('premultiply=inplace=1');
-	expect(vfValue).toContain('zscale=matrix=709');
-	expect(vfValue).toContain(',');
-
-	// Premultiply should come before zscale
-	const premultiplyIndex = vfValue.indexOf('premultiply');
-	const zscaleIndex = vfValue.indexOf('zscale');
-	expect(premultiplyIndex).toBeLessThan(zscaleIndex);
+	expect(flatArgs.join(' ')).toBe(
+		'-c:v prores_videotoolbox -colorspace:v bt709 -color_primaries:v bt709 -color_trc:v bt709 -color_range tv -vf premultiply=inplace=1,zscale=matrix=709:matrixin=709:range=limited -profile:v 4444 -pix_fmt yuva444p10le -video_track_timescale 90000',
+	);
 });
 
 test('Should NOT add premultiply filter when hasPreencoded is true', () => {
@@ -154,5 +135,5 @@ test('Should NOT add premultiply filter when hasPreencoded is true', () => {
 	});
 
 	const flatArgs = args.flat();
-	expect(flatArgs).not.toContain('premultiply=inplace=1');
+	expect(flatArgs.join(' ')).toBe('-c:v copy');
 });
