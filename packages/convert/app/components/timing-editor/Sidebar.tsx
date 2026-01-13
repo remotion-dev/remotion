fe680ef3a24064c878cb0a204cef4eb473b3f0f6
Jonny Burger <jonathanburger11@gmail.com>
2026-01-13 10:59:17 +0100

    remove code editor

--- packages/convert/app/components/timing-editor/Sidebar.tsx
+++ packages/convert/app/components/timing-editor/Sidebar.tsx
@@ -3,8 +3,6 @@ import {Slider} from '~/components/ui/slider';
 import {SliderLabel} from './SliderLabel';
 import {CheckboxWithLabel} from './CheckboxWithLabel';
 import {Spacing} from './Spacing';
-import {CodeFrameTabs} from './CodeFrame';
-import {Tabs, TabsContent, TabsList, TabsTrigger} from '~/components/ui/tabs';
 import {PADDING_LEFT} from './draw-trajectory';
 
 export const Sidebar: React.FC<{
@@ -52,100 +50,79 @@ export const Sidebar: React.FC<{
 				borderLeft: '1px solid #242424',
 			}}
 		>
-			<Tabs defaultValue="configuration">
-				<TabsList className="grid w-full grid-cols-2">
-					<TabsTrigger value="configuration">Configuration</TabsTrigger>
-					<TabsTrigger value="code">Code</TabsTrigger>
-				</TabsList>
-				<TabsContent value="configuration">
-					<Spacing y={3} />
-					<Slider
-						value={[mass]}
-						min={0.3}
-						step={0.1}
-						max={10}
-						onValueChange={onMassChange}
-						onPointerUp={onRelease}
-					/>
-					<SliderLabel label="mass" value={mass} toggleable={null} />
-					<br />
-					<Slider
-						min={1}
-						max={200}
-						value={[damping]}
-						onValueChange={onDampingChange}
-						onPointerUp={onRelease}
-					/>
-					<SliderLabel label="damping" value={damping} toggleable={null} />
-					<br />
-					<Slider
-						min={1}
-						max={200}
-						value={[stiffness]}
-						onValueChange={onStiffnessChange}
-						onPointerUp={onRelease}
-					/>
-					<SliderLabel toggleable={null} label="stiffness" value={stiffness} />
-					<br />
-					<Slider
-						min={0}
-						max={400}
-						value={[delay]}
-						onValueChange={(val) => {
-							onDelayChange(val[0]);
-						}}
-						onPointerUp={onRelease}
-					/>
-					<SliderLabel label="delay" toggleable={null} value={delay} />
-					<br />
-					<>
-						<Slider
-							min={1}
-							max={200}
-							value={[fixedDurationInFrames ?? calculatedDurationInFrames]}
-							style={{opacity: fixedDurationInFrames === null ? 0.5 : 1}}
-							onValueChange={(val) => {
-								onDurationInFramesChange(val[0]);
-							}}
-							onPointerUp={onRelease}
-						/>
-						<SliderLabel
-							label="durationInFrames"
-							toggleable={(enabled) => {
-								if (enabled) {
-									onDurationInFramesChange(calculatedDurationInFrames);
-								} else {
-									onDurationInFramesChange(null);
-								}
-							}}
-							value={fixedDurationInFrames ?? null}
-						/>
-					</>
-					<br />
-					<CheckboxWithLabel
-						checked={overshootClamping}
-						id="overshootClamping"
-						onCheckedChange={onOvershootClampingChange}
-					/>
-					<CheckboxWithLabel
-						checked={reverse}
-						id="reverse"
-						onCheckedChange={onReverseChange}
-					/>
-					<Spacing y={2} />
-				</TabsContent>
-				<TabsContent value="code">
-					<CodeFrameTabs
-						delay={delay}
-						damping={damping}
-						mass={mass}
-						stiffness={stiffness}
-						overshotClamping={overshootClamping}
-						reverse={reverse}
-						durationInFrames={fixedDurationInFrames}
-					/>
-				</TabsContent>
-			</Tabs>
+			<Spacing y={3} />
+			<Slider
+				value={[mass]}
+				min={0.3}
+				step={0.1}
+				max={10}
+				onValueChange={onMassChange}
+				onPointerUp={onRelease}
+			/>
+			<SliderLabel label="mass" value={mass} toggleable={null} />
+			<br />
+			<Slider
+				min={1}
+				max={200}
+				value={[damping]}
+				onValueChange={onDampingChange}
+				onPointerUp={onRelease}
+			/>
+			<SliderLabel label="damping" value={damping} toggleable={null} />
+			<br />
+			<Slider
+				min={1}
+				max={200}
+				value={[stiffness]}
+				onValueChange={onStiffnessChange}
+				onPointerUp={onRelease}
+			/>
+			<SliderLabel toggleable={null} label="stiffness" value={stiffness} />
+			<br />
+			<Slider
+				min={0}
+				max={400}
+				value={[delay]}
+				onValueChange={(val) => {
+					onDelayChange(val[0]);
+				}}
+				onPointerUp={onRelease}
+			/>
+			<SliderLabel label="delay" toggleable={null} value={delay} />
+			<br />
+			<Slider
+				min={1}
+				max={200}
+				value={[fixedDurationInFrames ?? calculatedDurationInFrames]}
+				style={{opacity: fixedDurationInFrames === null ? 0.5 : 1}}
+				onValueChange={(val) => {
+					onDurationInFramesChange(val[0]);
+				}}
+				onPointerUp={onRelease}
+			/>
+			<SliderLabel
+				label="durationInFrames"
+				toggleable={(enabled) => {
+					if (enabled) {
+						onDurationInFramesChange(calculatedDurationInFrames);
+					} else {
+						onDurationInFramesChange(null);
+					}
+				}}
+				value={fixedDurationInFrames ?? null}
+			/>
+			<br />
+			<CheckboxWithLabel
+				checked={overshootClamping}
+				id="overshootClamping"
+				onCheckedChange={onOvershootClampingChange}
+			/>
+			<CheckboxWithLabel
+				checked={reverse}
+				id="reverse"
+				onCheckedChange={onReverseChange}
+			/>
+			<Spacing y={2} />
 		</div>
 	);
 };
