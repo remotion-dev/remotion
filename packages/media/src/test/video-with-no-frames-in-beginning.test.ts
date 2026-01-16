56b8a7d12e8093e40733e1249fcbdcd1fb22d62e
Jonny Burger <jonathanburger11@gmail.com>
2026-01-16 10:54:09 +0100

    same optimization during rendering

--- packages/media/src/test/video-with-no-frames-in-beginning.test.ts
+++ packages/media/src/test/video-with-no-frames-in-beginning.test.ts
@@ -1,6 +1,8 @@
 import {ALL_FORMATS, Input, UrlSource} from 'mediabunny';
-import {expect, test} from 'vitest';
+import {assert, expect, test} from 'vitest';
+import {getMaxVideoCacheSize, keyframeManager} from '../caches';
 import {makeNonceManager} from '../nonce-manager';
+import {extractFrame} from '../video-extraction/extract-frame';
 import {videoIteratorManager} from '../video-iterator-manager';
 
 test('in preview, should properly buffer and draw frames', async () => {
@@ -60,4 +62,46 @@ test('in preview, should properly buffer and draw frames', async () => {
 
 	const iteratorsCreated2 = manager.getVideoIteratorsCreated();
 	expect(iteratorsCreated2).toBe(2);
+});
+
+test('in rendering, should also be smart', async () => {
+	let lastFrame;
+	for (let i = 0; i < 5; i++) {
+		const frame = await extractFrame({
+			src: '/no-frames-in-beginning.webm',
+			timeInSeconds: i * 0.4,
+			logLevel: 'info',
+			loop: false,
+			trimAfter: undefined,
+			trimBefore: undefined,
+			playbackRate: 1,
+			fps: 30,
+			maxCacheSize: getMaxVideoCacheSize('info'),
+		});
+		assert(frame.type === 'success');
+		if (lastFrame) {
+			expect(frame.sample === lastFrame).toBe(true);
+			continue;
+		}
+
+		expect(frame.sample?.timestamp).toBe(4.045);
+		lastFrame = frame.sample;
+	}
+
+	const firstRealFrame = await extractFrame({
+		src: '/no-frames-in-beginning.webm',
+		timeInSeconds: 5,
+		logLevel: 'info',
+		loop: false,
+		trimAfter: undefined,
+		trimBefore: undefined,
+		playbackRate: 1,
+		fps: 30,
+		maxCacheSize: getMaxVideoCacheSize('info'),
+	});
+
+	assert(firstRealFrame.type === 'success');
+	expect(firstRealFrame.sample?.timestamp).toBe(4.979);
+
+	keyframeManager.clearAll('info');
 });
