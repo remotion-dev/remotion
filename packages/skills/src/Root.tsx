31a8a272dcf3fdb9e9b8472d9073676e918c01db
Jonny Burger <jonathanburger11@gmail.com>
2026-01-19 15:15:52 +0100

    move skills to ./remotion

--- packages/skills/src/Root.tsx
+++ packages/skills/src/Root.tsx
@@ -1,16 +1,16 @@
 import {Composition} from 'remotion';
-import {MyAnimation as FallingSpheresAnimation} from '../skills/3d/assets/falling-spheres';
+import {MyAnimation as FallingSpheresAnimation} from '../skills/remotion/3d/assets/falling-spheres';
 import {
 	COMPOSITION_HEIGHT,
 	COMPOSITION_WIDTH,
 	MyAnimation,
-} from '../skills/charts/assets/animated-shapes';
-import {MyAnimation as BarChartAnimation} from '../skills/charts/assets/bar-chart';
-import {MyAnimation as LottieAnimation} from '../skills/lottie/assets/example';
-import {MyAnimation as TextRotationAnimation} from '../skills/text-animations/assets/text-rotation';
-import {MyAnimation as TypewriterAnimation} from '../skills/text-animations/assets/typewriter';
-import {MyAnimation as WordCarouselAnimation} from '../skills/text-animations/assets/word-carousel';
-import {MyAnimation as WordHighlightAnimation} from '../skills/text-animations/assets/word-highlight';
+} from '../skills/remotion/charts/assets/animated-shapes';
+import {MyAnimation as BarChartAnimation} from '../skills/remotion/charts/assets/bar-chart';
+import {MyAnimation as LottieAnimation} from '../skills/remotion/lottie/assets/example';
+import {MyAnimation as TextRotationAnimation} from '../skills/remotion/text-animations/assets/text-rotation';
+import {MyAnimation as TypewriterAnimation} from '../skills/remotion/text-animations/assets/typewriter';
+import {MyAnimation as WordCarouselAnimation} from '../skills/remotion/text-animations/assets/word-carousel';
+import {MyAnimation as WordHighlightAnimation} from '../skills/remotion/text-animations/assets/word-highlight';
 
 export const RemotionRoot = () => {
 	return (
