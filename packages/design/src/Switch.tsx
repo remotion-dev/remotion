c9713cf268f1552d8125449fb39610d19d02aac3
Jonny Burger <jonathanburger11@gmail.com>
2026-01-29 17:23:46 +0100

    Fix `@remotion/design`

--- packages/design/src/Switch.tsx
+++ packages/design/src/Switch.tsx
@@ -7,12 +7,12 @@ export const Switch: React.FC<{
 	return (
 		<div
 			data-active={active}
-			className="h-[24px] transition-all rounded-full w-[48px] border-2 border-b-4 bg-gray-200 p-[2px] cursor-pointer data-[active=true]:bg-brand border-black relative"
+			className="h-[34px] transition-all rounded-full w-[56px] border-2 border-b-4 bg-gray-200 p-[2px] cursor-pointer data-[active=true]:bg-brand border-black relative"
 			onClick={onToggle}
 		>
 			<div
 				data-active={active}
-				className="h-[16px] w-[16px] box-content left-[4px] top-[4px] transition-all rounded-full bg-white border-2 border-black absolute data-[active=true]:left-[calc(100%-24px)]"
+				className="h-[20px] w-[20px] box-content left-[4px] top-[4px] transition-all rounded-full bg-white border-2 border-black absolute data-[active=true]:left-[calc(100%-24px)]"
 			/>
 		</div>
 	);
