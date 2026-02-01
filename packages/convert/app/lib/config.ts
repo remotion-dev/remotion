3269156e0628f92d705dd1c382ef35cdaa71e7b1
JonnyBurger <jonathanburger11@gmail.com>
2026-02-01 11:46:25 +0100

    Update config.ts

--- packages/convert/app/lib/config.ts
+++ packages/convert/app/lib/config.ts
@@ -1,4 +1,4 @@
 export const TEST_FAST = false;
 export const SAMPLE_FILE_CONVERT = 'https://remotion.media/video-1m.mp4';
 export const SAMPLE_FILE_TRANSCRIBE = 'https://remotion.media/dialogue.wav';
-export const REMOTION_PRO_ORIGIN = 'http://localhost:3000';
+export const REMOTION_PRO_ORIGIN = 'https://www.remotion.pro';
