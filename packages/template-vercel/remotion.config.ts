1cab65585db07c3be9fa10874d1b5469bd3d1989
umungobungo <m.mcgillivray@outlook.com>
2026-02-03 17:04:18 +0800

    include tailwind in bundle so rendered video matches Player

--- packages/template-vercel/remotion.config.ts
+++ packages/template-vercel/remotion.config.ts
@@ -4,5 +4,7 @@
 // Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
 
 import { Config } from "@remotion/cli/config";
+import { enableTailwind } from "@remotion/tailwind-v4";
 
 Config.setVideoImageFormat("jpeg");
+Config.overrideWebpackConfig(enableTailwind);
