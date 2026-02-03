452a86111d683513495b07e858620efc80dabf33
umungobungo <m.mcgillivray@outlook.com>
2026-02-03 16:03:22 +0800

    run prettier

--- packages/template-vercel/src/app/layout.tsx
+++ packages/template-vercel/src/app/layout.tsx
@@ -2,24 +2,24 @@ import "../../styles/global.css";
 import { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
-  title: "Remotion and Next.js",
-  description: "Remotion and Next.js",
+	title: "Remotion and Next.js",
+	description: "Remotion and Next.js",
 };
 
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="bg-background">{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className="bg-background">{children}</body>
+		</html>
+	);
 }
--- packages/template-vercel/src/remotion/MyComp/Main.tsx
+++ packages/template-vercel/src/remotion/MyComp/Main.tsx
@@ -1,10 +1,10 @@
 import { z } from "zod";
 import {
-  AbsoluteFill,
-  Sequence,
-  spring,
-  useCurrentFrame,
-  useVideoConfig,
+	AbsoluteFill,
+	Sequence,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
 } from "remotion";
 import { CompositionProps } from "../../../types/constants";
 import { NextLogo } from "./NextLogo";
@@ -13,46 +13,46 @@ import { Rings } from "./Rings";
 import { TextFade } from "./TextFade";
 
 loadFont("normal", {
-  subsets: ["latin"],
-  weights: ["400", "700"],
+	subsets: ["latin"],
+	weights: ["400", "700"],
 });
 export const Main = ({ title }: z.infer<typeof CompositionProps>) => {
-  const frame = useCurrentFrame();
-  const { fps } = useVideoConfig();
+	const frame = useCurrentFrame();
+	const { fps } = useVideoConfig();
 
-  const transitionStart = 2 * fps;
-  const transitionDuration = 1 * fps;
+	const transitionStart = 2 * fps;
+	const transitionDuration = 1 * fps;
 
-  const logoOut = spring({
-    fps,
-    frame,
-    config: {
-      damping: 200,
-    },
-    durationInFrames: transitionDuration,
-    delay: transitionStart,
-  });
+	const logoOut = spring({
+		fps,
+		frame,
+		config: {
+			damping: 200,
+		},
+		durationInFrames: transitionDuration,
+		delay: transitionStart,
+	});
 
-  return (
-    <AbsoluteFill className="bg-white">
-      <Sequence durationInFrames={transitionStart + transitionDuration}>
-        <Rings outProgress={logoOut}></Rings>
-        <AbsoluteFill className="justify-center items-center">
-          <NextLogo outProgress={logoOut}></NextLogo>
-        </AbsoluteFill>
-      </Sequence>
-      <Sequence from={transitionStart + transitionDuration / 2}>
-        <TextFade>
-          <h1
-            className="text-[70px] font-bold"
-            style={{
-              fontFamily,
-            }}
-          >
-            {title}
-          </h1>
-        </TextFade>
-      </Sequence>
-    </AbsoluteFill>
-  );
+	return (
+		<AbsoluteFill className="bg-white">
+			<Sequence durationInFrames={transitionStart + transitionDuration}>
+				<Rings outProgress={logoOut}></Rings>
+				<AbsoluteFill className="justify-center items-center">
+					<NextLogo outProgress={logoOut}></NextLogo>
+				</AbsoluteFill>
+			</Sequence>
+			<Sequence from={transitionStart + transitionDuration / 2}>
+				<TextFade>
+					<h1
+						className="text-[70px] font-bold"
+						style={{
+							fontFamily,
+						}}
+					>
+						{title}
+					</h1>
+				</TextFade>
+			</Sequence>
+		</AbsoluteFill>
+	);
 };
