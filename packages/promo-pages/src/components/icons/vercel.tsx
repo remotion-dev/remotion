cf366bc182f342746d97d25ef86f84f445ad82ee
Jonny Burger <jonathanburger11@gmail.com>
2026-02-05 11:53:24 +0100

    restructure templates

---
+++ packages/promo-pages/src/components/icons/vercel.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const VercelIcon: React.FC<{
+	readonly style: React.CSSProperties;
+}> = ({style}) => {
+	return (
+		<svg
+			fill="currentcolor"
+			viewBox="0 0 76 65"
+			style={style}
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path d="M37.5274 0L75.0548 65H0L37.5274 0Z" />
+		</svg>
+	);
+};
