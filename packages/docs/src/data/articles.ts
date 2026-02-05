31f4a96ebec888aace9348ccb0bff36f4a65e42c
Jonny Burger <jonathanburger11@gmail.com>
2026-02-05 17:27:57 +0100

    update

--- packages/docs/src/data/articles.ts
+++ packages/docs/src/data/articles.ts
@@ -81,6 +81,15 @@ export const articles = [
 		slug: 'after-effects',
 	},
 	{
+		id: 'ai/ai-saas-template',
+		title: 'Prompt to Motion Graphics SaaS Starter Kit',
+		relativePath: 'docs/ai/ai-saas-template.mdx',
+		compId: 'articles-docs-ai-ai-saas-template',
+		crumb: 'AI',
+		noAi: false,
+		slug: 'ai/ai-saas-template',
+	},
+	{
 		id: 'ai/bolt',
 		title: 'Prompting a Remotion video with Bolt.new',
 		relativePath: 'docs/ai/bolt.mdx',
