c1606ea6500168082d33546a535eaa979f2acfc4
JonnyBurger <jonathanburger11@gmail.com>
2026-03-03 11:24:20 +0100

    Update index.tsx

--- packages/example/src/SlicedVideo/index.tsx
+++ packages/example/src/SlicedVideo/index.tsx
@@ -1,17 +1,17 @@
 import {Video} from '@remotion/media';
 import React from 'react';
-import {Sequence, staticFile, useVideoConfig} from 'remotion';
+import {Sequence, useVideoConfig} from 'remotion';
 
 const SLICE_DURATION_FRAMES = 3; // 0.1sec at 30fps
 const NUM_SLICES = 100;
 const PREMOUNT_SEC = 0.5;
 
 export const SlicedVideo: React.FC = () => {
-	const src = staticFile('sliced-video.mp4');
+	const src = 'https://remotion.media/video.mp4';
 	const {fps} = useVideoConfig();
 
 	return (
-		<Sequence from={45} premountFor={45}>
+		<Sequence premountFor={45}>
 			{new Array(NUM_SLICES).fill(0).map((_, i) => {
 				const from = i * SLICE_DURATION_FRAMES;
 				return (
