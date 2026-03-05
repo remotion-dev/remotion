f90bd9f7c78b2a045d8f94981af699a70ca08522
Saim Farid <saimfd00@gmail.com>
2026-03-05 12:26:06 +0200

    remove unused code

--- packages/web-renderer/src/test/filter.test.tsx
+++ packages/web-renderer/src/test/filter.test.tsx
@@ -1,7 +1,7 @@
 import {test} from 'vitest';
 import {renderStillOnWeb} from '../render-still-on-web';
 import '../symbol-dispose';
-import {filter, filterSvgUrl} from './fixtures/filter';
+import {filter} from './fixtures/filter';
 import {testImage} from './utils';
 
 test('should render filter', async () => {
@@ -14,16 +14,4 @@ test('should render filter', async () => {
 	});
 
 	await testImage({blob, testId: 'filter'});
-});
-
-test('should render filter with SVG url', async () => {
-	const {blob} = await renderStillOnWeb({
-		licenseKey: 'free-license',
-		composition: filterSvgUrl,
-		frame: 0,
-		inputProps: {},
-		imageFormat: 'png',
-	});
-
-	await testImage({blob, testId: 'filter-svg-url'});
 });
