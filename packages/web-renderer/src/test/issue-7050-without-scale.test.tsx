593e029cc44f7fda9acedd9c106eabc533ae5e6b
Saim Farid <saimfd00@gmail.com>
2026-04-26 19:49:43 +0300

    update test file names

--- packages/web-renderer/src/test/issue-7050-without-scale.test.tsx
+++ packages/web-renderer/src/test/issue-7050-without-scale.test.tsx
@@ -4,7 +4,7 @@ import '../symbol-dispose';
 import {issue7050Repro} from './fixtures/issue-7050-repro';
 import {testImage} from './utils';
 
-test('should render issue-7050-repro-without-scale', async () => {
+test('should render issue-7050-without-scale', async () => {
 	const blob = await (
 		await renderStillOnWeb({
 			licenseKey: 'free-license',
@@ -14,5 +14,5 @@ test('should render issue-7050-repro-without-scale', async () => {
 		})
 	).blob({format: 'png'});
 
-	await testImage({blob, testId: 'issue-7050-repro-without-scale'});
+	await testImage({blob, testId: 'issue-7050-without-scale'});
 });
