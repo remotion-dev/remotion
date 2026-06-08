1568f50100466ef69318091db7aae30672f374c5
Jonny Burger <jonathanburger11@gmail.com>
2026-06-08 13:05:23 +0200

    Update TimelineSelection.tsx

--- packages/studio/src/components/Timeline/TimelineSelection.tsx
+++ packages/studio/src/components/Timeline/TimelineSelection.tsx
@@ -63,7 +63,7 @@ export const getTimelineSelectedTrackHighlightStyle = (
 export const SELECTION_ENABLED = false;
 export const TIMELINE_TOP_DRAG = false;
 export const ENABLE_OUTLINES = false;
-export const EASING_SELECTION_ENABLED = true;
+export const EASING_SELECTION_ENABLED = false;
 
 type TimelineSelectionBase = {
 	readonly nodePathInfo: SequenceNodePathInfo;
