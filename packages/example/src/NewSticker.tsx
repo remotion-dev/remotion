b7af725708bc48af3b5aeb9da0a955cbea1077d0
JonnyBurger <jonathanburger11@gmail.com>
2026-06-11 18:13:58 +0200

    Update NewSticker.tsx

--- packages/example/src/NewSticker.tsx
+++ packages/example/src/NewSticker.tsx
@@ -1,13 +1,12 @@
-import {Arrow, Star} from '@remotion/shapes';
+import {Star} from '@remotion/shapes';
 import {visualControl} from '@remotion/studio';
 import {zColor} from '@remotion/zod-types';
 import React from 'react';
-import {Interactive, useCurrentFrame} from 'remotion';
+import {AbsoluteFill} from 'remotion';
 
 const NewSticker: React.FC = () => {
-	const frame = useCurrentFrame();
 	return (
-		<Interactive.Div
+		<AbsoluteFill
 			style={{
 				display: 'flex',
 				justifyContent: 'center',
@@ -16,36 +15,10 @@ const NewSticker: React.FC = () => {
 				position: 'absolute',
 				width: '100%',
 				height: '100%',
-				translate: '0.2px 0px',
 			}}
 		>
-			<Star
-				points={20}
-				innerRadius={174}
-				outerRadius={207}
-				fill={'#f2be0a'}
-				style={{
-					scale: 0.72867,
-					translate: '-1px -0.7px',
-				}}
-			/>
-			<Arrow
-				length={274}
-				headWidth={112}
-				headLength={66}
-				shaftWidth={47}
-				direction="right"
-				cornerRadius={0}
-				fill={'#e76862'}
-				style={{
-					position: 'absolute',
-					scale: 0.855948,
-					translate: '-360.6px 1.4px',
-					transformOrigin: '182.03% 56.08%',
-					rotate: '2540.702685deg',
-				}}
-			/>
-		</Interactive.Div>
+			<Star points={20} innerRadius={174} outerRadius={207} fill={'#f2be0a'} />
+		</AbsoluteFill>
 	);
 };
 
