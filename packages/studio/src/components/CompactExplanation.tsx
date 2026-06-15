9f238fdf5cf71ecc32b50b4d0bfe77ff7fd5464d
JonnyBurger <jonathanburger11@gmail.com>
2026-06-15 17:44:29 +0200

    `@remotion/studio`: Match compact help icon color

--- packages/studio/src/components/CompactExplanation.tsx
+++ packages/studio/src/components/CompactExplanation.tsx
@@ -11,7 +11,7 @@ const compactExplanation: CSSProperties = {
 
 const compactHelpLink: CSSProperties = {
 	alignItems: 'center',
-	color: LIGHT_TEXT,
+	color: 'inherit',
 	cursor: 'default',
 	display: 'inline-flex',
 	height: 13,
@@ -29,6 +29,7 @@ const compactHelpLinkHovered: CSSProperties = {
 };
 
 const compactHelpIcon: CSSProperties = {
+	color: 'inherit',
 	display: 'block',
 	fill: 'currentColor',
 	height: 13,
@@ -63,7 +64,10 @@ const CompactHelpLink = ({
 				style={compactHelpIcon}
 				aria-hidden="true"
 			>
-				<path d="M464 256a208 208 0 1 0 -416 0 208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0 256 256 0 1 1 -512 0zm256-80c-17.7 0-32 14.3-32 32l-48 0c0-44.2 35.8-80 80-80s80 35.8 80 80c0 35.1-20.5 57.5-38.2 70-6.3 4.4-12.5 7.8-17.8 10.4l0 21.9-48 0 0-56.7c1.4-.4 2.7-.7 4.1-1.1 12.2-3.2 23.3-6.1 34.1-13.7 10.4-7.3 17.8-16.9 17.8-30.8 0-17.7-14.3-32-32-32zM232 344l48 0 0 48-48 0 0-48z" />
+				<path
+					fill="currentColor"
+					d="M464 256a208 208 0 1 0 -416 0 208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0 256 256 0 1 1 -512 0zm256-80c-17.7 0-32 14.3-32 32l-48 0c0-44.2 35.8-80 80-80s80 35.8 80 80c0 35.1-20.5 57.5-38.2 70-6.3 4.4-12.5 7.8-17.8 10.4l0 21.9-48 0 0-56.7c1.4-.4 2.7-.7 4.1-1.1 12.2-3.2 23.3-6.1 34.1-13.7 10.4-7.3 17.8-16.9 17.8-30.8 0-17.7-14.3-32-32-32zM232 344l48 0 0 48-48 0 0-48z"
+				/>
 			</svg>
 		</a>
 	);
