5835055e9681d0f97b9ba82ae14c8fd2c23b4c1b
Aryan Singh <aryansinghpm27@gmail.com>
2026-06-16 13:39:16 +0000

    fix: formatting in pixelate.ts

--- packages/effects/src/pixelate.ts
+++ packages/effects/src/pixelate.ts
@@ -162,10 +162,10 @@ const createPixelateState = (gl: WebGL2RenderingContext): PixelateState => {
 
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.bindTexture(gl.TEXTURE_2D, null);
 
 	return {
 		gl,
