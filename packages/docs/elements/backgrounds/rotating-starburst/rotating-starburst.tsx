8ac9c46ebc9f0c0ead6f18d75e28c9ae4744fa5c
Jonny Burger <jonathanburger11@gmail.com>
2026-07-08 11:25:59 +0200

    Docs: Use blue starburst palette

--- packages/docs/elements/backgrounds/rotating-starburst/rotating-starburst.tsx
+++ packages/docs/elements/backgrounds/rotating-starburst/rotating-starburst.tsx
@@ -7,13 +7,13 @@ export const RotatingStarburst: React.FC = () => {
 
 	return (
 		<Solid
-			color="#111827"
+			color="#dff4ff"
 			width={1920}
 			height={1080}
 			effects={[
 				starburst({
 					rays: 28,
-					colors: ['#111827', '#f97316'],
+					colors: ['#dff4ff', '#7cc6ff'],
 					rotation: interpolate(frame, [0, 2000], [0, 360]),
 					origin: [0.5, 0.5],
 				}),
