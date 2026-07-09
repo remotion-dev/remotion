da32f2431cd89695a02976c0d2853c546cc7801e
Jonny Burger <jonathanburger11@gmail.com>
2026-07-09 11:22:47 +0200

    `@remotion/studio`: Align visual controls in inspector

--- packages/studio/src/components/VisualControls/VisualControlsContent.tsx
+++ packages/studio/src/components/VisualControls/VisualControlsContent.tsx
@@ -1,7 +1,17 @@
 import React, {useContext} from 'react';
 import {VisualControlsContext} from '../../visual-controls/VisualControls';
+import {INSPECTOR_PANEL_HORIZONTAL_PADDING} from '../InspectorPanelLayout';
+import {getSchemaEditorRootInset} from '../RenderModal/SchemaEditor/Fieldset';
 import {SchemaSeparationLine} from '../RenderModal/SchemaEditor/SchemaSeparationLine';
 import {VisualControlHandle} from './VisualControlHandle';
+
+const rootInset = getSchemaEditorRootInset(INSPECTOR_PANEL_HORIZONTAL_PADDING);
+
+const container: React.CSSProperties = {
+	boxSizing: 'border-box',
+	paddingLeft: rootInset,
+	paddingRight: rootInset,
+};
 
 export const VisualControlsContent = () => {
 	const {handles} = useContext(VisualControlsContext);
@@ -13,7 +23,7 @@ export const VisualControlsContent = () => {
 	}
 
 	return (
-		<div>
+		<div style={container}>
 			{entries.map(([key, value], i) => {
 				return (
 					<React.Fragment key={key}>
