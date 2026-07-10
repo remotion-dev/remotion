ab9a36bc9b0aea862a4374fd8122101d717dad24
Jonny Burger <jonathanburger11@gmail.com>
2026-07-10 18:57:21 +0200

    update structure of composition

--- packages/template-blank/src/Composition.tsx
+++ packages/template-blank/src/Composition.tsx
@@ -1,3 +1,18 @@
-export const MyComposition = () => {
+import { Composition } from "remotion";
+
+export const MyComponent = () => {
   return null;
+};
+
+export const MyComposition = () => {
+  return (
+    <Composition
+      id="MyComp"
+      component={MyComponent}
+      durationInFrames={60}
+      fps={30}
+      width={1280}
+      height={720}
+    />
+  );
 };
