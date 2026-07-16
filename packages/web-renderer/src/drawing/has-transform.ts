b806dbab8f2e0869fa85d1d0245f37bbf4123074
Jonny Burger <jonathanburger11@gmail.com>
2026-07-16 15:05:59 +0200

    `@remotion/web-renderer`: Improve client-side render performance (#9124)

--- packages/web-renderer/src/drawing/has-transform.ts
+++ packages/web-renderer/src/drawing/has-transform.ts
@@ -1,16 +1,22 @@
-export const hasTransformCssValue = (style: CSSStyleDeclaration) => {
+export const hasTransformCssValue = (
+	style: Pick<CSSStyleDeclaration, 'transform'>,
+) => {
 	return style.transform !== 'none' && style.transform !== '';
 };
 
-export const hasRotateCssValue = (style: CSSStyleDeclaration) => {
+export const hasRotateCssValue = (
+	style: Pick<CSSStyleDeclaration, 'rotate'>,
+) => {
 	return style.rotate !== 'none' && style.rotate !== '';
 };
 
-export const hasScaleCssValue = (style: CSSStyleDeclaration) => {
+export const hasScaleCssValue = (style: Pick<CSSStyleDeclaration, 'scale'>) => {
 	return style.scale !== 'none' && style.scale !== '';
 };
 
-export const hasAnyTransformCssValue = (style: CSSStyleDeclaration) => {
+export const hasAnyTransformCssValue = (
+	style: Pick<CSSStyleDeclaration, 'rotate' | 'scale' | 'transform'>,
+) => {
 	return (
 		hasTransformCssValue(style) ||
 		hasRotateCssValue(style) ||
