05fc00b96446b332a09d183218076e68798a062a
Jonny Burger <jonathanburger11@gmail.com>
2026-07-16 13:35:40 +0200

    `@remotion/example`: Remove Canary browser override

--- packages/example/remotion.config.ts
+++ packages/example/remotion.config.ts
@@ -1,13 +1,5 @@
-import {existsSync} from 'node:fs';
 import {Config} from '@remotion/cli/config';
 import {webpackOverride} from './src/webpack-override.mjs';
-
-const chromeCanaryExecutable =
-	'/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary';
-
-if (existsSync(chromeCanaryExecutable)) {
-	Config.setBrowserExecutable(chromeCanaryExecutable);
-}
 
 Config.setOverwriteOutput(true);
 Config.overrideWebpackConfig(async (config) => {
