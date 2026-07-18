78b72153db2f8d12b682d9c74d69dbd8df12f683
Jonny Burger <jonathanburger11@gmail.com>
2026-07-18 18:12:27 +0200

    Docs: Add light backgrounds to text highlight examples

--- packages/docs/components/demos/RoughNotationPosterizedSeedDemo.tsx
+++ packages/docs/components/demos/RoughNotationPosterizedSeedDemo.tsx
@@ -21,6 +21,7 @@ export const RoughNotationPosterizedSeedDemo: React.FC = () => {
 			style={{
 				justifyContent: 'center',
 				alignItems: 'center',
+				backgroundColor: '#fff',
 			}}
 		>
 			<Interactive.Div
