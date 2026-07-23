b0643cf8a7d419cbf40e3bede111907cd62d0011
Jonny Burger <jonathanburger11@gmail.com>
2026-07-23 09:55:27 +0200

    `@remotion/renderer`: Add object signature to getCompositions()

--- packages/it-tests/src/rendering/get-compositions.test.ts
+++ packages/it-tests/src/rendering/get-compositions.test.ts
@@ -6,4 +6,9 @@ test('getCompositions() should give a good error message if there is no composit
 	expect(() => getCompositions()).toThrow(
 		'No serve URL or webpack bundle directory was passed to getCompositions().',
 	);
+
+	// @ts-expect-error
+	expect(() => getCompositions({})).toThrow(
+		'No serve URL or webpack bundle directory was passed to getCompositions().',
+	);
 });
