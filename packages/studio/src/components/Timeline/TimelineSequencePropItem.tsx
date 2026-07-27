d5e81bafcd8ab24a162c3854020ad9a4022598a0
Jonny Burger <jonathanburger11@gmail.com>
2026-07-27 10:11:52 +0200

    `@remotion/studio`: Indent computed media sources

--- packages/studio/src/components/Timeline/TimelineSequencePropItem.tsx
+++ packages/studio/src/components/Timeline/TimelineSequencePropItem.tsx
@@ -17,6 +17,7 @@ import type {
 	TimelineFieldOnSave,
 } from '../../helpers/timeline-layout';
 import {ContextMenu} from '../ContextMenu';
+import {INSPECTOR_PANEL_HORIZONTAL_PADDING} from '../InspectorPanelLayout';
 import type {ComboboxValue} from '../NewComposition/ComboBox';
 import {callAddSequenceKeyframe} from './call-add-keyframe';
 import {getAnimationItemSelectionForSourceFrame} from './get-animation-item-selection-for-frame';
@@ -48,6 +49,11 @@ const inlineSourceFieldRow: React.CSSProperties = {
 	display: 'flex',
 	minWidth: 0,
 	width: '100%',
+};
+
+const computedSourceFieldRow: React.CSSProperties = {
+	boxSizing: 'border-box',
+	paddingInline: INSPECTOR_PANEL_HORIZONTAL_PADDING,
 };
 
 const isKeyframedStatus = (
@@ -567,7 +573,17 @@ export const TimelineSequencePropItem: React.FC<{
 	);
 
 	if (field.typeName === 'asset' && field.key === 'src') {
-		return <div style={{...inlineSourceFieldRow, ...style}}>{fieldValue}</div>;
+		return (
+			<div
+				style={{
+					...inlineSourceFieldRow,
+					...(propStatus.status === 'computed' ? computedSourceFieldRow : null),
+					...style,
+				}}
+			>
+				{fieldValue}
+			</div>
+		);
 	}
 
 	const row = (
