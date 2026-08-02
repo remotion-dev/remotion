dd9671c2fa5585fb5f32f33980460578f39e4a13
Jonny Burger <jonathanburger11@gmail.com>
2026-08-02 17:26:29 +0200

    `@remotion/studio`: Allow precise scale input

--- packages/example/src/EffectKeyframeE2e.tsx
+++ packages/example/src/EffectKeyframeE2e.tsx
@@ -5,7 +5,14 @@ import {AbsoluteFill, Solid} from 'remotion';
 export const EffectKeyframeE2e: React.FC = () => {
 	return (
 		<AbsoluteFill>
-			<Solid width={1080} height={1080} color="#1f2429" effects={[wave({})]} />
+			<Solid
+				name="Scale precision"
+				width={1080}
+				height={1080}
+				color="#1f2429"
+				style={{scale: 1}}
+				effects={[wave({})]}
+			/>
 		</AbsoluteFill>
 	);
 };
