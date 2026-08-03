bfda4e6615af8b10e1bf772a399b9aea53b23324
JonnyBurger <jonathanburger11@gmail.com>
2026-08-03 16:43:55 +0200

    remotion.dev/brand: Add responsive Studio animation

--- packages/brand/src/Root.tsx
+++ packages/brand/src/Root.tsx
@@ -25,6 +25,10 @@ import {
 } from './Compose/WhatIsRemotion';
 import {DesignSystems, designSystemsDurationInFrames} from './DesignSystems';
 import {
+	DesignSystemsResponsive,
+	designSystemsResponsiveDurationInFrames,
+} from './DesignSystemsResponsive';
+import {
 	DocsPagesShowcase,
 	INSTAGRAM_POST_HEIGHT,
 	INSTAGRAM_POST_WIDTH,
@@ -322,6 +326,14 @@ export const RemotionRoot: React.FC = () => {
 					fps={30}
 					width={1080}
 					height={1080}
+				/>
+				<Composition
+					id="DesignSystemsResponsive"
+					component={DesignSystemsResponsive}
+					durationInFrames={designSystemsResponsiveDurationInFrames}
+					fps={30}
+					width={1350}
+					height={796}
 				/>
 				<Composition
 					id="ApplicationSimpleApp"
