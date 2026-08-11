504547c9437e5fb1cf161327c99d2ce29c578377
JonnyBurger <jonathanburger11@gmail.com>
2026-08-11 08:54:33 +0200

    `@remotion/studio`: Fix menu Escape race

--- packages/studio/src/state/z-index.tsx
+++ packages/studio/src/state/z-index.tsx
@@ -2,6 +2,7 @@ import React, {
 	createContext,
 	useContext,
 	useEffect,
+	useLayoutEffect,
 	useMemo,
 	useRef,
 } from 'react';
@@ -27,7 +28,7 @@ const EscapeHook: React.FC<{
 }> = ({onEscape}) => {
 	const keybindings = useKeybinding();
 
-	useEffect(() => {
+	useLayoutEffect(() => {
 		const escape = keybindings.registerKeybinding({
 			event: 'keydown',
 			key: 'Escape',
@@ -78,7 +79,7 @@ export const HigherZIndex: React.FC<{
 		? context.currentIndex
 		: (stackedIndex.current ?? context.currentIndex + 1);
 
-	useEffect(() => {
+	useLayoutEffect(() => {
 		if (disabled) {
 			return;
 		}
