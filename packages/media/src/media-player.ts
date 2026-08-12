d2137ac387ba343c52f1fb602f810821a8306ae0
JonnyBurger <jonathanburger11@gmail.com>
2026-08-12 08:01:32 +0200

    `@remotion/media`: Remove desynchronized canvas hint

--- packages/media/src/media-player.ts
+++ packages/media/src/media-player.ts
@@ -182,7 +182,6 @@ export class MediaPlayer {
 		if (canvas) {
 			const context = canvas.getContext('2d', {
 				alpha: true,
-				desynchronized: true,
 			}) as OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D | null;
 
 			if (!context) {
