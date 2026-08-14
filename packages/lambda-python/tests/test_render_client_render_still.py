6ea4320e423f7a0ebc6af917411ed53c663b4f62
Jonny Burger <jonathanburger11@gmail.com>
2026-08-14 09:00:50 +0200

    `@remotion/lambda-python`: Respect forced bucket for still renders (#10421)

--- packages/lambda-python/tests/test_render_client_render_still.py
+++ packages/lambda-python/tests/test_render_client_render_still.py
@@ -5,6 +5,19 @@ from remotion_lambda.remotionclient import RemotionClient
 
 
 class TestRemotionClient(TestCase):
+    def test_force_bucket_name_is_serialized_as_bucket_name(self):
+        render_still_params = RenderStillParams(
+            composition="still-helloworld",
+            force_bucket_name="remotionlambda-useast1-testbucket",
+        )
+
+        serialized_params = render_still_params.serialize_params()
+
+        self.assertEqual(
+            serialized_params['bucketName'], 'remotionlambda-useast1-testbucket'
+        )
+        self.assertNotIn('forceBucketName', serialized_params)
+
     def test_remotion_construct_request(self):
         client = RemotionClient(
             region="us-east-1", serve_url="testbed", function_name="remotion-render"
