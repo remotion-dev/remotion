351d1ec6c78e50d0847b66765f1894acee2c75f9
Jonny Burger <jonathanburger11@gmail.com>
2026-08-19 10:29:58 +0200

    `remotion`: Avoid duplicate delay-render initialization

---
+++ packages/core/src/delay-render-constants.ts
@@ -0,0 +1,5 @@
+export const DELAY_RENDER_CALLSTACK_TOKEN = 'The delayRender was called:';
+export const DELAY_RENDER_RETRIES_LEFT = 'Retries left: ';
+export const DELAY_RENDER_RETRY_TOKEN =
+	'- Rendering the frame will be retried.';
+export const DELAY_RENDER_CLEAR_TOKEN = 'handle was cleared after';
