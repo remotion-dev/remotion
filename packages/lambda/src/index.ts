a659f3716d0031beabaa2089c4c6a55393769eb0
JonnyBurger <jonathanburger11@gmail.com>
2026-08-20 19:08:37 +0200

    Keep render cancellation in Lambda client

--- packages/lambda/src/index.ts
+++ packages/lambda/src/index.ts
@@ -1,7 +1,6 @@
 import type {
 	AwsPartition,
 	AwsRegion,
-	CancelRenderOnLambdaInput,
 	CustomCredentials,
 	DeleteFunctionInput,
 	DeleteRenderInput,
@@ -24,7 +23,6 @@ import type {
 	RenderStillOnLambdaOutput,
 } from '@remotion/lambda-client';
 import {
-	cancelRenderOnLambda as deprecatedCancelRenderOnLambda,
 	deleteFunction,
 	deleteRender,
 	getRenderProgress as deprecatedGetRenderProgress,
@@ -100,17 +98,6 @@ const renderMediaOnLambda = NoReactInternals.ENABLE_V5_BREAKING_CHANGES
 /**
  * @deprecated Import this from `@remotion/lambda-client` instead
  */
-const cancelRenderOnLambda = NoReactInternals.ENABLE_V5_BREAKING_CHANGES
-	? () => {
-			throw new Error(
-				'cancelRenderOnLambda() has moved to `@remotion/lambda-client`. Please import it from there.',
-			);
-		}
-	: deprecatedCancelRenderOnLambda;
-
-/**
- * @deprecated Import this from `@remotion/lambda-client` instead
- */
 const getRenderProgress = NoReactInternals.ENABLE_V5_BREAKING_CHANGES
 	? () => {
 			throw new Error(
@@ -156,7 +143,6 @@ export {
 	deleteFunction,
 	deleteRender,
 	deleteSite,
-	cancelRenderOnLambda,
 	deployFunction,
 	deploySite,
 	deploySiteFromBundle,
@@ -183,7 +169,6 @@ export {
 export type {
 	AwsPartition,
 	AwsRegion,
-	CancelRenderOnLambdaInput,
 	CustomCredentials,
 	DeleteFunctionInput,
 	DeleteRenderInput,
