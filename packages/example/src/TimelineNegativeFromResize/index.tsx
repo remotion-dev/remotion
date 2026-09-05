2190f110df5eb26277556adea2d9977a55970cf8
Jonny Burger <jonathanburger11@gmail.com>
2026-09-05 14:48:22 +0200

    Remove example fixture and browser test changes

--- packages/example/src/TimelineNegativeFromResize/index.tsx
+++ packages/example/src/TimelineNegativeFromResize/index.tsx
@@ -5,15 +5,22 @@ import {AbsoluteFill, Sequence} from 'remotion';
 export const TimelineNegativeFromResize: React.FC = () => {
 	return (
 		<>
-			<AbsoluteFill style={{backgroundColor: '#202020'}} from={-8}>
+			<AbsoluteFill style={{backgroundColor: '#202020'}}>
 				<Sequence
 					name="Negative start"
-					durationInFrames={43}
+					from={-20}
+					durationInFrames={21}
 					freeze={20}
 					style={{backgroundColor: 'black'}}
-					from={-42}
 				>
 					<Starburst rays={16} colors={['#ffdd00', '#ff8800']} />
+				</Sequence>
+				<Sequence
+					name="Negative start without freeze"
+					from={-20}
+					durationInFrames={21}
+				>
+					<div />
 				</Sequence>
 			</AbsoluteFill>
 			<Sequence name="Zero start" durationInFrames={36}>
