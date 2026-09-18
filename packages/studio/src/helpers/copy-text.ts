ae357a052e6c47a789dfa49647ef4e11c99657a0
Jonny Burger <jonathanburger11@gmail.com>
2026-09-18 10:39:59 +0200

    Fix Studio copy to clipboard in Safari

--- packages/studio/src/helpers/copy-text.ts
+++ packages/studio/src/helpers/copy-text.ts
@@ -1,18 +1,3 @@
-export const copyText = (cmd: string) => {
-	const permissionName = 'clipboard-write' as PermissionName;
-
-	return new Promise<void>((resolve, reject) => {
-		navigator.permissions
-			.query({name: permissionName})
-			.then((result) => {
-				if (result.state === 'granted' || result.state === 'prompt') {
-					navigator.clipboard.writeText(cmd).then(resolve).catch(reject);
-				} else {
-					reject(new Error('Permission to copy not granted'));
-				}
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
+export const copyText = (cmd: string): Promise<void> => {
+	return navigator.clipboard.writeText(cmd);
 };
