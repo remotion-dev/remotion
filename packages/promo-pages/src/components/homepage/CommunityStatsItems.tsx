c9d21f481182e7eb3fb215273d5348cd6fa8a988
Jonny Burger <jonathanburger11@gmail.com>
2026-09-23 08:48:05 +0200

    Update GitHub star count to 60k

--- packages/promo-pages/src/components/homepage/CommunityStatsItems.tsx
+++ packages/promo-pages/src/components/homepage/CommunityStatsItems.tsx
@@ -176,7 +176,7 @@ export const GitHubStars: React.FC = () => {
 					width="45px"
 				/>
 				<StatItemContent
-					content="59K"
+					content="60K"
 					width="80px"
 					fontSize="2.5rem"
 					fontWeight="bold"
